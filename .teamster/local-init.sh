a65871fe16a5738e44eec01e14aecf3a357aeda0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-08 19:29:47 -0500

    linting

--- .teamster/local-init.sh
+++ .teamster/local-init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read -p "Enter instance name: " instance_name
-export INSTANCE_NAME=$instance_name
+read -r -p "Enter instance name: " instance_name
+export INSTANCE_NAME=${instance_name}
 
 # create local.env dir
-mkdir -p ./env/${INSTANCE_NAME}
+mkdir -p ./env/"${INSTANCE_NAME}"
 
 # create local.env
 tmpfile=$(mktemp)
@@ -13,12 +13,12 @@ cat ./env/common.env \
 	>"${tmpfile}"
 envsubst <"${tmpfile}" >./env/"${INSTANCE_NAME}"/local.env
 
-cp ./env/${INSTANCE_NAME}/local.env ./env/${INSTANCE_NAME}/prod.env
+cp ./env/"${INSTANCE_NAME}"/local.env ./env/"${INSTANCE_NAME}"/prod.env
 echo "
 # deployment
 DAGSTER_CLOUD_DEPLOYMENT=prod" >>./env/"${INSTANCE_NAME}"/prod.env
 
-cp ./env/${INSTANCE_NAME}/local.env ./env/${INSTANCE_NAME}/stg.env
+cp ./env/"${INSTANCE_NAME}"/local.env ./env/"${INSTANCE_NAME}"/stg.env
 echo "
 # deployment
 DAGSTER_CLOUD_DEPLOYMENT=stg" >>./env/"${INSTANCE_NAME}"/stg.env
