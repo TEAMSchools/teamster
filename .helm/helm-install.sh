d4021fdfa735394ebdb654126e1cc8d9d920273a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-11 15:45:34 -0500

    remove tmpl

--- .helm/helm-install.sh
+++ .helm/helm-install.sh
@@ -5,8 +5,6 @@ helm repo update
 
 helm show values dagster-cloud/dagster-cloud-agent >./.helm/values.yaml
 
-envsubst <./.helm/values-override.yaml.tmpl >./.helm/values-override.yaml
-
 helm upgrade \
 	--install user-cloud dagster-cloud/dagster-cloud-agent \
 	--namespace dagster-cloud \
