e2f45eba6646767761b1977a037115cca1ac2eba
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-12 10:22:42 -0500

    fix import

--- teamster/kippmiami/jobs/powerschool/db.py
+++ teamster/kippmiami/jobs/powerschool/db.py
@@ -2,7 +2,7 @@ from dagster import config_from_files
 from dagster_gcp.gcs import gcs_pickle_io_manager
 from dagster_gcp.gcs.resources import gcs_resource
 
-from teamster.common.graphs.powerschool.api import run_queries
+from teamster.common.graphs.powerschool.db import run_queries
 from teamster.common.resources.db import oracle
 from teamster.common.resources.google import gcs_file_manager
 
--- teamster/kippmiami/repository.py
+++ teamster/kippmiami/repository.py
@@ -4,15 +4,10 @@ from teamster.kippmiami.jobs import datagun as datagun_jobs
 from teamster.kippmiami.jobs.powerschool import db as ps_db_jobs
 from teamster.kippmiami.schedules import datagun as datagun_schedules
 
-# from teamster.kippmiami.schedules.powerschool import db as ps_db_schedules
-
 
 @repository
 def powerschool():
     jobs = [v for k, v in vars(ps_db_jobs).items() if k in ps_db_jobs.__all__]
-    # schedules = [
-    #   v for k, v in vars(ps_schedules).items() if k in ps_schedules.__all__
-    # ]
     schedules = []
     sensors = []
 
