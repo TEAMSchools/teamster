ce5a93941cc99368b1fe7d4bbe9fdd7b5a557f1b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-15 15:20:52 -0500

    fix

--- teamster/common/ops/datagun.py
+++ teamster/common/ops/datagun.py
@@ -10,7 +10,9 @@ from dagster import In, RetryPolicy, Tuple, op
     retry_policy=RetryPolicy(max_retries=2),
 )
 def load_destination(context, transformed):
-    dest_type = transformed[0]["type"]
+    dest_config = transformed[0]
+    
+    dest_type = dest_config["type"]
     if dest_type == "gsheet":
         file_stem, df_dict = transformed[1:]
         context.resources.destination.update_named_range(
@@ -18,7 +20,7 @@ def load_destination(context, transformed):
         )
     elif dest_type == "sftp":
         file_handle = transformed[1]
-        dest_path = dest_type.get("path")
+        dest_path = dest_config.get("path")
 
         sftp_conn = context.resources.destination.get_connection()
         file_name = pathlib.Path(file_handle.gcs_key).name
