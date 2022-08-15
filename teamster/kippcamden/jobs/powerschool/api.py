e0533d61a29d9b06d8b927bd0546d605f3cb88f8
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-15 16:12:59 -0500

    merge branches

---
+++ teamster/kippcamden/jobs/powerschool/api.py
@@ -0,0 +1,65 @@
+from dagster import config_from_files
+from dagster_gcp.gcs import gcs_pickle_io_manager
+from dagster_gcp.gcs.resources import gcs_resource
+
+from teamster.common.graphs.powerschool.api import run_queries
+from teamster.common.resources.google import gcs_file_manager
+from teamster.common.resources.powerschool import powerschool_api
+
+powerschool_api_extract = run_queries.to_job(
+    name="powerschool_extract",
+    resource_defs={
+        "powerschool": powerschool_api,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+    },
+    config=config_from_files(
+        [
+            "./teamster/common/config/powerschool/api/resource.yaml",
+            "./teamster/common/config/google/resource.yaml",
+            "./teamster/kippcamden/config/powerschool/api/query-default.yaml",
+        ]
+    ),
+)
+
+powerschool_api_resync = run_queries.to_job(
+    name="powerschool_resync",
+    resource_defs={
+        "powerschool": powerschool_api,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+    },
+    config=config_from_files(
+        [
+            "./teamster/common/config/powerschool/api/resource.yaml",
+            "./teamster/common/config/google/resource.yaml",
+            "./teamster/kippcamden/config/powerschool/api/query-resync.yaml",
+        ]
+    ),
+)
+
+powerschool_api_resync_assignmentscore = run_queries.to_job(
+    name="powerschool_resync_assignmentscore",
+    resource_defs={
+        "powerschool": powerschool_api,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+    },
+    config=config_from_files(
+        [
+            "./teamster/common/config/powerschool/api/resource.yaml",
+            "./teamster/common/config/google/resource.yaml",
+            # trunk-ignore(flake8/E501)
+            "./teamster/kippcamden/config/powerschool/api/query-resync-assignmentscore.yaml",
+        ]
+    ),
+)
+
+__all__ = [
+    "powerschool_api_extract",
+    "powerschool_api_resync",
+    "powerschool_api_resync_assignmentscore",
+]
