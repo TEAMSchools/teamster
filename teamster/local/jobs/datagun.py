5e823873f9d37cefcc450b01727426fd22bbd278
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-17 15:37:53 -0500

    fix datagun

--- teamster/local/jobs/datagun.py
+++ teamster/local/jobs/datagun.py
@@ -1,11 +1,17 @@
-from dagster import config_from_files
+from dagster import config_from_files, resource
 from dagster_gcp.gcs import gcs_pickle_io_manager
 from dagster_gcp.gcs.resources import gcs_resource
 from dagster_ssh import ssh_resource
 
 from teamster.core.graphs.datagun import run_queries
-from teamster.core.resources.db import mssql
 from teamster.core.resources.google import gcs_file_manager, google_sheets
+from teamster.core.resources.sqlalchemy import mssql
+
+
+@resource()
+def dummy_ssh_resource(context):
+    return object
+
 
 datagun_etl_sftp = run_queries.to_job(
     name="datagun_etl_sftp",
@@ -15,6 +21,7 @@ datagun_etl_sftp = run_queries.to_job(
         "io_manager": gcs_pickle_io_manager,
         "gcs": gcs_resource,
         "destination": ssh_resource,
+        "ssh": dummy_ssh_resource,
     },
     config=config_from_files(
         [
@@ -33,6 +40,7 @@ datagun_etl_gsheets = run_queries.to_job(
         "io_manager": gcs_pickle_io_manager,
         "gcs": gcs_resource,
         "destination": google_sheets,
+        "ssh": dummy_ssh_resource,
     },
     config=config_from_files(
         [
