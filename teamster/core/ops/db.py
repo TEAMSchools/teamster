bc9c79ed95b48377c4f7b71789cad4dfc3f4f05d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-18 13:46:46 -0500

    fix cols

--- teamster/core/ops/db.py
+++ teamster/core/ops/db.py
@@ -5,7 +5,7 @@ import re
 
 import pandas as pd
 from dagster import Dict, DynamicOut, DynamicOutput, In, List, Out, Output, Tuple, op
-from sqlalchemy import Table, column, select, text
+from sqlalchemy import column, select, table, text
 
 from teamster.core.config.db import QUERY_CONFIG, SSH_TUNNEL_CONFIG
 from teamster.core.utils import TODAY, CustomJSONEncoder
@@ -30,19 +30,20 @@ def compose_queries(context):
             with pathlib.Path(value).absolute().open() as f:
                 query = text(f.read())
         elif query_type == "schema":
-            table_params = value["table"]
-            column_names = value["columns"]
             where_clause = value.get("where", "")
 
-            query_table = Table(*[column(c) for c in column_names], **table_params)
-            query = select(query_table).where(text(where_clause))
-
-            file_config["table_name"] = table_params["name"]
+            file_config["table_name"] = value["table"]["name"]
             file_config["query_where"] = re.sub(
                 r"[^a-zA-Z0-9=;,]",
                 "",
                 where_clause.replace("AND", ";").replace(" OR ", ","),
             )
+
+            query_table = table(**value["table"])
+            for col in value["columns"]:
+                query_table.append_column(column(col))
+
+            query = select(query_table).where(text(where_clause))
 
         file_suffix = file_config.get("suffix")
         if file_suffix is None:
