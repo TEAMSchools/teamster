633e172df9eac6f13fa588689b3381ab3bd47979
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-24 10:40:21 -0500

    update sql

--- teamster/local/config/datagun/sql/egencia-users.sql
+++ teamster/local/config/datagun/sql/egencia-users.sql
@@ -1,10 +1,12 @@
-SELECT [Username],
-      [Email],
-      [Single Sign On ID],
-      [Employee ID],
-      [Status],
-      [First name],
-      [Last name],
-      [Role],
-      [Traveler Group]
-FROM gabby.extracts.egencia_users;
+SELECT
+  [ Username ],
+  [ Email ],
+  [ Single Sign On ID ],
+  [ Employee ID ],
+  [ Status ],
+  [ First name ],
+  [ Last name ],
+  [ Role ],
+  [ Traveler Group ]
+FROM
+  gabby.extracts.egencia_users;
