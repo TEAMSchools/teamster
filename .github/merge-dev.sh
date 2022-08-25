e7e15762c577a07bffee9d091b4bbe3f1959ce18
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-25 15:09:35 -0500

    rename

--- .github/merge-dev.sh
+++ .github/merge-dev.sh
@@ -3,7 +3,7 @@
 for BRANCH in ./.git/refs/remotes/origin/kipp*; do
 	branch_name=$(basename -- "${BRANCH}")
 
-	git switch merge-dev-"${branch_name}"
+	git switch stg-"${branch_name}"
 	git merge dev
 done
 
