b5942697d522f6a5ddbe9f0d773e09d27870d982
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-25 11:08:14 -0500

    merge dev

--- teamster/local/repository.py
+++ teamster/local/repository.py
@@ -1,13 +1,13 @@
 from dagster import repository
 
-from teamster.local.jobs import datagun as datagun_jobs
-from teamster.local.jobs.powerschool import db as ps_db_jobs
-from teamster.local.schedules import datagun as datagun_schedules
+from teamster.core.jobs import datagun as datagun_jobs_core
+from teamster.core.jobs.powerschool import db as psdb_jobs_core
+from teamster.local.schedules import datagun as datagun_sched_local
 
 
 @repository
 def powerschool():
-    jobs = [v for k, v in vars(ps_db_jobs).items() if k in ps_db_jobs.__all__]
+    jobs = [v for k, v in vars(psdb_jobs_core).items() if k in psdb_jobs_core.__all__]
     schedules = []
     sensors = []
 
@@ -16,9 +16,13 @@ def powerschool():
 
 @repository
 def datagun():
-    jobs = [v for k, v in vars(datagun_jobs).items() if k in datagun_jobs.__all__]
+    jobs = [
+        v for k, v in vars(datagun_jobs_core).items() if k in datagun_jobs_core.__all__
+    ]
     schedules = [
-        v for k, v in vars(datagun_schedules).items() if k in datagun_schedules.__all__
+        v
+        for k, v in vars(datagun_sched_local).items()
+        if k in datagun_sched_local.__all__
     ]
 
     return jobs + schedules
