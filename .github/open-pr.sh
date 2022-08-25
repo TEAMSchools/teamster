8600bc9ade7b5aefc702dd286522c6ea259e68e8
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-25 15:13:29 -0500

    pr script

---
+++ .github/open-pr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for BRANCH in ./.git/refs/remotes/origin/stg-*; do
+	branch_name=$(basename -- "${BRANCH}")
+
+	git switch "${branch_name}"
+    gh pr create --base "${branch_name#*-}"
+done
+
+git switch dev
