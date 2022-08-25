3d2bc7f8757e3f7a3a3b6924687ff97ffe8637bf
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-25 15:23:19 -0500

    fmt

--- .github/open-pr.sh
+++ .github/open-pr.sh
@@ -4,7 +4,7 @@ for BRANCH in ./.git/refs/remotes/origin/stg-*; do
 	branch_name=$(basename -- "${BRANCH}")
 
 	git switch "${branch_name}"
-    gh pr create --base "${branch_name#*-}"
+	gh pr create --base "${branch_name#*-}"
 done
 
 git switch dev
