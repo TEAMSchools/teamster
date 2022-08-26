a14d9196179ee85e47a6831adf626362a43aab81
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-26 10:33:41 -0500

    scripts

--- .github/merge-dev.sh
+++ .github/merge-dev.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-for BRANCH in ./.git/refs/remotes/origin/kipp*; do
-	branch_name=$(basename -- "${BRANCH}")
+if [[ -z ${1} ]]; then
+	echo "Usage: ${0} <deployment>"
+	exit 1
+else
+	for BRANCH in ./.git/refs/remotes/origin/kipp*; do
+		branch_name=$(basename -- "${BRANCH}")
 
-	git switch stg-"${branch_name}"
-	git merge dev
-done
+		git switch "${1}"-"${branch_name}"
+		git merge dev
+	done
 
-git switch dev
+	git switch dev
+fi
