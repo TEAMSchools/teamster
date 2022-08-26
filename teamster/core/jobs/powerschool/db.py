1790d90dd117c872406e9b1a364b65d3d523b33e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-08-26 15:39:12 -0500

    merge core from branch

--- teamster/core/jobs/powerschool/db.py
+++ teamster/core/jobs/powerschool/db.py
@@ -4,7 +4,7 @@ from dagster_gcp.gcs.resources import gcs_resource
 from dagster_k8s import k8s_job_executor
 from dagster_ssh import ssh_resource
 
-from teamster.core.graphs.powerschool.db import run_queries
+from teamster.core.graphs.powerschool.db import resync, run_queries
 from teamster.core.resources.google import gcs_file_manager
 from teamster.core.resources.sqlalchemy import oracle
 
@@ -28,6 +28,30 @@ powerschool_db_extract = run_queries.to_job(
     ),
 )
 
-__all__ = [
-    "powerschool_db_extract",
-]
+powerschool_db_resync = resync.to_job(
+    name="powerschool_db_resync",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": oracle,
+        "ssh": ssh_resource,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+    },
+    config=config_from_files(
+        [
+            "./teamster/core/config/google/resource.yaml",
+            "./teamster/core/config/powerschool/db/resource.yaml",
+            "./teamster/local/config/powerschool/db/resource.yaml",
+            "./teamster/local/config/powerschool/db/query-resync.yaml",
+            "./teamster/local/config/powerschool/db/query-resync-full.yaml",
+            "./teamster/local/config/powerschool/db/query-resync-log.yaml",
+            "./teamster/local/config/powerschool/db/query-resync-attendance.yaml",
+            "./teamster/local/config/powerschool/db/query-resync-storedgrades.yaml",
+            "./teamster/local/config/powerschool/db/query-resync-pgfinalgrades.yaml",
+            "./teamster/local/config/powerschool/db/query-resync-assignmentscore.yaml",
+        ]
+    ),
+)
+
+__all__ = ["powerschool_db_extract", "powerschool_db_resync"]
