ef8c6aa49686a6ace718718388af0d610fc9e300
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-09 10:32:15 -0500

    remove try block

--- teamster/core/ops/db.py
+++ teamster/core/ops/db.py
@@ -3,7 +3,6 @@ import re
 
 from dagster import (
     Any,
-    DagsterExecutionInterruptedError,
     Dict,
     DynamicOut,
     DynamicOutput,
@@ -11,14 +10,13 @@ from dagster import (
     List,
     Out,
     Output,
-    RetryRequested,
     Tuple,
     op,
 )
 from sqlalchemy import literal_column, select, table, text
 
 from teamster.core.config.db.schema import QUERY_CONFIG, SSH_TUNNEL_CONFIG
-from teamster.core.utils.functions import get_last_schedule_run  # retry_on_exception
+from teamster.core.utils.functions import get_last_schedule_run
 from teamster.core.utils.variables import TODAY
 
 
@@ -27,49 +25,43 @@ from teamster.core.utils.variables import TODAY
     out={"dynamic_query": DynamicOut(dagster_type=Tuple)},
     tags={"dagster/priority": 1},
 )
-# @retry_on_exception
 def compose_queries(context):
-    try:
-        dest_config = context.op_config["destination"]
-        queries = context.op_config["queries"]
+    dest_config = context.op_config["destination"]
+    queries = context.op_config["queries"]
 
-        for i, q in enumerate(queries):
-            [(query_type, value)] = q["sql"].items()
+    for i, q in enumerate(queries):
+        [(query_type, value)] = q["sql"].items()
 
-            if query_type == "text":
-                table_name = "query"
-                query = text(value)
-            elif query_type == "file":
-                query_file = pathlib.Path(value).absolute()
-                table_name = query_file.stem
-                with query_file.open(mode="r") as f:
-                    query = text(f.read())
-            elif query_type == "schema":
-                table_name = value["table"]["name"]
-                where_fmt = value.get("where", "").format(
-                    today=TODAY.date().isoformat(),
-                    last_run=get_last_schedule_run(context=context)
-                    or TODAY.date().isoformat(),
-                )
-                query = (
-                    select(*[literal_column(col) for col in value["select"]])
-                    .select_from(table(**value["table"]))
-                    .where(text(where_fmt))
-                )
-
-            yield DynamicOutput(
-                value=(query, q.get("file", {}), dest_config),
-                output_name="dynamic_query",
-                mapping_key=re.sub(
-                    r"[^A-Za-z0-9_]+",
-                    "",
-                    f"{(q.get('file', {}).get('stem') or table_name)}_{i}",
-                ),
+        if query_type == "text":
+            table_name = "query"
+            query = text(value)
+        elif query_type == "file":
+            query_file = pathlib.Path(value).absolute()
+            table_name = query_file.stem
+            with query_file.open(mode="r") as f:
+                query = text(f.read())
+        elif query_type == "schema":
+            table_name = value["table"]["name"]
+            where_fmt = value.get("where", "").format(
+                today=TODAY.date().isoformat(),
+                last_run=get_last_schedule_run(context=context)
+                or TODAY.date().isoformat(),
             )
-    except DagsterExecutionInterruptedError as e:
-        raise RetryRequested() from e
-    except Exception as e:
-        raise RetryRequested(max_retries=2) from e
+            query = (
+                select(*[literal_column(col) for col in value["select"]])
+                .select_from(table(**value["table"]))
+                .where(text(where_fmt))
+            )
+
+        yield DynamicOutput(
+            value=(query, q.get("file", {}), dest_config),
+            output_name="dynamic_query",
+            mapping_key=re.sub(
+                r"[^A-Za-z0-9_]+",
+                "",
+                f"{(q.get('file', {}).get('stem') or table_name)}_{i}",
+            ),
+        )
 
 
 @op(
@@ -83,48 +75,42 @@ def compose_queries(context):
     required_resource_keys={"db", "ssh", "file_manager"},
     tags={"dagster/priority": 2},
 )
-# @retry_on_exception
 def extract(context, dynamic_query):
-    try:
-        mapping_key = context.get_mapping_key()
-        table_name = mapping_key[: mapping_key.rfind("_")]
+    mapping_key = context.get_mapping_key()
+    table_name = mapping_key[: mapping_key.rfind("_")]
 
-        query, file_config, dest_config = dynamic_query
+    query, file_config, dest_config = dynamic_query
 
-        if hasattr(context.resources.ssh, "get_tunnel"):
-            context.log.info("Starting SSH tunnel.")
-            ssh_tunnel = context.resources.ssh.get_tunnel(**context.op_config)
-            ssh_tunnel.start()
-        else:
-            ssh_tunnel = None
+    if hasattr(context.resources.ssh, "get_tunnel"):
+        context.log.info("Starting SSH tunnel.")
+        ssh_tunnel = context.resources.ssh.get_tunnel(**context.op_config)
+        ssh_tunnel.start()
+    else:
+        ssh_tunnel = None
 
+    if dest_config["type"] == "fs":
+        data = context.resources.db.execute_query(query, output_fmt="files")
+    else:
+        data = context.resources.db.execute_query(query)
+
+    if ssh_tunnel is not None:
+        context.log.info("Stopping SSH tunnel.")
+        ssh_tunnel.stop()
+
+    if data:
         if dest_config["type"] == "fs":
-            data = context.resources.db.execute_query(query, output_fmt="files")
+            file_handles = []
+            for fp in data:
+                with fp.open(mode="rb") as f:
+                    file_handle = context.resources.file_manager.write(
+                        file_obj=f, key=f"{table_name}/{fp.stem}", ext=fp.suffix
+                    )
+
+                context.log.info(f"Saved to {file_handle.path_desc}.")
+                file_handles.append(file_handle)
+
+            yield Output(value=file_handles, output_name="data")
         else:
-            data = context.resources.db.execute_query(query)
-
-        if ssh_tunnel is not None:
-            context.log.info("Stopping SSH tunnel.")
-            ssh_tunnel.stop()
-
-        if data:
-            if dest_config["type"] == "fs":
-                file_handles = []
-                for fp in data:
-                    with fp.open(mode="rb") as f:
-                        file_handle = context.resources.file_manager.write(
-                            file_obj=f, key=f"{table_name}/{fp.stem}", ext=fp.suffix
-                        )
-
-                    context.log.info(f"Saved to {file_handle.path_desc}.")
-                    file_handles.append(file_handle)
-
-                yield Output(value=file_handles, output_name="data")
-            else:
-                yield Output(value=file_config, output_name="file_config")
-                yield Output(value=dest_config, output_name="dest_config")
-                yield Output(value=data, output_name="data")
-    except DagsterExecutionInterruptedError as e:
-        raise RetryRequested() from e
-    except Exception as e:
-        raise RetryRequested(max_retries=2) from e
+            yield Output(value=file_config, output_name="file_config")
+            yield Output(value=dest_config, output_name="dest_config")
+            yield Output(value=data, output_name="data")
