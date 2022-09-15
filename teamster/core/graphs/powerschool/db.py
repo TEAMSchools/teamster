0cf3c694dc8784d75e87aa199f34c5bd10ab67ca
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-15 15:49:34 -0500

    new graph generation

--- teamster/core/graphs/powerschool/db.py
+++ teamster/core/graphs/powerschool/db.py
@@ -1,41 +1,53 @@
-from dagster import graph
+import pathlib
 
-from teamster.core.ops.db import compose_queries, extract
+from dagster import config_mapping, graph
+from sqlalchemy import literal_column, select, table, text
+
+from teamster.core.config.powerschool.db.schema import QUERY_CONFIG
+from teamster.core.ops.powerschool.db import extract
+from teamster.core.utils.variables import TODAY
+
+
+@config_mapping(config_schema=QUERY_CONFIG)
+def construct_query_config(config):
+    query_config = config["query"]
+    destination_config = config["destination"]
+
+    [(query_type, output_fmt, value)] = query_config["sql"].items()
+    if query_type == "text":
+        table_name = "query"
+        query = text(value)
+    elif query_type == "file":
+        query_file = pathlib.Path(value).absolute()
+        table_name = query_file.stem
+        with query_file.open(mode="r") as f:
+            query = text(f.read())
+    elif query_type == "schema":
+        table_name = value["table"]["name"]
+        where_fmt = value.get("where", "").format(today=TODAY.date().isoformat())
+        query = (
+            select(*[literal_column(col) for col in value["select"]])
+            .select_from(table(**value["table"]))
+            .where(text(where_fmt))
+        )
+
+    return {
+        "extract": {
+            "config": {
+                "query": query,
+                "table_name": table_name,
+                "output_fmt": output_fmt,
+                "destination_type": destination_config["type"],
+            }
+        }
+    }
+
+
+@graph(config=construct_query_config)
+def query():
+    extract()
 
 
 @graph
-def execute_query(dynamic_query):
-    # trunk-ignore(flake8/F841)
-    data = extract(dynamic_query=dynamic_query)
-
-
-@graph
-def run_queries():
-    # parse queries from run config file
-    dynamic_query = compose_queries()
-
-    # execute composed queries and transform to configured file type
-    dynamic_query.map(execute_query)
-
-
-@graph
-def resync():
-    # log
-    dynamic_query = compose_queries()
-    dynamic_query.map(execute_query)
-
-    # attendance
-    dynamic_query = compose_queries()
-    dynamic_query.map(execute_query)
-
-    # storedgrades
-    dynamic_query = compose_queries()
-    dynamic_query.map(execute_query)
-
-    # pgfinalgrades
-    dynamic_query = compose_queries()
-    dynamic_query.map(execute_query)
-
-    # assignmentscore
-    dynamic_query = compose_queries()
-    dynamic_query.map(execute_query)
+def generate_queries():
+    pass
