ea423733e17e2f0364dbc3f9bd2aa2a4ae0ddbbc
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-16 10:01:04 -0500

    broken ps jobs

--- teamster/local/repository.py
+++ teamster/local/repository.py
@@ -1,17 +1,17 @@
 from dagster import repository
 
 from teamster.core.jobs import datagun as datagun_jobs_core
-from teamster.core.jobs.powerschool import db as psdb_jobs_core
-from teamster.local.schedules import datagun as datagun_sched_local
+from teamster.local.schedules import datagun as datagun_schedules
 
+# from teamster.core.jobs.powerschool import db as psdb_jobs_core
 
-@repository
-def powerschool():
-    jobs = [v for k, v in vars(psdb_jobs_core).items() if k in psdb_jobs_core.__all__]
-    schedules = []
-    sensors = []
+# @repository
+# def powerschool():
+#     jobs = [v for k, v in vars(psdb_jobs_core).items() if k in psdb_jobs_core.__all__]
+#     schedules = []
+#     sensors = []
 
-    return jobs + schedules + sensors
+#     return jobs + schedules + sensors
 
 
 @repository
@@ -20,12 +20,13 @@ def datagun():
         v for k, v in vars(datagun_jobs_core).items() if k in datagun_jobs_core.__all__
     ]
     schedules = [
-        v
-        for k, v in vars(datagun_sched_local).items()
-        if k in datagun_sched_local.__all__
+        v for k, v in vars(datagun_schedules).items() if k in datagun_schedules.__all__
     ]
 
     return jobs + schedules
 
 
-__all__ = ["powerschool", "datagun"]
+__all__ = [
+    # "powerschool",
+    "datagun"
+]
