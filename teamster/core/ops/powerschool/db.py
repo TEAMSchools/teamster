88d2bea5e3ba862c09d06e514ef4ddece4318051
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-21 14:06:31 -0500

    trblsht

--- teamster/core/ops/powerschool/db.py
+++ teamster/core/ops/powerschool/db.py
@@ -1,14 +1,4 @@
-from dagster import (
-    Any,
-    DagsterExecutionInterruptedError,
-    Int,
-    List,
-    Out,
-    Output,
-    RetryRequested,
-    String,
-    op,
-)
+from dagster import Any, Int, List, Out, Output, String, op
 
 from teamster.core.utils.functions import get_last_schedule_run
 from teamster.core.utils.variables import TODAY
@@ -26,56 +16,51 @@ from teamster.core.utils.variables import TODAY
     tags={"dagster/priority": 2},
 )
 def extract(context):
-    try:
-        file_manager_key = context.solid_handle.path[1]
+    file_manager_key = str(context.solid_handle.path)
 
-        if context.op_config[
-            "destination_type"
-        ] == "file" and not context.resources.file_manager.blob_exists(
-            key=file_manager_key
-        ):
-            context.log.info(f"Running initial sync of {file_manager_key}")
-            context.op_config["query"].whereclause.text = ""
-        else:
-            context.op_config["query"].whereclause.text.format(
-                today=TODAY.date().isoformat(), last_run=get_last_schedule_run(context)
-            )
-
-        if context.resources.ssh.tunnel:
-            context.log.info("Starting SSH tunnel")
-            ssh_tunnel = context.resources.ssh.get_tunnel()
-            ssh_tunnel.start()
-        else:
-            ssh_tunnel = None
-
-        data = context.resources.db.execute_query(
-            query=context.op_config["query"],
-            partition_size=context.op_config["partition_size"],
-            output_fmt=context.op_config["output_fmt"],
+    if context.op_config[
+        "destination_type"
+    ] == "file" and not context.resources.file_manager.blob_exists(
+        key=file_manager_key
+    ):
+        context.log.info(f"Running initial sync of {file_manager_key}")
+        context.op_config["query"].whereclause.text = ""
+    else:
+        context.op_config["query"].whereclause.text.format(
+            today=TODAY.date().isoformat(), last_run=get_last_schedule_run(context)
         )
 
-        if ssh_tunnel is not None:
-            context.log.info("Stopping SSH tunnel")
-            ssh_tunnel.stop()
+    if context.resources.ssh.tunnel:
+        context.log.info("Starting SSH tunnel")
+        ssh_tunnel = context.resources.ssh.get_tunnel()
+        ssh_tunnel.start()
+    else:
+        ssh_tunnel = None
 
-        if data:
-            if context.op_config["destination_type"] == "file":
-                file_handles = []
-                for fp in data:
-                    with fp.open(mode="rb") as f:
-                        file_handle = context.resources.file_manager.write(
-                            file_obj=f,
-                            key=f"{file_manager_key}/{fp.stem}",
-                            ext=fp.suffix,
-                        )
+    data = context.resources.db.execute_query(
+        query=context.op_config["query"],
+        partition_size=context.op_config["partition_size"],
+        output_fmt=context.op_config["output_fmt"],
+    )
 
-                    context.log.info(f"Saved to {file_handle.path_desc}")
-                    file_handles.append(file_handle)
+    if ssh_tunnel is not None:
+        context.log.info("Stopping SSH tunnel")
+        ssh_tunnel.stop()
 
-                yield Output(value=file_handles, output_name="data")
-            else:
-                yield Output(value=data, output_name="data")
-    except DagsterExecutionInterruptedError as e:
-        raise RetryRequested() from e
-    except Exception as e:
-        raise RetryRequested(max_retries=2) from e
+    if data:
+        if context.op_config["destination_type"] == "file":
+            file_handles = []
+            for fp in data:
+                with fp.open(mode="rb") as f:
+                    file_handle = context.resources.file_manager.write(
+                        file_obj=f,
+                        key=f"{file_manager_key}/{fp.stem}",
+                        ext=fp.suffix,
+                    )
+
+                context.log.info(f"Saved to {file_handle.path_desc}")
+                file_handles.append(file_handle)
+
+            yield Output(value=file_handles, output_name="data")
+        else:
+            yield Output(value=data, output_name="data")
