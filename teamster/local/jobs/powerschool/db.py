592bdee933c92fcbdaa6c03c2551364667236adb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-29 17:33:11 -0500

    refactor ext runs

--- teamster/local/jobs/powerschool/db.py
+++ teamster/local/jobs/powerschool/db.py
@@ -2,9 +2,9 @@ from dagster import config_from_files
 from dagster_gcp.gcs import gcs_file_manager, gcs_pickle_io_manager, gcs_resource
 from dagster_k8s import k8s_job_executor
 
-from teamster.core.graphs.powerschool.db import sync_extensions
 from teamster.core.resources.sqlalchemy import oracle
 from teamster.core.resources.ssh import ssh_resource
+from teamster.local.graphs.powerschool.db import sync_extensions
 
 powerschool_db_sync_extensions = sync_extensions.to_job(
     name="powerschool_db_sync_extensions",
@@ -20,7 +20,7 @@ powerschool_db_sync_extensions = sync_extensions.to_job(
         [
             "./teamster/core/config/google/resource.yaml",
             "./teamster/core/config/powerschool/db/resource.yaml",
-            "./teamster/core/config/powerschool/db/query-sync-extensions.yaml",
+            "./teamster/local/config/powerschool/db/query-sync-extensions.yaml",
         ]
     ),
 )
@@ -39,7 +39,7 @@ powerschool_db_resync_extensions = sync_extensions.to_job(
         [
             "./teamster/core/config/google/resource.yaml",
             "./teamster/core/config/powerschool/db/resource.yaml",
-            "./teamster/core/config/powerschool/db/query-resync-extensions.yaml",
+            "./teamster/local/config/powerschool/db/query-resync-extensions.yaml",
         ]
     ),
 )
