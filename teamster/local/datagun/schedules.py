ef7fc7fa5621076d6dc2f694596daafe40c2f936
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-30 15:32:41 -0500

    fix naming

--- teamster/local/datagun/schedules.py
+++ teamster/local/datagun/schedules.py
@@ -7,14 +7,12 @@ from teamster.local.datagun.jobs import datagun_nps, datagun_ps_autocomm
 LOCAL_TIME_ZONE = os.getenv("LOCAL_TIME_ZONE")
 
 datagun_ps_autocomm = ScheduleDefinition(
-    name="datagun_powerschool",
     job=datagun_ps_autocomm,
     cron_schedule="15 2 * * *",
     execution_timezone=LOCAL_TIME_ZONE,
 )
 
 datagun_nps = ScheduleDefinition(
-    name="nps_datagun",
     job=datagun_nps,
     cron_schedule="0 0 * * *",
     execution_timezone=LOCAL_TIME_ZONE,
