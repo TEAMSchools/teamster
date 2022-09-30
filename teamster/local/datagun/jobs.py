092400db248559cf896c0d64f2ac32be0d4814ea
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-30 16:16:20 -0500

    local

---
+++ teamster/local/datagun/jobs.py
@@ -0,0 +1,48 @@
+from dagster import config_from_files
+from dagster_gcp.gcs import gcs_pickle_io_manager, gcs_resource
+from dagster_k8s import k8s_job_executor
+from dagster_ssh import ssh_resource
+
+from teamster.core.resources.google import gcs_file_manager
+from teamster.core.resources.sqlalchemy import mssql
+from teamster.local.datagun.graphs import cpn, powerschool_autocomm
+
+datagun_ps_autocomm = powerschool_autocomm.to_job(
+    name="datagun_ps_autocomm",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-powerschool.yaml",
+        ]
+    ),
+)
+
+datagun_cpn = cpn.to_job(
+    name="datagun_cpn",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-cpn.yaml",
+        ]
+    ),
+)
+
+__all__ = ["datagun_ps_autocomm", "datagun_cpn"]
