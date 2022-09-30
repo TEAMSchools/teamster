373357378e7c624ccdc0d1055af701747349f794
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-09-30 16:56:06 -0500

    some jobs

--- teamster/local/datagun/jobs.py
+++ teamster/local/datagun/jobs.py
@@ -27,8 +27,8 @@ datagun_ps_autocomm = powerschool_autocomm.to_job(
     ),
 )
 
-datagun_nps = etl_sftp.to_job(
-    name="datagun_nps",
+datagun_adp = etl_sftp.to_job(
+    name="datagun_adp",
     executor_def=k8s_job_executor,
     resource_defs={
         "db": mssql,
@@ -41,9 +41,192 @@ datagun_nps = etl_sftp.to_job(
         [
             "teamster/core/resources/config/google.yaml",
             "teamster/core/datagun/config/resource.yaml",
-            "teamster/local/datagun/config/query-nps.yaml",
+            "teamster/local/datagun/config/query-adp.yaml",
         ]
     ),
 )
 
-__all__ = ["datagun_ps_autocomm", "datagun_nps"]
+datagun_alchemer = etl_sftp.to_job(
+    name="datagun_alchemer",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-alchemer.yaml",
+        ]
+    ),
+)
+
+datagun_blissbook = etl_sftp.to_job(
+    name="datagun_blissbook",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-blissbook.yaml",
+        ]
+    ),
+)
+
+datagun_coupa = etl_sftp.to_job(
+    name="datagun_coupa",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-coupa.yaml",
+        ]
+    ),
+)
+
+datagun_egencia = etl_sftp.to_job(
+    name="datagun_egencia",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-egencia.yaml",
+        ]
+    ),
+)
+
+datagun_fpodms = etl_sftp.to_job(
+    name="datagun_fpodms",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-fpodms.yaml",
+        ]
+    ),
+)
+
+datagun_idauto = etl_sftp.to_job(
+    name="datagun_idauto",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-idauto.yaml",
+        ]
+    ),
+)
+
+datagun_littlesis = etl_sftp.to_job(
+    name="datagun_littlesis",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-littlesis.yaml",
+        ]
+    ),
+)
+
+datagun_njdoe = etl_sftp.to_job(
+    name="datagun_njdoe",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-njdoe.yaml",
+        ]
+    ),
+)
+
+datagun_whetstone = etl_sftp.to_job(
+    name="datagun_whetstone",
+    executor_def=k8s_job_executor,
+    resource_defs={
+        "db": mssql,
+        "file_manager": gcs_file_manager,
+        "io_manager": gcs_pickle_io_manager,
+        "gcs": gcs_resource,
+        "sftp": ssh_resource,
+    },
+    config=config_from_files(
+        [
+            "teamster/core/resources/config/google.yaml",
+            "teamster/core/datagun/config/resource.yaml",
+            "teamster/local/datagun/config/query-whetstone.yaml",
+        ]
+    ),
+)
+
+__all__ = [
+    "datagun_ps_autocomm",
+    "datagun_adp",
+    "datagun_alchemer",
+    "datagun_blissbook",
+    "datagun_coupa",
+    "datagun_egencia",
+    "datagun_fpodms",
+    "datagun_idauto",
+    "datagun_littlesis",
+    "datagun_njdoe",
+    "datagun_whetstone",
+]
