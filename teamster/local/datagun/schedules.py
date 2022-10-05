3ff6b690c0a3f2b338c47fb9a4d52eaadc2d3da9
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-10-05 13:26:32 -0500

    refactor tz

--- teamster/local/datagun/schedules.py
+++ teamster/local/datagun/schedules.py
@@ -2,6 +2,7 @@ import os
 
 from dagster import ScheduleDefinition
 
+from teamster.core.utils.variables import LOCAL_TIME_ZONE
 from teamster.local.datagun.jobs import (
     datagun_adp,
     datagun_blissbook,
@@ -21,120 +22,118 @@ from teamster.local.datagun.jobs import (
     datagun_whetstone,
 )
 
-LOCAL_TIME_ZONE = os.getenv("LOCAL_TIME_ZONE")
-
-datagun_gsheets = ScheduleDefinition(
+datagun_gsheets_schedule_schedule = ScheduleDefinition(
     job=datagun_gsheets,
     cron_schedule="0 6 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_deanslist = ScheduleDefinition(
+datagun_deanslist_schedule = ScheduleDefinition(
     job=datagun_deanslist,
     cron_schedule="25 1 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_illuminate = ScheduleDefinition(
+datagun_illuminate_schedule = ScheduleDefinition(
     job=datagun_illuminate,
     cron_schedule="30 2 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_clever = ScheduleDefinition(
+datagun_clever_schedule = ScheduleDefinition(
     job=datagun_clever,
     cron_schedule="@hourly",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_razkids = ScheduleDefinition(
+datagun_razkids_schedule = ScheduleDefinition(
     job=datagun_razkids,
     cron_schedule="45 2 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_read180 = ScheduleDefinition(
+datagun_read180_schedule = ScheduleDefinition(
     job=datagun_read180,
     cron_schedule="	15 3 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_gam = ScheduleDefinition(
+datagun_gam_schedule = ScheduleDefinition(
     job=datagun_gam,
     cron_schedule="0 2 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_littlesis = ScheduleDefinition(
+datagun_littlesis_schedule = ScheduleDefinition(
     job=datagun_littlesis,
     cron_schedule="30 2 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
 
-datagun_blissbook = ScheduleDefinition(
+datagun_blissbook_schedule = ScheduleDefinition(
     job=datagun_blissbook,
     cron_schedule="10 5 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_coupa = ScheduleDefinition(
+datagun_coupa_schedule = ScheduleDefinition(
     job=datagun_coupa,
     cron_schedule="20 2 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_egencia = ScheduleDefinition(
+datagun_egencia_schedule = ScheduleDefinition(
     job=datagun_egencia,
     cron_schedule="20 2 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_adp = ScheduleDefinition(
+datagun_adp_schedule = ScheduleDefinition(
     job=datagun_adp,
     cron_schedule="10 2 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_fpodms = ScheduleDefinition(
+datagun_fpodms_schedule = ScheduleDefinition(
     job=datagun_fpodms,
     cron_schedule="40 2 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_njdoe = ScheduleDefinition(
+datagun_njdoe_schedule = ScheduleDefinition(
     job=datagun_njdoe,
     cron_schedule="55 11 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_whetstone = ScheduleDefinition(
+datagun_whetstone_schedule = ScheduleDefinition(
     job=datagun_whetstone,
     cron_schedule="55 3 * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
-datagun_idauto = ScheduleDefinition(
+datagun_idauto_schedule = ScheduleDefinition(
     job=datagun_idauto,
     cron_schedule="55 * * * *",
-    execution_timezone=LOCAL_TIME_ZONE,
+    execution_timezone=str(LOCAL_TIME_ZONE),
 )
 
 __all__ = [
-    "datagun_deanslist",
-    "datagun_illuminate",
-    "datagun_gsheets",
-    "datagun_clever",
-    "datagun_razkids",
-    "datagun_read180",
-    "datagun_littlesis",
-    "datagun_gam",
-    "datagun_blissbook",
-    "datagun_coupa",
-    "datagun_egencia",
-    "datagun_adp",
-    "datagun_fpodms",
-    "datagun_njdoe",
-    "datagun_whetstone",
-    "datagun_idauto",
+    "datagun_deanslist_schedule",
+    "datagun_illuminate_schedule",
+    "datagun_gsheets_schedule",
+    "datagun_clever_schedule",
+    "datagun_razkids_schedule",
+    "datagun_read180_schedule",
+    "datagun_littlesis_schedule",
+    "datagun_gam_schedule",
+    "datagun_blissbook_schedule",
+    "datagun_coupa_schedule",
+    "datagun_egencia_schedule",
+    "datagun_adp_schedule",
+    "datagun_fpodms_schedule",
+    "datagun_njdoe_schedule",
+    "datagun_whetstone_schedule",
+    "datagun_idauto_schedule",
 ]
