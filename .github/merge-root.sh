932dcdac2f120be4e859e3aa39e68101f41281d2
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-10-11 11:17:26 -0500

    new merge script

---
+++ .github/merge-root.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+for BRANCH in ./.git/refs/remotes/origin/kipp*; do
+	branch_name=$(basename -- "${BRANCH}")
+
+	git switch "${branch_name}"
+	git pull
+
+	git merge --no-ff --no-commit dev
+	git reset HEAD teamster/
+	git checkout -- teamster/
+	git commit -m "merged dev root"
+
+	while true; do
+		read -rp "Push (y/N)? " yn
+		case ${yn} in
+		[Yy]*)
+			git push
+			break
+			;;
+		[Nn]*) break ;;
+		*) break ;;
+		esac
+	done
+done
+
+git switch dev
