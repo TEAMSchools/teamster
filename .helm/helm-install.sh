49936eff1e4f9a7b65a9ab05ce67077cbefe3cd2
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-10-24 11:32:59 -0500

    refactor deployment name

--- .helm/helm-install.sh
+++ .helm/helm-install.sh
@@ -8,5 +8,4 @@ helm show values dagster-cloud/dagster-cloud-agent >.helm/values.yaml
 helm upgrade \
   --install user-cloud dagster-cloud/dagster-cloud-agent \
   --namespace dagster-cloud \
-  --set dagsterCloud.deployment=prod \
   -f .helm/values-override.yaml
