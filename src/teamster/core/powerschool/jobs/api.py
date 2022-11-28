eaf76ce54e9968ae5ccbfc6fa9d0257b3b7adc9a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-11-28 14:00:09 -0600

    fix teamster dir

--- src/teamster/core/powerschool/jobs/api.py
+++ src/teamster/core/powerschool/jobs/api.py
@@ -16,9 +16,9 @@ powerschool_api_extract = run_queries.to_job(
     },
     config=config_from_files(
         [
-            "teamster/core/config/google/resource.yaml",
-            "teamster/core/config/powerschool/api/resource.yaml",
-            "teamster/core/config/powerschool/api/query-test.yaml",
+            "src/teamster/core/config/google/resource.yaml",
+            "src/teamster/core/config/powerschool/api/resource.yaml",
+            "src/teamster/core/config/powerschool/api/query-test.yaml",
         ]
     ),
 )
