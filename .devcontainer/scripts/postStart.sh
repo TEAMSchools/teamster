af561efb9672cb6a3c9305f4a5ee44e0c2df01d7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-12-01 21:44:28 +0000

    download to file

--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -14,3 +14,9 @@ pdm self update
 
 # update trunk
 trunk upgrade -y --no-progress
+
+# save github codespace secrets to .env files
+for branch in .git/refs/heads/kipp*; do
+  branch_name=$(basename -- "${branch^^}")
+  echo "${!branch_name}" >"./env/${branch_name}.env"
+done
