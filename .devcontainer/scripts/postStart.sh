f4de56003f17b2291b9852e248d2e79116d3ca67
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-12-02 04:46:26 +0000

    fix env export

--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -2,9 +2,9 @@
 
 # update apt packages
 sudo apt-get -qq -y update --no-install-recommends &&
-	sudo apt-get -qq -y upgrade --no-install-recommends &&
-	sudo apt-get -qq autoremove -y &&
-	sudo apt-get -qq clean -y
+  sudo apt-get -qq -y upgrade --no-install-recommends &&
+  sudo apt-get -qq autoremove -y &&
+  sudo apt-get -qq clean -y
 
 # update pip
 python -m pip install --no-cache-dir --upgrade pip
@@ -16,7 +16,15 @@ pdm self update
 trunk upgrade -y --no-progress
 
 # save github codespace secrets to .env files
-for branch in .git/refs/heads/kipp*; do
-  branch_name=$(basename -- "${branch^^}")
-  echo "${!branch_name}" >"./env/${branch_name}.env"
+for branch in $(
+  git for-each-ref --format='%(refname:short)' refs
+); do
+  branch_name=$(basename -- "${branch}")
+  branch_name_clean=$(
+    echo "${branch_name^^}" | tr -cd '[:alnum:]'
+  )
+
+  if [ -n "${!branch_name_clean}" ]; then
+    echo "${!branch_name_clean}" >"./env/${branch_name}.env"
+  fi
 done
