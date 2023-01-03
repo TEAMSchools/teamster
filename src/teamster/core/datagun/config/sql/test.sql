49d0760b388c6b995ffaae5fafc3d598b64f1cd4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-03 22:35:48 +0000

    update deps

--- src/teamster/core/datagun/config/sql/test.sql
+++ src/teamster/core/datagun/config/sql/test.sql
@@ -1,10 +1,10 @@
 SELECT
-    n,
-    n + 1 AS n2,
-    n + 2 AS n3,
-    n + 3 AS n4,
-    n + 4 AS n5
+  n,
+  n + 1 AS n2,
+  n + 2 AS n3,
+  n + 3 AS n4,
+  n + 4 AS n5
 FROM
-    gabby.utilities.row_generator_smallint
+  gabby.utilities.row_generator_smallint
 WHERE
-    n <= 5
+  n <= 5
