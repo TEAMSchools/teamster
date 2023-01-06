491eb415c700a7ec4e9cae0f81258391cc1fa5db
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-06 23:48:24 +0000

    fix branch name for stg

--- .trunk/scripts/post-checkout.sh
+++ .trunk/scripts/post-checkout.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 branch=$(git branch --show-current)
+branch_clean="${branch#stg-}"
 
-if [[ -f "env/${branch}/.env" ]]; then
-  cp --remove-destination "env/${branch}/.env" env/.env
+if [[ -f "env/${branch_clean}/.env" ]]; then
+  cp --remove-destination "env/${branch_clean}/.env" env/.env
 else
   echo >env/.env
 fi
