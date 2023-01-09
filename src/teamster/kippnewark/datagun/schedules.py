7a10b5501f9ecd3534759e85f66b19d1ed590160
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-09 17:40:52 +0000

    checkoutt local folders

---
+++ src/teamster/kippnewark/datagun/schedules.py
@@ -0,0 +1,21 @@
+from dagster import ScheduleDefinition
+from teamster.local.datagun.jobs import datagun_nps, datagun_ps_autocomm
+
+from teamster.core.utils.variables import LOCAL_TIME_ZONE
+
+datagun_ps_autocomm = ScheduleDefinition(
+    job=datagun_ps_autocomm,
+    cron_schedule="15 2 * * *",
+    execution_timezone=str(LOCAL_TIME_ZONE),
+)
+
+datagun_nps = ScheduleDefinition(
+    job=datagun_nps,
+    cron_schedule="0 0 * * *",
+    execution_timezone=str(LOCAL_TIME_ZONE),
+)
+
+__all__ = [
+    "datagun_ps_autocomm",
+    "datagun_nps",
+]
---
+++ src/teamster/kippnewark/repository.py
@@ -0,0 +1,39 @@
+from dagster import repository
+from teamster.local.datagun import jobs as datagun_jobs_local
+from teamster.local.datagun import schedules as datagun_schedules
+
+from teamster.core.datagun import jobs as datagun_jobs_core
+from teamster.core.powerschool.jobs import db as psdb_jobs_core
+from teamster.core.powerschool.schedules import db as psdb_sched_core
+
+
+@repository
+def powerschool():
+    jobs = [v for k, v in vars(psdb_jobs_core).items() if k in psdb_jobs_core.__all__]
+    schedules = [
+        v for k, v in vars(psdb_sched_core).items() if k in psdb_sched_core.__all__
+    ]
+    sensors = []
+
+    return jobs + schedules + sensors
+
+
+@repository
+def datagun():
+    core_jobs = [
+        v for k, v in vars(datagun_jobs_core).items() if k in datagun_jobs_core.__all__
+    ]
+    local_jobs = [
+        v
+        for k, v in vars(datagun_jobs_local).items()
+        if k in datagun_jobs_local.__all__
+    ]
+    jobs = core_jobs + local_jobs
+    schedules = [
+        v for k, v in vars(datagun_schedules).items() if k in datagun_schedules.__all__
+    ]
+
+    return jobs + schedules
+
+
+__all__ = ["powerschool", "datagun"]
