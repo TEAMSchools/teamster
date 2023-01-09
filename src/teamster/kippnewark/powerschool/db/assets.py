6c8e1f507c2c9047f69a8d20382852301f8edabf
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-09 23:45:17 +0000

    ay partition

--- src/teamster/kippnewark/powerschool/db/assets.py
+++ src/teamster/kippnewark/powerschool/db/assets.py
@@ -1,16 +1,21 @@
-from dagster import HourlyPartitionsDefinition
+from dagster import TimeWindowPartitionsDefinition
 
 from teamster.core.powerschool.db.assets import table_asset_factory
 
-hourly_partition = HourlyPartitionsDefinition(
-    start_date="2002-07-01T00:00:00.000000-05:00",
-    timezone="US/Eastern",
+academic_year_partition = TimeWindowPartitionsDefinition(
     fmt="%Y-%m-%dT%H:%M:%S.%f%z",
+    timezone="US/Eastern",
+    start="2002-07-01T00:00:00.000000-04:00",
+    cron_schedule="0 0 1 7 *",
 )
 
-students = table_asset_factory(table_name="students", partitions_def=hourly_partition)
-schools = table_asset_factory(table_name="schools", partitions_def=hourly_partition)
-gen = table_asset_factory(table_name="gen", partitions_def=hourly_partition)
-cc = table_asset_factory(table_name="cc", partitions_def=hourly_partition)
+students = table_asset_factory(table_name="students")
+schools = table_asset_factory(table_name="schools")
+gen = table_asset_factory(table_name="gen")
+cc = table_asset_factory(
+    table_name="cc",
+    where={"column": "transaction_date"},
+    partitions_def=academic_year_partition,
+)
 
 __all__ = [students, schools, gen, cc]
