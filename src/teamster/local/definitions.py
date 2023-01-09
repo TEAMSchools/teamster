cb99aff86dd13a6d23bdc090cb418275fda81b17
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-09 04:24:30 +0000

    config

--- src/teamster/local/definitions.py
+++ src/teamster/local/definitions.py
@@ -14,7 +14,11 @@ defs = Definitions(
         "ssh": ssh_resource.configured(
             config_from_files(["src/teamster/core/powerschool/config/ssh.yaml"])
         ),
-        "io_manager": gcs_pickle_io_manager,
-        "gcs": gcs_resource,
+        "io_manager": gcs_pickle_io_manager.configured(
+            config_from_files(["src/teamster/core/powerschool/config/io.yaml"])
+        ),
+        "gcs": gcs_resource.configured(
+            config_from_files(["src/teamster/core/powerschool/config/io.yaml"])
+        ),
     },
 )
