d7b149031fe835a4827e2c784b7354f36c437196
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-09 04:16:50 +0000

    config

--- src/teamster/local/definitions.py
+++ src/teamster/local/definitions.py
@@ -1,4 +1,4 @@
-from dagster import Definitions
+from dagster import Definitions, config_from_files
 from dagster_gcp.gcs import gcs_pickle_io_manager, gcs_resource
 
 from teamster.core.powerschool.assets.db import students
@@ -8,8 +8,12 @@ from teamster.core.resources.ssh import ssh_resource
 defs = Definitions(
     assets=[students],
     resources={
-        "db": oracle,
-        "ssh": ssh_resource,
+        "db": oracle.configured(
+            config_from_files(["src/teamster/core/powerschool/config/db.yaml"])
+        ),
+        "ssh": ssh_resource.configured(
+            config_from_files(["src/teamster/core/powerschool/config/ssh.yaml"])
+        ),
         "io_manager": gcs_pickle_io_manager,
         "gcs": gcs_resource,
     },
