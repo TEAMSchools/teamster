72fd3b9d0813fae67474155a45173eed81fa307d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-10 18:26:30 +0000

    cleanup

--- src/teamster/core/powerschool/db/assets.py
+++ src/teamster/core/powerschool/db/assets.py
@@ -1,5 +1,6 @@
+from datetime import timedelta
+
 from dagster import asset
-from dateutil.relativedelta import relativedelta
 from sqlalchemy import literal_column, select, table, text
 
 
@@ -9,10 +10,9 @@ def construct_sql(context, table_name, columns, where):
     elif isinstance(where, str):
         constructed_sql_where = where
     elif context.has_partition_key:
-        context.log.debug(context.asset_partitions_def_for_output())
         where_column = where["column"]
         start_datetime = context.partition_time_window.start
-        end_datetime = start_datetime + relativedelta(hou=1)
+        end_datetime = start_datetime + timedelta(hours=1)
 
         constructed_sql_where = (
             f"{where_column} >= TO_TIMESTAMP_TZ('"
