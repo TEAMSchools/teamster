0fca24b74e21327b6b3b9a4b732f49a50b1aafc4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-10 21:14:13 +0000

    load assets

--- src/teamster/kippnewark/definitions.py
+++ src/teamster/kippnewark/definitions.py
@@ -1,13 +1,16 @@
-from dagster import Definitions, config_from_files
+from dagster import Definitions, config_from_files, load_assets_from_modules
 from dagster_gcp.gcs import gcs_pickle_io_manager, gcs_resource
 
-from teamster.core.powerschool.db.assets import core_ps_db_assets
+from teamster.core.powerschool.db import assets as core_ps_db_assets
 from teamster.core.resources.sqlalchemy import oracle
 from teamster.core.resources.ssh import ssh_resource
-from teamster.kippnewark.powerschool.db.assets import local_ps_db_assets
+from teamster.kippnewark.powerschool.db import assets as local_ps_db_assets
 
 defs = Definitions(
-    assets=core_ps_db_assets + local_ps_db_assets,
+    # assets=core_ps_db_assets + local_ps_db_assets,
+    assets=load_assets_from_modules(
+        modules=[core_ps_db_assets, local_ps_db_assets], group_name="powerschool"
+    ),
     resources={
         "ps_db": oracle.configured(
             config_from_files(["src/teamster/core/powerschool/db/config/db.yaml"])
