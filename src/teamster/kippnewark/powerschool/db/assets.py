cc1525493dccfb49268ef28b91629675ebaec9ea
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-10 20:40:46 +0000

    local assets

--- src/teamster/kippnewark/powerschool/db/assets.py
+++ src/teamster/kippnewark/powerschool/db/assets.py
@@ -1,97 +1,19 @@
-from dagster import HourlyPartitionsDefinition
+from teamster.core.powerschool.db.assets import hourly_partition, table_asset_factory
 
-from teamster.core.powerschool.db.assets import table_asset_factory
-
-hourly_partition = HourlyPartitionsDefinition(
-    start_date="2002-07-01T00:00:00.000000-0400",
-    timezone="US/Eastern",
-    fmt="%Y-%m-%dT%H:%M:%S.%f%z",
-)
-
-ps_db_assets = []
-
-# not partitionable
-for table_name in [
-    "attendance_conversion_items",
-    "gen",
-    "test",
-    "testscore",
-    "attendance_code",
-    "bell_schedule",
-    "cycle_day",
-    "fte",
-    "period",
-    "reenrollments",
-    "calendar_day",
-    "spenrollments",
-]:
-    ps_db_assets.append(table_asset_factory(table_name=table_name))
-
-# table-specific partition
-ps_db_assets.append(
-    table_asset_factory(
-        table_name="log",
-        where={"column": "entry_date"},
-        partitions_def=hourly_partition,
-    )
-)
-
-# transaction_date
-for table_name in [
-    "attendance",
-    "cc",
-    "courses",
-    "pgfinalgrades",
-    "prefs",
-    "schools",
-    "sections",
-    "storedgrades",
-    "students",
-    "termbins",
-    "terms",
-]:
-    ps_db_assets.append(
-        table_asset_factory(
-            table_name=table_name,
-            where={"column": "transaction_date"},
-            partitions_def=hourly_partition,
-        )
-    )
+local_ps_db_assets = []
 
 # whenmodified
 for table_name in [
-    "assignmentcategoryassoc",
-    "assignmentscore",
-    "assignmentsection",
-    "codeset",
-    "districtteachercategory",
-    "emailaddress",
-    "gradecalcformulaweight",
-    "gradecalcschoolassoc",
-    "gradecalculationtype",
-    "gradeformulaset",
-    "gradescaleitem",
-    "gradeschoolconfig",
-    "gradeschoolformulaassoc",
-    "gradesectionconfig",
-    "originalcontactmap",
-    "person",
-    "personaddress",
-    "personaddressassoc",
-    "personemailaddressassoc",
-    "personphonenumberassoc",
-    "phonenumber",
-    "roledef",
-    "schoolstaff",
-    "sectionteacher",
-    "studentcontactassoc",
-    "studentcontactdetail",
-    "studentcorefields",
-    "studentrace",
-    "teachercategory",
-    "users",
+    "s_nj_crs_x",
+    "s_nj_ren_x",
+    "s_nj_stu_x",
+    "s_nj_usr_x",
+    "u_clg_et_stu",
+    "u_clg_et_stu_alt",
+    "u_def_ext_students",
+    "u_studentsuserfields",
 ]:
-    ps_db_assets.append(
+    local_ps_db_assets.append(
         table_asset_factory(
             table_name=table_name,
             where={"column": "whenmodified"},
