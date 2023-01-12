4f3ec27eb1cd9b296fd150468ca6eca4a24eaf87
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-12 01:44:13 +0000

    rename

--- src/teamster/kipptaf/datagun/assets.py
+++ src/teamster/kipptaf/datagun/assets.py
@@ -6,14 +6,14 @@ from teamster.core.datagun.assets import (
 )
 
 sftp_extract_assets = []
-with open("src/teamster/kipptaf/datagun/config/sftp_extracts.yaml") as f:
+with open("src/teamster/kipptaf/datagun/config/assets_sftp.yaml") as f:
     sftp_extract_configs = yaml.safe_load(f)
 
 for sec in sftp_extract_configs:
     sftp_extract_assets.append(sftp_extract_asset_factory(**sec))
 
 gsheet_extract_assets = []
-with open("src/teamster/kipptaf/datagun/config/gsheet_extracts.yaml") as f:
+with open("src/teamster/kipptaf/datagun/config/assets_gsheet.yaml") as f:
     gsheet_extract_configs = yaml.safe_load(f)
 
 for gec in gsheet_extract_configs:
