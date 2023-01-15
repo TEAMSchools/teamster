1f4b5cbe5534f5dc716dfed214f492576547ce9b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-15 09:16:38 +0000

    avro schema

--- src/teamster/core/powerschool/db/config/schema.py
+++ src/teamster/core/powerschool/db/config/schema.py
@@ -1,5 +1,35 @@
 from dagster import Array, Bool, Field, Int, ScalarUnion, Shape, String
 
+AVRO_TYPES = {
+    "DB_TYPE_CHAR": ["null", "bytes", "string"],
+    "DB_TYPE_NCHAR": ["null", "bytes", "string"],
+    "DB_TYPE_VARCHAR": ["null", "bytes", "string"],
+    "DB_TYPE_NVARCHAR": ["null", "bytes", "string"],
+    "DB_TYPE_RAW": ["null", "bytes", "string"],
+    "DB_TYPE_LONG": ["null", "bytes", "string"],
+    "DB_TYPE_LONG_RAW": ["null", "bytes", "string"],
+    "DB_TYPE_ROWID": ["null", "bytes", "string"],
+    "DB_TYPE_UROWID": ["null", "bytes", "string"],
+    "DB_TYPE_CLOB": ["null", "bytes", "string"],
+    "DB_TYPE_NCLOB": ["null", "bytes", "string"],
+    "DB_TYPE_BLOB": ["null", "bytes", "string"],
+    "DB_TYPE_NUMBER": ["null", "boolean", "int", "float", "long", "double"],
+    "DB_TYPE_BINARY_INTEGER": ["null", "boolean", "int", "float", "long", "double"],
+    "DB_TYPE_BINARY_FLOAT": ["null", "boolean", "int", "float", "long", "double"],
+    "DB_TYPE_BINARY_DOUBLE": ["null", "boolean", "int", "float", "long", "double"],
+    "DB_TYPE_BOOLEAN": ["null", "boolean"],
+    "DB_TYPE_DATE": ["null", "string"],
+    "DB_TYPE_TIMESTAMP": ["null", "string"],
+    "DB_TYPE_TIMESTAMP_TZ": ["null", "string"],
+    "DB_TYPE_TIMESTAMP_LTZ": ["null", "string"],
+    "DB_TYPE_INTERVAL_YM": ["null"],
+    "DB_TYPE_INTERVAL_DS": ["null"],
+    "DB_TYPE_BFILE": ["null"],
+    "DB_TYPE_JSON": ["null"],
+    "DB_TYPE_CURSOR": ["null"],
+    "DB_TYPE_OBJECT": ["null"],
+}
+
 QUERY_CONFIG = Shape(
     {
         "partition_size": Field(Int, is_required=False, default_value=100000),
