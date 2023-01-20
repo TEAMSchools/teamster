6676ed4df3f08cb61db2b761efe5232c83478469
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-20 15:20:21 +0000

    hardcode start date

--- src/teamster/kippcamden/powerschool/db/jobs.py
+++ src/teamster/kippcamden/powerschool/db/jobs.py
@@ -1,7 +1,8 @@
 from dagster import AssetSelection, HourlyPartitionsDefinition, define_asset_job
 
-from teamster.core.utils.variables import LOCAL_TIME_ZONE, TODAY
+from teamster.core.utils.variables import LOCAL_TIME_ZONE
 
+from ... import POWERSCHOOL_PARTITION_START_DATE
 from .assets import ps_db_assets, ps_db_partitioned_assets
 
 ps_db_asset_job = define_asset_job(
@@ -13,7 +14,7 @@ ps_db_partitioned_asset_job = define_asset_job(
     name="ps_db_partitioned_asset_job",
     selection=AssetSelection.assets(*ps_db_partitioned_assets),
     partitions_def=HourlyPartitionsDefinition(
-        start_date=TODAY.isoformat(timespec="microseconds"),
+        start_date=POWERSCHOOL_PARTITION_START_DATE,
         timezone=str(LOCAL_TIME_ZONE),
         fmt="%Y-%m-%dT%H:%M:%S.%f%z",
     ),
