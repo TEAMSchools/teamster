e4ab1960dce02e9caa9def43bafae6c5bb457ac8
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-20 21:43:28 +0000

    add sensor

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -1,17 +1,24 @@
 from dagster import AssetSelection, build_asset_reconciliation_sensor
 
-from teamster.kippcamden.powerschool.db.assets import (
-    ps_db_assets,
-    ps_db_partitioned_assets,
-)
+from teamster.kippcamden.powerschool.db import assets
 
 ps_db_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*ps_db_assets), name="ps_db_assets_sensor"
+    asset_selection=AssetSelection.assets(*assets.ps_db_assets),
+    name="ps_db_assets_sensor",
 )
 
 ps_db_partitioned_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*ps_db_partitioned_assets),
+    asset_selection=AssetSelection.assets(*assets.ps_db_partitioned_assets),
     name="ps_db_partitioned_assets_sensor",
 )
 
-__all__ = [ps_db_assets_sensor, ps_db_partitioned_assets_sensor]
+ps_assignment_assets_sensor = build_asset_reconciliation_sensor(
+    asset_selection=AssetSelection.assets(*assets.ps_assignment_assets),
+    name="ps_assignment_assets_sensor",
+)
+
+__all__ = [
+    ps_db_assets_sensor,
+    ps_db_partitioned_assets_sensor,
+    ps_assignment_assets_sensor,
+]
