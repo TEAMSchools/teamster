7931c23a37bcbe12cfa8fd5e607338f5c92b0b8e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-20 22:25:34 +0000

    add partition back

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -2,14 +2,14 @@ from dagster import AssetSelection, build_asset_reconciliation_sensor
 
 from teamster.kippcamden.powerschool.db import assets
 
-ps_db_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*assets.ps_db_assets),
-    name="ps_db_assets_sensor",
+ps_daily_assets_sensor = build_asset_reconciliation_sensor(
+    asset_selection=AssetSelection.assets(*assets.ps_daily_assets),
+    name="ps_daily_assets_sensor",
 )
 
-ps_db_partitioned_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*assets.ps_db_partitioned_assets),
-    name="ps_db_partitioned_assets_sensor",
+ps_hourly_assets_sensor = build_asset_reconciliation_sensor(
+    asset_selection=AssetSelection.assets(*assets.ps_hourly_assets),
+    name="ps_hourly_assets_sensor",
 )
 
 ps_assignment_assets_sensor = build_asset_reconciliation_sensor(
@@ -28,8 +28,8 @@ ps_custom_assets_sensor = build_asset_reconciliation_sensor(
 )
 
 __all__ = [
-    ps_db_assets_sensor,
-    ps_db_partitioned_assets_sensor,
+    ps_daily_assets_sensor,
+    ps_hourly_assets_sensor,
     ps_assignment_assets_sensor,
     ps_custom_assets_sensor,
     ps_contacts_assets_sensor,
