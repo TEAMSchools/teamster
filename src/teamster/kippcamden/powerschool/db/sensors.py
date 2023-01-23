9a2032e25ba6dc2da45a3714d63a959e815aa374
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-23 18:41:55 +0000

    dev recon sensor

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -1,4 +1,8 @@
-from dagster import AssetSelection, build_asset_reconciliation_sensor
+from dagster import AssetSelection, build_asset_reconciliation_sensor, sensor
+from dagster._core.definitions.asset_reconciliation_sensor import (
+    AssetReconciliationCursor,
+    reconcile,
+)
 
 from teamster.kippcamden.powerschool.db import assets
 
@@ -31,6 +35,31 @@ ps_custom_assets_sensor = build_asset_reconciliation_sensor(
     asset_selection=AssetSelection.assets(*assets.ps_custom_assets),
     name="ps_custom_assets_sensor",
 )
+
+asset_selection = AssetSelection.assets(*assets.ps_assignment_assets)
+
+
+@sensor(asset_selection=asset_selection, minimum_interval_seconds=3600)
+def ps_incremental_sensor(context):
+    cursor = (
+        AssetReconciliationCursor.from_serialized(
+            context.cursor, context.repository_def.asset_graph
+        )
+        if context.cursor
+        else AssetReconciliationCursor.empty()
+    )
+    context.log.info(cursor)
+
+    # run_requests, updated_cursor = reconcile(
+    #     repository_def=context.repository_def,
+    #     asset_selection=asset_selection,
+    #     instance=context.instance,
+    #     cursor=cursor,
+    # )
+
+    # context.update_cursor(updated_cursor.serialize())
+    # return run_requests
+
 
 __all__ = [
     ps_daily_assets_sensor,
