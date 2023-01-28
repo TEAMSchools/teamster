aa08f1fa4f879c8ba883c220d68703ace9150009
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-28 04:25:39 +0000

    pendulum fn

--- src/teamster/core/utils/variables.py
+++ src/teamster/core/utils/variables.py
@@ -4,4 +4,4 @@ import pendulum
 
 LOCAL_TIME_ZONE = pendulum.timezone(name=os.getenv("LOCAL_TIME_ZONE"))
 NOW = pendulum.now(tz=LOCAL_TIME_ZONE)
-TODAY = NOW.replace(hour=0, minute=0, second=0, microsecond=0)
+TODAY = NOW.start_of(unit="day")
