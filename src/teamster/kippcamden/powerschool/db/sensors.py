b89f816848dd076d7f7ca7d65d9f4caeeef78541
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-30 22:40:48 +0000

    space out sensors

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -10,35 +10,35 @@ assignments_sensor = build_powerschool_incremental_sensor(
     name="ps_assignments_sensor",
     asset_selection=AssetSelection.assets(*assets.assignments_assets),
     where_column="whenmodified",
-    minimum_interval_seconds=60,
+    minimum_interval_seconds=90,
 )
 
 extensions_sensor = build_powerschool_incremental_sensor(
     name="ps_extensions_sensor",
     asset_selection=AssetSelection.assets(*assets.extensions_assets),
     where_column="whenmodified",
-    minimum_interval_seconds=60,
+    minimum_interval_seconds=90,
 )
 
 contacts_sensor = build_powerschool_incremental_sensor(
     name="ps_contacts_sensor",
     asset_selection=AssetSelection.assets(*assets.contacts_assets),
     where_column="whenmodified",
-    minimum_interval_seconds=60,
+    minimum_interval_seconds=90,
 )
 
 whenmodified_sensor = build_powerschool_incremental_sensor(
     name="ps_whenmodified_sensor",
     asset_selection=AssetSelection.assets(*assets.whenmodified_assets),
     where_column="whenmodified",
-    minimum_interval_seconds=60,
+    minimum_interval_seconds=90,
 )
 
 transactiondate_sensor = build_powerschool_incremental_sensor(
     name="ps_transactiondate_sensor",
     asset_selection=AssetSelection.assets(*assets.transactiondate_assets),
     where_column="transaction_date",
-    minimum_interval_seconds=60,
+    minimum_interval_seconds=90,
 )
 
 __all__ = [
