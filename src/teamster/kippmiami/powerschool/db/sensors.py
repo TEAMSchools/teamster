205238b0989b992e865479c8bcd97220816b13ad
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-30 23:54:12 +0000

    ps

--- src/teamster/kippmiami/powerschool/db/sensors.py
+++ src/teamster/kippmiami/powerschool/db/sensors.py
@@ -1,42 +1,51 @@
-from dagster import AssetSelection, build_asset_reconciliation_sensor
+from dagster import AssetSelection
 
-from teamster.kippmiami.powerschool.db import assets
+from teamster.core.powerschool.db.sensors import (
+    build_powerschool_incremental_sensor,
+    powerschool_ssh_tunnel,
+)
+from teamster.kippcamden.powerschool.db import assets
 
-ps_daily_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*assets.ps_daily_assets),
-    name="ps_daily_assets_sensor",
+assignments_sensor = build_powerschool_incremental_sensor(
+    name="ps_assignments_sensor",
+    asset_selection=AssetSelection.assets(*assets.assignments_assets),
+    where_column="whenmodified",
+    minimum_interval_seconds=90,
 )
 
-ps_misc_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*assets.ps_misc_assets),
-    name="ps_misc_assets_sensor",
+extensions_sensor = build_powerschool_incremental_sensor(
+    name="ps_extensions_sensor",
+    asset_selection=AssetSelection.assets(*assets.extensions_assets),
+    where_column="whenmodified",
+    minimum_interval_seconds=90,
 )
 
-ps_transactiondate_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*assets.ps_transactiondate_assets),
-    name="ps_transactiondate_assets_sensor",
+contacts_sensor = build_powerschool_incremental_sensor(
+    name="ps_contacts_sensor",
+    asset_selection=AssetSelection.assets(*assets.contacts_assets),
+    where_column="whenmodified",
+    minimum_interval_seconds=90,
 )
 
-ps_assignment_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*assets.ps_assignment_assets),
-    name="ps_assignment_assets_sensor",
+whenmodified_sensor = build_powerschool_incremental_sensor(
+    name="ps_whenmodified_sensor",
+    asset_selection=AssetSelection.assets(*assets.whenmodified_assets),
+    where_column="whenmodified",
+    minimum_interval_seconds=90,
 )
 
-ps_contacts_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*assets.ps_contacts_assets),
-    name="ps_contacts_assets_sensor",
-)
-
-ps_custom_assets_sensor = build_asset_reconciliation_sensor(
-    asset_selection=AssetSelection.assets(*assets.ps_custom_assets),
-    name="ps_custom_assets_sensor",
+transactiondate_sensor = build_powerschool_incremental_sensor(
+    name="ps_transactiondate_sensor",
+    asset_selection=AssetSelection.assets(*assets.transactiondate_assets),
+    where_column="transaction_date",
+    minimum_interval_seconds=90,
 )
 
 __all__ = [
-    ps_daily_assets_sensor,
-    ps_assignment_assets_sensor,
-    ps_custom_assets_sensor,
-    ps_contacts_assets_sensor,
-    ps_misc_assets_sensor,
-    ps_transactiondate_assets_sensor,
+    powerschool_ssh_tunnel,
+    assignments_sensor,
+    contacts_sensor,
+    extensions_sensor,
+    whenmodified_sensor,
+    transactiondate_sensor,
 ]
