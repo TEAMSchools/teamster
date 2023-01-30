8ff00ce345cda7e42884f9c2943eb02f92e6de5b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-30 02:25:45 +0000

    consolidate assets andd sensors

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -1,53 +1,24 @@
-from dagster import AssetSelection  # , build_asset_reconciliation_sensor
+from dagster import AssetSelection
 
 from teamster.core.powerschool.db.sensors import build_powerschool_incremental_sensor
 from teamster.kippcamden.powerschool.db import assets
 
-ps_incremental_sensors = [
+whenmodified_incremental_sensors = [
     build_powerschool_incremental_sensor(
         name=a.key.to_python_identifier(),
         asset_selection=AssetSelection.assets(a),
         where_column="whenmodified",
     )
-    for a in assets.ps_test_assets
+    for a in assets.whenmodified_assets
 ]
 
-# ps_daily_assets_sensor = build_asset_reconciliation_sensor(
-#     asset_selection=AssetSelection.assets(*assets.ps_daily_assets),
-#     name="ps_daily_assets_sensor",
-# )
+transactiondate_incremental_sensors = [
+    build_powerschool_incremental_sensor(
+        name=a.key.to_python_identifier(),
+        asset_selection=AssetSelection.assets(a),
+        where_column="transaction_date",
+    )
+    for a in assets.transactiondate_assets
+]
 
-# ps_misc_assets_sensor = build_asset_reconciliation_sensor(
-#     asset_selection=AssetSelection.assets(*assets.ps_misc_assets),
-#     name="ps_misc_assets_sensor",
-# )
-
-# ps_transactiondate_assets_sensor = build_asset_reconciliation_sensor(
-#     asset_selection=AssetSelection.assets(*assets.ps_transactiondate_assets),
-#     name="ps_transactiondate_assets_sensor",
-# )
-
-# ps_assignment_assets_sensor = build_asset_reconciliation_sensor(
-#     asset_selection=AssetSelection.assets(*assets.ps_assignment_assets),
-#     name="ps_assignment_assets_sensor",
-# )
-
-# ps_contacts_assets_sensor = build_asset_reconciliation_sensor(
-#     asset_selection=AssetSelection.assets(*assets.ps_contacts_assets),
-#     name="ps_contacts_assets_sensor",
-# )
-
-# ps_custom_assets_sensor = build_asset_reconciliation_sensor(
-#     asset_selection=AssetSelection.assets(*assets.ps_custom_assets),
-#     name="ps_custom_assets_sensor",
-# )
-
-__all__ = ps_incremental_sensors
-# [
-# ps_daily_assets_sensor,
-# ps_assignment_assets_sensor,
-# ps_custom_assets_sensor,
-# ps_contacts_assets_sensor,
-# ps_misc_assets_sensor,
-# ps_transactiondate_assets_sensor,
-# ]
+__all__ = whenmodified_incremental_sensors + transactiondate_incremental_sensors
