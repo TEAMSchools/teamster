bd476c0f26b95d1b0991b65abc487a32c941c210
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-30 01:48:47 +0000

    generate sensors

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -3,11 +3,14 @@ from dagster import AssetSelection  # , build_asset_reconciliation_sensor
 from teamster.core.powerschool.db.sensors import build_powerschool_incremental_sensor
 from teamster.kippcamden.powerschool.db import assets
 
-ps_incremental_sensor = build_powerschool_incremental_sensor(
-    name="ps_incremental_sensor",
-    asset_selection=AssetSelection.assets(*assets.ps_test_assets),
-    where_column="whenmodified",
-)
+ps_incremental_sensors = [
+    build_powerschool_incremental_sensor(
+        name=a.key.to_python_identifier(),
+        asset_selection=AssetSelection.assets(a),
+        where_column="whenmodified",
+    )
+    for a in assets.ps_test_assets
+]
 
 # ps_daily_assets_sensor = build_asset_reconciliation_sensor(
 #     asset_selection=AssetSelection.assets(*assets.ps_daily_assets),
@@ -39,12 +42,12 @@ ps_incremental_sensor = build_powerschool_incremental_sensor(
 #     name="ps_custom_assets_sensor",
 # )
 
-__all__ = [
-    # ps_daily_assets_sensor,
-    # ps_assignment_assets_sensor,
-    # ps_custom_assets_sensor,
-    # ps_contacts_assets_sensor,
-    # ps_misc_assets_sensor,
-    # ps_transactiondate_assets_sensor,
-    ps_incremental_sensor,
-]
+__all__ = ps_incremental_sensors
+# [
+# ps_daily_assets_sensor,
+# ps_assignment_assets_sensor,
+# ps_custom_assets_sensor,
+# ps_contacts_assets_sensor,
+# ps_misc_assets_sensor,
+# ps_transactiondate_assets_sensor,
+# ]
