27f4c2994b1aa8804fd5efa467d127190592693c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-01-31 04:47:53 +0000

    fix name

--- src/teamster/kippcamden/powerschool/db/schedules.py
+++ src/teamster/kippcamden/powerschool/db/schedules.py
@@ -3,10 +3,11 @@ from dagster import ScheduleDefinition
 from teamster.core.utils.variables import LOCAL_TIME_ZONE
 from teamster.kippcamden.powerschool.db.jobs import nonpartition_assets_job
 
-powerschool_extract_assets_schedule = ScheduleDefinition(
+powerschool_nonpartition_assets_job_schedule = ScheduleDefinition(
+    name="powerschool_nonpartition_assets_job_schedule",
     job=nonpartition_assets_job,
     cron_schedule="0 0 * * *",
     execution_timezone=LOCAL_TIME_ZONE.name,
 )
 
-__all__ = [powerschool_extract_assets_schedule]
+__all__ = [powerschool_nonpartition_assets_job_schedule]
