0dd77e0238c7067986807424c923a8d041c60284
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-02 17:01:09 +0000

    debug

--- src/teamster/kippcamden/definitions.py
+++ src/teamster/kippcamden/definitions.py
@@ -12,7 +12,6 @@ defs = Definitions(
     assets=(
         load_assets_from_modules(modules=[powerschool.assets], group_name="powerschool")
         + load_assets_from_modules(modules=[datagun.assets], group_name="datagun")
-        # + load_assets_from_modules(modules=[dbt.assets], group_name="dbt")
     ),
     jobs=datagun.jobs.__all__,
     schedules=datagun.schedules.__all__ + powerschool.schedules.__all__,
