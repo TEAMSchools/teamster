eb2a9ee432c2b7f627af0fafc5c3cfa9edc31a2f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-03 21:31:53 +0000

    rm group name

--- src/teamster/kippcamden/dbt/assets.py
+++ src/teamster/kippcamden/dbt/assets.py
@@ -1,5 +1,8 @@
 from dagster_dbt import load_assets_from_dbt_project
 
 dbt_assets = load_assets_from_dbt_project(
-    project_dir="dbt", profiles_dir="dbt", key_prefix="dbt", source_key_prefix="dbt"
+    project_dir="teamster-dbt",
+    profiles_dir="teamster-dbt",
+    key_prefix="dbt",
+    source_key_prefix="dbt",
 )
--- src/teamster/kippcamden/definitions.py
+++ src/teamster/kippcamden/definitions.py
@@ -14,15 +14,15 @@ defs = Definitions(
     assets=(
         load_assets_from_modules(modules=[powerschool.assets], group_name="powerschool")
         + load_assets_from_modules(modules=[datagun.assets], group_name="datagun")
-        + load_assets_from_modules(modules=[dbt.assets], group_name="dbt")
+        + load_assets_from_modules(modules=[dbt.assets])
     ),
     jobs=datagun.jobs.__all__,
     schedules=datagun.schedules.__all__ + powerschool.schedules.__all__,
     sensors=powerschool.sensors.__all__,
     resources={
-        "dbt_cli": dbt_cli_resource.configured(
-            {"project_dir": "dbt", "profiles_dir": "dbt"}
-        ),
+        # "dbt_cli": dbt_cli_resource.configured(
+        #     {"project_dir": "dbt", "profiles_dir": "dbt"}
+        # ),
         "io_manager": gcs_pickle_io_manager.configured(
             config_from_files(
                 [f"src/teamster/{CODE_LOCATION}/resources/config/io.yaml"]
