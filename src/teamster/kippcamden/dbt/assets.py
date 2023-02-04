241cea3a6f078529ae5bc5a01c0b0931f62af9a5
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-04 19:02:24 +0000

    move dbt asset

--- src/teamster/kippcamden/dbt/assets.py
+++ src/teamster/kippcamden/dbt/assets.py
@@ -1,5 +1,60 @@
-from dagster_dbt import load_assets_from_dbt_project
+import pathlib
+from urllib.parse import ParseResult
+
+from dagster import (
+    AssetIn,
+    HourlyPartitionsDefinition,
+    OpExecutionContext,
+    Output,
+    asset,
+)
+from dagster_dbt import DbtCliResource, load_assets_from_dbt_project
+from google.cloud import bigquery
+
+from teamster.core.utils.variables import LOCAL_TIME_ZONE
+from teamster.kippcamden import PS_PARTITION_START_DATE
 
 dbt_assets = load_assets_from_dbt_project(
     project_dir="teamster-dbt", profiles_dir="teamster-dbt"
 )
+
+
+@asset(
+    name="src_assignmentcategoryassoc",
+    ins={
+        "upstream": AssetIn(
+            key=["kippcamden", "powerschool", "assignmentcategoryassoc"]
+        )
+    },
+    # key_prefix=["kippcamden_powerschool"],
+    partitions_def=HourlyPartitionsDefinition(
+        start_date=PS_PARTITION_START_DATE,
+        timezone=LOCAL_TIME_ZONE.name,
+        fmt="%Y-%m-%dT%H:%M:%S.%f",
+    ),
+    required_resource_keys={"warehouse_bq", "dbt"},
+)
+def src_assignmentcategoryassoc(context: OpExecutionContext, upstream: ParseResult):
+    file_path_parts = pathlib.Path(upstream.path).parts
+
+    code_location = file_path_parts[2]
+    schema_name = file_path_parts[3]
+    table_name = file_path_parts[4]
+
+    dataset = f"{code_location}_{schema_name}"
+
+    # create BigQuery dataset, if not exists
+    bq: bigquery.Client = context.resources.warehouse_bq
+    context.log.debug(f"Creating dataset {dataset}")
+    bq.create_dataset(dataset=dataset, exists_ok=True)
+
+    # dbt run-operation stage_external_sources
+    dbt: DbtCliResource = context.resources.dbt
+
+    dbt_output = dbt.run_operation(
+        macro="stage_external_sources",
+        args={"select": f"{dataset}.src_{table_name}"},
+        vars={"ext_full_refresh": True},
+    )
+
+    return Output(dbt_output)
--- src/teamster/kippcamden/powerschool/db/assets.py
+++ src/teamster/kippcamden/powerschool/db/assets.py
@@ -1,18 +1,6 @@
-import pathlib
-from urllib.parse import ParseResult
-
-from dagster import (
-    HourlyPartitionsDefinition,
-    OpExecutionContext,
-    Output,
-    asset,
-    config_from_files,
-)
-from dagster_dbt import DbtCliResource
-from google.cloud import bigquery
+from dagster import config_from_files
 
 from teamster.core.powerschool.db.assets import build_powerschool_table_asset
-from teamster.core.utils.variables import LOCAL_TIME_ZONE
 from teamster.kippcamden import CODE_LOCATION, PS_PARTITION_START_DATE
 
 nonpartition_assets = [
@@ -83,44 +71,3 @@ whenmodified_assets = [
         [f"src/teamster/{CODE_LOCATION}/powerschool/db/config/assets-whenmodified.yaml"]
     )["assets"]
 ]
-
-
-assignmentcategoryassoc = assignments_assets[0]
-
-
-@asset(
-    name="src_assignmentcategoryassoc",
-    key_prefix=["kippcamden_powerschool"],
-    partitions_def=HourlyPartitionsDefinition(
-        start_date=PS_PARTITION_START_DATE,
-        timezone=LOCAL_TIME_ZONE.name,
-        fmt="%Y-%m-%dT%H:%M:%S.%f",
-    ),
-    required_resource_keys={"warehouse_bq", "dbt"},
-)
-def src_assignmentcategoryassoc(
-    context: OpExecutionContext, assignmentcategoryassoc: ParseResult
-):
-    file_path_parts = pathlib.Path(assignmentcategoryassoc.path).parts
-
-    code_location = file_path_parts[2]
-    schema_name = file_path_parts[3]
-    table_name = file_path_parts[4]
-
-    dataset = f"{code_location}_{schema_name}"
-
-    # create BigQuery dataset, if not exists
-    bq: bigquery.Client = context.resources.warehouse_bq
-    context.log.debug(f"Creating dataset {dataset}")
-    bq.create_dataset(dataset=dataset, exists_ok=True)
-
-    # dbt run-operation stage_external_sources
-    dbt: DbtCliResource = context.resources.dbt
-
-    dbt_output = dbt.run_operation(
-        macro="stage_external_sources",
-        args={"select": f"{dataset}.src_{table_name}"},
-        vars={"ext_full_refresh": True},
-    )
-
-    return Output(dbt_output)
