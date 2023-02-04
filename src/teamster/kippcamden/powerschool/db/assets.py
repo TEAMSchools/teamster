0d2a90abbbf984995c1c2413eb3cff628ecb6946
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-04 22:54:43 +0000

    rm old sensors

--- src/teamster/kippcamden/powerschool/db/assets.py
+++ src/teamster/kippcamden/powerschool/db/assets.py
@@ -24,55 +24,7 @@ transactiondate_assets = [
     )["assets"]
 ]
 
-# assignments_assets = [
-#     build_powerschool_table_asset(
-#         **cfg,
-#         code_location=CODE_LOCATION,
-#         partition_start_date=PS_PARTITION_START_DATE,
-#         where_column="whenmodified",
-#     )
-#     for cfg in config_from_files(
-#         [f"src/teamster/{CODE_LOCATION}/powerschool/db/config/assets-assignments.yaml"]
-#     )["assets"]
-# ]
-
-# contacts_assets = [
-#     build_powerschool_table_asset(
-#         **cfg,
-#         code_location=CODE_LOCATION,
-#         partition_start_date=PS_PARTITION_START_DATE,
-#         where_column="whenmodified",
-#     )
-#     for cfg in config_from_files(
-#         [f"src/teamster/{CODE_LOCATION}/powerschool/db/config/assets-contacts.yaml"]
-#     )["assets"]
-# ]
-
-# extensions_assets = [
-#     build_powerschool_table_asset(
-#         **cfg,
-#         code_location=CODE_LOCATION,
-#         partition_start_date=PS_PARTITION_START_DATE,
-#         where_column="whenmodified",
-#     )
-#     for cfg in config_from_files(
-#         [f"src/teamster/{CODE_LOCATION}/powerschool/db/config/assets-extensions.yaml"]
-#     )["assets"]
-# ]
-
-# whenmodified_assets = [
-#     build_powerschool_table_asset(
-#         **cfg,
-#         code_location=CODE_LOCATION,
-#         partition_start_date=PS_PARTITION_START_DATE,
-#         where_column="whenmodified",
-#     )
-#     for cfg in config_from_files(
-#         [f"src/teamster/{CODE_LOCATION}/powerschool/db/config/assets-whenmodified.yaml"]
-#     )["assets"]
-# ]
-
-assets = [
+whenmodified_assets = [
     build_powerschool_table_asset(
         **cfg,
         code_location=CODE_LOCATION,
@@ -80,6 +32,6 @@ assets = [
         where_column="whenmodified",
     )
     for cfg in config_from_files(
-        [f"src/teamster/{CODE_LOCATION}/powerschool/db/config/assets.yaml"]
+        [f"src/teamster/{CODE_LOCATION}/powerschool/db/config/assets-whenmodified.yaml"]
     )["assets"]
 ]
