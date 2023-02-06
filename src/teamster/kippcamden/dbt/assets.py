27e2c993d5036faf4cba2f1c14e24ff4742b6161
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-06 18:03:09 +0000

    fn-ize the dbt assets

--- src/teamster/kippcamden/dbt/assets.py
+++ src/teamster/kippcamden/dbt/assets.py
@@ -1,19 +1,8 @@
-import pathlib
-from typing import Sequence
-from urllib.parse import ParseResult
-
-from dagster import (
-    AssetIn,
-    HourlyPartitionsDefinition,
-    OpExecutionContext,
-    Output,
-    asset,
-)
+from dagster import AssetIn, OpExecutionContext, Output, asset
 from dagster_dbt import DbtCliResource, load_assets_from_dbt_project
 from google.cloud import bigquery
 
-from teamster.core.utils.variables import LOCAL_TIME_ZONE
-from teamster.kippcamden import CODE_LOCATION, PS_PARTITION_START_DATE
+from teamster.kippcamden import CODE_LOCATION
 
 dbt_assets = load_assets_from_dbt_project(
     project_dir="teamster-dbt",
@@ -24,43 +13,37 @@ dbt_assets = load_assets_from_dbt_project(
 )
 
 
-@asset(
-    name="src_assignmentcategoryassoc",
-    ins={
-        "upstream": AssetIn(
-            key=[CODE_LOCATION, "powerschool", "assignmentcategoryassoc"]
-        )
-    },
-    key_prefix=[CODE_LOCATION, "dbt", f"{CODE_LOCATION}_powerschool"],
-    # partitions_def=HourlyPartitionsDefinition(
-    #     start_date=PS_PARTITION_START_DATE,
-    #     timezone=LOCAL_TIME_ZONE.name,
-    #     fmt="%Y-%m-%dT%H:%M:%S.%f",
-    # ),
-    required_resource_keys={"warehouse_bq", "dbt"},
-)
-def src_assignmentcategoryassoc(context: OpExecutionContext, upstream):
-    context.log.debug(upstream)
-    file_path_parts = pathlib.Path(upstream[-1].path).parts
-
-    code_location = file_path_parts[2]
-    schema_name = file_path_parts[3]
-    table_name = file_path_parts[4]
-
-    dataset = f"{code_location}_{schema_name}"
-
-    # create BigQuery dataset, if not exists
-    bq: bigquery.Client = context.resources.warehouse_bq
-    context.log.debug(f"Creating dataset {dataset}")
-    bq.create_dataset(dataset=dataset, exists_ok=True)
-
-    # dbt run-operation stage_external_sources
-    dbt: DbtCliResource = context.resources.dbt
-
-    dbt_output = dbt.run_operation(
-        macro="stage_external_sources",
-        args={"select": f"{dataset}.src_{table_name}"},
-        vars={"ext_full_refresh": True},
+def build_dbt_external_source_asset(asset_name, code_location, source_system):
+    @asset(
+        name=f"src_{asset_name}",
+        ins={"upstream": AssetIn(key=[code_location, source_system, asset_name])},
+        key_prefix=[code_location, "dbt", f"{code_location}_{source_system}"],
+        required_resource_keys={"warehouse_bq", "dbt"},
     )
+    def _asset(context: OpExecutionContext, upstream):
+        dataset = f"{code_location}_{source_system}"
 
-    return Output(dbt_output)
+        # create BigQuery dataset, if not exists
+        bq: bigquery.Client = context.resources.warehouse_bq
+        context.log.debug(f"Creating dataset {dataset}")
+        bq.create_dataset(dataset=dataset, exists_ok=True)
+
+        # dbt run-operation stage_external_sources
+        dbt: DbtCliResource = context.resources.dbt
+
+        dbt_output = dbt.run_operation(
+            macro="stage_external_sources",
+            args={"select": f"{dataset}.src_{asset_name}"},
+            vars={"ext_full_refresh": True},
+        )
+
+        return Output(dbt_output)
+
+    return _asset
+
+
+src_assignmentcategoryassoc = build_dbt_external_source_asset(
+    asset_name="assignmentcategoryassoc",
+    code_location=CODE_LOCATION,
+    source_system="powerschool",
+)
