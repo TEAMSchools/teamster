bd420ef2cc8459bb9017aa51e604038271126c2c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-06 22:40:44 +0000

    fix pk map

--- src/teamster/kippcamden/dbt/assets/powerschool.py
+++ src/teamster/kippcamden/dbt/assets/powerschool.py
@@ -1,3 +1,4 @@
+import pendulum
 from dagster_dbt import load_assets_from_dbt_project
 
 from teamster.core.dbt.assets import build_dbt_external_source_asset
@@ -7,6 +8,17 @@ from teamster.kippcamden.powerschool.db.assets import (
     whenmodified_assets,
 )
 
+
+def partition_key_to_vars(partition_key):
+    partition_key_datetime = pendulum.parser.parse(text=partition_key)
+    return {
+        "_file_name": (
+            f"dt={partition_key_datetime.date()}/"
+            f"{partition_key_datetime.format(fmt='HH')}"
+        )
+    }
+
+
 assignmentcategoryassoc = load_assets_from_dbt_project(
     project_dir="teamster-dbt",
     profiles_dir="teamster-dbt",
@@ -14,7 +26,7 @@ assignmentcategoryassoc = load_assets_from_dbt_project(
     key_prefix=[CODE_LOCATION, "dbt"],
     source_key_prefix=[CODE_LOCATION, "dbt"],
     partitions_def=hourly_partitions_def,
-    partition_key_to_vars_fn=lambda x: {"_file_name": x},
+    partition_key_to_vars_fn=partition_key_to_vars,
 )
 
 src_assignmentcategoryassoc = build_dbt_external_source_asset(whenmodified_assets[0])
