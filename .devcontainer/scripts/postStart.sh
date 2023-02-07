0c70da862111988fcc99ce2f27ffe202dca81de9
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-07 19:27:35 +0000

    devcontainer

---
+++ .devcontainer/scripts/postStart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# update apt packages
+sudo apt-get -qq -y update --no-install-recommends &&
+	sudo apt-get -qq -y upgrade --no-install-recommends &&
+	sudo apt-get -qq autoremove -y &&
+	sudo apt-get -qq clean -y
+
+# update pip
+python -m pip install --no-cache-dir --upgrade pip
+
+# update pdm
+sudo pdm self update
+
+# update trunk
+trunk upgrade -y --no-progress
+
+# set up dbt env
+# mkdir -p ~/.dbt
+# echo "${DBT_USER_CREDS}" >"${KEYFILE}"
+# echo "${PROFILE}" >"${DBT_PROFILES_DIR}"/profiles.yml
