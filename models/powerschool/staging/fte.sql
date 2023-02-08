c5f6fda74f5c314368a0a543f9cc2be783242feb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-08 23:59:54 +0000

    nonincremental models

---
+++ models/powerschool/staging/fte.sql
@@ -0,0 +1,13 @@
+{{
+    transform_cols_base_model(
+        source_name=model.fqn[1],
+        model_name=this.identifier,
+        transform_cols=[
+            {"name": "dcid", "type": "int_value"},
+            {"name": "id", "type": "int_value"},
+            {"name": "schoolid", "type": "int_value"},
+            {"name": "yearid", "type": "int_value"},
+            {"name": "fte_value", "type": "double_value"},
+        ],
+    )
+}}
