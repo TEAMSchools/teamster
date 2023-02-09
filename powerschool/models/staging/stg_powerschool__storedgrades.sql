723f52188d8257afbc98aa842e70072be0dff4a0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-09 23:03:55 +0000

    fix param

--- powerschool/models/staging/stg_powerschool__storedgrades.sql
+++ powerschool/models/staging/stg_powerschool__storedgrades.sql
@@ -2,7 +2,7 @@
     incremental_merge_source_file(
         from_source=source("powerschool", this.identifier | replace("stg", "src")),
         file_uri=get_gcs_uri(
-            code_location="powerschool",
+            code_location=var("code_location"),
             system_name="powerschool",
             model_name=this.identifier | replace("stg_powerschool__", ""),
             partition_path=var("partition_path"),
