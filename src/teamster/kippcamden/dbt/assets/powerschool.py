9f9eae212e835651322e8b6c036ceeefd7ad4a23
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-10 16:10:13 +0000

    fix key prefix

--- src/teamster/kippcamden/dbt/assets/powerschool.py
+++ src/teamster/kippcamden/dbt/assets/powerschool.py
@@ -33,8 +33,8 @@ incremental_stg_assets = [
     load_assets_from_dbt_manifest(
         manifest_json=manifest_json,
         select=f"stg_powerschool__{a.key.path[-1]}+",
-        key_prefix=[CODE_LOCATION, "dbt"],
-        source_key_prefix=[CODE_LOCATION, "dbt"],
+        key_prefix=[CODE_LOCATION, "dbt", "powerschool"],
+        source_key_prefix=[CODE_LOCATION, "dbt", "powerschool"],
         partitions_def=hourly_partitions_def,
         partition_key_to_vars_fn=partition_key_to_vars,
     )
