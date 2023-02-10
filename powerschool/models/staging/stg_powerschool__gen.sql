543bb13234f6fc9d6e6ec73303103995b24b86fc
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-10 22:16:46 +0000

    refactors

--- powerschool/models/staging/stg_powerschool__gen.sql
+++ powerschool/models/staging/stg_powerschool__gen.sql
@@ -1,6 +1,5 @@
 {{
     teamster_utils.transform_cols_base_model(
-        from_source=source("powerschool", this.identifier | replace("stg", "src")),
         transform_cols=[
             {"name": "dcid", "type": "int_value"},
             {"name": "id", "type": "int_value"},
--- powerschool/models/staging/stg_powerschool__gradecalcformulaweight.sql
+++ powerschool/models/staging/stg_powerschool__gradecalcformulaweight.sql
@@ -1,12 +1,6 @@
 {{
     teamster_utils.incremental_merge_source_file(
-        from_source=source("powerschool", this.identifier | replace("stg", "src")),
-        file_uri=teamster_utils.get_gcs_uri(
-            code_location=var("code_location"),
-            system_name="powerschool",
-            model_name=this.identifier | replace("stg_powerschool__", ""),
-            partition_path=var("partition_path"),
-        ),
+        file_uri=teamster_utils.get_gcs_uri(partition_path=var("partition_path")),
         unique_key="gradecalcformulaweightid",
         transform_cols=[
             {"name": "gradecalcformulaweightid", "type": "int_value"},
