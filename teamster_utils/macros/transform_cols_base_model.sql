41bb40125a19777b4ab343e03206a377dc18a630
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-11 00:16:37 +0000

    fix package name

--- teamster_utils/macros/transform_cols_base_model.sql
+++ teamster_utils/macros/transform_cols_base_model.sql
@@ -1,6 +1,6 @@
 {%- macro transform_cols_base_model(transform_cols=[]) -%}
 
-{%- set from_source = source(project_name, model.name | replace("stg", "src")) -%}
+{%- set from_source = source(model.package_name, model.name | replace("stg", "src")) -%}
 {%- set star_except = dbt_utils.star(
     from=from_source, except=transform_cols | map(attribute="name")
 ) -%}
