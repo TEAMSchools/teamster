2d09fee4ff43a3bd5712cab1a292e47533157389
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-15 23:07:12 +0000

    dl resource

---
+++ src/teamster/core/resources/deanslist.py
@@ -0,0 +1,48 @@
+from dagster import Field, InitResourceContext, Int, Map, String, StringSource, resource
+from requests import Session
+
+
+class DeansList(Session):
+    def __init__(
+        self,
+        logger: InitResourceContext.log,
+        resource_config: InitResourceContext.resource_config,
+    ):
+        super().__init__()
+
+        self.log = logger
+        self.subdomain = resource_config["subdomain"]
+        self.api_version = resource_config["api_version"]
+        self.api_key_map = resource_config["api_key_map"]
+        self.base_url = f"https://{self.subdomain}.deanslistsoftware.com/api"
+
+    def _get_url(self, endpoint, *args):
+        if endpoint in ["behavior", "users"]:
+            return f"{self.base_url}/{endpoint}"
+        elif args is not None:
+            return f"{self.base_url}/{self.api_version}/{endpoint}{'/'.join(args)}"
+        else:
+            return f"{self.base_url}/{self.api_version}/{endpoint}"
+
+    def get_endpoint_json(self, endpoint, school_id, *args, **kwargs):
+        url = self._get_url(endpoint, args)
+
+        self.log.info(f"GET: {url}\nPARAMS: {kwargs}")
+
+        params = kwargs.update(apikey=self.api_key_map[school_id])
+        response = self.get(url=url, params=params)
+
+        return response.json()
+
+
+@resource(
+    config_schema={
+        "subdomain": StringSource,
+        "api_key_map": Map({"school_id": Int, "api_key": String}),
+        "api_version": Field(
+            config=StringSource, default_value="v1", is_required=False
+        ),
+    }
+)
+def deanslist_resource(context: InitResourceContext):
+    return DeansList(logger=context.log, resource_config=context.resource_config)
