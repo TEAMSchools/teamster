20733b2a3fad08e0217b6adbf43379532583f6f3
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-02-22 17:33:21 +0000

    partition assets

--- src/teamster/test/deanslist/assets.py
+++ src/teamster/test/deanslist/assets.py
@@ -1,16 +1,31 @@
-from dagster import config_from_files
+from dagster import DailyPartitionsDefinition, config_from_files
 
 from teamster.core.deanslist.assets import build_deanslist_endpoint_asset
+from teamster.core.utils.variables import LOCAL_TIME_ZONE
 from teamster.test import CODE_LOCATION
 
-config = config_from_files(
-    [f"src/teamster/{CODE_LOCATION}/deanslist/config/assets.yaml"]
+daily_partitions_def = DailyPartitionsDefinition(
+    start_date="2023-02-01", timezone=LOCAL_TIME_ZONE.name
 )
-school_ids = config["school_ids"]
+
+school_ids = config_from_files(
+    [f"src/teamster/{CODE_LOCATION}/deanslist/config/school_ids.yaml"]
+)["school_ids"]
 
 nonpartition_assets = [
     build_deanslist_endpoint_asset(
         code_location=CODE_LOCATION, school_ids=school_ids, **endpoint
     )
-    for endpoint in config["endpoints"]
+    for endpoint in config_from_files(
+        [f"src/teamster/{CODE_LOCATION}/deanslist/config/nonpartition-assets.yaml"]
+    )["endpoints"]
+]
+
+partition_assets = [
+    build_deanslist_endpoint_asset(
+        code_location=CODE_LOCATION, school_ids=school_ids, **endpoint
+    )
+    for endpoint in config_from_files(
+        [f"src/teamster/{CODE_LOCATION}/deanslist/config/partition-assets.yaml"]
+    )["endpoints"]
 ]
