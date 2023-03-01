dd0a4ca7c33507d1faf4ff8ed20fad267410b63f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-01 17:15:51 +0000

    fix import

--- src/teamster/kippnewark/dbt/assets/powerschool.py
+++ src/teamster/kippnewark/dbt/assets/powerschool.py
@@ -2,7 +2,7 @@ from teamster.core.dbt.assets import build_external_source_asset, build_staging_
 from teamster.kippnewark import CODE_LOCATION
 from teamster.kippnewark.powerschool.db.assets import (
     all_assets,
-    hourly_partitions_def,
+    daily_partitions_def,
     nonpartition_assets,
     partition_assets,
 )
@@ -22,7 +22,7 @@ incremental_stg_assets = build_staging_assets(
     manifest_json_path=manifest_json_path,
     key_prefix=key_prefix,
     assets=partition_assets,
-    partitions_def=hourly_partitions_def,
+    partitions_def=daily_partitions_def,
 )
 
 __all__ = src_assets + incremental_stg_assets + nonpartition_stg_assets
