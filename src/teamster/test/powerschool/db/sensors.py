4544a1a84ae4a8137a006a572b201084c379f7b0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-02 16:49:37 +0000

    test sensor

--- src/teamster/test/powerschool/db/sensors.py
+++ src/teamster/test/powerschool/db/sensors.py
@@ -1,4 +1,9 @@
-from dagster import AssetSelection
+from dagster import AssetSelection, SensorEvaluationContext, sensor
+from dagster._core.definitions.asset_reconciliation_sensor import (
+    AssetReconciliationCursor,
+    find_never_materialized_or_requested_root_asset_partitions,
+)
+from dagster._utils.caching_instance_queryer import CachingInstanceQueryer
 
 from teamster.core.powerschool.db.sensors import (
     build_powerschool_incremental_sensor,
@@ -20,4 +25,42 @@ transactiondate_sensor = build_powerschool_incremental_sensor(
     minimum_interval_seconds=3600,
 )
 
-__all__ = [powerschool_ssh_tunnel, whenmodified_sensor, transactiondate_sensor]
+
+@sensor(
+    # name=name,
+    asset_selection=AssetSelection.assets(*assets.whenmodified_assets),
+    # minimum_interval_seconds=minimum_interval_seconds,
+    # description=description,
+    # default_status=default_status,
+)
+def test_dynamic_partition_sensor(context: SensorEvaluationContext):
+    cursor = (
+        AssetReconciliationCursor.from_serialized(
+            context.cursor, context.repository_def.asset_graph
+        )
+        if context.cursor
+        else AssetReconciliationCursor.empty()
+    )
+
+    (
+        never_materialized_or_requested_roots,
+        newly_materialized_root_asset_keys,
+        newly_materialized_root_partitions_by_asset_key,
+    ) = find_never_materialized_or_requested_root_asset_partitions(
+        instance_queryer=CachingInstanceQueryer(instance=context.instance),
+        cursor=cursor,
+        target_asset_selection=AssetSelection.assets(*assets.whenmodified_assets),
+        asset_graph=context.repository_def.asset_graph,
+    )
+
+    context.log.info(never_materialized_or_requested_roots)
+    context.log.info(newly_materialized_root_asset_keys)
+    context.log.info(newly_materialized_root_partitions_by_asset_key)
+
+
+__all__ = [
+    powerschool_ssh_tunnel,
+    whenmodified_sensor,
+    transactiondate_sensor,
+    test_dynamic_partition_sensor,
+]
