b2bf83c397e12a34e3e971ced7ca1d4d5631d8ca
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-02 22:44:14 +0000

    fix job def

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -1,14 +1,11 @@
-from dagster import AssetSelection, DynamicPartitionsDefinition
+from dagster import AssetSelection
 
 from teamster.core.powerschool.db.sensors import build_dynamic_parition_sensor
 from teamster.kippcamden.powerschool.db import assets
 
-dynamic_partitions_def = DynamicPartitionsDefinition(name="partition_column")
-
 test_dynamic_partition_sensor = build_dynamic_parition_sensor(
     name="test_dynamic_partition_sensor",
     asset_selection=AssetSelection.assets(*assets.partition_assets),
-    partitions_def=dynamic_partitions_def,
 )
 
 __all__ = [test_dynamic_partition_sensor]
