0ecc34037e09e515eba4f89d5565d089f22dec6b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-02 23:24:27 +0000

    fix imports

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -6,6 +6,7 @@ from teamster.kippcamden.powerschool.db import assets
 test_dynamic_partition_sensor = build_dynamic_parition_sensor(
     name="test_dynamic_partition_sensor",
     asset_selection=AssetSelection.assets(*assets.partition_assets),
+    partitions_def=assets.dynamic_partitions_def,
 )
 
 __all__ = [test_dynamic_partition_sensor]
