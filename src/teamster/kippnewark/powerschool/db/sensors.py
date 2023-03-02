329909a77d77e79a7b3d148d18c5b96c55fca05e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-02 20:10:40 +0000

    refactor partition col

--- src/teamster/kippnewark/powerschool/db/sensors.py
+++ src/teamster/kippnewark/powerschool/db/sensors.py
@@ -1,23 +1,24 @@
-from dagster import AssetSelection
+# from dagster import AssetSelection
 
-from teamster.core.powerschool.db.sensors import (
-    build_powerschool_incremental_sensor,
-    powerschool_ssh_tunnel,
-)
-from teamster.kippnewark.powerschool.db import assets
+# from teamster.core.powerschool.db.sensors import (
+#     build_powerschool_incremental_sensor,
+#     powerschool_ssh_tunnel,
+# )
+# from teamster.kippnewark.powerschool.db import assets
 
-whenmodified_sensor = build_powerschool_incremental_sensor(
-    name="ps_whenmodified_sensor",
-    asset_selection=AssetSelection.assets(*assets.whenmodified_assets),
-    where_column="whenmodified",
-    minimum_interval_seconds=3600,
-)
+# whenmodified_sensor = build_powerschool_incremental_sensor(
+#     name="ps_whenmodified_sensor",
+#     asset_selection=AssetSelection.assets(*assets.whenmodified_assets),
+#     partition_column="whenmodified",
+#     minimum_interval_seconds=3600,
+# )
 
-transactiondate_sensor = build_powerschool_incremental_sensor(
-    name="ps_transactiondate_sensor",
-    asset_selection=AssetSelection.assets(*assets.transactiondate_assets),
-    where_column="transaction_date",
-    minimum_interval_seconds=3600,
-)
+# transactiondate_sensor = build_powerschool_incremental_sensor(
+#     name="ps_transactiondate_sensor",
+#     asset_selection=AssetSelection.assets(*assets.transactiondate_assets),
+#     partition_column="transaction_date",
+#     minimum_interval_seconds=3600,
+# )
 
-__all__ = [powerschool_ssh_tunnel, whenmodified_sensor, transactiondate_sensor]
+# __all__ = [powerschool_ssh_tunnel, whenmodified_sensor, transactiondate_sensor]
+__all__ = []
