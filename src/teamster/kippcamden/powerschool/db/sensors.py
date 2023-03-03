89c84f480fddf7049f03a8f0a48c09ce4f824d81
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-03 18:07:24 +0000

    rename sensor

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -3,11 +3,11 @@ from dagster import AssetSelection
 from teamster.core.powerschool.db.sensors import build_dynamic_parition_sensor
 from teamster.kippcamden.powerschool.db import assets
 
-test_dynamic_partition_sensor = build_dynamic_parition_sensor(
-    name="test_dynamic_partition_sensor",
+powerschool_dynamic_partition_sensor = build_dynamic_parition_sensor(
+    name="powerschool_dynamic_partition_sensor",
     asset_selection=AssetSelection.assets(*assets.partition_assets),
     partitions_def=assets.dynamic_partitions_def,
     minimum_interval_seconds=300,
 )
 
-__all__ = [test_dynamic_partition_sensor]
+__all__ = [powerschool_dynamic_partition_sensor]
