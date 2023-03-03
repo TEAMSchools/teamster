e40e07506cf46a2eba5d1bd8d971b78b60cb9ca2
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-03 19:47:31 +0000

    rename partiiton

--- src/teamster/kippnewark/datagun/jobs.py
+++ src/teamster/kippnewark/datagun/jobs.py
@@ -6,12 +6,12 @@ from teamster.kippnewark.datagun.assets import (
 )
 
 nps_extract_assets_job = define_asset_job(
-    name="nps_extract_assets_job",
+    name="datagun_nps_extract_assets_job",
     selection=AssetSelection.assets(*nps_extract_assets),
 )
 
 powerschool_extract_assets_job = define_asset_job(
-    name="powerschool_extract_assets_job",
+    name="datagun_powerschool_extract_assets_job",
     selection=AssetSelection.assets(*powerschool_extract_assets),
 )
 
