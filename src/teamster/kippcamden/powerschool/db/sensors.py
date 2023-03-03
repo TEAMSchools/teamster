88f98897c255c5378b846ea25265ab59b77779b6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-03 19:12:05 +0000

    update locations

--- src/teamster/kippcamden/powerschool/db/sensors.py
+++ src/teamster/kippcamden/powerschool/db/sensors.py
@@ -7,7 +7,7 @@ powerschool_dynamic_partition_sensor = build_dynamic_parition_sensor(
     name="powerschool_dynamic_partition_sensor",
     asset_selection=AssetSelection.assets(*assets.partition_assets),
     partitions_def=assets.dynamic_partitions_def,
-    minimum_interval_seconds=300,
+    minimum_interval_seconds=900,
 )
 
 __all__ = [powerschool_dynamic_partition_sensor]
