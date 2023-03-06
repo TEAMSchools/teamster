3864d2f03e8757e1aec2e29b12664299e5758916
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-06 19:48:21 +0000

    update start date

--- src/teamster/kippcamden/deanslist/assets.py
+++ src/teamster/kippcamden/deanslist/assets.py
@@ -18,7 +18,7 @@ static_partitions_def = StaticPartitionsDefinition(school_ids)
 multi_partitions_def = MultiPartitionsDefinition(
     partitions_defs={
         "date": DailyPartitionsDefinition(
-            start_date="2023-03-01", timezone=LOCAL_TIME_ZONE.name
+            start_date="2023-03-06", timezone=LOCAL_TIME_ZONE.name
         ),
         "school": static_partitions_def,
     }
