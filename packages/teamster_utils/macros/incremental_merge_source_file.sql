17b51f8d85cba4501630c2498e5eff2fd1a8cbc5
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-08 23:14:29 +0000

    fix except_cols

--- packages/teamster_utils/macros/incremental_merge_source_file.sql
+++ packages/teamster_utils/macros/incremental_merge_source_file.sql
@@ -3,8 +3,12 @@
 ) -%}
 
 {%- set from_source = source(model.package_name, model.name | replace("stg", "src")) -%}
-{%- set except_cols = except_cols.append(
-    [
+{%- set transform_col_names = transform_cols | map(attribute="name") | list -%}
+
+{%- set except_cols = (
+    except_cols
+    + transform_col_names
+    + [
         "_dagster_partition_fiscal_year",
         "_dagster_partition_date",
         "_dagster_partition_hour",
@@ -12,37 +16,41 @@
     ]
 ) -%}
 
-{%- set star_except = dbt_utils.star(
-    from=from_source, except=transform_cols | map(attribute="name")
+{%- set star = [] -%}
+{%- set star_except = dbt_utils.get_filtered_columns_in_relation(
+    from=from_source, except=except_cols
 ) -%}
 
-{{
+{{-
     config(
         materialized="incremental",
         incremental_strategy="merge",
         unique_key=unique_key,
     )
-}}
+-}}
 
 with
     using_clause as (
         select
             _file_name,
+
             /* column transformations */
             {% for col in transform_cols %}
+            {%- set col_alias = col.alias or dbt_utils.slugify(col.name) -%}
+            {%- do star.append(col_alias) -%}
             {%- if col.transformation == "cast" -%}
-            cast(
-                {{ col.name }} as {{ col.type }}
-            ) as {{ col.alias or dbt_utils.slugify(col.name) }},
+            cast({{ col.name }} as {{ col.type }}) as {{ col_alias }},
             {%- elif col.transformation == "extract" -%}
-            {{ col.name }}.{{ col.type }}
-            as {{ col.alias or dbt_utils.slugify(col.name) }},
-            {%- else -%}
-            {{ col.name }} as {{ col.alias or dbt_utils.slugify(col.name) }},
-            {%- endif %}
+            {{ col.name }}.{{ col.type }} as {{ col_alias }},
+            {%- else -%} {{ col.name }} as {{ col_alias }},
+            {% endif %}
             {% endfor %}
             /* remaining columns */
-            {{- star_except | indent(width=12) }}
+            {% for col in star_except %}
+            {%- set col_alias = dbt_utils.slugify(col) -%}
+            {%- do star.append(col_alias) -%}
+            {{ col }} as {{ col_alias }},
+            {% endfor %}
         from {{ from_source }}
         {% if is_incremental() -%} where _file_name = '{{ file_uri }}' {%- endif %}
     ),
@@ -71,12 +79,12 @@ with
         where {{ unique_key }} not in (select {{ unique_key }} from updates)
     )
 
-select *
+select {% for col in star -%} {{ col }}, {% endfor %}
 from updates
 
 union all
 
-select *
+select {% for col in star -%} {{ col }}, {% endfor %}
 from inserts
 
-{% endmacro %}
+{%- endmacro -%}
