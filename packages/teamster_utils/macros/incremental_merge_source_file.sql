352b887373cfe676fde3fc5f3b51ca204bba9f34
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-08 20:14:05 +0000

    deanslist models

--- packages/teamster_utils/macros/incremental_merge_source_file.sql
+++ packages/teamster_utils/macros/incremental_merge_source_file.sql
@@ -12,7 +12,6 @@
     ]
 ) -%}
 
-{%- set star = dbt_utils.star(from=from_source, except=except_cols) -%}
 {%- set star_except = dbt_utils.star(
     from=from_source, except=transform_cols | map(attribute="name")
 ) -%}
@@ -32,15 +31,20 @@ with
             /* column transformations */
             {% for col in transform_cols %}
             {%- if col.transformation == "cast" -%}
-            cast({{ col.name }} as {{ col.type }}) as {{ col.name }},
+            cast(
+                {{ col.name }} as {{ col.type }}
+            ) as {{ col.alias or dbt_utils.slugify(col.name) }},
             {%- elif col.transformation == "extract" -%}
-            {{ col.name }}.{{ col.type }} as {{ col.name }},
+            {{ col.name }}.{{ col.type }}
+            as {{ col.alias or dbt_utils.slugify(col.name) }},
+            {%- else -%}
+            {{ col.name }} as {{ col.alias or dbt_utils.slugify(col.name) }},
             {%- endif %}
             {% endfor %}
             /* remaining columns */
             {{- star_except | indent(width=12) }}
         from {{ from_source }}
-        {% if is_incremental() -%} where _file_name in ('{{ file_uri }}') {%- endif %}
+        {% if is_incremental() -%} where _file_name = '{{ file_uri }}' {%- endif %}
     ),
 
     deduplicate as (
@@ -67,14 +71,12 @@ with
         where {{ unique_key }} not in (select {{ unique_key }} from updates)
     )
 
-select  --
-    {{ star | indent(width=4) }}
+select *
 from updates
 
 union all
 
-select  --
-    {{ star | indent(width=4) }}
+select *
 from inserts
 
 {% endmacro %}
--- packages/teamster_utils/macros/transform_cols_base_model.sql
+++ packages/teamster_utils/macros/transform_cols_base_model.sql
@@ -1,6 +1,15 @@
 {%- macro transform_cols_base_model(transform_cols=[]) -%}
 
 {%- set from_source = source(model.package_name, model.name | replace("stg", "src")) -%}
+{%- set except_cols = except_cols.append(
+    [
+        "_dagster_partition_fiscal_year",
+        "_dagster_partition_date",
+        "_dagster_partition_hour",
+        "_dagster_partition_minute",
+    ]
+) -%}
+
 {%- set star_except = dbt_utils.star(
     from=from_source, except=transform_cols | map(attribute="name")
 ) -%}
@@ -9,9 +18,12 @@ select
     /* column transformations */
     {% for col in transform_cols %}
     {%- if col.transformation == "cast" -%}
-    cast({{ col.name }} as {{ col.type }}) as {{ col.name }},
+    cast(
+        {{ col.name }} as {{ col.type }}
+    ) as {{ col.alias or dbt_utils.slugify(col.name) }},
     {%- elif col.transformation == "extract" -%}
-    {{ col.name }}.{{ col.type }} as {{ col.name }},
+    {{ col.name }}.{{ col.type }} as {{ col.alias or dbt_utils.slugify(col.name) }},
+    {%- else -%} {{ col.name }} as {{ col.alias or dbt_utils.slugify(col.name) }},
     {%- endif %}
     {% endfor %}
     /* remaining columns */
