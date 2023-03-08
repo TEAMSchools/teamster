a6991ba0f258c48c470a00c7f08e55de6674ddd1
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-08 23:14:29 +0000

    fix except_cols

--- packages/teamster_utils/macros/transform_cols_base_model.sql
+++ packages/teamster_utils/macros/transform_cols_base_model.sql
@@ -1,8 +1,12 @@
 {%- macro transform_cols_base_model(transform_cols=[], except_cols=[]) -%}
 
 {%- set from_source = source(model.package_name, model.name | replace("stg", "src")) -%}
-{%- set except_cols = except_cols.append(
-    [
+{%- set transform_col_names = transform_cols | map(attribute="name") | list -%}
+
+{%- set except_cols = (
+    except_cols
+    + transform_col_names
+    + [
         "_dagster_partition_fiscal_year",
         "_dagster_partition_date",
         "_dagster_partition_hour",
@@ -10,24 +14,25 @@
     ]
 ) -%}
 
-{%- set star_except = dbt_utils.star(
-    from=from_source, except=transform_cols | map(attribute="name")
+{%- set star_except = dbt_utils.get_filtered_columns_in_relation(
+    from=from_source, except=except_cols
 ) -%}
 
 select
     /* column transformations */
     {% for col in transform_cols %}
+    {%- set col_alias = col.alias or dbt_utils.slugify(col.name) -%}
     {%- if col.transformation == "cast" -%}
-    cast(
-        {{ col.name }} as {{ col.type }}
-    ) as {{ col.alias or dbt_utils.slugify(col.name) }},
+    cast({{ col.name }} as {{ col.type }}) as {{ col_alias }},
     {%- elif col.transformation == "extract" -%}
-    {{ col.name }}.{{ col.type }} as {{ col.alias or dbt_utils.slugify(col.name) }},
-    {%- else -%} {{ col.name }} as {{ col.alias or dbt_utils.slugify(col.name) }},
-    {%- endif %}
+    {{ col.name }}.{{ col.type }} as {{ col_alias }},
+    {%- else -%} {{ col.name }} as {{ col_alias }},
+    {% endif %}
     {% endfor %}
     /* remaining columns */
-    {{ star_except | indent(width=4) }}
+    {% for col in star_except %}
+    {%- set col_alias = dbt_utils.slugify(col) -%} {{ col }} as {{ col_alias }},
+    {% endfor %}
 from {{ from_source }}
 
 {%- endmacro -%}
