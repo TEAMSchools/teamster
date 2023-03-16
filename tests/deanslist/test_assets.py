388016f62f6ff7ef6e3d34490f438cd054191f12
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-16 20:56:21 +0000

    remove improt

--- tests/deanslist/test_assets.py
+++ tests/deanslist/test_assets.py
@@ -5,16 +5,14 @@ from teamster.core.resources.deanslist import deanslist_resource
 from teamster.core.resources.google import gcs_avro_io_manager
 from teamster.test.deanslist import assets
 
-CODE_LOCATION = "test"
-
 deanslist_endpoint_asset = with_resources(
-    definitions=[*assets.nonpartition_assets],
+    definitions=[*assets.__all__],
     resource_defs={
         "deanslist": deanslist_resource.configured(
             config_from_files(
                 [
                     "src/teamster/core/resources/config/deanslist.yaml",
-                    f"src/teamster/{CODE_LOCATION}/resources/config/deanslist.yaml",
+                    "src/teamster/test/resources/config/deanslist.yaml",
                 ]
             )
         ),
@@ -22,13 +20,12 @@ deanslist_endpoint_asset = with_resources(
             config_from_files(["src/teamster/core/resources/config/gcs.yaml"])
         ),
         "gcs_avro_io": gcs_avro_io_manager.configured(
-            config_from_files(
-                [f"src/teamster/{CODE_LOCATION}/resources/config/io.yaml"]
-            )
+            config_from_files(["src/teamster/test/resources/config/io.yaml"])
         ),
     },
 )
 
 
 def test_powerschool_table_asset():
-    deanslist_endpoint_asset[0](build_op_context())
+    for asset in deanslist_endpoint_asset:
+        asset(build_op_context())
