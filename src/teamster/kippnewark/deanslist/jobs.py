7c0103ca9c0329668db2a247b1211b9216b18c99
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-17 20:50:33 +0000

    rename, add schedules

--- src/teamster/kippnewark/deanslist/jobs.py
+++ src/teamster/kippnewark/deanslist/jobs.py
@@ -3,7 +3,7 @@ from dagster import AssetSelection, define_asset_job
 from . import assets
 
 deanslist_static_partition_assets_job = define_asset_job(
-    name="deanslist_school_partition_assets_job",
+    name="deanslist_static_partition_assets_job",
     selection=AssetSelection.assets(*assets.static_partition_assets),
     partitions_def=assets.static_partitions_def,
 )
