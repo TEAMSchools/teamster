56c2c68a28b8446e88cffdb361f1d8b97fdcdabb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-21 16:56:32 +0000

    fix sensor

--- .pdm/scripts/helm-install.sh
+++ .pdm/scripts/helm-install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+kubectl create secret generic dagster-cloud-agent-token \
+  --save-config \
+  --dry-run=client \
+  --namespace=dagster-cloud \
+  --from-literal=DAGSTER_CLOUD_AGENT_TOKEN="${DAGSTER_CLOUD_AGENT_TOKEN}" \
+  --output=yaml |
+  kubectl apply -f -
+
 helm repo add dagster-cloud https://dagster-io.github.io/helm-user-cloud
 helm repo update
 
