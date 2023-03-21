770fac00609ad142ab781be6d0679e5e59b4fb78
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-21 17:16:04 +0000

    rm env

--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -14,29 +14,3 @@ sudo pdm self update
 
 # update trunk
 trunk upgrade -y --no-progress
-
-# save KIPP* github codespace secrets to .env files
-for envvar in $(compgen -A variable | grep "^KIPP"); do
-  envvar_lower="${envvar,,}"
-
-  if [[ ${envvar_lower} != *"_"* ]]; then
-    mkdir -p "./env/${envvar_lower}"
-    echo "${!envvar}" >"./env/${envvar_lower}/.env"
-  else
-    IFS='_' read -ra VAR <<<"${envvar_lower}"
-    fpath=""
-    for i in "${VAR[@]}"; do
-      fpath+="${i}/"
-    done
-
-    mkdir -p -- "./env/${fpath%/*}"
-    echo "${!envvar}" >"./env/${fpath}rsa-private-key"
-  fi
-
-done
-
-mkdir -p ./env/staging
-echo "${STAGING}" >./env/staging/.env
-
-mkdir -p ./env/test
-echo "${TEST}" >./env/test/.env
