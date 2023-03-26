3a24c202aab3675268f3b40f860296d4a3e8b4a1
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-26 16:40:35 +0000

    update schema

--- .pdm/scripts/secrets.sh
+++ .pdm/scripts/secrets.sh
@@ -9,6 +9,9 @@ file_envvars=(
   "EGENCIA_RSA_PRIVATE_KEY"
   "GCLOUD_SERVICE_ACCOUNT_KEY"
   "DBT_USER_CREDS"
+  "USE_GKE_GCLOUD_AUTH_PLUGIN"
+  "GCP_PROJECT_ID"
+  "GCP_REGION"
 )
 
 # clear env file
@@ -16,30 +19,34 @@ file_envvars=(
 
 while read -ra array; do
   envvar="${array[0]}"
-  # envvar_lower="${envvar,,}"
-  # secret_name="${envvar_lower//_/-}"
 
   if [[ ! ${file_envvars[*]} =~ ${envvar} ]]; then
     echo "${envvar}"="${!envvar}" >>env/.env
-
-    # kubectl create secret generic "${secret_name}" \
-    #   --save-config \
-    #   --dry-run=client \
-    #   --namespace=dagster-cloud \
-    #   --from-literal="${envvar}"="${!envvar}" \
-    #   --output=yaml |
-    #   kubectl apply -f - ||
-    #   true
   fi
 done < <(gh secret list --app codespaces || true)
 
-# kubectl create secret generic "secret-files" \
-#   --save-config \
-#   --dry-run=client \
-#   --namespace=dagster-cloud \
-#   --from-literal="id_rsa_egencia"="${EGENCIA_RSA_PRIVATE_KEY}" \
-#   --from-literal="dbt_user_creds_json"="${DBT_USER_CREDS}" \
-#   --from-literal="deanslist_api_key_map_yaml"="${DEANSLIST_API_KEY_MAP}" \
-#   --output=yaml |
-#   kubectl apply -f - ||
-#   true
+while IFS="=" read -ra envvar || [[ -n ${envvar[*]} ]]; do
+  envvar_lower="${envvar[0],,}"
+
+  secret_name="${envvar_lower//_/-}"
+
+  kubectl create secret generic "${secret_name}" \
+    --save-config \
+    --dry-run=client \
+    --namespace=dagster-cloud \
+    --from-literal="${envvar}"="${!envvar}" \
+    --output=yaml |
+    kubectl apply -f - ||
+    true
+done <env/.env
+
+kubectl create secret generic "secret-files" \
+  --save-config \
+  --dry-run=client \
+  --namespace=dagster-cloud \
+  --from-literal="id_rsa_egencia"="${EGENCIA_RSA_PRIVATE_KEY}" \
+  --from-literal="dbt_user_creds_json"="${DBT_USER_CREDS}" \
+  --from-literal="deanslist_api_key_map_yaml"="${DEANSLIST_API_KEY_MAP}" \
+  --output=yaml |
+  kubectl apply -f - ||
+  true
