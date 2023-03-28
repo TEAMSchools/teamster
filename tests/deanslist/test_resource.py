a5f70986eb673674655e965747336a7bd835bb61
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-03-28 23:31:24 +0000

    finish schema

--- tests/deanslist/test_resource.py
+++ tests/deanslist/test_resource.py
@@ -4,39 +4,44 @@ import random
 from dagster import build_resources, config_from_files
 from fastavro import parse_schema, validation, writer
 
-from teamster.core.resources.schoolmint import Grow, schoolmint_grow_resource
-from teamster.core.schoolmint.schema import ENDPOINT_FIELDS, get_avro_record_schema
+from teamster.core.deanslist.schema import ENDPOINT_FIELDS, get_avro_record_schema
+from teamster.core.resources.deanslist import DeansList, deanslist_resource
 
-asset_config = config_from_files(["tests/config/schoolmint.yaml"])
+TEST_SCHOOL_ID = 126
+
+asset_config = config_from_files(["tests/config/deanslist.yaml"])
+resource_config = config_from_files(
+    ["src/teamster/core/config/resources/deanslist.yaml"]
+)
 
 
-def test_schoolmint_grow_schema():
+def test_deanslist_schema():
     with build_resources(
-        resources={"grow": schoolmint_grow_resource},
-        resource_config={
-            "grow": {
-                "config": config_from_files(
-                    ["src/teamster/core/config/resources/schoolmint.yaml"]
-                )
-            }
-        },
+        resources={"deanslist": deanslist_resource},
+        resource_config={"deanslist": {"config": resource_config}},
     ) as resources:
-        grow: Grow = resources.grow
+        dl: DeansList = resources.deanslist
 
         for endpoint in asset_config["endpoints"]:
             endpoint_name = endpoint["asset_name"]
+            endpoint_version = endpoint["api_version"]
 
             parsed_schema = parse_schema(
                 get_avro_record_schema(
-                    name=endpoint_name, fields=ENDPOINT_FIELDS[endpoint_name]
+                    name=endpoint_name,
+                    fields=ENDPOINT_FIELDS[endpoint_name],
+                    version=endpoint_version,
                 )
             )
 
-            data = grow.get(endpoint=endpoint_name, **endpoint.get("params", {}))
+            row_count, records = dl.get_endpoint(
+                endpoint=endpoint_name,
+                api_version=endpoint_version,
+                school_id=TEST_SCHOOL_ID,
+                **endpoint.get("params", {}),
+            )
 
-            records = data["data"]
-
-            sample_record = records[random.randint(a=0, b=(data["count"] - 1))]
+            sample_record = records[random.randint(a=0, b=(row_count - 1))]
             # sample_record = [
             #     r for r in records if "" in json.dumps(r)
             # ]
@@ -50,7 +55,7 @@ def test_schoolmint_grow_schema():
                 records=records, schema=parsed_schema, strict=True
             )
 
-            with open(file="env/schoolmint_grow_test.json", mode="w+") as f:
+            with open(file="env/deanslist_test.json", mode="w+") as f:
                 json.dump(obj=records, fp=f)
 
             with open(file="/dev/null", mode="wb") as fo:
