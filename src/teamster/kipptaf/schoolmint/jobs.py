43e40908d827a73112f90702f5103c7a4fa895c5
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-06 19:20:31 +0000

    explicit partitions def

--- src/teamster/kipptaf/schoolmint/jobs.py
+++ src/teamster/kipptaf/schoolmint/jobs.py
@@ -5,13 +5,13 @@ from . import assets
 static_partition_asset_job = define_asset_job(
     name="schoolmint_grow_static_partition_asset_job",
     selection=AssetSelection.assets(*assets.static_partition_assets),
-    # partitions_def=assets.static_partitions_def,
+    partitions_def=assets.static_partitions_def,
 )
 
 multi_partition_asset_job = define_asset_job(
     name="schoolmint_grow_multi_partition_asset_job",
     selection=AssetSelection.assets(*assets.multi_partition_assets),
-    # partitions_def=assets.multi_partitions_def,
+    partitions_def=assets.multi_partitions_def,
 )
 
 __all__ = [
