335bbdd3b76f170cc94fe562b0b9e545fa60fccc
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-06 17:31:46 +0000

    fix schema

--- tests/schoolmint/test_resource.py
+++ tests/schoolmint/test_resource.py
@@ -38,10 +38,7 @@ def test_schoolmint_grow_schema():
             records = data["data"]
 
             sample_record = records[random.randint(a=0, b=(data["count"] - 1))]
-            # sample_record = [
-            #     r for r in records if "observationTypesHidden" in json.dumps(r)
-            # ]
-            print(sample_record)
+            # sample_record = [r for r in records if "" in json.dumps(r)]
 
             assert validation.validate(
                 datum=sample_record, schema=parsed_schema, strict=True
