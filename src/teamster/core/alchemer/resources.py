f96c660282d82d6dd6bd587d40a47620c1cdacdb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-12 20:30:04 +0000

    convert to classic resource

--- src/teamster/core/alchemer/resources.py
+++ src/teamster/core/alchemer/resources.py
@@ -1,3 +1,20 @@
 from alchemer import AlchemerSession
+from dagster import InitResourceContext, StringSource, resource
 
-alchemer_resource = AlchemerSession
+from teamster.core.utils.variables import LOCAL_TIME_ZONE
+
+
+@resource(
+    config_schema={
+        "api_token": StringSource,
+        "api_token_secret": StringSource,
+        "api_version": StringSource,
+    }
+)
+def alchemer_resource(context: InitResourceContext):
+    return AlchemerSession(
+        api_token=context.resource_config["api_token"],
+        api_token_secret=context.resource_config["api_token_secret"],
+        api_version=context.resource_config["api_version"],
+        time_zone=LOCAL_TIME_ZONE,
+    )
