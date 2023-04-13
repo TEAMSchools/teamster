a6c8ccd37b4bd4d1c9edd03581751dcfd4a20e16
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-13 02:19:18 +0000

    break up dq

--- src/teamster/kipptaf/alchemer/assets.py
+++ src/teamster/kipptaf/alchemer/assets.py
@@ -1,8 +1,12 @@
 from teamster.core.alchemer.assets import build_partition_assets
 from teamster.kipptaf import CODE_LOCATION
 
-partition_assets = build_partition_assets(code_location=CODE_LOCATION)
+survey_assets, survey_response, survey_response_disqualified = build_partition_assets(
+    code_location=CODE_LOCATION
+)
 
 __all__ = [
-    *partition_assets,
+    survey_assets,
+    survey_response,
+    survey_response_disqualified,
 ]
