8601e5dc5fa28ed0b93d0de3df2b2bda25f7eccd
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-14 16:26:26 +0000

    rename ps sensors

--- src/teamster/kippnewark/powerschool/sensors.py
+++ src/teamster/kippnewark/powerschool/sensors.py
@@ -5,7 +5,7 @@ from . import assets
 
 powerschool_dynamic_partition_sensor = build_dynamic_partition_sensor(
     code_location=CODE_LOCATION,
-    name="powerschool_dynamic_partition_sensor",
+    name=f"{CODE_LOCATION}_powerschool_dynamic_partition_sensor",
     asset_defs=[*assets.partition_assets],
     minimum_interval_seconds=900,
 )
