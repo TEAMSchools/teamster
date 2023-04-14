ea4705064bf5c4ae34748778f21ee2de646842c4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-14 16:09:46 +0000

    rename sensors

--- src/teamster/kipptaf/dbt/sensors.py
+++ src/teamster/kipptaf/dbt/sensors.py
@@ -1,10 +1,11 @@
 from dagster import AssetSelection, build_asset_reconciliation_sensor
 
+from .. import CODE_LOCATION
 from . import assets
 
 asset_reconciliation_sensor = build_asset_reconciliation_sensor(
     asset_selection=AssetSelection.assets(*assets.__all__),
-    name="dbt_asset_reconciliation_sensor",
+    name=f"{CODE_LOCATION}_dbt_asset_reconciliation_sensor",
 )
 
 __all__ = [
