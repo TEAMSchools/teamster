7e3b3c902c166f803f924bc761db98c0d85aa833
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-14 16:20:18 +0000

    update sensors

--- src/teamster/kippmiami/dbt/assets.py
+++ src/teamster/kippmiami/dbt/assets.py
@@ -2,18 +2,28 @@ from teamster.core.dbt.assets import build_external_source_asset, build_staging_
 
 from .. import CODE_LOCATION, deanslist, powerschool
 
-dbt_src_assets = [
-    build_external_source_asset(a)
-    for a in [*powerschool.assets.__all__, *deanslist.assets.__all__]
+powerschool_dbt_src_assets = [
+    build_external_source_asset(a) for a in [*powerschool.assets.__all__]
 ]
 
-dbt_stg_assets = build_staging_assets(
+powerschool_dbt_stg_assets = build_staging_assets(
     manifest_json_path=f"teamster-dbt/{CODE_LOCATION}/target/manifest.json",
     key_prefix=[CODE_LOCATION, "dbt"],
     assets=[*powerschool.assets.__all__],
 )
 
+deanslist_dbt_src_assets = [
+    build_external_source_asset(a) for a in [*deanslist.assets.__all__]
+]
+
+deanslist_dbt_stg_assets = build_staging_assets(
+    manifest_json_path=f"teamster-dbt/{CODE_LOCATION}/target/manifest.json",
+    key_prefix=[CODE_LOCATION, "dbt"],
+    assets=[*deanslist.assets.__all__],
+)
+
 __all__ = [
-    *dbt_src_assets,
-    *dbt_stg_assets,
+    *powerschool_dbt_src_assets,
+    *powerschool_dbt_stg_assets,
+    *deanslist_dbt_src_assets,
 ]
