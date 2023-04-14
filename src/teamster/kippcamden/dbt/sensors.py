bb31b68eaa9730d32db4775ca9586efc12ef2973
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-14 16:09:46 +0000

    rename sensors

--- src/teamster/kippcamden/dbt/sensors.py
+++ src/teamster/kippcamden/dbt/sensors.py
@@ -1,20 +1,21 @@
 from dagster import AssetSelection, build_asset_reconciliation_sensor
 
+from .. import CODE_LOCATION
 from . import assets
 
 powerschool_dbt_src_asset_reconciliation_sensor = build_asset_reconciliation_sensor(
     asset_selection=AssetSelection.assets(*assets.powerschool_dbt_src_assets),
-    name="powerschool_dbt_src_asset_reconciliation_sensor",
+    name=f"{CODE_LOCATION}_powerschool_dbt_src_asset_reconciliation_sensor",
 )
 
 powerschool_dbt_stg_asset_reconciliation_sensor = build_asset_reconciliation_sensor(
     asset_selection=AssetSelection.assets(*assets.powerschool_dbt_stg_assets),
-    name="powerschool_dbt_stg_asset_reconciliation_sensor",
+    name=f"{CODE_LOCATION}_powerschool_dbt_stg_asset_reconciliation_sensor",
 )
 
 deanslist_dbt_src_asset_reconciliation_sensor = build_asset_reconciliation_sensor(
     asset_selection=AssetSelection.assets(*assets.deanslist_dbt_src_assets),
-    name="deanslist_dbt_src_asset_reconciliation_sensor",
+    name=f"{CODE_LOCATION}_deanslist_dbt_src_asset_reconciliation_sensor",
 )
 
 __all__ = [
