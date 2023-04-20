6155a97a69edd54d8eefb0a0c1eba50c90c0f60a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-20 16:24:04 +0000

    add sensor

--- src/teamster/kipptaf/alchemer/sensors.py
+++ src/teamster/kipptaf/alchemer/sensors.py
@@ -1,12 +1,20 @@
-from teamster.core.alchemer.sensors import build_survey_metadata_asset_sensor
+from teamster.core.alchemer.sensors import (
+    build_survey_metadata_asset_sensor,
+    build_survey_response_asset_sensor,
+)
 
 from .. import CODE_LOCATION
-from .assets import survey_metadata_assets
+from .assets import survey_metadata_assets, survey_response
 
 survey_metadata_asset_sensor = build_survey_metadata_asset_sensor(
     code_location=CODE_LOCATION, asset_defs=survey_metadata_assets
 )
 
+survey_response_asset_sensor = build_survey_response_asset_sensor(
+    code_location=CODE_LOCATION, asset_def=survey_response
+)
+
 __all__ = [
     survey_metadata_asset_sensor,
+    survey_response_asset_sensor,
 ]
