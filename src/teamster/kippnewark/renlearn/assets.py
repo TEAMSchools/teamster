8c2f5d835d751eca5bfd447e68d24df22aac585b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-04-24 19:20:26 +0000

    directly config resource

--- src/teamster/kippnewark/renlearn/assets.py
+++ src/teamster/kippnewark/renlearn/assets.py
@@ -1,5 +1,4 @@
-from dagster import ResourceParam, config_from_files
-from dagster_ssh import SSHResource
+from dagster import config_from_files
 
 from teamster.core.renlearn.assets import build_sftp_asset
 
@@ -8,12 +7,7 @@ from .. import CODE_LOCATION
 config_dir = f"src/teamster/{CODE_LOCATION}/config/assets/renlearn"
 
 sftp_assets = [
-    build_sftp_asset(
-        sftp_renlearn=ResourceParam[SSHResource],
-        code_location=CODE_LOCATION,
-        source_system="renlearn",
-        **a,
-    )
+    build_sftp_asset(code_location=CODE_LOCATION, source_system="renlearn", **a)
     for a in config_from_files([f"{config_dir}/assets.yaml"])["assets"]
 ]
 
