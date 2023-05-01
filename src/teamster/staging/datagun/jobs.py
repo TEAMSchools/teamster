1cdcc6710b494d7c037d3189fc70656f996abc67
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-01 14:43:14 +0000

    update deps

--- src/teamster/staging/datagun/jobs.py
+++ src/teamster/staging/datagun/jobs.py
@@ -1,10 +1,10 @@
 from dagster import AssetSelection, define_asset_job
 
-from . import assets
+from .assets import gsheet_extract_assets, sftp_extract_assets
 
 test_extract_asset_job = define_asset_job(
     name="datagun_test_extract_asset_job",
-    selection=AssetSelection.assets(*assets.__all__),
+    selection=AssetSelection.assets(*gsheet_extract_assets, *sftp_extract_assets),
 )
 
 __all__ = [
--- src/teamster/staging/powerschool/sensors.py
+++ src/teamster/staging/powerschool/sensors.py
@@ -1,12 +1,12 @@
 from teamster.core.powerschool.sensors import build_dynamic_partition_sensor
 
 from .. import CODE_LOCATION
-from . import assets
+from .assets import partition_assets
 
 powerschool_dynamic_partition_sensor = build_dynamic_partition_sensor(
     code_location=CODE_LOCATION,
-    name="powerschool_dynamic_partition_sensor",
-    asset_defs=[*assets.partition_assets],
+    name=f"{CODE_LOCATION}_powerschool_dynamic_partition_sensor",
+    asset_defs=[*partition_assets],
     minimum_interval_seconds=3600,
 )
 
