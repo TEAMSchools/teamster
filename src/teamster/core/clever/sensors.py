0e213e3b8df16c9f6edffecf9a34ad0704d06774
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-02 22:55:09 +0000

    return reqs

--- src/teamster/core/clever/sensors.py
+++ src/teamster/core/clever/sensors.py
@@ -88,9 +88,9 @@ def build_sftp_sensor(
                 cursor[remote_filepath] = now.timestamp()
 
         return SensorResult(
-            # run_requests=run_requests,
+            run_requests=run_requests,
             cursor=json.dumps(obj=cursor),
-            # dynamic_partitions_requests=dynamic_partitions_requests,
+            dynamic_partitions_requests=dynamic_partitions_requests,
         )
 
     return _sensor
