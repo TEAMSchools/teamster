788622d5111c1b5628a1649df839b282a48f56c2
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-03 20:44:47 +0000

    test sensor w/o tunnel

--- src/teamster/kippmiami/docker-entrypoint.sh
+++ src/teamster/kippmiami/docker-entrypoint.sh
@@ -5,7 +5,7 @@ sshpass -p "${KIPPMIAMI_PS_SSH_PASSWORD}" \
   ssh "${KIPPMIAMI_PS_SSH_USERNAME}"@"${KIPPMIAMI_PS_SSH_HOST}" \
   -p "${KIPPMIAMI_PS_SSH_PORT}" \
   -L 1521:"${KIPPMIAMI_PS_SSH_REMOTE_BIND_HOST}":1521 \
-  -fN
+  -o StrictHostKeyChecking=no -4fN
 
 # Run the passed-in Dagster CMD
 exec "$@"
