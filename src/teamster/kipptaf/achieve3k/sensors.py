c163a0f4b8993970906d8624f5b5e7a08af5dfe4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-08 15:31:19 +0000

    fixes

--- src/teamster/kipptaf/achieve3k/sensors.py
+++ src/teamster/kipptaf/achieve3k/sensors.py
@@ -3,7 +3,9 @@ from teamster.core.achieve3k.sensors import build_sftp_sensor
 from .. import CODE_LOCATION
 from . import assets
 
-sftp_sensor = build_sftp_sensor(code_location=CODE_LOCATION, asset_defs=assets)
+sftp_sensor = build_sftp_sensor(
+    code_location=CODE_LOCATION, source_system="achieve3k", asset_defs=assets
+)
 
 __all__ = [
     sftp_sensor,
