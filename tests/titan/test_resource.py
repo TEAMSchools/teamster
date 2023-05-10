bc653bdfbde31f5b82c659d18189a75199b41071
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-10 04:01:21 +0000

    titan + refactor sftp

---
+++ tests/titan/test_resource.py
@@ -0,0 +1,87 @@
+import random
+import re
+
+from dagster import build_resources, config_from_files
+from dagster_ssh import ssh_resource
+from fastavro import parse_schema, validation, writer
+from numpy import nan
+from pandas import read_csv
+from slugify import slugify
+
+from teamster.core.titan.schema import ASSET_FIELDS
+from teamster.core.utils.functions import get_avro_record_schema
+
+SOURCE_SYSTEM = "titan"
+CODE_LOCATION = "kippnewark"
+# "kippcamden"
+REMOTE_FILEPATH = "."
+REMOTE_FILE_REGEX = r"incomeformdata(?P<fiscal_year>\d{4})\.csv"
+# r"persondata(?P<fiscal_year>\d{4})\.csv"
+ASSET_NAME = "income_form_data"
+# "person_data"
+
+CONFIG_PATH = f"src/teamster/{CODE_LOCATION}/config/resources"
+
+
+def test_schema():
+    with build_resources(
+        resources={"sftp": ssh_resource},
+        resource_config={
+            "sftp": {
+                "config": config_from_files(
+                    [f"{CONFIG_PATH}/sftp_{SOURCE_SYSTEM}.yaml"]
+                )
+            }
+        },
+    ) as resources:
+        conn = resources.sftp.get_connection()
+        with conn.open_sftp() as sftp_client:
+            ls = sftp_client.listdir_attr(path=REMOTE_FILEPATH)
+        conn.close()
+
+        file_matches = [
+            f
+            for f in ls
+            if re.match(pattern=REMOTE_FILE_REGEX, string=f.filename) is not None
+        ]
+
+        for f in file_matches:
+            local_filepath = resources.sftp.sftp_get(
+                remote_filepath=f"{REMOTE_FILEPATH}/{f.filename}",
+                local_filepath=f"./env/{f.filename}",
+            )
+
+            df = read_csv(filepath_or_buffer=local_filepath, low_memory=False)
+            df = df.replace({nan: None})
+            df.rename(columns=lambda x: slugify(text=x, separator="_"), inplace=True)
+
+            count = df.shape[0]
+            records = df.to_dict(orient="records")
+            print(df.dtypes.to_dict())
+
+            sample_record = records[random.randint(a=0, b=(count - 1))]
+            print(sample_record)
+
+            schema = get_avro_record_schema(
+                name=ASSET_NAME, fields=ASSET_FIELDS[ASSET_NAME]
+            )
+            # print(schema)
+
+            parsed_schema = parse_schema(schema)
+
+            assert validation.validate(
+                datum=sample_record, schema=parsed_schema, strict=True
+            )
+
+            assert validation.validate_many(
+                records=records, schema=parsed_schema, strict=True
+            )
+
+            with open(file="/dev/null", mode="wb") as fo:
+                writer(
+                    fo=fo,
+                    schema=parsed_schema,
+                    records=records,
+                    codec="snappy",
+                    strict_allow_default=True,
+                )
