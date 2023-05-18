27b7d78d0040f1641527c7a6dc61a770343baea7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-18 14:49:38 +0000

    fix name

--- src/teamster/kipptaf/adp/jobs.py
+++ src/teamster/kipptaf/adp/jobs.py
@@ -2,15 +2,13 @@ from dagster import AssetSelection, define_asset_job
 
 from .assets import wfm_assets_daily
 
-daily_partition_asset_jobs = [
-    define_asset_job(
-        name="deanslist_static_partition_asset_job",
-        selection=AssetSelection.assets(asset),
-        partitions_def=asset.partitions_def,
-    )
-    for asset in wfm_assets_daily
-]
+daily_partition_asset_job = define_asset_job(
+    name="adp_daily_partition_asset_job",
+    selection=AssetSelection.assets(*wfm_assets_daily),
+    partitions_def=wfm_assets_daily[0].partitions_def,
+)
+
 
 __all__ = [
-    *daily_partition_asset_jobs,
+    daily_partition_asset_job,
 ]
--- src/teamster/kipptaf/adp/schedules.py
+++ src/teamster/kipptaf/adp/schedules.py
@@ -1,12 +1,12 @@
 from dagster import build_schedule_from_partitioned_job
 
-from .jobs import daily_partition_asset_jobs
+from .jobs import daily_partition_asset_job
 
-daily_partition_asset_job_schedules = [
-    build_schedule_from_partitioned_job(job=job, hour_of_day=0, minute_of_hour=0)
-    for job in daily_partition_asset_jobs
-]
+daily_partition_asset_job_schedule = build_schedule_from_partitioned_job(
+    job=daily_partition_asset_job, hour_of_day=0, minute_of_hour=0
+)
+
 
 __all__ = [
-    *daily_partition_asset_job_schedules,
+    daily_partition_asset_job_schedule,
 ]
