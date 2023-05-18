e8b702f1c766b7496b6ed165c45a3f59df8feb0a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-18 16:28:30 +0000

    wfm schedule

--- src/teamster/kipptaf/adp/jobs.py
+++ src/teamster/kipptaf/adp/jobs.py
@@ -1,6 +1,6 @@
 from dagster import AssetSelection, define_asset_job
 
-from .assets import wfm_assets_daily
+from .assets import wfm_assets_daily, wfm_assets_dynamic
 
 daily_partition_asset_job = define_asset_job(
     name="adp_daily_partition_asset_job",
@@ -8,7 +8,13 @@ daily_partition_asset_job = define_asset_job(
     partitions_def=wfm_assets_daily[0].partitions_def,
 )
 
+dynamic_partition_asset_job = define_asset_job(
+    name="adp_dynamic_partition_asset_job",
+    selection=AssetSelection.assets(*wfm_assets_dynamic),
+    partitions_def=wfm_assets_dynamic[0].partitions_def,
+)
 
 __all__ = [
     daily_partition_asset_job,
+    dynamic_partition_asset_job,
 ]
