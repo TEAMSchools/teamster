b0e95d7545d21e91010e183e5f537900ea0a4a20
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-18 14:12:52 +0000

    asset selection

--- src/teamster/kipptaf/adp/sensors.py
+++ src/teamster/kipptaf/adp/sensors.py
@@ -1,12 +1,12 @@
 from teamster.core.adp.sensors import build_sftp_sensor
 
 from .. import CODE_LOCATION
-from . import assets
+from .assets import sftp_assets
 
 sftp_sensor = build_sftp_sensor(
     code_location=CODE_LOCATION,
     source_system="adp",
-    asset_defs=assets,
+    asset_defs=sftp_assets,
     minimum_interval_seconds=600,
 )
 
