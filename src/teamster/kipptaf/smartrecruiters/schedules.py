86be502fd0baf5b0b21d533d6cd0a1f14e130ed9
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-19 16:09:41 +0000

    sr sched

---
+++ src/teamster/kipptaf/smartrecruiters/schedules.py
@@ -0,0 +1,14 @@
+from dagster import ScheduleDefinition
+
+from .. import LOCAL_TIMEZONE
+from .jobs import smartrecruiters_report_asset_job
+
+smartrecruiters_report_assets_schedule = ScheduleDefinition(
+    job=smartrecruiters_report_asset_job,
+    cron_schedule="0 6 * * *",
+    execution_timezone=LOCAL_TIMEZONE.name,
+)
+
+__all__ = [
+    smartrecruiters_report_assets_schedule,
+]
