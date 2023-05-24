da33f2897a2c8db180b0a06d3430696fe2d2e96e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-24 22:06:37 +0000

    debug

--- src/teamster/core/google/assets.py
+++ src/teamster/core/google/assets.py
@@ -11,22 +11,6 @@ def build_gsheet_asset(name, code_location, sheet_id):
     )
     def _asset(context: OpExecutionContext, gsheets: GoogleSheetsResource):
         spreadsheet = gsheets.open(sheet_id=sheet_id)
-        context.log.debug(spreadsheet._properties)
-
-        named_ranges = spreadsheet.list_named_ranges()
-        context.log.debug(named_ranges)
-
-        named_range_match = [
-            nr for nr in spreadsheet.list_named_ranges() if nr["name"] == name
-        ]
-
-        if named_range_match:
-            worksheet = spreadsheet.get_worksheet_by_id(
-                id=named_range_match[0]["range"].get("sheetId", 0)
-            )
-        else:
-            worksheet = spreadsheet.sheet1
-
-        context.log.debug(worksheet._properties)
+        context.log.debug(spreadsheet.lastUpdateTime)
 
     return _asset
