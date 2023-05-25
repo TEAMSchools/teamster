91ae2dbef0f051c7ac7b260d4d0f4542d745e37b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-25 19:16:20 +0000

    debug

--- tests/google/test_resources.py
+++ tests/google/test_resources.py
@@ -1,15 +1,15 @@
 from dagster import build_resources
-from google.oauth2.service_account import Credentials
-from gspread import Spreadsheet
 
 from teamster.core.google.resources.sheets import GoogleSheetsResource
 
 
 def test_gsheet_resource():
     with build_resources(resources={"gsheets": GoogleSheetsResource()}) as resources:
-        spreadsheet: Spreadsheet = resources.gsheets.open(
+        gsheets: GoogleSheetsResource = resources.gsheets
+
+        print(vars(gsheets._client.auth))
+
+        spreadsheet = gsheets.open(
             sheet_id="1xSa3dznVaGeqjo3Y0tS9GzkhVrpeCQ0aaKWlgI3kHik"
         )
-        creds: Credentials = spreadsheet.client.auth
-        print(creds.service_account_email)
         print(spreadsheet.lastUpdateTime)
