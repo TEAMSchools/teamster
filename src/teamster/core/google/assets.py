91df80a3f7562948b84ee6cf196c13b9a161f0d6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-25 15:16:12 +0000

    fix params

--- src/teamster/core/google/assets.py
+++ src/teamster/core/google/assets.py
@@ -1,21 +1,22 @@
-from dagster import OpExecutionContext, asset
+import pendulum
+from dagster import DataVersion, observable_source_asset
 
 from teamster.core.google.resources.sheets import GoogleSheetsResource
 
 
 def build_gsheet_asset(name, code_location, sheet_id):
-    @asset(
+    @observable_source_asset(
         name=name,
         key_prefix=[code_location, "gsheets"],
         metadata={"sheet_id": sheet_id},
     )
-    def _asset(context: OpExecutionContext, gsheets: GoogleSheetsResource):
-        latest_materialization_event = (
-            context.instance.get_latest_materialization_event(context.assets_def.key)
-        )
-        context.log.debug(latest_materialization_event)
-
+    def _asset(gsheets: GoogleSheetsResource):
         spreadsheet = gsheets.open(sheet_id=sheet_id)
-        context.log.debug(spreadsheet.lastUpdateTime)
+
+        last_update_timestamp = pendulum.parser.parse(
+            text=spreadsheet.lastUpdateTime
+        ).timestamp()
+
+        return DataVersion(last_update_timestamp)
 
     return _asset
