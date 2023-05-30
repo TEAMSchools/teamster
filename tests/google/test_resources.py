d861e8f6c0e70e5ff051c7196d665c9a0894bd95
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-30 16:43:05 +0000

    debug

--- tests/google/test_resources.py
+++ tests/google/test_resources.py
@@ -8,18 +8,22 @@ def test_gsheet_resource():
     with build_resources(
         resources={
             "gsheets": GoogleSheetsResource(
-                # service_account_file_path="env/gcloud_service_account_json"
-                service_account_file_path="/etc/secret-volume/gcloud_service_account_json"
+                service_account_file_path="env/gcloud_service_account_json"
             )
         }
     ) as resources:
         gsheets: GoogleSheetsResource = resources.gsheets
-        print(gsheets._client.auth.service_account_email)
+
+        print(vars(gsheets._client.auth))
+
         spreadsheet = gsheets.open(
             sheet_id="1xSa3dznVaGeqjo3Y0tS9GzkhVrpeCQ0aaKWlgI3kHik"
         )
-        print(spreadsheet.client._get_file_drive_metadata(spreadsheet.id))
+
+        print(vars(spreadsheet.client.auth))
+
         last_update_timestamp = pendulum.parser.parse(
             text=spreadsheet.lastUpdateTime
         ).timestamp()
+
         print(last_update_timestamp)
