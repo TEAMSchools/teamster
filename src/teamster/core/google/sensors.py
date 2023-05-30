944dd0e6b87c0abe66daee27ff7e7c2268d8fa75
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-30 15:02:07 +0000

    debug

--- src/teamster/core/google/sensors.py
+++ src/teamster/core/google/sensors.py
@@ -25,52 +25,51 @@ def build_gsheet_sensor(
         job=asset_observation_job,
     )
     def _sensor(context: SensorEvaluationContext, gsheets: GoogleSheetsResource):
-        cursor: dict = json.loads(context.cursor or "{}")
+        print(gsheets._client.auth.service_account_email)
+        # cursor: dict = json.loads(context.cursor or "{}")
 
-        print(vars(gsheets._client.auth))
+        # asset_keys = []
+        # for asset in asset_defs:
+        #     asset_key_str = asset.key.to_user_string()
 
-        asset_keys = []
-        for asset in asset_defs:
-            asset_key_str = asset.key.to_user_string()
+        #     context.log.info(asset_key_str)
 
-            context.log.info(asset_key_str)
+        #     spreadsheet = gsheets.open(sheet_id=asset.metadata["sheet_id"])
 
-            spreadsheet = gsheets.open(sheet_id=asset.metadata["sheet_id"])
+        #     last_update_timestamp = pendulum.parser.parse(
+        #         text=spreadsheet.lastUpdateTime
+        #     ).timestamp()
+        #     context.log.debug(f"last_update_time:\t{last_update_timestamp}")
 
-            last_update_timestamp = pendulum.parser.parse(
-                text=spreadsheet.lastUpdateTime
-            ).timestamp()
-            context.log.debug(f"last_update_time:\t{last_update_timestamp}")
+        #     latest_materialization_event = (
+        #         context.instance.get_latest_materialization_event(asset.key)
+        #     )
 
-            latest_materialization_event = (
-                context.instance.get_latest_materialization_event(asset.key)
-            )
+        #     latest_materialization_timestamp = (
+        #         latest_materialization_event.timestamp
+        #         if latest_materialization_event
+        #         else 0
+        #     )
 
-            latest_materialization_timestamp = (
-                latest_materialization_event.timestamp
-                if latest_materialization_event
-                else 0
-            )
+        #     context.log.debug(
+        #         "latest_materialization_event:\t"
+        #         + str(latest_materialization_timestamp)
+        #     )
 
-            context.log.debug(
-                "latest_materialization_event:\t"
-                + str(latest_materialization_timestamp)
-            )
+        #     if last_update_timestamp > latest_materialization_timestamp:
+        #         asset_keys.append(asset_key_str)
 
-            if last_update_timestamp > latest_materialization_timestamp:
-                asset_keys.append(asset_key_str)
+        #         cursor[asset_key_str] = last_update_timestamp
 
-                cursor[asset_key_str] = last_update_timestamp
-
-        if asset_keys:
-            return SensorResult(
-                run_requests=[
-                    RunRequest(
-                        run_key=f"{context._sensor_name}_{pendulum.now().timestamp()}",
-                        run_config=ObservationOpConfig(asset_keys=asset_keys),
-                    )
-                ],
-                cursor=json.dumps(obj=cursor),
-            )
+        # if asset_keys:
+        #     return SensorResult(
+        #         run_requests=[
+        #             RunRequest(
+        #                 run_key=f"{context._sensor_name}_{pendulum.now().timestamp()}",
+        #                 run_config=ObservationOpConfig(asset_keys=asset_keys),
+        #             )
+        #         ],
+        #         cursor=json.dumps(obj=cursor),
+        #     )
 
     return _sensor
