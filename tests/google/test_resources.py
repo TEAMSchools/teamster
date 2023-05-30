0e8b7d3a7dfd28eec30275ea0bc8dbd72009a2ba
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-30 15:23:15 +0000

    debug

--- tests/google/test_resources.py
+++ tests/google/test_resources.py
@@ -1,3 +1,4 @@
+import pendulum
 from dagster import build_resources
 
 from teamster.core.google.resources.sheets import GoogleSheetsResource
@@ -7,16 +8,17 @@ def test_gsheet_resource():
     with build_resources(
         resources={
             "gsheets": GoogleSheetsResource(
-                service_account_file_path="env/gcloud-service-account.json"
+                # service_account_file_path="env/gcloud_service_account_json"
+                service_account_file_path="/etc/secret-volume/gcloud_service_account_json"
             )
         }
     ) as resources:
         gsheets: GoogleSheetsResource = resources.gsheets
-
         print(gsheets._client.auth.service_account_email)
-
         spreadsheet = gsheets.open(
             sheet_id="1xSa3dznVaGeqjo3Y0tS9GzkhVrpeCQ0aaKWlgI3kHik"
         )
-
-        print(spreadsheet.lastUpdateTime)
+        last_update_timestamp = pendulum.parser.parse(
+            text=spreadsheet.lastUpdateTime
+        ).timestamp()
+        print(last_update_timestamp)
