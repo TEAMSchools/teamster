40aea390d233d69b4597c93a4b3d9648006adf61
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-05-30 17:32:13 +0000

    debug

--- src/teamster/core/google/sensors.py
+++ src/teamster/core/google/sensors.py
@@ -33,50 +33,51 @@ def build_gsheet_sensor(
             asset_key_str = asset.key.to_user_string()
 
             context.log.info(asset_key_str)
+            context.log.debug(asset.metadata["sheet_id"].value)
 
-            spreadsheet = gsheets.open(sheet_id=asset.metadata["sheet_id"].value)
+        #     spreadsheet = gsheets.open(sheet_id=asset.metadata["sheet_id"].value)
 
-            last_update_timestamp = pendulum.parser.parse(
-                text=spreadsheet.lastUpdateTime
-            ).timestamp()
+        #     last_update_timestamp = pendulum.parser.parse(
+        #         text=spreadsheet.lastUpdateTime
+        #     ).timestamp()
 
-            context.log.debug(f"last_update_time:\t{last_update_timestamp}")
+        #     context.log.debug(f"last_update_time:\t{last_update_timestamp}")
 
-            latest_materialization_event = (
-                context.instance.get_latest_materialization_event(asset.key)
-            )
+        #     latest_materialization_event = (
+        #         context.instance.get_latest_materialization_event(asset.key)
+        #     )
 
-            latest_materialization_timestamp = (
-                latest_materialization_event.timestamp
-                if latest_materialization_event
-                else 0
-            )
+        #     latest_materialization_timestamp = (
+        #         latest_materialization_event.timestamp
+        #         if latest_materialization_event
+        #         else 0
+        #     )
 
-            context.log.debug(
-                "latest_materialization_event:\t"
-                + str(latest_materialization_timestamp)
-            )
+        #     context.log.debug(
+        #         "latest_materialization_event:\t"
+        #         + str(latest_materialization_timestamp)
+        #     )
 
-            if last_update_timestamp > latest_materialization_timestamp:
-                asset_keys.append(asset_key_str)
+        #     if last_update_timestamp > latest_materialization_timestamp:
+        #         asset_keys.append(asset_key_str)
 
-                cursor[asset_key_str] = last_update_timestamp
+        #         cursor[asset_key_str] = last_update_timestamp
 
-        if asset_keys:
-            return SensorResult(
-                run_requests=[
-                    RunRequest(
-                        run_key=f"{context._sensor_name}_{pendulum.now().timestamp()}",
-                        run_config=RunConfig(
-                            ops={
-                                "asset_observation_op": ObservationOpConfig(
-                                    asset_keys=asset_keys
-                                )
-                            }
-                        ),
-                    )
-                ],
-                cursor=json.dumps(obj=cursor),
-            )
+        # if asset_keys:
+        #     return SensorResult(
+        #         run_requests=[
+        #             RunRequest(
+        #                 run_key=f"{context._sensor_name}_{pendulum.now().timestamp()}",
+        #                 run_config=RunConfig(
+        #                     ops={
+        #                         "asset_observation_op": ObservationOpConfig(
+        #                             asset_keys=asset_keys
+        #                         )
+        #                     }
+        #                 ),
+        #             )
+        #         ],
+        #         cursor=json.dumps(obj=cursor),
+        #     )
 
     return _sensor
---
+++ tests/google/test_sensors.py
@@ -0,0 +1,32 @@
+from dagster import build_sensor_context, instance_for_test
+
+from teamster.core.google.assets import build_gsheet_asset
+from teamster.core.google.resources.sheets import GoogleSheetsResource
+from teamster.core.google.sensors import build_gsheet_sensor
+
+CODE_LOCATION = "kipptaf"
+
+test_asset = build_gsheet_asset(
+    code_location=CODE_LOCATION,
+    sheet_id="1EwHv7MajgHTq-HphLpwrS0KG_2Y_FXlPuPvj98YuFS8",
+    name="standards_translation",
+    range_name="standards_translation",
+)
+
+gsheets_sensor = build_gsheet_sensor(
+    code_location=CODE_LOCATION, asset_defs=[test_asset]
+)
+
+
+def test_sensor():
+    with instance_for_test() as instance:
+        result = gsheets_sensor(
+            build_sensor_context(
+                instance=instance,
+                resources={
+                    "gsheets": GoogleSheetsResource(
+                        service_account_file_path="env/gcloud_service_account_json"
+                    )
+                },
+            )
+        )
