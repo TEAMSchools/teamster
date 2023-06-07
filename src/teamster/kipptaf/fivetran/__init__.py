2427a31ef5beedc55bd8c3e52b8af8edb7d61502
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-06-07 16:42:05 +0000

    fivetran

---
+++ src/teamster/kipptaf/fivetran/__init__.py
@@ -0,0 +1,9 @@
+FIVETRAN_CONNECTOR_IDS = [
+    "philosophical_overbite",  # zendesk
+    "jinx_credulous",  # illuminate
+    "repay_spelled",  # kippadb
+    "bellows_curliness",  # coupa
+    "genuine_describing",  # illuminate_xmin
+    "aspirate_uttering",  # hubspot
+    "sameness_cunning",  # adp_workforce_now
+]
