cc0570da3d88bcdbabf057b0a36e1d4e0c6cd305
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-06-25 19:52:16 +0000

    gsheet source assets

--- src/teamster/kipptaf/dbt/assets.py
+++ src/teamster/kipptaf/dbt/assets.py
@@ -3,7 +3,7 @@ import json
 from dagster_dbt import load_assets_from_dbt_manifest
 
 from teamster.core.dbt.assets import build_external_source_asset_from_key
-from teamster.kipptaf import CODE_LOCATION, fivetran
+from teamster.kipptaf import CODE_LOCATION, fivetran, google
 
 with open(file=f"teamster-dbt/{CODE_LOCATION}/target/manifest.json") as f:
     manifest_json = json.load(f)
@@ -21,7 +21,15 @@ fivetran_source_assets = [
     for table_name, asset_key in assets_def.keys_by_output_name.items()
 ]
 
+gsheet_source_assets = [
+    build_external_source_asset_from_key(
+        asset_key=asset.key, group_name=asset.key.path[-1].split("__")[0]
+    )
+    for asset in google.assets
+]
+
 __all__ = [
     *dbt_assets,
     *fivetran_source_assets,
+    *gsheet_source_assets,
 ]
