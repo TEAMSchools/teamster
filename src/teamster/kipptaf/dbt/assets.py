cdd6f84358e9d4bfbbdf507746fd212c456e5b40
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-06-26 15:53:45 +0000

    fix output

--- src/teamster/kipptaf/dbt/assets.py
+++ src/teamster/kipptaf/dbt/assets.py
@@ -38,9 +38,7 @@ with open(file=manifest_path) as f:
 
 manifest = CustomizedDbtManifest.read(path=manifest_path)
 
-dbt_assets = build_dbt_assets(
-    manifest=manifest, select=CODE_LOCATION, exclude="source:*_powerschool"
-)
+dbt_assets = build_dbt_assets(manifest=manifest, select=CODE_LOCATION)
 
 fivetran_source_assets = [
     build_external_source_asset_new(
