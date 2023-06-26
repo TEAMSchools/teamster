fd25b14198ec653d96433155639959b09feae7ed
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-06-26 01:30:19 +0000

    refactor

--- src/teamster/kipptaf/dbt/assets.py
+++ src/teamster/kipptaf/dbt/assets.py
@@ -1,34 +1,31 @@
 import json
 
-# from dagster_dbt import load_assets_from_dbt_manifest
 from dagster_dbt.cli import DbtManifest
 
-from teamster.core.dbt.assets import (
-    build_dbt_assets,
-    build_external_source_asset_from_key,
-    build_staging_assets_from_source,
-)
+from teamster.core.dbt.assets import build_dbt_assets, build_staging_assets_from_source
 from teamster.kipptaf import CODE_LOCATION, fivetran, google
 
 manifest_path = f"teamster-dbt/{CODE_LOCATION}/target/manifest.json"
+
 manifest = DbtManifest.read(path=manifest_path)
 
 with open(file=manifest_path) as f:
     manifest_json = json.load(f)
 
-
-# dbt_assets = load_assets_from_dbt_manifest(
-#     manifest_json=manifest_json,
-#     key_prefix=[CODE_LOCATION, "dbt"],
-#     source_key_prefix=[CODE_LOCATION, "dbt"],
-# )
 dbt_assets = build_dbt_assets(manifest=manifest)
 
 fivetran_source_assets = [
-    build_external_source_asset_from_key(asset_key=asset_key)
+    build_staging_assets_from_source(
+        manifest=manifest,
+        code_location=CODE_LOCATION,
+        table_name=object_identifier.split(sep=".")[1],
+        dbt_package_name=object_identifier.split(sep=".")[0],
+        upstream_package_name=asset_key.path[1],
+        group_name=asset_key.path[1],
+    )
     for assets in fivetran.assets
     for assets_def in assets.compute_cacheable_data()
-    for table_name, asset_key in assets_def.keys_by_output_name.items()
+    for object_identifier, asset_key in assets_def.keys_by_output_name.items()
 ]
 
 gsheet_source_assets = [
