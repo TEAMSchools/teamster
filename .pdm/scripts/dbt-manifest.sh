2a02121f80b045b7cd7b036ab377313361f31abe
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-06-29 16:37:49 +0000

    fix dep

--- .pdm/scripts/dbt-manifest.sh
+++ .pdm/scripts/dbt-manifest.sh
@@ -4,7 +4,9 @@ if [[ -z ${1} ]]; then
   echo "Code Location is required"
   exit 1
 else
-  git submodule update --remote
-  dbt deps --project-dir "teamster-dbt/${1}" --profiles-dir "teamster-dbt/${1}"
-  dbt list --project-dir "teamster-dbt/${1}" --profiles-dir "teamster-dbt/${1}"
+  OUTPUT=$(git submodule update --remote)
+  if [[ -n ${OUTPUT} ]]; then
+    dbt deps --project-dir "teamster-dbt/${1}" --profiles-dir "teamster-dbt/${1}"
+    dbt list --project-dir "teamster-dbt/${1}" --profiles-dir "teamster-dbt/${1}"
+  fi
 fi
