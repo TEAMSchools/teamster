2d2b82d65f05d93d96d557260a30aae357ff3875
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-06-29 16:14:58 +0000

    caps

--- kipptaf/models/extracts/rpt_blissbook__employee_list.sql
+++ kipptaf/models/extracts/rpt_blissbook__employee_list.sql
@@ -1,8 +1,8 @@
 select
-    employee_number as `employee id`,
-    mail as `email address`,
-    business_unit_assigned as `groups`,
-    concat(preferred_name_given_name, ' ', preferred_name_family_name) as `name`,
-    coalesce(worker_rehire_date, worker_original_hire_date) as `latest hire date`
+    employee_number as `Employee ID`,
+    mail as `Email Address`,
+    business_unit_assigned as `Groups`,
+    concat(preferred_name_given_name, ' ', preferred_name_family_name) as `Name`,
+    coalesce(worker_rehire_date, worker_original_hire_date) as `Latest Hire Date`
 from {{ ref("base_people__staff_roster") }}
 where status_value != 'Terminated' and mail is not null
