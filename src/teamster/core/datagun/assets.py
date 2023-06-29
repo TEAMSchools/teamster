b4ec846a9780728670962f2e702f91173ba6f928
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-06-29 16:37:49 +0000

    fix dep

--- src/teamster/core/datagun/assets.py
+++ src/teamster/core/datagun/assets.py
@@ -282,7 +282,7 @@ def build_bigquery_extract_sftp_asset(
 
     @asset(
         key=[code_location, "extracts", destination_name, asset_name],
-        non_argument_deps=[AssetKey([code_location, dataset_id, table_id])],
+        non_argument_deps=[AssetKey([code_location, "extracts", table_id])],
         required_resource_keys={"gcs", "db_bigquery", f"ssh_{destination_name}"},
         op_tags=op_tags,
     )
