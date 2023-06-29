3ec510105c764484caafb46f033f37531fb3e03e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-06-29 21:44:42 +0000

    egencia

--- kipptaf/models/extracts/rpt_egencia__users.sql
+++ kipptaf/models/extracts/rpt_egencia__users.sql
@@ -1,49 +1,41 @@
-{{ config(enabled=False) }}
-
 select
-    concat(sub.employee_number, '@kippnj.org') as `Username`,
+    concat(sr.employee_number, '@kippnj.org') as `Username`,
     sr.mail as `Email`,
-    sr.userprincipalname as `Single Sign On ID`,
+    sr.user_principal_name as `Single Sign On ID`,
     sr.employee_number as `Employee ID`,
-    case
-        when sr.position_status = 'Terminated' then 'Disabled' else 'Active'
-    end as `Status`,
-    sr.first_name as `First name`,  /* legal name */
-    sr.last_name as `Last name`,  /* legal name */
-    case
-        when sub.employee_number in (100219, 100412, 100566, 102298)
-        then 'Travel Manager'
-        else 'Traveler'
-    end as `Role`,
+    if(sr.assignment_status = 'Terminated', 'Disabled', 'Active') as `Status`,
+    sr.legal_name_given_name as `First name`,  /* legal name */
+    sr.legal_name_family_name as `Last name`,  /* legal name */
+    if(
+        sr.employee_number in (100219, 100412, 100566, 102298),
+        'Travel Manager',
+        'Traveler'
+    ) as `Role`,
 
-    /* cascading match on location/dept/job */
+    /* cascading match on home_work_location_name/dept/job */
     coalesce(
         tg.egencia_traveler_group,
         tg2.egencia_traveler_group,
         tg3.egencia_traveler_group,
         'General Traveler Group'
     ) as `Traveler Group`,
-
-{# sr.location,
-    sr.home_department,
-    sr.job_title, #}
 from {{ ref("base_people__staff_roster") }} as sr
 left join
-    egencia.traveler_groups as tg
-    on sub.location = tg.location
-    and sub.home_department = tg.home_department
-    and sub.job_title = tg.job_title
+    {{ source("egencia", "src_egencia__traveler_groups") }} as tg
+    on sr.home_work_location_name = tg.adp_home_work_location_name
+    and sr.department_home_name = tg.adp_department_home_name
+    and sr.job_title = tg.adp_job_title
 left join
-    egencia.traveler_groups as tg2
-    on sub.location = tg2.location
-    and sub.home_department = tg2.home_department
-    and tg2.job_title = 'Default'
+    {{ source("egencia", "src_egencia__traveler_groups") }} as tg2
+    on sr.home_work_location_name = tg2.adp_home_work_location_name
+    and sr.department_home_name = tg2.adp_department_home_name
+    and tg2.adp_job_title = 'Default'
 left join
-    egencia.traveler_groups as tg3
-    on sub.location = tg3.location
-    and tg3.home_department = 'Default'
-    and tg3.job_title = 'Default'
+    {{ source("egencia", "src_egencia__traveler_groups") }} as tg3
+    on sr.home_work_location_name = tg3.adp_home_work_location_name
+    and tg3.adp_department_home_name = 'Default'
+    and tg3.adp_job_title = 'Default'
 where
-    (sr.worker_category not in ('Intern', 'Part Time') or sr.worker_category is null)
-    and coalesce(sr.termination_date, current_timestamp)
-    >= datefromparts(utilities.global_academic_year(), 7, 1)
+    (sr.worker_type not in ('Intern', 'Part Time') or sr.worker_type is null)
+    and coalesce(sr.worker_termination_date, current_date('America/New_York'))
+    >= date({{ var("current_fiscal_year") }} - 1, 7, 1)
