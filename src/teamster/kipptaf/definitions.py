c022f7ba66d808ba943005b21ac8dcef7bf6fa44
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-01 20:15:21 +0000

    fix group names

--- src/teamster/kipptaf/definitions.py
+++ src/teamster/kipptaf/definitions.py
@@ -1,5 +1,3 @@
-# import os
-
 from dagster import (
     AutoMaterializePolicy,
     Definitions,
--- src/teamster/kipptaf/fivetran/assets.py
+++ src/teamster/kipptaf/fivetran/assets.py
@@ -1,27 +1,16 @@
 import json
-import os
 import pathlib
 
-# from dagster import with_resources
 from dagster_fivetran import build_fivetran_assets
 
 schema_path = pathlib.Path(__file__).parent / "schema"
-# fivetran_instance = FivetranResource(
-#     api_key=os.getenv("FIVETRAN_API_KEY"), api_secret=os.getenv("FIVETRAN_API_SECRET")
-# )
 
 assets = []
 for schema_file in schema_path.glob("*.json"):
     with schema_file.open(mode="r") as fp:
         build_fivetran_assets_kwargs = json.load(fp=fp)
 
-    assets.extend(
-        build_fivetran_assets(**build_fivetran_assets_kwargs)
-        # with_resources(
-        #     definitions=build_fivetran_assets(**build_fivetran_assets_kwargs),
-        #     resource_defs={"fivetran": fivetran_instance},
-        # )
-    )
+    assets.extend(build_fivetran_assets(**build_fivetran_assets_kwargs))
 
 __all__ = [
     *assets,
