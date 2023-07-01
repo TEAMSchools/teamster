44adff427cf031927c6398dddab1bebb49a1f4a9
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-01 19:21:10 +0000

    fivetran manual

---
+++ .pdm/scripts/fivetran-schema.py
@@ -0,0 +1,65 @@
+import argparse
+import json
+
+from dagster import EnvVar, build_resources
+from dagster_fivetran import FivetranResource
+
+"""
+drab_headwear  # kipptaf
+
+sameness_cunning  # adp_workforce_now
+aspirate_uttering  # hubspot
+bellows_curliness  # coupa
+philosophical_overbite  # zendesk
+repay_spelled  # kippadb
+genuine_describing  # illuminate_xmin
+jinx_credulous  # illuminate
+"""
+
+
+def main(args):
+    connector_id = args.connector_id
+
+    with build_resources(
+        resources={
+            "fivetran": FivetranResource(
+                api_key=EnvVar("FIVETRAN_API_KEY"),
+                api_secret=EnvVar("FIVETRAN_API_SECRET"),
+            )
+        }
+    ) as resources:
+        instance: FivetranResource = resources.fivetran
+
+        schemas = instance.make_request(
+            method="GET", endpoint=f"connectors/{connector_id}/schemas"
+        )
+
+    destination_tables = []
+    for connector_type, schema in schemas["schemas"].items():
+        schema_name = schema["name_in_destination"]
+
+        for table_type, table in schema["tables"].items():
+            table_name = table["name_in_destination"]
+
+            if table["enabled"]:
+                destination_tables.append(f"{schema_name}.{table_name}")
+
+    with open(
+        file=f"src/teamster/{args.code_location}/fivetran/schema/{connector_id}.json",
+        mode="w",
+    ) as fp:
+        json.dump(
+            obj={
+                "connector_id": connector_id,
+                "destination_tables": destination_tables,
+            },
+            fp=fp,
+        )
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("code_location")
+    args.add_argument("connector_id")
+
+    main(args.parse_args())
--- src/teamster/kipptaf/fivetran/assets.py
+++ src/teamster/kipptaf/fivetran/assets.py
@@ -1,32 +1,31 @@
+import json
 import os
+import pathlib
 
-from dagster_fivetran import FivetranResource
+from dagster import with_resources
+from dagster_fivetran import FivetranResource, build_fivetran_assets
 
-from teamster.core.fivetran.resources import load_assets_from_fivetran_instance
-from teamster.kipptaf import CODE_LOCATION
-
-FIVETRAN_CONNECTOR_IDS = [
-    "philosophical_overbite",  # zendesk
-    "jinx_credulous",  # illuminate
-    "repay_spelled",  # kippadb
-    "bellows_curliness",  # coupa
-    "genuine_describing",  # illuminate_xmin
-    "aspirate_uttering",  # hubspot
-    "sameness_cunning",  # adp_workforce_now
-]
-
-assets = load_assets_from_fivetran_instance(
-    fivetran=FivetranResource(
-        api_key=os.getenv("FIVETRAN_API_KEY"),
-        api_secret=os.getenv("FIVETRAN_API_SECRET"),
-    ),
-    key_prefix=[CODE_LOCATION],
-    connector_filter=(lambda meta: meta.connector_id in FIVETRAN_CONNECTOR_IDS),
-    connector_files=[
-        f"src/teamster/core/fivetran/schema/{connector_id}.pickle"
-        for connector_id in FIVETRAN_CONNECTOR_IDS
-    ],
+schema_path = pathlib.Path(__file__).parent / "schema"
+fivetran_instance = FivetranResource(
+    api_key=os.getenv("FIVETRAN_API_KEY"), api_secret=os.getenv("FIVETRAN_API_SECRET")
 )
+
+assets = []
+for schema_file in schema_path.glob("*.json"):
+    connector_id = schema_file.stem
+    destination_tables = []
+
+    with schema_file.open(mode="r") as fp:
+        build_fivetran_assets_kwargs = json.load(fp=fp)
+
+    assets.extend(
+        with_resources(
+            definitions=build_fivetran_assets(**build_fivetran_assets),
+            resource_defs={"fivetran": fivetran_instance},
+        )
+    )
+
+print(assets)
 
 __all__ = [
     assets,
