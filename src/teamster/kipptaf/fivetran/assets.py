0c284301dee8149e8603d3feeb113c9103bfea41
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-02 01:18:14 +0000

    key prefix

--- src/teamster/kipptaf/fivetran/assets.py
+++ src/teamster/kipptaf/fivetran/assets.py
@@ -3,6 +3,8 @@ import pathlib
 
 from dagster_fivetran import build_fivetran_assets
 
+from .. import CODE_LOCATION
+
 schema_path = pathlib.Path(__file__).parent / "schema"
 
 assets = []
@@ -10,7 +12,11 @@ for schema_file in schema_path.glob("*.json"):
     with schema_file.open(mode="r") as fp:
         build_fivetran_assets_kwargs = json.load(fp=fp)
 
-    assets.extend(build_fivetran_assets(**build_fivetran_assets_kwargs))
+    assets.extend(
+        build_fivetran_assets(
+            asset_key_prefix=CODE_LOCATION, **build_fivetran_assets_kwargs
+        )
+    )
 
 __all__ = [
     *assets,
