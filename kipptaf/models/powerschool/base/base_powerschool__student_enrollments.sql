d3ecad6ddbddf39f8051db8264eac2b007695902
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-06 18:33:16 +0000

    ops dash

--- kipptaf/models/powerschool/base/base_powerschool__student_enrollments.sql
+++ kipptaf/models/powerschool/base/base_powerschool__student_enrollments.sql
@@ -21,11 +21,98 @@ with
     )
 
 select
-    seu.*,
+    seu._dbt_source_relation,
+    seu.studentid,
+    seu.students_dcid,
+    seu.student_number,
+    seu.state_studentnumber,
+    seu.yearid,
+    seu.academic_year,
+    seu.schoolid,
+    seu.school_name,
+    seu.school_abbreviation,
+    seu.reporting_schoolid,
+    seu.reporting_school_name,
+    seu.school_level,
+    seu.is_self_contained,
+    seu.is_out_of_district,
+    seu.grade_level,
+    seu.grade_level_prev,
+    seu.lastfirst,
+    seu.first_name,
+    seu.middle_name,
+    seu.last_name,
+    seu.entrydate,
+    seu.exitdate,
+    seu.entrycode,
+    seu.exitcode,
+    seu.exit_code_kf,
+    seu.exit_code_ts,
+    seu.exitcomment,
+    seu.enroll_status,
+    seu.cohort,
+    seu.dob,
+    seu.gender,
+    seu.ethnicity,
+    seu.track,
+    seu.fteid,
+    seu.rn_year,
+    seu.rn_school,
+    seu.rn_all,
+    seu.rn_undergrad,
+    seu.is_retained_year,
+    seu.is_retained_ever,
+    seu.year_in_school,
+    seu.year_in_network,
+    seu.entry_schoolid,
+    seu.entry_grade_level,
+    seu.highest_grade_level_achieved,
+    seu.street,
+    seu.city,
+    seu.state,
+    seu.zip,
+    seu.advisory_name,
+    seu.advisor_teachernumber,
+    seu.advisor_lastfirst,
+    seu.home_phone,
+    seu.contact_1_name,
+    seu.contact_1_phone_home,
+    seu.contact_1_phone_mobile,
+    seu.contact_1_phone_daytime,
+    seu.contact_1_email_current,
+    seu.contact_2_name,
+    seu.contact_2_phone_home,
+    seu.contact_2_phone_mobile,
+    seu.contact_2_phone_daytime,
+    seu.contact_2_email_current,
+    seu.is_enrolled_y1,
+    seu.is_enrolled_oct01,
+    seu.is_enrolled_oct15,
+    seu.is_enrolled_recent,
+    initcap(regexp_extract(seu._dbt_source_relation, r'kipp(\w+)_\w+')) as region,
 
     suf.newark_enrollment_number,
-    suf.c_504_status,
+    ifnull(suf.is_504, false) as is_504,
 
+    nj.districtcoderesident,
+    nj.referral_date,
+    nj.parental_consent_eval_date,
+    nj.eligibility_determ_date,
+    nj.initial_iep_meeting_date,
+    nj.parent_consent_intial_iep_date,
+    nj.annual_iep_review_meeting_date,
+    nj.reevaluation_date,
+    nj.parent_consent_obtain_code,
+    nj.initial_process_delay_reason,
+    nj.special_education_placement,
+    nj.time_in_regular_program,
+    nj.early_intervention_yn,
+    nj.determined_ineligible_yn,
+    nj.counseling_services_yn,
+    nj.occupational_therapy_serv_yn,
+    nj.physical_therapy_services_yn,
+    nj.speech_lang_theapy_services_yn,
+    nj.other_related_services_yn,
     case
         when nj.lepbegindate is null
         then false
@@ -47,10 +134,6 @@ select
                 seu.academic_year = {{ var("current_academic_year") }}
                 and seu.rn_year = 1
             then if(tpd.is_directly_certified, 'F', ifd.eligibility_name)
-            when
-                seu.academic_year < {{ var("current_academic_year") }}
-                and seu.entrydate = seu.current_entrydate
-            then if(seu.current_lunchstatus = 'NoD', null, seu.current_lunchstatus)
             else seu.lunch_status
         end
     ) as lunch_status,
@@ -64,14 +147,14 @@ select
                 then 'No Application'
                 else ifd.lunch_application_status
             end
-        when
-            seu.academic_year < {{ var("current_academic_year") }}
-            and seu.entrydate = seu.current_entrydate
-        then seu.current_lunchstatus
         else seu.lunch_status
     end as lunch_application_status,
-{# saa.student_web_id,
-    saa.student_web_password, #}
+
+    sr.mail as advisor_email,
+    sr.communication_business_mobile as advisor_phone,
+
+    null as student_web_id,
+    null as student_web_password,
 from student_enrollments_union seu
 left join
     {{ ref("stg_powerschool__u_studentsuserfields") }} as suf
@@ -86,6 +169,7 @@ left join
     on seu.student_number = sped.student_number
     and seu.academic_year = sped.academic_year
     and {{ union_dataset_join_clause(left_alias="seu", right_alias="sped") }}
+    and seu.academic_year = {{ var("current_academic_year") }}
 left join
     {{ ref("stg_titan__person_data") }} as tpd
     on seu.student_number = tpd.person_identifier
@@ -96,6 +180,9 @@ left join
     on seu.student_number = ifd.student_identifier
     and seu.academic_year = ifd.academic_year
     and {{ union_dataset_join_clause(left_alias="seu", right_alias="ifd") }}
+left join
+    {{ ref("base_people__staff_roster") }} as sr
+    on seu.advisor_teachernumber = sr.powerschool_teacher_number
     {# left join
     {{ ref("stg_students__access_accounts") }} as saa
     on seu.student_number = saa.student_number #}
--- powerschool/models/staging/stg_powerschool__u_studentsuserfields.sql
+++ powerschool/models/staging/stg_powerschool__u_studentsuserfields.sql
@@ -1,7 +1,10 @@
 {{
     teamster_utils.generate_staging_model(
         unique_key="studentsdcid.int_value",
-        transform_cols=[{"name": "studentsdcid", "extract": "int_value"}],
+        transform_cols=[
+            {"name": "studentsdcid", "extract": "int_value"},
+            {"name": "c_504_status", "cast": "int"},
+        ],
         except_cols=[
             "_dagster_partition_fiscal_year",
             "_dagster_partition_date",
@@ -11,5 +14,5 @@
     )
 }}
 
-select *
+select *, if(c_504_status = 1, true, false) as is_504
 from staging
