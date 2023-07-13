dd0546bb3c87a81632fce2afcc8b4afd9056bd4b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-13 17:08:49 +0000

    data as iterable

--- src/teamster/kipptaf/fivetran/schedules.py
+++ src/teamster/kipptaf/fivetran/schedules.py
@@ -5,4 +5,7 @@ from . import jobs
 __all__ = []
 
 for job in jobs:
-    __all__.append(ScheduleDefinition(cron_schedule="0 * * * *", job=job))
+    if job.name == "kipptaf_fivetran_adp_workforce_now_asset_job":
+        __all__.append(ScheduleDefinition(cron_schedule="0 * * * *", job=job))
+    else:
+        __all__.append(ScheduleDefinition(cron_schedule="0 0 * * *", job=job))
