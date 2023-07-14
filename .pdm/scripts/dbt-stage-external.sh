2f1b4fb4fdd56b4d35e61757a7fd498a7214cb4c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-14 14:34:14 +0000

    mv dbt

--- .pdm/scripts/dbt-stage-external.sh
+++ .pdm/scripts/dbt-stage-external.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dbt run-operation --project-dir dbt/"${1}" \
+dbt run-operation --project-dir src/dbt/"${1}" \
   stage_external_sources \
   --vars "ext_full_refresh: true" \
   --args "select: ${2}"
