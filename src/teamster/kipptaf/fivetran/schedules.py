2319bd75e08449cd53983e3db305f2b0bc7c2cb0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-16 00:07:24 +0000

    fix tz

--- src/teamster/kipptaf/fivetran/schedules.py
+++ src/teamster/kipptaf/fivetran/schedules.py
@@ -1,11 +1,20 @@
 from dagster import ScheduleDefinition
 
+from .. import LOCAL_TIMEZONE
 from . import jobs
 
 __all__ = []
 
 for job in jobs:
     if job.name == "kipptaf_fivetran_adp_workforce_now_asset_job":
-        __all__.append(ScheduleDefinition(cron_schedule="0 * * * *", job=job))
+        __all__.append(
+            ScheduleDefinition(
+                cron_schedule="0 * * * *", job=job, execution_timezone=LOCAL_TIMEZONE
+            )
+        )
     else:
-        __all__.append(ScheduleDefinition(cron_schedule="0 0 * * *", job=job))
+        __all__.append(
+            ScheduleDefinition(
+                cron_schedule="0 0 * * *", job=job, execution_timezone=LOCAL_TIMEZONE
+            )
+        )
