ea84e6f4d5de6263cec8ff7e014cac98fe85e6b7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-16 00:12:22 +0000

    fix tz

--- src/teamster/kipptaf/fivetran/schedules.py
+++ src/teamster/kipptaf/fivetran/schedules.py
@@ -9,12 +9,16 @@ for job in jobs:
     if job.name == "kipptaf_fivetran_adp_workforce_now_asset_job":
         __all__.append(
             ScheduleDefinition(
-                cron_schedule="0 * * * *", job=job, execution_timezone=LOCAL_TIMEZONE
+                cron_schedule="0 * * * *",
+                job=job,
+                execution_timezone=LOCAL_TIMEZONE.name,
             )
         )
     else:
         __all__.append(
             ScheduleDefinition(
-                cron_schedule="0 0 * * *", job=job, execution_timezone=LOCAL_TIMEZONE
+                cron_schedule="0 0 * * *",
+                job=job,
+                execution_timezone=LOCAL_TIMEZONE.name,
             )
         )
