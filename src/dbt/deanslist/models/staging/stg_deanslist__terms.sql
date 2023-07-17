d553bcb22a5e677ee1f1fe85b964e9c76263607e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-17 23:20:52 +0000

    dl fixes and incidents

--- src/dbt/deanslist/models/staging/stg_deanslist__terms.sql
+++ src/dbt/deanslist/models/staging/stg_deanslist__terms.sql
@@ -7,17 +7,17 @@ select
     nullif(schoolid, '') as `school_id`,
     nullif(secondarygradekey, '') as `secondary_grade_key`,
     nullif(secondaryintegrationid, '') as `secondary_integration_id`,
-    nullif(storedgrades, '') as `stored_grades`,
     nullif(termid, '') as `term_id`,
     nullif(termname, '') as `term_name`,
     nullif(termtype, '') as `term_type`,
     nullif(termtypeid, '') as `term_type_id`,
+    storedgrades as `stored_grades`,
 
     {# records #}
+    startdate.timezone_type as `start_date_timezone_type`,
     nullif(startdate.timezone, '') as `start_date_timezone`,
-    nullif(startdate.timezone_type, '') as `start_date_timezone_type`,
-    nullif(enddate.timezone, '') as `end_date_timezone`,
-    nullif(enddate.timezone_type, '') as `end_date_timezone_type`,
     safe_cast(nullif(startdate.date, '') as datetime) as `start_date_date`,
+    enddate.timezone_type as `end_date_timezone_type`,
+    nullif(enddate.timezone, '') as `end_date_timezone`,
     safe_cast(nullif(enddate.date, '') as datetime) as `end_date_date`,
 from {{ source("deanslist", "src_deanslist__terms") }}
