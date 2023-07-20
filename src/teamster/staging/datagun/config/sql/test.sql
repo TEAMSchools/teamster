b4380dda7de1e2d8e5188c7eb316fe6279b22fa8
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-20 02:22:52 +0000

    lint

--- src/teamster/staging/datagun/config/sql/test.sql
+++ src/teamster/staging/datagun/config/sql/test.sql
@@ -1,10 +1,3 @@
-SELECT
-  n,
-  n + 1 AS n2,
-  n + 2 AS n3,
-  n + 3 AS n4,
-  n + 4 AS n5
-FROM
-  utilities.row_generator_smallint
-WHERE
-  n <= 5
+select n, n + 1 as n2, n + 2 as n3, n + 3 as n4, n + 4 as n5
+from utilities.row_generator_smallint
+where n <= 5
