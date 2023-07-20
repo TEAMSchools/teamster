7a47a5c580fa185b0cccf373cf592671a0c1a559
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-20 02:17:24 +0000

    schoolmint grow tabler

--- src/dbt/kipptaf/models/schoolmint/grow/staging/stg_schoolmint_grow__observations__observation_scores.sql
+++ src/dbt/kipptaf/models/schoolmint/grow/staging/stg_schoolmint_grow__observations__observation_scores.sql
@@ -1,13 +1,15 @@
 select
-    o.observation_id as `observation_id`,
+    o.observation_id,
 
-    os.lastmodified as `observation_score_last_modified`,
-    os.measurement as `observation_score_measurement`,
-    os.measurementgroup as `observation_score_measurement_group`,
-    os.percentage as `observation_score_percentage`,
-    os.valuescore as `observation_score_value_score`,
-    os.valuetext as `observation_score_value_text`,
-    os.textboxes as `observation_score_text_boxes`,
-    os.checkboxes as `observation_score_checkboxes`,
+    os.lastmodified as `last_modified`,
+    os.measurement as `measurement`,
+    os.measurementgroup as `measurement_group`,
+    os.percentage as `percentage`,
+    os.valuescore as `value_score`,
+    os.valuetext as `value_text`,
+
+    {# repeated records #}
+    os.textboxes as `text_boxes`,
+    os.checkboxes as `checkboxes`,
 from {{ ref("stg_schoolmint_grow__observations") }} as o
 cross join unnest(o.observation_scores) as os
--- src/dbt/kipptaf/models/tableau/rpt_tableau__schoolmint_grow_goals.sql
+++ src/dbt/kipptaf/models/tableau/rpt_tableau__schoolmint_grow_goals.sql
@@ -8,14 +8,8 @@ select
     a.creator_id,
     a.creator_name,
 
-    gt.tag_id,
-    gt.name as tag_name,
-
-    null as assignment_status,
-    null as exclude_from_bank,
-    null as mastered_date,
-    null as user_email,
-    null as creator_email,
+    ast.tag_id,
+    ast.tag_name,
 
     sr.home_work_location_name as user_default_school_name,
     sr.department_home_name as user_default_course_name,
@@ -25,17 +19,21 @@ select
 
     rt.academic_year,
     rt.name as reporting_term_name,
+
+    null as assignment_status,
+    null as exclude_from_bank,
+    null as mastered_date,
+    null as user_email,
+    null as creator_email,
 from {{ ref("stg_schoolmint_grow__assignments") }} as a
 inner join
     {{ ref("stg_schoolmint_grow__assignments__tags") }} as ast
     on a.assignment_id = ast.assignment_id
-inner join
-    {{ ref("stg_schoolmint_grow__generic_tags") }} as gt on ast.tag_id = gt.tag_id
 inner join {{ ref("stg_schoolmint_grow__users") }} as u on a.user_id = u.user_id
-inner join
+left join
     {{ ref("base_people__staff_roster") }} as sr
     on u.internal_id = safe_cast(sr.employee_number as string)
-inner join
+left join
     {{ ref("stg_reporting__terms") }} as rt
     on cast(a.created as date) between rt.start_date and rt.end_date
     and rt.type = 'RT'
