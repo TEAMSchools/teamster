b4b2c88258b0c98fb2ecac48c8fa4a7dbe8381f0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-20 02:22:52 +0000

    lint

--- src/teamster/kipptaf/datagun/config/sql/whetstone_users.sql
+++ src/teamster/kipptaf/datagun/config/sql/whetstone_users.sql
@@ -1,44 +1,40 @@
-SELECT DISTINCT
-  user_internal_id,
-  group_type,
-  group_name,
-  [user_id],
-  archived_at,
-  user_email,
-  user_email_ws,
-  [user_name],
-  user_name_ws,
-  inactive,
-  inactive_ws,
-  coach_id,
-  coach_id_ws,
-  school_id,
-  school_id_ws,
-  grade_id,
-  grade_id_ws,
-  course_id,
-  course_id_ws,
-  role_id,
-  role_id_ws,
-  role_names
-FROM
-  extracts.whetstone_users
-WHERE
-  NOT (
-    CONCAT(inactive, inactive_ws) = '11'
-    AND archived_at IS NOT NULL
-  )
-  AND (
-    /* create user */
-    [user_id] IS NULL
-    /* update user */
-    OR ISNULL(inactive, '') != ISNULL(inactive_ws, '')
-    OR ISNULL(coach_id, '') != ISNULL(coach_id_ws, '')
-    OR ISNULL(school_id, '') != ISNULL(school_id_ws, '')
-    OR ISNULL(grade_id, '') != ISNULL(grade_id_ws, '')
-    OR ISNULL(course_id, '') != ISNULL(course_id_ws, '')
-    OR ISNULL(role_id, '') != ISNULL(role_id_ws, '')
-    OR ISNULL(user_email, '') != ISNULL(user_email_ws, '')
-    OR ISNULL([user_name], '') != ISNULL(user_name_ws, '')
-    OR ISNULL(group_type, '') != ISNULL(group_type_ws, '')
-  )
+select distinct
+    user_internal_id,
+    group_type,
+    group_name,
+    [user_id],
+    archived_at,
+    user_email,
+    user_email_ws,
+    [user_name],
+    user_name_ws,
+    inactive,
+    inactive_ws,
+    coach_id,
+    coach_id_ws,
+    school_id,
+    school_id_ws,
+    grade_id,
+    grade_id_ws,
+    course_id,
+    course_id_ws,
+    role_id,
+    role_id_ws,
+    role_names
+from extracts.whetstone_users
+where
+    not (concat(inactive, inactive_ws) = '11' and archived_at is not null)
+    and (
+        /* create user */
+        [user_id] is null
+        /* update user */
+        or isnull (inactive, '') != isnull (inactive_ws, '')
+        or isnull (coach_id, '') != isnull (coach_id_ws, '')
+        or isnull (school_id, '') != isnull (school_id_ws, '')
+        or isnull (grade_id, '') != isnull (grade_id_ws, '')
+        or isnull (course_id, '') != isnull (course_id_ws, '')
+        or isnull (role_id, '') != isnull (role_id_ws, '')
+        or isnull (user_email, '') != isnull (user_email_ws, '')
+        or isnull ([user_name], '') != isnull (user_name_ws, '')
+        or isnull (group_type, '') != isnull (group_type_ws, '')
+    )
