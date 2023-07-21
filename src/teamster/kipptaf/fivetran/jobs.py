258561e2adbafcde9e743bbc7768179680bd41e5
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-21 20:43:26 +0000

    fix job

--- src/teamster/kipptaf/fivetran/jobs.py
+++ src/teamster/kipptaf/fivetran/jobs.py
@@ -1,37 +1,30 @@
 import re
 
-from dagster import AssetSelection, define_asset_job, job  # , RunConfig, configured
+from dagster import AssetSelection, RunConfig, define_asset_job, job
 
 from teamster.core.fivetran.ops import SyncConfig, fivetran_sync_op
 from teamster.kipptaf import CODE_LOCATION, fivetran
 
-# op_config = {}
-# for asset in fivetran.assets:
-#     connector_id = re.match(
-#         pattern=r"fivetran_sync_(?P<connector_id>\w+)", string=asset.node_def.name
-#     ).groupdict()["connector_id"]
+op_config = {}
+for asset in fivetran.assets:
+    connector_id = re.match(
+        pattern=r"fivetran_sync_(?P<connector_id>\w+)", string=asset.node_def.name
+    ).groupdict()["connector_id"]
 
-#     op_config[connector_id] = SyncConfig(
-#         connector_id=connector_id, yield_materializations=False
-#     )
+    op_config[connector_id] = SyncConfig(
+        connector_id=connector_id, yield_materializations=False
+    )
 
 
-# @job(config=RunConfig(ops=op_config))
-@job
+@job(config=RunConfig(ops=op_config))
 def fivetran_sync_job():
     for asset in fivetran.assets:
         connector_id = re.match(
             pattern=r"fivetran_sync_(?P<connector_id>\w+)", string=asset.node_def.name
         ).groupdict()["connector_id"]
 
-        fivetran_sync_op.configured(
-            config_or_config_fn=SyncConfig(
-                connector_id=connector_id, yield_materializations=False
-            ),
-            name=connector_id,
-        )
-        # fivetran_sync_op_aliased = fivetran_sync_op.alias(connector_id)
-        # fivetran_sync_op_aliased()
+        fivetran_sync_op_aliased = fivetran_sync_op.alias(connector_id)
+        fivetran_sync_op_aliased()
 
 
 __all__ = [
