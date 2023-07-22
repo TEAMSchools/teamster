9ecd0820f9da43fa5cf734b0abc12ef37202f64c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-22 06:07:27 +0000

    fix key

--- src/teamster/core/fivetran/assets.py
+++ src/teamster/core/fivetran/assets.py
@@ -7,8 +7,9 @@ def build_fivetran_asset(
 ):
     @observable_source_asset(
         name=name,
-        key_prefix=[code_location, group_name],
+        key_prefix=[code_location, connector_name],
         metadata={"connector_id": connector_id, "connector_name": connector_name},
+        group_name=group_name,
         **kwargs,
     )
     def _asset():
