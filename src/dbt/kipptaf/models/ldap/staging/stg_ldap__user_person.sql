f9c869de587bda398e828438e349a5816caffd4a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-25 16:49:31 +0000

    clever extracts

--- src/dbt/kipptaf/models/ldap/staging/stg_ldap__user_person.sql
+++ src/dbt/kipptaf/models/ldap/staging/stg_ldap__user_person.sql
@@ -1,9 +1,9 @@
 select
-    distinguishedname as distinguished_name,
-    userprincipalname as user_principal_name,
-    mail,
-    samaccountname as sam_account_name,
-    safe_cast(employeenumber as int) as employee_number,
-    physicaldeliveryofficename as physical_delivery_office_name,
-    useraccountcontrol & 2 as uac_account_disable,
+    distinguishedname as `distinguished_name`,
+    userprincipalname as `user_principal_name`,
+    mail as `mail`,
+    samaccountname as `sam_account_name`,
+    physicaldeliveryofficename as `physical_delivery_office_name`,
+    safe_cast(employeenumber as int) as `employee_number`,
+    useraccountcontrol & 2 as `uac_account_disable`,
 from {{ source("ldap", "src_ldap__user_person") }}
