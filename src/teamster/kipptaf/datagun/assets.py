153958b3a6f16ad4244cadae1a7d2a7e2a95ee0e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-25 17:16:37 +0000

    clever extracts

--- src/teamster/kipptaf/datagun/assets.py
+++ src/teamster/kipptaf/datagun/assets.py
@@ -18,6 +18,13 @@ blissbook_extract_assets = [
     for a in config_from_files([f"{config_dir}/blissbook.yaml"])["assets"]
 ]
 
+clever_extract_assets = [
+    build_bigquery_extract_sftp_asset(
+        code_location=CODE_LOCATION, timezone=LOCAL_TIMEZONE, **a
+    )
+    for a in config_from_files([f"{config_dir}/clever.yaml"])["assets"]
+]
+
 coupa_extract_assets = [
     build_bigquery_extract_sftp_asset(
         code_location=CODE_LOCATION, timezone=LOCAL_TIMEZONE, **a
@@ -47,32 +54,10 @@ idauto_extract_assets = [
     for a in config_from_files([f"{config_dir}/idauto.yaml"])["assets"]
 ]
 
-
 # OLD
-alchemer_extract_assets = generate_extract_assets(
-    code_location=CODE_LOCATION,
-    name="alchemer",
-    extract_type="sftp",
-    timezone=LOCAL_TIMEZONE,
-)
-
-clever_extract_assets = generate_extract_assets(
-    code_location=CODE_LOCATION,
-    name="clever",
-    extract_type="sftp",
-    timezone=LOCAL_TIMEZONE,
-)
-
 deanslist_extract_assets = generate_extract_assets(
     code_location=CODE_LOCATION,
     name="deanslist",
-    extract_type="sftp",
-    timezone=LOCAL_TIMEZONE,
-)
-
-gam_extract_assets = generate_extract_assets(
-    code_location=CODE_LOCATION,
-    name="gam",
     extract_type="sftp",
     timezone=LOCAL_TIMEZONE,
 )
@@ -84,25 +69,15 @@ littlesis_extract_assets = generate_extract_assets(
     timezone=LOCAL_TIMEZONE,
 )
 
-whetstone_extract_assets = generate_extract_assets(
-    code_location=CODE_LOCATION,
-    name="whetstone",
-    extract_type="sftp",
-    timezone=LOCAL_TIMEZONE,
-)
-
 __all__ = [
-    *alchemer_extract_assets,
     *blissbook_extract_assets,
     *clever_extract_assets,
     *coupa_extract_assets,
     *deanslist_extract_assets,
     *egencia_extract_assets,
-    *gam_extract_assets,
     *idauto_extract_assets,
     *illuminate_extract_assets,
     *littlesis_extract_assets,
-    *whetstone_extract_assets,
 ]
 
 # RETIRED #
@@ -112,9 +87,21 @@ __all__ = [
 #     extract_type="sftp",
 #     timezone=LOCAL_TIMEZONE,
 # )
+# alchemer_extract_assets = generate_extract_assets(
+#     code_location=CODE_LOCATION,
+#     name="alchemer",
+#     extract_type="sftp",
+#     timezone=LOCAL_TIMEZONE,
+# )
 # fpodms_extract_assets = generate_extract_assets(
 #     code_location=CODE_LOCATION,
 #     name="fpodms",
+#     extract_type="sftp",
+#     timezone=LOCAL_TIMEZONE,
+# )
+# gam_extract_assets = generate_extract_assets(
+#     code_location=CODE_LOCATION,
+#     name="gam",
 #     extract_type="sftp",
 #     timezone=LOCAL_TIMEZONE,
 # )
@@ -139,6 +126,12 @@ __all__ = [
 # read180_extract_assets = generate_extract_assets(
 #     code_location=CODE_LOCATION,
 #     name="read180",
+#     extract_type="sftp",
+#     timezone=LOCAL_TIMEZONE,
+# )
+# whetstone_extract_assets = generate_extract_assets(
+#     code_location=CODE_LOCATION,
+#     name="whetstone",
 #     extract_type="sftp",
 #     timezone=LOCAL_TIMEZONE,
 # )
