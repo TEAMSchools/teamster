85a5bfe93da88e3dd29570a093626f6dbb592de3
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-27 20:55:13 +0000

    fix order

--- src/dbt/kipptaf/models/adp_workforce_now/staging/stg_adp_workforce_now__person_history.sql
+++ src/dbt/kipptaf/models/adp_workforce_now/staging/stg_adp_workforce_now__person_history.sql
@@ -6,7 +6,7 @@ with
             dbt_utils.deduplicate(
                 relation=source("adp_workforce_now", "person_history"),
                 partition_by="worker_id",
-                order_by="_fivetran_synced",
+                order_by="_fivetran_synced desc",
             )
         }}
     )
