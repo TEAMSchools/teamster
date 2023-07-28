ad540fc8a3fbc320b36ffea9c050a43b07a30360
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-28 18:36:15 +0000

    debug

--- src/teamster/kipptaf/schoolmint/jobs.py
+++ src/teamster/kipptaf/schoolmint/jobs.py
@@ -3,6 +3,14 @@ from dagster import AssetSelection, define_asset_job, job
 from .assets import multi_partition_assets, static_partition_assets
 from .ops import schoolmint_grow_get_user_update_data_op, schoolmint_grow_user_update_op
 
+
+@job
+def schoolmint_grow_user_update_job():
+    users = schoolmint_grow_get_user_update_data_op()
+
+    schoolmint_grow_user_update_op(users)
+
+
 static_partition_asset_job = define_asset_job(
     name="schoolmint_grow_static_partition_asset_job",
     selection=AssetSelection.assets(*static_partition_assets),
@@ -14,14 +22,6 @@ multi_partition_asset_job = define_asset_job(
     selection=AssetSelection.assets(*multi_partition_assets),
     partitions_def=multi_partition_assets[0].partitions_def,
 )
-
-
-@job
-def schoolmint_grow_user_update_job():
-    users = schoolmint_grow_get_user_update_data_op()
-
-    schoolmint_grow_user_update_op(users)
-
 
 __all__ = [
     static_partition_asset_job,
