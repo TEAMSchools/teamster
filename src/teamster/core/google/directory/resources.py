874734e8afdf510b1a8203c954fe13d9cfc5100d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-30 19:59:46 +0000

    directory api methods

--- src/teamster/core/google/directory/resources.py
+++ src/teamster/core/google/directory/resources.py
@@ -9,7 +9,11 @@ class GoogleDirectoryResource(ConfigurableResource):
     service_account_file_path: str = None
     delegated_account: str = None
     version: str = "v1"
-    scopes: list = ["https://www.googleapis.com/auth/admin.directory.user"]
+    scopes: list = [
+        "https://www.googleapis.com/auth/admin.directory.user",
+        "https://www.googleapis.com/auth/admin.directory.group",
+        "https://www.googleapis.com/auth/admin.directory.rolemanagement",
+    ]
     max_results: int = 500
 
     _service: discovery.Resource = PrivateAttr()
@@ -30,32 +34,60 @@ class GoogleDirectoryResource(ConfigurableResource):
             credentials=credentials,
         )
 
-    def list_users(self, **kwargs):
-        customer = kwargs.get("customer", self.customer_id)
-
-        users = []
+    def _list(self, api_name, **kwargs):
+        data = []
         next_page_token = None
+
+        response_data_key = kwargs.pop("response_data_key", api_name)
+        max_results = kwargs.pop("max_results", self.max_results)
+
         while True:
-            data = (
-                self._service.users()
-                .list(
-                    customer=customer,
-                    pageToken=next_page_token,
-                    maxResults=self.max_results,
-                    **kwargs,
-                )
+            response = (
+                getattr(self._service, api_name)()
+                .list(pageToken=next_page_token, maxResults=max_results, **kwargs)
                 .execute()
             )
 
-            next_page_token = data.get("nextPageToken")
-            users.extend(data["users"])
+            next_page_token = response.get("nextPageToken")
+            data.extend(response[response_data_key])
 
-            self.get_resource_context().log.debug(f"Retrieved {len(users)} users")
+            self.get_resource_context().log.debug(f"Retrieved {len(data)} records")
 
             if next_page_token is None:
                 break
 
-        return users
+        return data
+
+    def list_users(self, **kwargs):
+        return self._list(
+            "users", customer=kwargs.get("customer", self.customer_id), **kwargs
+        )
 
     def get_user(self, user_key, **kwargs):
         return self._service.users().get(userKey=user_key, **kwargs).execute()
+
+    def list_groups(self, **kwargs):
+        return self._list(
+            "groups", customer=kwargs.get("customer", self.customer_id), **kwargs
+        )
+
+    def list_members(self, group_key, **kwargs):
+        return self._list("members", groupKey=group_key, **kwargs)
+
+    def list_roles(self, **kwargs):
+        return self._list(
+            "roles",
+            customer=kwargs.get("customer", self.customer_id),
+            max_results=kwargs.get("max_results", 100),
+            response_data_key="items",
+            **kwargs,
+        )
+
+    def list_role_assignments(self, **kwargs):
+        return self._list(
+            "roleAssignments",
+            customer=kwargs.get("customer", self.customer_id),
+            max_results=kwargs.get("max_results", 200),
+            response_data_key="items",
+            **kwargs,
+        )
--- tests/google/directory/test_resources_directory.py
+++ tests/google/directory/test_resources_directory.py
@@ -16,13 +16,41 @@ with build_resources(
     DIRECTORY: GoogleDirectoryResource = resources.directory
 
 
+def test_list_roles():
+    data = DIRECTORY.list_roles()
+
+    with open(file="env/roles.json", mode="w") as f:
+        json.dump(data, f)
+
+
+def test_list_role_assignments():
+    data = DIRECTORY.list_role_assignments()
+
+    with open(file="env/role_assignments.json", mode="w") as f:
+        json.dump(data, f)
+
+
+def test_list_members():
+    data = DIRECTORY.list_members(group_key="group-students-miami@teamstudents.org")
+
+    with open(file="env/members.json", mode="w") as f:
+        json.dump(data, f)
+
+
+def test_list_groups():
+    data = DIRECTORY.list_groups()
+
+    with open(file="env/groups.json", mode="w") as f:
+        json.dump(data, f)
+
+
 def test_get_user():
-    user = DIRECTORY.get_user(user_key="113203151440162455385")
-    print(user)
+    data = DIRECTORY.get_user(user_key="113203151440162455385")
+    print(data)
 
 
 def test_list_users():
-    users = DIRECTORY.list_users(projection="full")
+    data = DIRECTORY.list_users(projection="full")
 
     with open(file="env/users.json", mode="w") as f:
-        json.dump(users, f)
+        json.dump(data, f)
