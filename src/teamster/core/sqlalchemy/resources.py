5930a4902f2a0ea0163233d3e3de765f536e604b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-31 15:34:48 +0000

    refactor ps assets

--- src/teamster/core/sqlalchemy/resources.py
+++ src/teamster/core/sqlalchemy/resources.py
@@ -26,7 +26,14 @@ class SqlAlchemyEngineResource(ConfigurableResource):
 
     _engine: Engine = PrivateAttr()
 
-    def execute_query(self, query, partition_size, output_format, connect_kwargs={}):
+    def execute_query(
+        self,
+        query,
+        partition_size,
+        connect_kwargs={},
+        output_format=None,
+        data_filepath="data/data.avro",
+    ):
         context = self.get_resource_context()
 
         context.log.info("Opening connection to engine")
@@ -77,6 +84,7 @@ class SqlAlchemyEngineResource(ConfigurableResource):
                 output = self.result_to_avro(
                     partitions=cursor_result.partitions(size=partition_size),
                     schema=avro_schema,
+                    data_filepath=pathlib.Path(data_filepath).absolute(),
                 )
 
         return output
@@ -108,14 +116,15 @@ class SqlAlchemyEngineResource(ConfigurableResource):
         return output_data
 
     def result_to_avro(
-        self, partitions: Iterator[Sequence[row.Row[result._TP]]], schema
+        self,
+        partitions: Iterator[Sequence[row.Row[result._TP]]],
+        schema,
+        data_filepath: pathlib.Path,
     ):
         context = self.get_resource_context()
 
-        data_file_path = pathlib.Path("data").absolute() / "data.avro"
-
-        context.log.info(f"Saving results to {data_file_path}")
-        data_file_path.parent.mkdir(parents=True, exist_ok=True)
+        context.log.info(f"Saving results to {data_filepath}")
+        data_filepath.parent.mkdir(parents=True, exist_ok=True)
 
         len_data = 0
         for i, pt in enumerate(partitions):
@@ -129,16 +138,16 @@ class SqlAlchemyEngineResource(ConfigurableResource):
 
             context.log.debug(f"Saving partition {i}")
             if i == 0:
-                with data_file_path.open("wb") as f:
+                with data_filepath.open("wb") as f:
                     writer(fo=f, schema=schema, records=data, codec="snappy")
             else:
-                with data_file_path.open("a+b") as f:
+                with data_filepath.open("a+b") as f:
                     writer(fo=f, schema=schema, records=data, codec="snappy")
 
             del data
             gc.collect()
 
-        return data_file_path
+        return data_filepath
 
 
 class MSSQLResource(ConfigurableResource):
--- tests/powerschool/test_resources_powerschool.py
+++ tests/powerschool/test_resources_powerschool.py
@@ -1,105 +1,101 @@
 import pendulum
-from dagster import EnvVar, build_resources
+from dagster import EnvVar, build_op_context, build_resources
 from fastavro import block_reader
 from sqlalchemy import literal_column, select, table, text
 
 from teamster.core.sqlalchemy.resources import OracleResource, SqlAlchemyEngineResource
 from teamster.core.ssh.resources import SSHConfigurableResource
 
-TEST_ASSETS = [
-    {"asset_name": "gen"},
-    {
-        "asset_name": "schools",
-        "partition_column": "transaction_date",
-        "partition_key": "1970-01-01T00:00:00-00:00",
-    },
-    {
-        "asset_name": "codeset",
-        "partition_column": "whenmodified",
-        "partition_key": "1970-01-01T00:00:00-00:00",
-    },
-]
+with build_resources(
+    resources={
+        "ssh_powerschool": SSHConfigurableResource(
+            remote_host="teamacademy.clgpstest.com",
+            remote_port=EnvVar("STAGING_PS_SSH_PORT"),
+            username=EnvVar("STAGING_PS_SSH_USERNAME"),
+            password=EnvVar("STAGING_PS_SSH_PASSWORD"),
+            tunnel_remote_host=EnvVar("STAGING_PS_SSH_REMOTE_BIND_HOST"),
+        ),
+        "db_powerschool": OracleResource(
+            engine=SqlAlchemyEngineResource(
+                dialect="oracle",
+                driver="oracledb",
+                username="PSNAVIGATOR",
+                host="localhost",
+                database="PSPRODDB",
+                port=1521,
+                password=EnvVar("STAGING_PS_DB_PASSWORD"),
+            ),
+            version="19.0.0.0.0",
+            prefetchrows=100000,
+            arraysize=100000,
+        ),
+    }
+) as resources:
+    SSH_POWERSCHOOL: SSHConfigurableResource = resources.ssh_powerschool
+    DB_POWERSCHOOL: OracleResource = resources.db_powerschool
 
 
-def test_resource():
-    with build_resources(
-        resources={
-            "ssh_powerschool": SSHConfigurableResource(
-                remote_host="teamacademy.clgpstest.com",
-                remote_port=EnvVar("STAGING_PS_SSH_PORT"),
-                username=EnvVar("STAGING_PS_SSH_USERNAME"),
-                password=EnvVar("STAGING_PS_SSH_PASSWORD"),
-                tunnel_remote_host=EnvVar("STAGING_PS_SSH_REMOTE_BIND_HOST"),
-            ),
-            "db_powerschool": OracleResource(
-                engine=SqlAlchemyEngineResource(
-                    dialect="oracle",
-                    driver="oracledb",
-                    username="PSNAVIGATOR",
-                    host="localhost",
-                    database="PSPRODDB",
-                    port=1521,
-                    password=EnvVar("STAGING_PS_DB_PASSWORD"),
-                ),
-                version="19.0.0.0.0",
-                prefetchrows=100000,
-                arraysize=100000,
-            ),
-        }
-    ) as resources:
-        ssh_powerschool: SSHConfigurableResource = resources.ssh_powerschool
-        db_powerschool: OracleResource = resources.db_powerschool
+def _test(asset_name, partition_column=None, partition_key=None, select_columns="*"):
+    context = build_op_context(partition_key=partition_key)
+
+    context.log.info((asset_name, partition_column, partition_key, select_columns))
+
+    # now = pendulum.now()
+
+    if not context.has_partition_key:
+        constructed_where = ""
+    elif context.partition_key == pendulum.from_timestamp(0).to_iso8601_string():
+        constructed_where = ""
+    else:
+        window_start = pendulum.from_format(
+            string=context.partition_key, fmt="YYYY-MM-DDTHH:mm:ssZ"
+        )
+
+        window_start_fmt = window_start.format("YYYY-MM-DDTHH:mm:ss.SSSSSS")
+
+        constructed_where = (
+            f"{partition_column} >= "
+            f"TO_TIMESTAMP('{window_start_fmt}', 'YYYY-MM-DD\"T\"HH24:MI:SS.FF6')"
+        )
+
+    sql = (
+        select(*[literal_column(col) for col in select_columns])
+        .select_from(table(asset_name))
+        .where(text(constructed_where))
+    )
+
+    ssh_tunnel = SSH_POWERSCHOOL.get_tunnel(remote_port=1521, local_port=1521)
+
+    try:
+        context.log.info("Starting SSH tunnel")
+        ssh_tunnel.start()
+
+        file_path = DB_POWERSCHOOL.engine.execute_query(
+            query=sql,
+            partition_size=100000,
+            output_format="avro",
+            data_filepath="env/data.avro",
+        )
 
         try:
-            ssh_tunnel = ssh_powerschool.get_tunnel(remote_port=1521, local_port=1521)
+            with file_path.open(mode="rb") as f:
+                num_records = sum(block.num_records for block in block_reader(f))
+        except FileNotFoundError:
+            num_records = 0
 
-            ssh_tunnel.start()
+        context.log.info(num_records)
+    finally:
+        context.log.info("Stopping SSH tunnel")
+        ssh_tunnel.stop()
 
-            for asset in TEST_ASSETS:
-                partition_key = asset.get("partition_key")
 
-                if partition_key is None:
-                    constructed_where = ""
-                elif partition_key == pendulum.from_timestamp(0).to_iso8601_string():
-                    constructed_where = ""
-                else:
-                    window_start = pendulum.from_format(
-                        string=partition_key, fmt="YYYY-MM-DDTHH:mm:ssZ"
-                    )
+def test_assignmentsection():
+    _test(asset_name="assignmentsection")
 
-                    window_start_fmt = window_start.format("YYYY-MM-DDTHH:mm:ss.SSSSSS")
 
-                    constructed_where = (
-                        f"{asset.get('partition_column')} >= "
-                        f"TO_TIMESTAMP('{window_start_fmt}', "
-                        "'YYYY-MM-DD\"T\"HH24:MI:SS.FF6')"
-                    )
+def test_storedgrades():
+    _test(asset_name="storedgrades")
 
-                sql = (
-                    select(
-                        *[
-                            literal_column(col)
-                            for col in asset.get("select_columns", "*")
-                        ]
-                    )
-                    .select_from(table(asset["asset_name"]))
-                    .where(text(constructed_where))
-                )
 
-                file_path = db_powerschool.engine.execute_query(
-                    query=sql, partition_size=100000, output_format="avro"
-                )
-
-                try:
-                    with file_path.open(mode="rb") as f:
-                        num_records = sum(
-                            block.num_records for block in block_reader(f)
-                        )
-                except FileNotFoundError:
-                    num_records = 0
-
-                if num_records > 0:
-                    print(file_path)
-                    print(num_records)
-        finally:
-            ssh_tunnel.stop()
+def test_schools():
+    _test(asset_name="schools")
