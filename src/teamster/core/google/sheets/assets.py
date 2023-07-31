54eb01cc3a67f6a5edc370cf8007e746f67909c6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-07-31 14:15:17 +0000

    rename keyt

--- src/teamster/core/google/sheets/assets.py
+++ src/teamster/core/google/sheets/assets.py
@@ -6,7 +6,7 @@ from dagster import DataVersion, observable_source_asset
 def build_gsheet_asset(name, code_location, sheet_id, range_name):
     @observable_source_asset(
         name=name,
-        key_prefix=[code_location, "gsheets"],
+        key_prefix=[code_location, "google", "sheets"],
         metadata={"sheet_id": sheet_id, "range_name": range_name},
     )
     def _asset():
