c67ac5b27c68a1804a7b8e616b0022c917515a3f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-08-02 18:01:26 +0000

    staff attrition

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_attrition.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_attrition.sql
@@ -2,87 +2,96 @@ with
     terminations as (
         select
             position_id,
-            status_effective_date,
-            status_effective_end_date,
-            termination_reason_description,
-            lag(status_effective_end_date) over (
-                partition by position_id order by status_effective_date
-            ) as prev_end_date,
-        from people.status_history_static
-        where assignment_status = 'Terminated'
-    )
+            assignment_status_effective_date as termination_effective_date,
+            ifnull(
+                assignment_status_reason_long_name, assignment_status_reason_short_name
+            ) as termination_reason,
+
+            lag(assignment_status_effective_date) over (
+                partition by position_id order by assignment_status_effective_date
+            ) as termination_effective_date_prev
+        from {{ source("adp_workforce_now", "work_assignment_history") }}
+        where assignment_status_long_name = 'Terminated' and _fivetran_active
+    ),
 
     /* final termination record */
     final_termination as (
         select
             *,
             row_number() over (
-                partition by position_id order by status_effective_date desc
-            ) as rn,
+                partition by position_id order by termination_effective_date desc
+            ) as rn_position_termination_desc,
         from terminations
-        where ifnull(date_diff(day, prev_end_date, status_effective_date), 2) > 1
+        where
+            termination_effective_date_prev is null
+            or date_diff(
+                termination_effective_date, termination_effective_date_prev, day
+            )
+            > 1
     ),
 
     roster_terminations as (
         select
+            r.work_assignment_id,
             r.employee_number,
             r.position_id,
-            r.preferred_first_name,
-            r.preferred_last_name,
-            r.race_ethnicity_reporting,
-            r.gender_reporting,
-            r.original_hire_date,
-            r.rehire_date,
-            r.kipp_alumni_status,
-            ifnull(r.rehire_date, r.original_hire_date) as position_start_date,
-
-            if(
-                r.position_status = 'Terminated',
-                ifnull(t.status_effective_date, r.termination_date),
-                null
-            ) as termination_date,
-
+            r.assignment_status,
+            r.preferred_name_given_name,
+            r.preferred_name_family_name,
+            r.ethnicity_long_name,
+            r.gender_long_name,
+            r.worker_original_hire_date,
+            r.worker_rehire_date,
+            r.worker_termination_date,
+            r.home_work_location_grade_band,
+            r.home_work_location_reporting_school_id,
+            null as kipp_alumni_status,
             ifnull(
-                t.termination_reason_description, r.termination_reason
-            ) as status_reason,
-        from people.staff_roster as r
-        left join final_termination as t on r.position_id = t.position_id and t.rn = 1
+                r.worker_rehire_date, r.worker_original_hire_date
+            ) as most_recent_hire_date,
+
+            t.termination_effective_date,
+
+            ifnull(t.termination_reason, r.assignment_status_reason) as status_reason,
+        from {{ ref("base_people__staff_roster") }} as r
+        left join
+            final_termination as t
+            on r.position_id = t.position_id
+            and t.rn_position_termination_desc = 1
     ),
 
-    roster as (
+    with_academic_year as (
         select
             *,
-            {{
-                teamster_utils.date_to_fiscal_year(
-                    date_field="position_start_date",
-                    start_month=9,
-                    year_source="start",
-                )
-            }} as start_academic_year,
-            {{
-                teamster_utils.date_to_fiscal_year(
-                    date_field="termination_date",
-                    start_month=9,
-                    year_source="start",
-                )
-            }} as end_academic_year,
+            if(
+                extract(month from most_recent_hire_date) >= 9,
+                extract(year from most_recent_hire_date),
+                extract(year from most_recent_hire_date) - 1
+
+            ) as start_academic_year,
+            if(
+                extract(month from worker_termination_date) >= 9,
+                extract(year from worker_termination_date),
+                extract(year from worker_termination_date) - 1
+
+            ) as end_academic_year,
         from roster_terminations
     ),
 
-    years as (
+    dates as (
         select
-            n as academic_year,
-            n + 1 as next_academic_year,
+            extract(year from date_day) as academic_year,
+            extract(year from date_day) + 1 as next_academic_year,
 
-            date(n, 7, 1) as default_entry_date,
-            date((n + 1), 6, 30) as default_exit_date,
+            date(extract(year from date_day), 7, 1) as default_entry_date,
+            date((extract(year from date_day) + 1), 6, 30) as default_exit_date,
 
-            date(n, 9, 1) as denominator_start_date
-            date((n + 1), 8, 31) as attrition_date,
+            date(extract(year from date_day), 9, 1) as denominator_start_date,
+            date((extract(year from date_day) + 1), 8, 31) as attrition_date,
 
-            date((n + 1), 4, 30) as effective_date,
+            date((extract(year from date_day) + 1), 4, 30) as effective_date,
         from {{ ref("utils__date_spine") }}
-        where n between 2002 and ({{ var("current_academic_year") }} + 1)
+        where extract(month from date_day) = 7 and extract(day from date_day) = 1
     ),
 
     roster_year_scaffold as (
@@ -92,84 +101,87 @@ with
             y.*,
 
             if(
-                r.end_academic_year = y.academic_year, r.termination_date, null
+                r.assignment_status = 'Terminated'
+                and r.end_academic_year = y.academic_year,
+                ifnull(r.termination_effective_date, r.worker_termination_date),
+                null
             ) as termination_date,
 
             if(
                 r.start_academic_year = y.academic_year,
-                r.position_start_date,
+                r.most_recent_hire_date,
                 y.default_entry_date
             ) as academic_year_entrydate,
+        from with_academic_year as r
+        inner join
+            dates as y
+            on y.academic_year between r.start_academic_year and r.end_academic_year
+    ),
 
+    with_academic_year_exitdate as (
+        select
+            *,
             ifnull(
-                if(r.end_academic_year = y.academic_year, r.termination_date, null),
+                if(academic_year = end_academic_year, termination_date, null),
                 default_exit_date
             ) as academic_year_exitdate,
-        from roster as r
-        inner join
-            years as y
-            on y.academic_year between r.start_academic_year and r.end_academic_year
+        from roster_year_scaffold
     ),
 
     scaffold as (
         select
             rys.*,
 
-            w.business_unit,
-            w.job_title,
-            w.location,
-            w.home_department,
-
-            scw.school_level,
-            scw.reporting_school_id,
+            w.work_assignment_job_title as job_title,
+            w.organizational_unit_business_unit_home_name as business_unit_home_name,
+            w.organizational_unit_department_home_name as department_home_name,
+            ifnull(
+                w.work_assignment_home_work_location_name_long_name,
+                w.work_assignment_home_work_location_name_short_name
+            ) as home_work_location_name,
 
             lead(rys.academic_year_exitdate, 1) over (
                 partition by rys.position_id order by rys.academic_year
             ) as academic_year_exitdate_next,
-        from roster_year_scaffold as rys
+        from with_academic_year_exitdate as rys
         left join
-            people.employment_history_static as w
-            on rys.position_id = w.position_id
-            and rys.effective_date
-            between w.effective_start_date and w.effective_end_date
-        left join
-            people.school_crosswalk as scw
-            on w.location = scw.site_name
-            and scw._fivetran_deleted = 0
+            {{ ref("base_adp_workforce_now__worker_person") }} as w
+            on rys.work_assignment_id = w.work_assignment_id
+            and w.work_assignment__fivetran_active
         where rys.academic_year_exitdate > rys.academic_year_entrydate
     ),
 
     with_dates as (
         select
-            *
-            ifnull(rehire_date, original_hire_date) as most_recent_hire_date,
+            *,
             ifnull(academic_year_exitdate_next, termination_date) as attrition_exitdate,
         from scaffold
     )
 
 select
     employee_number as df_employee_number,
-    preferred_first_name,
-    preferred_last_name,
-    race_ethnicity_reporting as primary_ethnicity,
-    gender_reporting,
+    preferred_name_given_name as preferred_first_name,
+    preferred_name_family_name as preferred_last_name,
+    ethnicity_long_name as primary_ethnicity,
+    gender_long_name as gender_reporting,
     academic_year,
     academic_year_entrydate,
     academic_year_exitdate,
-    original_hire_date,
-    rehire_date,
+    worker_original_hire_date as original_hire_date,
+    worker_rehire_date as rehire_date,
     termination_date,
     status_reason,
     job_title as primary_job,
-    home_department as primary_on_site_department,
-    `location` as primary_site,
-    business_unit as legal_entity_name,
-    reporting_school_id as primary_site_reporting_schoolid,
-    school_level as primary_site_school_level,
+    department_home_name as primary_on_site_department,
+    home_work_location_name as primary_site,
+    business_unit_home_name as legal_entity_name,
+    home_work_location_reporting_school_id as primary_site_reporting_schoolid,
+    home_work_location_grade_band as primary_site_school_level,
     kipp_alumni_status,
     academic_year_exitdate_next as next_academic_year_exitdate,
+
     case
-        when date_diff(day, academic_year_entrydate, academic_year_exitdate) <= 0
+        when date_diff(academic_year_exitdate, academic_year_entrydate, day) <= 0
         then 0
         when
             academic_year_exitdate >= denominator_start_date
@@ -178,11 +190,11 @@ select
         else 0
     end as is_denominator,
 
-    if(attrition_exitdate <= attrition_date, 1, 0) as is_attrition,
+    if(attrition_exitdate <= attrition_date, 1.0, 0.0) as is_attrition,
 
     if(
         most_recent_hire_date > attrition_exitdate,
-        round(datediff(day, original_hire_date, attrition_exitdate) / 365, 0),
-        round(datediff(day, most_recent_hire_date, attrition_exitdate) / 365, 0)
+        round(date_diff(attrition_exitdate, worker_original_hire_date, day) / 365, 0),
+        round(date_diff(attrition_exitdate, most_recent_hire_date, day) / 365, 0)
     ) as years_at_kipp,
-from attrition_exitdate
+from with_dates
