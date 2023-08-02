9dd811b49f5a9eb309ab577f5eb4c6e56bc7d561
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-08-02 03:39:03 +0000

    alchemer models

--- src/dbt/kipptaf/models/surveys/intermediate/int_surveys__survey_response_identifiers.sql
+++ src/dbt/kipptaf/models/surveys/intermediate/int_surveys__survey_response_identifiers.sql
@@ -1,3 +1,5 @@
+{{ config(enabled=False) }}
+
 {%- set identifier_shortnames = [
     "respondent_df_employee_number",
     "respondent_userprincipalname",
@@ -30,7 +32,7 @@ with
             regexp_extract(sc.name, r'\d+s+(\w+)') as campaign_reporting_term,
         from {{ ref("stg_alchemer__survey_question") }} as sq
         inner join
-            {{ ref("stg_alchemer__survey_response_data") }} as srd
+            {{ ref("stg_alchemer__survey_response__survey_data") }} as srd
             on sq.survey_id = srd.survey_id
             and sq.id = srd.question_id
             and srd.string_value is not null
@@ -59,25 +61,24 @@ with
             campaign_fiscal_year,
             campaign_name,
             campaign_reporting_term,
-
             salesforce_id as respondent_salesforce_id,
-            safe_cast(respondent_adp_associate_id as string) as respondent_associate_id,
+            safe_cast(respondent_adp_worker_id as string) as respondent_associate_id,
             safe_cast(
-                lower(coalesce(respondent_userprincipalname, email)) as string
-            ) as respondent_userprincipalname,
+                lower(ifnull(respondent_userprincipalname, email)) as string
+            ) as respondent_user_principal_name,
+
             if
             (
                 safe_cast(respondent_df_employee_number as int) is not null,
                 safe_cast(respondent_df_employee_number as int),
                 safe_cast(
                     regexp_extract(
-                        coalesce(
-                            respondent_df_employee_number, employee_preferred_name
-                        ),
+                        ifnull(respondent_df_employee_number, employee_preferred_name),
                         r'\[(\d+)\]'
                     ) as int
                 )
             ) as respondent_employee_number,
+
             if
             (
                 safe_cast(subject_df_employee_number as int) is not null,
@@ -89,11 +90,13 @@ with
                     ) as int
                 )
             ) as subject_employee_number,
+
             if(
                 regexp_extract(subject_df_employee_number, r'\[(\d+)\]') is null,
                 subject_df_employee_number,
                 null
             ) as subject_preferred_name,
+
             case
                 is_manager
                 when 'Yes - I am their manager.'
@@ -101,7 +104,7 @@ with
                 when 'No - I am their peer.'
                 then false
                 else safe_cast(safe_cast(is_manager as int) as boolean)
-            end as is_manager
+            end as is_manager,
         from
             identifier_responses pivot (
                 max(answer) for shortname
@@ -117,8 +120,8 @@ with
                 order_by="response_date_submitted desc",
             )
         }}
-    )
-{# 
+    ),
+
     response_clean as (
         select
             rp.survey_response_id,
@@ -127,37 +130,37 @@ with
             rp.subject_preferred_name,
             rp.is_manager,
             rp.salesforce_id,
+            rp.respondent_user_principal_name as nonstaff_email,
+
             ab.subject_preferred_name_duplicate,
+
             coalesce(
-                rp.subject_employee_number, ab.subject_df_employee_number
+                rp.subject_employee_number, ab.subject_employee_number
             ) as subject_employee_number,
+
             coalesce(
                 rp.respondent_employee_number,
-                upn.df_employee_number,
-                adp.df_employee_number,
-                mail.df_employee_number,
-                ab.df_employee_number
+                upn.employee_number,
+                adp.employee_number,
+                mail.employee_number,
+                ab.employee_number
             ) as respondent_employee_number,
-            rp.respondent_userprincipalname as nonstaff_email
-        from identifier_responses_pivot as rp
+        from identifier_responses_deduplicate as rp
         left join
             {{ ref("base_people__staff_roster") }} as upn
-            on (rp.respondent_userprincipalname = upn.userprincipalname)
+            on rp.respondent_user_principal_name = upn.user_principal_name
         left join
             {{ ref("base_people__staff_roster") }} as adp
-            on (rp.respondent_associate_id = adp.adp_associate_id_legacy)
+            on rp.respondent_associate_id = adp.adp_worker_id_legacy
         left join
             {{ ref("base_people__staff_roster") }} as mail
-            on (rp.respondent_userprincipalname = mail.mail)
+            on rp.respondent_user_principal_name = mail.mail
         left join
-            surveys.surveygizmo_abnormal_respondents as ab
-            on (
-                rp.survey_id = ab.survey_id
-                and rp.survey_response_id = ab.survey_response_id
-                and ab._fivetran_deleted = 0
-            )
+            {{ source("alchemer", "src_alchemer__response_id_override") }} as ab
+            on rp.survey_id = ab.survey_id
+            and rp.survey_response_id = ab.survey_response_id
     )
- #}
+
 select
     rc.survey_response_id,
     rc.survey_id,
@@ -173,73 +176,76 @@ select
     rc.respondent_employee_number,
     rc.respondent_salesforce_id,
 
-{# resp.preferred_name as respondent_preferred_name,
-    resp.adp_associate_id as respondent_adp_associate_id,
+    resp.preferred_name_lastfirst as respondent_preferred_name,
+    resp.worker_id as respondent_adp_worker_id,
     resp.mail as respondent_mail,
-    resp.samaccountname as respondent_samaccountname,
+    resp.sam_account_name as respondent_sam_account_name,
 
-    reh.business_unit as respondent_legal_entity_name,
-    reh.location as respondent_primary_site,
-    reh.home_department as respondent_department_name,
+    reh.business_unit_home_name as respondent_legal_entity_name,
+    reh.home_work_location_name as respondent_primary_site,
+    reh.department_home_name as respondent_department_name,
     reh.job_title as respondent_primary_job,
-    reh.position_status as respondent_position_status,
-    reh.reports_to_employee_number as respondent_manager_df_employee_number,
+    reh.assignment_status as respondent_assignment_status,
+    reh.report_to_employee_number as respondent_manager_df_employee_number,
 
-    rsch.ps_school_id as respondent_primary_site_schoolid,
-    rsch.school_level as respondent_primary_site_school_level,
+    rsch.powerschool_school_id as respondent_primary_site_schoolid,
+    rsch.grade_band as respondent_primary_site_grade_band,
 
-    rmgr.preferred_name as respondent_manager_name,
+    rmgr.preferred_name_lastfirst as respondent_manager_name,
     rmgr.mail as respondent_manager_mail,
-    rmgr.userprincipalname as respondent_manager_userprincipalname,
-    rmgr.samaccountname as respondent_manager_samaccountname,
+    rmgr.user_principal_name as respondent_manager_user_principal_name,
+    rmgr.sam_account_name as respondent_manager_sam_account_name,
 
-    subj.preferred_name as subject_preferred_name,
-    subj.adp_associate_id as subject_adp_associate_id,
-    subj.userprincipalname as subject_userprincipalname,
+    subj.preferred_name_lastfirst as subject_preferred_name,
+    subj.worker_id as subject_adp_worker_id,
+    subj.user_principal_name as subject_user_principal_name,
     subj.mail as subject_mail,
-    subj.samaccountname as subject_samaccountname,
+    subj.sam_account_name as subject_sam_account_name,
 
-    seh.business_unit as subject_legal_entity_name,
-    seh.location as subject_primary_site,
-    seh.home_department as subject_department_name,
+    seh.business_unit_home_name as subject_legal_entity_name,
+    seh.home_work_location_name as subject_primary_site,
+    seh.department_home_name as subject_department_name,
     seh.job_title as subject_primary_job,
-    seh.reports_to_employee_number as subject_manager_df_employee_number,
+    seh.report_to_employee_number as subject_manager_df_employee_number,
 
-    ssch.ps_school_id as subject_primary_site_schoolid,
-    ssch.school_level as subject_primary_site_school_level,
+    ssch.powerschool_school_id as subject_powerschool_school_id,
+    ssch.grade_band as subject_primary_site_grade_band,
 
-    smgr.preferred_name as subject_manager_name,
+    smgr.preferred_name_lastfirst as subject_manager_name,
     smgr.mail as subject_manager_mail,
-    smgr.userprincipalname as subject_manager_userprincipalname,
-    smgr.samaccountname as subject_manager_samaccountname,
+    smgr.user_principal_name as subject_manager_user_principal_name,
+    smgr.sam_account_name as subject_manager_sam_account_name,
 
-    coalesce(
+    ifnull(
         rc.is_manager,
-        if(rc.respondent_employee_number = seh.reports_to_employee_number, true, false)
+        if(rc.respondent_employee_number = seh.report_to_employee_number, true, false)
     ) as is_manager,
-    coalesce(resp.userprincipalname, rc.nonstaff_email) as respondent_userprincipalname, #}
-from
-    identifier_responses_deduplicate as rc
-    {# left join
+
+    ifnull(
+        resp.user_principal_name, rc.nonstaff_email
+    ) as respondent_user_principal_name,
+from identifier_responses_deduplicate as rc
+left join
     {{ ref("base_people__staff_roster") }} as resp
-    on rc.respondent_employee_number = resp.df_employee_number
+    on rc.respondent_employee_number = resp.employee_number
 left join
     people.employment_history_static as reh
     on resp.position_id = reh.position_id
     and sc.link_close_date between reh.effective_start_date and reh.effective_end_date
 left join
     {{ ref("base_people__staff_roster") }} as rmgr
-    on reh.reports_to_employee_number = rmgr.df_employee_number
-left join people.school_crosswalk as rsch on reh.location = rsch.site_name
+    on reh.report_to_employee_number = rmgr.employee_number
+left join
+    people.school_crosswalk as rsch on reh.home_work_location_name = rsch.site_name
 left join
     {{ ref("base_people__staff_roster") }} as subj
-    on rc.subject_employee_number = subj.df_employee_number
+    on rc.subject_employee_number = subj.employee_number
 left join
     people.employment_history_static as seh
     on subj.position_id = seh.position_id
     and sc.link_close_date between seh.effective_start_date and seh.effective_end_date
 left join
     {{ ref("base_people__staff_roster") }} as smgr
-    on seh.reports_to_employee_number = smgr.df_employee_number
-left join people.school_crosswalk as ssch on seh.location = ssch.site_name #}
-    
+    on seh.report_to_employee_number = smgr.employee_number
+left join
+    people.school_crosswalk as ssch on seh.home_work_location_name = ssch.site_name
