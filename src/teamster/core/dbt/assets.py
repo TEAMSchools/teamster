e433be4c508fc206c1eb303be7c36082d023c24e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-08-04 00:57:45 +0000

    rm bq, add selection

--- src/teamster/core/dbt/assets.py
+++ src/teamster/core/dbt/assets.py
@@ -17,7 +17,6 @@ from dagster_dbt.asset_utils import (
     MANIFEST_METADATA_KEY,
 )
 from dagster_dbt.dagster_dbt_translator import DbtManifestWrapper
-from dagster_gcp import BigQueryResource
 
 
 def get_custom_dagster_dbt_translator(code_location):
@@ -85,7 +84,7 @@ def build_dbt_external_source_assets(code_location):
         key_prefix = source["fqn"][1:-2]
 
         if not key_prefix:
-            key_prefix = [source["source_name"]]
+            key_prefix = [source_name]
 
         identifier = source["identifier"]
 
@@ -106,7 +105,6 @@ def build_dbt_external_source_assets(code_location):
                 **dagster_dbt_translator.get_metadata(source),
                 MANIFEST_METADATA_KEY: DbtManifestWrapper(manifest=manifest),
                 DAGSTER_DBT_TRANSLATOR_METADATA_KEY: dagster_dbt_translator,
-                "dataset": source["schema"],
             },
         )
         dep_key = AssetKey([code_location, *key_prefix, dep_name])
@@ -127,28 +125,23 @@ def build_dbt_external_source_assets(code_location):
         can_subset=True,
         op_tags={"dagster-dbt/select": "tag:stage_external_sources"},
     )
-    def _assets(
-        context: AssetExecutionContext,
-        db_bigquery: BigQueryResource,
-        dbt_cli: DbtCliResource,
-    ):
-        # unpack datasets from selected assets
-        datasets = set()
+    def _assets(context: AssetExecutionContext, dbt_cli: DbtCliResource):
+        # create source selection
+        source_selection = []
         for asset_key in context.selected_asset_keys:
-            datasets.add(context.assets_def.metadata_by_key[asset_key]["dataset"])
-
-        # ensure dataset exists in BQ
-        with db_bigquery.get_client() as bq:
-            bq_client = bq
-
-        for dataset in datasets:
-            bq_client.create_dataset(dataset=dataset, exists_ok=True)
+            source_selection.append(
+                context.assets_def.group_names_by_key[asset_key]
+                + "."
+                + asset_key.path[-1]
+            )
 
         # run dbt stage_external_sources
         dbt_run_operation = dbt_cli.cli(
             args=[
                 "run-operation",
                 "stage_external_sources",
+                "--args",
+                json.dumps({"select": " ".join(source_selection)}),
                 "--vars",
                 json.dumps({"ext_full_refresh": True}),
             ],
@@ -163,49 +156,3 @@ def build_dbt_external_source_assets(code_location):
             yield Output(value=None, output_name=output_name)
 
     return _assets
-
-
-def build_external_source_asset(
-    code_location, name, dbt_package_name, upstream_asset_key, group_name
-):
-    dagster_dbt_translator = get_custom_dagster_dbt_translator(code_location)
-    manifest_file = pathlib.Path(f"src/dbt/{code_location}/target/manifest.json")
-
-    manifest = json.loads(s=manifest_file.read_text())
-
-    @asset(
-        key=[code_location, dbt_package_name, name],
-        deps=[upstream_asset_key],
-        compute_kind="dbt",
-        group_name=group_name,
-    )
-    def _asset(
-        context: AssetExecutionContext,
-        dbt_cli: DbtCliResource,
-        db_bigquery: BigQueryResource,
-    ):
-        dataset_name = f"{code_location}_{dbt_package_name}"
-
-        # create BigQuery dataset, if not exists
-        context.log.info(f"Creating dataset {dataset_name}")
-        with db_bigquery.get_client() as bq:
-            bq.create_dataset(dataset=dataset_name, exists_ok=True)
-
-        # stage_external_sources
-        dbt_run_operation = dbt_cli.cli(
-            args=[
-                "run-operation",
-                "stage_external_sources",
-                "--args",
-                json.dumps({"select": f"{dbt_package_name}.{name}"}),
-                "--vars",
-                json.dumps({"ext_full_refresh": True}),
-            ],
-            manifest=manifest,
-            dagster_dbt_translator=dagster_dbt_translator(),
-        )
-
-        for event in dbt_run_operation.stream_raw_events():
-            context.log.info(event)
-
-    return _asset
