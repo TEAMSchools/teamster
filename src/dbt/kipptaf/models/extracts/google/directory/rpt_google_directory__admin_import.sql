39466b822060dc3afc4b23d4ef5a84f7c062c9dc
Charlie Bini <cbini87@gmail.com>
2023-08-07 03:16:20 +0000

    admin import

--- src/dbt/kipptaf/models/extracts/google/directory/rpt_google_directory__admin_import.sql
+++ src/dbt/kipptaf/models/extracts/google/directory/rpt_google_directory__admin_import.sql
@@ -1,58 +1,72 @@
-select
-    google_email,
-    home_work_location_powerschool_school_id,
-    case
-    home_work_location_dagster_code_location
-    when 'kippnewark' then '/Students/TEAM/'
-    when 'kippcamden' then '/Students/KCNA/'
-    when 'kippmiami' then '/Students/Miami/'
-    end as ou_region,
-    case
-        home_work_location_powerschool_school_id
-        /* TEAM */
-        when 133570965
-        then 'TEAM Academy'
-        when 73252
-        then 'Rise'
-        when 73253
-        then 'NCA'
-        when 73254
-        then 'SPARK'
-        when 73255
-        then 'THRIVE'
-        when 73256
-        then 'Seek'
-        when 73257
-        then 'Life'
-        when 73258
-        then 'BOLD'
-        when 73259
-        then 'Upper Roseville'
-        when 732511
-        then 'Newark Lab'
-        when 732513
-        then 'KJA'
-        when 732514
-        then 'KPA'
-        /* KCNA */
-        when 179901
-        then 'LSP'
-        when 179902
-        then 'LSM'
-        when 179903
-        then 'KHM'
-        when 179904
-        then 'KCNHS'
-        when 179905
-        then 'KSE'
-        /* KMS */
-        when 30200803
-        then 'Courage'
-        when 30200804
-        then 'Royalty Academy'
-    end as ou_school
-from {{ ref("base_people__staff_roster") }}
-where
-    user_principal_name is not null
-    and assignment_status not in ('Terminated', 'Deceased')
-    and home_work_location_powerschool_school_id != 0
+with
+    staff_roster as (
+        select
+            sr.google_email,
+            case
+                sr.home_work_location_dagster_code_location
+                when 'kippnewark'
+                then '/Students/TEAM/'
+                when 'kippcamden'
+                then '/Students/KCNA/'
+                when 'kippmiami'
+                then '/Students/Miami/'
+            end || case
+                sr.home_work_location_powerschool_school_id
+                /* TEAM */
+                when 133570965
+                then 'TEAM Academy'
+                when 73252
+                then 'Rise'
+                when 73253
+                then 'NCA'
+                when 73254
+                then 'SPARK'
+                when 73255
+                then 'THRIVE'
+                when 73256
+                then 'Seek'
+                when 73257
+                then 'Life'
+                when 73258
+                then 'BOLD'
+                when 73259
+                then 'Upper Roseville'
+                when 732511
+                then 'Newark Lab'
+                when 732513
+                then 'KJA'
+                when 732514
+                then 'KPA'
+                /* KCNA */
+                when 179901
+                then 'LSP'
+                when 179902
+                then 'LSM'
+                when 179903
+                then 'KHM'
+                when 179904
+                then 'KCNHS'
+                when 179905
+                then 'KSE'
+                /* KMS */
+                when 30200803
+                then 'Courage'
+                when 30200804
+                then 'Royalty Academy'
+            end as ou_name,
+
+            u.id as `assignedTo`,
+        from {{ ref("base_people__staff_roster") }} as sr
+        inner join
+            {{ ref("stg_google_directory__users") }} as u
+            on sr.google_email = u.primary_email
+        where
+            sr.user_principal_name is not null
+            and sr.assignment_status not in ('Terminated', 'Deceased')
+            and sr.home_work_location_powerschool_school_id != 0
+    )
+
+select sr.`assignedTo`, r.role_id as `roleId`, 'ORG_UNIT' as `scopeType`,
+from staff_roster as sr
+inner join
+    {{ ref("stg_google_directory__roles") }} as r on r.role_name = 'Reset Student PW'
