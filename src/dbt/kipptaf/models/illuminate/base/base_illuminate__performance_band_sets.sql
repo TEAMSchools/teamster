be1f7f8f2c7f79312f20d352e50da1c1e5a61bfc
Charlie Bini <cbini87@gmail.com>
2023-08-09 20:50:21 +0000

    finish assessment views

--- src/dbt/kipptaf/models/illuminate/base/base_illuminate__performance_band_sets.sql
+++ src/dbt/kipptaf/models/illuminate/base/base_illuminate__performance_band_sets.sql
@@ -9,6 +9,11 @@ select
             from=ref_pb, relation_alias="pb", except=["performance_band_set_id"]
         )
     }},
+    
+    lead(minimum_value, 1, 9999) over (
+        partition by pbs.performance_band_set_id order by pb.label_number
+    )
+    - 0.1 as maximum_value,
 from {{ ref_pbs }} as pbs
 inner join
     {{ ref_pb }} as pb on pbs.performance_band_set_id = pb.performance_band_set_id
