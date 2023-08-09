8282c2ba740d36ad07fa11b65555620fc878710b
Charlie Bini <cbini87@gmail.com>
2023-08-09 21:07:42 +0000

    fmt

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada.sql
@@ -1,14 +1,4 @@
-SELECT
-    mem._dbt_source_relation,
-    mem.yearid,
-    mem.studentid,
-    ROUND(AVG(CAST(mem.attendancevalue AS FLOAT64)), 3) AS ada
-FROM
-    {{ ref("int_powerschool__ps_adaadm_daily_ctod") }} AS mem
-WHERE
-    mem.membershipvalue = 1
-    AND mem.calendardate <= CAST(CURRENT_TIMESTAMP() AS DATE)
-GROUP BY
-    mem._dbt_source_relation,
-    mem.yearid,
-    mem.studentid
+select _dbt_source_relation, yearid, studentid, avg(attendancevalue) as ada,
+from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }}
+where membershipvalue = 1 and calendardate <= current_date('America/New_York')
+group by _dbt_source_relation, yearid, studentid
