1a356bb6c7fa77cb9beda7bd9aaea84a99647514
Charlie Bini <cbini87@gmail.com>
2023-08-10 17:21:47 +0000

    rename

--- src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__people_staff_info_feed_and_links.sql
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__people_staff_info_feed_and_links.sql
@@ -7,7 +7,7 @@ select
     w.home_work_location_name as work_location,
     w.job_title,
     w.worker_original_hire_date as hire_date,
-    w.communication_business_email,
+    w.communication_business_email as business_email,
     concat(
         coalesce(w.preferred_name_given_name, w.legal_name_given_name),
         ' ',
@@ -15,18 +15,16 @@ select
         ' - ',
         w.home_work_location_name,
         ' (',
-        ltrim(cast(w.employee_number as string format '999999')),
+        w.employee_number,
         ')'
-    ) as respondant_response_name,
+    ) as respondent_response_name,
     regexp_extract(w.communication_business_email, r'^(.*?)@') as worker_username,
 
     s.last_submitted_time,
-
     s.race_ethnicity as last_submitted_race_ethnicity,
     s.gender_identity as last_submitted_gender_identity,
     s.community_grew_up as last_submitted_community_grew_up,
-    s.community_professional_exp
-    as last_submitted_community_professional_experience,
+    s.community_professional_exp as last_submitted_community_professional_experience,
     s.languages_spoken as last_submitted_languages_spoken,
     s.level_of_education as last_submitted_level_of_education,
     s.undergraduate_school as last_submitted_undergraduate_school,
@@ -38,7 +36,11 @@ select
     s.path_to_education as last_submitted_path_to_education,
     replace(
         concat(
-            "https://docs.google.com/forms/d/e/1FAIpQLSdxkHheRKAQQL5WjhbWwwGiC34weMX1LKcIDAt94cR78csfXw/viewform?usp=pp_url&entry.1744062351=",
+            'https://docs.google.com/forms/d/e/',
+            '1FAIpQLSdxkHheRKAQQL5WjhbWwwGiC34weMX1LKcIDAt94cR78csfXw',
+            '/viewform',
+            '?usp=pp_url',
+            '&entry.1744062351=',
             coalesce(w.preferred_name_given_name, w.legal_name_given_name),
             ' ',
             coalesce(w.preferred_name_family_name, w.legal_name_family_name),
@@ -46,7 +48,8 @@ select
             ifnull(w.home_work_location_name, ''),
             ' (',
             ltrim(cast(w.employee_number as string format '999999')),  -- Name + ID
-            ')&entry.1688914034=',
+            ')',
+            '&entry.1688914034=',
             replace(ifnull(w.race_ethnicity, ''), ', ', '&entry.1688914034='),  -- Race/Ethnicity
             '&entry.600247632=',
             ifnull(w.gender_identity, ''),  -- gender identity
@@ -54,9 +57,7 @@ select
             replace(ifnull(w.community_grew_up, ''), ', ', '&entry.2102492257='),  -- Community Grew Up
             '&entry.1197736651=',
             replace(
-                ifnull(w.community_professional_exp, ''),
-                ', ',
-                '&entry.1197736651='
+                ifnull(w.community_professional_exp, ''), ', ', '&entry.1197736651='
             ),  -- Community Work Exp
             '&entry.1551531542=',
             replace(ifnull(w.languages_spoken, ''), ',', '&entry.1551531542='),  -- languages spoken
@@ -78,27 +79,22 @@ select
             ifnull(w.relay_status, ''),  -- relay status
             '&entry.1130804124=',
             replace(ifnull(w.path_to_education, ''), ',', '&entry.1130804124='),  -- path to education'
-            case
-                when
-                    w.job_title in (
-                        'Teacher',
-                        'Teacher ESL',
-                        'Learning Specialist',
-                        'Licensed Practical Nurse',
-                        'Registered Nurse'
-                    )
-                then '&entry.1444365185=Yes'
-                else ''
-            end
+            if(
+                w.job_title in (
+                    'Teacher',
+                    'Teacher ESL',
+                    'Learning Specialist',
+                    'Licensed Practical Nurse',
+                    'Registered Nurse'
+                ),
+                '&entry.1444365185=Yes',
+                ''
+            )
         ),
         ' ',
         '+'
-    ) as staff_info_update_personal_link
-
+    ) as staff_info_update_personal_link,
 from {{ ref("base_people__staff_roster") }} as w
 left join
-    {{ ref('int_surveys__staff_information_survey_pivot') }} as s
-    on (
-        w.employee_number = s.employee_number
-        and s.rn_submission = 1
-    )
+    {{ ref("int_surveys__staff_information_survey_pivot") }} as s
+    on w.employee_number = s.employee_number
