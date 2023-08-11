9ad8a207a9a0c3971986f0dfd865e0af810adfdf
Charlie Bini <cbini87@gmail.com>
2023-08-11 23:31:46 +0000

    iready views

--- src/dbt/kipptaf/models/assessments/staging/stg_assessments__iready_crosswalk.sql
+++ src/dbt/kipptaf/models/assessments/staging/stg_assessments__iready_crosswalk.sql
@@ -1,1 +1,2 @@
-select * from {{ source("assessments", "src_assessments__iready_crosswalk") }}
+select *, safe_cast(grade_level as string) as grade_level_string,
+from {{ source("assessments", "src_assessments__iready_crosswalk") }}
