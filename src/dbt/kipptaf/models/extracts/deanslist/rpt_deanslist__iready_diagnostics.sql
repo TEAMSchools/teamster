d7d8e84b9b75b6a26b3908ae3e8bccf82f3d2b8f
Anthony Walters <awalters@kippteamandfamily.org>
2023-09-07 23:12:08 +0000

    no more steps I won't be doing more steps

--- src/dbt/kipptaf/models/extracts/deanslist/rpt_deanslist__iready_diagnostics.sql
+++ src/dbt/kipptaf/models/extracts/deanslist/rpt_deanslist__iready_diagnostics.sql
@@ -1,53 +1,43 @@
-SELECT
-    'Test Rounds' AS DOMAIN,
-    LEFT(IR.ACADEMIC_YEAR, 4) AS ACADEMIC_YEAR,
-    IR.STUDENT_ID AS STUDENT_NUMBER,
-    IR.SUBJECT,
-    CAST(IR.COMPLETION_DATE AS DATE) AS COMPLETION_DATE,
-    IR.TEST_ROUND,
-    IR.TEST_ROUND_DATE,
-    IR.PERCENTILE,
-    IR.OVERALL_SCALE_SCORE,
-    IR.PERCENT_PROGRESS_TO_ANNUAL_TYPICAL_GROWTH AS PCT_PROGRESS_TYPICAL,
-    IR.PERCENT_PROGRESS_TO_ANNUAL_STRETCH_GROWTH AS PCT_PROGRESS_STRETCH
-FROM
-    {{ ref('base_iready__diagnostic_results') }} AS IR
-WHERE
-    IR.RN_SUBJ_ROUND = 1
-    AND IR.TEST_ROUND != 'Outside Round'
-UNION ALL
-SELECT
-    'YTD Growth' AS DOMAIN,
-    LEFT(IR.ACADEMIC_YEAR, 4) AS ACADEMIC_YEAR,
-    IR.STUDENT_ID AS STUDENT_NUMBER,
-    IR.SUBJECT,
-    CAST(IR.COMPLETION_DATE AS DATE) AS COMPLETION_DATE,
-    IR.TEST_ROUND,
-    IR.TEST_ROUND_DATE,
-    IR.PERCENTILE,
-    IR.OVERALL_SCALE_SCORE,
-    IR.PERCENT_PROGRESS_TO_ANNUAL_TYPICAL_GROWTH AS PCT_PROGRESS_TYPICAL,
-    IR.PERCENT_PROGRESS_TO_ANNUAL_STRETCH_GROWTH AS PCT_PROGRESS_STRETCH
-FROM
-    {{ ref('base_iready__diagnostic_results') }} AS IR
-INNER JOIN (
-    SELECT
-        ACADEMIC_YEAR,
-        SUBJECT,
-        STUDENT_ID,
-        MAX(COMPLETION_DATE) AS MAX_COMPLETION_DATE
-    FROM
-        {{ ref('base_iready__diagnostic_results') }}
-    GROUP BY
-        ACADEMIC_YEAR,
-        SUBJECT,
-        STUDENT_ID
-) AS SUB
-    ON
-        IR.ACADEMIC_YEAR = SUB.ACADEMIC_YEAR
-        AND IR.STUDENT_ID = SUB.STUDENT_ID
-        AND IR.SUBJECT = SUB.SUBJECT
-        AND IR.COMPLETION_DATE = SUB.MAX_COMPLETION_DATE
-WHERE
-    CAST(LEFT(IR.ACADEMIC_YEAR, 4) AS INT)
-    = {{ var("current_academic_year") }}
+select
+    'Test Rounds' as domain,
+    left(ir.academic_year, 4) as academic_year,
+    ir.student_id as student_number,
+    ir.subject,
+    cast(ir.completion_date as date) as completion_date,
+    ir.test_round,
+    ir.test_round_date,
+    ir.percentile,
+    ir.overall_scale_score,
+    ir.percent_progress_to_annual_typical_growth as pct_progress_typical,
+    ir.percent_progress_to_annual_stretch_growth as pct_progress_stretch
+from {{ ref("base_iready__diagnostic_results") }} as ir
+where ir.rn_subj_round = 1 and ir.test_round != 'Outside Round'
+union all
+select
+    'YTD Growth' as domain,
+    left(ir.academic_year, 4) as academic_year,
+    ir.student_id as student_number,
+    ir.subject,
+    cast(ir.completion_date as date) as completion_date,
+    ir.test_round,
+    ir.test_round_date,
+    ir.percentile,
+    ir.overall_scale_score,
+    ir.percent_progress_to_annual_typical_growth as pct_progress_typical,
+    ir.percent_progress_to_annual_stretch_growth as pct_progress_stretch
+from {{ ref("base_iready__diagnostic_results") }} as ir
+inner join
+    (
+        select
+            academic_year,
+            subject,
+            student_id,
+            max(completion_date) as max_completion_date
+        from {{ ref("base_iready__diagnostic_results") }}
+        group by academic_year, subject, student_id
+    ) as sub
+    on ir.academic_year = sub.academic_year
+    and ir.student_id = sub.student_id
+    and ir.subject = sub.subject
+    and ir.completion_date = sub.max_completion_date
+where cast(left(ir.academic_year, 4) as int) = {{ var("current_academic_year") }}
