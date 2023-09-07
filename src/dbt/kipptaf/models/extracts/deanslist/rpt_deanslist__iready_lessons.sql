23b7f2a01158aa3e02ce4a96cd40264f45c2223b
Anthony Walters <awalters@kippteamandfamily.org>
2023-09-07 23:13:00 +0000

    format

--- src/dbt/kipptaf/models/extracts/deanslist/rpt_deanslist__iready_lessons.sql
+++ src/dbt/kipptaf/models/extracts/deanslist/rpt_deanslist__iready_lessons.sql
@@ -1,52 +1,35 @@
-SELECT
+select
     pl.student_id,
     pl.subject,
-    t.name AS term,
-    CAST(
-        SUM(
-            CASE
-                WHEN pl.passed_or_not_passed = 'Passed' THEN 1
-                ELSE 0
-            END
-        ) AS FLOAT64
-    ) AS lessons_passed,
-    CAST(
-        COUNT(DISTINCT pl.lesson_id) AS FLOAT64
-    ) AS total_lessons,
-    ROUND(
-        CAST(
-            SUM(
-                CASE
-                    WHEN pl.passed_or_not_passed = 'Passed' THEN 1
-                    ELSE 0
-                END
-            ) AS FLOAT64
-        ) / CAST(COUNT(pl.lesson_id) AS FLOAT64),
-        2
-    ) * 100 AS pct_passed
-FROM
-    {{ ref('stg_iready__personalized_instruction_by_lesson') }} AS pl
-INNER JOIN
-    {{ ref('stg_people__location_crosswalk') }} AS sc
-    ON (pl.school = sc.name)
-INNER JOIN {{ ref('stg_reporting__terms') }} AS t
-    ON (
-        sc.powerschool_school_id = t.school_id
-        AND CAST(LEFT(pl.academic_year, 4) AS INT64) = t.academic_year
-        AND (
-            PARSE_DATE(
-                '%m/%d/%Y', pl.completion_date
-            ) BETWEEN t.start_date AND t.end_date
+    t.name as term,
+    cast(
+        sum(case when pl.passed_or_not_passed = 'Passed' then 1 else 0 end) as float64
+    ) as lessons_passed,
+    cast(count(distinct pl.lesson_id) as float64) as total_lessons,
+    round(
+        cast(
+            sum(
+                case when pl.passed_or_not_passed = 'Passed' then 1 else 0 end
+            ) as float64
         )
-        AND t.type = 'RT'
+        / cast(count(pl.lesson_id) as float64),
+        2
     )
-WHERE
-    PARSE_DATE('%m/%d/%Y', pl.completion_date) >= DATE(
-        {{ var("current_academic_year") }},
-        7,
-        1
+    * 100 as pct_passed
+from {{ ref("stg_iready__personalized_instruction_by_lesson") }} as pl
+inner join {{ ref("stg_people__location_crosswalk") }} as sc on (pl.school = sc.name)
+inner join
+    {{ ref("stg_reporting__terms") }} as t
+    on (
+        sc.powerschool_school_id = t.school_id
+        and cast(left(pl.academic_year, 4) as int64) = t.academic_year
+        and (
+            parse_date('%m/%d/%Y', pl.completion_date)
+            between t.start_date and t.end_date
+        )
+        and t.type = 'RT'
     )
-GROUP BY
-    pl.student_id,
-    pl.subject,
-    t.name
+where
+    parse_date('%m/%d/%Y', pl.completion_date)
+    >= date({{ var("current_academic_year") }}, 7, 1)
+group by pl.student_id, pl.subject, t.name
