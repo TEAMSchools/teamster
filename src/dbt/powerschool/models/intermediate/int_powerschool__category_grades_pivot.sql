87c0620a4fff315682f1faf3f08fb91b67295861
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-09-08 22:11:44 +0000

    grades views

---
+++ src/dbt/powerschool/models/intermediate/int_powerschool__category_grades_pivot.sql
@@ -0,0 +1,187 @@
+with
+    category_grades as (
+        select
+            cat.studentid,
+            cat.yearid,
+            cat.course_number,
+            cat.storecode_type,
+            cat.reporting_term,
+            cat.category_pct,
+            cat.citizenship,
+            if(
+                current_date('America/New_York')
+                between cat.termbin_start_date and cat.termbin_end_date,
+                true,
+                false
+            ) as is_current,
+
+            si.credittype,
+
+            max(cat.schoolid) over (
+                partition by
+                    cat.studentid, cat.yearid, cat.course_number, cat.reporting_term
+                order by cat.termbin_start_date desc
+            ) as schoolid,
+        from {{ ref('int_powerschool__category_grades') }} as cat
+        inner join
+            {{ ref('base_powerschool__sections') }} as si
+            on cat.sectionid = si.sectionid
+        where cat.yearid = ({{ var('current_academic_year') }} - 1990)
+    ),
+
+    with_cur as (
+        select
+            studentid,
+            yearid,
+            schoolid,
+            course_number,
+            reporting_term,
+            storecode_type,
+            category_pct,
+            citizenship,
+            is_current,
+            credittype,
+
+            reporting_term as rt,
+        from category_grades
+
+        union all
+
+        select
+            studentid,
+            yearid,
+            schoolid,
+            course_number,
+            reporting_term,
+            storecode_type,
+            category_pct,
+            citizenship,
+            is_current,
+            credittype,
+
+            'CUR' as rt,
+        from category_grades
+        where is_current
+    ),
+
+    {#
+        select
+            studentid,
+            schoolid,
+            yearid,
+
+            'ALL' as course_number,
+
+            reporting_term,
+            reporting_term as rt,
+            storecode_type,
+            round(avg(category_pct), 0) as category_pct,
+
+            null as citizenship,
+
+            if(
+                current_date('America/New_York')
+                between cat.termbin_start_date and cat.termbin_end_date,
+                true,
+                false
+            ) as is_curterm,
+
+            'ALL' as credittype
+        from {{ ref('int_powerschool__category_grades') }}
+        where yearid = ({{ var('current_academic_year') }} - 1990)
+        group by
+            studentid,
+            schoolid,
+            yearid,
+            reporting_term,
+            storecode_type,
+            termbin_start_date,
+            termbin_end_date 
+    #}
+    grades_pivot as (
+        select
+            studentid,
+            yearid,
+            schoolid
+            credittype,
+            course_number,
+            reporting_term,
+            is_current,
+            value,
+            concat(storecode_type, '_', rt) as pivot_field,
+        from
+            grades_unpivot pivot (
+                max(category_pct)
+                for pivot_field in (
+                    f_cur,
+                    f_rt1,
+                    f_rt2,
+                    f_rt3,
+                    f_rt4,
+                    s_cur,
+                    s_rt1,
+                    s_rt2,
+                    s_rt3,
+                    s_rt4,
+                    w_cur,
+                    w_rt1,
+                    w_rt2,
+                    w_rt3,
+                    w_rt4,
+                    e_cur,
+                    e_rt1,
+                    e_rt2,
+                    e_rt3,
+                    e_rt4,
+                    ctz_cur,
+                    ctz_rt1,
+                    ctz_rt2,
+                    ctz_rt3,
+                    ctz_rt4
+                )
+            )
+    )
+
+select
+    studentid,
+    yearid,
+    schoolid,
+    credittype,
+    course_number,
+    reporting_term,
+    is_current,
+
+    ctz_cur,
+    ctz_rt1,
+    ctz_rt2,
+    ctz_rt3,
+    ctz_rt4,
+
+    {% for cat in ["f", "s", "w", "e"] %}
+        {{ cat }}_cur,
+        max({{ cat }}_rt1) over (
+            partition by studentid, yearid, course_number order by reporting_term asc
+        ) as {{ cat }}_rt1,
+        max({{ cat }}_rt2) over (
+            partition by studentid, yearid, course_number order by reporting_term asc
+        ) as {{ cat }}_rt2,
+        max({{ cat }}_rt3) over (
+            partition by studentid, yearid, course_number order by reporting_term asc
+        ) as {{ cat }}_rt3,
+        max({{ cat }}_rt4) over (
+            partition by studentid, yearid, course_number order by reporting_term asc
+        ) as {{ cat }}_rt4,
+        round(
+            avg({{ cat }}_cur) over (
+                partition by studentid, yearid, course_number
+                order by reporting_term asc
+            ),
+            0
+        ) as {{ cat }}_y1,
+    {% endfor %}
+
+    row_number() over (
+        partition by studentid, yearid, reporting_term, credittype
+        order by course_number asc
+    ) as rn_credittype,
+from grades_repivot
