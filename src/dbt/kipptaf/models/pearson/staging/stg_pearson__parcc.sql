7e7f8d1c888ec48ff2a25dd56b28d0e5200980fa
Charlie Bini <cbini87@gmail.com>
2023-09-08 19:19:50 +0000

    pearson views

--- src/dbt/kipptaf/models/pearson/staging/stg_pearson__parcc.sql
+++ src/dbt/kipptaf/models/pearson/staging/stg_pearson__parcc.sql
@@ -10,5 +10,18 @@ with
         }}
     )
 
-select *, safe_cast(left(assessmentyear, 4) as int) as academic_year,
+select
+    * except (shipreportschoolcode, shipreportdistrictcode, statefield9),
+    safe_cast(left(assessmentyear, 4) as int) as academic_year,
+    coalesce(
+        shipreportschoolcode.long_value,
+        safe_cast(shipreportschoolcode.float_value as int)
+    ) as shipreportschoolcode,
+    coalesce(
+        shipreportdistrictcode.long_value,
+        safe_cast(shipreportdistrictcode.float_value as int)
+    ) as shipreportdistrictcode,
+    coalesce(
+        statefield9.string_value, safe_cast(statefield9.double_value as string)
+    ) as statefield9,
 from union_relations
