7bd110959bdf4b6dc3e613cb35d41eb6f95fe6a6
Charlie Bini <cbini87@gmail.com>
2023-09-08 19:58:46 +0000

    refactor

--- src/dbt/kipptaf/models/pearson/staging/stg_pearson__parcc.sql
+++ src/dbt/kipptaf/models/pearson/staging/stg_pearson__parcc.sql
@@ -1,27 +1,8 @@
-with
-    union_relations as (
-        {{
-            dbt_utils.union_relations(
-                relations=[
-                    source("kippnewark_pearson", model.name),
-                    source("kippcamden_pearson", model.name),
-                ]
-            )
-        }}
+{{
+    dbt_utils.union_relations(
+        relations=[
+            source("kippnewark_pearson", model.name),
+            source("kippcamden_pearson", model.name),
+        ]
     )
-
-select
-    * except (shipreportschoolcode, shipreportdistrictcode, statefield9),
-    safe_cast(left(assessmentyear, 4) as int) as academic_year,
-    coalesce(
-        shipreportschoolcode.long_value,
-        safe_cast(shipreportschoolcode.float_value as int)
-    ) as shipreportschoolcode,
-    coalesce(
-        shipreportdistrictcode.long_value,
-        safe_cast(shipreportdistrictcode.float_value as int)
-    ) as shipreportdistrictcode,
-    coalesce(
-        statefield9.string_value, safe_cast(statefield9.double_value as string)
-    ) as statefield9,
-from union_relations
+}}
