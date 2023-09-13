5f838a69192c17206ecd105c29f2a9f1a62327a9
Charlie Bini <cbini87@gmail.com>
2023-09-13 15:21:14 +0000

    rm sources

--- src/dbt/kipptaf/models/illuminate/base/base_illuminate__repository_data.sql
+++ src/dbt/kipptaf/models/illuminate/base/base_illuminate__repository_data.sql
@@ -3,9 +3,7 @@
 {% set relations = dbt_utils.get_relations_by_prefix(
     schema=model.schema,
     prefix="stg_illuminate__repository_",
-    exclude=[
-        "stg_illuminate__repository_grade_levels", "stg_illuminate__repository_fields"
-    ],
+    exclude="ANY ('stg_illuminate__repository_grade_levels', 'stg_illuminate__repository_fields')",
 ) %}
 
 {{ dbt_utils.union_relations(relations=relations) }}
