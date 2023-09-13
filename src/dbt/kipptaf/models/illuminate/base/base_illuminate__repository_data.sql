6025bd60571be51525275c4c8af47094397d00c4
Charlie Bini <cbini87@gmail.com>
2023-09-13 15:24:29 +0000

    fix pattern

--- src/dbt/kipptaf/models/illuminate/base/base_illuminate__repository_data.sql
+++ src/dbt/kipptaf/models/illuminate/base/base_illuminate__repository_data.sql
@@ -3,7 +3,7 @@
 {% set relations = dbt_utils.get_relations_by_prefix(
     schema=model.schema,
     prefix="stg_illuminate__repository_",
-    exclude="ANY ('stg_illuminate__repository_grade_levels', 'stg_illuminate__repository_fields')",
+    exclude="stg_illuminate__repository_%s",
 ) %}
 
 {{ dbt_utils.union_relations(relations=relations) }}
