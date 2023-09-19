e273c8a3a61ae979037131a0aeffd2048b24a23e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-09-19 17:36:39 +0000

    temp rm cpn extract

--- src/teamster/kippcamden/datagun/schedules.py
+++ src/teamster/kippcamden/datagun/schedules.py
@@ -1,7 +1,7 @@
 from dagster import ScheduleDefinition
 
 from .. import LOCAL_TIMEZONE
-from .jobs import cpn_extract_asset_job, powerschool_extract_asset_job
+from .jobs import powerschool_extract_asset_job  # cpn_extract_asset_job
 
 powerschool_extract_assets_schedule = ScheduleDefinition(
     job=powerschool_extract_asset_job,
@@ -9,13 +9,13 @@ powerschool_extract_assets_schedule = ScheduleDefinition(
     execution_timezone=LOCAL_TIMEZONE.name,
 )
 
-cpn_extract_assets_schedule = ScheduleDefinition(
-    job=cpn_extract_asset_job,
-    cron_schedule="0 0 * * *",
-    execution_timezone=LOCAL_TIMEZONE.name,
-)
+# cpn_extract_assets_schedule = ScheduleDefinition(
+#     job=cpn_extract_asset_job,
+#     cron_schedule="0 0 * * *",
+#     execution_timezone=LOCAL_TIMEZONE.name,
+# )
 
 __all__ = [
-    cpn_extract_assets_schedule,
+    # cpn_extract_assets_schedule,
     powerschool_extract_assets_schedule,
 ]
