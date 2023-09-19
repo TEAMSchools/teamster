a65c6da137da03b4b2689d9a98fa5234220d96b7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-09-19 21:21:20 +0000

    dayforce test

---
+++ tests/dayforce/test_dayforce_schema.py
@@ -0,0 +1,48 @@
+# trunk-ignore-all(ruff/E501)
+
+import random
+
+from fastavro import parse_schema, validation, writer
+from numpy import nan
+from pandas import read_csv
+from slugify import slugify
+
+from teamster.core.dayforce.schema import ASSET_FIELDS
+from teamster.core.utils.functions import get_avro_record_schema
+
+
+def _test_schema(local_filepath, asset_name):
+    df = read_csv(filepath_or_buffer=local_filepath, low_memory=False)
+    df.replace({nan: None}, inplace=True)
+    df.rename(columns=lambda x: slugify(text=x, separator="_"), inplace=True)
+    print(df)
+
+    print(df.dtypes.to_dict())
+
+    count = df.shape[0]
+    records = df.to_dict(orient="records")
+
+    schema = get_avro_record_schema(name=asset_name, fields=ASSET_FIELDS[asset_name])
+    # print(schema)
+
+    parsed_schema = parse_schema(schema)
+
+    sample_record = records[random.randint(a=0, b=(count - 1))]
+    # sample_record = [r for r in records if "" in json.dumps(obj=r)]
+
+    assert validation.validate(datum=sample_record, schema=parsed_schema, strict=True)
+
+    assert validation.validate_many(records=records, schema=parsed_schema, strict=True)
+
+    with open(file="/dev/null", mode="wb") as fo:
+        writer(
+            fo=fo,
+            schema=parsed_schema,
+            records=records,
+            codec="snappy",
+            strict_allow_default=True,
+        )
+
+
+def test_employees():
+    _test_schema(asset_name="employees", local_filepath="env/dayforce/employees.csv")
