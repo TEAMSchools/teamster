ae2e7b66f3608aa9bfd5db3370d2b571659675e7
Charlie Bini <cbini87@gmail.com>
2023-09-20 19:39:11 +0000

    manager and status models

--- src/dbt/kipptaf/models/dayforce/staging/stg_dayforce__employee_status.sql
+++ src/dbt/kipptaf/models/dayforce/staging/stg_dayforce__employee_status.sql
@@ -1,1 +1,38 @@
-select * from {{ source("dayforce", "src_dayforce__employee_status") }}
+with
+    parsed_dates as (
+        select
+            number,
+            status,
+            status_reason_description,
+            base_salary,
+            extract(
+                date
+                from
+                    parse_timestamp(
+                        '%Y-%m-%dT%H:%M:%S%Ez', effective_start
+                    ) at time zone 'America/New_York'
+            ) as effective_start,
+            coalesce(
+                extract(
+                    date
+                    from
+                        parse_timestamp(
+                            '%Y-%m-%dT%H:%M:%S%Ez', effective_end
+                        ) at time zone 'America/New_York'
+                ),
+                '2020-12-31'
+            ) as effective_end,
+        from {{ source("dayforce", "src_dayforce__employee_status") }}
+    ),
+
+    start_order as (
+        select
+            *,
+            row_number() over (
+                partition by number, effective_start order by effective_end asc
+            ) as rn_employee_start,
+        from parsed_dates
+        where effective_start <= '2020-12-31'
+    )
+
+
