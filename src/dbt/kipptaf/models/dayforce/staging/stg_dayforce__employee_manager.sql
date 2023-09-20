0927a5985b4be6b9466fb998bef21485c545b9b5
Charlie Bini <cbini87@gmail.com>
2023-09-20 16:43:49 +0000

    staging models

--- src/dbt/kipptaf/models/dayforce/staging/stg_dayforce__employee_manager.sql
+++ src/dbt/kipptaf/models/dayforce/staging/stg_dayforce__employee_manager.sql
@@ -1,1 +1,16 @@
-select * from {{ source("dayforce", "src_dayforce__employee_manager") }}
+with
+    employee_manager as (
+        select
+            employee_reference_code,
+            manager_employee_number,
+            parse_date('%m/%d/%Y', manager_effective_start) as manager_effective_start,
+            coalesce(
+                parse_date('%m/%d/%Y', manager_effective_end), '2020-12-31'
+            ) as manager_effective_end,
+        from {{ source("dayforce", "src_dayforce__employee_manager") }}
+        where manager_derived_method = 'Direct Report'
+    )
+
+select *
+from employee_manager
+where manager_effective_start < manager_effective_end
