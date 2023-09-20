e931ea333923d4942e52e172e7deb21db612b84b
Charlie Bini <cbini87@gmail.com>
2023-09-20 19:39:11 +0000

    manager and status models

--- src/dbt/kipptaf/models/dayforce/staging/stg_dayforce__employee_manager.sql
+++ src/dbt/kipptaf/models/dayforce/staging/stg_dayforce__employee_manager.sql
@@ -1,5 +1,5 @@
 with
-    employee_manager as (
+    parsed_dates as (
         select
             employee_reference_code,
             manager_employee_number,
@@ -9,8 +9,32 @@ with
             ) as manager_effective_end,
         from {{ source("dayforce", "src_dayforce__employee_manager") }}
         where manager_derived_method = 'Direct Report'
+    ),
+
+    start_order as (
+        select
+            *,
+            row_number() over (
+                partition by employee_reference_code, manager_effective_start
+                order by manager_effective_end asc
+            ) as rn_employee_start,
+        from parsed_dates
+        where manager_effective_start <= '2020-12-31'
     )
 
-select *
-from employee_manager
-where manager_effective_start < manager_effective_end
+select
+    employee_reference_code,
+    manager_employee_number,
+    manager_effective_end,
+    coalesce(
+        date_add(
+            lag(manager_effective_end, 1) over (
+                partition by employee_reference_code, manager_effective_start
+                order by manager_effective_end asc
+            ),
+            interval 1 day
+        ),
+        manager_effective_start
+    ) as manager_effective_start,
+from start_order
+where manager_effective_end > manager_effective_start
