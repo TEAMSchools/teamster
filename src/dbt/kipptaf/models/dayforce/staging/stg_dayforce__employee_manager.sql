0f0f90803c0994a1d623cd5b283e3aa7828d1088
Charlie Bini <cbini87@gmail.com>
2023-09-21 22:51:12 +0000

    rework deduplicate, emp hist

--- src/dbt/kipptaf/models/dayforce/staging/stg_dayforce__employee_manager.sql
+++ src/dbt/kipptaf/models/dayforce/staging/stg_dayforce__employee_manager.sql
@@ -9,23 +9,34 @@ with
             ) as manager_effective_end,
         from {{ source("dayforce", "src_dayforce__employee_manager") }}
         where manager_derived_method = 'Direct Report'
+    ),
+
+    deduplicate as (
+        {{
+            dbt_utils.deduplicate(
+                relation="parsed_dates",
+                partition_by="employee_reference_code, manager_effective_start",
+                order_by="manager_effective_end desc",
+            )
+        }}
+    ),
+
+    with_next as (
+        select
+            *,
+            lead(manager_effective_start, 1) over (
+                partition by employee_reference_code
+                order by manager_effective_start asc
+            ) as manager_effective_start_next,
+        from deduplicate
     )
 
 select
     employee_reference_code as employee_number,
     manager_employee_number,
-    manager_effective_end,
+    manager_effective_start as manager_effective_start_date,
     coalesce(
-        date_add(
-            lag(manager_effective_end, 1) over (
-                partition by employee_reference_code, manager_effective_start
-                order by manager_effective_end asc
-            ),
-            interval 1 day
-        ),
-        manager_effective_start
-    ) as manager_effective_start,
-from parsed_dates
-where
-    manager_effective_end > manager_effective_start
-    and manager_effective_start <= '2020-12-31'
+        date_sub(manager_effective_start_next, interval 1 day), '2020-12-31'
+    ) as manager_effective_end_date,
+from with_next
+where manager_effective_start <= '2020-12-31'
