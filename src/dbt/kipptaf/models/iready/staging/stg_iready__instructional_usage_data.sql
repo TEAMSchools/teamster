2b93b1b653f5df7baa68a813ce515bce5f087c89
Charlie Bini <cbini87@gmail.com>
2023-10-06 17:34:34 +0000

    fix snapshot

--- src/dbt/kipptaf/models/iready/staging/stg_iready__instructional_usage_data.sql
+++ src/dbt/kipptaf/models/iready/staging/stg_iready__instructional_usage_data.sql
@@ -1,8 +1,20 @@
-{{
-    dbt_utils.union_relations(
-        relations=[
-            source("kippnj_iready", model.name),
-            source("kippmiami_iready", model.name),
-        ]
+with
+    union_relations as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    source("kippnj_iready", model.name),
+                    source("kippmiami_iready", model.name),
+                ]
+            )
+        }}
     )
-}}
+
+select
+    *,
+    {{
+        dbt_utils.generate_surrogate_key(
+            ["student_id", "subject", "last_week_start_date", "_dbt_source_relation"]
+        )
+    }} as surrogate_key,
+from union_relations
