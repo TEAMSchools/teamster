336c2d9765c730237ff08022c73037788fb888f5
Charlie Bini <cbini87@gmail.com>
2023-10-06 16:06:26 +0000

    tighten up date cte

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__iready_apm.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__iready_apm.sql
@@ -1,12 +1,12 @@
 with
     date_range as (
-        select
-            {{
-                teamster_utils.date_to_fiscal_year(
-                    date_field="date_day", start_month=7, year_source="start"
-                )
-            }} as academic_year, cast(date_day as date) as date,
+        select safe_cast(date_day as date) as date_day
         from {{ ref("utils__date_spine") }}
+        where
+            date_day
+            between date({{ var("current_academic_year") }}, 7, 1) and current_date(
+                '{{ var("local_timezone") }}'
+            )
     ),
 
     expanded_terms as (
@@ -52,7 +52,7 @@ select
 
     subj as subject,
 
-    w.date,
+    w.date_day as `date`,
 
     hr.sections_section_number as team,
 
@@ -75,52 +75,50 @@ select
     if(sp.specprog_name is not null, true, false) as is_tutoring,
 from {{ ref("base_powerschool__student_enrollments") }} as co
 cross join unnest(["Reading", "Math"]) as subj
-inner join
-    date_range as w
-    on co.academic_year = w.academic_year
-    and w.date <= current_date({{ var("local_timezone") }})
+cross join date_range as w
 left join
     expanded_terms as rt
-    on rt.academic_year = co.academic_year
-    and w.date between rt.start_date and rt.end_date
+    on co.academic_year = rt.academic_year
     and co.region = rt.region
+    and w.date_day between rt.start_date and rt.end_date
 left join
     {{ ref("stg_iready__personalized_instruction_by_lesson") }} as il
-    on il.student_id = co.student_number
-    and il.academic_year_int = co.academic_year
+    on co.student_number = il.student_id
+    and co.academic_year = il.academic_year_int
     and subj = il.subject
-    and il.completion_date = w.date
+    and w.date_day = il.completion_date
 left join
     {{ ref("base_iready__diagnostic_results") }} as dr
     on co.student_number = dr.student_id
     and co.academic_year = dr.academic_year_int
     and subj = dr.subject
-    and dr.test_round = rt.name
+    and rt.name = dr.test_round
     and dr.rn_subj_round = 1
 left join
     {{ ref("stg_iready__instructional_usage_data") }} as iu
     on co.student_number = iu.student_id
     and co.academic_year = iu.academic_year_int
     and subj = iu.subject
-    and w.date = iu.last_week_start_date
+    and w.date_day = iu.last_week_start_date
 left join
     {{ ref("base_powerschool__course_enrollments") }} as hr
     on co.student_number = hr.students_student_number
     and co.yearid = hr.cc_yearid
     and co.schoolid = hr.cc_schoolid
     and {{ union_dataset_join_clause(left_alias="co", right_alias="hr") }}
-    and hr.cc_course_number = 'HR'
     and not hr.is_dropped_section
+    and hr.cc_course_number = 'HR'
     and hr.rn_course_number_year = 1
 left join
     {{ ref("int_powerschool__spenrollments") }} as sp
     on co.studentid = sp.studentid
-    and {{ union_dataset_join_clause(left_alias="co", right_alias="sp") }}
     and co.academic_year = sp.academic_year
-    and current_date({{ var("local_timezone") }}) between sp.enter_date and sp.exit_date
+    and {{ union_dataset_join_clause(left_alias="co", right_alias="sp") }}
+    and current_date('{{ var("local_timezone") }}')
+    between sp.enter_date and sp.exit_date
     and sp.specprog_name = 'Tutoring'
 where
     co.academic_year = {{ var("current_academic_year") }}
     and co.rn_year = 1
     and co.enroll_status = 0
-    and co.grade_level < 9
+    and co.grade_level <= 8
