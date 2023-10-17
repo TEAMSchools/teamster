3875090f0eb8bf9807077bd6baf54474b0f46150
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-10-17 21:14:41 +0000

    replace definitions

--- src/teamster/staging/definitions.py
+++ src/teamster/staging/definitions.py
@@ -1,42 +1,48 @@
-from dagster import (
-    AutoMaterializePolicy,
-    Definitions,
-    EnvVar,
-    config_from_files,
-    load_assets_from_modules,
-)
+from dagster import AutoMaterializePolicy, Definitions, EnvVar, load_assets_from_modules
 from dagster_dbt import DbtCliResource
-from dagster_gcp import GCSResource
+from dagster_gcp import GCSPickleIOManager, GCSResource
 
-from teamster.core.google.io.resources import gcs_io_manager
+from teamster.core.sqlalchemy.resources import OracleResource, SqlAlchemyEngineResource
 from teamster.core.ssh.resources import SSHConfigurableResource
-from teamster.staging import CODE_LOCATION, GCS_PROJECT_NAME, dbt, fldoe, renlearn
-
-resource_config_dir = f"src/teamster/{CODE_LOCATION}/config/resources"
+from teamster.staging import CODE_LOCATION, GCS_PROJECT_NAME, dbt, powerschool
 
 defs = Definitions(
     assets=[
         *load_assets_from_modules(
             modules=[dbt], auto_materialize_policy=AutoMaterializePolicy.eager()
         ),
-        *load_assets_from_modules(modules=[fldoe]),
-        *load_assets_from_modules(modules=[renlearn]),
+        *load_assets_from_modules(modules=[powerschool]),
     ],
     resources={
-        "gcs": GCSResource(project=GCS_PROJECT_NAME),
-        "io_manager_gcs_avro": gcs_io_manager.configured(
-            config_from_files([f"{resource_config_dir}/io_avro.yaml"])
+        "io_manager_gcs_avro": GCSPickleIOManager(
+            gcs=GCSResource(project=GCS_PROJECT_NAME),
+            gcs_bucket=f"teamster-{CODE_LOCATION}",
+        ),
+        "io_manager_gcs_file": GCSPickleIOManager(
+            gcs=GCSResource(project=GCS_PROJECT_NAME),
+            gcs_bucket=f"teamster-{CODE_LOCATION}",
         ),
         "dbt_cli": DbtCliResource(project_dir=f"src/dbt/{CODE_LOCATION}"),
-        "ssh_couchdrop": SSHConfigurableResource(
-            remote_host="kipptaf.couchdrop.io",
-            username=EnvVar("COUCHDROP_SFTP_USERNAME"),
-            password=EnvVar("COUCHDROP_SFTP_PASSWORD"),
+        "db_powerschool": OracleResource(
+            engine=SqlAlchemyEngineResource(
+                dialect="oracle",
+                driver="oracledb",
+                username="PSNAVIGATOR",
+                host="localhost",
+                database="PSPRODDB",
+                port=1521,
+                password=EnvVar("KIPPCAMDEN_PS_DB_PASSWORD"),
+            ),
+            version="19.0.0.0.0",
+            prefetchrows=100000,
+            arraysize=100000,
         ),
-        "ssh_renlearn": SSHConfigurableResource(
-            remote_host="sftp.renaissance.com",
-            username=EnvVar("KIPPMIAMI_RENLEARN_SFTP_USERNAME"),
-            password=EnvVar("KIPPMIAMI_RENLEARN_SFTP_PASSWORD"),
+        "ssh_powerschool": SSHConfigurableResource(
+            remote_host="pskcna.kippnj.org",
+            remote_port=EnvVar("KIPPCAMDEN_PS_SSH_PORT"),
+            username=EnvVar("KIPPCAMDEN_PS_SSH_USERNAME"),
+            password=EnvVar("KIPPCAMDEN_PS_SSH_PASSWORD"),
+            tunnel_remote_host=EnvVar("KIPPCAMDEN_PS_SSH_REMOTE_BIND_HOST"),
         ),
     },
 )
