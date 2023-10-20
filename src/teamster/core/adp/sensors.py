1e7dd50212060c1bf5d34eaf142b705625caeb5f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-10-20 14:45:29 +0000

    asdf

--- src/teamster/core/adp/sensors.py
+++ src/teamster/core/adp/sensors.py
@@ -32,6 +32,7 @@ def build_sftp_sensor(
         context: SensorEvaluationContext, ssh_adp_workforce_now: SSHConfigurableResource
     ):
         cursor: dict = json.loads(context.cursor or "{}")
+        now = pendulum.now(tz=timezone)
 
         try:
             ls = get_sftp_ls(ssh=ssh_adp_workforce_now, asset_defs=asset_defs)
@@ -73,7 +74,7 @@ def build_sftp_sensor(
                         )
                     )
 
-            cursor[asset_identifier] = pendulum.now(tz=timezone).timestamp()
+            cursor[asset_identifier] = now.timestamp()
 
         return SensorResult(run_requests=run_requests, cursor=json.dumps(obj=cursor))
 
