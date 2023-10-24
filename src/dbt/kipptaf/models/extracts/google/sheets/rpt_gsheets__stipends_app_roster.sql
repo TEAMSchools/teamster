812b2d34c1519719ab0889809cc976ea93b9c945
Cristina Baldor <cbaldor@kippteamandfamily.org>
2023-10-24 18:08:23 +0000

    column order

--- src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__stipends_app_roster.sql
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__stipends_app_roster.sql
@@ -9,14 +9,15 @@ with
             google_email as first_approver_google,
             report_to_preferred_name_lastfirst as second_approver_name,
             report_to_mail as second_approver_email,
+            employee_number as first_approver_employee_number,
+            report_to_employee_number as second_approver_employee_number,
             case
                 when business_unit_home_name not like '%Miami%'
                 then concat(report_to_sam_account_name, '@apps.teamschools.org')
                 when business_unit_home_name like '%Miami%'
                 then concat(report_to_sam_account_name, '@kippmiami.org')
             end as second_approver_google,
-            employee_number as first_approver_employee_number,
-            report_to_employee_number as second_approver_employee_number,
+
             case
                 when department_home_name = 'Operations'
                 then 'Operations'
