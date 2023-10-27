d1f60433ce79c3a32adac503fbeb0d4a4e7d6775
Charlie Bini <cbini87@gmail.com>
2023-10-27 16:19:52 +0000

    remove extra parentheses, use regex, remove OR join

--- src/dbt/kipptaf/models/reporting/intermediate/int_reporting__student_filters.sql
+++ src/dbt/kipptaf/models/reporting/intermediate/int_reporting__student_filters.sql
@@ -2,31 +2,20 @@ with
     subjects as (
         select
             subject as iready_subject,
-            (
-                case
-                    when subject = 'Reading'
-                    then 'Text Study'
-                    when subject = 'Math'
-                    then 'Mathematics'
-                end
-            ) as illuminate_subject_area,
-            (
-                case
-                    when subject = 'Reading'
-                    then 'ENG'
-                    when subject = 'Math'
-                    then 'MATH'
-                end
-            ) as powerschool_credittype,
+            case
+                subject when 'Reading' then 'Text Study' when 'Math' then 'Mathematics'
+            end as illuminate_subject_area,
+            case
+                subject when 'Reading' then 'ENG' when 'Math' then 'MATH'
+            end as powerschool_credittype,
         from unnest(['Reading', 'Math']) as subject
     ),
 
     intervention_nj as (
         select
             st.local_student_id as student_number,
-            right(
-                left(g.group_name, length(g.group_name) - 8), length(g.group_name) - 19
-            ) as subject,
+
+            regexp_extract(g.group_name, r'Bucket 2 - (\w+) - Gr\w-\w') as subject,
         from {{ source("illuminate", "group_student_aff") }} as s
         inner join
             {{ source("illuminate", "groups") }} as g
@@ -34,33 +23,31 @@ with
             and g.group_name like 'Bucket 2%'
         left join
             {{ ref("stg_illuminate__students") }} as st on s.student_id = st.student_id
-        where s.end_date is null or s.end_date < current_date('America/New_York')
+        where
+            s.end_date is null
+            or s.end_date < current_date('{{ var("local_timezone") }}')
     ),
 
     prev_yr_state_test as (
         select
             localstudentidentifier,
-            statestudentidentifier,
+            safe_cast(statestudentidentifier as string) as statestudentidentifier,
             academic_year + 1 as academic_year_plus,
-            (
-                case
-                    when subject like 'English Language Arts%'
-                    then 'Text Study'
-                    when subject in ('Algebra I', 'Algebra II', 'Geometry')
-                    then 'Mathematics'
-                    else subject
-                end
-            ) as subject,
-            (
-                case
-                    when testperformancelevel < 3
-                    then 'Below/Far Below'
-                    when testperformancelevel = 3
-                    then 'Approaching'
-                    when testperformancelevel > 3
-                    then 'At/Above'
-                end
-            ) as njsla_proficiency,
+            case
+                when subject like 'English Language Arts%'
+                then 'Text Study'
+                when subject in ('Algebra I', 'Algebra II', 'Geometry')
+                then 'Mathematics'
+                else subject
+            end as subject,
+            case
+                when testperformancelevel < 3
+                then 'Below/Far Below'
+                when testperformancelevel = 3
+                then 'Approaching'
+                when testperformancelevel > 3
+                then 'At/Above'
+            end as njsla_proficiency,
         from {{ ref("stg_pearson__njsla") }}
     ),
 
@@ -68,21 +55,24 @@ with
         select _dbt_source_relation, studentid, academic_year, 'Math' as iready_subject,
         from {{ ref("int_powerschool__spenrollments") }}
         where
-            (current_date('America/New_York') between enter_date and exit_date)
-            and specprog_name = 'Tutoring'
+            specprog_name = 'Tutoring'
+            and current_date('America/New_York') between enter_date and exit_date
     )
 
 select
     co._dbt_source_relation,
     co.student_number,
     co.studentid,
+
     sj.iready_subject,
     sj.illuminate_subject_area,
     sj.powerschool_credittype,
-    if(nj.subject is not null, true, false) as bucket_two,
-    coalesce(py.njsla_proficiency, 'No Test') as state_test_proficiency,
-    if(t.iready_subject is not null, true, false) as tutoring_nj,
 
+    if(nj.subject is not null, true, false) as bucket_two,
+
+    coalesce(py.njsla_proficiency, 'No Test') as state_test_proficiency,
+
+    if(t.iready_subject is not null, true, false) as tutoring_nj,
 from {{ ref("base_powerschool__student_enrollments") }} as co
 cross join subjects as sj
 left join
@@ -91,10 +81,8 @@ left join
     and sj.iready_subject = nj.subject
 left join
     prev_yr_state_test as py
-    on (
-        co.student_number = py.localstudentidentifier
-        or cast(co.state_studentnumber as int64) = py.statestudentidentifier
-    )
+    {# TODO: find records that only match on SID #}
+    on co.student_number = py.localstudentidentifier
     and co.academic_year = py.academic_year_plus
     and sj.illuminate_subject_area = py.subject
 left join
