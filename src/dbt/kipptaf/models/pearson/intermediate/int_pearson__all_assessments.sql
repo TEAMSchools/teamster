a75cfacf8aafaf71de9b9d36ecf7fe12f57dd5c8
Gaby Rangel <grangel@kippteamandfamily.org>
2023-10-31 11:29:09 +0000

    who knows?

--- src/dbt/kipptaf/models/pearson/intermediate/int_pearson__all_assessments.sql
+++ src/dbt/kipptaf/models/pearson/intermediate/int_pearson__all_assessments.sql
@@ -39,7 +39,8 @@ select  -- noqa: AM04
         then 'Did Not Yet Meet Expectations'
     end as testperformancelevel_text,
     case
-        when subject = 'Science' and testperformancelevel >= 3 then true
+        when subject = 'Science' and testperformancelevel >= 3
+        then true
         when testperformancelevel >= 4
         then true
         when testperformancelevel < 4
