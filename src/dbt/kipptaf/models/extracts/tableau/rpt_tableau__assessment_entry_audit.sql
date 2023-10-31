dc2701942fed3278059bfc1ec20a98f57b4c6330
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-10-31 19:08:36 +0000

    fix join

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__assessment_entry_audit.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__assessment_entry_audit.sql
@@ -9,8 +9,6 @@ select
     s.module_type,
     s.module_number,
 
-    rta.name as term,
-
     co.lastfirst,
     co.enroll_status,
     co.region,
@@ -18,15 +16,12 @@ select
     co.grade_level,
     co.advisory_name as team,
 
+    rta.name as term,
+
     asr.percent_correct,
 
     null as is_replacement,
 from {{ ref("int_assessments__scaffold") }} as s
-left join
-    {{ ref("stg_reporting__terms") }} as rta
-    on s.administered_at between rta.start_date and rta.end_date
-    and s.powerschool_school_id = rta.school_id
-    and rta.type = 'RT'
 inner join
     {{ ref("base_powerschool__student_enrollments") }} as co
     on s.powerschool_student_number = co.student_number
@@ -34,7 +29,12 @@ inner join
     and co.rn_year = 1
     and co.enroll_status = 0
 left join
+    {{ ref("stg_reporting__terms") }} as rta
+    on s.administered_at between rta.start_date and rta.end_date
+    and s.powerschool_school_id = rta.school_id
+    and rta.type = 'RT'
+left join
     {{ ref("int_illuminate__agg_student_responses") }} as asr
     on s.student_assessment_id = asr.student_assessment_id
-where
-    s.is_internal_assessment and not s.is_replacement and asr.response_type = 'overall'
+    and asr.response_type = 'overall'
+where s.is_internal_assessment and not s.is_replacement
