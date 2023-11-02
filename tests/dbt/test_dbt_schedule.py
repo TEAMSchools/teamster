86395f83258774a857c75eadf9445b77c4334ffb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-11-02 22:18:57 +0000

    add dbt sched

--- tests/dbt/test_dbt_schedule.py
+++ tests/dbt/test_dbt_schedule.py
@@ -1,14 +1,37 @@
 from dagster import build_schedule_context
 
 from teamster.core.utils.functions import get_dagster_cloud_instance
-from teamster.kipptaf.dbt.schedules import dbt_code_version_schedule
 
 
-def test_schedule():
+def _test_dbt_code_version_schedule(schedule):
     context = build_schedule_context(
         instance=get_dagster_cloud_instance("/workspaces/teamster/.dagster/home")
     )
 
-    output = dbt_code_version_schedule(context=context)
+    output = schedule(context=context)
 
     context.log.info(output)
+
+
+def test_kipptaf():
+    from teamster.kipptaf.dbt.schedules import dbt_code_version_schedule
+
+    _test_dbt_code_version_schedule(schedule=dbt_code_version_schedule)
+
+
+def test_kippnewark():
+    from teamster.kippnewark.dbt.schedules import dbt_code_version_schedule
+
+    _test_dbt_code_version_schedule(schedule=dbt_code_version_schedule)
+
+
+def test_kippcamden():
+    from teamster.kippcamden.dbt.schedules import dbt_code_version_schedule
+
+    _test_dbt_code_version_schedule(schedule=dbt_code_version_schedule)
+
+
+def test_kippmiami():
+    from teamster.kippmiami.dbt.schedules import dbt_code_version_schedule
+
+    _test_dbt_code_version_schedule(schedule=dbt_code_version_schedule)
