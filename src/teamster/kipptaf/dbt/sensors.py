ccbba6f60fa5c746f746306e832e27c1880a19e7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-11-02 21:35:21 +0000

    code version sensor

---
+++ src/teamster/kipptaf/dbt/sensors.py
@@ -0,0 +1,32 @@
+from dagster import (
+    AssetSelection,
+    RunRequest,
+    SensorEvaluationContext,
+    SensorResult,
+    sensor,
+)
+
+from .assets import _dbt_assets
+
+_dbt_assets.code_versions_by_key
+
+
+@sensor(asset_selection=AssetSelection.assets(_dbt_assets))
+def dbt_code_version_sensor(context: SensorEvaluationContext):
+    latest_code_versions = context.instance.get_latest_materialization_code_versions(
+        asset_keys=list(_dbt_assets.code_versions_by_key.keys())
+    )
+
+    asset_selection = []
+    for asset_key, current_code_version in _dbt_assets.code_versions_by_key.items():
+        latest_code_version = latest_code_versions.get(asset_key)
+
+        if current_code_version != latest_code_version:
+            asset_selection.append(asset_key)
+
+    return SensorResult(run_requests=[RunRequest(asset_selection=asset_selection)])
+
+
+__all__ = [
+    dbt_code_version_sensor,
+]
