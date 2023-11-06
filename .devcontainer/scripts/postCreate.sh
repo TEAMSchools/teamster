511fe3712fdba41d12f136bf6c4b23fb323d500c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-11-06 17:31:42 +0000

    dev updates

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -3,9 +3,7 @@
 # update/install apt packages
 sudo apt-get -y --no-install-recommends update &&
   sudo apt-get -y --no-install-recommends upgrade &&
-  sudo apt-get -y --no-install-recommends install \
-    bash-completion \
-    google-cloud-sdk-gke-gcloud-auth-plugin &&
+  sudo apt-get -y --no-install-recommends install bash-completion &&
   sudo apt-get -y autoremove &&
   sudo apt-get -y clean
 
