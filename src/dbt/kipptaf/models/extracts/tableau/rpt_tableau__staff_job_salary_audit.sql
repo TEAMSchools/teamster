1753eb8dffe64ca5bad016464426197d3e4c563a
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2023-11-07 19:57:44 +0000

    fixing trunk errors

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_job_salary_audit.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_job_salary_audit.sql
@@ -8,6 +8,15 @@ select
     h.assignment_status_reason as job_change_reason,
     h.base_remuneration_annual_rate_amount_amount_value as annual_salary,
     h.assignment_status_reason as compensation_change_reason,
+    h.work_assignment__fivetran_active,
+
+    r.preferred_name_given_name as preferred_first_name,
+    r.preferred_name_family_name as preferred_last_name,
+    r.report_to_preferred_name_lastfirst as manager_name,
+    r.business_unit_home_name as legal_entity_name,
+    r.home_work_location_name as primary_site,
+    r.assignment_status as current_status,
+
     lag(h.department_home_name, 1) over (
         partition by h.employee_number order by h.work_assignment__fivetran_start asc
     ) as prev_home_department,
@@ -17,7 +26,6 @@ select
     lag(h.base_remuneration_annual_rate_amount_amount_value, 1) over (
         partition by h.employee_number order by h.work_assignment__fivetran_start asc
     ) as prev_annual_salary,
-    h.work_assignment__fivetran_active,
     /* dedupe positions */
     row_number() over (
         partition by h.employee_number
@@ -27,15 +35,9 @@ select
             case when h.assignment_status = 'Terminated' then 0 else 1 end desc,
             h.work_assignment__fivetran_start desc
     ) as rn_position,
-    r.preferred_name_given_name as preferred_first_name,
-    r.preferred_name_family_name as preferred_last_name,
-    r.report_to_preferred_name_lastfirst as manager_name,
-    r.business_unit_home_name as legal_entity_name,
-    r.home_work_location_name as primary_site,
-    r.assignment_status as current_status,
     row_number() over (
         partition by h.employee_number order by h.work_assignment__fivetran_start desc
-    ) as rn_curr
+    ) as rn_curr,
 from {{ ref("base_people__staff_roster_history") }} as h
 inner join
     {{ ref("base_people__staff_roster") }} as r
