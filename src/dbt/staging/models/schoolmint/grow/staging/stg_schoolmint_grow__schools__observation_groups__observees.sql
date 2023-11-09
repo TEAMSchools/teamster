b1ce8391e3a88be3ef1f9dad91ccb4bde69e47d7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-11-09 21:03:38 +0000

    rm staging dbt models

--- src/dbt/staging/models/schoolmint/grow/staging/stg_schoolmint_grow__schools__observation_groups__observees.sql
+++
@@ -1,8 +0,0 @@
-select
-    og.observation_group_id,
-
-    ogo._id as observee_id,
-    ogo.name as observee_name,
-    ogo.email as observee_email,
-from {{ ref("stg_schoolmint_grow__schools__observation_groups") }} as og
-cross join unnest(og.observation_group_observees) as ogo
