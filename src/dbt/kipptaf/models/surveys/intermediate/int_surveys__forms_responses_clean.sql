07d6b949a4ba9c96a4e01a1fdb9e228247ff5e79
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2023-11-14 21:09:33 +0000

    part of first of support survey view

--- src/dbt/kipptaf/models/surveys/intermediate/int_surveys__forms_responses_clean.sql
+++
@@ -1,5 +0,0 @@
-SELECT * --clean this up
-FROM {{ ref("base_google_forms__form_responses") }} as fr
-INNER JOIN {{ ref("src_google_forms__form_items_extension")}} as fi
-ON fr.form_id = fi.form_id
-AND fr.item_id = fi.question_id
