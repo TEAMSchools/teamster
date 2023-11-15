18f87b61344787cbea5a9650b6d3d9486e9430fd
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-11-15 01:45:10 +0000

    schedule new fivetran jobs

--- src/teamster/kipptaf/fivetran/schedules.py
+++ src/teamster/kipptaf/fivetran/schedules.py
@@ -3,7 +3,9 @@ from dagster import ScheduleDefinition
 from teamster.kipptaf import LOCAL_TIMEZONE
 from teamster.kipptaf.fivetran.jobs import (
     fivetran_start_resync_jobs,
-    fivetran_start_sync_jobs,
+    kipptaf_fivetran_adp_workforce_now_start_sync_job,
+    kipptaf_fivetran_illuminate_start_sync_job,
+    kipptaf_fivetran_start_syncs_job,
 )
 
 adp_wfn_resync_schedule = ScheduleDefinition(
@@ -16,20 +18,27 @@ adp_wfn_resync_schedule = ScheduleDefinition(
     ][0],
 )
 
+kipptaf_fivetran_start_syncs_schedule = ScheduleDefinition(
+    cron_schedule="0 * * * *",
+    execution_timezone=LOCAL_TIMEZONE.name,
+    job=kipptaf_fivetran_start_syncs_job,
+)
+
+kipptaf_fivetran_adp_workforce_now_start_sync_schedule = ScheduleDefinition(
+    cron_schedule="0 0-19 * * *",
+    execution_timezone=LOCAL_TIMEZONE.name,
+    job=kipptaf_fivetran_adp_workforce_now_start_sync_job,
+)
+
+kipptaf_fivetran_illuminate_start_sync_schedule = ScheduleDefinition(
+    cron_schedule="5 * * * *",
+    execution_timezone=LOCAL_TIMEZONE.name,
+    job=kipptaf_fivetran_illuminate_start_sync_job,
+)
+
 __all__ = [
     adp_wfn_resync_schedule,
+    kipptaf_fivetran_adp_workforce_now_start_sync_schedule,
+    kipptaf_fivetran_illuminate_start_sync_schedule,
+    kipptaf_fivetran_start_syncs_schedule,
 ]
-
-for job in fivetran_start_sync_jobs:
-    if job.name == "kipptaf_adp_workforce_now_fivetran_start_sync_job":
-        cron_schedule = "0 0-19 * * *"
-    elif job.name == "kipptaf_illuminate_fivetran_start_sync_job":
-        cron_schedule = "5 * * * *"
-    else:
-        cron_schedule = "0 * * * *"
-
-    __all__.append(
-        ScheduleDefinition(
-            cron_schedule=cron_schedule, execution_timezone=LOCAL_TIMEZONE.name, job=job
-        )
-    )
