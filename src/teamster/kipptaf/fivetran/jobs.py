4c7ea86967e303b4a40159a60c951c37869e37c5
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-11-15 01:14:21 +0000

    group fivetran sync starts

--- src/teamster/kipptaf/fivetran/jobs.py
+++ src/teamster/kipptaf/fivetran/jobs.py
@@ -1,11 +1,13 @@
+import pathlib
 import re
 
-from dagster import AssetSelection, define_asset_job
+from dagster import AssetSelection, RunConfig, config_from_files, define_asset_job, job
 
 from teamster.core.fivetran.jobs import (
     build_fivetran_start_resync_job,
     build_fivetran_start_sync_job,
 )
+from teamster.core.fivetran.ops import SyncConfig, fivetran_start_sync_op
 from teamster.kipptaf import CODE_LOCATION, fivetran
 
 fivetran_materialization_jobs = []
@@ -41,8 +43,36 @@ for asset in fivetran.assets:
         )
     )
 
+asset_configs = [
+    config_from_files([str(config)])
+    for config in pathlib.Path("src/teamster/kipptaf/fivetran/config").glob("*.yaml")
+]
+
+
+@job(
+    config=RunConfig(
+        ops={
+            config["connector_name"]: SyncConfig(
+                connector_id=config["connector_id"], yield_materializations=False
+            )
+            for config in asset_configs
+            if config["connector_name"] not in ["adp_workforce_now", "illuminate"]
+        }
+    ),
+)
+def kipptaf_fivetran_start_syncs_job():
+    for config in asset_configs:
+        connector_name = config["connector_name"]
+
+        if connector_name not in ["adp_workforce_now", "illuminate"]:
+            fivetran_sync_op_aliased = fivetran_start_sync_op.alias(connector_name)
+
+            fivetran_sync_op_aliased()
+
+
 __all__ = [
     *fivetran_materialization_jobs,
     *fivetran_start_sync_jobs,
     *fivetran_start_resync_jobs,
+    kipptaf_fivetran_start_syncs_job,
 ]
