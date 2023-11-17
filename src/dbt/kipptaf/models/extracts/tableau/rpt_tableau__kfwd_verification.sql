294c34578eb250296b603d9a8b2852ca3a649cc6
Casey Gibson <cgibson@kippteamandfamily.org>
2023-11-17 17:13:19 +0000

    verification data source

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__kfwd_verification.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__kfwd_verification.sql
@@ -1,38 +1,39 @@
-SELECT
-  ar.contact_id,
-  ar.contact_full_name AS student_name,
-  ar.ktc_cohort AS hs_cohort,
-  ar.ktc_status AS status,
-  ar.contact_currently_enrolled_school AS currently_enrolled_school,
-  ar.contact_owner_name,
-  enr.ugrad_account_type,
-  enr.ugrad_account_name,
-  enr.ugrad_date_last_verified,
-  enr.ugrad_anticipated_graduation,
-  enr.ugrad_status,
-  t.Term_Season__c AS semester,
-  LEFT(t.Year__c, 4) AS year,
-  CONCAT(UPPER(LEFT(t.Term_Season__c, 2)), ' ', RIGHT(LEFT(t.Year__c, 4), 2)) AS term,
-  CASE
-    WHEN t.Verified_by_Advisor__c IS NOT NULL THEN 'Verified by Counselor'
-    WHEN t.Verified_by_NSC__c IS NOT NULL THEN 'Verified by NSC'
-  ELSE
-  'Unverified'
-END
-  AS verification_status,
-  t.Term_Verification_Status__c AS verification_status_src,
-  COALESCE(t.Verified_by_Advisor__c, t.Verified_by_NSC__c) AS verification_date,
-  ROW_NUMBER() OVER(PARTITION BY ar.contact_id, t.Term_Season__c, t.Year__c ORDER BY t.Term_Verification_Status__c DESC) AS rn_term
-FROM
-  {{ ref('int_kippadb__roster') }} AS ar
-INNER JOIN
-  {{ ref('int_kippadb__enrollment_pivot') }} AS enr
-ON
-  (ar.contact_id = enr.student
-    AND enr.ugrad_account_name IS NOT NULL
-    AND enr.ugrad_status != 'Matriculated')
-INNER JOIN
-  `kipptaf_kippadb.Term__c` AS t
-ON
-  (enr.ugrad_enrollment_id = t.Enrollment__c
-    AND t.Term_Season__c != 'Summer')
+select
+    ar.contact_id,
+    ar.contact_full_name as student_name,
+    ar.ktc_cohort as hs_cohort,
+    ar.ktc_status as status,
+    ar.contact_currently_enrolled_school as currently_enrolled_school,
+    ar.contact_owner_name,
+    enr.ugrad_account_type,
+    enr.ugrad_account_name,
+    enr.ugrad_date_last_verified,
+    enr.ugrad_anticipated_graduation,
+    enr.ugrad_status,
+    t.term_season as semester,
+    left(t.year, 4) as year,
+    concat(upper(left(t.term_season, 2)), ' ', right(left(t.year, 4), 2)) as term,
+    case
+        when t.verified_by_advisor is not null
+        then 'Verified by Counselor'
+        when t.verified_by_nsc is not null
+        then 'Verified by NSC'
+        else 'Unverified'
+    end as verification_status,
+    t.term_verification_status as verification_status_src,
+    coalesce(t.verified_by_advisor, t.verified_by_nsc) as verification_date,
+    row_number() over (
+        partition by ar.contact_id, t.term_season, t.year
+        order by t.term_verification_status desc
+    ) as rn_term,
+from {{ ref("int_kippadb__roster") }} as ar
+inner join
+    {{ ref("int_kippadb__enrollment_pivot") }} as enr
+    on (
+        ar.contact_id = enr.student
+        and enr.ugrad_account_name is not null
+        and enr.ugrad_status != 'Matriculated'
+    )
+inner join
+    {{ ref("stg_kippadb__term") }} as t
+    on (enr.ugrad_enrollment_id = t.enrollment and t.term_season != 'Summer')
