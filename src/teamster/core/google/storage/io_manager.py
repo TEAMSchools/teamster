71dbf232e7af9e8f6dc652d24b9ca1b10a4a2768
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-11-20 16:29:03 +0000

    file io mgr

--- src/teamster/core/google/storage/io_manager.py
+++ src/teamster/core/google/storage/io_manager.py
@@ -1,14 +1,83 @@
+from urllib.parse import urlparse
+
 import fastavro
-from dagster import Any, InputContext, OutputContext
+import pendulum
+from dagster import Any, Dict, InputContext, MultiPartitionKey, OutputContext
 from dagster._utils.backoff import backoff
 from dagster._utils.cached_method import cached_method
 from dagster_gcp.gcs import GCSPickleIOManager, PickledObjectGCSIOManager
 from google.api_core.exceptions import Forbidden, ServiceUnavailable, TooManyRequests
 from google.cloud import storage
+from pendulum.parsing.exceptions import ParserError
 from upath import UPath
 
+from teamster.core.utils.classes import FiscalYear
 
-class AvroGCSIOManager(PickledObjectGCSIOManager):
+
+class GCSUPathIOManager(PickledObjectGCSIOManager):
+    def _get_hive_partition(
+        self, partition_value: str, partition_key: str = "key"
+    ) -> list:
+        try:
+            datetime = pendulum.parser.parse(partition_value)
+
+            return "/".join(
+                [
+                    (
+                        "_dagster_partition_fiscal_year="
+                        + str(FiscalYear(datetime=datetime, start_month=7).fiscal_year)
+                    ),
+                    f"_dagster_partition_date={datetime.to_date_string()}",
+                    f"_dagster_partition_hour={datetime.format('HH')}",
+                    f"_dagster_partition_minute={datetime.format('mm')}",
+                ]
+            )
+        except ParserError:
+            return f"_dagster_partition_{partition_key}={partition_value}"
+
+    def _formatted_multipartitioned_path(self, partition_key: MultiPartitionKey) -> str:
+        ordered_dimension_keys = []
+
+        for key, value in sorted(
+            partition_key.keys_by_dimension.items(), key=lambda x: x[0]
+        ):
+            ordered_dimension_keys.append(
+                self._get_hive_partition(partition_key=key, partition_value=value)
+            )
+
+        return "/".join(ordered_dimension_keys)
+
+    def _get_paths_for_partitions(
+        self, context: InputContext | OutputContext
+    ) -> Dict[str, UPath]:
+        if not context.has_asset_partitions:
+            raise TypeError(
+                f"Detected {context.dagster_type.typing_type} input type but the asset"
+                " is not partitioned"
+            )
+
+        formatted_partition_keys = {
+            partition_key: (
+                self._formatted_multipartitioned_path(partition_key)
+                if isinstance(partition_key, MultiPartitionKey)
+                else self._get_hive_partition(partition_value=partition_key)
+            )
+            for partition_key in context.asset_partition_keys
+        }
+
+        return {
+            partition_key: self._with_extension(
+                self.get_path_for_partition(
+                    context,
+                    self._get_path_without_extension(context),
+                    f"{partition}/data",
+                )
+            )
+            for partition_key, partition in formatted_partition_keys.items()
+        }
+
+
+class AvroGCSIOManager(GCSUPathIOManager):
     def load_from_path(self, context: InputContext, path: UPath) -> Any:
         bucket_obj: storage.Bucket = self.bucket_obj
 
@@ -26,11 +95,29 @@ class AvroGCSIOManager(PickledObjectGCSIOManager):
             fn=fastavro.writer,
             retry_on=(TooManyRequests, Forbidden, ServiceUnavailable),
             kwargs={
-                "fo": bucket_obj.blob(str(path)).open(mode="wb"),
+                "fo": bucket_obj.blob(str(path)).open(mode="wb", ignore_flush=True),
                 "schema": fastavro.parse_schema(schema),
                 "records": records,
                 "codec": "snappy",
             },
+        )
+
+
+class FileGCSIOManager(GCSUPathIOManager):
+    def load_from_path(self, context: InputContext, path: UPath) -> Any:
+        return urlparse(self._uri_for_path(path))
+
+    def dump_to_path(self, context: OutputContext, obj: Any, path: UPath) -> None:
+        bucket_obj: storage.Bucket = self.bucket_obj
+
+        if self.path_exists(path):
+            context.log.warning(f"Removing existing GCS key: {path}")
+            self.unlink(path)
+
+        backoff(
+            fn=bucket_obj.blob(path).upload_from_filename,
+            retry_on=(TooManyRequests, Forbidden, ServiceUnavailable),
+            kwargs={"filename": obj},
         )
 
 
@@ -46,7 +133,7 @@ class GCSIOManager(GCSPickleIOManager):
                 client=self.gcs.get_client(),
                 prefix=self.gcs_prefix,
             )
-        else:
+        elif self.object_type == "pickle":
             return PickledObjectGCSIOManager(
                 bucket=self.gcs_bucket,
                 client=self.gcs.get_client(),
