dae4dd13bed68445ae599c3e24c5da92deda4b17
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-11-27 21:39:31 +0000

    add MAX_RUNTIME_SECONDS_TAG

--- src/teamster/kippnewark/powerschool/assets.py
+++ src/teamster/kippnewark/powerschool/assets.py
@@ -1,5 +1,9 @@
 import pendulum
-from dagster import MonthlyPartitionsDefinition, config_from_files
+from dagster import (
+    MAX_RUNTIME_SECONDS_TAG,
+    MonthlyPartitionsDefinition,
+    config_from_files,
+)
 
 from teamster.core.powerschool.assets import build_powerschool_table_asset
 from teamster.core.utils.classes import FiscalYearPartitionsDefinition
@@ -9,12 +13,16 @@ from .. import CODE_LOCATION, LOCAL_TIMEZONE
 config_dir = f"src/teamster/{CODE_LOCATION}/powerschool/config"
 
 full_assets = [
-    build_powerschool_table_asset(**cfg, code_location=CODE_LOCATION)
+    build_powerschool_table_asset(
+        **cfg, code_location=CODE_LOCATION, op_tags={MAX_RUNTIME_SECONDS_TAG: (60 * 10)}
+    )
     for cfg in config_from_files([(f"{config_dir}/assets-full.yaml")])["assets"]
 ]
 
 nonpartition_assets = [
-    build_powerschool_table_asset(**cfg, code_location=CODE_LOCATION)
+    build_powerschool_table_asset(
+        **cfg, code_location=CODE_LOCATION, op_tags={MAX_RUNTIME_SECONDS_TAG: (60 * 10)}
+    )
     for cfg in config_from_files([(f"{config_dir}/assets-nonpartition.yaml")])["assets"]
 ]
 
@@ -29,6 +37,7 @@ transaction_date_partition_assets = [
             fmt="%Y-%m-%dT%H:%M:%S%z",
         ),
         partition_column="transaction_date",
+        op_tags={MAX_RUNTIME_SECONDS_TAG: (60 * 10)},
     )
     for asset in config_from_files([f"{config_dir}/assets-transactiondate.yaml"])[
         "assets"
@@ -46,6 +55,7 @@ assignment_assets = [
             end_offset=1,
         ),
         partition_column="whenmodified",
+        op_tags={MAX_RUNTIME_SECONDS_TAG: (60 * 10)},
     )
     for asset in config_from_files([f"{config_dir}/assets-whenmodified.yaml"])["assets"]
 ]
