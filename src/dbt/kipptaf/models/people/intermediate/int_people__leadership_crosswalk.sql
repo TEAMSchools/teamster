67ea0a1998a60cb907119ba89fbb43f1a749c235
Charlie Bini <cbini@apps.teamschools.org>
2023-11-28 17:47:02 +0000

    rename, cleanup

--- src/dbt/kipptaf/models/people/intermediate/int_people__leadership_crosswalk.sql
+++ src/dbt/kipptaf/models/people/intermediate/int_people__leadership_crosswalk.sql
@@ -1,30 +1,18 @@
 with
     school_leadership as (
         select
-            home_work_location_name as location,
-            business_unit_home_name as region,
+            home_work_location_powerschool_school_id,
+            home_work_location_name,
+            business_unit_home_name,
             coalesce(
                 max(
-                    case
-                        when job_title = 'Director School Operations'
-                        then employee_number
-                    end
+                    if(job_title = 'Director School Operations', employee_number, null)
                 ),
-                max(
-                    case
-                        when job_title = 'Director Campus Operations'
-                        then employee_number
-                    end
-                )
+                max(if(job_title = 'Director Campus Operations', employee_number, null))
             ) as dso_employee_number,
             coalesce(
-                max(case when job_title = 'School Leader' then employee_number end),
-                max(
-                    case
-                        when job_title = 'School Leader in Residence'
-                        then employee_number
-                    end
-                )
+                max(if(job_title = 'School Leader', employee_number, null)),
+                max(if(job_title = 'School Leader in Residence', employee_number, null))
             ) as sl_employee_number,
         from {{ ref("base_people__staff_roster") }}
         where
@@ -35,47 +23,50 @@ with
                 'School Leader in Residence'
             )
             and assignment_status != 'Terminated'
-        group by home_work_location_name, business_unit_home_name
+        group by
+            home_work_location_powerschool_school_id,
+            home_work_location_name,
+            business_unit_home_name
     )
 
 select
-    l.location,
-    l.region,
+    l.home_work_location_powerschool_school_id,
+    l.home_work_location_name,
+    l.business_unit_home_name,
     l.dso_employee_number,
     l.sl_employee_number,
 
-    sr1.preferred_name_lastfirst as dso,
-    sr1.mail as dso_mail,
-    sr1.google_email as dso_google_mail,
-    sr1.job_title as dso_title,
-    sr1.report_to_employee_number as mdso_employee_number,
+    sl.preferred_name_lastfirst as school_leader_preferred_name_lastfirst,
+    sl.mail as school_leader_mail,
+    sl.google_email as school_leader_google_email,
+    sl.job_title as school_leader_job_title,
+    sl.report_to_employee_number as school_leader_report_to_employee_number,
 
-    sr2.preferred_name_lastfirst as school_leader,
-    sr2.mail as school_leader_mail,
-    sr2.google_email as school_leader_google_mail,
-    sr2.job_title as school_leader_job_title,
-    sr2.report_to_employee_number as hos_employee_number,
+    hos.preferred_name_lastfirst as head_of_school_preferred_name_lastfirst,
+    hos.mail as head_of_school_mail,
+    hos.google_email as head_of_school_google_email,
+    hos.job_title as head_of_school_job_title,
 
-    sr3.preferred_name_lastfirst as mdso,
-    sr3.mail as mdso_mail,
-    sr3.google_email as mdso_google_mail,
-    sr3.job_title as mdso_job_title,
+    dso.preferred_name_lastfirst as dso_preferred_name_lastfirst,
+    dso.mail as dso_mail,
+    dso.google_email as dso_google_email,
+    dso.job_title as dso_job_title,
+    dso.report_to_employee_number as dso_report_to_employee_number,
 
-    sr4.preferred_name_lastfirst as head_of_schools,
-    sr4.mail as head_of_schools_mail,
-    sr4.google_email as head_of_schools_google_mail,
-    sr4.job_title as head_of_schools_job_title,
-
+    mdso.preferred_name_lastfirst as mdso_preferred_name_lastfirst,
+    mdso.mail as mdso_mail,
+    mdso.google_email as mdso_google_email,
+    mdso.job_title as mdso_job_title,
 from school_leadership as l
 left join
-    {{ ref("base_people__staff_roster") }} as sr1
-    on l.dso_employee_number = sr1.employee_number
+    {{ ref("base_people__staff_roster") }} as sl
+    on l.sl_employee_number = sl.employee_number
 left join
-    {{ ref("base_people__staff_roster") }} as sr2
-    on l.sl_employee_number = sr2.employee_number
+    {{ ref("base_people__staff_roster") }} as hos
+    on sl.report_to_employee_number = hos.employee_number
 left join
-    {{ ref("base_people__staff_roster") }} as sr3
-    on sr1.report_to_employee_number = sr3.employee_number
+    {{ ref("base_people__staff_roster") }} as dso
+    on l.dso_employee_number = dso.employee_number
 left join
-    {{ ref("base_people__staff_roster") }} as sr4
-    on sr2.report_to_employee_number = sr4.employee_number
+    {{ ref("base_people__staff_roster") }} as mdso
+    on dso.report_to_employee_number = mdso.employee_number
