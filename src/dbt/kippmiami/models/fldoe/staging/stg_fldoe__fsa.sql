321e99f298c2d0693b6af2f63bb0f7a7c1e5a0ff
Gaby Rangel <grangel@kippteamandfamily.org>
2023-12-05 18:13:09 +0000

    fixed if

--- src/dbt/kippmiami/models/fldoe/staging/stg_fldoe__fsa.sql
+++ src/dbt/kippmiami/models/fldoe/staging/stg_fldoe__fsa.sql
@@ -92,6 +92,5 @@ select
         when 5
         then 'Mastery'
     end as achievement_level,
-    if performance_level >= 3
-then true end as is_proficient,
+    if(performance_level >= 3, 'true', 'false') as is_proficient,
 from fsa
