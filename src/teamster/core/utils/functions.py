3850671cc0aba0696ca86ad9a39f0c1059d1ac7e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-12-08 22:29:00 +0000

    add avro asset check

--- src/teamster/core/utils/functions.py
+++ src/teamster/core/utils/functions.py
@@ -2,7 +2,13 @@ import os
 import pathlib
 
 import pendulum
-from dagster import DagsterInstance, MultiPartitionKey
+from dagster import (
+    AssetCheckResult,
+    AssetCheckSeverity,
+    AssetCheckSpec,
+    DagsterInstance,
+    MultiPartitionKey,
+)
 
 from teamster.core.utils.classes import FiscalYear
 
@@ -134,3 +140,28 @@ def get_dagster_cloud_instance(dagster_home_path):
     pathlib.Path(dagster_home_path).mkdir(parents=True, exist_ok=True)
 
     return DagsterInstance.get()
+
+
+def get_avro_schema_valid_check_spec(asset):
+    return AssetCheckSpec(
+        name="avro_schema_valid",
+        asset=asset,
+        description=(
+            "Checks output records against the supplied schema and warns if any "
+            "unexpected fields are discovered"
+        ),
+    )
+
+
+def check_avro_schema_valid(asset_key, records, schema):
+    extras = set().union(*(d.keys() for d in records)) - set(
+        field["name"] for field in schema["fields"]
+    )
+
+    return AssetCheckResult(
+        passed=len(extras) == 0,
+        asset_key=asset_key,
+        check_name="avro_schema_valid",
+        metadata={"extras": extras},
+        severity=AssetCheckSeverity.WARN,
+    )
--- src/teamster/staging/definitions.py
+++ src/teamster/staging/definitions.py
@@ -1,8 +1,9 @@
-from dagster import Definitions  # EnvVar, load_assets_from_modules
+from dagster import Definitions, EnvVar, load_assets_from_modules
 from dagster_gcp import GCSResource
 
 from teamster import GCS_PROJECT_NAME
 from teamster.core.google.storage.io_manager import GCSIOManager
+from teamster.kipptaf import ldap
 
 # from teamster.core.sqlalchemy.resources import OracleResource, SqlAlchemyEngineResource
 # from teamster.core.ssh.resources import SSHResource
@@ -11,20 +12,27 @@ from teamster.staging import CODE_LOCATION
 GCS_RESOURCE = GCSResource(project=GCS_PROJECT_NAME)
 
 defs = Definitions(
-    # assets=load_assets_from_modules(modules=[]),
+    assets=load_assets_from_modules(modules=[ldap]),
     resources={
         "gcs": GCS_RESOURCE,
-        "io_manager": GCSIOManager(
-            gcs=GCS_RESOURCE,
-            gcs_bucket=f"teamster-{CODE_LOCATION}",
-            object_type="pickle",
-        ),
+        # "io_manager": GCSIOManager(
+        #     gcs=GCS_RESOURCE,
+        #     gcs_bucket=f"teamster-{CODE_LOCATION}",
+        #     object_type="pickle",
+        # ),
         "io_manager_gcs_avro": GCSIOManager(
             gcs=GCS_RESOURCE, gcs_bucket=f"teamster-{CODE_LOCATION}", object_type="avro"
         ),
-        "io_manager_gcs_file": GCSIOManager(
-            gcs=GCS_RESOURCE, gcs_bucket=f"teamster-{CODE_LOCATION}", object_type="file"
+        "ldap": ldap.LdapResource(
+            # host="ldap1.kippnj.org",
+            host="204.8.89.213",
+            port=636,
+            user=EnvVar("LDAP_USER"),
+            password=EnvVar("LDAP_PASSWORD"),
         ),
+        # "io_manager_gcs_file": GCSIOManager(
+        #     gcs=GCS_RESOURCE, gcs_bucket=f"teamster-{CODE_LOCATION}", object_type="file"
+        # ),
         # "db_powerschool": OracleResource(
         #     engine=SqlAlchemyEngineResource(
         #         dialect="oracle",
