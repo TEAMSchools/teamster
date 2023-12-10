d7804e884cc130ee791e3f4e440138850acafcc6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-12-10 00:13:08 +0000

    fix import

--- src/teamster/staging/definitions.py
+++ src/teamster/staging/definitions.py
@@ -1,13 +1,14 @@
 from dagster import Definitions, load_assets_from_modules
 
 from teamster.core.resources import get_io_manager_gcs_avro
-from teamster.kipptaf import ldap, resources
+from teamster.kipptaf import ldap
+from teamster.kipptaf.resources import LDAP_RESOURCE
 from teamster.staging import CODE_LOCATION
 
 defs = Definitions(
     assets=load_assets_from_modules(modules=[ldap]),
     resources={
         "io_manager_gcs_avro": get_io_manager_gcs_avro(CODE_LOCATION),
-        "ldap": resources.LDAP_RESOURCE,
+        "ldap": LDAP_RESOURCE,
     },
 )
