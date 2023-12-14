30f923959370f49d9bd38d91c5c40503a59179b3
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-12-14 18:12:51 +0000

    for the last time

--- src/teamster/kipptaf/google/sheets/__init__.py
+++ src/teamster/kipptaf/google/sheets/__init__.py
@@ -1,20 +1,5 @@
-from ...dbt.assets import dbt_manifest
-from .assets import build_google_sheets_asset
-from .sensors import build_google_sheets_asset_sensor
-
-assets = [
-    build_google_sheets_asset(
-        source_name=source["source_name"],
-        name=source["name"].split("__")[-1],
-        uri=source["external"]["options"]["uris"][0],
-        range_name=source["external"]["options"]["sheet_range"],
-    )
-    for source in dbt_manifest["sources"].values()
-    if source.get("external")
-    and source["external"]["options"]["format"] == "GOOGLE_SHEETS"
-]
-
-sensors = build_google_sheets_asset_sensor(assets)
+from .assets import _all as assets
+from .sensors import _all as sensors
 
 _all = [
     assets,
--- src/teamster/staging/google/sheets/assets.py
+++ src/teamster/staging/google/sheets/assets.py
@@ -1,10 +1,26 @@
-from teamster.kipptaf.google.sheets.assets import build_google_sheets_asset
+import re
+
+from dagster import AssetSpec, external_asset_from_spec
+
+from ... import CODE_LOCATION
+
+uri = "https://docs.google.com/spreadsheets/d/1G2z9rwXsFaMdFL6iOYdfQTVjZ7bctXMyz_Q09IhP4QE"
+range_name = "src_assessments__course_subject_crosswalk"
+
+re_match = re.match(
+    pattern=r"https:\/{2}docs\.google\.com\/spreadsheets\/d\/([\w-]+)", string=uri
+)
+
+google_sheets_assets = [
+    external_asset_from_spec(
+        AssetSpec(
+            key=[CODE_LOCATION, "google", "sheets", "test"],
+            metadata={"sheet_id": re_match.group(1), "range_name": range_name},
+            group_name="google_sheets",
+        )
+    )
+]
 
 _all = [
-    build_google_sheets_asset(
-        source_name="test",
-        name="asset",
-        uri="https://docs.google.com/spreadsheets/d/1G2z9rwXsFaMdFL6iOYdfQTVjZ7bctXMyz_Q09IhP4QE",
-        range_name="src_assessments__course_subject_crosswalk",
-    )
+    google_sheets_assets,
 ]
