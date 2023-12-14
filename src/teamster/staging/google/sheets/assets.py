58e73625634a731ccbdd2ba99bee3a18c382b7df
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-12-14 16:43:04 +0000

    fmt

--- src/teamster/staging/google/sheets/assets.py
+++ src/teamster/staging/google/sheets/assets.py
@@ -5,6 +5,6 @@ _all = [
         source_name="test",
         name="asset",
         uri="https://docs.google.com/spreadsheets/d/1G2z9rwXsFaMdFL6iOYdfQTVjZ7bctXMyz_Q09IhP4QE",
-        range_name="src_assessments__course_subject_crosswalk"
+        range_name="src_assessments__course_subject_crosswalk",
     )
 ]
--- src/teamster/staging/google/sheets/sensors.py
+++ src/teamster/staging/google/sheets/sensors.py
@@ -1,11 +1,8 @@
-
-
 from teamster.kipptaf.google.sheets.sensors import build_google_sheets_asset_sensor
 
 from .assets import _all as google_sheets_assets
 
 google_sheets_asset_sensor = build_google_sheets_asset_sensor(google_sheets_assets)
-
 
 _all = [
     google_sheets_asset_sensor,
