262cf431c5fca01bd3c8b23165ad0b8caa80731f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-12-18 21:14:53 +0000

    edplan archive asset

--- src/teamster/kippcamden/edplan/assets.py
+++ src/teamster/kippcamden/edplan/assets.py
@@ -1,4 +1,4 @@
-from dagster import AutoMaterializePolicy, DailyPartitionsDefinition, config_from_files
+from dagster import AutoMaterializePolicy, DailyPartitionsDefinition
 
 from teamster.core.edplan.schema import ASSET_FIELDS
 from teamster.core.sftp.assets import build_sftp_asset
@@ -6,23 +6,34 @@ from teamster.core.utils.functions import get_avro_record_schema
 
 from .. import CODE_LOCATION, LOCAL_TIMEZONE
 
+avro_schema = get_avro_record_schema(
+    name="njsmart_powerschool", fields=ASSET_FIELDS["njsmart_powerschool"]
+)
+
+njsmart_powerschool = build_sftp_asset(
+    asset_key=[CODE_LOCATION, "edplan", "njsmart_powerschool"],
+    remote_dir="Reports",
+    remote_file_regex=r"NJSMART-Power[Ss]chool\.txt",
+    ssh_resource_key="ssh_edplan",
+    avro_schema=avro_schema,
+    partitions_def=DailyPartitionsDefinition(
+        start_date="2023-05-08",
+        timezone=LOCAL_TIMEZONE.name,
+        fmt="%Y-%m-%d",
+        end_offset=1,
+    ),
+    auto_materialize_policy=AutoMaterializePolicy.eager(),
+)
+
+njsmart_powerschool_archive = build_sftp_asset(
+    asset_key=[CODE_LOCATION, "edplan", "njsmart_powerschool_archive"],
+    remote_dir=f"/teamster-{CODE_LOCATION}/couchdrop/edplan/njsmart_powerschool_archive",
+    remote_file_regex=r"src_edplan__njsmart_powerschool_archive\.csv",
+    ssh_resource_key="ssh_couchdrop",
+    avro_schema=avro_schema,
+)
+
 _all = [
-    build_sftp_asset(
-        asset_key=[CODE_LOCATION, "edplan", a["asset_name"]],
-        ssh_resource_key="ssh_edplan",
-        avro_schema=get_avro_record_schema(
-            name=a["asset_name"], fields=ASSET_FIELDS[a["asset_name"]]
-        ),
-        partitions_def=DailyPartitionsDefinition(
-            start_date=a["partition_start_date"],
-            timezone=LOCAL_TIMEZONE.name,
-            fmt="%Y-%m-%d",
-            end_offset=1,
-        ),
-        auto_materialize_policy=AutoMaterializePolicy.eager(),
-        **a,
-    )
-    for a in config_from_files(
-        [f"src/teamster/{CODE_LOCATION}/edplan/config/assets.yaml"]
-    )["assets"]
+    njsmart_powerschool,
+    njsmart_powerschool_archive,
 ]
