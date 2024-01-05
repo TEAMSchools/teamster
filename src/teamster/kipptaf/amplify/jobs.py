86594f3205d1928003b1df3c030c58ca0910bd28
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-05 15:32:11 +0000

    hotfix: add max runtime taf

--- src/teamster/kipptaf/amplify/jobs.py
+++ src/teamster/kipptaf/amplify/jobs.py
@@ -1,4 +1,4 @@
-from dagster import AssetSelection, define_asset_job
+from dagster import MAX_RUNTIME_SECONDS_TAG, AssetSelection, define_asset_job
 
 from .assets import mclass_assets
 
@@ -6,6 +6,7 @@ mclass_asset_job = define_asset_job(
     name="mclass_asset_job",
     selection=AssetSelection.assets(*mclass_assets),
     partitions_def=mclass_assets[0].partitions_def,
+    tags={MAX_RUNTIME_SECONDS_TAG: (60 * 15)},
 )
 
 _all = [
