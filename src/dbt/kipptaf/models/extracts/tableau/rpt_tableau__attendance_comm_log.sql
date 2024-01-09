78a1c40ee93b9174e206423e1eacc42c91b77d0d
Anthony Walters <awalters@kippteamandfamily.org>
2024-01-09 16:17:23 +0000

    attendance comm log view update with working date joins

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__attendance_comm_log.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__attendance_comm_log.sql
@@ -8,13 +8,14 @@ with
             c.call_date_time as commlog_datetime,
             c.call_date_time as commlog_date,
             c._dbt_source_relation,
+            c.call_type as commlog_type,
+            c.call_status as commlog_status,
 
             f.init_notes as followup_init_notes,
             f.followup_notes as followup_close_notes,
             f.outstanding,
 
             concat(u.first_name, ' ', u.last_name) as commlog_staff_name,
-
             concat(f.c_first, ' ', f.c_last) as followup_staff_name,
         from {{ ref("stg_deanslist__comm_log") }} as c
         inner join
@@ -25,9 +26,8 @@ with
             {{ ref("stg_deanslist__followups") }} as f
             on c.record_id = f.source_id
             and {{ union_dataset_join_clause(left_alias="c", right_alias="f") }}
-        where (c.reason like 'Att:%' or c.reason like 'Chronic%')
+        where c.reason like 'Att:%' and c.call_status = 'Completed'
     ),
-
     ada as (
         select
             studentid,
@@ -37,17 +37,17 @@ with
         from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }}
         where
             membershipvalue = 1
-            and calendardate
-            <= cast(current_date('{{ var("local_timezone") }}') as date)
+            and calendardate <= cast(current_date('America/New_York') as date)
         group by studentid, yearid, _dbt_source_relation
     )
-
 select
     co.student_number,
     co.lastfirst,
     co.academic_year,
     co.region,
+    co.school_level,
     co.reporting_schoolid,
+    co.school_abbreviation,
     co.grade_level,
     co.advisory_name as team,
     co.spedlep as iep_status,
@@ -64,25 +64,58 @@ select
     cl.commlog_reason,
     cl.commlog_notes,
     cl.commlog_topic,
+    cl.commlog_type,
+    cl.commlog_status,
     cl.followup_staff_name,
     cl.followup_init_notes,
     cl.followup_close_notes,
 
-    ada.ada,
-
     gpa.gpa_y1,
     gpa.n_failing_y1,
 
-    {# TODO #}
     null as read_lvl,
     null as goal_status,
 
     rt.name as term,
 
     case
+        when co.school_level = 'HS'
+        then co.advisor_lastfirst
+        else cast(co.grade_level as string)
+    end as drill_down,
+    case
+        when
+            (
+                cl.commlog_reason is not null
+                and cl.commlog_reason not like 'Att: Unknown%'
+            )
+        then true
+        else false
+    end as is_successful,
+    count(distinct att.att_date) over (partition by co.student_number) as abs_count,
+    max(
+        case when cl.commlog_reason = 'Chronic Absence: 3' then true else false end
+    ) over (partition by co.student_number, co.academic_year) as is_chronic_3,
+    max(
+        case when cl.commlog_reason = 'Chronic Absence: 6' then true else false end
+    ) over (partition by co.student_number, co.academic_year) as is_chronic_6,
+    max(
+        case when cl.commlog_reason = 'Chronic Absence: 10' then true else false end
+    ) over (partition by co.student_number, co.academic_year) as is_chronic_10,
+    max(
+        case when cl.commlog_reason = 'Chronic Absence: 10+' then true else false end
+    ) over (partition by co.student_number, co.academic_year) as is_chronic_10_plus,
+    max(
+        case when cl.commlog_reason = 'Chronic Absence: 20' then true else false end
+    ) over (partition by co.student_number, co.academic_year) as is_chronic_20,
+    max(
+        case when cl.commlog_reason = 'Chronic Absence: 20+' then true else false end
+    ) over (partition by co.student_number, co.academic_year) as is_chronic_20_plus,
+    ada.ada,
+
+    case
         when att.schoolid = 73253 then co.advisor_lastfirst else cc.section_number
     end as homeroom,
-
     row_number() over (
         partition by co.studentid, att.att_date order by cl.commlog_datetime desc
     ) as rn_date,
@@ -113,7 +146,7 @@ left join
 left join
     commlog as cl
     on co.student_number = cl.student_school_id
-    and att.att_date = cl.commlog_date
+    and att.att_date = safe_cast(cl.commlog_date as date)
     and {{ union_dataset_join_clause(left_alias="co", right_alias="cl") }}
 left join
     ada
@@ -125,11 +158,6 @@ left join
     on co.schoolid = rt.school_id
     and att.att_date between rt.start_date and rt.end_date
     and rt.type = 'RT'
-{# TODO
-left join
-    lit.achieved_by_round_static as r
-    on co.student_number = r.student_number
-    and co.academic_year = r.academic_year
-    and r.is_curterm = 1 
-#}
-where co.academic_year = {{ var("current_academic_year") }}
+where
+    co.academic_year = {{ var("current_academic_year") }}
+    and att.att_date <= current_date('America/New_York')
