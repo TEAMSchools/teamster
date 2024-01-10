0c5f1d5eebb0f386e00f6fac7e893af62051d923
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-10 22:07:08 +0000

    refactor: update sensors to use external assets

--- src/teamster/kipptaf/fivetran/assets.py
+++ src/teamster/kipptaf/fivetran/assets.py
@@ -6,6 +6,8 @@ from dagster import AssetKey, AssetsDefinition, AssetSpec
 from teamster.core.definitions.external_asset import external_assets_from_specs
 from teamster.kipptaf import CODE_LOCATION
 
+CONNECTORS = {}
+
 config_path = pathlib.Path(__file__).parent / "config"
 
 specs = []
@@ -13,21 +15,26 @@ for config_file in config_path.glob("*.yaml"):
     config = yaml.safe_load(config_file.read_text())
 
     connector_name = config["connector_name"]
+    connector_id = config["connector_id"]
 
-    asset_key_prefix = [CODE_LOCATION, connector_name]
+    CONNECTORS[connector_id] = set()
 
     for schema in config["schemas"]:
+        asset_key_prefix = [CODE_LOCATION, connector_name]
+
         schema_name = schema.get("name")
 
         if schema_name is not None:
             asset_key_prefix.append(schema_name)
+
+        CONNECTORS[connector_id].add(".".join(asset_key_prefix[1:]))
 
         for table in schema["destination_tables"]:
             specs.append(
                 AssetSpec(
                     key=AssetKey([*asset_key_prefix, table]),
                     group_name=config.get("group_name", connector_name),
-                    metadata={"connector_id": config["connector_id"]},
+                    metadata={"connector_id": connector_id},
                 )
             )
 
