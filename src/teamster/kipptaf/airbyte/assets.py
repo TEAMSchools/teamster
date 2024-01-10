9ad4dff569d44d76343f096ac75b2d85daed693d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-10 22:35:45 +0000

    fix: group name

--- src/teamster/kipptaf/airbyte/assets.py
+++ src/teamster/kipptaf/airbyte/assets.py
@@ -10,6 +10,7 @@ specs = [
     AssetSpec(
         key=AssetKey([CODE_LOCATION, a["group_name"], table]),
         metadata={"connection_id": a["connection_id"]},
+        group_name=a["group_name"],
     )
     for a in config_from_files([f"{pathlib.Path(__file__).parent}/config/assets.yaml"])[
         "assets"
