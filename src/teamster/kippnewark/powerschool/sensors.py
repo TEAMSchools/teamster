4f6053ff0bdf551c9f0b3464fb4fb564c5908a08
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-11 21:17:36 +0000

    fix: convert tz

--- src/teamster/kippnewark/powerschool/sensors.py
+++ src/teamster/kippnewark/powerschool/sensors.py
@@ -1,11 +1,12 @@
 from teamster.core.powerschool.sensors import build_powerschool_sensor
 
-from .. import CODE_LOCATION
+from .. import CODE_LOCATION, LOCAL_TIMEZONE
 from .assets import partition_assets
 
 partition_sensor = build_powerschool_sensor(
     name=f"{CODE_LOCATION}_powerschool_partition_sensor",
     asset_defs=partition_assets,
+    execution_timezone=LOCAL_TIMEZONE,
     minimum_interval_seconds=(60 * 10),
 )
 
