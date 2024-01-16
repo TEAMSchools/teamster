f10b84f17a1c949102b954295d3282e476d7a1ae
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-16 21:02:16 +0000

    feat: add test assets

--- src/teamster/staging/definitions.py
+++ src/teamster/staging/definitions.py
@@ -3,8 +3,14 @@ from dagster import Definitions, load_assets_from_modules
 from teamster.core.resources import get_io_manager_gcs_avro
 from teamster.staging import CODE_LOCATION
 
+from . import assets
+
 defs = Definitions(
-    assets=load_assets_from_modules(modules=[]),
+    assets=load_assets_from_modules(
+        modules=[
+            assets,
+        ]
+    ),
     sensors=[],
     schedules=[],
     resources={
