ef1d10bb2b926403c15d141ead2b02faf26dcc54
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-16 20:48:45 +0000

    feat: add server_k8s_config

--- src/teamster/staging/definitions.py
+++ src/teamster/staging/definitions.py
@@ -1,27 +1,13 @@
 from dagster import Definitions, load_assets_from_modules
 
 from teamster.core.resources import get_io_manager_gcs_avro
-from teamster.kipptaf import airbyte, fivetran
-from teamster.staging import CODE_LOCATION, resources
+from teamster.staging import CODE_LOCATION
 
 defs = Definitions(
-    assets=load_assets_from_modules(
-        modules=[
-            airbyte,
-            fivetran,
-        ]
-    ),
-    sensors=[
-        *airbyte.sensors,
-        *fivetran.sensors,
-    ],
-    schedules=[
-        *airbyte.schedules,
-        *fivetran.schedules,
-    ],
+    assets=load_assets_from_modules(modules=[]),
+    sensors=[],
+    schedules=[],
     resources={
         "io_manager_gcs_avro": get_io_manager_gcs_avro(CODE_LOCATION),
-        "airbyte": resources.AIRBYTE_CLOUD_RESOURCE,
-        "fivetran": resources.FIVETRAN_RESOURCE,
     },
 )
