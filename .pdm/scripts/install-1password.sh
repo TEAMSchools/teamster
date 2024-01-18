566ba246b3c5b81d910afe4e6cfe173d77b73e7d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-18 19:48:28 +0000

    fix: key

--- .pdm/scripts/install-1password.sh
+++ .pdm/scripts/install-1password.sh
@@ -3,11 +3,11 @@
 helm repo add 1password https://1password.github.io/connect-helm-charts/
 helm repo update
 
-helm show values 1password/connect >.helm/1password/values.yaml
+helm show values 1password/connect >.k8s/1password/values.yaml
 
 helm upgrade \
   --install connect 1password/connect \
   --set-file connect.credentials=/etc/secret-volume/op_credentials_json \
   --set operator.token.value="${OP_CONNECT_TOKEN}" \
   --namespace dagster-cloud \
-  -f .helm/1password/values-override.yaml
+  -f .k8s/1password/values-override.yaml
