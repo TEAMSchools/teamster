c06c0902012c36c5ba7bc317f842cad1873cfab3
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-19 17:21:02 +0000

    refactor: remove regional env vars

--- tests/sensors/test_powerschool_sensor.py
+++ tests/sensors/test_powerschool_sensor.py
@@ -1,10 +1,7 @@
 from dagster import build_sensor_context
 
 from teamster.core.powerschool.sensors import build_powerschool_sensor
-from teamster.core.resources import (
-    get_oracle_resource_powerschool,
-    get_ssh_resource_powerschool,
-)
+from teamster.core.resources import DB_POWERSCHOOL, get_ssh_resource_powerschool
 from teamster.core.utils.functions import get_dagster_cloud_instance
 from teamster.kippnewark.powerschool.assets import partition_assets
 from teamster.staging import LOCAL_TIMEZONE
@@ -21,8 +18,8 @@ def test_powerschool_sensor():
 
     sensor_results = dynamic_partition_sensor(
         context=context,
-        ssh_powerschool=get_ssh_resource_powerschool("KIPPNEWARK"),
-        db_powerschool=get_oracle_resource_powerschool("KIPPNEWARK"),
+        ssh_powerschool=get_ssh_resource_powerschool("psteam.kippnj.org"),
+        db_powerschool=DB_POWERSCHOOL,
     )
 
     for result in sensor_results:
