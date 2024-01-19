0f6a77bd514db4b307ba8d701d7bfb7980acdb01
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-19 17:21:02 +0000

    refactor: remove regional env vars

--- src/teamster/kippcamden/definitions.py
+++ src/teamster/kippcamden/definitions.py
@@ -3,17 +3,17 @@ from dagster_k8s import k8s_job_executor
 
 from teamster.core.resources import (
     BIGQUERY_RESOURCE,
+    DB_POWERSCHOOL,
     DEANSLIST_RESOURCE,
     GCS_RESOURCE,
     SSH_COUCHDROP,
+    SSH_EDPLAN,
+    SSH_TITAN,
     get_dbt_cli_resource,
     get_io_manager_gcs_avro,
     get_io_manager_gcs_file,
     get_io_manager_gcs_pickle,
-    get_oracle_resource_powerschool,
-    get_ssh_resource_edplan,
     get_ssh_resource_powerschool,
-    get_ssh_resource_titan,
 )
 
 from . import (
@@ -55,18 +55,18 @@ defs = Definitions(
     ],
     resources={
         "gcs": GCS_RESOURCE,
-        "db_bigquery": BIGQUERY_RESOURCE,
         "deanslist": DEANSLIST_RESOURCE,
+        "db_bigquery": BIGQUERY_RESOURCE,
+        "db_powerschool": DB_POWERSCHOOL,
         "ssh_couchdrop": SSH_COUCHDROP,
+        "ssh_edplan": SSH_EDPLAN,
+        "ssh_titan": SSH_TITAN,
         "io_manager": get_io_manager_gcs_pickle(CODE_LOCATION),
         "io_manager_gcs_avro": get_io_manager_gcs_avro(CODE_LOCATION),
         "io_manager_gcs_file": get_io_manager_gcs_file(CODE_LOCATION),
         "dbt_cli": get_dbt_cli_resource(CODE_LOCATION),
-        "db_powerschool": get_oracle_resource_powerschool(CODE_LOCATION_UPPER),
-        "ssh_edplan": get_ssh_resource_edplan(CODE_LOCATION_UPPER),
         "ssh_powerschool": get_ssh_resource_powerschool(
-            remote_host="pskcna.kippnj.org", code_location=CODE_LOCATION_UPPER
+            remote_host="pskcna.kippnj.org"
         ),
-        "ssh_titan": get_ssh_resource_titan(CODE_LOCATION_UPPER),
     },
 )
