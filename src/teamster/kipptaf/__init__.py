825404466bbdab22ab3a73cfe4a2d24d04943cf5
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-02-02 20:49:32 +0000

    fix: timezone

--- src/teamster/kipptaf/__init__.py
+++ src/teamster/kipptaf/__init__.py
@@ -1,11 +1,11 @@
 import pendulum
-from pendulum.tz import timezone
+from pendulum import timezone
 
 from teamster.core.utils.classes import FiscalYear
 
 CODE_LOCATION = "kipptaf"
 
-LOCAL_TIMEZONE = timezone(name="America/New_York")
+LOCAL_TIMEZONE = timezone("America/New_York")
 NOW = pendulum.now(tz=LOCAL_TIMEZONE)
 TODAY = NOW.start_of(unit="day")
 CURRENT_FISCAL_YEAR = FiscalYear(datetime=TODAY, start_month=7)
