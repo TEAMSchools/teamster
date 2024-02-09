0179a103da398e45a9f637e02ec48ecad66d1dab
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-02-09 16:53:43 +0000

    refactor: project tools

--- .gcloud/create-gke-cluster.sh
+++ .gcloud/create-gke-cluster.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
+# https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity#authenticating_to
 
-project=teamster-332318
+project_id=teamster-332318
 service_account_name=user-cloud-dagster-cloud-agent
 service_account=${service_account_name}@${project}.iam.gserviceaccount.com
 
 kubectl create namespace dagster-cloud
 
-# https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity#authenticating_to
-
 # Create an IAM service account for your application
 # or use an existing IAM service account instead.
 gcloud iam service-accounts create \
   "${service_account_name}" \
-  --project="${project}"
+  --project="${project_id}"
 
 # Ensure that your IAM service account has the roles you need.
 # You can grant additional roles using the following command:
 gcloud projects add-iam-policy-binding \
-  "${project}" \
+  "${project_id}" \
   --role "roles/storage.admin" \
   --member "serviceAccount:${service_account}"
 
@@ -27,13 +26,41 @@ gcloud projects add-iam-policy-binding \
 gcloud iam service-accounts add-iam-policy-binding \
   "${service_account}" \
   --role roles/iam.workloadIdentityUser \
-  --member "serviceAccount:${project}.svc.id.goog[dagster-cloud/${service_account_name}]"
+  --member "serviceAccount:${project_id}.svc.id.goog[dagster-cloud/${service_account_name}]"
+
+# set up Workload Identity Federation for GitHub actions
+# create WI pool
+gcloud iam workload-identity-pools create \
+  "github-pool" \
+  --project="teamster-332318" \
+  --location="global" \
+  --display-name="GitHub Pool"
+
+# create WI provider for pool
+gcloud iam workload-identity-pools providers create-oidc \
+  "github-provider" \
+  --project="${project_id}" \
+  --location="global" \
+  --display-name="GitHub Provider" \
+  --workload-identity-pool="github-pool" \
+  --issuer-uri="https://token.actions.githubusercontent.com" \
+  --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository"
+
+# bind service account to WI pool
+gh_org_name=$(gh repo view --json owner --jq '.owner.login')
+
+gcloud iam service-accounts add-iam-policy-binding \
+  "${service_account}" \
+  --project="${project_id}" \
+  --role="roles/iam.workloadIdentityUser" \
+  --member="principalSet://iam.googleapis.com/projects/624231820004/locations/global/workloadIdentityPools/github-pool/attribute.repository/${gh_org_name}/teamster"
 
 # Annotate the Kubernetes service account
 # with the email address of the IAM service account.
 kubectl annotate serviceaccount \
   "${service_account}" \
-  --namespace dagster-cloud "iam.gke.io/gcp-service-account=${service_account}"
+  --namespace=dagster-cloud \
+  "iam.gke.io/gcp-service-account=${service_account}"
 
 kubectl create secret generic \
   dagster-cloud-agent-token \
