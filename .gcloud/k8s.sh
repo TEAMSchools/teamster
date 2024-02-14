2f986f89f24494b5603fb8ad94e69afb71b3fe72
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-02-14 21:46:18 +0000

    build: add-iam-policy-binding

--- .gcloud/k8s.sh
+++ .gcloud/k8s.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity#authenticating_to
 
-gh_org_name=TEAMSchools
 project_id=teamster-332318
 service_account_name=user-cloud-dagster-cloud-agent
 service_account=${service_account_name}@${project}.iam.gserviceaccount.com
@@ -28,37 +27,6 @@ gcloud iam service-accounts add-iam-policy-binding \
   "${service_account}" \
   --role roles/iam.workloadIdentityUser \
   --member "serviceAccount:${project_id}.svc.id.goog[dagster-cloud/${service_account_name}]"
-
-# set up Workload Identity Federation for GitHub actions
-# create WI pool
-gcloud iam workload-identity-pools create \
-  "github" \
-  --project="${project_id}" \
-  --location="global" \
-  --display-name="GitHub Actions Pool"
-
-gcloud iam workload-identity-pools describe \
-  "github" \
-  --project="${project_id}" \
-  --location="global" \
-  --format="value(name)"
-
-# create WI provider for pool
-gcloud iam workload-identity-pools providers create-oidc \
-  "teamster" \
-  --project="${project_id}" \
-  --location="global" \
-  --display-name="TEAMster Provider" \
-  --workload-identity-pool="github" \
-  --issuer-uri="https://token.actions.githubusercontent.com" \
-  --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository,attribute.repository_owner=assertion.repository_owner" \
-  --attribute-condition="assertion.repository_owner == '${gh_org_name}'"
-
-gcloud iam workload-identity-pools providers describe "teamster" \
-  --project="${project_id}" \
-  --location="global" \
-  --workload-identity-pool="github" \
-  --format="value(name)"
 
 # Annotate the Kubernetes service account
 # with the email address of the IAM service account.
