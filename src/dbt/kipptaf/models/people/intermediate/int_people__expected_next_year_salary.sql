67cdfb836655400c997907b0454749b621e7d52b
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2024-02-16 19:01:37 +0000

    cleaning up a bit

--- src/dbt/kipptaf/models/people/intermediate/int_people__expected_next_year_salary.sql
+++ src/dbt/kipptaf/models/people/intermediate/int_people__expected_next_year_salary.sql
@@ -1,67 +1,24 @@
-with
-    step_scales as (
-        select
-            sr.employee_number,
-            sr.business_unit_assigned_code,
-            sr.job_title,
-            pss.salary_rule,
-            pss.scale_cy_salary,
-            pss.scale_ny_salary,
-            pss.scale_step,
-            pss.school_level,
-        from {{ ref("base_people__staff_roster") }} as sr
-        left join
-            {{ source("people", "src_people__salary_scale") }} as pss
-            on sr.job_title = pss.job_title
-            and sr.business_unit_assigned_name = pss.region
-            and sr.base_remuneration_annual_rate_amount_amount_value + 150
-            >= pss.scale_cy_salary
-            and sr.base_remuneration_annual_rate_amount_amount_value + 150
-            < pss.scale_ny_salary
-            and pss.school_level is null
-        where
-            sr.assignment_status not in ('Terminated', 'Deceased')
-            and pss.school_level is null
-
-        union all
-
-        select
-            sr.employee_number,
-            sr.business_unit_assigned_code,
-            sr.job_title,
-            pss.salary_rule,
-            pss.scale_cy_salary,
-            pss.scale_ny_salary,
-            pss.scale_step,
-            pss.school_level,
-        from {{ ref("base_people__staff_roster") }} as sr
-        left join
-            {{ source("people", "src_people__salary_scale") }} as pss
-            on sr.job_title = pss.job_title
-            and sr.business_unit_assigned_name = pss.region
-            and sr.base_remuneration_annual_rate_amount_amount_value + 150
-            >= pss.scale_cy_salary
-            and sr.base_remuneration_annual_rate_amount_amount_value + 150
-            < pss.scale_ny_salary
-            and pss.school_level = sr.home_work_location_grade_band
-        where
-            sr.assignment_status not in ('Terminated', 'Deceased')
-            and pss.school_level is not null
-            and sr.home_work_location_grade_band is not null
-
-    )
-
 select
-    employee_number,
-    job_title,
-    scale_cy_salary,
-    scale_ny_salary,
-    scale_step,
+    sr.employee_number,
+    sr.job_title,
+    sr.base_remuneration_annual_rate_amount_amount_value,
+    pss.scale_cy_salary,
+    pss.scale_ny_salary,
+    pss.scale_step,
     case
-        when salary_rule is not null
-        then salary_rule
-        when job_title in ('Teacher', 'Teacher ESL', 'Learrning Specialist')
-        then concat('Teacher PM - ', business_unit_assigned_code)
+        when pss.salary_rule is not null
+        then pss.salary_rule
+        when sr.job_title in ('Teacher', 'Teacher ESL', 'Learrning Specialist')
+        then concat('Teacher PM - ', sr.business_unit_assigned_code)
         else 'Annual Adjustment'
     end as salary_rule,
-from step_scales
+from {{ ref("base_people__staff_roster") }} as sr
+left join
+    {{ ref("stg_people__salary_scale") }} as pss
+    on sr.job_title = pss.job_title
+    and sr.business_unit_assigned_name = pss.region
+    and sr.home_work_location_grade_band
+    = coalesce(pss.school_level, sr.home_work_location_grade_band)
+    and (sr.base_remuneration_annual_rate_amount_amount_value + 150)
+    between pss.scale_cy_salary and (pss.scale_ny_salary + 0.01)
+where sr.assignment_status not in ('Terminated', 'Deceased')
