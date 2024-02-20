235d07e23bf594220964482ef4f24dfc3e865b08
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2024-02-20 20:36:16 +0000

    setting up view for gsheet

---
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__intact_integration_file.sql
@@ -0,0 +1,54 @@
+select
+    gl.journal,
+    gl.date,
+    -- trunk-ignore-begin(sqlfluff)
+    concat(
+        'adp_payroll_',
+        extract(year from gl.date),
+        case
+            when safe_cast(extract(month from gl.date) as int) < 10 then '0' else ''
+        end,
+        extract(month from gl.date),
+        case when safe_cast(extract(day from gl.date) as int) < 10 then '0' else '' end,
+        extract(day from gl.date),
+        '_',
+        gl.group_code
+    ) as description,
+    -- trunk-ignore-end(sqlfluff)
+    gl.reference_no,
+    gl.state,
+    gl.source_entity,
+    gl.line_no,
+    gl.document,
+    gl.acct_no,
+    gl.debit,
+    gl.credit,
+    gl.memo,
+    gl.location_id,
+    gl.dept_id,
+    gl.gl_entry_class_id,
+    -- trunk-ignore-begin(sqlfluff)
+    coalesce(cm.project_id, gl.gl_entry_project_id) as project_id_corrected,
+    -- trunk-ignore-end(sqlfluff)
+    gl.gl_dim_function,
+    gl.gl_dim_donor_restriction,
+    srh.employee_number,
+    srh.assignment_status as status,
+    srh.worker_original_hire_date as hire_date,
+    srh.work_assignment_actual_start_date as effective_position_start_date,
+    srh.worker_termination_date as termination_date,
+    srh.department_home_name as department,
+    srh.home_work_location_name as location,
+    srh.job_title,
+    srh.preferred_name_lastfirst as preferred_firstlast,
+    srh.legal_name_formatted_name as legal_firstlast,
+from {{ ref("stg_adp_payroll__general_ledger_file") }} as gl
+left join
+    {{ ref("stg_finance__payroll_code_mapping") }} as cm
+    on gl.gl_entry_project_id = cm.old_project_id_alt_nj
+left join
+    {{ ref("base_people__staff_roster_history") }} as srh
+    on gl.position_id = srh.position_id
+    and primary_indicator
+    and safe_cast(gl.date as timestamp)
+    between srh.work_assignment__fivetran_start and srh.work_assignment__fivetran_end
