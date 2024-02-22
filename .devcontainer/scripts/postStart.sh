a85f4331d8837648c76a1f696c84135aa444e6c0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-02-22 20:34:30 +0000

    feat: tableau sensor

--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# inject 1Password secrets into .env
+op inject -f --in-file=.devcontainer/tpl/.env.tpl --out-file=env/.env
+
 # update apt packages
 sudo apt-get -y --no-install-recommends update &&
   sudo apt-get -y --no-install-recommends upgrade &&
