5aebd050ae4e46350d3e310a795e55f14cb0a0d4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-02-26 21:36:21 +0000

    fix: io mgr

--- src/teamster/staging/definitions.py
+++ src/teamster/staging/definitions.py
@@ -1,5 +1,6 @@
 from dagster import Definitions, load_assets_from_modules
 
+from teamster.core.resources import get_io_manager_gcs_avro
 from teamster.kipptaf import tableau
 
 from . import assets, resources
@@ -12,6 +13,7 @@ defs = Definitions(
         ]
     ),
     resources={
+        "io_manager_gcs_avro": get_io_manager_gcs_avro("staging"),
         "tableau": resources.TABLEAU_SERVER_RESOURCE,
     },
 )
