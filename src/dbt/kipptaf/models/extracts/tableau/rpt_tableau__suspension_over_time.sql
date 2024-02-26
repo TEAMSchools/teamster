e26ae04afe12e7bff8e617992148bb3c9e8b714d
Anthony Walters <awalters@kippteamandfamily.org>
2024-02-26 19:00:01 +0000

    add total days suspended running

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__suspension_over_time.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__suspension_over_time.sql
@@ -45,6 +45,21 @@ with
             and p.is_suspension
         inner join suspension_type as s on p.penalty_name = s.penalty_name
         group by i._dbt_source_relation, i.student_school_id, i.create_ts_academic_year
+    ),
+
+    susp_days as (
+        select
+            i.create_ts_academic_year as academic_year,
+            i.student_school_id as student_number,
+            p.end_date,
+            p.num_days,
+        from {{ ref("stg_deanslist__incidents") }} as i
+        inner join
+            {{ ref("stg_deanslist__incidents__penalties") }} as p
+            on i.incident_id = p.incident_id
+            and {{ union_dataset_join_clause(left_alias="i", right_alias="p") }}
+            and p.is_suspension
+        inner join suspension_type as s on p.penalty_name = s.penalty_name
     )
 
 select
@@ -77,6 +92,13 @@ select
     if(date_day >= s.first_suspension_date, 1, 0) as is_susp_running,
     if(date_day >= s.first_suspension_date_iss, 1, 0) as is_iss_running,
     if(date_day >= s.first_suspension_date_oss, 1, 0) as is_oss_running,
+
+    sum(sd.num_days) over (
+        partition by co.student_number, co.academic_year
+        order by date_day
+        rows between unbounded preceding and current row
+    ) as total_suspended_days
+
 from {{ ref("base_powerschool__student_enrollments") }} as co
 inner join
     unnest(
@@ -91,6 +113,11 @@ left join
     on co.student_number = s.student_number
     and co.academic_year = s.academic_year
     and {{ union_dataset_join_clause(left_alias="co", right_alias="s") }}
+left join
+    susp_days as sd
+    on co.student_number = sd.student_number
+    and co.academic_year = sd.academic_year
+    and date_day = sd.end_date
 where
     co.rn_year = 1
     and co.academic_year >= {{ var("current_academic_year") }} - 1
