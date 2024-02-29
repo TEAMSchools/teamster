d1233817c389b716404c3bee75c1639a28e5d156
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-02-29 19:01:49 +0000

    feat(dbt): adp models

---
+++ src/dbt/kipptaf/models/adp_workforce_manager/staging/stg_adp_workforce_now__workers__work_assignments.sql
@@ -0,0 +1,47 @@
+select
+    w._dagster_partition_date as as_of_date,
+    w.associateoid as associate_oid,
+
+    /* workAssignments */
+    wa.itemid as item_id,
+
+    /* workAssignments.baseRemuneration */
+    wa.baseremuneration.effectivedate as base_remuneration__effective_date,
+
+    wa.baseremuneration.annualrateamount.amountvalue
+    as base_remuneration__annual_rate_amount__amount_value,
+    wa.baseremuneration.annualrateamount.currencycode
+    as base_remuneration__annual_rate_amount__currency_code,
+    wa.baseremuneration.annualrateamount.namecode.codevalue
+    as base_remuneration__annual_rate_amount__name_code__code_value,
+    wa.baseremuneration.annualrateamount.namecode.shortname
+    as base_remuneration__annual_rate_amount__name_code__short_name,
+
+    wa.baseremuneration.dailyrateamount.amountvalue
+    as base_remuneration__daily_rate_amount__amount_value,
+    wa.baseremuneration.dailyrateamount.currencycode
+    as base_remuneration__daily_rate_amount__currency_code,
+    wa.baseremuneration.dailyrateamount.namecode.codevalue
+    as base_remuneration__daily_rate_amount__name_code__code_value,
+    wa.baseremuneration.dailyrateamount.namecode.shortname
+    as base_remuneration__daily_rate_amount__name_code__short_name,
+
+    wa.baseremuneration.hourlyrateamount.amountvalue
+    as base_remuneration__hourly_rate_amount__amount_value,
+    wa.baseremuneration.hourlyrateamount.currencycode
+    as base_remuneration__hourly_rate_amount__currency_code,
+    wa.baseremuneration.hourlyrateamount.namecode.codevalue
+    as base_remuneration__hourly_rate_amount__name_code__code_value,
+    wa.baseremuneration.hourlyrateamount.namecode.shortname
+    as base_remuneration__hourly_rate_amount__name_code__short_name,
+
+    wa.baseremuneration.payperiodrateamount.amountvalue
+    as base_remuneration__pay_period_rate_amount__amount_value,
+    wa.baseremuneration.payperiodrateamount.currencycode
+    as base_remuneration__pay_period_rate_amount__currency_code,
+    wa.baseremuneration.payperiodrateamount.namecode.codevalue
+    as base_remuneration__pay_period_rate_amount__name_code__code_value,
+    wa.baseremuneration.payperiodrateamount.namecode.shortname
+    as base_remuneration__pay_period_rate_amount__name_code__short_name,
+from {{ source("adp_workforce_now", "src_adp_workforce_now__workers") }} as w
+cross join unnest(w.workassignments) as wa
