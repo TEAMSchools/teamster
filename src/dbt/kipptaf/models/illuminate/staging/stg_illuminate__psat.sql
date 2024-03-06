01371005572d08a202401a24b08cd01b2222a55d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-03-06 16:22:33 +0000

    feat: psat models

---
+++ src/dbt/kipptaf/models/illuminate/staging/stg_illuminate__psat.sql
@@ -0,0 +1,122 @@
+select
+    student_id,
+
+    psat_2023_cbstudentid as cb_student_id,
+    psat_2023_districtstudentid as district_student_id,
+    psat_2023_localstudentid as local_student_id,
+    psat_2023_statestudentid as state_student_id,
+    psat_2023_newdistrictid as new_district_id,
+    psat_2023_newssid as new_ssid,
+
+    psat_2023_id as id,
+    psat_2023_formcode as form_code,
+    psat_2023_gradeassessed as grade_assessed,
+
+    psat_2023_testdate as test_date,
+    psat_2023_reportdate as report_date,
+    psat_2023_selfassessmentdate as self_assessment_date,
+
+    psat_2023_firstchoicemajor as first_choice_major,
+    psat_2023_gpa as gpa,
+    psat_2023_latestpsataccesscode as latest_psat_access_code,
+    psat_2023_notvalidnationalmerit as not_valid_national_merit,
+    psat_2023_numyearsgrade912 as num_years_grade_9_12,
+    psat_2023_selectionindex as selection_index,
+
+    psat_2023_studentfirstname as student_first_name,
+    psat_2023_studentmiddleinitial as student_middle_initial,
+    psat_2023_studentlastname as student_last_name,
+    psat_2023_cohortyear as cohort_year,
+    psat_2023_birthdate as birth_date,
+    psat_2023_hsstudent as hs_student,
+
+    psat_2023_totalscore as total_score,
+    psat_2023_totalreppercentile as total_rep_percentile,
+    psat_2023_totalusrpercentile as total_usr_percentile,
+
+    psat_2023_advancedmathsubscore as advanced_math_subscore,
+    psat_2023_advancedmathreppercentile as advanced_math_rep_percentile,
+    psat_2023_advancedmathusrpercentile as advanced_math_usr_percentile,
+
+    psat_2023_algebrareppercentile as algebra_rep_percentile,
+    psat_2023_algebrausrpercentile as algebra_usr_percentile,
+
+    psat_2023_commandevidencesubscore as command_evidence_subscore,
+    psat_2023_commandreppercentile as command_rep_percentile,
+    psat_2023_commandusrpercentile as command_usr_percentile,
+
+    psat_2023_ebreadwritesectionscore as eb_read_write_section_score,
+    psat_2023_ebrwreppercentile as ebrw_rep_percentile,
+    psat_2023_ebrwusrpercentile as ebrw_usr_percentile,
+    psat_2023_ebrwccrbenchmark as ebrw_ccr_benchmark,
+
+    psat_2023_englishconvsubscore as english_conv_subscore,
+    psat_2023_engconventionsreppercentile as eng_conventions_rep_percentile,
+    psat_2023_engconventionsusrpercentile as eng_conventions_usr_percentile,
+
+    psat_2023_expressionideassubscore as expression_ideas_subscore,
+    psat_2023_expressionideareppercentile as expression_idea_rep_percentile,
+    psat_2023_expressionideausrpercentile as expression_idea_usr_percentile,
+
+    psat_2023_heartalgebrasubscore as heart_algebra_subscore,
+
+    psat_2023_historycrosstestscore as history_cross_test_score,
+    psat_2023_histreppercentile as hist_rep_percentile,
+    psat_2023_histusrpercentile as hist_usr_percentile,
+
+    psat_2023_mathtestscore as math_test_score,
+    psat_2023_mathreppercentile as math_rep_percentile,
+    psat_2023_mathusrpercentile as math_usr_percentile,
+    psat_2023_mathccrbenchmark as math_ccr_benchmark,
+
+    psat_2023_mathsectionscore as math_section_score,
+    psat_2023_mathsectreppercentile as math_sect_rep_percentile,
+    psat_2023_mathsectusrpercentile as math_sect_usr_percentile,
+
+    psat_2023_probsolvedatasubscore as prob_solve_data_subscore,
+    psat_2023_problemsolvereppercentile as problem_solve_rep_percentile,
+    psat_2023_problemsolveusrpercentile as problem_solve_usr_percentile,
+
+    psat_2023_readingtestscore as reading_test_score,
+    psat_2023_readingreppercentile as reading_rep_percentile,
+    psat_2023_readingusrpercentile as reading_usr_percentile,
+
+    psat_2023_relevantwordssubscore as relevant_words_subscore,
+
+    psat_2023_sciencecrosstestscore as science_cross_test_score,
+    psat_2023_scireppercentile as sci_rep_percentile,
+    psat_2023_sciusrpercentile as sci_usr_percentile,
+
+    psat_2023_wordscontextreppercentile as words_context_rep_percentile,
+    psat_2023_wordscontextusrpercentile as words_context_usr_percentile,
+
+    psat_2023_writingtestscore as writing_test_score,
+    psat_2023_writinglangreppercentile as writing_lang_rep_percentile,
+    psat_2023_writinglangusrpercentile as writing_lang_usr_percentile,
+
+    psat_2023_aparthist as ap_arthist,
+    psat_2023_apbio as ap_bio,
+    psat_2023_apcalc as ap_calc,
+    psat_2023_apchem as ap_chem,
+    psat_2023_apcompgovpol as ap_compgovpol,
+    psat_2023_apcompsci as ap_compsci,
+    psat_2023_apenglang as ap_englang,
+    psat_2023_apenglit as ap_englit,
+    psat_2023_apenvsci as ap_envsci,
+    psat_2023_apeurohist as ap_eurohist,
+    psat_2023_aphumgeo as ap_humgeo,
+    psat_2023_apmacecon as ap_macecon,
+    psat_2023_apmicecon as ap_micecon,
+    psat_2023_apmusic as ap_music,
+    psat_2023_apphysi as ap_physi,
+    psat_2023_apphysii as ap_physii,
+    psat_2023_apphysmag as ap_physmag,
+    psat_2023_apphysmech as ap_physmech,
+    psat_2023_appsych as ap_psych,
+    psat_2023_apseminar as ap_seminar,
+    psat_2023_apstat as ap_stat,
+    psat_2023_apusgovpol as ap_usgovpol,
+    psat_2023_apushist as ap_ushist,
+    psat_2023_apworldhist as ap_worldhist,
+from {{ source("illuminate", "psat_2023") }}
+where not _fivetran_deleted
