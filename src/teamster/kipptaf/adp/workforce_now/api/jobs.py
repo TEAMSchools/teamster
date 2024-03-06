b8119a258340d06d7372622c6df12dc3688a5dde
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-03-06 20:22:14 +0000

    feat: schedule

--- src/teamster/kipptaf/adp/workforce_now/api/jobs.py
+++ src/teamster/kipptaf/adp/workforce_now/api/jobs.py
@@ -1,6 +1,8 @@
-from dagster import RunConfig, job
+from dagster import RunConfig, define_asset_job, job
 
+from .... import CODE_LOCATION
 from ....google.bigquery.ops import BigQueryGetTableOpConfig, bigquery_get_table_op
+from .assets import workers
 from .ops import adp_wfn_update_workers_op
 
 
@@ -21,6 +23,11 @@ def adp_wfn_update_workers_job():
     adp_wfn_update_workers_op(worker_data)
 
 
+adp_wfn_api_workers_asset_job = define_asset_job(
+    name=f"{CODE_LOCATION}_adp_workforce_now_api_workers_asset_job", selection=[workers]
+)
+
 _all = [
     adp_wfn_update_workers_job,
+    adp_wfn_api_workers_asset_job,
 ]
