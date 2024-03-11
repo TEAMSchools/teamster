6b5c5e059463b6daccf4367eea73233c6a16461c
Anthony Walters <awalters@kippteamandfamily.org>
2024-03-11 02:44:21 +0000

    migrate home instruction queue to BQ/dbt

---
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__home_instruction_queue.sql
@@ -0,0 +1,57 @@
+select
+    co.student_number,
+    co.state_studentnumber,
+    co.lastfirst,
+    co.academic_year,
+    co.school_abbreviation,
+    co.grade_level,
+    co.advisory_name as team,
+    co.advisor_lastfirst as advisor_name,
+    co.spedlep as iep_status,
+    co.gender,
+    co.ethnicity,
+    co.region,
+    dli.student_id as dl_student_id,
+    dli.incident_id as dl_id,
+    dli.status,
+    dli.reported_details,
+    dli.admin_summary,
+    dli.context,
+    concat(dli.create_first, ' ', dli.create_last) as referring_teacher_name,
+    concat(dli.update_first, ' ', dli.update_last) as reviewed_by,
+    dli.create_ts_date as dl_timestamp,
+    dli.infraction,
+    coalesce(dli.category, 'Referral') as dl_behavior,
+    cast(d.name as string) as term,
+    cf.final_approval as `Final Approval`,
+    concat(u.first_name, ' ', u.last_name) as `Approver Name`,
+    cf.instructor_source as `Instructor Source`,
+    cf.instructor_name as `Instructor Name`,
+    cf.hours_per_week as `Hours per week`,
+    cf.hourly_rate as `Hourly rate`,
+    cf.board_approval_date as `Board Approval Date`,
+    cf.hi_start_date as `HI start date`,
+    cf.hi_end_date as `HI end date`
+from {{ ref("stg_deanslist__incidents") }} as dli
+left join
+    {{ ref("int_deanslist__incidents__custom_fields__pivot") }} as cf
+    on dli.incident_id = cf.incident_id
+    and {{ union_dataset_join_clause(left_alias="dli", right_alias="cf") }}
+left join
+    {{ ref("stg_deanslist__users") }} as u
+    on cast(u.dl_user_id as string) = cf.approver_name
+    and {{ union_dataset_join_clause(left_alias="u", right_alias="cf") }}
+inner join
+    {{ ref("base_powerschool__student_enrollments") }} as co
+    on dli.student_school_id = co.student_number
+    and dli.create_ts_academic_year = co.academic_year
+    and {{ union_dataset_join_clause(left_alias="dli", right_alias="co") }}
+    and co.rn_year = 1
+inner join
+    {{ ref("stg_reporting__terms") }} as d
+    on co.schoolid = d.school_id
+    and (cast(dli.create_ts_date as date) between d.start_date and d.end_date)
+    and d.type = 'RT'
+where
+    dli.create_ts_academic_year = {{ var("current_academic_year") }}
+    and (dli.category = 'TX - HI Request (admin only)' or cf.hi_start_date is not null)
