190232081fbf18faf25e1fe4f844ab7144776ab3
Gaby Rangel <grangel@kippteamandfamily.org>
2024-03-13 21:00:02 +0000

    trying to fix sqlfluff(ST06)

--- src/dbt/kipptaf/models/illuminate/staging/stg_illuminate__psat.sql
+++ src/dbt/kipptaf/models/illuminate/staging/stg_illuminate__psat.sql
@@ -23,77 +23,76 @@ select
     psat_2023_birthdate as birth_date,
     psat_2023_hsstudent as hs_student,
 
-    safe_cast(psat_2023_totalscore as float64) as total_score,
     psat_2023_totalreppercentile as total_rep_percentile,
     psat_2023_totalusrpercentile as total_usr_percentile,
 
-    safe_cast(psat_2023_advancedmathsubscore as float64) as advanced_math_subscore,
     psat_2023_advancedmathreppercentile as advanced_math_rep_percentile,
     psat_2023_advancedmathusrpercentile as advanced_math_usr_percentile,
 
     psat_2023_algebrareppercentile as algebra_rep_percentile,
     psat_2023_algebrausrpercentile as algebra_usr_percentile,
 
-    safe_cast(
-        psat_2023_commandevidencesubscore as float64
-    ) as command_evidence_subscore,
     psat_2023_commandreppercentile as command_rep_percentile,
     psat_2023_commandusrpercentile as command_usr_percentile,
 
-    safe_cast(
-        psat_2023_ebreadwritesectionscore as float64
-    ) as eb_read_write_section_score,
     psat_2023_ebrwreppercentile as ebrw_rep_percentile,
     psat_2023_ebrwusrpercentile as ebrw_usr_percentile,
     psat_2023_ebrwccrbenchmark as ebrw_ccr_benchmark,
 
-    safe_cast(psat_2023_englishconvsubscore as float64) as english_conv_subscore,
     psat_2023_engconventionsreppercentile as eng_conventions_rep_percentile,
     psat_2023_engconventionsusrpercentile as eng_conventions_usr_percentile,
 
-    safe_cast(
-        psat_2023_expressionideassubscore as float64
-    ) as expression_ideas_subscore,
     psat_2023_expressionideareppercentile as expression_idea_rep_percentile,
     psat_2023_expressionideausrpercentile as expression_idea_usr_percentile,
 
-    safe_cast(psat_2023_heartalgebrasubscore as float64) as heart_algebra_subscore,
-
-    safe_cast(psat_2023_historycrosstestscore as float64) as history_cross_test_score,
     psat_2023_histreppercentile as hist_rep_percentile,
     psat_2023_histusrpercentile as hist_usr_percentile,
 
-    safe_cast(psat_2023_mathtestscore as float64) as math_test_score,
     psat_2023_mathreppercentile as math_rep_percentile,
     psat_2023_mathusrpercentile as math_usr_percentile,
     psat_2023_mathccrbenchmark as math_ccr_benchmark,
 
-    safe_cast(psat_2023_mathsectionscore as float64) as math_section_score,
     psat_2023_mathsectreppercentile as math_sect_rep_percentile,
     psat_2023_mathsectusrpercentile as math_sect_usr_percentile,
 
-    safe_cast(psat_2023_probsolvedatasubscore as float64) as prob_solve_data_subscore,
     psat_2023_problemsolvereppercentile as problem_solve_rep_percentile,
     psat_2023_problemsolveusrpercentile as problem_solve_usr_percentile,
 
-    safe_cast(psat_2023_readingtestscore as float64) as reading_test_score,
     psat_2023_readingreppercentile as reading_rep_percentile,
     psat_2023_readingusrpercentile as reading_usr_percentile,
 
-    safe_cast(psat_2023_relevantwordssubscore as float64) as relevant_words_subscore,
-
-    safe_cast(psat_2023_sciencecrosstestscore as float64) as science_cross_test_score,
     psat_2023_scireppercentile as sci_rep_percentile,
     psat_2023_sciusrpercentile as sci_usr_percentile,
 
     psat_2023_wordscontextreppercentile as words_context_rep_percentile,
     psat_2023_wordscontextusrpercentile as words_context_usr_percentile,
 
-    safe_cast(psat_2023_writingtestscore as float64) as writing_test_score,
     psat_2023_writinglangreppercentile as writing_lang_rep_percentile,
     psat_2023_writinglangusrpercentile as writing_lang_usr_percentile,
 
     psat_2023_apseminar as ap_seminar,
+
+    safe_cast(psat_2023_totalscore as float64) as total_score,
+    safe_cast(psat_2023_advancedmathsubscore as float64) as advanced_math_subscore,
+    safe_cast(
+        psat_2023_commandevidencesubscore as float64
+    ) as command_evidence_subscore,
+    safe_cast(
+        psat_2023_ebreadwritesectionscore as float64
+    ) as eb_read_write_section_score,
+    safe_cast(psat_2023_englishconvsubscore as float64) as english_conv_subscore,
+    safe_cast(
+        psat_2023_expressionideassubscore as float64
+    ) as expression_ideas_subscore,
+    safe_cast(psat_2023_heartalgebrasubscore as float64) as heart_algebra_subscore,
+    safe_cast(psat_2023_historycrosstestscore as float64) as history_cross_test_score,
+    safe_cast(psat_2023_mathtestscore as float64) as math_test_score,
+    safe_cast(psat_2023_mathsectionscore as float64) as math_section_score,
+    safe_cast(psat_2023_probsolvedatasubscore as float64) as prob_solve_data_subscore,
+    safe_cast(psat_2023_readingtestscore as float64) as reading_test_score,
+    safe_cast(psat_2023_relevantwordssubscore as float64) as relevant_words_subscore,
+    safe_cast(psat_2023_sciencecrosstestscore as float64) as science_cross_test_score,
+    safe_cast(psat_2023_writingtestscore as float64) as writing_test_score,
 
     nullif(psat_2023_newdistrictid, '') as new_district_id,
     nullif(psat_2023_newssid, '') as new_ssid,
