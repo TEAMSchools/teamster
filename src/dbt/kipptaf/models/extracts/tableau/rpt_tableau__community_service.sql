522bc185d37cb5808c1515d1edac1e5c6d15f006
Anthony Walters <awalters@kippteamandfamily.org>
2024-03-16 02:06:23 +0000

    trunk, pt. 3

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__community_service.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__community_service.sql
@@ -17,7 +17,7 @@ select
     b.behavior_date,
     b.behavior,
     b.notes,
-    
+
     cast(left(b.behavior, length(b.behavior) - 5) as int64) as cs_hours,
     concat(b.staff_last_name, ', ', b.staff_first_name) as staff_name,
 from {{ ref("base_powerschool__student_enrollments") }} as co
