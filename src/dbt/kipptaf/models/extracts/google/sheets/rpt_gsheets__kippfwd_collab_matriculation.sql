3e303b30b80986f2e83e2b42cb526e7fea1ab639
Casey Gibson <cgibson@kippteamandfamily.org>
2024-03-18 14:40:09 +0000

    updates and cte add

--- src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__kippfwd_collab_matriculation.sql
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__kippfwd_collab_matriculation.sql
@@ -1,20 +1,40 @@
-select
-    kt.contact_id,
-    kt.last_name,
-    kt.first_name,
-    ap.id,
-    ap.name,
-    ap.matriculation_decision,
-    coalesce(cn.ccdm, 0) as ccdm_complete,
-    count(*) over (partition by ap.applicant order by ap.id asc) as row_count,
-    ap.type,
-    ap.intended_degree_type,
+with
+    ccdm_collab as (
+        select
+            kt.contact_id,
+            kt.last_name,
+            kt.first_name,
 
-from {{ ref("int_kippadb__roster") }} as kt
-left join {{ ref("stg_kippadb__application") }} as ap on (kt.contact_id = ap.applicant)
-left join
-    {{ ref("int_kippadb__contact_note_rollup") }} as cn
-    on (kt.contact_id = cn.contact_id)
-where
-    ap.matriculation_decision = 'Matriculated (Intent to Enroll)'
-    and cn.academic_year = 2023
+            ap.id,
+            ap.name,
+            ap.matriculation_decision,
+            ap.type,
+            ap.intended_degree_type,
+
+            coalesce(cn.ccdm, 0) as ccdm_complete,
+            row_number() over (
+                partition by ap.applicant order by ap.id asc
+            ) as rn_applicant,
+        from {{ ref("int_kippadb__roster") }} as kt
+        left join
+            {{ ref("stg_kippadb__application") }} as ap
+            on (kt.contact_id = ap.applicant)
+        left join
+            {{ ref("int_kippadb__contact_note_rollup") }} as cn
+            on (kt.contact_id = cn.contact_id)
+        where
+            ap.matriculation_decision = 'Matriculated (Intent to Enroll)'
+            and cn.academic_year = {{ var("current_academic_year") }}
+    )
+select
+    contact_id,
+    last_name,
+    first_name,
+    id,
+    name,
+    matriculation_decision,
+    type,
+    intended_degree_type,
+    ccdm_complete,
+from ccdm_collab
+where rn_applicant = 1
