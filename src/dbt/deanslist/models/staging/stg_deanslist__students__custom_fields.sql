0abeb1befcbbdd4779d3f92f06c54d1a7e0cdc40
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-03-19 20:36:07 +0000

    feat: dl students asset and dbt models

--- src/dbt/deanslist/models/staging/stg_deanslist__students__custom_fields.sql
+++ src/dbt/deanslist/models/staging/stg_deanslist__students__custom_fields.sql
@@ -1,20 +1,19 @@
 select
-    i.incident_id,
+    s.studentid as student_id,
 
     cf.numvalue as num_value,
-    cf.selectedoptions as selected_options,
 
     nullif(cf.fieldcategory, '') as field_category,
     nullif(cf.fieldkey, '') as field_key,
     nullif(cf.fieldname, '') as field_name,
     nullif(cf.fieldtype, '') as field_type,
-    nullif(cf.inputname, '') as input_name,
-    nullif(cf.sourcetype, '') as source_type,
-    nullif(cf.labelhtml, '') as label_html,
     nullif(cf.inputhtml, '') as input_html,
+    nullif(cf.inputname, '') as input_name,
+    nullif(cf.labelhtml, '') as label_html,
     nullif(cf.options, '') as `options`,
-    nullif(cf.value, '') as `value`,
+    nullif(cf.sourcetype, '') as source_type,
     nullif(cf.stringvalue, '') as string_value,
+    nullif(cf.value, '') as `value`,
 
     safe_cast(nullif(cf.customfieldid, '') as int) as custom_field_id,
     safe_cast(nullif(cf.sourceid, '') as int) as source_id,
@@ -22,6 +21,5 @@ select
 
     if(nullif(cf.isfrontend, '') = 'Y', true, false) as is_front_end,
     if(nullif(cf.isrequired, '') = 'Y', true, false) as is_required,
-from {{ ref("stg_deanslist__incidents") }} as i
-cross join unnest(i.custom_fields) as cf
-where nullif(cf.value, '') is not null
+from {{ source("deanslist", "src_deanslist__students") }} as s
+cross join unnest(s.customfields) as cf
