b1d4e96bc82b0448f9a10446d2b0a5888bb9a74b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-03-27 15:02:48 +0000

    fix: schema

--- src/dbt/kipptaf/models/adp/workforce_now/staging/stg_adp_workforce_now__additional_earnings_report.sql
+++ src/dbt/kipptaf/models/adp/workforce_now/staging/stg_adp_workforce_now__additional_earnings_report.sql
@@ -5,10 +5,22 @@
 with
     additional_earnings_report as (
         select
-            {{ dbt_utils.star(from=src_aer, except=["pay_date", "gross_pay"]) }},
+            {{
+                dbt_utils.star(
+                    from=src_aer,
+                    except=["pay_date", "gross_pay", "check_voucher_number"],
+                )
+            }},
+
             parse_date('%m/%d/%Y', pay_date) as pay_date,
-            cast(regexp_replace(gross_pay, r'[^\d\.-]', '') as numeric) as gross_pay,
             concat(payroll_company_code, file_number_pay_statements) as position_id,
+            coalesce(
+                safe_cast(check_voucher_number.long_value as string),
+                check_voucher_number.string_value
+            ) as check_voucher_number,
+            safe_cast(
+                regexp_replace(gross_pay, r'[^\d\.-]', '') as numeric
+            ) as gross_pay,
         from {{ src_aer }}
         where additional_earnings_description not in ('Sick', 'C-SICK')
     )
