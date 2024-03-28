67b112a790ef96f4cf946fab2530d6566c8a0a9d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-03-28 18:44:56 +0000

    fix dbt script

--- .pdm/scripts/dbt.sh
+++ .pdm/scripts/dbt.sh
@@ -2,18 +2,14 @@
 
 project="${1}"
 dbt_command="${2}"
-flags=${*:3}
+flags=("${@:3}")
 
 if [[ ${dbt_command} == "sxs" ]]; then
   dbt run-operation --project-dir src/dbt/"${project}" \
     stage_external_sources \
     --vars "ext_full_refresh: true" \
-    --args "select: ${flags}"
-elif [[ ${dbt_command} == "deps" ]]; then
-  dbt "${dbt_command}" --project-dir src/dbt/"${project}"
-elif [[ ${dbt_command} == "parse" ]]; then
-  dbt "${dbt_command}" --project-dir src/dbt/"${project}"
+    --args "select: ${flags[*]}"
 else
-  echo dbt "${dbt_command}" --project-dir src/dbt/"${project}" "${flags}"
-  dbt "${dbt_command}" --project-dir src/dbt/"${project}" "${flags}"
+  # trunk-ignore(shellcheck/SC2068)
+  dbt "${dbt_command}" --project-dir src/dbt/"${project}" ${flags[@]}
 fi
