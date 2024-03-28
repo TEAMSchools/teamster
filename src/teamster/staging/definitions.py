c8616665fabb5415bc4c8ae74dc3671fc4f11217
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-03-28 20:12:24 +0000

    fix: defs

--- src/teamster/staging/definitions.py
+++ src/teamster/staging/definitions.py
@@ -1,13 +1,12 @@
 from dagster import Definitions, load_assets_from_modules
 
 from teamster.core.resources import get_dbt_cli_resource, get_io_manager_gcs_avro
-
-from . import CODE_LOCATION, dbt
+from teamster.staging.dbt import assets as dbt_assets
 
 defs = Definitions(
-    assets=load_assets_from_modules(modules=[dbt]),
+    assets=load_assets_from_modules(modules=[dbt_assets]),
     resources={
         "io_manager_gcs_avro": get_io_manager_gcs_avro("staging"),
-        "dbt_cli": get_dbt_cli_resource(CODE_LOCATION),
+        "dbt_cli": get_dbt_cli_resource("staging"),
     },
 )
