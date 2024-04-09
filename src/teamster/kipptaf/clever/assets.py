3283e7b5c02155a711b620ca1321f98ee8bb205f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-04-09 20:54:36 +0000

    refactor: remove avro fn

--- src/teamster/kipptaf/clever/assets.py
+++ src/teamster/kipptaf/clever/assets.py
@@ -8,7 +8,6 @@ from dagster import (
 )
 
 from teamster.core.sftp.assets import build_sftp_asset
-from teamster.core.utils.functions import get_avro_record_schema
 
 from .. import CODE_LOCATION
 from .schema import ASSET_FIELDS
@@ -17,9 +16,7 @@ _all = [
     build_sftp_asset(
         asset_key=[CODE_LOCATION, "clever_reports", a["asset_name"]],
         ssh_resource_key="ssh_clever_reports",
-        avro_schema=get_avro_record_schema(
-            name=a["asset_name"], fields=ASSET_FIELDS[a["asset_name"]]
-        ),
+        avro_schema=ASSET_FIELDS[a["asset_name"]],
         partitions_def=MultiPartitionsDefinition(
             {
                 "date": DynamicPartitionsDefinition(
