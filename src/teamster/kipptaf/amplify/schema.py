e0ccd7f0df184f6e14e4775a0014e04810418948
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-04-16 20:18:18 +0000

    fix: pas opts

--- src/teamster/kipptaf/amplify/schema.py
+++ src/teamster/kipptaf/amplify/schema.py
@@ -138,17 +138,15 @@ class pm_student_summary_record(PMStudentSummary):
     """helper classes for backwards compatibility"""
 
 
+pas_options = py_avro_schema.Option.NO_DOC | py_avro_schema.Option.NO_AUTO_NAMESPACE
+
 ASSET_SCHEMA = {
     "benchmark_student_summary": json.loads(
         py_avro_schema.generate(
-            py_type=benchmark_student_summary_record,
-            namespace="benchmark_student_summary",
+            py_type=benchmark_student_summary_record, options=pas_options
         )
     ),
     "pm_student_summary": json.loads(
-        py_avro_schema.generate(
-            py_type=pm_student_summary_record,
-            namespace="pm_student_summary",
-        )
+        py_avro_schema.generate(py_type=pm_student_summary_record, options=pas_options)
     ),
 }
