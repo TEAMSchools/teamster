8a3710b418c2f49dda1b9c679f04545f6fd6e2fe
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-04-16 18:48:13 +0000

    fix: imports

--- src/teamster/kippmiami/iready/assets.py
+++ src/teamster/kippmiami/iready/assets.py
@@ -6,9 +6,8 @@ from dagster import (
     config_from_files,
 )
 
-from teamster.core.iready.schema import ASSET_FIELDS
+from teamster.core.iready.schema import ASSET_SCHEMA
 from teamster.core.sftp.assets import build_sftp_asset
-from teamster.core.utils.functions import get_avro_record_schema
 
 from .. import CODE_LOCATION
 
@@ -16,9 +15,7 @@ _all = [
     build_sftp_asset(
         asset_key=[CODE_LOCATION, "iready", a["asset_name"]],
         ssh_resource_key="ssh_iready",
-        avro_schema=get_avro_record_schema(
-            name=a["asset_name"], fields=ASSET_FIELDS[a["asset_name"]]
-        ),
+        avro_schema=ASSET_SCHEMA,
         partitions_def=MultiPartitionsDefinition(
             {
                 "subject": StaticPartitionsDefinition(["ela", "math"]),
