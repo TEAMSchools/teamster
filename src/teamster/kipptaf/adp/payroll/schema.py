8fc44471195c05aaa2af9f6d6f3a56c9309446b1
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-04-17 18:25:41 +0000

    refactor: rename schema

--- src/teamster/kipptaf/adp/payroll/schema.py
+++ src/teamster/kipptaf/adp/payroll/schema.py
@@ -37,7 +37,7 @@ class general_ledger_file_record(GeneralLedger):
     """helper classes for backwards compatibility"""
 
 
-GENERAL_LEDGER_FILE = json.loads(
+GENERAL_LEDGER_FILE_SCHEMA = json.loads(
     py_avro_schema.generate(
         py_type=general_ledger_file_record,
         options=py_avro_schema.Option.NO_DOC | py_avro_schema.Option.NO_AUTO_NAMESPACE,
