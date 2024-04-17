6130cdedae269e8fd25ef658dc1785ee96bd50c1
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-04-17 18:09:17 +0000

    rename schema var

--- src/teamster/kipptaf/achieve3k/assets.py
+++ src/teamster/kipptaf/achieve3k/assets.py
@@ -3,14 +3,14 @@ from dagster import DynamicPartitionsDefinition
 from teamster.core.sftp.assets import build_sftp_asset
 
 from .. import CODE_LOCATION
-from .schema import ASSET_SCHEMA
+from .schema import STUDENT_SCHEMA
 
 students = build_sftp_asset(
     asset_key=[CODE_LOCATION, "achieve3k", "students"],
     remote_dir="outgoing",
     remote_file_regex=r"(?P<date>\d{4}[-\d{2}]+)-\d+_D[\d+_]+(\w\d{4}[-\d{2}]+_){2}student\.\w+",
     ssh_resource_key="ssh_achieve3k",
-    avro_schema=ASSET_SCHEMA["students"],
+    avro_schema=STUDENT_SCHEMA,
     partitions_def=DynamicPartitionsDefinition(
         name=f"{CODE_LOCATION}__achieve3k__students"
     ),
