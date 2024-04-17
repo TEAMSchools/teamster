ab354be10e2de95becbb808d2d003b07425602eb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-04-17 18:23:47 +0000

    refactor: rename schema

--- src/teamster/kipptaf/adp/payroll/assets.py
+++ src/teamster/kipptaf/adp/payroll/assets.py
@@ -8,17 +8,16 @@ from dagster import (
 from teamster.core.sftp.assets import build_sftp_asset
 
 from ... import CODE_LOCATION
-from .schema import ASSET_SCHEMA
+from .schema import GENERAL_LEDGER_FILE
 
-asset_name = "general_ledger_file"
-asset_key = AssetKey([CODE_LOCATION, "adp", "payroll", asset_name])
+asset_key = AssetKey([CODE_LOCATION, "adp", "payroll", "general_ledger_file"])
 
 general_ledger_file = build_sftp_asset(
     asset_key=asset_key.path,
     remote_dir="/teamster-kipptaf/couchdrop/adp/payroll",
     remote_file_regex=r"adp_payroll_(?P<date>\d+)_(?P<group_code>\w+)\.csv",
     ssh_resource_key="ssh_couchdrop",
-    avro_schema=ASSET_SCHEMA[asset_name],
+    avro_schema=GENERAL_LEDGER_FILE,
     partitions_def=MultiPartitionsDefinition(
         {
             "group_code": StaticPartitionsDefinition(["2Z3", "3LE", "47S", "9AM"]),
