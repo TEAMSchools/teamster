ed06ff4db03fd857e6f4f6fbbaee22b8b2276d75
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2024-04-17 18:49:17 +0000

    lag + errors

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__finance_accounting_people_model.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__finance_accounting_people_model.sql
@@ -38,16 +38,12 @@ select
     hd.legal_last_name,
     hd.race_ethnicity_reporting as primary_race_ethnicity_reporting,
     hd.gender,
-    hd.management_position_indicator as is_manager,
+    hd.is_manager,
     hd.worker_original_hire_date as original_hire_date,
     hd.worker_rehire_date as rehire_date,
     hd.worker_termination_date as termination_date,
     hd.overall_score as most_recent_pm_score,
     hd.overall_tier as most_recent_pm_tier,
-
-    ly.business_unit_home_name as last_year_business_unit,
-    ly.job_title as last_year_job_title,
-    ly.historic_salary as last_year_salary,
 
     ye.years_at_kipp_total as years_at_kipp_total_current,
 
@@ -58,24 +54,27 @@ select
 
     ye.years_teaching_total,
 
-    lag(business_unit_home_name, 1) over (
-        partition by hd.employee_number order by hd.academic_year desc
-    ),
+    lag(hd.historic_legal_entity, 1) over (
+        partition by hd.employee_number order by hd.academic_year asc
+    ) as last_year_business_unit,
+
+    lag(hd.historic_role, 1) over (
+        partition by hd.employee_number order by hd.academic_year asc
+    ) last_year_job_title,
+    lag(hd.historic_salary, 1) over (
+        partition by hd.employee_number order by hd.academic_year asc
+    ) as last_year_salary,
 
     row_number() over (
-        partition by cw.employee_number
-        order by y.academic_year desc, eh.assignment_status asc
+        partition by hd.employee_number
+        order by hd.academic_year desc, hd.historic_position_status asc
     ) as rn_curr,
 from {{ ref("int_people__annual_historic_data") }} as hd
 left join
-    {{ ref("int_people__annual_historic_data") }} as ly
-    on hd.employee_number = ly.employee_number
-    and ly.academic_year = (hd.academic_year - 1)
-left join
     {{ ref("int_people__years_experience") }} as ye
-    on eh.employee_number = ye.employee_number
-    and y.academic_year = ye.academic_year
+    on hd.employee_number = ye.employee_number
+    and hd.academic_year = ye.academic_year
 left join
     additional_earnings as ae
     on hd.employee_number = ae.employee_number
-    and y.academic_year = ae.academic_year
+    and hd.academic_year = ae.academic_year
