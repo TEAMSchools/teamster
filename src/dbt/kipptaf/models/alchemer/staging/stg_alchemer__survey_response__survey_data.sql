9ca8520bb49367d16fc4e8177b64825c96c5f4da
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-04-19 20:55:46 +0000

    fix: alchemer models

--- src/dbt/kipptaf/models/alchemer/staging/stg_alchemer__survey_response__survey_data.sql
+++ src/dbt/kipptaf/models/alchemer/staging/stg_alchemer__survey_response__survey_data.sql
@@ -4,11 +4,11 @@ select
 
     sd.value.id as question_id,
     sd.value.answer.string_value,
-    null as map_value,
+
     null as option_value,
     null as rank_value,
 from {{ ref("stg_alchemer__survey_response") }} as sr
-cross join unnest(sr.survey_data.map_survey_data_record_value) as sd
+cross join unnest(sr.survey_data.map_union_answer_null_value) as sd
 where sd.value.answer.string_value is not null
 
 union all
@@ -18,12 +18,11 @@ select
     sr.id as survey_response_id,
 
     sd.value.id as question_id,
+
     null as string_value,
 
-    ans.value.string_value as map_value,
-    ans.value.surveyresponse_answer_record_value.option as option_value,
-    ans.value.surveyresponse_answer_record_value.`rank` as rank_value,
+    ans.value.`option` as option_value,
+    ans.value.rank as rank_value,
 from {{ ref("stg_alchemer__survey_response") }} as sr
-cross join unnest(sr.survey_data.map_survey_data_record_value) as sd
-cross join
-    unnest(sd.value.answer.map_union_string_surveyresponse_answer_record_value) as ans
+cross join unnest(sr.survey_data.map_union_answer_null_value) as sd
+cross join unnest(sd.value.answer.map_union_optionanswer_null_value) as ans
