b09d55f9fb8f5d3b52d623dcdf2b5a7948d0feba
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-05-06 23:20:43 +0000

    feat: dbt external decorator refactor: various feat: add parent asset key

--- src/teamster/kipptaf/dbt/assets.py
+++ src/teamster/kipptaf/dbt/assets.py
@@ -1,10 +1,7 @@
 import json
 import pathlib
 
-from dagster import AssetExecutionContext, Output
-from dagster_dbt import DbtCliResource
-
-from teamster.core.dbt.assets import build_dbt_assets, dbt_external_source_assets
+from teamster.core.dbt.assets import build_dbt_assets, build_dbt_external_source_assets
 from teamster.core.dbt.dagster_dbt_translator import CustomDagsterDbtTranslator
 from teamster.kipptaf import CODE_LOCATION
 from teamster.kipptaf.adp.payroll.assets import GENERAL_LEDGER_FILE_PARTITIONS_DEF
@@ -20,47 +17,8 @@ dagster_dbt_translator = CustomDagsterDbtTranslator(
 dbt_assets = build_dbt_assets(
     manifest=manifest,
     dagster_dbt_translator=dagster_dbt_translator,
-    exclude=" ".join(
-        [
-            "tag:stage_external_sources",
-            # "fqn:kipptaf.extracts.google.sheets.rpt_gsheets__intacct_integration_file",
-            # "fqn:kipptaf.adp.payroll.staging.stg_adp_payroll__general_ledger_file",
-        ]
-    ),
+    exclude="tag:stage_external_sources",
 )
-
-
-@dbt_external_source_assets(
-    manifest=manifest,
-    dagster_dbt_translator=dagster_dbt_translator,
-    select="tag:stage_external_sources",
-)
-def external_source_dbt_assets(context: AssetExecutionContext, dbt_cli: DbtCliResource):
-    source_selection = [
-        f"{context.assets_def.group_names_by_key[asset_key]}.{asset_key.path[-1]}"
-        for asset_key in context.selected_asset_keys
-    ]
-
-    # run dbt stage_external_sources
-    dbt_run_operation = dbt_cli.cli(
-        args=[
-            "run-operation",
-            "stage_external_sources",
-            "--args",
-            json.dumps({"select": " ".join(source_selection)}),
-            "--vars",
-            json.dumps({"ext_full_refresh": True}),
-        ],
-        manifest=manifest,
-        dagster_dbt_translator=dagster_dbt_translator,
-    )
-
-    for event in dbt_run_operation.stream_raw_events():
-        context.log.info(event)
-
-    for output_name in context.selected_output_names:
-        yield Output(value=None, output_name=output_name)
-
 
 adp_payroll_dbt_assets = build_dbt_assets(
     manifest=manifest,
@@ -74,11 +32,12 @@ adp_payroll_dbt_assets = build_dbt_assets(
     ),
 )
 
-# external_source_dbt_assets = build_dbt_external_source_assets(
-#     code_location=CODE_LOCATION,
-#     manifest=manifest,
-#     dagster_dbt_translator=dagster_dbt_translator,
-# )
+external_source_dbt_assets = build_dbt_external_source_assets(
+    manifest=manifest,
+    dagster_dbt_translator=dagster_dbt_translator,
+    select="tag:stage_external_sources",
+)
+
 
 assets = [
     dbt_assets,
