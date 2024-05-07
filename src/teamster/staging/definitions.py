3801caab9787c24e5e170e02778f9ede5fd4d6e0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-05-07 20:04:49 +0000

    fix: defs

--- src/teamster/staging/definitions.py
+++ src/teamster/staging/definitions.py
@@ -1,32 +1,16 @@
-from dagster import Definitions, EnvVar, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules
 
-from teamster.core.resources import get_io_manager_gcs_avro
-from teamster.core.ssh.resources import SSHResource
-from teamster.kipptaf import ldap
-from teamster.kipptaf.adp.workforce_now import sftp as adp_wfn_sftp
-from teamster.kipptaf.ldap.resources import LdapResource
+from teamster.core.resources import get_dbt_cli_resource, get_io_manager_gcs_avro
+from teamster.staging import CODE_LOCATION, dbt
 
 defs = Definitions(
     assets=load_assets_from_modules(
         modules=[
-            adp_wfn_sftp,
-            ldap,
+            dbt,
         ]
     ),
     resources={
         "io_manager_gcs_avro": get_io_manager_gcs_avro("staging"),
-        "ldap": LdapResource(
-            # host="ldap1.kippnj.org",
-            host="204.8.89.213",
-            port=636,
-            user=EnvVar("LDAP_USER"),
-            password=EnvVar("LDAP_PASSWORD"),
-        ),
-        "ssh_adp_workforce_now": SSHResource(
-            # remote_host="sftp.kippnj.org",
-            remote_host="204.8.89.221",
-            username=EnvVar("ADP_SFTP_USERNAME"),
-            password=EnvVar("ADP_SFTP_PASSWORD"),
-        ),
+        "dbt_cli": get_dbt_cli_resource(CODE_LOCATION),
     },
 )
