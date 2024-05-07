3bc0170318ee02432b02e1ca76c0a7fb84b25ceb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-05-07 22:16:15 +0000

    fix: parent asset key

--- .pdm/scripts/validate.sh
+++ .pdm/scripts/validate.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 if [[ -z ${1} ]]; then
-  echo "Code Location is required"
-  exit 1
+  dagster dev \
+    -m teamster.kippcamden.definitions \
+    -m teamster.kippmiami.definitions \
+    -m teamster.kippnewark.definitions \
+    -m teamster.kipptaf.definitions
 else
   dagster dev -m teamster."${1}".definitions
 fi
