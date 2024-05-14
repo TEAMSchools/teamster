72b9fae7fd701b500a6eaa96b898a32df79eb399
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-05-14 15:22:51 +0000

    reverting bad commit

--- src/dbt/kipptaf/models/alchemer/staging/stg_alchemer__survey_response.sql
+++ src/dbt/kipptaf/models/alchemer/staging/stg_alchemer__survey_response.sql
@@ -1,3 +1,5 @@
+{%- set src_response = source("alchemer", "src_alchemer__survey_response") -%}
+
 with
     parse_partition_key as (
         select
@@ -12,7 +14,7 @@ with
             _dagster_partition_key,
 
             regexp_extract(_dagster_partition_key, r'\d+', 1, 1) as survey_id,
-        from {{ source("alchemer", "src_alchemer__survey_response") }}
+        from {{ src_response }}
         where is_test_data = '0' and `status` = 'Complete'
     ),
 
@@ -39,48 +41,6 @@ with
                 order_by="_dagster_partition_key desc",
             )
         }}
-<<<<<<< HEAD
-    ),
-
-    transformed as (
-        select
-            session_id,
-            status,
-            response_time,
-            survey_data,
-
-            safe_cast(survey_id as int) as survey_id,
-            safe_cast(id as int) as id,
-            safe_cast(contact_id as int) as contact_id,
-
-            concat(survey_id, '_', id) as surrogate_key,
-
-            safe_cast(
-                left(
-                    date_started, length(date_started) - 4
-                ) as timestamp format 'YYYY-MM-DD HH24:MI:SS'
-                at time zone '{{ var("local_timezone") }}'
-            ) as date_started,
-
-            safe_cast(
-                left(
-                    date_submitted, length(date_submitted) - 4
-                ) as timestamp format 'YYYY-MM-DD HH24:MI:SS'
-                at time zone '{{ var("local_timezone") }}'
-            ) as date_submitted,
-
-        from deduplicate
-    )
-
-select *,
-from transformed
-where
-    surrogate_key not in (
-        select surrogate_key,
-        from {{ ref("stg_alchemer__survey_response_disqualified") }}
-    )
-    and status = 'Complete'
-=======
     )
 
 select
@@ -107,4 +67,3 @@ select
         at time zone '{{ var("local_timezone") }}'
     ) as date_submitted,
 from deduplicate
->>>>>>> 3e34f7969a89130b3f37d5f89fdfb788008d8e77
