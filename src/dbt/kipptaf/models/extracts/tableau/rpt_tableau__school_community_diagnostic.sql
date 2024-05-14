98a34c09d8219e74719dc4c19a820e0431420d8f
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-05-14 15:06:02 +0000

    delete separate int view, add family responses link to student numbers to tableau, use only 'Complete' survey responses from Alchemer

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__school_community_diagnostic.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__school_community_diagnostic.sql
@@ -5,16 +5,17 @@ with
             survey_id,
             survey_response_id,
 
-            max(
-                case
-                    when
-                        question_shortname
-                        in ('student_number', 'family_respondent_number')
-                    then answer
-                    else null
-                end
-            ) over (partition by answer order by date_submitted)
-            as respondent_number,
+            safe_cast(
+                max(
+                    case
+                        when
+                            question_shortname
+                            in ('student_number', 'family_respondent_number')
+                        then answer
+                        else null
+                    end
+                ) over (partition by answer order by date_submitted) as int
+            ) as respondent_number,
 
         from {{ ref("int_surveys__survey_responses") }}
         where
@@ -39,17 +40,22 @@ select
 
     srh.job_title as staff_job_title,
 
-    se.special_education_code as student_special_education_code,
-
-    coalesce(srh.home_work_location_region, se.region) as region,
-    coalesce(srh.home_work_location_name, se.school_name) as location,
-    coalesce(srh.race_ethnicity, se.ethnicity) as race_ethnicity,
-    coalesce(srh.gender_identity, se.gender) as gender,
-    coalesce(srh.home_work_location_grade_band, se.school_level) as grade_band,
-    coalesce(srh.primary_grade_level_taught, se.grade_level) as grade_level,
     coalesce(
-        srh.employee_number, se.student_number, safe_cast(fr.respondent_number as int)
+        se1.special_education_code, se2.special_education_code
+    ) as student_special_education_code,
+    coalesce(
+        sr.employee_number, se1.student_number, fr.respondent_number
     ) as respondent_number,
+    coalesce(srh.home_work_location_region, se1.region, se2.region) as region,
+    coalesce(srh.home_work_location_name, se1.school_name, se2.school_name) as location,
+    coalesce(srh.race_ethnicity, se1.ethnicity, se2.ethnicity) as race_ethnicity,
+    coalesce(srh.gender_identity, se1.gender, se2.gender) as gender,
+    coalesce(
+        srh.home_work_location_grade_band, se1.school_level, se2.school_level
+    ) as grade_band,
+    coalesce(
+        srh.primary_grade_level_taught, se1.grade_level, se2.grade_level
+    ) as grade_level,
 
     case
         when sr.survey_title like '%Staff%'
@@ -65,14 +71,20 @@ select
 from {{ ref("int_surveys__survey_responses") }} as sr
 left join
     {{ ref("base_people__staff_roster_history") }} as srh
-    on sr.respondent_email = srh.google_email
+    on sr.employee_number = srh.employee_number
     and sr.date_submitted
     between srh.work_assignment_start_date and srh.work_assignment_end_date
 left join
-    {{ ref("base_powerschool__student_enrollments") }} as se
-    on sr.respondent_email = se.student_email_google
-    and sr.academic_year = se.academic_year
-left join family_responses as fr on safe_cast(fr.respondent_number as int) = se.student_number
+    {{ ref("base_powerschool__student_enrollments") }} as se1
+    on sr.respondent_email = se1.student_email_google
+    and sr.academic_year = se1.academic_year
+left join
+    family_responses as fr
+    on fr.survey_id = sr.survey_id
+    and fr.survey_response_id = fr.survey_response_id
+left join
+    {{ ref("base_powerschool__student_enrollments") }} as se2
+    on fr.respondent_number = se2.student_number
 where
     sr.survey_id in (
         '5300913',
