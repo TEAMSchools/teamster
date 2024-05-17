3df8b2d14d8c10e7f7e135ed9b73963e8a9c976b
Gaby Rangel <grangel@kippteamandfamily.org>
2024-05-17 18:29:19 +0000

    wip_please_dont_merge_yet

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard.sql
@@ -269,13 +269,12 @@ left join
     and e.expected_test_type = o.test_type
     and e.expected_scope = o.scope
     and e.expected_subject_area = o.subject_area
-    and o.test_type != 'PSAT10'
 left join
     course_subjects_roster as c
     on o.contact = c.contact_id
     and o.test_academic_year = c.academic_year
     and o.course_discipline = c.courses_credittype
-where e.expected_test_type = 'Official'
+where e.expected_test_type = 'Official' and e.expected_test_scope != 'PSAT10'
 
 union all
 
@@ -334,17 +333,16 @@ select
 from roster as e
 left join
     college_assessments_official as o
-    on cast(e.student_number as string) = o.contact
+    on safe_cast(e.student_number as string) = o.contact
     and e.expected_test_type = o.test_type
     and e.expected_scope = o.scope
     and e.expected_subject_area = o.subject_area
-    and o.test_type = 'PSAT10'
 left join
     course_subjects_roster as c
     on o.contact = c.contact_id
     and o.test_academic_year = c.academic_year
     and o.course_discipline = c.courses_credittype
-where e.expected_test_type = 'Official'
+where e.expected_test_type = 'Official' and e.expected_test_scope = 'PSAT10'
 
 union all
 
