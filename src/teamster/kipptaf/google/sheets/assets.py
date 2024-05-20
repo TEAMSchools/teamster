09990b998c2551fd1e885c04ad9ceec25bdc0e79
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-05-20 18:50:32 +0000

    refactor: misc

--- src/teamster/kipptaf/google/sheets/assets.py
+++ src/teamster/kipptaf/google/sheets/assets.py
@@ -1,23 +1,6 @@
-import re
-
-from dagster import AssetSpec
-
 from teamster.core.definitions.external_asset import external_assets_from_specs
-from teamster.kipptaf import CODE_LOCATION
+from teamster.google.sheets.assets import build_google_sheets_asset_spec
 from teamster.kipptaf.dbt.assets import manifest
-
-
-def build_google_sheets_asset_spec(source_name, name, uri, range_name):
-    re_match = re.match(
-        pattern=r"https:\/{2}docs\.google\.com\/spreadsheets\/d\/([\w-]+)", string=uri
-    )
-
-    return AssetSpec(
-        key=[CODE_LOCATION, source_name, name],
-        metadata={"sheet_id": re_match.group(1), "range_name": range_name},
-        group_name="google_sheets",
-    )
-
 
 specs = [
     build_google_sheets_asset_spec(
