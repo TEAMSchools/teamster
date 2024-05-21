270bcfea37eafc09e236f83eabbe0e5430ebe550
Anthony Walters <awalters@kippteamandfamily.org>
2024-05-21 00:45:03 +0000

    add if statement for transfer grades

--- src/dbt/kipptaf/models/powerschool/staging/stg_powerschool__storedgrades.sql
+++ src/dbt/kipptaf/models/powerschool/staging/stg_powerschool__storedgrades.sql
@@ -1,9 +1,21 @@
-{{
-    dbt_utils.union_relations(
-        relations=[
-            source("kippnewark_powerschool", model.name),
-            source("kippcamden_powerschool", model.name),
-            source("kippmiami_powerschool", model.name),
-        ]
+with
+    union_relations as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    source("kippnewark_powerschool", model.name),
+                    source("kippcamden_powerschool", model.name),
+                    source("kippmiami_powerschool", model.name),
+                ]
+            )
+        }}
     )
-}}
+
+select
+    u.*,
+
+    if(
+        l.name is not null or u.schoolname = 'KIPP Summer School', false, true
+    ) as is_transfer_grade,
+from union_relations as u
+left join {{ ref("stg_people__location_crosswalk") }} as l on u.schoolname = l.name
