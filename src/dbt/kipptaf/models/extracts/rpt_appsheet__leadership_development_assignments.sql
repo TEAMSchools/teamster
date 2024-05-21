26f42b5651914dcd615d42241680f613e32758d0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-05-21 16:42:05 +0000

    feat: add report_to_google_email feat: filter disabled refactor: ldm to cte

--- src/dbt/kipptaf/models/extracts/rpt_appsheet__leadership_development_assignments.sql
+++ src/dbt/kipptaf/models/extracts/rpt_appsheet__leadership_development_assignments.sql
@@ -1,4 +1,14 @@
 with
+    leadership_development_metrics as (
+        select *,
+        from {{ ref("stg_performance_management__leadership_development_metrics") }}
+        /*
+            Need '2024' to make visible before start of next academic year will switch
+            after 7/1/2024
+        */
+        where academic_year = 2024
+    ),
+
     assignment_group as (
         select
             sr.employee_number,
@@ -6,7 +16,8 @@ with
             sr.preferred_name_lastfirst,
             sr.google_email,
             sr.assignment_status,
-            sr2.google_email as google_email_manager,
+            sr.report_to_google_email,
+
             if(
                 sr.job_title in (
                     'Assistant School Leader',
@@ -24,15 +35,13 @@ with
                 'CMO and Other Leaders'
             ) as route,
         from {{ ref("base_people__staff_roster") }} as sr
-        left join
-            {{ ref("base_people__staff_roster") }} as sr2
-            on sr.report_to_employee_number = sr2.employee_number
         where sr.assignment_status in ('Active', 'Leave')
     )
+
 select
     ag.employee_number,
     ag.google_email,
-    ag.google_email_manager,
+    ag.report_to_google_email,
     ag.route,
 
     ldm.academic_year,
@@ -44,14 +53,28 @@ select
     ldm.description,
 
     concat(ag.employee_number, ldm.metric_id) as assignment_id,
-
 from assignment_group as ag
-cross join
-    {{ ref("stg_performance_management__leadership_development_metrics") }} as ldm
-where
-    ag.route = ldm.role
-    and (ag.business_unit_home_name = ldm.region or ldm.region = 'All')
-    and ag.assignment_status in ('Active', 'Leave')
-    /* Need '2024' to make visible before start of next academic year,
-    will switch after 7/1/2024*/
-    and ldm.academic_year = 2024
+inner join
+    leadership_development_metrics as ldm
+    on ag.route = ldm.role
+    and ag.business_unit_home_name = ldm.region
+
+union all
+
+select
+    ag.employee_number,
+    ag.google_email,
+    ag.report_to_google_email,
+    ag.route,
+
+    ldm.academic_year,
+    ldm.role as assignment_group,
+    ldm.region as assignment_region,
+    ldm.metric_id,
+    ldm.bucket,
+    ldm.type,
+    ldm.description,
+
+    concat(ag.employee_number, ldm.metric_id) as assignment_id,
+from assignment_group as ag
+inner join leadership_development_metrics as ldm on ldm.region = 'All'
