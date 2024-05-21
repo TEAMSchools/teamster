1279214a8483793de8a7d0c276382569beb5c1d9
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-05-21 09:41:40 -0500

    Update stg_powerschool__storedgrades.sql
    fix: lint

--- src/dbt/kipptaf/models/powerschool/staging/stg_powerschool__storedgrades.sql
+++ src/dbt/kipptaf/models/powerschool/staging/stg_powerschool__storedgrades.sql
@@ -14,6 +14,6 @@ with
 select
     u.*,
 
-    if(l.name is null , true, false) as is_transfer_grade,
+    if(l.name is null, true, false) as is_transfer_grade,
 from union_relations as u
 left join {{ ref("stg_people__location_crosswalk") }} as l on u.schoolname = l.name
