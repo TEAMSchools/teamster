5bedc44927794c62a7190d317b48788be95b981c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-05-22 05:15:28 +0000

    fix schema

--- src/teamster/kipptaf/deanslist/assets.py
+++ src/teamster/kipptaf/deanslist/assets.py
@@ -1,18 +1,27 @@
-import pathlib
-
-from dagster import config_from_files
-
 from teamster.core.sftp.assets import build_sftp_asset
 from teamster.kipptaf import CODE_LOCATION
+from teamster.kipptaf.deanslist.schema import (
+    RECONCILE_ATTENDANCE_SCHEMA,
+    RECONCILE_SUSPENSIONS_SCHEMA,
+)
+
+reconcile_attendance = build_sftp_asset(
+    asset_key=[CODE_LOCATION, "deanslist", "reconcile_attendance"],
+    remote_dir="reconcile_report_files",
+    remote_file_regex=r"ktaf_reconcile_att\.csv",
+    ssh_resource_key="ssh_deanslist",
+    avro_schema=RECONCILE_ATTENDANCE_SCHEMA,
+)
+
+reconcile_suspensions = build_sftp_asset(
+    asset_key=[CODE_LOCATION, "deanslist", "reconcile_suspensions"],
+    remote_dir="reconcile_report_files",
+    remote_file_regex=r"ktaf_reconcile_susp\.csv",
+    ssh_resource_key="ssh_deanslist",
+    avro_schema=RECONCILE_SUSPENSIONS_SCHEMA,
+)
 
 assets = [
-    build_sftp_asset(
-        asset_key=[CODE_LOCATION, "deanslist", a["asset_name"]],
-        ssh_resource_key="ssh_deanslist",
-        avro_schema=...,
-        **a,
-    )
-    for a in config_from_files(
-        [f"{pathlib.Path(__file__).parent}/config/assets.yaml"],
-    )["assets"]
+    reconcile_attendance,
+    reconcile_suspensions,
 ]
