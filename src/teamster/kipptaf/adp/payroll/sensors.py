e8e9317f586ae25005de4f599ce25b7ca5db89a6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-05-30 20:18:56 +0000

    fix: adp payroll sensor

--- src/teamster/kipptaf/adp/payroll/sensors.py
+++ src/teamster/kipptaf/adp/payroll/sensors.py
@@ -1,4 +1,3 @@
-import json
 import re
 
 import pendulum
@@ -10,6 +9,7 @@ from dagster import (
     RunRequest,
     SensorEvaluationContext,
     SensorResult,
+    _check,
     sensor,
 )
 
@@ -45,11 +45,15 @@ def adp_payroll_sftp_sensor(
 
         asset_identifier = asset.key.to_python_identifier()
         metadata_by_key = asset.metadata_by_key[asset.key]
-        partitions_def: MultiPartitionsDefinition = asset.partitions_def  # type: ignore
 
-        date_partition: DynamicPartitionsDefinition = (
-            partitions_def.get_partitions_def_for_dimension("date")
-        )  # type: ignore
+        partitions_def = _check.inst(
+            obj=asset.partitions_def, ttype=MultiPartitionsDefinition
+        )
+
+        date_partition = _check.inst(
+            obj=partitions_def.get_partitions_def_for_dimension("date"),
+            ttype=DynamicPartitionsDefinition,
+        )
 
         context.log.info(asset_identifier)
         pattern = re.compile(
@@ -65,7 +69,7 @@ def adp_payroll_sftp_sensor(
         ]
 
         for f in file_matches:
-            match = pattern.match(string=f.filepath)
+            match = _check.not_none(value=pattern.match(string=f.filepath))
 
             group_dict = match.groupdict()
 
@@ -83,7 +87,9 @@ def adp_payroll_sftp_sensor(
 
         dynamic_partitions_requests.append(
             AddDynamicPartitionsRequest(
-                partitions_def_name=date_partition.name,  # type: ignore
+                partitions_def_name=_check.str_param(
+                    obj=date_partition.name, param_name="partitions_def_name"
+                ),
                 partition_keys=list(add_dynamic_partition_keys),
             )
         )
@@ -94,7 +100,7 @@ def adp_payroll_sftp_sensor(
     return SensorResult(
         run_requests=run_requests,
         dynamic_partitions_requests=dynamic_partitions_requests,
-        cursor=json.dumps(obj=str(tick_cursor)),
+        cursor=str(tick_cursor),
     )
 
 
