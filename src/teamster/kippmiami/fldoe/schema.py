fcc6889ace96c08ecb97fef6d22d76c9884e9ce6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-06-06 18:44:01 +0000

    refactor: fldoe assets

--- src/teamster/kippmiami/fldoe/schema.py
+++ src/teamster/kippmiami/fldoe/schema.py
@@ -19,13 +19,17 @@ pas_options = (
     | py_avro_schema.Option.USE_FIELD_ALIAS
 )
 
-ASSET_SCHEMA = {
-    "fast": json.loads(
-        py_avro_schema.generate(py_type=fast_record, options=pas_options)
-    ),
-    "fsa": json.loads(py_avro_schema.generate(py_type=fsa_record, options=pas_options)),
-    "science": json.loads(
-        py_avro_schema.generate(py_type=Science, options=pas_options)
-    ),
-    "eoc": json.loads(py_avro_schema.generate(py_type=EOC, options=pas_options)),
-}
+
+FAST_SCHEMA = json.loads(
+    py_avro_schema.generate(py_type=fast_record, options=pas_options)
+)
+
+FSA_SCHEMA = json.loads(
+    py_avro_schema.generate(py_type=fsa_record, options=pas_options)
+)
+
+SCIENCE_SCHEMA = json.loads(
+    py_avro_schema.generate(py_type=Science, options=pas_options)
+)
+
+EOC_SCHEMA = json.loads(py_avro_schema.generate(py_type=EOC, options=pas_options))
