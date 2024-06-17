ebf3a20783fdf38c3411363cd3e28893b2931468
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-06-17 18:43:37 +0000

    checkout

--- src/dbt/powerschool/models/staging/stg_powerschool__termbins.sql
+++ src/dbt/powerschool/models/staging/stg_powerschool__termbins.sql
@@ -1,32 +1,54 @@
-{{
-    teamster_utils.generate_staging_model(
-        unique_key="dcid.int_value",
-        transform_cols=[
-            {"name": "dcid", "extract": "int_value"},
-            {"name": "id", "extract": "int_value"},
-            {"name": "termid", "extract": "int_value"},
-            {"name": "schoolid", "extract": "int_value"},
-            {"name": "creditpct", "extract": "double_value"},
-            {"name": "collect", "extract": "int_value"},
-            {"name": "yearid", "extract": "int_value"},
-            {"name": "showonspreadsht", "extract": "int_value"},
-            {"name": "currentgrade", "extract": "int_value"},
-            {"name": "storegrades", "extract": "int_value"},
-            {"name": "numattpoints", "extract": "double_value"},
-            {"name": "suppressltrgrd", "extract": "int_value"},
-            {"name": "gradescaleid", "extract": "int_value"},
-            {"name": "suppresspercentscr", "extract": "int_value"},
-            {"name": "aregradeslocked", "extract": "int_value"},
-            {"name": "whomodifiedid", "extract": "int_value"},
-        ],
-        except_cols=[
-            "_dagster_partition_fiscal_year",
-            "_dagster_partition_date",
-            "_dagster_partition_hour",
-            "_dagster_partition_minute",
-        ],
-    )
-}}
+with
+    deduplicate as (
+        {{
+            dbt_utils.deduplicate(
+                relation=source("powerschool", "src_powerschool__termbins"),
+                partition_by="dcid.int_value",
+                order_by="_file_name desc",
+            )
+        }}
+    ),
 
-select *
+    staging as (
+        -- trunk-ignore(sqlfluff/AM04)
+        select
+            * except (
+                dcid,
+                id,
+                termid,
+                schoolid,
+                creditpct,
+                collect,
+                yearid,
+                showonspreadsht,
+                currentgrade,
+                storegrades,
+                numattpoints,
+                suppressltrgrd,
+                gradescaleid,
+                suppresspercentscr,
+                aregradeslocked,
+                whomodifiedid
+            ),
+
+            dcid.int_value as dcid,
+            id.int_value as id,
+            termid.int_value as termid,
+            schoolid.int_value as schoolid,
+            creditpct.double_value as creditpct,
+            collect.int_value as collect,
+            yearid.int_value as yearid,
+            showonspreadsht.int_value as showonspreadsht,
+            currentgrade.int_value as currentgrade,
+            storegrades.int_value as storegrades,
+            numattpoints.double_value as numattpoints,
+            suppressltrgrd.int_value as suppressltrgrd,
+            gradescaleid.int_value as gradescaleid,
+            suppresspercentscr.int_value as suppresspercentscr,
+            aregradeslocked.int_value as aregradeslocked,
+            whomodifiedid.int_value as whomodifiedid,
+        from deduplicate
+    )
+
+select *, left(storecode, 1) as storecode_type, right(storecode, 1) as storecode_order,
 from staging
