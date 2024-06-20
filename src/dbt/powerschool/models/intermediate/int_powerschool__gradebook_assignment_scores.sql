e2c699d7355cb68a2cdc8a70fa85fba5c90765bf
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-06-20 23:46:51 +0000

    refactor: assignment scores

--- src/dbt/powerschool/models/intermediate/int_powerschool__gradebook_assignment_scores.sql
+++
@@ -1,5 +0,0 @@
-select asec.*, a.studentsdcid, a.scorepoints, a.islate, a.isexempt, a.ismissing,
-from {{ ref("int_powerschool__gradebook_assignments") }} as asec
-left join
-    {{ ref("stg_powerschool__assignmentscore") }} as a
-    on asec.assignmentsectionid = a.assignmentsectionid
