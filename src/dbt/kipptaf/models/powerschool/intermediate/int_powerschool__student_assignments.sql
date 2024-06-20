68260260b4db477dac1af65dd9ebdbda04694893
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-06-20 18:26:43 +0000

    refactor: cal weeks

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__student_assignments.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__student_assignments.sql
@@ -1,66 +1,4 @@
 with
-    calendar as (
-        select
-            cd._dbt_source_relation,
-            cd.schoolid,
-            cd.week_start_date,
-            cd.week_end_date,
-
-            t.yearid,
-            t.abbreviation as `quarter`,
-
-            initcap(regexp_extract(cd._dbt_source_relation, r'kipp(\w+)_')) as region,
-
-            t.yearid + 1990 as academic_year,
-
-            if(t.abbreviation in ('Q1', 'Q2'), 'S1', 'S2') as semester,
-
-            min(cd.date_value) as school_week_start_date,
-            max(cd.date_value) as school_week_end_date,
-
-            row_number() over (
-                partition by
-                    cd._dbt_source_relation, cd.schoolid, t.yearid, t.abbreviation
-                order by cd.week_start_date asc
-            ) as week_number_quarter,
-            row_number() over (
-                partition by cd._dbt_source_relation, cd.schoolid, t.yearid
-                order by cd.week_start_date asc
-            ) as week_number_academic_year,
-
-            lead(min(cd.date_value)) over (
-                partition by cd._dbt_source_relation, cd.schoolid, t.yearid
-                order by cd.week_start_date
-            ) as school_week_start_date_lead,
-        from {{ ref("stg_powerschool__calendar_day") }} as cd
-        inner join
-            {{ ref("stg_powerschool__terms") }} as t
-            on cd.schoolid = t.schoolid
-            and cd.date_value between t.firstday and t.lastday
-            and {{ union_dataset_join_clause(left_alias="cd", right_alias="t") }}
-            and t.portion = 4  /* quarters */
-        inner join
-            {{ ref("stg_powerschool__cycle_day") }} as cy
-            on cd.cycle_day_id = cy.id
-            and {{ union_dataset_join_clause(left_alias="cd", right_alias="cy") }}
-        inner join
-            {{ ref("stg_powerschool__bell_schedule") }} as bs
-            on cd.schoolid = bs.schoolid
-            and cd.bell_schedule_id = bs.id
-            and {{ union_dataset_join_clause(left_alias="cd", right_alias="bs") }}
-        where
-            cd.insession = 1
-            and cd.membershipvalue > 0
-            and cd.date_value >= date({{ var("current_academic_year") }}, 7, 1)
-        group by
-            cd._dbt_source_relation,
-            cd.schoolid,
-            cd.week_start_date,
-            cd.week_end_date,
-            t.yearid,
-            t.abbreviation
-    ),
-
     assignments as (
         select
             _dbt_source_relation,
@@ -176,7 +114,7 @@ with
                 'SEM22106S1'
             )
         inner join
-            calendar as c
+            {{ ref("int_powerschool__calendar_week") }} as c
             on ce.cc_schoolid = c.schoolid
             and ce.cc_yearid = c.yearid
             and c.week_end_date between ce.cc_dateenrolled and ce.cc_dateleft
