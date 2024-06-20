975c9e00e643c3a6d3a7f903ae49e4c081c2a385
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-06-20 23:46:22 +0000

    refactor

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__gradebook_assignments.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__gradebook_assignments.sql
@@ -55,7 +55,7 @@ left join
     and a.duedate between gb.term_start_date and gb.term_end_date
     and {{ union_dataset_join_clause(left_alias="gb", right_alias="a") }}
 left join
-    {{ ref("int_powerschool__gradebook_assignment_scores") }} as s
+    {{ ref("stg_powerschool__assignmentscore") }} as s
     on a.assignmentsectionid = s.assignmentsectionid
     and {{ union_dataset_join_clause(left_alias="a", right_alias="s") }}
     and enr.students_dcid = s.studentsdcid
@@ -121,7 +121,7 @@ left join
     and a.duedate between gb.term_start_date and gb.term_end_date
     and {{ union_dataset_join_clause(left_alias="gb", right_alias="a") }}
 left join
-    {{ ref("int_powerschool__gradebook_assignment_scores") }} as s
+    {{ ref("stg_powerschool__assignmentscore") }} as s
     on a.assignmentsectionid = s.assignmentsectionid
     and {{ union_dataset_join_clause(left_alias="a", right_alias="s") }}
     and enr.students_dcid = s.studentsdcid
