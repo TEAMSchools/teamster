5651b7841d3a36d1ee41fa4bde54dc1b9124d9f0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-06-20 20:46:50 +0000

    refactor: mat config

--- src/dbt/powerschool/models/intermediate/int_powerschool__gradebook_assignment_scores.sql
+++ src/dbt/powerschool/models/intermediate/int_powerschool__gradebook_assignment_scores.sql
@@ -1,14 +1,5 @@
-select
-    a.studentsdcid,
-    a.scorepoints,
-    a.islate,
-    a.isexempt,
-    a.ismissing,
-    a.assignmentsectionid,
-
-    asec.assignmentid,
-    asec.sectionsdcid,
-from {{ ref("stg_powerschool__assignmentscore") }} as a
-inner join
-    {{ ref("stg_powerschool__assignmentsection") }} as asec
-    on a.assignmentsectionid = asec.assignmentsectionid
+select asec.*, a.studentsdcid, a.scorepoints, a.islate, a.isexempt, a.ismissing,
+from {{ ref("int_powerschool__gradebook_assignments") }} as asec
+left join
+    {{ ref("stg_powerschool__assignmentscore") }} as a
+    on asec.assignmentsectionid = a.assignmentsectionid
