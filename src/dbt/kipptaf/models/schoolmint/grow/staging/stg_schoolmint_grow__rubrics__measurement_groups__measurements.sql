4a962a5db5d34ab52677183c6c7989fc7a2aa55f
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-06-24 17:14:15 +0000

    unnesting measurements to tie to strand names

--- src/dbt/kipptaf/models/schoolmint/grow/staging/stg_schoolmint_grow__rubrics__measurement_groups__measurements.sql
+++ src/dbt/kipptaf/models/schoolmint/grow/staging/stg_schoolmint_grow__rubrics__measurement_groups__measurements.sql
@@ -1,9 +1,9 @@
 select
-    r._id as rubric_id,
-    mg.name as strand_name,
-    mg._id as strand_id,
-    mg.`key` as strand_key,
-    mg.description as strand_description,
-    mg.measurements,
-from {{ ref("stg_schoolmint_grow__rubrics") }} as r
-cross join unnest(r.measurementgroups) as mg
+    mg.rubric_id,
+    mg.strand_name,
+    mg.strand_id,
+    mg.strand_key,
+    mg.strand_description,
+    m.measurement as measurement_id,
+from {{ ref("stg_schoolmint_grow__rubrics__measurement_groups") }} as mg
+cross join unnest(mg.measurements) as m
