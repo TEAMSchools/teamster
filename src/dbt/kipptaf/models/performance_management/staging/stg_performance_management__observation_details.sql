e2287993e81707fca678ef32777647419b9c7e5a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-06-26 16:30:38 +0000

    refactor: smg, pm staging

--- src/dbt/kipptaf/models/performance_management/staging/stg_performance_management__observation_details.sql
+++ src/dbt/kipptaf/models/performance_management/staging/stg_performance_management__observation_details.sql
@@ -1,4 +1,48 @@
-select *,
+select
+    academic_year,
+    employee_number,
+    etr_score,
+    form_long_name as rubric_name,
+    form_term as code,
+    form_type,
+    glows,
+    grows,
+    measurement_name,
+    observation_id,
+    observer_employee_number,
+    overall_score as score,
+    rn_submission,
+    row_score_value as value_score,
+    rubric_id,
+    score_measurement_id,
+    score_measurement_shortname,
+    score_measurement_type,
+    so_score,
+    teacher_id,
+    text_box,
+    _dagster_partition_academic_year,
+    _dagster_partition_term,
+
+    true as locked,
+    'Teacher Performance Management' as observation_type,
+    'PM' as observation_type_abbreviation,
+    'Coaching Tool: Coach ETR and Reflection' as `name`,
+
+    date(observed_at) as observed_at_date_local,
+
+    case
+        when score_measurement_type = 'etr'
+        then 'Excellent Teaching Rubric'
+        when score_measurement_type = 's&o'
+        then 'Self & Others: Manager Feedback'
+        else 'Comments'
+    end as measurement_group_name,
+    case
+        when score_measurement_type = 'etr'
+        then etr_score
+        when score_measurement_type = 's&o'
+        then so_score
+    end as score_averaged_by_strand,
 from
     {{
         source(
--- src/dbt/kipptaf/models/schoolmint/grow/staging/stg_schoolmint_grow__rubrics__measurement_groups.sql
+++ src/dbt/kipptaf/models/schoolmint/grow/staging/stg_schoolmint_grow__rubrics__measurement_groups.sql
@@ -1,9 +1,13 @@
 select
-    r._id as rubric_id,
-    mg.name as strand_name,
-    mg._id as strand_id,
-    mg.`key` as strand_key,
-    mg.description as strand_description,
+    r.rubric_id,
+
+    mg._id as measurement_group_id,
+    mg.name as measurement_group_name,
+    mg.key as measurement_group_key,
+    mg.weight as measurement_group_weight,
+    mg.description as measurement_group_description,
+
+    /* repeated records */
     mg.measurements,
 from {{ ref("stg_schoolmint_grow__rubrics") }} as r
-cross join unnest(r.measurementgroups) as mg
+cross join unnest(r.measurement_groups) as mg
