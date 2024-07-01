e71340912d1e733cbe6b8d02fbde941ff2c99bf3
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-01 17:50:04 +0000

    fix: reduce suf cols to only those used

--- src/dbt/kipptaf/models/powerschool/staging/stg_powerschool__u_studentsuserfields.sql
+++ src/dbt/kipptaf/models/powerschool/staging/stg_powerschool__u_studentsuserfields.sql
@@ -1,9 +1,23 @@
-{{
-    dbt_utils.union_relations(
-        relations=[
-            source("kippnewark_powerschool", model.name),
-            source("kippcamden_powerschool", model.name),
-            source("kippmiami_powerschool", model.name),
-        ]
+with
+    union_relations as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    source("kippnewark_powerschool", model.name),
+                    source("kippcamden_powerschool", model.name),
+                    source("kippmiami_powerschool", model.name),
+                ]
+            )
+        }}
     )
-}}
+
+select
+    studentsdcid,
+    fleid,
+    newark_enrollment_number,
+    infosnap_id,
+    infosnap_opt_in,
+    media_release,
+    rides_staff,
+    is_504,
+from union_relations
