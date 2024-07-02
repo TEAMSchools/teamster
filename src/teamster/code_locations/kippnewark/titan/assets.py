3bc4e01b19f77b821bef145109c8246f996f7e9f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-02 19:16:50 +0000

    refactor: titan assets

--- src/teamster/code_locations/kippnewark/titan/assets.py
+++ src/teamster/code_locations/kippnewark/titan/assets.py
@@ -1,35 +1,32 @@
-from teamster.code_locations.kippnewark import CODE_LOCATION, LOCAL_TIMEZONE
+from teamster.code_locations.kippnewark import (
+    CODE_LOCATION,
+    CURRENT_FISCAL_YEAR,
+    LOCAL_TIMEZONE,
+)
 from teamster.code_locations.kippnewark.titan.schema import (
     INCOME_FORM_DATA_SCHEMA,
     PERSON_DATA_SCHEMA,
 )
-from teamster.libraries.core.utils.classes import FiscalYearPartitionsDefinition
-from teamster.libraries.sftp.assets import build_sftp_asset
+from teamster.libraries.titan.assets import build_titan_sftp_asset
 
 asset_key_prefix = [CODE_LOCATION, "titan"]
-remote_dir = "."
-ssh_resource_key = "ssh_titan"
 
-person_data = build_sftp_asset(
-    asset_key=[*asset_key_prefix, "person_data"],
-    remote_dir=remote_dir,
+person_data = build_titan_sftp_asset(
+    key=[*asset_key_prefix, "person_data"],
     remote_file_regex=r"Person Data(?P<fiscal_year>\d{4})\.csv",
-    ssh_resource_key=ssh_resource_key,
-    avro_schema=PERSON_DATA_SCHEMA,
-    partitions_def=FiscalYearPartitionsDefinition(
-        start_date="2020-07-01", start_month=7, timezone=LOCAL_TIMEZONE.name, fmt="%Y"
-    ),
+    schema=PERSON_DATA_SCHEMA,
+    partition_start_date="2020-07-01",
+    timezone=LOCAL_TIMEZONE,
+    current_fiscal_year=CURRENT_FISCAL_YEAR,
 )
 
-income_form_data = build_sftp_asset(
-    asset_key=[*asset_key_prefix, "income_form_data"],
-    remote_dir=remote_dir,
+income_form_data = build_titan_sftp_asset(
+    key=[*asset_key_prefix, "income_form_data"],
     remote_file_regex=r"Income Form Data(?P<fiscal_year>\d{4})\.csv",
-    ssh_resource_key=ssh_resource_key,
-    avro_schema=INCOME_FORM_DATA_SCHEMA,
-    partitions_def=FiscalYearPartitionsDefinition(
-        start_date="2021-07-01", start_month=7, timezone=LOCAL_TIMEZONE.name, fmt="%Y"
-    ),
+    schema=INCOME_FORM_DATA_SCHEMA,
+    partition_start_date="2021-07-01",
+    timezone=LOCAL_TIMEZONE,
+    current_fiscal_year=CURRENT_FISCAL_YEAR,
 )
 
 assets = [
---
+++ src/teamster/libraries/titan/assets.py
@@ -0,0 +1,35 @@
+import pendulum
+from dagster import StaticPartitionsDefinition
+
+from teamster.libraries.core.utils.classes import FiscalYear
+from teamster.libraries.sftp.assets import build_sftp_asset
+
+
+def build_titan_sftp_asset(
+    key,
+    remote_file_regex,
+    schema,
+    partition_start_date,
+    timezone,
+    current_fiscal_year,
+):
+    start_fy = FiscalYear(
+        datetime=pendulum.from_format(
+            string=partition_start_date, fmt="YYYY-MM-DD", tz=timezone
+        ),
+        start_month=7,
+    )
+
+    partition_keys = [
+        str(y - 1)
+        for y in range(start_fy.fiscal_year, (current_fiscal_year.fiscal_year + 1))
+    ]
+
+    return build_sftp_asset(
+        asset_key=key,
+        remote_dir=".",
+        remote_file_regex=remote_file_regex,
+        ssh_resource_key="ssh_titan",
+        avro_schema=schema,
+        partitions_def=StaticPartitionsDefinition(partition_keys),
+    )
