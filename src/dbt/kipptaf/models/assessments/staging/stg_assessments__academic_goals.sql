82f63b6c23f270de83e98826cc2958f094770e7e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-03 15:44:17 +0000

    fix: region extract refactor: use int_powerschool__calendar_week feat: add day count and monday/sunday

--- src/dbt/kipptaf/models/assessments/staging/stg_assessments__academic_goals.sql
+++ src/dbt/kipptaf/models/assessments/staging/stg_assessments__academic_goals.sql
@@ -1,7 +1,3 @@
-select
-    g.*,
-    initcap(
-        (substr(regexp_extract(s._dbt_source_relation, r'(kipp\w+)_'), 5))
-    ) as region,
+select g.*, initcap(regexp_extract(s._dbt_source_relation, r'kipp(\w+)_')) as region,
 from {{ source("assessments", "src_assessments__academic_goals") }} as g
-left join {{ ref("stg_powerschool__schools") }} as s on g.school_id = s.school_number
+inner join {{ ref("stg_powerschool__schools") }} as s on g.school_id = s.school_number
