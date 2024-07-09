e29b9ae27f56826678e0b8077fcfc461922a2ffa
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-09 17:06:38 +0000

    refactor: exclude_dirs test: fix

--- src/teamster/code_locations/kippnewark/iready/sensors.py
+++ src/teamster/code_locations/kippnewark/iready/sensors.py
@@ -6,7 +6,7 @@ sftp_sensor = build_iready_sftp_sensor(
     code_location=CODE_LOCATION,
     asset_defs=assets,
     timezone=LOCAL_TIMEZONE,
-    remote_dir="/exports/nj-kipp_nj",
+    remote_dir="/exports/nj-kipp_nj/Current_Year",
     minimum_interval_seconds=(60 * 10),
 )
 
