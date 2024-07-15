def6fcdce0e0be1f4421ebf9eae967467cb4a112
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-15 19:26:02 +0000

    fix: schedule

--- src/teamster/code_locations/kippmiami/deanslist/schedules.py
+++ src/teamster/code_locations/kippmiami/deanslist/schedules.py
@@ -2,51 +2,50 @@ from dagster import AssetKey
 
 from teamster.code_locations.kippmiami import CODE_LOCATION, LOCAL_TIMEZONE
 from teamster.code_locations.kippmiami.deanslist.assets import (
-    multi_partition_fiscal_assets,
-    multi_partition_monthly_assets,
-    static_partition_assets,
+    DEANSLIST_FISCAL_MULTI_PARTITIONS_DEF,
+    DEANSLIST_MONTHLY_MULTI_PARTITIONS_DEF,
+    DEANSLIST_STATIC_PARTITIONS_DEF,
+    fiscal_multi_partitions_assets,
+    monthly_multi_partitions_assets,
+    static_partitions_assets,
 )
 from teamster.libraries.deanslist.schedules import build_deanslist_job_schedule
 
-deanslist_static_partition_asset_job_schedule = build_deanslist_job_schedule(
-    code_location=CODE_LOCATION,
-    partitions_type="static",
-    selection=static_partition_assets,
+deanslist_static_partitions_assets_job_schedule = build_deanslist_job_schedule(
+    job_name=f"{CODE_LOCATION}_deanslist_static_partitions_assets_job",
+    selection=static_partitions_assets,
+    partitions_def=DEANSLIST_STATIC_PARTITIONS_DEF,
     cron_schedule="0 0 * * *",
     execution_timezone=LOCAL_TIMEZONE.name,
 )
 
-deanslist_multi_partition_monthly_asset_job_schedule = build_deanslist_job_schedule(
-    code_location=CODE_LOCATION,
-    partitions_type="monthly",
-    selection=multi_partition_monthly_assets,
+deanslist_monthly_multi_partitions_assets_job_schedule = build_deanslist_job_schedule(
+    job_name=f"{CODE_LOCATION}_deanslist_monthly_multi_partitions_assets_job",
+    selection=monthly_multi_partitions_assets,
+    partitions_def=DEANSLIST_MONTHLY_MULTI_PARTITIONS_DEF,
     cron_schedule="0 0 * * *",
     execution_timezone=LOCAL_TIMEZONE.name,
 )
 
-deanslist_multi_partition_fiscal_asset_job_schedule = build_deanslist_job_schedule(
-    code_location=CODE_LOCATION,
-    partitions_type="fiscal",
-    selection=multi_partition_fiscal_assets,
+deanslist_fiscal_multi_partitions_assets_job_schedule = build_deanslist_job_schedule(
+    job_name=f"{CODE_LOCATION}_deanslist_fiscal_multi_partitions_assets_job",
+    selection=fiscal_multi_partitions_assets,
+    partitions_def=DEANSLIST_FISCAL_MULTI_PARTITIONS_DEF,
     cron_schedule="0 0 * * *",
     execution_timezone=LOCAL_TIMEZONE.name,
 )
 
-deanslist_comm_log_midday_job_schedule = build_deanslist_job_schedule(
-    code_location=CODE_LOCATION,
-    partitions_type="comm_log",
-    selection=[
-        a
-        for a in multi_partition_fiscal_assets
-        if a.key == AssetKey([CODE_LOCATION, "deanslist", "comm_log"])
-    ],
+deanslist_midday_commlog_job_schedule = build_deanslist_job_schedule(
+    job_name=f"{CODE_LOCATION}_deanslist_midday_commlog_job",
+    selection=[AssetKey([CODE_LOCATION, "deanslist", "comm_log"])],
+    partitions_def=DEANSLIST_FISCAL_MULTI_PARTITIONS_DEF,
     cron_schedule="0 14 * * *",
     execution_timezone=LOCAL_TIMEZONE.name,
 )
 
 schedules = [
-    deanslist_comm_log_midday_job_schedule,
-    deanslist_multi_partition_fiscal_asset_job_schedule,
-    deanslist_multi_partition_monthly_asset_job_schedule,
-    deanslist_static_partition_asset_job_schedule,
+    deanslist_fiscal_multi_partitions_assets_job_schedule,
+    deanslist_midday_commlog_job_schedule,
+    deanslist_monthly_multi_partitions_assets_job_schedule,
+    deanslist_static_partitions_assets_job_schedule,
 ]
