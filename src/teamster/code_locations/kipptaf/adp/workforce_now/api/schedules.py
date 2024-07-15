48d0178ac778f8dfaf1cb09d176805b3fe1685aa
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-15 20:41:02 +0000

    refactor: clean up jobs

--- src/teamster/code_locations/kipptaf/adp/workforce_now/api/schedules.py
+++ src/teamster/code_locations/kipptaf/adp/workforce_now/api/schedules.py
@@ -1,8 +1,12 @@
-from dagster import ScheduleDefinition, build_schedule_from_partitioned_job
+from dagster import (
+    ScheduleDefinition,
+    build_schedule_from_partitioned_job,
+    define_asset_job,
+)
 
-from teamster.code_locations.kipptaf import LOCAL_TIMEZONE
+from teamster.code_locations.kipptaf import CODE_LOCATION, LOCAL_TIMEZONE
+from teamster.code_locations.kipptaf.adp.workforce_now.api.assets import workers
 from teamster.code_locations.kipptaf.adp.workforce_now.api.jobs import (
-    adp_wfn_api_workers_asset_job,
     adp_wfn_update_workers_job,
 )
 
@@ -13,7 +17,10 @@ adp_wfn_worker_fields_update_schedule = ScheduleDefinition(
 )
 
 adp_wfn_api_workers_asset_schedule = build_schedule_from_partitioned_job(
-    job=adp_wfn_api_workers_asset_job
+    job=define_asset_job(
+        name=f"{CODE_LOCATION}_adp_workforce_now_api_workers_asset_job",
+        selection=[workers],
+    )
 )
 
 schedules = [
