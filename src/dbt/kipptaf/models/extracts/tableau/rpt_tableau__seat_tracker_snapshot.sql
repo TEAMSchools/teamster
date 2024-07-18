19c29fd8f79ca788adb234e9d4641e54d50f8742
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-07-18 18:31:36 +0000

    changing past plan statuses to be consistent

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
@@ -2,7 +2,6 @@ select
     log_id,
     snapshot_staffing_model_id,
     snapshot_teammate,
-    snapshot_plan_status,
     snapshot_staffing_status,
     snapshot_status_detail,
     snapshot_mid_year_hire,
@@ -16,5 +15,10 @@ select
     if(snapshot_staffing_status = 'Staffed', 1, 0) as snapshot_staffed,
     if(snapshot_plan_status IN ('Active','TRUE'), 1, 0) as snapshot_active,
     if(snapshot_mid_year_hire = true, 1, 0) as snapshot_mid_year_hire_int,
+
+    case
+    when snapshot_plan_status = 'TRUE' then 'Active'
+    when snapshot_plan_status = 'FALSE' then 'Inactive'
+    end as snapshot_plan_status,
 
 from {{ ref("stg_google_appsheet__src_seat_tracker__log_archive") }}
