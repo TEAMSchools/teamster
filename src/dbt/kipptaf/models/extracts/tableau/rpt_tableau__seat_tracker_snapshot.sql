9d427c5435bf3e4829553612263e48b05f5b528e
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-07-18 16:02:21 +0000

    staging and reporting views

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
@@ -1,1 +1,18 @@
-select * from {{ source("google_appsheet", "src_seat_tracker__log_archive") }}
+select
+    log_id,
+    snapshot_staffing_model_id,
+    snapshot_teammate,
+    snapshot_plan_status,
+    snapshot_staffing_status,
+    snapshot_status_detail,
+    snapshot_mid_year_hire,
+    export_date,
+    snapshot_academic_year,
+
+    if(snapshot_staffing_status = 'Open', 1, 0) as snapshot_open,
+    if(snapshot_status_detail in ('New Hire', 'Transfer In'), 1, 0) as snapshot_new_hire,
+    if(snapshot_staffing_status = 'Staffed', 1, 0) as snapshot_staffed,
+    if(snapshot_plan_status = 'Active', 1, 0) as snapshot_active,
+    if(snapshot_mid_year_hire = true, 1, 0) as snapshot_mid_year_hire,
+
+from {{ ref("stg_google_appsheet__src_seat_tracker__log_archive") }}
--- src/dbt/kipptaf/models/staging/google_appsheet/stg_google_appsheet__src_seat_tracker__log_archive.sql
+++ src/dbt/kipptaf/models/staging/google_appsheet/stg_google_appsheet__src_seat_tracker__log_archive.sql
@@ -10,14 +10,14 @@ renamed as (
 
     select
         log_id,
-        staffing_model_id,
-        teammate,
-        plan_status,
-        staffing_status,
-        status_detail,
-        mid_year_hire,
+        staffing_model_id as snapshot_staffing_model_id,
+        teammate as snapshot_teammate,
+        plan_status as snapshot_plan_status,
+        staffing_status as snapshot_staffing_status,
+        status_detail as snapshot_status_detail,
+        mid_year_hire as snapshot_mid_year_hire,
         export_date,
-        academic_year
+        academic_year as snapshot_academic_year,
 
     from source
 
