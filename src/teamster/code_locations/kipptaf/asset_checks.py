1e7920eea3b3cc912e89263031536c319fa5449b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-18 16:28:12 +0000

    feeat

--- src/teamster/code_locations/kipptaf/asset_checks.py
+++ src/teamster/code_locations/kipptaf/asset_checks.py
@@ -1,12 +1,19 @@
 from datetime import timedelta
 
-from dagster import build_last_update_freshness_checks
+from dagster import AssetSelection, build_last_update_freshness_checks
 
-from teamster.code_locations.kipptaf._dbt.assets import dbt_assets
+from teamster.code_locations.kipptaf import CODE_LOCATION
+from teamster.code_locations.kipptaf._dbt.assets import (
+    dbt_assets,
+    external_source_dbt_assets,
+)
 from teamster.code_locations.kipptaf.fivetran.assets import adp_workforce_now_assets
 
-dbt_assets.keys
+dbt_asset_selection = AssetSelection.assets(
+    dbt_assets, external_source_dbt_assets
+).key_prefixes([CODE_LOCATION, "adp_workforce_now"])
 
 asset1_freshness_checks = build_last_update_freshness_checks(
-    assets=adp_workforce_now_assets, lower_bound_delta=timedelta(hours=2)
+    assets=[*adp_workforce_now_assets, *dbt_asset_selection],
+    lower_bound_delta=timedelta(hours=2),
 )
