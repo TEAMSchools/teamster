c9c32e3927fe8e006ac546be5011fdfd225ebb11
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-07-19 15:06:12 +0000

    adding phone and resume scores

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__smartrecruiters.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__smartrecruiters.sql
@@ -1,29 +1,33 @@
-select
-    application_id,
-    candidate_id,
-    job_city,
-    recruiters,
-    department_internal,
-    job_title,
-    application_status,
-    candidate_first_name,
-    candidate_last_name,
-    --candidate_last_first,
-    candidate_source,
-    candidate_source_type,
-    candidate_source_subtype,
-    reason_for_rejection,
-
-    status_type,
-    date_val,
-from
-    {{ ref("stg_smartrecruiters__applications") }} unpivot (
-        date_val for status_type in (
-            demo_date,
-            hired_date,
-            new_date,
-            offer_date,
-            phone_screen_complete_date,
-            phone_screen_requested_date
-        )
+with
+    applications as (
+        select
+            application_id,
+            candidate_id,
+            job_city,
+            recruiters,
+            department_internal,
+            job_title,
+            application_status,
+            reason_for_rejection,
+            phone_interview_score,
+            resume_score,
+            status_type,
+            date_val,
+        from
+            {{ ref("stg_smartrecruiters__applications") }} unpivot (
+                date_val for status_type in (
+                    demo_date,
+                    hired_date,
+                    new_date,
+                    offer_date,
+                    phone_screen_complete_date,
+                    phone_screen_requested_date
+                )
+            )
     )
+
+select a.*
+from applications as a
+-- left join
+--     {{ ref("stg_smartrecruiters__applicants") }} as s as s
+--     on a.candidate_id = s.candidate_id
