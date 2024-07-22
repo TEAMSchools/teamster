e9425349b1d619ba831b4256bb5064a632160c19
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-22 19:19:54 +0000

    refactor: rename, calcs

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
@@ -1,27 +1,17 @@
 select
     log_id,
-    snapshot_staffing_model_id,
-    snapshot_teammate,
-    snapshot_staffing_status,
-    snapshot_status_detail,
-    snapshot_mid_year_hire,
     export_date,
-    snapshot_academic_year,
+    academic_year as snapshot_academic_year,
+    staffing_model_id as snapshot_staffing_model_id,
+    teammate as snapshot_teammate,
+    staffing_status as snapshot_staffing_status,
+    status_detail as snapshot_status_detail,
+    plan_status as snapshot_plan_status,
+    is_mid_year_hire as snapshot_mid_year_hire,
+    is_mid_year_hire_int as snapshot_mid_year_hire_int,
 
-    if(snapshot_staffing_status = 'Open', 1, 0) as snapshot_open,
-    if(
-        snapshot_status_detail in ('New Hire', 'Transfer In'), 1, 0
-    ) as snapshot_new_hire,
-    if(snapshot_staffing_status = 'Staffed', 1, 0) as snapshot_staffed,
-    if(snapshot_plan_status in ('Active', 'TRUE'), 1, 0) as snapshot_active,
-    if(snapshot_mid_year_hire = true, 1, 0) as snapshot_mid_year_hire_int,
-
-    case
-        when snapshot_plan_status = 'TRUE'
-        then 'Active'
-        when snapshot_plan_status = 'FALSE'
-        then 'Inactive'
-        else snapshot_plan_status
-    end as snapshot_plan_status,
-
-from {{ ref("stg_google_appsheet__src_seat_tracker__log_archive") }}
+    if(is_open, 1, 0) as snapshot_open,
+    if(is_new_hire, 1, 0) as snapshot_new_hire,
+    if(is_staffed, 1, 0) as snapshot_staffed,
+    if(is_active, 1, 0) as snapshot_active,
+from {{ ref("stg_seat_tracker__log_archive") }}
