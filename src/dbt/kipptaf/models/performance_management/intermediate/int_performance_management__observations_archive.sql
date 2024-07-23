8bc415dde980ebb58c46336b346e0cfd1306c5c7
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-07-23 16:38:22 +0000

    moving observer_employee_number to a cte

--- src/dbt/kipptaf/models/performance_management/intermediate/int_performance_management__observations_archive.sql
+++ src/dbt/kipptaf/models/performance_management/intermediate/int_performance_management__observations_archive.sql
@@ -1,4 +1,43 @@
+with
+    observer as (
+        select
+            observation_id,
+            if(
+                academic_year < 2023,
+                'Multiple',
+                cast(observer_employee_number as string)
+            ) as observer_employee_number,
+        from {{ ref("stg_performance_management__observation_details_archive") }}
+    )
+
 select
+    ods.employee_number,
+    ods.observation_id,
+    ods.academic_year,
+    ods.term_code,
+    ods.term_name,
+    ods.rubric_name,
+    ods.observation_type,
+    ods.observation_type_abbreviation,
+    ods.eval_date,
+    ods.score,
+    ods.overall_tier,
+    ods.etr_score,
+    ods.etr_tier,
+    ods.so_score,
+    ods.so_tier,
+    ods.final_score,
+    ods.final_tier,
+    ods.glows,
+    ods.grows,
+    ods.locked,
+    o.observer_employee_number,
+
+    max(ods.observed_at) as observed_at,
+    max(ods.observed_at_date_local) as observed_at_date_local,
+from {{ ref("stg_performance_management__observation_details_archive") }} as ods
+left join observer as o on ods.observation_id = o.observation_id
+group by
     employee_number,
     observation_id,
     academic_year,
@@ -19,31 +58,4 @@ select
     glows,
     grows,
     locked,
-    max(observed_at) as observed_at,
-    max(observed_at_date_local) as observed_at_date_local,
-    if(
-        academic_year < 2023, 'Multiple', cast(observer_employee_number as string)
-    ) as observer_employee_number,
-from {{ ref("stg_performance_management__observation_details_archive") }}
-group by
-    employee_number,
-    observation_id,
-    academic_year,
-    term_code,
-    term_name,
-    rubric_name,
-    observation_type,
-    observation_type_abbreviation,
-    observer_employee_number,
-    eval_date,
-    score,
-    overall_tier,
-    etr_score,
-    etr_tier,
-    so_score,
-    so_tier,
-    final_score,
-    final_tier,
-    glows,
-    grows,
-    locked
+    observer_employee_number
