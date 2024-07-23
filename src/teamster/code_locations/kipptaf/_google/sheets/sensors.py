1b2053b4939772e39c29cba16044fd878d7e29cb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-23 17:17:01 +0000

    fix: exception handling

--- src/teamster/code_locations/kipptaf/_google/sheets/sensors.py
+++ src/teamster/code_locations/kipptaf/_google/sheets/sensors.py
@@ -38,7 +38,7 @@ def google_sheets_asset_sensor(
         try:
             spreadsheet = _check.not_none(value=gsheets.open(sheet_id=sheet_id))
         except APIError as e:
-            if e.code == 503:
+            if str(e.code)[0] == "5":
                 context.log.error(msg=str(e))
                 continue
             else:
