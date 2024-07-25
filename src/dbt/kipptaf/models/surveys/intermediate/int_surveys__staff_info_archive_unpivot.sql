a88f0b29e37a72d6f5eb64eb9a88867e09819cef
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-25 16:17:54 +0000

    refactor: employee number to base

--- src/dbt/kipptaf/models/surveys/intermediate/int_surveys__staff_info_archive_unpivot.sql
+++ src/dbt/kipptaf/models/surveys/intermediate/int_surveys__staff_info_archive_unpivot.sql
@@ -1,26 +1,24 @@
 select
     respondent_email,
-    last_submitted_time,
     employee_number,
+    last_submitted_timestamp,
     pivot_column_value,
     item_abbreviation,
-
 from
     {{ ref("stg_surveys__staff_info_archive") }} unpivot (
         pivot_column_value for item_abbreviation in (
-            respondent_name,
-            gender_identity,
-            level_of_education,
-            undergraduate_school,
-            years_exp_outside_kipp,
-            years_teaching_in_njfl,
-            years_teaching_outside_njfl,
             alumni_status,
             community_grew_up,
             community_professional_exp,
+            gender_identity,
             languages_spoken,
+            level_of_education,
             path_to_education,
             race_ethnicity,
-            relay_status
+            relay_status,
+            undergraduate_school,
+            years_exp_outside_kipp,
+            years_teaching_in_njfl,
+            years_teaching_outside_njfl
         )
     )
--- src/dbt/kipptaf/models/surveys/intermediate/int_surveys__staff_information_survey_pivot.sql
+++ src/dbt/kipptaf/models/surveys/intermediate/int_surveys__staff_information_survey_pivot.sql
@@ -1,32 +1,31 @@
 with
     form_responses as (
         select
-            response_id,
-            respondent_email,
+            employee_number,
             item_abbreviation,
-            safe_cast(last_submitted_time as timestamp) as last_submitted_time,
-            coalesce(text_value, file_upload_file_id) as pivot_column_value,
+            last_submitted_timestamp,
 
+            coalesce(text_value, file_upload_file_id) as pivot_column_value,
         from {{ ref("base_google_forms__form_responses") }}
         where form_id = '1jpeMof_oQ9NzTw85VFsA5A7G9VrH3XkSc_nZDFz07nA'
     ),
 
     agg_multis as (
         select
-            response_id,
-            respondent_email,
-            last_submitted_time,
+            employee_number,
+            last_submitted_timestamp,
             item_abbreviation,
+
             string_agg(pivot_column_value) as pivot_column_value,
         from form_responses
-        group by response_id, respondent_email, last_submitted_time, item_abbreviation
+        group by employee_number, last_submitted_timestamp, item_abbreviation
     ),
 
+    -- trunk-ignore(sqlfluff/ST03)
     response_union as (
         select
-            response_id,
-            respondent_email,
-            last_submitted_time,
+            employee_number,
+            last_submitted_timestamp,
             item_abbreviation,
             pivot_column_value,
         from agg_multis
@@ -34,11 +33,10 @@ with
         union all
 
         select
-            null as response_id,
-            respondent_email,
-            last_submitted_time,
-            pivot_column_value,
+            employee_number,
+            last_submitted_timestamp,
             item_abbreviation,
+            pivot_column_value,
         from {{ ref("int_surveys__staff_info_archive_unpivot") }}
     ),
 
@@ -46,15 +44,18 @@ with
         {{
             dbt_utils.deduplicate(
                 relation="response_union",
-                partition_by="respondent_email, item_abbreviation",
-                order_by="last_submitted_time desc",
+                partition_by="employee_number, item_abbreviation",
+                order_by="last_submitted_timestamp desc",
             )
         }}
+    ),
+
+    pre_pivot as (
+        select employee_number, item_abbreviation, pivot_column_value, from deduplicate
     )
+
 select
-    response_id,
-    respondent_email,
-    last_submitted_time,
+    employee_number,
 
     /* pivot cols */
     additional_languages,
@@ -86,15 +87,26 @@ select
     nj_cert_type_3,
     nj_cert_type_4,
     nj_cert_type_5,
+    praxis_document_link,
     race_ethnicity,
     respondent_name,
     undergraduate_school,
     updates_open_ended,
-    praxis_document_link,
-    safe_cast(years_teaching_in_njfl as numeric) as years_teaching_in_njfl,
+
+    safe_cast(fl_cert_expiration_1 as date) as fl_cert_expiration_1,
+    safe_cast(fl_cert_expiration_2 as date) as fl_cert_expiration_2,
+    safe_cast(fl_cert_expiration_3 as date) as fl_cert_expiration_3,
+    safe_cast(fl_cert_expiration_4 as date) as fl_cert_expiration_4,
+    safe_cast(fl_cert_expiration_5 as date) as fl_cert_expiration_5,
+    safe_cast(nj_cert_issue_date_1 as date) as nj_cert_issue_date_1,
+    safe_cast(nj_cert_issue_date_2 as date) as nj_cert_issue_date_2,
+    safe_cast(nj_cert_issue_date_3 as date) as nj_cert_issue_date_3,
+    safe_cast(nj_cert_issue_date_4 as date) as nj_cert_issue_date_4,
+    safe_cast(nj_cert_issue_date_5 as date) as nj_cert_issue_date_5,
     safe_cast(years_exp_outside_kipp as numeric) as years_exp_outside_kipp,
+    safe_cast(years_teaching_in_njfl as numeric) as years_teaching_in_njfl,
     safe_cast(years_teaching_outside_njfl as numeric) as years_teaching_outside_njfl,
-    safe_cast(regexp_extract(respondent_name, r'(\d{6})') as int) as employee_number,
+
     replace(alumni_status, ',', '.') as alumni_status,
     replace(path_to_education, ',', '.') as path_to_education,
     replace(relay_status, ',', '.') as relay_status,
@@ -106,37 +118,26 @@ select
         'Newark, Camden, and/or Miami',
         'the cities we serve'
     ) as community_professional_exp,
-    safe_cast(fl_cert_expiration_1 as date) as fl_cert_expiration_1,
-    safe_cast(fl_cert_expiration_2 as date) as fl_cert_expiration_2,
-    safe_cast(fl_cert_expiration_3 as date) as fl_cert_expiration_3,
-    safe_cast(fl_cert_expiration_4 as date) as fl_cert_expiration_4,
-    safe_cast(fl_cert_expiration_5 as date) as fl_cert_expiration_5,
-    safe_cast(nj_cert_issue_date_1 as date) as nj_cert_issue_date_1,
-    safe_cast(nj_cert_issue_date_2 as date) as nj_cert_issue_date_2,
-    safe_cast(nj_cert_issue_date_3 as date) as nj_cert_issue_date_3,
-    safe_cast(nj_cert_issue_date_4 as date) as nj_cert_issue_date_4,
-    safe_cast(nj_cert_issue_date_5 as date) as nj_cert_issue_date_5,
+
     if(cert_required = 'Yes', true, false) as cert_required,
     if(fl_additional_cert_1 = 'Yes', true, false) as fl_additional_cert_1,
     if(nj_cert_additional_1 = 'Yes', true, false) as nj_cert_additional_1,
+
     case
-        when regexp_contains(race_ethnicity, 'I decline to state')
+        when contains_substr(race_ethnicity, 'I decline to state')
         then 'Decline to State'
         when race_ethnicity = 'Latinx/Hispanic/Chicana(o)'
         then 'Latinx/Hispanic/Chicana(o)'
         when race_ethnicity = 'My racial/ethnic identity is not listed'
         then 'Race/Ethnicity Not Listed'
-        when regexp_contains(race_ethnicity, 'Bi/Multiracial')
+        when contains_substr(race_ethnicity, 'Bi/Multiracial')
         then 'Bi/Multiracial'
-        when regexp_contains(race_ethnicity, ',')
+        when contains_substr(race_ethnicity, ',')
         then 'Bi/Multiracial'
         else race_ethnicity
     end as race_ethnicity_reporting,
-    -- row_number() over (
-    --     partition by employee_number order by last_submitted_time desc
-    -- ) as rn_submission,
 from
-    deduplicate pivot (
+    pre_pivot pivot (
         max(pivot_column_value) for item_abbreviation in (
             'additional_languages',
             'alumni_status',
@@ -184,14 +185,14 @@ from
             'nj_cert_type_4',
             'nj_cert_type_5',
             'path_to_education',
+            'praxis_document_link',
             'race_ethnicity',
             'relay_status',
             'respondent_name',
             'undergraduate_school',
+            'updates_open_ended',
             'years_exp_outside_kipp',
             'years_teaching_in_njfl',
-            'years_teaching_outside_njfl',
-            'updates_open_ended',
-            'praxis_document_link'
+            'years_teaching_outside_njfl'
         )
     )
