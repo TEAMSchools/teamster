06e8a8236f709d299c0996c8a0302bd903db8025
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-25 21:14:36 +0000

    refactor: staff join

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__schoolmint_grow_observation_details.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__schoolmint_grow_observation_details.sql
@@ -1,12 +1,28 @@
 /* tracking for current year */
 select
+    srh.employee_number,
+    srh.preferred_name_lastfirst as teammate,
+    srh.business_unit_home_name as entity,
+    srh.home_work_location_name as `location`,
+    srh.home_work_location_grade_band as grade_band,
+    srh.department_home_name as department,
+    srh.primary_grade_level_taught as grade_taught,
+    srh.job_title,
+    srh.report_to_preferred_name_lastfirst as manager,
+    srh.worker_original_hire_date,
+    srh.assignment_status,
+    srh.sam_account_name,
+    srh.report_to_sam_account_name,
+
     t.type as tracking_type,
     t.code as tracking_code,
     t.name as tracking_rubric,
     t.academic_year as tracking_academic_year,
     t.is_current,
 
-    od.employee_number,
+    os.final_score,
+    os.final_tier,
+
     od.observer_employee_number,
     od.observation_id,
     od.rubric_name,
@@ -26,23 +42,8 @@ select
     od.strand_name,
     od.text_box,
     od.overall_tier,
-    od.sam_account_name,
-    od.report_to_sam_account_name,
-    od.observer_name,
 
-    os.final_score,
-    os.final_tier,
-
-    srh.preferred_name_lastfirst as teammate,
-    srh.business_unit_home_name as entity,
-    srh.home_work_location_name as `location`,
-    srh.home_work_location_grade_band as grade_band,
-    srh.department_home_name as department,
-    srh.primary_grade_level_taught as grade_taught,
-    srh.job_title,
-    srh.report_to_preferred_name_lastfirst as manager,
-    srh.worker_original_hire_date,
-    srh.assignment_status,
+    sro.preferred_name_lastfirst as observer_name,
 
     if(od.observation_id is not null, 1, 0) as is_observed,
     if(
@@ -63,17 +64,17 @@ inner join
     and t.type in ('PMS', 'PMC', 'TR', 'O3', 'WT')
     and t.academic_year = {{ var("current_academic_year") }}
 left join
-    {{ ref("int_performance_management__observations") }} as o
-    on t.type = o.observation_type_abbreviation
-    and srh.employee_number = o.employee_number
-    and o.observed_at between t.start_date and t.end_date
+    {{ ref("int_performance_management__overall_scores") }} as os
+    on srh.employee_number = os.employee_number
+    and t.academic_year = os.academic_year
 left join
     {{ ref("int_performance_management__observation_details") }} as od
-    on o.observation_id = od.observation_id
+    on srh.employee_number = od.employee_number
+    and t.type = od.observation_type_abbreviation
+    and od.observed_at between t.start_date and t.end_date
 left join
-    {{ ref("int_performance_management__overall_scores") }} as os
-    on od.employee_number = os.employee_number
-    and od.academic_year = os.academic_year
+    {{ ref("base_people__staff_roster") }} as sro
+    on od.observer_employee_number = sro.employee_number
 where
     srh.job_title in ('Teacher', 'Teacher in Residence', 'Learning Specialist')
     and srh.assignment_status = 'Active'
@@ -82,13 +83,29 @@ union all
 
 /* actual responses from past years*/
 select
+    srh.employee_number,
+    srh.preferred_name_lastfirst as teammate,
+    srh.business_unit_home_name as entity,
+    srh.home_work_location_name as `location`,
+    srh.home_work_location_grade_band as grade_band,
+    srh.department_home_name as department,
+    srh.primary_grade_level_taught as grade_taught,
+    srh.job_title,
+    srh.report_to_preferred_name_lastfirst as manager,
+    srh.worker_original_hire_date,
+    srh.assignment_status,
+    srh.sam_account_name,
+    srh.report_to_sam_account_name,
+
     null as tracking_type,
     null as tracking_code,
     null as tracking_rubric,
     null as tracking_academic_year,
     false as is_current,
 
-    od.employee_number,
+    os.final_score,
+    os.final_tier,
+
     od.observer_employee_number,
     od.observation_id,
     od.rubric_name,
@@ -108,35 +125,23 @@ select
     od.strand_name,
     od.text_box,
     od.overall_tier,
-    od.sam_account_name,
-    od.report_to_sam_account_name,
-    od.observer_name,
 
-    os.final_score,
-    os.final_tier,
-
-    srh.preferred_name_lastfirst as teammate,
-    srh.business_unit_home_name as entity,
-    srh.home_work_location_name as `location`,
-    srh.home_work_location_grade_band as grade_band,
-    srh.department_home_name as department,
-    srh.primary_grade_level_taught as grade_taught,
-    srh.job_title,
-    srh.report_to_preferred_name_lastfirst as manager,
-    srh.worker_original_hire_date,
-    srh.assignment_status,
+    sro.preferred_name_lastfirst as observer_name,
 
     if(od.observation_id is not null, 1, 0) as is_observed,
     if(
         od.observation_score = 1 and od.observation_type_abbreviation = 'WT', 1, 0
     ) as met_goal_miami,
-from {{ ref("int_performance_management__observation_details") }} as od
-left join
-    {{ ref("int_performance_management__overall_scores") }} as os
-    on od.employee_number = os.employee_number
-    and od.academic_year = os.academic_year
+from {{ ref("base_people__staff_roster_history") }} as srh
 inner join
-    {{ ref("base_people__staff_roster_history") }} as srh
-    on od.employee_number = srh.employee_number
+    {{ ref("int_performance_management__observation_details") }} as od
+    on srh.employee_number = od.employee_number
     and od.observed_at
     between date(srh.work_assignment_start_date) and date(srh.work_assignment_end_date)
+left join
+    {{ ref("int_performance_management__overall_scores") }} as os
+    on srh.employee_number = os.employee_number
+    and od.academic_year = os.academic_year
+left join
+    {{ ref("base_people__staff_roster") }} as sro
+    on od.observer_employee_number = sro.employee_number
