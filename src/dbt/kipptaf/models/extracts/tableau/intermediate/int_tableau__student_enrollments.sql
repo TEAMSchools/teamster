6b9f434e623a253645091598a355843ee006a3c3
Gaby Rangel <grangel@kippteamandfamily.org>
2024-07-29 14:56:01 +0000

    new custom fields for tableau enrollment view

--- src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__student_enrollments.sql
+++ src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__student_enrollments.sql
@@ -1,31 +1,68 @@
+with
+    ms_grad_sub as (
+        select
+            _dbt_source_relation,
+            student_number,
+            school_abbreviation as ms_attended,
+            row_number() over (
+                partition by student_number order by exitdate desc
+            ) as rn,
+        from {{ ref("base_powerschool__student_enrollments") }}
+        where school_level = 'MS'
+    ),
+
+    dlm as (
+        select distinct student_number,
+        from {{ ref("int_students__graduation_path_codes") }}
+        where code = 'M'
+    )
+
 select
-    _dbt_source_relation,
-    student_number,
-    lastfirst as student_name,
-    enroll_status,
-    academic_year,
-    region,
-    schoolid,
-    school_abbreviation as school,
-    grade_level,
-    cohort,
-    is_out_of_district,
-    lunch_status,
-    gender,
+    e._dbt_source_relation,
+    e.academic_year,
+    e.region,
+    e.schoolid,
+    e.school_abbreviation as school,
+    e.student_number,
+    e.lastfirst as student_name,
+    e.grade_level,
+    e.gender,
+    e.enroll_status,
+    e.cohort,
+    e.is_out_of_district,
+    e.lunch_status,
+
+    m.ms_attended,
+
+    adb.contact_id,
+    adb.ktc_cohort,
+    adb.contact_owner_name,
 
     'KTAF' as `organization`,
 
-    if(region = 'Miami', fleid, state_studentnumber) as state_studentnumber,
-    if(spedlep like 'SPED%', 'Has IEP', 'No IEP') as iep_status,
+    if(e.region = 'Miami', e.fleid, e.state_studentnumber) as state_studentnumber,
+    if(e.spedlep like 'SPED%', 'Has IEP', 'No IEP') as iep_status,
+    if(d.student_number is null, false, true) as dlm,
 
     case
-        when school_level in ('ES', 'MS')
-        then advisory_name
-        when school_level = 'HS'
-        then advisor_lastfirst
+        when e.school_level in ('ES', 'MS')
+        then e.advisory_name
+        when e.school_level = 'HS'
+        then e.advisor_lastfirst
     end as advisory,
     case
-        when region in ('Camden', 'Newark') then 'NJ' when region = 'Miami' then 'FL'
+        when e.region in ('Camden', 'Newark')
+        then 'NJ'
+        when e.region = 'Miami'
+        then 'FL'
     end as `state`,
-from {{ ref("base_powerschool__student_enrollments") }}
-where rn_year = 1 and schoolid != 999999
+from {{ ref("base_powerschool__student_enrollments") }} as e
+left join
+    ms_grad_sub as m
+    on e.student_number = m.student_number
+    and {{ union_dataset_join_clause(left_alias="e", right_alias="m") }}
+    and m.rn = 1
+left join
+    {{ ref("int_kippadb__roster") }} as adb on e.student_number = adb.student_number
+left join dlm as d on e.student_number = d.student_number
+where e.rn_year = 1 and e.schoolid != 999999
