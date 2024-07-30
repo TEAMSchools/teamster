4dade52d770b86472c58e617f443dc6bd19d2c3c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-30 15:34:16 +0000

    asdf

--- src/teamster/libraries/powerschool/sis/sensors.py
+++ src/teamster/libraries/powerschool/sis/sensors.py
@@ -1,7 +1,6 @@
 import pendulum
 from dagster import (
     MAX_RUNTIME_SECONDS_TAG,
-    AssetKey,
     AssetMaterialization,
     AssetsDefinition,
     EventLogEntry,
@@ -41,9 +40,9 @@ def build_powerschool_sensor(
         ssh_powerschool: SSHResource,
         db_powerschool: OracleResource,
     ) -> SensorResult | SkipReason:
-        run_requests = []
-
         now = pendulum.now()
+
+        run_requests = []
 
         ssh_tunnel = ssh_powerschool.get_tunnel(remote_port=1521, local_port=1521)
 
@@ -59,24 +58,51 @@ def build_powerschool_sensor(
             raise e
 
         for asset in asset_defs:
-            context.log.info(asset.key)
+            asset_key_identifier = asset.key.to_python_identifier()
 
-            table_name = asset.key.path[-1]
-            partition_column = asset.metadata_by_key[asset.key]["partition_column"]
+            if isinstance(asset.partitions_def, MonthlyPartitionsDefinition):
+                partition_keys = asset.partitions_def.get_partition_keys_in_time_window(
+                    time_window=TimeWindow(
+                        start=latest_materialization_datetime.start_of("month"),
+                        end=now.end_of("month"),
+                    )
+                )
+            elif isinstance(asset.partitions_def, PartitionsDefinition):
+                partition_keys = [asset.partitions_def.get_last_partition_key()]
+            else:
+                partition_keys = []
 
-            latest_materialization_event = _check.inst(
-                context.instance.get_latest_materialization_event(asset.key),
-                EventLogEntry,
+            latest_materialization_event = (
+                context.instance.get_latest_materialization_event(asset.key)
             )
 
-            asset_materialization = _check.inst(
-                latest_materialization_event.asset_materialization, AssetMaterialization
+            if latest_materialization_event is None:
+                run_requests.extend(
+                    [
+                        RunRequest(
+                            run_key=(
+                                f"{asset_key_identifier}_"
+                                f"{partition_key}_"
+                                f"{now.timestamp()}"
+                            ),
+                            asset_selection=[asset.key],
+                            partition_key=partition_key,
+                            tags={MAX_RUNTIME_SECONDS_TAG: max_runtime_seconds},
+                        )
+                        for partition_key in partition_keys
+                    ]
+                )
+
+            asset_materialization = _check.not_none(
+                value=latest_materialization_event.asset_materialization
             )
 
-            latest_materialization_timestamp = (
-                asset_materialization.metadata.get("latest_materialization_timestamp")
-                if latest_materialization_event is not None
-                else None
+            latest_materialization_timestamp = asset_materialization.metadata.get(
+                "latest_materialization_timestamp"
+            )
+
+            records = _check.inst(
+                obj=asset_materialization.metadata.get("records"), ttype=int
             )
 
             latest_materialization_datetime = pendulum.from_timestamp(
@@ -91,11 +117,139 @@ def build_powerschool_sensor(
                 tz=execution_timezone
             ).format("YYYY-MM-DDTHH:mm:ss.SSSSSS")
 
+            try:
+                [(modified_count,)] = _check.inst(
+                    db_powerschool.engine.execute_query(
+                        query=text(
+                            f"SELECT COUNT(*) FROM {asset.key.path[-1]} "
+                            f"WHERE {asset.metadata_by_key[asset.key]["partition_column"]} >= "
+                            f"TO_TIMESTAMP('{latest_materialization_fmt}', "
+                            "'YYYY-MM-DD\"T\"HH24:MI:SS.FF6')"
+                        ),
+                        partition_size=1,
+                        output_format=None,
+                    ),
+                    list,
+                )
+
+                [(partition_count,)] = _check.inst(
+                    db_powerschool.engine.execute_query(
+                        query=text(
+                            f"SELECT COUNT(*) FROM {asset.key.path[-1]} "
+                            f"WHERE {asset.metadata_by_key[asset.key]["partition_column"]} >= "
+                            f"TO_TIMESTAMP('{latest_materialization_fmt}', "
+                            "'YYYY-MM-DD\"T\"HH24:MI:SS.FF6')"
+                        ),
+                        partition_size=1,
+                        output_format=None,
+                    ),
+                    list,
+                )
+            except OperationalError as e:
+                if "DPY-6003" in str(e):
+                    context.log.error(msg=str(e))
+                    return SkipReason(str(e))
+                else:
+                    raise e
+            except Exception as e:
+                context.log.error(msg=str(e))
+                raise e
+
+            if modified_count > 0:
+                context.log.info(asset.key)
+                context.log.info(f"modified_count: {modified_count}")
+
+                context.log.info(partition_keys)
+
+                run_requests.extend(
+                    [
+                        RunRequest(
+                            run_key=(
+                                f"{asset_key_identifier}_"
+                                f"{partition_key}_"
+                                f"{now.timestamp()}"
+                            ),
+                            asset_selection=[asset.key],
+                            partition_key=partition_key,
+                            tags={MAX_RUNTIME_SECONDS_TAG: max_runtime_seconds},
+                        )
+                        for partition_key in partition_keys
+                    ]
+                )
+
+        ssh_tunnel.stop()
+
+        return SensorResult(run_requests=run_requests)
+
+    return _sensor
+
+
+def build_powerschool_partitioned_asset_count_sensor(
+    name,
+    asset_selection: list[AssetsDefinition],
+    max_runtime_seconds,
+    minimum_interval_seconds=None,
+):
+    @sensor(
+        name=name,
+        asset_selection=asset_selection,
+        minimum_interval_seconds=minimum_interval_seconds,
+    )
+    def _sensor(
+        context: SensorEvaluationContext,
+        ssh_powerschool: SSHResource,
+        db_powerschool: OracleResource,
+    ) -> SensorResult | SkipReason:
+        now_timestamp = pendulum.now().timestamp()
+
+        run_requests = []
+
+        ssh_tunnel = ssh_powerschool.get_tunnel(remote_port=1521, local_port=1521)
+
+        try:
+            ssh_tunnel.start()
+        except HandlerSSHTunnelForwarderError as e:
+            if "An error occurred while opening tunnels." in e.args:
+                return SkipReason(str(e))
+            else:
+                raise e
+        except Exception as e:
+            context.log.error(msg=str(e))
+            raise e
+
+        for asset in asset_selection:
+            context.log.info(asset.key)
+
+            partition_column = asset.metadata_by_key[asset.key]["partition_column"]
+            partitions_def = _check.not_none(value=asset.partitions_def)
+
+            last_partition_key = _check.not_none(
+                value=partitions_def.get_last_partition_key()
+            )
+
+            last_partition_key_fmt = pendulum.from_format(
+                string=last_partition_key, fmt="YYYY-MM-DDTHH:mm:ssZZ"
+            ).format("YYYY-MM-DDTHH:mm:ss.SSSSSS")
+
+            latest_materialization_event = _check.inst(
+                context.instance.get_latest_materialization_event(asset.key),
+                EventLogEntry,
+            )
+
+            asset_materialization = _check.inst(
+                obj=latest_materialization_event.asset_materialization,
+                ttype=AssetMaterialization,
+            )
+
+            records = _check.inst(
+                obj=asset_materialization.metadata["records"], ttype=int
+            )
+
             query = text(
                 # trunk-ignore(bandit/B608)
-                f"SELECT COUNT(*) FROM {table_name} "
+                f"SELECT COUNT(*) FROM {asset.key.path[-1]} "
                 f"WHERE {partition_column} >= "
-                f"TO_TIMESTAMP('{latest_materialization_fmt}', "
+                f"TO_TIMESTAMP('{last_partition_key_fmt}', "
                 "'YYYY-MM-DD\"T\"HH24:MI:SS.FF6')"
             )
 
@@ -118,50 +272,18 @@ def build_powerschool_sensor(
 
             context.log.info(f"count: {count}")
 
-            if int(count) > 0:
-                if isinstance(asset.partitions_def, MonthlyPartitionsDefinition):
-                    partition_keys = (
-                        asset.partitions_def.get_partition_keys_in_time_window(
-                            time_window=TimeWindow(
-                                start=latest_materialization_datetime.start_of("month"),
-                                end=now.end_of("month"),
-                            )
-                        )
+            if int(count) > records:
+                run_requests.append(
+                    RunRequest(
+                        run_key=(
+                            f"{asset.key.to_python_identifier()}_{last_partition_key}_"
+                            f"{now_timestamp}"
+                        ),
+                        asset_selection=[asset.key],
+                        partition_key=last_partition_key,
+                        tags={MAX_RUNTIME_SECONDS_TAG: max_runtime_seconds},
                     )
-                elif isinstance(asset.partitions_def, PartitionsDefinition):
-                    partition_keys = [asset.partitions_def.get_last_partition_key()]
-                else:
-                    partition_keys = []
-
-                context.log.info(partition_keys)
-
-                hour_ts = now.start_of("hour").timestamp()
-
-                run_requests.extend(
-                    [
-                        RunRequest(
-                            run_key=(
-                                f"{asset.key.to_python_identifier()}_"
-                                f"{partition_key}_{hour_ts}"
-                            ),
-                            asset_selection=[asset.key],
-                            partition_key=partition_key,
-                            tags={MAX_RUNTIME_SECONDS_TAG: max_runtime_seconds},
-                        )
-                        for partition_key in partition_keys
-                    ]
                 )
-
-                if table_name == "storedgrades":
-                    run_requests.append(
-                        RunRequest(
-                            run_key=f"storedgrades_dcid_{hour_ts}",
-                            asset_selection=[
-                                AssetKey([*asset.key.path[:-1], "storedgrades_dcid"])
-                            ],
-                            tags={MAX_RUNTIME_SECONDS_TAG: max_runtime_seconds},
-                        )
-                    )
 
         ssh_tunnel.stop()
 
