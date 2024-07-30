7318545f4db064b47da6e3db3baeb1458ae7a184
Gaby Rangel <grangel@kippteamandfamily.org>
2024-07-30 13:54:49 +0000

    added counseling and athlete

--- src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__student_enrollments.sql
+++ src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__student_enrollments.sql
@@ -31,6 +31,7 @@ select
     e.enroll_status,
     e.cohort,
     e.is_out_of_district,
+    e.is_self_contained,
     e.lunch_status,
     e.gifted_and_talented,
 
@@ -40,11 +41,13 @@ select
     adb.ktc_cohort,
     adb.contact_owner_name,
 
-    'KTAF' as `organization`,
+    'KTAF' as district,
 
     if(e.region = 'Miami', e.fleid, e.state_studentnumber) as state_studentnumber,
     if(e.spedlep like 'SPED%', 'Has IEP', 'No IEP') as iep_status,
     if(d.student_number is null, false, true) as dlm,
+    if(sp.studentid is not null, 1, null) as is_counseling_services,
+    if(sa.studentid is not null, 1, null) as is_student_athlete,
 
     case
         when e.school_level in ('ES', 'MS')
@@ -52,6 +55,7 @@ select
         when e.school_level = 'HS'
         then e.advisor_lastfirst
     end as advisory,
+
     case
         when e.region in ('Camden', 'Newark')
         then 'NJ'
@@ -67,4 +71,18 @@ left join
 left join
     {{ ref("int_kippadb__roster") }} as adb on e.student_number = adb.student_number
 left join dlm as d on e.student_number = d.student_number
+left join
+    {{ ref("int_powerschool__spenrollments") }} as sp
+    on e.studentid = sp.studentid
+    and {{ union_dataset_join_clause(left_alias="e", right_alias="sp") }}
+    and current_date('{{ var("local_timezone") }}')
+    between sp.enter_date and sp.exit_date
+    and sp.specprog_name = 'Counseling Services'
+left join
+    {{ ref("int_powerschool__spenrollments") }} as sa
+    on e.studentid = sa.studentid
+    and {{ union_dataset_join_clause(left_alias="e", right_alias="sa") }}
+    and current_date('{{ var("local_timezone") }}')
+    between sa.enter_date and sa.exit_date
+    and sa.specprog_name = 'Student Athlete'
 where e.rn_year = 1 and e.schoolid != 999999
