47be038aab343d4f7c3ae96221fd37f336d0ad53
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-31 20:34:34 +0000

    refactor: use datetimes

--- src/dbt/kipptaf/models/smartrecruiters/staging/stg_smartrecruiters__applications.sql
+++ src/dbt/kipptaf/models/smartrecruiters/staging/stg_smartrecruiters__applications.sql
@@ -7,36 +7,33 @@ with
             job_title,
             job_city,
             recruiters,
+            application_status,
             application_field_phone_interview_score as phone_interview_score,
             application_field_resume_score as resume_score,
             application_reason_for_rejection as reason_for_rejection,
             application_reason_for_withdrawal as reason_for_withdrawal,
-            application_status,
-            cast(application_state_hired_date as timestamp) as hired_date_timestamp,
-            cast(application_state_new_date as timestamp) as new_date_timestamp,
-            cast(application_state_offer_date as timestamp) as offer_date_timestamp,
-            cast(
-                application_state_rejected_date as timestamp
-            ) as rejected_date_timestamp,
-            cast(
-                application_status_interview_demo_date as timestamp
-            ) as demo_date_timestamp,
-            cast(
-                application_status_interview_phone_screen_complete_date as timestamp
-            ) as phone_screen_complete_date_timestamp,
-            cast(
-                application_status_interview_phone_screen_requested_date as timestamp
-            ) as phone_screen_requested_date_timestamp,
+
+            datetime(application_state_hired_date) as hired_datetime,
+            datetime(application_state_new_date) as new_datetime,
+            datetime(application_state_offer_date) as offer_datetime,
+            datetime(application_state_rejected_date) as rejected_datetime,
+            datetime(application_status_interview_demo_date) as demo_datetime,
+            datetime(
+                application_status_interview_phone_screen_complete_date
+            ) as phone_screen_complete_datetime,
+            datetime(
+                application_status_interview_phone_screen_requested_date
+            ) as phone_screen_requested_datetime,
         from {{ source("smartrecruiters", "src_smartrecruiters__applications") }}
     )
 
 select
     a.*,
 
-    cast(new_date_timestamp as date) as new_date,
-    cast(phone_screen_complete_date_timestamp as date) as phone_screen_complete_date,
-    cast(phone_screen_requested_date_timestamp as date) as phone_screen_requested_date,
-    cast(demo_date_timestamp as date) as demo_date,
-    cast(offer_date_timestamp as date) as offer_date,
-    cast(hired_date_timestamp as date) as hired_date,
+    date(new_datetime) as new_date,
+    date(phone_screen_complete_datetime) as phone_screen_complete_date,
+    date(phone_screen_requested_datetime) as phone_screen_requested_date,
+    date(demo_datetime) as demo_date,
+    date(offer_datetime) as offer_date,
+    date(hired_datetime) as hired_date,
 from applications as a
