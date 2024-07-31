a3c72eef675674b4d6807305469d6f2f53c6e523
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-07-31 02:47:19 +0000

    refactor: rename

--- src/teamster/code_locations/kippnewark/powerschool/schedules.py
+++ src/teamster/code_locations/kippnewark/powerschool/schedules.py
@@ -5,9 +5,9 @@ from teamster.code_locations.kippnewark.powerschool.assets import (
     powerschool_table_assets_no_partition,
 )
 
-nonpartition_asset_job_schedule = ScheduleDefinition(
+powerschool_sis_asset_no_partition_job_schedule = ScheduleDefinition(
     job=define_asset_job(
-        name=f"{CODE_LOCATION}_powerschool_nonpartition_asset_job",
+        name=f"{CODE_LOCATION}_powerschool_sis_assets_no_partition_job",
         selection=powerschool_table_assets_no_partition,
     ),
     cron_schedule="0 0 * * *",
@@ -15,5 +15,5 @@ nonpartition_asset_job_schedule = ScheduleDefinition(
 )
 
 schedules = [
-    nonpartition_asset_job_schedule,
+    powerschool_sis_asset_no_partition_job_schedule,
 ]
