3e4064a0b350891d0e387ad678418e473f95809b
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-08-07 19:22:23 +0000

    first pass staging and rpt

---
+++ src/dbt/kipptaf/models/performance_management/staging/stg_performance_management__teacher_development_observation_details.sql
@@ -0,0 +1,11 @@
+select
+    obvs_micro_id as row_id,
+    observation_id,
+    employee_number,
+    microgoal_id,
+    esog as text_box,
+    row_score,
+    measurement_name,
+    strand_name,
+from {{ source("google_appsheet", "src_teacher_development__observation_details_archive") }}
+where esog is not null
