7321b36f7f9fedd94bfcea2ecaf71a8b93f3c629
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-08-09 17:49:13 +0000

    int view to union to new data coming from schoolmint grow, reporting view, adding course and grade columns

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__teacher_development.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__teacher_development.sql
@@ -1,69 +1,50 @@
-
 select
-    od.observer_employee_number,
-    od.observation_id,
-    od.rubric_name,
-    od.observation_score,
-    od.observed_at,
-    od.academic_year,
-    od.observation_type,
-    od.observation_type_abbreviation,
-    od.row_score,
-    od.measurement_name,
-    od.strand_name,
-    od.text_box,
-    null as observation_notes
-    null as observation_subject
-    null as growth_area,
-    null as growth_notes,
-    null as glow_area,
-    null as glow_notes,
-    null as observer_team,
-from {{ ref("int_performance_management__observation_details") }} as od
-where observation_type_abbreviation = 'TDT'
+    td.employee_number,
+    td.observer_employee_number,
+    td.observation_id,
+    td.rubric_name,
+    td.observation_score,
+    td.observed_at,
+    td.academic_year,
+    td.observation_type,
+    td.observation_type_abbreviation,
+    td.observation_subject,
+    td.observation_grade,
+    td.row_score,
+    td.measurement_name,
+    td.strand_name,
+    td.text_box,
+    td.growth_area,
+    td.glow_area,
 
-union all
+    srh.preferred_name_lastfirst as teammate,
+    srh.business_unit_home_name as entity,
+    srh.home_work_location_name as `location`,
+    srh.home_work_location_grade_band as grade_band,
+    srh.department_home_name as department,
+    srh.primary_grade_level_taught as grade_taught,
+    srh.job_title,
+    srh.report_to_preferred_name_lastfirst as manager,
+    srh.worker_original_hire_date,
+    srh.assignment_status,
+    srh.sam_account_name,
+    srh.report_to_sam_account_name,
 
-select
-    null as observer_employee_number,
-    o.observation_id,
-    concat('Teacher Development: ',o.observation_type) as rubric_name,
-    average(row_score) as observation_score,
-    o.observed_at,
-    '2023' as academic_year,
-    'Teacher Development' as observation_type,
-    'TDT' as observation_type_abbreviation,
-    od.row_score,
-    od.measurement_name,
-    od.strand_name,
-    od.text_box,
+    coalesce(sr.preferred_name_lastfirst, td.observer_name) as observer_name,
+    if(sr.department_home_name = 'Teacher Development', 'TDT', 'NTNC') as observer_team,
 
-    o.observation_notes,
-    o.observation_subject,
-    o.growth_area,
-    o.growth_notes,
-    o.glow_area,
-    o.glow_notes,
-    o.observer_team,
-from {{ ref('stg_performance_management__teacher_development_observations') }} as o
-left join {{ ref('stg_performance_management__teacher_development_observation_details') }} as od
-on o.observation_id = od.observation_id
-
-
-
-
-    -- srh.employee_number,
-    -- srh.preferred_name_lastfirst as teammate,
-    -- srh.business_unit_home_name as entity,
-    -- srh.home_work_location_name as `location`,
-    -- srh.home_work_location_grade_band as grade_band,
-    -- srh.department_home_name as department,
-    -- srh.primary_grade_level_taught as grade_taught,
-    -- srh.job_title,
-    -- srh.report_to_preferred_name_lastfirst as manager,
-    -- srh.worker_original_hire_date,
-    -- srh.assignment_status,
-    -- srh.sam_account_name,
-    -- srh.report_to_sam_account_name,
-
-    -- sro.preferred_name_lastfirst as observer_name,
+    os.final_score as performance_management_final_score,
+    os.final_tier as performance_management_final_tier,
+from {{ ref("int_performance_management__teacher_development") }} as td
+left join
+    {{ ref("base_people__staff_roster_history") }} as srh
+    on td.employee_number = srh.employee_number
+    and cast(td.observed_at as timestamp)
+    between srh.work_assignment_start_date and srh.work_assignment_end_date
+left join
+    {{ ref("base_people__staff_roster") }} as sr
+    on td.observer_employee_number = sr.employee_number
+left join
+    {{ ref("int_performance_management__overall_scores") }} as os
+    on td.employee_number = os.employee_number
+    and td.academic_year = os.academic_year
