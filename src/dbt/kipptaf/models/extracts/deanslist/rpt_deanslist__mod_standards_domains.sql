f021bf0d55e7d60cbb804223483138a9035ab938
Anthony Walters <awalters@kippteamandfamily.org>
2024-08-12 19:30:57 +0000

    new assessment model retrofit for DL extract

--- src/dbt/kipptaf/models/extracts/deanslist/rpt_deanslist__mod_standards_domains.sql
+++ src/dbt/kipptaf/models/extracts/deanslist/rpt_deanslist__mod_standards_domains.sql
@@ -1,69 +1,101 @@
 with
-    response_rollup as (
-        select
-            sr.powerschool_student_number,
-            sr.academic_year,
-            sr.term_administered,
-            sr.percent_correct,
-
-            st.standard_domain,
-
-            if(
-                sr.subject_area = 'Writing', 'Text Study', sr.subject_area
-            ) as subject_area,
-        from {{ ref("int_assessments__response_rollup") }} as sr
-        inner join
-            {{ ref("stg_assessments__standard_domains") }} as st
-            on sr.response_type_code = st.standard_code
-        where
-            sr.response_type = 'standard'
-            and sr.is_internal_assessment
-            and sr.academic_year = {{ var("current_academic_year") }}
-            and sr.subject_area in ('Text Study', 'Mathematics', 'Writing')
-            and sr.module_type in ('QA', 'MQQ', 'GWT')
-            and sr.powerschool_student_number in (
-                select student_number,
-                from {{ ref("stg_powerschool__students") }}
-                where
-                    grade_level < 5
-                    and not regexp_contains(_dbt_source_relation, r'kippmiami')
-            )
+    progress_report_region as (
+        select assessment_id, progress_report_region,
+        from
+            {{ ref("stg_google_appsheet__illuminate_assessments_extension") }},
+            unnest(split(regions_progress_report, ' , ')) as progress_report_region
     ),
 
-    standard_avg as (
-        select
-            powerschool_student_number,
-            academic_year,
-            term_administered,
-            subject_area,
-            standard_domain,
-            round(avg(percent_correct), 0) as avg_percent_correct,
-        from response_rollup
-        group by
-            powerschool_student_number,
-            academic_year,
-            term_administered,
-            subject_area,
-            standard_domain
+    report_card_region as (
+        select assessment_id, report_card_region,
+        from
+            {{ ref("stg_google_appsheet__illuminate_assessments_extension") }},
+            unnest(split(regions_report_card, ' , ')) as report_card_region
     )
 
 select
-    powerschool_student_number as local_student_id,
-    academic_year,
-    term_administered,
-    subject_area,
-    standard_domain,
-    avg_percent_correct,
+    'Progress Report' as report_type,
+
+    ar.academic_year,
+    ar.powerschool_student_number as student_number,
+    ar.subject_area,
+    ar.term_administered,
+
+    null as standard_domain,
+    round(avg(ar.percent_correct), 2) as avg_percent_correct,
     case
-        when avg_percent_correct >= 85
+        when round(avg(ar.percent_correct), 2) >= 85
         then 'Exceeds Expectations'
-        when avg_percent_correct >= 70
+        when round(avg(ar.percent_correct), 2) >= 70
         then 'Met Expectations'
-        when avg_percent_correct >= 50
+        when round(avg(ar.percent_correct), 2) >= 50
         then 'Approaching Expectations'
-        when avg_percent_correct >= 30
+        when round(avg(ar.percent_correct), 2) >= 30
         then 'Below Expectations'
-        when avg_percent_correct >= 0
+        when round(avg(ar.percent_correct), 2) >= 0
         then 'Far Below Expectations'
     end as performance_level,
-from standard_avg
+from {{ ref("int_assessments__response_rollup") }} as ar
+inner join
+    {{ ref("base_powerschool__student_enrollments") }} as co
+    on ar.academic_year = co.academic_year
+    and ar.powerschool_student_number = co.student_number
+    and co.rn_year = 1
+    and co.grade_level < 5
+inner join
+    progress_report_region as r
+    on ar.assessment_id = r.assessment_id
+    and co.region = r.progress_report_region
+where ar.response_type = 'overall'
+group by
+    ar.academic_year,
+    ar.powerschool_student_number,
+    ar.subject_area,
+    ar.term_administered
+
+union all
+
+select
+    'Report Card' as report_type,
+
+    ar.academic_year,
+    ar.powerschool_student_number as student_number,
+    ar.subject_area,
+    ar.term_administered,
+
+    sd.standard_domain,
+
+    round(avg(ar.percent_correct), 2) as avg_percent_correct,
+    case
+        when round(avg(ar.percent_correct), 2) >= 85
+        then 'Exceeds Expectations'
+        when round(avg(ar.percent_correct), 2) >= 70
+        then 'Met Expectations'
+        when round(avg(ar.percent_correct), 2) >= 50
+        then 'Approaching Expectations'
+        when round(avg(ar.percent_correct), 2) >= 30
+        then 'Below Expectations'
+        when round(avg(ar.percent_correct), 2) >= 0
+        then 'Far Below Expectations'
+    end as performance_level,
+from {{ ref("int_assessments__response_rollup") }} as ar
+inner join
+    {{ ref("stg_assessments__standard_domains") }} as sd
+    on ar.response_type_code = sd.standard_code
+inner join
+    {{ ref("base_powerschool__student_enrollments") }} as co
+    on ar.academic_year = co.academic_year
+    and ar.powerschool_student_number = co.student_number
+    and co.rn_year = 1
+    and co.grade_level < 5
+inner join
+    report_card_region as r
+    on ar.assessment_id = r.assessment_id
+    and co.region = r.progress_report_region
+where ar.response_type = 'standard'
+group by
+    ar.academic_year,
+    ar.powerschool_student_number,
+    ar.subject_area,
+    ar.term_administered,
+    sd.standard_domain
