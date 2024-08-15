9b2e397c0c64674f07f52cb70776599709f3d6de
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-08-15 22:54:30 +0000

    adding historical to current

--- src/dbt/kipptaf/models/google/appsheet/intermediate/int__seat_tracker_snapshot.sql
+++ src/dbt/kipptaf/models/google/appsheet/intermediate/int__seat_tracker_snapshot.sql
@@ -1,41 +1,73 @@
+with
+    combined_snapshot as (
+        select
+            staffing_model_id,
+            academic_year,
+            staffing_status,
+            status_detail,
+            mid_year_hire as is_mid_year_hire,
+            plan_status,
+            cast(dbt_valid_from as date) as valid_from,
+            cast(dbt_valid_to as date) as valid_to,
+            cast(teammate as string) as teammate,
+            if(staffing_status = 'Open', true, false) as is_open,
+            if(staffing_status = 'Staffed', true, false) as is_staffed,
+            if(plan_status in ('Active', 'TRUE'), true, false) as is_active,
+            if(
+                status_detail in ('New Hire', 'Transfer In'), true, false
+            ) as is_new_hire,
+        from {{ ref("snapshot__seat_tracker__seats") }}
+        /*last day of manual snapshot from appsheet*/
+        where dbt_updated_at > ('2024-08-07')
+
+        union all
+
+        select
+            staffing_model_id,
+            academic_year,
+            staffing_status,
+            status_detail,
+            is_mid_year_hire,
+            plan_status,
+            valid_from,
+            null as valid_to,
+            teammate,
+            is_open,
+            is_active,
+            is_staffed,
+            is_new_hire,
+        from {{ ref("stg_seat_tracker__log_archive") }}
+    ),
+
+    ordered_snapshot as (
+        select
+            *,
+            row_number() over (
+                partition by staffing_model_id order by valid_from desc
+            ) as rn_valid_from,
+        from combined_snapshot
+    )
+
 select
-    staffing_model_id,
-    staffing_status,
-    status_detail,
-    mid_year_hire as is_mid_year_hire,
-    plan_status,
-    cast(dbt_valid_from as date) as valid_from,
-    cast(dbt_valid_to as date) as valid_to,
-    cast(academic_year as string) as academic_year,
-    cast(teammate as string) as teammate,
-    if(staffing_status = 'Open', true, false) as is_open,
-    if(staffing_status = 'Staffed', true, false) as is_staffed,
-    if(plan_status in ('Active', 'TRUE'), true, false) as is_active,
-    if(status_detail in ('New Hire', 'Transfer In'), true, false) as is_new_hire,
-from {{ ref("snapshot__seat_tracker__seats") }}
-where dbt_updated_at > ('2024-08-07')
-
-union all
-
-select
-    la1.staffing_model_id,
-    la1.staffing_status,
-    la1.status_detail,
-    la1.is_mid_year_hire,
-    la1.plan_status,
-    la1.valid_from,
-    /* using the previous export date minus 1 day as valid_to, 
-    if no next export date then using date of last app export, 2024-08-07*/
-    if(la2.valid_from - 1 is null, date(2024, 08, 07), la2.valid_from - 1) as valid_to,
-    la1.academic_year,
-    la1.teammate,
-    la1.is_open,
-    la1.is_active,
-    la1.is_staffed,
-    la1.is_new_hire,
-
-from {{ ref("stg_seat_tracker__log_archive") }} as la1
+    os1.staffing_model_id,
+    os1.staffing_status,
+    os1.status_detail,
+    os1.is_mid_year_hire,
+    os1.plan_status,
+    os1.academic_year,
+    os1.teammate,
+    os1.is_open,
+    os1.is_staffed,
+    os1.is_active,
+    os1.is_new_hire,
+    os1.valid_from,
+    case
+    when os1.academic_year >= {{ var('current_academic_year') }} 
+    then COALESCE(os1.valid_to, os2.valid_from - 1)
+    else date(os1.academic_year, 06,30)
+    end as valid_to,
+from ordered_snapshot as os1
 left join
-    {{ ref("stg_seat_tracker__log_archive") }} as la2
-    on la1.staffing_model_id = la2.staffing_model_id
-    and la1.rn_valid_from - 1 = la2.rn_valid_from
+    ordered_snapshot as os2
+    on os1.staffing_model_id = os2.staffing_model_id
+    and os1.rn_valid_from - 1 = os2.rn_valid_from
