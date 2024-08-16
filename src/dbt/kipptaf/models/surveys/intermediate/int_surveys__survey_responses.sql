548ee52688c3657a38034f7ca754e9029b82e6a0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-08-16 14:58:57 +0000

    refactor: rm employees

--- src/dbt/kipptaf/models/surveys/intermediate/int_surveys__survey_responses.sql
+++ src/dbt/kipptaf/models/surveys/intermediate/int_surveys__survey_responses.sql
@@ -12,28 +12,6 @@ select
     rt.type as survey_type,
     rt.academic_year,
 
-    eh.employee_number,
-    eh.preferred_name_lastfirst as respondent_name,
-    eh.management_position_indicator as is_manager,
-    eh.department_home_name as respondent_department_name,
-    eh.business_unit_home_name as respondent_legal_entity_name,
-    eh.report_to_preferred_name_lastfirst as respondent_manager_name,
-    eh.job_title as respondent_primary_job,
-    eh.home_work_location_name as respondent_primary_site,
-    eh.race_ethnicity_reporting,
-    eh.gender_identity as gender,
-    eh.mail,
-    eh.report_to_preferred_name_lastfirst as manager_name,
-    eh.report_to_mail as manager_email,
-    eh.report_to_user_principal_name as manager_user_principal_name,
-    eh.alumni_status,
-    eh.community_grew_up,
-    eh.community_professional_exp,
-    eh.level_of_education,
-    eh.assignment_status,
-
-    tgl.grade_level as primary_grade_level_taught,
-
     safe_cast(fr.text_value as numeric) as answer_value,
     timestamp(fr.create_time) as date_started,
     timestamp(fr.last_submitted_time) as date_submitted,
@@ -48,19 +26,9 @@ select
     if(safe_cast(fr.text_value as int) is null, 1, 0) as is_open_ended,
 from {{ ref("base_google_forms__form_responses") }} as fr
 left join
-    {{ ref("base_people__staff_roster_history") }} as eh
-    on fr.respondent_email = eh.google_email
-    and timestamp(fr.last_submitted_time)
-    between eh.work_assignment_start_date and eh.work_assignment_end_date
-left join
     {{ ref("stg_reporting__terms") }} as rt
     on rt.name = fr.info_title
     and date(fr.last_submitted_time) between rt.start_date and rt.end_date
-left join
-    {{ ref("int_powerschool__teacher_grade_levels") }} as tgl
-    on eh.powerschool_teacher_number = tgl.teachernumber
-    and rt.academic_year = tgl.academic_year
-    and tgl.grade_level_rank = 1
 
 union all
 
@@ -77,28 +45,6 @@ select
     coalesce(regexp_extract(sr.campaign_name, r'\s(.*)'), rt.code) as survey_code,
     'SURVEY' as survey_type,
     coalesce(sr.campaign_fiscal_year - 1, rt.academic_year) as academic_year,
-
-    eh.employee_number,
-    eh.preferred_name_lastfirst as respondent_name,
-    eh.management_position_indicator as is_manager,
-    eh.department_home_name as respondent_department_name,
-    eh.business_unit_home_name as respondent_legal_entity_name,
-    eh.report_to_preferred_name_lastfirst as respondent_manager_name,
-    eh.job_title as respondent_primary_job,
-    eh.home_work_location_name as respondent_primary_site,
-    eh.race_ethnicity_reporting,
-    eh.gender_identity as gender,
-    eh.mail,
-    eh.report_to_preferred_name_lastfirst as manager_name,
-    eh.report_to_mail as manager_email,
-    eh.report_to_user_principal_name as manager_user_principal_name,
-    eh.alumni_status,
-    eh.community_grew_up,
-    eh.community_professional_exp,
-    eh.level_of_education,
-    eh.assignment_status,
-
-    tgl.grade_level as primary_grade_level_taught,
 
     safe_cast(sr.response_value as numeric) as answer_value,
 
@@ -119,13 +65,3 @@ left join
     {{ ref("int_surveys__response_identifiers") }} as ri
     on sr.survey_id = ri.survey_id
     and sr.response_id = ri.response_id
-left join
-    {{ ref("base_people__staff_roster_history") }} as eh
-    on ri.respondent_employee_number = eh.employee_number
-    and sr.response_date_submitted
-    between eh.work_assignment_start_date and eh.work_assignment_end_date
-left join
-    {{ ref("int_powerschool__teacher_grade_levels") }} as tgl
-    on eh.powerschool_teacher_number = tgl.teachernumber
-    and rt.academic_year = tgl.academic_year
-    and tgl.grade_level_rank = 1
