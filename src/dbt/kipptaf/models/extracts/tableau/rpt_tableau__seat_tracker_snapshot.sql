0a6e904e62af3e32b3cb9902828078d95babb28b
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-08-16 15:05:00 +0000

    adding date scaffold

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
@@ -1,17 +1,24 @@
-select
-    log_id,
-    export_date,
-    academic_year as snapshot_academic_year,
-    staffing_model_id as snapshot_staffing_model_id,
-    teammate as snapshot_teammate,
-    staffing_status as snapshot_staffing_status,
-    status_detail as snapshot_status_detail,
-    plan_status as snapshot_plan_status,
-    is_mid_year_hire as snapshot_mid_year_hire,
-    is_mid_year_hire_int as snapshot_mid_year_hire_int,
+with reporting_dates as (
 
-    if(is_open, 1, 0) as snapshot_open,
-    if(is_new_hire, 1, 0) as snapshot_new_hire,
-    if(is_staffed, 1, 0) as snapshot_staffed,
-    if(is_active, 1, 0) as snapshot_active,
-from {{ ref("stg_seat_tracker__log_archive") }}
+select start_date, end_date
+from {{ ref('stg_reporting__terms') }}
+where type = 'REC'
+   
+
+
+select
+    ts.staffing_model_id,
+    ts.valid_from,
+    ts.valid_to,
+    ts.staffing_status,
+    ts.status_detail,
+    ts.plan_status,
+    ts.academic_year,
+    ts.teammate,
+    if(ts.is_open, 1, 0) as snapshot_open,
+    if(ts.is_new_hire, 1, 0) as snapshot_new_hire,
+    if(ts.is_staffed, 1, 0) as snapshot_staffed,
+    if(ts.is_active, 1, 0) as snapshot_active,
+    if(ts.is_mid_year_hire, 1, 0) as snapshot_mid_year_hire,
+
+from {{ ref("int_seat_tracker_snapshot") }} as ts
