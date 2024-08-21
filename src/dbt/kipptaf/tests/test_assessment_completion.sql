95e1cdbb1609e32ce00bc8be970e3cdfd883e459
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-08-21 21:12:04 +0000

    refactor: rm week scaffold

--- src/dbt/kipptaf/tests/test_assessment_completion.sql
+++ src/dbt/kipptaf/tests/test_assessment_completion.sql
@@ -1,41 +1,23 @@
 {{- config(severity="warn") -}}
 
 with
-    week_scaffold as (
-        /* monday thru sunday */
-        select
-            date_add(date_trunc(date_value, week), interval 1 day) as week_start_date,
-            date_add(date_trunc(date_value, week), interval 7 day) as week_end_date,
-        from
-            unnest(
-                generate_date_array(
-                    '{{ var("current_academic_year") }}-07-01',
-                    '{{ var("current_academic_year") + 1 }}-06-30',
-                    interval 1 week
-                )
-            ) as date_value
-    ),
-
     week_assessment_scaffold as (
         select
-            ws.week_end_date,
+            assessment_id,
+            title,
+            administered_at,
+            region,
 
-            a.assessment_id,
-            a.title,
-            a.administered_at,
-            a.region,
-
-            avg(if(a.student_assessment_id is not null, 1, 0)) as pct_completion,
-        from week_scaffold as ws
-        inner join
-            {{ ref("int_assessments__scaffold") }} as a
-            on a.administered_at between ws.week_start_date and ws.week_end_date
-            and a.is_internal_assessment
-            and not a.is_replacement
-        where ws.week_end_date <= current_date('{{ var("local_timezone") }}')
+            avg(if(student_assessment_id is not null, 1, 0)) as pct_completion,
+        from {{ ref("int_assessments__scaffold") }}
+        where
+            academic_year = {{ var("current_academic_year") }}
+            and is_internal_assessment
+            and not is_replacement
+            and administered_at < current_date('{{ var("local_timezone") }}')
         group by all
     )
 
-select *
+select *,
 from week_assessment_scaffold
 where pct_completion < 0.5
