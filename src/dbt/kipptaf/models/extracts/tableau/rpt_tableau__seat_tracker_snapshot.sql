cdcdc7631bdcd3cdfce2e6fe0e488925f278f3aa
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-08-21 14:36:20 +0000

    format

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
@@ -1,17 +1,19 @@
-with date_spine as (
-{%- set end_date = (
-    "cast('" + var("current_fiscal_year") | string + "-06-30' as date)"
-) -%}
+with
+    date_spine as (
+        {%- set end_date = (
+            "cast('" + var("current_fiscal_year")
+            | string + "-06-30' as date)"
+        ) -%}
 
-{{-
-    dbt_utils.date_spine(
-        start_date="cast('2023-08-04' as date)",
-        end_date=end_date,
-        datepart="week",
+        {{-
+            dbt_utils.date_spine(
+                start_date="cast('2023-08-04' as date)",
+                end_date=end_date,
+                datepart="week",
+            )
+        -}}
+
     )
--}}
-
-)
 
 select
     ds.date_week,
@@ -30,5 +32,6 @@ select
     if(ts.is_mid_year_hire, 1, 0) as snapshot_mid_year_hire,
 
 from date_spine as ds
-left join {{ ref("int_seat_tracker_snapshot") }} as ts
-on ds.date_week between ts.valid_from and ts.valid_to
+left join
+    {{ ref("int_seat_tracker_snapshot") }} as ts
+    on ds.date_week between ts.valid_from and ts.valid_to
