48120ed6daf7c57e69ec11c5a887f9d3dfd060db
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-08-22 20:57:44 +0000

    refactor: calcs to staging

--- src/dbt/kipptaf/models/adp/workforce_now/staging/stg_adp_workforce_now__person_history.sql
+++ src/dbt/kipptaf/models/adp/workforce_now/staging/stg_adp_workforce_now__person_history.sql
@@ -7,8 +7,40 @@ with
                 order_by="_fivetran_synced desc",
             )
         }}
+    ),
+
+    transformations as (
+        -- trunk-ignore(sqlfluff/AM04)
+        select
+            * except (preferred_name_given_name, preferred_name_middle_name),
+
+            case
+                race_long_name
+                when 'Black or African American'
+                then 'Black/African American'
+                when 'Hispanic or Latino'
+                then 'Latinx/Hispanic/Chicana(o)'
+                when 'Two or more races (Not Hispanic or Latino)'
+                then 'Bi/Multiracial'
+                else race_long_name
+            end as race_ethnicity_reporting,
+
+            coalesce(
+                preferred_name_given_name, legal_name_given_name
+            ) as preferred_name_given_name,
+            coalesce(
+                preferred_name_middle_name, legal_name_middle_name
+            ) as preferred_name_middle_name,
+            coalesce(
+                preferred_name_family_name_1, legal_name_family_name_1
+            ) as preferred_name_family_name,
+        from deduplicate
     )
 
--- trunk-ignore(sqlfluff/AM04)
-select *,
-from deduplicate
+select
+    *,
+
+    preferred_name_family_name
+    || ', '
+    || preferred_name_given_name as preferred_name_lastfirst,
+from transformations
