cb191b6c9139e1ee9f52783f3834f35c5aaa0b5f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-08-23 16:59:24 +0000

    refactor: overgrad to regions

--- src/teamster/code_locations/kipptaf/overgrad/assets.py
+++ src/teamster/code_locations/kipptaf/overgrad/assets.py
@@ -1,35 +1,8 @@
 from dagster import AutomationCondition, DynamicPartitionsDefinition
 
 from teamster.code_locations.kipptaf import CODE_LOCATION
-from teamster.code_locations.kipptaf.overgrad.schema import (
-    ADMISSION_SCHEMA,
-    CUSTOM_FIELD_SCHEMA,
-    FOLLOWING_SCHEMA,
-    SCHOOL_SCHEMA,
-    STUDENT_SCHEMA,
-    UNIVERSITY_SCHEMA,
-)
+from teamster.code_locations.kipptaf.overgrad.schema import UNIVERSITY_SCHEMA
 from teamster.libraries.overgrad.assets import build_overgrad_asset
-
-schools = build_overgrad_asset(
-    code_location=CODE_LOCATION, name="schools", schema=SCHOOL_SCHEMA
-)
-
-students = build_overgrad_asset(
-    code_location=CODE_LOCATION, name="students", schema=STUDENT_SCHEMA
-)
-
-custom_fields = build_overgrad_asset(
-    code_location=CODE_LOCATION, name="custom_fields", schema=CUSTOM_FIELD_SCHEMA
-)
-
-admissions = build_overgrad_asset(
-    code_location=CODE_LOCATION, name="admissions", schema=ADMISSION_SCHEMA
-)
-
-followings = build_overgrad_asset(
-    code_location=CODE_LOCATION, name="followings", schema=FOLLOWING_SCHEMA
-)
 
 universities = build_overgrad_asset(
     code_location=CODE_LOCATION,
@@ -37,14 +10,8 @@ universities = build_overgrad_asset(
     schema=UNIVERSITY_SCHEMA,
     partitions_def=DynamicPartitionsDefinition(name="overgrad__universities__id"),
     automation_condition=AutomationCondition.missing(),
-    deps=[admissions.key, followings.key],
 )
 
 assets = [
-    admissions,
-    custom_fields,
-    followings,
-    schools,
-    students,
     universities,
 ]
