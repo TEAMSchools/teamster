ff26c95c582233eb7edc19f8a2ec4aef8fba775f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-08-26 20:48:38 +0000

    feat: more custom fields

--- src/dbt/kipptaf/models/adp/workforce_now/workers/intermediate/int_adp_workforce_now__workers__custom_fields__pivot.sql
+++ src/dbt/kipptaf/models/adp/workforce_now/workers/intermediate/int_adp_workforce_now__workers__custom_fields__pivot.sql
@@ -15,10 +15,16 @@ select
     associate_oid,
 
     string_value_employeenumber as employee_number,
+    code_value_lifeexperienceincommunitiesweserve
+    as life_experience_in_communities_we_serve,
     string_value_miamiacesnumber as miami_aces_number,
     string_value_njpensionnumber as nj_pension_number,
+    code_value_preferredraceethnicity as preferred_race_ethnicity,
+    code_value_professionalexperienceincommunitiesweserve
+    as professional_experience_in_communities_we_serve,
     indicator_value_receivedsignonbonus as received_sign_on_bonus,
     code_value_remoteworkstatus as remote_work_status,
+    code_value_teacherprepprogram as teacher_prep_program,
     code_value_wfmgraccrualprofile as wf_mgr_accrual_profile,
     string_value_wfmgrbadgenumber as wf_mgr_badge_number,
     code_value_wfmgreetype as wf_mgr_ee_type,
@@ -36,10 +42,14 @@ from
         max(code_value) as code_value
         for regexp_replace(name_code__code_value, r'\W', '') in (
             'EmployeeNumber',
+            'LifeExperienceinCommunitiesWeServe',
             'MiamiACESNumber',
             'NJPensionNumber',
+            'PreferredRaceEthnicity',
+            'ProfessionalExperienceinCommunitiesWeServe',
             'ReceivedSignOnBonus',
             'RemoteWorkStatus',
+            'TeacherPrepProgram',
             'WFMgrAccrualProfile',
             'WFMgrBadgeNumber',
             'WFMgrEEType',
