ffc28d4246e76f0be7280a55be8f764a4f57984f
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-08-26 20:48:38 +0000

    feat: more custom fields

--- src/dbt/kipptaf/models/adp/workforce_now/workers/intermediate/int_adp_workforce_now__workers__work_assignments__custom_fields.sql
+++ src/dbt/kipptaf/models/adp/workforce_now/workers/intermediate/int_adp_workforce_now__workers__work_assignments__custom_fields.sql
@@ -1,57 +1,50 @@
 with
-    workers_current_record as (
+    work_assignments as (
         select
             associate_oid,
+            item_id,
+            effective_date_start,
+            effective_date_end,
+            effective_date_timestamp,
+            is_current_record,
             custom_field_group__code_fields,
             custom_field_group__date_fields,
             custom_field_group__indicator_fields,
             custom_field_group__multi_code_fields,
             custom_field_group__number_fields,
             custom_field_group__string_fields,
-            person__custom_field_group__code_fields,
-            person__custom_field_group__date_fields,
-            person__custom_field_group__indicator_fields,
-            person__custom_field_group__multi_code_fields,
-            person__custom_field_group__number_fields,
-            person__custom_field_group__string_fields,
-        from {{ ref("stg_adp_workforce_now__workers") }}
-        where is_current_record
+        from {{ ref("stg_adp_workforce_now__workers__work_assignments") }}
     ),
 
     multi_code_fields as (
         select
-            wcr.associate_oid,
+            wa.associate_oid,
+            wa.item_id,
+            wa.effective_date_start,
+            wa.effective_date_end,
+            wa.effective_date_timestamp,
+            wa.is_current_record,
 
-            cfg.itemid as item_id,
+            cfg.itemid as custom_field_item_id,
             cfg.namecode.codevalue as name_code__code_value,
             cfg.namecode.longname as name_code__long_name,
             cfg.namecode.shortname as name_code__short_name,
             cfg.namecode.effectivedate as name_code__effective_date,
 
             cfg.codes,
-        from workers_current_record as wcr
-        cross join unnest(wcr.custom_field_group__multi_code_fields) as cfg
-
-        union all
-
-        select
-            wcr.associate_oid,
-
-            cfg.itemid as item_id,
-            cfg.namecode.codevalue as name_code__code_value,
-            cfg.namecode.longname as name_code__long_name,
-            cfg.namecode.shortname as name_code__short_name,
-            cfg.namecode.effectivedate as name_code__effective_date,
-
-            cfg.codes,
-        from workers_current_record as wcr
-        cross join unnest(wcr.person__custom_field_group__multi_code_fields) as cfg
+        from work_assignments as wa
+        cross join unnest(wa.custom_field_group__multi_code_fields) as cfg
     )
 
 select
-    wcr.associate_oid,
+    wa.associate_oid,
+    wa.item_id,
+    wa.effective_date_start,
+    wa.effective_date_end,
+    wa.effective_date_timestamp,
+    wa.is_current_record,
 
-    cfg.itemid as item_id,
+    cfg.itemid as custom_field_item_id,
     cfg.namecode.codevalue as name_code__code_value,
     cfg.namecode.longname as name_code__long_name,
     cfg.namecode.shortname as name_code__short_name,
@@ -66,15 +59,20 @@ select
     null as long_name,
     null as short_name,
     null as effective_date,
-from workers_current_record as wcr
-cross join unnest(wcr.custom_field_group__date_fields) as cfg
+from work_assignments as wa
+cross join unnest(wa.custom_field_group__date_fields) as cfg
 
 union all
 
 select
-    wcr.associate_oid,
+    wa.associate_oid,
+    wa.item_id,
+    wa.effective_date_start,
+    wa.effective_date_end,
+    wa.effective_date_timestamp,
+    wa.is_current_record,
 
-    cfg.itemid as item_id,
+    cfg.itemid as custom_field_item_id,
     cfg.namecode.codevalue as name_code__code_value,
     cfg.namecode.longname as name_code__long_name,
     cfg.namecode.shortname as name_code__short_name,
@@ -90,15 +88,20 @@ select
     null as long_name,
     null as short_name,
     null as effective_date,
-from workers_current_record as wcr
-cross join unnest(wcr.custom_field_group__indicator_fields) as cfg
+from work_assignments as wa
+cross join unnest(wa.custom_field_group__indicator_fields) as cfg
 
 union all
 
 select
-    wcr.associate_oid,
+    wa.associate_oid,
+    wa.item_id,
+    wa.effective_date_start,
+    wa.effective_date_end,
+    wa.effective_date_timestamp,
+    wa.is_current_record,
 
-    cfg.itemid as item_id,
+    cfg.itemid as custom_field_item_id,
     cfg.namecode.codevalue as name_code__code_value,
     cfg.namecode.longname as name_code__long_name,
     cfg.namecode.shortname as name_code__short_name,
@@ -114,15 +117,20 @@ select
     null as long_name,
     null as short_name,
     null as effective_date,
-from workers_current_record as wcr
-cross join unnest(wcr.custom_field_group__number_fields) as cfg
+from work_assignments as wa
+cross join unnest(wa.custom_field_group__number_fields) as cfg
 
 union all
 
 select
-    wcr.associate_oid,
+    wa.associate_oid,
+    wa.item_id,
+    wa.effective_date_start,
+    wa.effective_date_end,
+    wa.effective_date_timestamp,
+    wa.is_current_record,
 
-    cfg.itemid as item_id,
+    cfg.itemid as custom_field_item_id,
     cfg.namecode.codevalue as name_code__code_value,
     cfg.namecode.longname as name_code__long_name,
     cfg.namecode.shortname as name_code__short_name,
@@ -138,15 +146,20 @@ select
     null as long_name,
     null as short_name,
     null as effective_date,
-from workers_current_record as wcr
-cross join unnest(wcr.custom_field_group__string_fields) as cfg
+from work_assignments as wa
+cross join unnest(wa.custom_field_group__string_fields) as cfg
 
 union all
 
 select
-    wcr.associate_oid,
+    wa.associate_oid,
+    wa.item_id,
+    wa.effective_date_start,
+    wa.effective_date_end,
+    wa.effective_date_timestamp,
+    wa.is_current_record,
 
-    cfg.itemid as item_id,
+    cfg.itemid as custom_field_item_id,
     cfg.namecode.codevalue as name_code__code_value,
     cfg.namecode.longname as name_code__long_name,
     cfg.namecode.shortname as name_code__short_name,
@@ -161,14 +174,19 @@ select
     cfg.longname as long_name,
     cfg.shortname as short_name,
     cfg.effectivedate as effective_date,
-from workers_current_record as wcr
-cross join unnest(wcr.custom_field_group__code_fields) as cfg
+from work_assignments as wa
+cross join unnest(wa.custom_field_group__code_fields) as cfg
 
 union all
 
 select
     mcf.associate_oid,
     mcf.item_id,
+    mcf.effective_date_start,
+    mcf.effective_date_end,
+    mcf.effective_date_timestamp,
+    mcf.is_current_record,
+    mcf.custom_field_item_id,
     mcf.name_code__code_value,
     mcf.name_code__long_name,
     mcf.name_code__short_name,
