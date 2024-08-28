5fe54de9fbfa375d540be3b158aad4b3930620d7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-08-28 04:40:16 +0000

    refactor

--- src/dbt/kipptaf/models/adp/workforce_now/workers/intermediate/int_adp_workforce_now__workers__custom_fields__pivot.sql
+++ src/dbt/kipptaf/models/adp/workforce_now/workers/intermediate/int_adp_workforce_now__workers__custom_fields__pivot.sql
@@ -2,12 +2,13 @@ with
     from_item as (
         select
             associate_oid,
-            name_code__code_value,
             date_value,
             indicator_value,
             number_value,
             string_value,
             code_value,
+
+            regexp_replace(name_code__code_value, r'\W', '') as pivot_column,
         from {{ ref("int_adp_workforce_now__workers__custom_fields") }}
     )
 
@@ -19,10 +20,8 @@ select
     indicator_value_receivedsignonbonus as received_sign_on_bonus,
     indicator_value_wfmgrloa as wf_mgr_loa,
 
-    string_value_employeenumber as employee_number,
     string_value_miamiacesnumber as miami_aces_number,
     string_value_njpensionnumber as nj_pension_number,
-    string_value_wfmgrbadgenumber as wf_mgr_badge_number,
     string_value_wfmgrtrigger as wf_mgr_trigger,
 
     code_value_preferredraceethnicity as preferred_race_ethnicity,
@@ -36,6 +35,9 @@ select
     as life_experience_in_communities_we_serve,
     code_value_professionalexperienceincommunitiesweserve
     as professional_experience_in_communities_we_serve,
+
+    safe_cast(string_value_employeenumber as int) as employee_number,
+    safe_cast(string_value_wfmgrbadgenumber as int) as wf_mgr_badge_number,
 from
     from_item pivot (
         max(date_value) as date_value,
@@ -43,7 +45,7 @@ from
         max(number_value) as number_value,
         max(string_value) as string_value,
         max(code_value) as code_value
-        for regexp_replace(name_code__code_value, r'\W', '') in (
+        for pivot_column in (
             'EmployeeNumber',
             'LifeExperienceinCommunitiesWeServe',
             'MiamiACESNumber',
