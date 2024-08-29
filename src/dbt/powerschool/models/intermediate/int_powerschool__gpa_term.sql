10eb93dc388e8ec3144ad58d0050535b2211c1fe
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-08-29 18:38:05 +0000

    refactor: scaffold Y1 storedgrades by term to account for semester courses

--- src/dbt/powerschool/models/intermediate/int_powerschool__gpa_term.sql
+++ src/dbt/powerschool/models/intermediate/int_powerschool__gpa_term.sql
@@ -13,18 +13,15 @@ with
             y1_grade_points,
             y1_grade_points_unweighted,
             potential_credit_hours as potential_credit_hours_default,
+            termbin_is_current as is_current,
+
             if(
                 term_percent_grade is null, null, potential_credit_hours
             ) as potential_credit_hours_term,
+
             if(
                 y1_percent_grade_adjusted is null, null, potential_credit_hours
             ) as potential_credit_hours_y1,
-            if(
-                current_date('{{ var("local_timezone") }}')
-                between termbin_start_date and termbin_end_date,
-                true,
-                false
-            ) as is_current,
         from {{ ref("base_powerschool__final_grades") }}
         where exclude_from_gpa = 0 and potential_credit_hours > 0
 
@@ -32,10 +29,12 @@ with
 
         /* previous years */
         select
-            sg.studentid,
-            sg.schoolid,
-            sg.yearid,
-            sg.storecode,
+            y1.studentid,
+            y1.schoolid,
+            y1.yearid,
+
+            storecode,
+
             sg.percent as term_percent_grade,
             sg.gpa_points as term_grade_points,
 
@@ -46,25 +45,28 @@ with
             null as y1_grade_points_unweighted,
 
             c.credit_hours as potential_credit_hours_default,
-            if(sg.percent is null, null, c.credit_hours) as potential_credit_hours_term,
-            if(y1.percent is null, null, c.credit_hours) as potential_credit_hours_y1,
 
-            if(sg.storecode in ('Q4', 'T3'), true, false) as is_current,
-        from {{ ref("stg_powerschool__storedgrades") }} as sg
+            if(storecode in ('Q4', 'T3'), true, false) as is_current,
+
+            if(sg.percent is null, null, c.credit_hours) as potential_credit_hours_term,
+
+            if(y1.percent is null, null, c.credit_hours) as potential_credit_hours_y1,
+        from {{ ref("stg_powerschool__storedgrades") }} as y1
+        cross join unnest(["Q1", "Q2", "Q3", "Q4"]) as storecode
         inner join
             {{ ref("stg_powerschool__courses") }} as c
-            on sg.course_number = c.course_number
+            on y1.course_number = c.course_number
             and c.credit_hours > 0
         left join
-            {{ ref("stg_powerschool__storedgrades") }} as y1
-            on sg.studentid = y1.studentid
-            and sg.yearid = y1.yearid
-            and sg.course_number = y1.course_number
-            and y1.storecode = 'Y1'
+            {{ ref("stg_powerschool__storedgrades") }} as sg
+            on y1.studentid = sg.studentid
+            and y1.yearid = sg.yearid
+            and y1.course_number = sg.course_number
+            and storecode = sg.storecode
         where
-            sg.storecode_type in ('Q', 'T')
-            and sg.excludefromgpa = 0
-            and sg.yearid < {{ var("current_academic_year") - 1990 }}
+            y1.storecode = 'Y1'
+            and y1.excludefromgpa = 0
+            and y1.yearid < {{ var("current_academic_year") - 1990 }}
     ),
 
     grade_rollup as (
@@ -74,6 +76,7 @@ with
             yearid,
             storecode,
             is_current,
+
             case
                 when storecode in ('Q1', 'Q2')
                 then 'S1'
@@ -81,50 +84,46 @@ with
                 then 'S2'
             end as semester,
 
-            /* gpa term */
-            round(avg(term_percent_grade), 0) as grade_avg_term,
+            sum(potential_credit_hours_term) as total_credit_hours_term,
+            sum(potential_credit_hours_y1) as total_credit_hours_y1,
+
             sum(term_grade_points) as gpa_points_total_term,
+            sum(y1_grade_points) as gpa_points_total_y1,
+
+            avg(term_percent_grade) as grade_avg_term,
+            avg(y1_percent_grade_adjusted) as grade_avg_y1,
+
             sum(
                 potential_credit_hours_default * term_grade_points
             ) as weighted_gpa_points_term,
-            sum(potential_credit_hours_term) as credit_hours_term,
-            round(
-                safe_divide(
-                    sum(potential_credit_hours_default * term_grade_points),
-                    sum(potential_credit_hours_term)
-                ),
-                2
-            ) as gpa_term,
-
-            /* gpa Y1 */
-            round(avg(y1_percent_grade_adjusted), 0) as grade_avg_y1,
-            sum(y1_grade_points) as gpa_points_total_y1,
             sum(
                 potential_credit_hours_default * y1_grade_points
             ) as weighted_gpa_points_y1,
             sum(
                 potential_credit_hours_default * y1_grade_points_unweighted
             ) as weighted_gpa_points_y1_unweighted,
-            sum(potential_credit_hours_y1) as total_credit_hours,
-            round(
-                safe_divide(
-                    sum(potential_credit_hours_default * y1_grade_points),
-                    sum(potential_credit_hours_y1)
-                ),
-                2
-            ) as gpa_y1,
-            round(
-                safe_divide(
-                    sum(potential_credit_hours_default * y1_grade_points_unweighted),
-                    sum(potential_credit_hours_y1)
-                ),
-                2
-            ) as gpa_y1_unweighted,
 
-            /* other */
             sum(if(y1_letter_grade like 'F%', 1, 0)) as n_failing_y1,
         from grade_detail
         group by studentid, yearid, storecode, is_current, schoolid
+    ),
+
+    gpa_calcs as (
+        select
+            *,
+
+            round(
+                safe_divide(weighted_gpa_points_term, total_credit_hours_term), 2
+            ) as gpa_term,
+
+            round(
+                safe_divide(weighted_gpa_points_y1, total_credit_hours_y1), 2
+            ) as gpa_y1,
+
+            round(
+                safe_divide(weighted_gpa_points_y1_unweighted, total_credit_hours_y1), 2
+            ) as gpa_y1_unweighted,
+        from grade_rollup
     )
 
 select
@@ -140,10 +139,11 @@ select
     gpa_y1,
     gpa_y1_unweighted,
     n_failing_y1,
-    credit_hours_term,
-    total_credit_hours,
-    grade_avg_term,
-    grade_avg_y1,
+    total_credit_hours_term,
+    total_credit_hours_y1,
+
+    round(grade_avg_term, 0) as grade_avg_term,
+    round(grade_avg_y1, 0) as grade_avg_y1,
     round(weighted_gpa_points_term, 2) as weighted_gpa_points_term,
     round(weighted_gpa_points_y1, 2) as weighted_gpa_points_y1,
     round(weighted_gpa_points_y1_unweighted, 2) as weighted_gpa_points_y1_unweighted,
@@ -152,18 +152,26 @@ select
     sum(gpa_points_total_term) over (
         partition by studentid, yearid, semester
     ) as gpa_points_total_semester,
-    round(
-        avg(grade_avg_term) over (partition by studentid, yearid, semester), 0
-    ) as grade_avg_semester,
+
     round(
         sum(weighted_gpa_points_term) over (partition by studentid, yearid, semester), 2
     ) as weighted_gpa_points_semester,
+
     round(
-        sum(total_credit_hours) over (partition by studentid, yearid, semester), 2
+        sum(total_credit_hours_y1) over (partition by studentid, yearid, semester), 2
     ) as total_credit_hours_semester,
+
     round(
-        sum(weighted_gpa_points_term) over (partition by studentid, yearid, semester)
-        / sum(credit_hours_term) over (partition by studentid, yearid, semester),
+        avg(grade_avg_term) over (partition by studentid, yearid, semester), 0
+    ) as grade_avg_semester,
+
+    round(
+        safe_divide(
+            sum(weighted_gpa_points_term) over (
+                partition by studentid, yearid, semester
+            ),
+            sum(total_credit_hours_term) over (partition by studentid, yearid, semester)
+        ),
         2
     ) as gpa_semester,
-from grade_rollup
+from gpa_calcs
