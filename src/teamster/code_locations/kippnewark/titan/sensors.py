cafa4c06d5873e5acb7359c418e4552dd10f1e09
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-04 20:10:45 +0000

    fix: retire titan income form

--- src/teamster/code_locations/kippnewark/titan/sensors.py
+++ src/teamster/code_locations/kippnewark/titan/sensors.py
@@ -1,13 +1,10 @@
 from teamster.code_locations.kippnewark import CODE_LOCATION, LOCAL_TIMEZONE
-from teamster.code_locations.kippnewark.titan.assets import (
-    income_form_data,
-    person_data,
-)
+from teamster.code_locations.kippnewark.titan.assets import person_data
 from teamster.libraries.titan.sensors import build_titan_sftp_sensor
 
 sftp_sensor = build_titan_sftp_sensor(
     code_location=CODE_LOCATION,
-    asset_selection=[income_form_data, person_data],
+    asset_selection=[person_data],
     timezone=LOCAL_TIMEZONE,
     minimum_interval_seconds=(60 * 10),
     exclude_dirs=["Scipt", "Script"],
