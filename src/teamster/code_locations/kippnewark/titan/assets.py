3e760bb72e49c1e67a179b24a38db9fb7be67c18
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-04 20:10:45 +0000

    fix: retire titan income form

--- src/teamster/code_locations/kippnewark/titan/assets.py
+++ src/teamster/code_locations/kippnewark/titan/assets.py
@@ -3,10 +3,7 @@ from teamster.code_locations.kippnewark import (
     CURRENT_FISCAL_YEAR,
     LOCAL_TIMEZONE,
 )
-from teamster.code_locations.kippnewark.titan.schema import (
-    INCOME_FORM_DATA_SCHEMA,
-    PERSON_DATA_SCHEMA,
-)
+from teamster.code_locations.kippnewark.titan.schema import PERSON_DATA_SCHEMA
 from teamster.libraries.titan.assets import build_titan_sftp_asset
 
 asset_key_prefix = [CODE_LOCATION, "titan"]
@@ -20,16 +17,6 @@ person_data = build_titan_sftp_asset(
     current_fiscal_year=CURRENT_FISCAL_YEAR,
 )
 
-income_form_data = build_titan_sftp_asset(
-    key=[*asset_key_prefix, "income_form_data"],
-    remote_file_regex=r"[Ii]ncome\s?[Ff]orm\s?[Dd]ata(?P<fiscal_year>\d{4})\.csv",
-    schema=INCOME_FORM_DATA_SCHEMA,
-    partition_start_date="2021-07-01",
-    timezone=LOCAL_TIMEZONE,
-    current_fiscal_year=CURRENT_FISCAL_YEAR,
-)
-
 assets = [
-    income_form_data,
     person_data,
 ]
