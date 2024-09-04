3cdb50402b75de26ab631513c172c8bbe183468d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-04 19:03:41 +0000

    feat: dff stats dbt models

--- src/dbt/deanslist/models/staging/stg_deanslist__comm_log.sql
+++ src/dbt/deanslist/models/staging/stg_deanslist__comm_log.sql
@@ -10,34 +10,36 @@ with
     )
 
 select
-    safe_cast(recordid as int) as `record_id`,
-    safe_cast(callstatusid as int) as `call_status_id`,
-    safe_cast(reasonid as int) as `reason_id`,
-    safe_cast(userid as int) as `user_id`,
+    isdraft as is_draft,
 
-    safe_cast(nullif(calldatetime, '') as datetime) as `call_date_time`,
+    /* repeated records */
+    followups,
 
-    nullif(callstatus, '') as `call_status`,
-    nullif(calltype, '') as `call_type`,
-    nullif(educatorname, '') as `educator_name`,
-    nullif(email, '') as `email`,
-    nullif(mailingaddress, '') as `mailing_address`,
-    nullif(phonenumber, '') as `phone_number`,
-    nullif(reason, '') as `reason`,
-    nullif(recordtype, '') as `record_type`,
-    nullif(response, '') as `response`,
-    nullif(topic, '') as `topic`,
+    /* transformations */
+    safe_cast(recordid as int) as record_id,
+    safe_cast(callstatusid as int) as call_status_id,
+    safe_cast(reasonid as int) as reason_id,
+    safe_cast(userid as int) as user_id,
 
-    isdraft as `is_draft`,
+    nullif(callstatus, '') as call_status,
+    nullif(calltype, '') as call_type,
+    nullif(educatorname, '') as educator_name,
+    nullif(email, '') as email,
+    nullif(mailingaddress, '') as mailing_address,
+    nullif(phonenumber, '') as phone_number,
+    nullif(reason, '') as reason,
+    nullif(recordtype, '') as record_type,
+    nullif(response, '') as response,
+    nullif(topic, '') as topic,
 
-    {# records #}
-    safe_cast(nullif(student.secondarystudentid, '') as int) as `secondary_student_id`,
-    safe_cast(nullif(student.studentid, '') as int) as `student_id`,
-    safe_cast(nullif(student.studentschoolid, '') as int) as `student_school_id`,
-    nullif(student.studentfirstname, '') as `student_first_name`,
-    nullif(student.studentmiddlename, '') as `student_middle_name`,
-    nullif(student.studentlastname, '') as `student_last_name`,
+    nullif(student.studentfirstname, '') as student_first_name,
+    nullif(student.studentmiddlename, '') as student_middle_name,
+    nullif(student.studentlastname, '') as student_last_name,
 
-    {# repeated records #}
-    followups as `followups`,
+    safe_cast(nullif(calldatetime, '') as datetime) as call_date_time,
+
+    safe_cast(nullif(student.secondarystudentid, '') as int) as secondary_student_id,
+    safe_cast(nullif(student.studentid, '') as int) as student_id,
+    safe_cast(nullif(student.studentschoolid, '') as int) as student_school_id,
 from deduplicate
+where not is_deleted or is_deleted is null
