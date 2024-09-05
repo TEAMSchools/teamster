154d13d5831515174ba2177d5b15c3dbe6c3da16
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-05 20:44:45 +0000

    refactor: school level

--- src/dbt/powerschool/models/intermediate/int_powerschool__calendar_week.sql
+++ src/dbt/powerschool/models/intermediate/int_powerschool__calendar_week.sql
@@ -1,19 +1,11 @@
 with
-    schools as (
-        select
-            school_number,
-
-            case
-                high_grade when 12 then 'HS' when 8 then 'MS' when 4 then 'ES'
-            end as school_level,
-        from {{ ref("stg_powerschool__schools") }}
-    ),
-
     week_rollup as (
         select
             cd.schoolid,
             cd.week_start_date,
             cd.week_end_date,
+
+            sch.school_level,
 
             t.yearid,
             t.abbreviation as `quarter`,
@@ -36,31 +28,36 @@ with
             ) as week_number_quarter,
         from {{ ref("stg_powerschool__calendar_day") }} as cd
         inner join
+            {{ ref("stg_powerschool__schools") }} as sch
+            on cd.schoolid = sch.school_number
+        inner join
+            {{ ref("stg_powerschool__cycle_day") }} as cy on cd.cycle_day_id = cy.id
+        inner join
             {{ ref("stg_powerschool__terms") }} as t
             on cd.schoolid = t.schoolid
             and cd.date_value between t.firstday and t.lastday
             and t.portion = 4  /* quarters */
-        inner join
-            {{ ref("stg_powerschool__cycle_day") }} as cy on cd.cycle_day_id = cy.id
         inner join
             {{ ref("stg_powerschool__bell_schedule") }} as bs
             on cd.schoolid = bs.schoolid
             and cd.bell_schedule_id = bs.id
         where cd.insession = 1 and cd.membershipvalue > 0
         group by
-            cd.schoolid, cd.week_start_date, cd.week_end_date, t.yearid, t.abbreviation
+            cd.schoolid,
+            cd.week_start_date,
+            cd.week_end_date,
+            sch.school_level,
+            t.yearid,
+            t.abbreviation
     )
 
 select
-    w.*,
+    *,
 
-    s.school_level,
+    date_add(week_start_date, interval 1 day) as week_start_monday,
+    date_add(week_end_date, interval 1 day) as week_end_sunday,
 
-    date_add(w.week_start_date, interval 1 day) as week_start_monday,
-    date_add(w.week_end_date, interval 1 day) as week_end_sunday,
-
-    lead(w.school_week_start_date) over (
-        partition by w.schoolid, w.yearid order by w.week_start_date asc
+    lead(school_week_start_date) over (
+        partition by schoolid, yearid order by week_start_date asc
     ) as school_week_start_date_lead,
-from week_rollup as w
-left join schools as s on w.schoolid = s.school_number
+from week_rollup
