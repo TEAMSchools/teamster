b4b4975c76e7edf9ff8751203cbc146f400b10df
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-05 21:48:56 +0000

    feat:dds pm staging

--- src/dbt/kipptaf/models/amplify/dibels/staging/stg_amplify__dibels_progress_export.sql
+++ src/dbt/kipptaf/models/amplify/dibels/staging/stg_amplify__dibels_progress_export.sql
@@ -1,1 +1,9 @@
-select *, from {{ source("amplify", "src_amplify__dibels_progress_export") }}
+select
+    student_id,
+    measure,
+    score,
+
+    _dagster_partition_fiscal_year - 1 as academic_year,
+
+    parse_date('%m/%d/%Y', assessment_dates) as assessment_dates,
+from {{ source("amplify", "src_amplify__dibels_progress_export") }}
