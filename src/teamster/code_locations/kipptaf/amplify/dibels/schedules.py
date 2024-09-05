cd1662fddcbb16e7c45452d87e9300ea091ba4a4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-05 21:20:00 +0000

    feat: schedule

--- src/teamster/code_locations/kipptaf/amplify/dibels/schedules.py
+++ src/teamster/code_locations/kipptaf/amplify/dibels/schedules.py
@@ -1,25 +1,22 @@
-from dagster import RunRequest, ScheduleEvaluationContext, define_asset_job, schedule
-
-from teamster.code_locations.kipptaf import CURRENT_FISCAL_YEAR, LOCAL_TIMEZONE
-from teamster.code_locations.kipptaf.amplify.dibels.assets import data_farming
-
-amplify_dibels_asset_job = define_asset_job(
-    name=f"{data_farming.key.to_python_identifier()}_asset_job",
-    selection=[data_farming],
-    partitions_def=data_farming.partitions_def,
+from teamster.code_locations.kipptaf import LOCAL_TIMEZONE
+from teamster.code_locations.kipptaf.amplify.dibels.assets import (
+    data_farming,
+    progress_export,
 )
+from teamster.libraries.amplify.dibels.schedules import build_amplify_dibels_schedule
 
-
-@schedule(
-    name=f"{amplify_dibels_asset_job.name}_schedule",
+amplify_dibels_data_farming_asset_job_schedule = build_amplify_dibels_schedule(
+    asset=data_farming,
     cron_schedule="0 4 * * *",
     execution_timezone=LOCAL_TIMEZONE.name,
-    job=amplify_dibels_asset_job,
 )
-def amplify_dibels_asset_job_schedule(context: ScheduleEvaluationContext):
-    yield RunRequest(partition_key=CURRENT_FISCAL_YEAR.start.to_date_string())
 
+amplify_dibels_progress_export_asset_job_schedule = build_amplify_dibels_schedule(
+    asset=progress_export,
+    cron_schedule="0 4 * * *",
+    execution_timezone=LOCAL_TIMEZONE.name,
+)
 
 schedules = [
-    amplify_dibels_asset_job_schedule,
+    amplify_dibels_data_farming_asset_job_schedule,
 ]
