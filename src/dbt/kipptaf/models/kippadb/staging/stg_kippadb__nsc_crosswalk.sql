7311730d3c0d5ad9e287de6683ca445d4a4959b5
Anthony Walters <awalters@kippteamandfamily.org>
2024-09-06 17:16:42 +0000

    add row_number() to dedupe NSC crosswalk

--- src/dbt/kipptaf/models/kippadb/staging/stg_kippadb__nsc_crosswalk.sql
+++ src/dbt/kipptaf/models/kippadb/staging/stg_kippadb__nsc_crosswalk.sql
@@ -1,1 +1,6 @@
-select *, from {{ source("kippadb", "src_kippadb__nsc_crosswalk") }}
+select
+    *,
+    row_number() over (
+        partition by account_id order by college_name_nsc desc
+    ) as rn_account,
+from {{ source("kippadb", "src_kippadb__nsc_crosswalk") }}
