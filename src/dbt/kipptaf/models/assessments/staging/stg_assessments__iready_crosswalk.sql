785d9a9d6ca2afdb325a42c30a2a9201906c4545
Anthony Walters <awalters@kippteamandfamily.org>
2024-09-08 23:08:21 +0000

    level add to i-Ready crosswalk

--- src/dbt/kipptaf/models/assessments/staging/stg_assessments__iready_crosswalk.sql
+++ src/dbt/kipptaf/models/assessments/staging/stg_assessments__iready_crosswalk.sql
@@ -1,6 +1,7 @@
 select
     *,
     safe_cast(grade_level as string) as grade_level_string,
+    safe_cast(right(sublevel_name, 1) as numeric) as level,
     case
         when destination_system = 'FL' and sublevel_number >= 6
         then true
