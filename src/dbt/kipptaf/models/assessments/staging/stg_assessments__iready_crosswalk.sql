7e5e0cb29c4abc0126a031eebcf8ba64bcb7b684
Anthony Walters <awalters@kippteamandfamily.org>
2024-09-08 22:10:21 +0000

    add is_proficient boolean to i-Ready crosswalk

--- src/dbt/kipptaf/models/assessments/staging/stg_assessments__iready_crosswalk.sql
+++ src/dbt/kipptaf/models/assessments/staging/stg_assessments__iready_crosswalk.sql
@@ -1,2 +1,11 @@
-select *, safe_cast(grade_level as string) as grade_level_string,
+select
+    *,
+    safe_cast(grade_level as string) as grade_level_string,
+    case
+        when destination_system = 'FL' and sublevel_number >= 6
+        then true
+        when destination_system = 'NJSLA' and sublevel_number >= 4
+        then true
+        else false
+    end as is_proficient,
 from {{ source("assessments", "src_assessments__iready_crosswalk") }}
