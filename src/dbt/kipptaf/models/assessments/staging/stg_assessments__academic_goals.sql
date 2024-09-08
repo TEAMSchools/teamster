35b3334bfd8db863e6eb2990450345567c713c6e
Anthony Walters <awalters@kippteamandfamily.org>
2024-09-08 19:06:49 +0000

    add grade band to goals lookup table

--- src/dbt/kipptaf/models/assessments/staging/stg_assessments__academic_goals.sql
+++ src/dbt/kipptaf/models/assessments/staging/stg_assessments__academic_goals.sql
@@ -1,3 +1,7 @@
-select g.*, initcap(regexp_extract(s._dbt_source_relation, r'kipp(\w+)_')) as region,
+select
+    g.*,
+    initcap(regexp_extract(s._dbt_source_relation, r'kipp(\w+)_')) as region,
+    cw.grade_band,
 from {{ source("assessments", "src_assessments__academic_goals") }} as g
 inner join {{ ref("stg_powerschool__schools") }} as s on g.school_id = s.school_number
+inner join {{ ref("stg_people__location_crosswalk") }} as cw on s.name = cw.name
