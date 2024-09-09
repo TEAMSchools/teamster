3ffc0ee77241ea37c4e36e440917eda5db66bba8
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-09 15:08:51 +0000

    fix: ps sensor asset selection

--- src/teamster/code_locations/kippmiami/powerschool/sensors.py
+++ src/teamster/code_locations/kippmiami/powerschool/sensors.py
@@ -1,10 +1,18 @@
 from teamster.code_locations.kippmiami import CODE_LOCATION, LOCAL_TIMEZONE
-from teamster.code_locations.kippmiami.powerschool.assets import assets
+from teamster.code_locations.kippmiami.powerschool.assets import (
+    powerschool_table_assets_full,
+    powerschool_table_assets_no_partition,
+    powerschool_table_assets_transaction_date,
+)
 from teamster.libraries.powerschool.sis.sensors import build_powerschool_asset_sensor
 
 powerschool_asset_sensor = build_powerschool_asset_sensor(
     code_location=CODE_LOCATION,
-    asset_selection=assets,
+    asset_selection=[
+        *powerschool_table_assets_full,
+        *powerschool_table_assets_no_partition,
+        *powerschool_table_assets_transaction_date,
+    ],
     execution_timezone=LOCAL_TIMEZONE,
     minimum_interval_seconds=(60 * 15),
 )
