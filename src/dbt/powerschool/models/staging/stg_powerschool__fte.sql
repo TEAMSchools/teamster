7771f47f523ad9a530daa9c0213b07587f3c38a1
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-10 21:27:48 +0000

    fix: fte stg

--- src/dbt/powerschool/models/staging/stg_powerschool__fte.sql
+++ src/dbt/powerschool/models/staging/stg_powerschool__fte.sql
@@ -1,10 +1,10 @@
 select
-    * except (dcid, id, schoolid, year_id, attendance_conversion_id),
+    * except (dcid, id, schoolid, yearid, fte_value),
 
     /* column transformations */
     dcid.int_value as dcid,
     id.int_value as id,
     schoolid.int_value as schoolid,
-    year_id.int_value as year_id,
-    attendance_conversion_id.int_value as attendance_conversion_id,
-from {{ source("powerschool", "src_powerschool__bell_schedule") }}
+    yearid.int_value as yearid,
+    fte_value.double_value as fte_value,
+from {{ source("powerschool", "src_powerschool__fte") }}
