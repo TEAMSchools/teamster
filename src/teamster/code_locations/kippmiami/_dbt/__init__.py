12a841431e6079ab7906324968117608dcb41b6a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-17 17:40:34 +0000

    refactor: use dbtproject build: prepare-and-package ci: rm dbt step

--- src/teamster/code_locations/kippmiami/_dbt/__init__.py
+++ src/teamster/code_locations/kippmiami/_dbt/__init__.py
@@ -1,5 +1,6 @@
-from teamster.code_locations.kippmiami._dbt.assets import assets
+from teamster.code_locations.kippmiami._dbt.assets import assets, dbt_project
 
 __all__ = [
     "assets",
+    "dbt_project",
 ]
