9e96b06da71586fe05e38fcbccdea0773e9d554a
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-09-24 21:08:49 +0000

    refining ktaf approvals

--- src/dbt/kipptaf/models/extracts/appsheet/rpt_appsheet__stipend_app_roster.sql
+++ src/dbt/kipptaf/models/extracts/appsheet/rpt_appsheet__stipend_app_roster.sql
@@ -1,37 +1,61 @@
 with
-    ktaf_approvers as (
+    /* departments with chiefs */
+    department_chiefs as (
+        select
+            sr1.department_home_name,
+            sr1.preferred_name_lastfirst,
+            sr1.employee_number as ktaf_approver,
+        from {{ ref("base_people__staff_roster") }} as sr1
+        where
+            (
+                sr1.job_title like '%Chief%Officer'
+                or sr1.job_title like '%Chief%Strategist%'
+            )
+            and sr1.department_home_name <> 'Executive'
+            and sr1.assignment_status in ('Active', 'Leave')
+    ),
+    /* chiefs/presidents above departments without chiefs*/
+    other_chiefs as (
         select
             sr1.department_home_name,
             sr1.report_to_preferred_name_lastfirst,
             coalesce(
-                case
-                    when sr2.job_title like '%Chief%Officer%'
-                    then max(sr1.report_to_employee_number)
-                    when sr2.job_title like '%Chief%Strategist%'
-                    then max(sr1.report_to_employee_number)
-                end,
-                case
-                    when sr2.job_title like '%President%'
-                    then max(sr1.report_to_employee_number)
-                end
-            ) as ktaf_approver,
+                max(
+                    case
+                        when sr2.job_title like '%Chief%Officer%'
+                        then sr1.report_to_employee_number
+                        when sr2.job_title like '%Chief%Strategist%'
+                        then sr1.report_to_employee_number
+                        when sr2.job_title like '%President%'
+                        then sr1.report_to_employee_number
+                    end
+                ),
+                null
+            ) as ktaf_approver
         from {{ ref("base_people__staff_roster") }} as sr1
         left join
             {{ ref("base_people__staff_roster") }} as sr2
             on sr1.report_to_employee_number = sr2.employee_number
         where
             sr1.business_unit_home_code = 'KIPP_TAF'
-            and sr2.job_title not in ('Chief Executive Officer', 'Chief of Staff')
             and sr1.assignment_status in ('Active', 'Leave')
             and (sr2.job_title like '%Chief%' or sr2.job_title like '%President%')
-            and sr1.job_title not like '%Chief%'
             and sr1.department_home_name <> 'Executive'
-        group by
-            sr1.department_home_name,
-            sr1.report_to_preferred_name_lastfirst,
-            sr2.job_title
+            and sr1.department_home_name
+            not in (select department_home_name from department_chiefs)
+        group by sr1.department_home_name, sr1.report_to_preferred_name_lastfirst
     ),
 
+    /* combining all departments to one KTAF list of departments and Chief/President approver*/
+    ktaf_approvers as (
+        select d.*
+        from department_chiefs as d
+        union all
+        select o.*
+        from other_chiefs as o
+    ),
+
+    /* selecting the MDO from each region*/
     mdo as (
         select
             lc.region,
@@ -50,7 +74,8 @@ with
         group by lc.region
     ),
 
-    roster as (
+    /* assigning approval routes according to location, entity, manager's entity*/
+    route_assignments as (
         select
             sr.employee_number,
             sr.payroll_group_code,
@@ -118,9 +143,6 @@ with
             {{ ref("base_people__staff_roster") }} as sr2
             on sr.report_to_employee_number = sr2.employee_number
         left join
-            {{ ref("base_people__staff_roster") }} as sr3
-            on sr2.report_to_employee_number = sr3.employee_number
-        left join
             {{ ref("stg_people__campus_crosswalk") }} as cc
             on sr.home_work_location_name = cc.location_name
         left join
@@ -131,11 +153,11 @@ with
             ktaf_approvers as k on sr.department_home_name = k.department_home_name
         where
             sr.worker_termination_date is null
-            or sr.worker_termination_date >= '{{ var("current_academic_year") }}-07-01'
+            or sr.worker_termination_date >= '2024-07-01'
     ),
 
-    /* first and second approver assignments that follow set rules */
-    rule_assignments as (
+    /* assigning approvers based on approval route*/
+    approver_assignments as (
         select
             r.employee_number,
             r.payroll_group_code,
@@ -201,97 +223,49 @@ with
                 when r.route = 'KTAF'
                 then r.ktaf_approver
             end as second_approver_employee_number,
-        from roster as r
+        from route_assignments as r
 
-    ),
-
-    whole_roster as (
-        select
-            r.employee_number,
-            r.payroll_group_code,
-            r.worker_id,
-            r.payroll_file_number,
-            r.position_id,
-            r.job_title,
-            r.home_work_location_name,
-            r.department_home_name,
-            r.preferred_name_lastfirst,
-            r.user_principal_name,
-            r.google_email,
-            r.assignment_status,
-            r.business_unit_home_name,
-            r.business_unit_home_code,
-            r.worker_termination_date,
-            r.route,
-            r.campus,
-            r.dso_employee_number,
-            r.sl_employee_number,
-            r.head_of_school_employee_number,
-            r.mdso_employee_number,
-            r.mdo_employee_number,
-            r.ktaf_approver,
-            r.manager_employee_number,
-            r.grandmanager_employee_number,
-            r.first_approver_employee_number,
-            r.second_approver_employee_number,
-        from rule_assignments as r
-        where
-            r.first_approver_employee_number <> r.employee_number
-            and r.second_approver_employee_number <> r.employee_number
-
-        /* exceptions with no route or self approvals go to manager and manager's manager */
-        union all
-
-        select
-            r.employee_number,
-            r.payroll_group_code,
-            r.worker_id,
-            r.payroll_file_number,
-            r.position_id,
-            r.job_title,
-            r.home_work_location_name,
-            r.department_home_name,
-            r.preferred_name_lastfirst,
-            r.user_principal_name,
-            r.google_email,
-            r.assignment_status,
-            r.business_unit_home_name,
-            r.business_unit_home_code,
-            r.worker_termination_date,
-            r.route,
-            r.campus,
-            r.dso_employee_number,
-            r.sl_employee_number,
-            r.head_of_school_employee_number,
-            r.mdso_employee_number,
-            r.mdo_employee_number,
-            r.ktaf_approver,
-            r.manager_employee_number,
-            r.grandmanager_employee_number,
-
-            case
-                when
-                    r.first_approver_employee_number is null
-                    or r.first_approver_employee_number = r.employee_number
-                then r.manager_employee_number
-            end as first_approver_employee_number,
-            case
-                when
-                    r.second_approver_employee_number is null
-                    or r.second_approver_employee_number = r.employee_number
-                then coalesce(r.grandmanager_employee_number, r.manager_employee_number)
-            end as second_approver_employee_number,
-        from rule_assignments as r
-        where
-            r.first_approver_employee_number = r.employee_number
-            or r.second_approver_employee_number = r.employee_number
     )
+/* roster that feeds into Stipend and Bonus AppSheet*/
+select
+    r.employee_number,
+    r.payroll_group_code,
+    r.worker_id,
+    r.payroll_file_number,
+    r.position_id,
+    r.job_title,
+    r.home_work_location_name,
+    r.department_home_name,
+    r.preferred_name_lastfirst,
+    r.user_principal_name,
+    r.google_email,
+    r.assignment_status,
+    r.business_unit_home_name,
+    r.business_unit_home_code,
+    r.worker_termination_date,
+    r.route,
+    r.campus,
+    r.dso_employee_number,
+    r.sl_employee_number,
+    r.head_of_school_employee_number,
+    r.mdso_employee_number,
+    r.mdo_employee_number,
+    r.ktaf_approver,
+    r.manager_employee_number,
+    r.grandmanager_employee_number,
+    case
+        when r.first_approver_employee_number = r.employee_number
+        then r.manager_employee_number
+        when r.first_approver_employee_number is null and r.route <> 'MDSO'
+        then r.manager_employee_number
 
-select r.*,sr1.preferred_name_lastfirst as first_approver_name, sr2.preferred_name_lastfirst as second_approver_name,
-from whole_roster as r 
-left join
-    {{ ref("base_people__staff_roster") }} as sr1
-    on r.first_approver_employee_number = sr1.employee_number
-left join
-    {{ ref("base_people__staff_roster") }} as sr2
-    on r.second_approver_employee_number = sr2.employee_number
+        else r.first_approver_employee_number
+    end as first_approver_employee_number,
+    case
+        when r.second_approver_employee_number = r.employee_number
+        then r.grandmanager_employee_number
+        when r.second_approver_employee_number is null and r.route <> 'MDSO'
+        then r.grandmanager_employee_number
+        else r.second_approver_employee_number
+    end as second_approver_employee_number,
+from approver_assignments as r
