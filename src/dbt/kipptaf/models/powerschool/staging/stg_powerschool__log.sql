9997df327fddf96e07d79e7a223a1e22e4653c5d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-25 19:57:40 +0000

    refactor: macros

--- src/dbt/kipptaf/models/powerschool/staging/stg_powerschool__log.sql
+++ src/dbt/kipptaf/models/powerschool/staging/stg_powerschool__log.sql
@@ -1,9 +1,22 @@
-{{
-    dbt_utils.union_relations(
-        relations=[
-            source("kippnewark_powerschool", model.name),
-            source("kippcamden_powerschool", model.name),
-            source("kippmiami_powerschool", model.name),
-        ]
+with
+    union_relations as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    source("kippnewark_powerschool", model.name),
+                    source("kippcamden_powerschool", model.name),
+                    source("kippmiami_powerschool", model.name),
+                ]
+            )
+        }}
     )
-}}
+
+select
+    *,
+
+    {{
+        date_to_fiscal_year(
+            date_field="entry_date", start_month=7, year_source="start"
+        )
+    }} as academic_year,
+from union_relations
