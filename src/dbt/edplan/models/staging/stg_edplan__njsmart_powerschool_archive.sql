267730b1c7e7bb773ec83588bc33169e3acbae99
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-26 21:22:33 +0000

    build: dbt dev config

--- src/dbt/edplan/models/staging/stg_edplan__njsmart_powerschool_archive.sql
+++ src/dbt/edplan/models/staging/stg_edplan__njsmart_powerschool_archive.sql
@@ -2,11 +2,14 @@ select
     academic_year,
     effective_date,
     effective_end_date,
+
     academic_year + 1 as fiscal_year,
+
     nullif(nj_se_parental_consentobtained, 'NULL') as nj_se_parentalconsentobtained,
     nullif(special_education, 'NULL') as special_education,
     nullif(spedlep, 'NULL') as spedlep,
     nullif(special_education_code, 'NULL') as special_education_code,
+
     safe_cast(student_number as int) as student_number,
     safe_cast(state_studentnumber as int) as state_studentnumber,
     safe_cast(nj_se_delayreason as int) as nj_se_delayreason,
@@ -20,6 +23,7 @@ select
     safe_cast(nj_se_initialiepmeetingdate as date) as nj_se_initialiepmeetingdate,
     safe_cast(nj_se_consenttoimplementdate as date) as nj_se_consenttoimplementdate,
     safe_cast(row_hash as string) as row_hash,
+
     if(ti_serv_counseling, 'Y', 'N') as ti_serv_counseling,
     if(ti_serv_occup, 'Y', 'N') as ti_serv_occup,
     if(ti_serv_other, 'Y', 'N') as ti_serv_other,
