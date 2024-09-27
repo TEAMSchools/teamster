1c17c226a9458894285dbeb31338b76805804a01
Cristina Baldor <cbaldor@kippteamandfamily.org>
2024-09-27 18:59:33 +0000

    refactor

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__stipend_and_bonus_app.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__stipend_and_bonus_app.sql
@@ -1,28 +1,28 @@
 select
-    r.preferred_name_lastfirst,
+    o.*,
+    
+    r.preferred_name_lastfirst as teammate,
     r.job_title,
-    r.department_home_name,
-    r.home_work_location_name,
-    r.business_unit_home_name,
+    r.department_home_name as department,
+    r.home_work_location_name as location,
+    r.business_unit_home_name as entity,
     r.route,
 
-    o.*,
-
     case
-    when o.first_approver_employee_number is null and route = 'MDSO'
-    then 'MDSO Queue'
-    else r1.preferred_name_lastfirst
+        when o.first_approver_employee_number is null and route = 'MDSO'
+        then 'MDSO Queue'
+        else r1.preferred_name_lastfirst
     end as first_approver,
     case
-    when o.second_approver_employee_number is null and route = 'MDSO'
-    then 'MDSO Queue'
-    else r2.preferred_name_lastfirst
+        when o.second_approver_employee_number is null and route = 'MDSO'
+        then 'MDSO Queue'
+        else r2.preferred_name_lastfirst
     end as second_approver,
 
-from {{ ref("rpt_appsheet__stipend_app_roster") }} as r
+from {{ ref("stg_stipend_and_bonus__output") }} as o
 left join
-    {{ ref("stg_stipend_and_bonus__output") }} as o
-    on r.employee_number = o.employee_number
+    {{ ref("rpt_appsheet__stipend_app_roster") }} as r
+    on o.employee_number = r.employee_number
 left join
     {{ ref("base_people__staff_roster") }} as r1
     on o.first_approver_employee_number = r1.employee_number
