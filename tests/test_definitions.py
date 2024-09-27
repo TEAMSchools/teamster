82d275333b3c63cd453ad53b484d146126a40f02
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-09-27 19:23:56 +0000

    build(deps): python update

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -1,22 +1,50 @@
-def test_definitions_kipptaf():
-    from teamster.code_locations.kipptaf.definitions import defs
+def _test_definitions_validate(kwargs):
+    from dagster import __version__, instance_for_test
+    from dagster._cli.workspace.cli_target import get_workspace_from_kwargs
 
-    defs.get_inner_repository()
+    with instance_for_test() as instance:
+        with get_workspace_from_kwargs(
+            instance=instance, version=__version__, kwargs=kwargs
+        ) as workspace:
+            for code_location, entry in workspace.get_code_location_entries().items():
+                assert not entry.load_error, (
+                    f"Validation failed for code location {code_location} with "
+                    f"exception: {entry.load_error.message}."
+                )
+
+
+def test_definitions_kipptaf():
+    _test_definitions_validate(
+        {"module_name": ("teamster.code_locations.kipptaf.definitions",)}
+    )
 
 
 def test_definitions_kippcamden():
-    from teamster.code_locations.kippcamden.definitions import defs
-
-    defs.get_inner_repository()
+    _test_definitions_validate(
+        {"module_name": ("teamster.code_locations.kippcamden.definitions",)}
+    )
 
 
 def test_definitions_kippmiami():
-    from teamster.code_locations.kippmiami.definitions import defs
-
-    defs.get_inner_repository()
+    _test_definitions_validate(
+        {"module_name": ("teamster.code_locations.kippmiami.definitions",)}
+    )
 
 
 def test_definitions_kippnewark():
-    from teamster.code_locations.kippnewark.definitions import defs
+    _test_definitions_validate(
+        {"module_name": ("teamster.code_locations.kippnewark.definitions",)}
+    )
 
-    defs.get_inner_repository()
+
+def test_definitions_all():
+    _test_definitions_validate(
+        {
+            "module_name": (
+                "teamster.code_locations.kippcamden.definitions",
+                "teamster.code_locations.kippmiami.definitions",
+                "teamster.code_locations.kippnewark.definitions",
+                "teamster.code_locations.kipptaf.definitions",
+            )
+        }
+    )
