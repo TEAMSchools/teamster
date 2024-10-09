65c93f7e7409455f14c246b05275bcb35ab324d6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-10-09 15:05:02 +0000

    feat: fldoe fte transformations

--- src/dbt/kippmiami/models/fldoe/staging/stg_fldoe__fte.sql
+++ src/dbt/kippmiami/models/fldoe/staging/stg_fldoe__fte.sql
@@ -1,1 +1,14 @@
-select *, from {{ source("fldoe", "src_fldoe__fte") }}
+select
+    school_number,
+    student_id,
+    florida_student_id,
+    student_name,
+    grade,
+    _dagster_partition_survey as survey_number,
+
+    cast(fte_capped as numeric) as fte_capped,
+    cast(fte_uncapped as numeric) as fte_uncapped,
+
+    _dagster_partition_school_year + 2000 as fiscal_year,
+    _dagster_partition_school_year + 1999 as academic_year,
+from {{ source("fldoe", "src_fldoe__fte") }}
