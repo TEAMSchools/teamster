6b7a31fc88dee18666d4a4334549bd21072b3f96
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-10-11 21:10:22 +0000

    refactor: naming

--- src/teamster/code_locations/kippnewark/extracts/jobs.py
+++ src/teamster/code_locations/kippnewark/extracts/jobs.py
@@ -6,6 +6,6 @@ from teamster.code_locations.kippnewark.extracts.assets import (
 )
 
 powerschool_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_powerschool_extract_asset_job",
+    name=f"{CODE_LOCATION}__extracts__powerschool__asset_job",
     selection=powerschool_extract_assets,
 )
--- src/teamster/code_locations/kipptaf/extracts/jobs.py
+++ src/teamster/code_locations/kipptaf/extracts/jobs.py
@@ -12,40 +12,42 @@ from teamster.code_locations.kipptaf.extracts.assets import (
     littlesis_extract,
 )
 
-clever_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_clever_extract_asset_job",
-    selection=clever_extract_assets,
-)
-
 coupa_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_coupa_extract_asset_job", selection=[coupa_extract]
-)
-
-deanslist_annual_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_deanslist_annual_extract_asset_job",
-    selection=deanslist_annual_extract_assets,
+    name=f"{coupa_extract.key.to_python_identifier()}__asset_job",
+    selection=[coupa_extract],
 )
 
 deanslist_continuous_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_deanslist_continuous_extract_asset_job",
+    name=f"{deanslist_continuous_extract.key.to_python_identifier()}__asset_job",
     selection=[deanslist_continuous_extract],
 )
 
 egencia_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_egencia_extract_asset_job",
+    name=f"{egencia_extract.key.to_python_identifier()}__asset_job",
     selection=[egencia_extract],
 )
 
 idauto_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_idauto_extract_asset_job", selection=[idauto_extract]
-)
-
-illuminate_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_illuminate_extract_asset_job",
-    selection=illuminate_extract_assets,
+    name=f"{idauto_extract.key.to_python_identifier()}__asset_job",
+    selection=[idauto_extract],
 )
 
 littlesis_extract_asset_job = define_asset_job(
-    name=f"{CODE_LOCATION}_datagun_littlesis_extract_asset_job",
+    name=f"{littlesis_extract.key.to_python_identifier()}__asset_job",
     selection=[littlesis_extract],
+)
+
+clever_extract_asset_job = define_asset_job(
+    name=f"{CODE_LOCATION}__extracts__clever__asset_job",
+    selection=clever_extract_assets,
+)
+
+deanslist_annual_extract_asset_job = define_asset_job(
+    name=f"{CODE_LOCATION}__extracts__deanslist_annual__asset_job",
+    selection=deanslist_annual_extract_assets,
+)
+
+illuminate_extract_asset_job = define_asset_job(
+    name=f"{CODE_LOCATION}__extracts__illuminate__asset_job",
+    selection=illuminate_extract_assets,
 )
