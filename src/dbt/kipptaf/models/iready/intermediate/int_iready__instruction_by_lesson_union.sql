f39685cc93c04abcd8dea6e087d749ca9040016c
Anthony Walters <awalters@kippteamandfamily.org>
2024-10-17 15:25:57 +0000

    fix build issues

--- src/dbt/kipptaf/models/iready/intermediate/int_iready__instruction_by_lesson_union.sql
+++ src/dbt/kipptaf/models/iready/intermediate/int_iready__instruction_by_lesson_union.sql
@@ -6,6 +6,11 @@ select
     `subject`,
     lesson_id,
     lesson_name,
+    lesson_objective,
+    lesson_level,
+    lesson_grade,
+    passed_or_not_passed,
+    total_time_on_lesson_min,
     completion_date,
     passed_or_not_passed_numeric,
 
@@ -22,6 +27,11 @@ select
     `subject`,
     lesson as lesson_id,
     lesson as lesson_name,
+    null as lesson_objective,
+    null as lesson_level,
+    null as lesson_grade,
+    lesson_result as passed_or_not_passed,
+    lesson_time_on_task_min,
     completion_date,
     passed_or_not_passed_numeric,
 
