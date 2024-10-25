3f3b6419a6422afce57d1643691fa6ad89496779
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-10-25 15:57:37 +0000

    refactor: organize google dir joins

--- src/dbt/kipptaf/models/extracts/google/directory/rpt_google_directory__users_import.sql
+++ src/dbt/kipptaf/models/extracts/google/directory/rpt_google_directory__users_import.sql
@@ -4,9 +4,8 @@ with
             se.first_name as `givenName`,
             se.last_name as `familyName`,
             se.student_email_google as `primaryEmail`,
+            se.school_name,
             se.is_out_of_district,
-
-            o.org_unit_path,
 
             to_hex(sha1(se.student_web_password)) as `password`,
             if(se.grade_level >= 3, true, false) as `changePasswordAtNextLogin`,
@@ -15,10 +14,6 @@ with
                 'group-students-', lower(se.region), '@teamstudents.org'
             ) as `groupKey`,
         from {{ ref("base_powerschool__student_enrollments") }} as se
-        left join
-            {{ ref("stg_google_directory__orgunits") }} as o
-            on se.school_name = o.description
-            and o.org_unit_path like '/Students/%'
         where se.rn_all = 1 and se.student_email_google is not null
     ),
 
@@ -35,14 +30,15 @@ with
             u.name__given_name as given_name_target,
             u.name__family_name as family_name_target,
             u.suspended as suspended_target,
-            u.org_unit_path as org_unit_path_target,
+
+            o.org_unit_path as org_unit_path_target,
 
             'SHA-1' as `hashFunction`,
 
             if(
                 s.suspended or s.is_out_of_district,
                 '/Students/Disabled',
-                s.org_unit_path
+                o.org_unit_path
             ) as `orgUnitPath`,
 
             if(u.primary_email is not null, true, false) as is_matched,
@@ -50,6 +46,10 @@ with
         left join
             {{ ref("stg_google_directory__users") }} as u
             on s.primaryemail = u.primary_email
+        left join
+            {{ ref("stg_google_directory__orgunits") }} as o
+            on s.school_name = o.description
+            and o.org_unit_path like '/Students/%'
     ),
 
     final as (
