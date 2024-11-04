dd79daca848599b09d325ec3c1503aaf009e8725
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-11-04 18:17:19 +0000

    fix: kinds arg

--- src/teamster/libraries/dbt/asset_decorator.py
+++ src/teamster/libraries/dbt/asset_decorator.py
@@ -88,7 +88,7 @@ def dbt_external_source_assets(
         internal_asset_deps=internal_asset_deps,
         deps=deps,
         required_resource_keys=required_resource_keys,
-        kinds={"dbt"},
+        compute_kind="dbt",
         partitions_def=partitions_def,
         can_subset=True,
         op_tags=resolved_op_tags,
