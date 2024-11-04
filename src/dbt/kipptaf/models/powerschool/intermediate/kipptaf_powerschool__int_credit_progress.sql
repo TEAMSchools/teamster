f9d3b65f98e4fa513fd20dc689cf7808f995eef7
Gaby Rangel <grangel@kippteamandfamily.org>
2024-11-04 21:13:47 +0000

    hs credits

--- src/dbt/kipptaf/models/powerschool/intermediate/kipptaf_powerschool__int_credit_progress.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/kipptaf_powerschool__int_credit_progress.sql
@@ -27,6 +27,15 @@ with
             on e.student_number = i.local_student_id
     ),
 
+    -- note for charlie: open to finding a better way to do this, but this CTE is here
+    -- cuz: 1) the majority of students have two grad plans set on PS - the basic one,
+    -- which is called NJ State Diploma; and the advanced one: HS Distinction Diploma.
+    -- all students are expected to finish the advanced one, but if they don't, they
+    -- get defaulted to the basic one. however, PS allows credit waivers to be entered
+    -- only on non-basic grad plans and the unique id used to track the waivers match
+    -- only the advanced degree rows (i.e. i cannot bring waiver data for the basic
+    -- plan, and there is no way to tag which plan a student will end up completing,so
+    -- i need to bring both)
     waivers as (
         select
             sub.waivedcredits,
@@ -67,7 +76,129 @@ with
             and {{ union_dataset_join_clause(left_alias="c", right_alias="stuw") }}
             and stuw.gpnodeidforwaived = gpn.id
         where gpn.name != gp.name and sub.waivedcredits > 0
+    ),
+
+    -- note for charlie: this CTE saves me the time and error-possibilities when
+    -- calculating total credits completed, waived and the goal.
+    total_credits as (
+        select
+            e._dbt_source_relation,
+            e.academic_year,
+            e.students_dcid,
+            e.studentid,
+            e.student_number,
+
+            sub.earnedcredits as total_earnedcredits,  -- from stored grades
+            sub.enrolledcredits as total_enrolledcredits,  -- from cc table
+            sub.requestedcredits as total_requestedcredits,  -- from schedule requests table
+            sub.requiredcredits as total_requiredcredits,
+            sub.waivedcredits as total_waivedcredits,
+
+            gpn.id as gpnode_id,
+            gpn.name as gpnode_name,
+            gpn.creditcapacity as total_creditcapacity,
+
+            gp.name as gradplan_name,
+
+        from {{ ref("base_powerschool__student_enrollments") }} as e
+        left join
+            {{ ref("stg_powerschool__gpprogresssubject") }} as sub
+            on e.students_dcid = sub.studentsdcid
+            and {{ union_dataset_join_clause(left_alias="e", right_alias="sub") }}
+        inner join
+            {{ ref("stg_powerschool__gpnode") }} as gpn
+            on sub.gpnodeid = gpn.id
+            and sub.nodetype = gpn.nodetype
+            and {{ union_dataset_join_clause(left_alias="sub", right_alias="gpn") }}
+            and gpn.name in ('Total Credits', 'Overall Credits')
+        inner join
+            {{ ref("stg_powerschool__gpversion") }} as gpver
+            on gpn.gpversionid = gpver.id
+            and {{ union_dataset_join_clause(left_alias="gpn", right_alias="gpver") }}
+        inner join
+            {{ ref("stg_powerschool__gradplan") }} as gp
+            on gpver.gradplanid = gp.id
+            and gp.name in ('HS Distinction Diploma', 'NJ State Diploma')
+            and {{ union_dataset_join_clause(left_alias="gpver", right_alias="gp") }}
+        where
+            e.academic_year = {{ var("current_academic_year") }}
+            and e.school_level = 'HS'
+            and e.rn_year = 1
     )
 
-select *
-from waivers
+select
+    e._dbt_source_relation,
+    e.academic_year,
+    e.students_dcid,
+    e.studentid,
+    e.student_number,
+    e.grade_level,
+
+    sub.earnedcredits,  -- from stored grades
+    sub.enrolledcredits,  -- from cc table
+    sub.requestedcredits,  -- from schedule requests table
+    sub.requiredcredits,
+    sub.waivedcredits,
+    sub.isadvancedplan,
+
+    gpn.id as gpnode_id,
+    gpn.name as gpnode_name,
+    gpn.allowanyof,
+    gpn.allowwaiver,
+    gpn.altcompletioncount,
+    gpn.completioncount,
+    gpn.creditcapacity,
+    gpn.sortorder,
+
+    gpver.startyear,
+    gpver.endyear,
+    gpver.minimumgrade,
+    gpver.minimumgradepercentage,
+    coalesce(
+        gpver.minimumgpa.int_value,
+        gpver.minimumgpa.double_value,
+        gpver.minimumgpa.bytes_decimal_value
+    ) as minimumgpa,
+
+    gp.name as gradplan_name,
+
+    waiv.authorizedby,
+    waiv.waiveddate,
+
+    tc.total_earnedcredits,
+    tc.total_enrolledcredits,
+    tc.total_requestedcredits,
+    tc.total_waivedcredits,
+    tc.total_requiredcredits,
+    tc.total_creditcapacity,
+
+from {{ ref("base_powerschool__student_enrollments") }} as e
+left join
+    {{ ref("stg_powerschool__gpprogresssubject") }} as sub
+    on e.students_dcid = sub.studentsdcid
+    and {{ union_dataset_join_clause(left_alias="e", right_alias="sub") }}
+inner join
+    {{ ref("stg_powerschool__gpnode") }} as gpn
+    on sub.gpnodeid = gpn.id
+    and sub.nodetype = gpn.nodetype
+    and {{ union_dataset_join_clause(left_alias="sub", right_alias="gpn") }}
+    and gpn.name not in ('Total Credits', 'Overall Credits')
+inner join
+    {{ ref("stg_powerschool__gpversion") }} as gpver
+    on gpn.gpversionid = gpver.id
+    and {{ union_dataset_join_clause(left_alias="gpn", right_alias="gpver") }}
+inner join
+    {{ ref("stg_powerschool__gradplan") }} as gp
+    on gpver.gradplanid = gp.id
+    and gp.name in ('HS Distinction Diploma', 'NJ State Diploma')
+    and {{ union_dataset_join_clause(left_alias="gpver", right_alias="gp") }}
+left join waivers as waiv on e.student_number = waiv.student_number
+left join
+    total_credits as tc
+    on e.student_number = tc.student_number
+    and gp.name = tc.gradplan_name
+where
+    e.academic_year = {{ var("current_academic_year") }}
+    and e.school_level = 'HS'
+    and e.rn_year = 1
+    and gpn.name != gp.name
