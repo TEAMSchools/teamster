e2e09dae46dfd8af02a492c81c765887b662aa7e
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2024-11-07 17:43:12 +0000

    removing apostrophe from college because it causes some downstream problems for folks

--- src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__people_staff_info_feed_and_links.sql
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__people_staff_info_feed_and_links.sql
@@ -81,7 +81,7 @@ select
             coalesce(level_of_education, ''),
             /* undergraduate school */
             '&entry.844913390=',
-            coalesce(undergraduate_school, ''),
+            coalesce(replace(undergraduate_school, "'", ""), ''),
             /* years outside of kipp */
             '&entry.2136123484=',
             coalesce(cast(years_exp_outside_kipp as string), ''),
