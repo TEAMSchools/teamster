b72bfa115fd92c4a305ead7e35a5b7c9dbd55551
Anthony Walters <awalters@kippteamandfamily.org>
2024-11-12 16:43:29 +0000

    in progress

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__kfwd_dashboard.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__kfwd_dashboard.sql
@@ -7,247 +7,6 @@ with
         select {{ var("current_academic_year") - 1 }} as academic_year
     ),
 
-    app_rollup as (
-        select
-            applicant,
-            max(is_eof) as is_eof_applicant,
-            max(is_matriculated) as is_matriculated,
-            max(is_submitted_aa) as is_submitted_aa,
-            max(is_submitted_ba) as is_submitted_ba,
-            max(is_submitted_certificate) as is_submitted_certificate,
-            max(is_accepted_aa) as is_accepted_aa,
-            max(is_accepted_ba) as is_accepted_ba,
-            max(is_accepted_certificate) as is_accepted_certificate,
-            max(is_early_action_decision) as is_early_action_decision,
-
-            sum(if(is_submitted, 1, 0)) as n_submitted,
-            sum(if(is_accepted, 1, 0)) as n_accepted,
-
-            sum(if(application_submission_status = 'Wishlist', 1, 0)) as n_wishlist,
-
-            max(
-                case
-                    when is_early_action_decision and is_submitted and is_accepted
-                    then true
-                    when is_early_action_decision and is_submitted and not is_accepted
-                    then false
-                end
-            ) as is_accepted_early,
-            max(
-                case
-                    when
-                        is_early_action_decision
-                        and is_submitted
-                        and is_accepted
-                        and adjusted_6_year_minority_graduation_rate >= 60
-                    then true
-                    when
-                        is_early_action_decision
-                        and is_submitted
-                        and not is_accepted
-                        and adjusted_6_year_minority_graduation_rate >= 60
-                    then false
-                end
-            ) as is_accepted_early_ecc_60_plus,
-            max(
-                case
-                    when
-                        is_early_action_decision
-                        and is_submitted
-                        and is_accepted
-                        and adjusted_6_year_minority_graduation_rate >= 90
-                    then true
-                    when
-                        is_early_action_decision
-                        and is_submitted
-                        and not is_accepted
-                        and adjusted_6_year_minority_graduation_rate >= 90
-                    then false
-                end
-            ) as is_accepted_early_ecc_90_plus,
-            round(
-                avg(
-                    if(
-                        application_submission_status = 'Wishlist',
-                        adjusted_6_year_minority_graduation_rate,
-                        null
-                    )
-                ),
-                0
-            ) as ecc_wishlist_avg,
-            round(
-                min(
-                    if(
-                        application_submission_status = 'Wishlist',
-                        adjusted_6_year_minority_graduation_rate,
-                        null
-                    )
-                ),
-                0
-            ) as ecc_wishlist_min,
-            round(
-                avg(
-                    if(
-                        application_submission_status = 'Submitted',
-                        adjusted_6_year_minority_graduation_rate,
-                        null
-                    )
-                ),
-                0
-            ) as ecc_submitted_avg,
-            round(
-                min(
-                    if(
-                        application_submission_status = 'Submitted',
-                        adjusted_6_year_minority_graduation_rate,
-                        null
-                    )
-                ),
-                0
-            ) as ecc_submitted_min,
-            round(
-                avg(
-                    if(
-                        application_status = 'Accepted',
-                        adjusted_6_year_minority_graduation_rate,
-                        null
-                    )
-                ),
-                0
-            ) as ecc_accepted_avg,
-            round(
-                min(
-                    if(
-                        application_status = 'Accepted',
-                        adjusted_6_year_minority_graduation_rate,
-                        null
-                    )
-                ),
-                0
-            ) as ecc_accepted_min,
-            round(
-                avg(
-                    if(
-                        matriculation_decision = 'Matriculated (Intent to Enroll)'
-                        and not transfer_application,
-                        adjusted_6_year_minority_graduation_rate,
-                        null
-                    )
-                ),
-                0
-            ) as ecc_matriculated_avg,
-            round(
-                min(
-                    if(
-                        matriculation_decision = 'Matriculated (Intent to Enroll)'
-                        and not transfer_application,
-                        adjusted_6_year_minority_graduation_rate,
-                        null
-                    )
-                ),
-                0
-            ) as ecc_matriculated_min,
-            sum(
-                if(
-                    account_type in ('Public 4 yr', 'Private 4 yr')
-                    and application_submission_status = 'Wishlist',
-                    1,
-                    0
-                )
-            ) as n_4_year_wishlist,
-            count(
-                if(
-                    adjusted_6_year_minority_graduation_rate >= 55
-                    and application_submission_status = 'Wishlist',
-                    id,
-                    null
-                )
-            ) as n_55plus_ecc_wishlist,
-            count(
-                if(
-                    adjusted_6_year_minority_graduation_rate >= 60
-                    and application_submission_status = 'Wishlist',
-                    id,
-                    null
-                )
-            ) as n_60plus_ecc_wishlist,
-            count(
-                if(
-                    adjusted_6_year_minority_graduation_rate >= 68
-                    and application_submission_status = 'Wishlist',
-                    id,
-                    null
-                )
-            ) as n_68plus_ecc_wishlist,
-            count(
-                if(
-                    account_billing_state = 'NJ'
-                    and application_submission_status = 'Wishlist',
-                    id,
-                    null
-                )
-            ) as n_nj_wishlist,
-            count(
-                if(
-                    meets_full_need and application_submission_status = 'Wishlist',
-                    id,
-                    null
-                )
-            ) as n_meets_full_need_wishlist,
-            count(
-                if(
-                    is_strong_oos_option and application_submission_status = 'Wishlist',
-                    id,
-                    null
-                )
-            ) as n_strong_oos_wishlist,
-            count(
-                if(
-                    account_type in (
-                        'Non-profit',
-                        'NonProfit',
-                        'Private',
-                        'Private 2 yr',
-                        'Public 2 yr'
-                    )
-                    and application_submission_status = 'Wishlist',
-                    id,
-                    null
-                )
-            ) as n_aa_cte_wishlist,
-            count(
-                if(
-                    adjusted_6_year_minority_graduation_rate >= 68
-                    and application_submission_status = 'Wishlist'
-                    and is_early_action_decision,
-                    id,
-                    null
-                )
-            ) as n_68plus_ecc_ea_ed_wishlist,
-            count(
-                if(
-                    meets_full_need
-                    and application_submission_status = 'Wishlist'
-                    and is_early_action_decision,
-                    id,
-                    null
-                )
-            ) as n_meets_full_need_ea_ed_wishlist,
-            count(
-                if(
-                    meets_full_need
-                    and application_submission_status = 'Wishlist'
-                    and is_early_action_decision
-                    and adjusted_6_year_minority_graduation_rate >= 85,
-                    id,
-                    null
-                )
-            ) as n_meets_full_need_ea_ed_85ecc_wishlist,
-            sum(if(application_status is not null, 1, 0)) as n_app_outcomes,
-        from {{ ref("base_kippadb__application") }}
-        group by applicant
-    ),
-
     gpa_by_semester as (
         select
             student,
@@ -945,6 +704,22 @@ select
         else 0
     end as is_wishlist_quality_bar_int,
 
+    case
+        when
+            round(c.contact_college_match_display_gpa, 2) >= 3.50
+            and ar.n_meets_full_need_85plus_ecc_ea_ed_submitted >= 1
+            and ar.n_68_plus_ecc_submitted >= 2
+        then 1
+        when
+            ocf.is_ed_ea = 'Yes'
+            and ar.n_68_plus_ecc_submitted >= 2
+            and ar.n_meets_full_need_68plus_ecc_ea_ed_submitted >= 1
+        then 1
+        when
+            cf.best_guess_pathway = '4-year'
+        else 0 
+    end as is_submitted_quality_bar_int,
+
     if(
         ei.ecc_pursuing_degree_type in ("Bachelor's (4-year)", "Associate's (2 year)"),
         true,
@@ -963,7 +738,7 @@ left join
     and apps.matriculation_decision = 'Matriculated (Intent to Enroll)'
     and not apps.transfer_application
     and apps.rn_app_enr = 1
-left join app_rollup as ar on c.contact_id = ar.applicant
+left join {{ ref("int_kippadb__app_rollup") }} as ar on c.contact_id = ar.applicant
 left join
     {{ ref("int_kippadb__contact_note_rollup") }} as cnr
     on c.contact_id = cnr.contact_id
