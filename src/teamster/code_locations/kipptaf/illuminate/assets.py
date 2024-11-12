033918f41e876758f39fe586a7aa8c546489f3fc
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-11-12 15:27:22 +0000

    feat: dlt

--- src/teamster/code_locations/kipptaf/illuminate/assets.py
+++ src/teamster/code_locations/kipptaf/illuminate/assets.py
@@ -1,8 +1,7 @@
 from dagster import AssetExecutionContext
 from dagster_embedded_elt.dlt import DagsterDltResource, dlt_assets
 from dlt import pipeline
-
-from teamster.libraries.dlt_sources.sql_database import sql_database
+from dlt.sources.sql_database import sql_database
 
 
 @dlt_assets(
