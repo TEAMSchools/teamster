3a4f46dc2e1e1929a845b8e895631bdcc087e68a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-12-03 15:27:10 +0000

    fix: resources

--- src/teamster/code_locations/kippnewark/deanslist/assets.py
+++ src/teamster/code_locations/kippnewark/deanslist/assets.py
@@ -92,7 +92,12 @@ behavior = build_deanslist_paginated_multi_partition_asset(
     partitions_def=DEANSLIST_FISCAL_MULTI_PARTITIONS_DEF,
     op_tags={
         "dagster-k8s/config": {
-            "container_config": {"resources": {"limits": {"memory": "4.0Gi"}}}
+            "container_config": {
+                "resources": {
+                    "requests": {"cpu": "250m"},
+                    "limits": {"cpu": "1250m", "memory": "4.0Gi"},
+                }
+            }
         }
     },
 )
