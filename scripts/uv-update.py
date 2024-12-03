1d801f709650e3e5d2cdfc3017f74733b99aa05d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-12-03 18:29:20 +0000

    chore: uv update script

---
+++ scripts/uv-update.py
@@ -0,0 +1,21 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = []
+# ///
+
+import subprocess
+
+
+def main() -> None:
+    commands = [
+        "uv lock --upgrade",
+        "uv export --output-file requirements.txt",
+        "uv sync",
+    ]
+
+    for cmd in commands:
+        subprocess.run(args=cmd.split(sep=" "))
+
+
+if __name__ == "__main__":
+    main()
