0143612962a56e512ce72bf11a71a47ecbd67457
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-12-10 17:13:38 +0000

    chore: retire tableau wkbk stats asset

--- src/teamster/code_locations/kipptaf/tableau/assets.py
+++ src/teamster/code_locations/kipptaf/tableau/assets.py
@@ -3,22 +3,11 @@ import pathlib
 from dagster import config_from_files
 
 from teamster.code_locations.kipptaf import CODE_LOCATION, LOCAL_TIMEZONE
-from teamster.code_locations.kipptaf.tableau.schema import WORKBOOK_SCHEMA
-from teamster.libraries.tableau.assets import (
-    build_tableau_workbook_refresh_asset,
-    build_tableau_workbook_stats_asset,
-)
+from teamster.libraries.tableau.assets import build_tableau_workbook_refresh_asset
 
 config_assets = config_from_files(
     [f"{pathlib.Path(__file__).parent}/config/assets.yaml"]
 )["assets"]
-
-workbook_stats = build_tableau_workbook_stats_asset(
-    code_location=CODE_LOCATION,
-    workbook_ids=[a["metadata"]["id"] for a in config_assets],
-    partition_start_date="2024-02-26",
-    schema=WORKBOOK_SCHEMA,
-)
 
 workbook_refresh_assets = [
     build_tableau_workbook_refresh_asset(
@@ -31,6 +20,5 @@ workbook_refresh_assets = [
 ]
 
 assets = [
-    workbook_stats,
     *workbook_refresh_assets,
 ]
