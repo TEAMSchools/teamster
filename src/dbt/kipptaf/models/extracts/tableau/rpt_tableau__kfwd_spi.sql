2148f22c9fff951588e781bb9d5058e5a4081f39
Anthony Walters <awalters@kippteamandfamily.org>
2024-12-11 21:49:21 +0000

    add CTE to enrollments union, change persisting calc

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__kfwd_spi.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__kfwd_spi.sql
@@ -44,8 +44,9 @@ select
     if(e.id = ei.ugrad_enrollment_id, true, false) as is_ugrad_enrollment,
     if(
         e.status in ('Attending', 'Graduated')
-        and a.id = ei.ecc_account_id
-        and r.ktc_cohort <= {{ var("current_academic_year") - 7 }},
+        and r.ktc_cohort
+        between {{ var("current_academic_year") - 5 }}
+        and {{ var("current_academic_year") }},
         true,
         false
     ) as is_continuing_completing,
@@ -121,7 +122,6 @@ from {{ ref("int_kippadb__roster") }} as r
 inner join
     {{ ref("stg_kippadb__enrollment") }} as e
     on r.contact_id = e.student
-    and e.pursuing_degree_type in ("Bachelor's (4-year)", "Associate's (2 year)")
     and e.status != 'Did Not Enroll'
 left join {{ ref("stg_kippadb__account") }} as a on e.school = a.id
 left join {{ ref("int_kippadb__enrollment_pivot") }} as ei on r.contact_id = ei.student
