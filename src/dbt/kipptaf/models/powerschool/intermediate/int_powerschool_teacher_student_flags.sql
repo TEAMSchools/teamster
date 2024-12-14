5407998fbfac04a9f5f853c32b73ed37911b1d86
Gaby Rangel <grangel@kippteamandfamily.org>
2024-12-14 21:38:07 +0000

    trunk

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool_teacher_student_flags.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool_teacher_student_flags.sql
@@ -19,28 +19,3 @@ left join
     and t.school_level = f.school_level
     and t.assignment_category_code = f.code
     and f.cte_grouping = 'assignment_student'
-
-    /*
-    a.student_number,
-    a.raw_score,
-    a.score_entered,
-    a.assign_final_score_percent,
-    a.is_exempt,
-    a.is_late,
-    a.is_missing,
-
-    a.cte_grouping,
-    a.audit_flag_name,
-    a.audit_flag_value,
-
-left join
-    {{ ref("int_tableau__gradebook_audit_flags") }} as a
-    on t.quarter = a.quarter
-    and t.week_number_quarter = a.week_number
-    and t.schoolid = a.schoolid
-    and t.sectionid = a.sectionid
-    and t.assignment_category_code = a.assignment_category_code
-    and t.teacher_assign_id = a.teacher_assign_id
-    and a.cte_grouping = 'assignment_student'
-    */
-    
