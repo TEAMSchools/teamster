5b40224902e06588375cc525e9f1b9a30873c271
Gaby Rangel <grangel@kippteamandfamily.org>
2024-12-14 21:36:56 +0000

    trunk

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__gradebook_audit_ms_hs.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__gradebook_audit_ms_hs.sql
@@ -92,41 +92,6 @@ with
             and r.assignment_category_code = f.code
             and f.cte_grouping = 'assignment_student'
         where r.school_level = 'HS'
-    ),
-
-    teacher_student_assignments as (
-        select
-            t.quarter,
-            t.week_number_quarter,
-            t.schoolid,
-
-            t.sectionid,
-            t.assignment_category_code,
-            t.teacher_assign_id,
-
-            a.student_number,
-            a.raw_score,
-            a.score_entered,
-            a.assign_final_score_percent,
-            a.is_exempt,
-            a.is_late,
-            a.is_missing,
-
-            a.cte_grouping,
-            a.audit_flag_name,
-            a.audit_flag_value,
-
-        from {{ ref("int_powerschool__teacher_assignment_audit_base") }} as t
-        left join
-            {{ ref("int_tableau__gradebook_audit_flags") }} as a
-            on t.quarter = a.quarter
-            and t.week_number_quarter = a.week_number
-            and t.schoolid = a.schoolid
-            and t.sectionid = a.sectionid
-            and t.assignment_category_code = a.assignment_category_code
-            and t.teacher_assign_id = a.teacher_assign_id
-            and a.cte_grouping = 'assignment_student'
-            and t.school_level != 'ES'
     )
 
 select
