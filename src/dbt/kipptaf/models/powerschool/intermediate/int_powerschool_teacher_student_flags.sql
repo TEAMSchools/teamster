a4a71a911a9e92417fb05f671cf5a89d69aec200
Gaby Rangel <grangel@kippteamandfamily.org>
2024-12-16 04:08:54 +0000

    new builds

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool_teacher_student_flags.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool_teacher_student_flags.sql
@@ -1,5 +1,32 @@
+with
+    expected_flags as (
+        select
+            t._dbt_source_relation,
+            t.school_level,
+            t.quarter,
+            t.week_number_quarter,
+            t.schoolid,
+
+            t.sectionid,
+            t.assignment_category_code,
+            t.teacher_assign_id,
+
+            f.audit_category,
+            f.audit_flag_name,
+            f.cte_grouping,
+
+        from {{ ref("int_powerschool__teacher_assignment_audit_base") }} as t
+        left join
+            {{ ref("stg_reporting__gradebook_flags") }} as f
+            on t.region = f.region
+            and t.school_level = f.school_level
+            and t.assignment_category_code = f.code
+            and f.cte_grouping = 'assignment_student'
+    )
+
 select
     t._dbt_source_relation,
+    t.school_level,
     t.quarter,
     t.week_number_quarter,
     t.schoolid,
@@ -8,14 +35,28 @@ select
     t.assignment_category_code,
     t.teacher_assign_id,
 
-    f.audit_category,
-    f.audit_flag_name,
-    f.cte_grouping,
+    t.audit_category,
+    t.audit_flag_name as expected_audit_flag_name,
+    t.cte_grouping,
 
-from {{ ref("int_powerschool__teacher_assignment_audit_base") }} as t
+    a.student_number,
+    a.raw_score,
+    a.score_entered,
+    a.assign_final_score_percent,
+    a.is_exempt,
+    a.is_late,
+    a.is_missing,
+
+    a.audit_flag_name,
+    a.audit_flag_value,
+
+from expected_flags as t
 left join
-    {{ ref("stg_reporting__gradebook_flags") }} as f
-    on t.region = f.region
-    and t.school_level = f.school_level
-    and t.assignment_category_code = f.code
-    and f.cte_grouping = 'assignment_student'
+    {{ ref("int_tableau__gradebook_audit_flags") }} as a
+    on t.quarter = a.quarter
+    and t.week_number_quarter = a.week_number
+    and t.schoolid = a.schoolid
+    and t.sectionid = a.sectionid
+    and t.assignment_category_code = a.assignment_category_code
+    and t.teacher_assign_id = a.teacher_assign_id
+    and t.cte_grouping = a.cte_grouping
