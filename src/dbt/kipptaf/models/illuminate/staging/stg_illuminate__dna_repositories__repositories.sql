4fa53d5469a480386a1863d4d44c05097a867b00
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-12-19 16:17:50 +0000

    fix: convert date

--- src/dbt/kipptaf/models/illuminate/staging/stg_illuminate__dna_repositories__repositories.sql
+++ src/dbt/kipptaf/models/illuminate/staging/stg_illuminate__dna_repositories__repositories.sql
@@ -1,1 +1,2 @@
-select *, from {{ source("illuminate_dna_repositories", "repositories") }}
+select * except (date_administered), date(date_administered) as date_administered,
+from {{ source("illuminate_dna_repositories", "repositories") }}
