9296b2f9c9905a35797f6e4530e1885f81c0bcce
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-01-02 22:07:55 +0000

    feat: g&t/lep fields

---
+++ src/dbt/kipptaf/models/extracts/illuminate/rpt_illuminate__programs.sql
@@ -0,0 +1,19 @@
+select
+    -- trunk-ignore-begin(sqlfluff/RF05)
+    null as `01 Import Student ID`,
+    null as `02 State Student ID`,
+    null as `03 Student Last Name`,
+    null as `04 Student First Name`,
+    null as `05 Student Middle Name`,
+    null as `06 Birth Date`,
+    null as `07 Program ID`,
+    null as `08 Eligibility Start Date`,
+    null as `09 Eligibility End Date`,
+    null as `10 Program Start Date`,
+    null as `11 Program End Date`,
+    null as `12 Academic Year`,
+    null as `13 Program Eligibility Code`,
+    null as `14 Program Exit Code`,
+    null as `15 Site ID`,
+    -- trunk-ignore-end(sqlfluff/RF05)
+    
