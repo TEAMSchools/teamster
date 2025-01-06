b74f4060cf6a6b7167046b83e2310fe44fbf84d6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-01-06 16:46:45 +0000

    refactor: rm observation details assert

--- src/teamster/code_locations/kipptaf/couchdrop/sensors.py
+++ src/teamster/code_locations/kipptaf/couchdrop/sensors.py
@@ -1,14 +1,11 @@
 from teamster.code_locations.kipptaf import CODE_LOCATION, LOCAL_TIMEZONE
-from teamster.code_locations.kipptaf.performance_management.assets import (
-    observation_details,
-)
-from teamster.code_locations.kipptaf.tableau.assets import traffic_to_views
+from teamster.code_locations.kipptaf.tableau.assets import view_count_per_view
 from teamster.libraries.couchdrop.sensors import build_couchdrop_sftp_sensor
 
 couchdrop_sftp_sensor = build_couchdrop_sftp_sensor(
     code_location=CODE_LOCATION,
     local_timezone=LOCAL_TIMEZONE,
-    asset_selection=[observation_details, traffic_to_views],
+    asset_selection=[view_count_per_view],
     minimum_interval_seconds=(60 * 10),
     exclude_dirs=[
         "/data-team/kipptaf/dayforce",
