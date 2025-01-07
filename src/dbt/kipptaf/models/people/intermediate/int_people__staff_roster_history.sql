e634fef0c7bc838f3b263d4587760c1955f7349d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-01-07 20:31:07 +0000

    fix: join to employee number

--- src/dbt/kipptaf/models/people/intermediate/int_people__staff_roster_history.sql
+++ src/dbt/kipptaf/models/people/intermediate/int_people__staff_roster_history.sql
@@ -2,94 +2,103 @@ with
     adp_df_union as (
         select
             '{{ ref("int_adp_workforce_now__workers") }}' as _dbt_source_relation,
-            associate_oid,
-            effective_date_end,
-            effective_date_end_timestamp,
-            is_current_record,
-            worker_id__id_value as worker_id,
-            worker_dates__original_hire_date as worker_original_hire_date,
-            worker_dates__rehire_date as worker_rehire_date,
-            worker_dates__termination_date as worker_termination_date,
-            worker_status__status_code__code_value as worker_status_code,
-            work_assignment__termination_date as work_assignment_termination_date,
-            work_assignment__actual_start_date as work_assignment_actual_start_date,
-            person__formatted_name as formatted_name,
-            person__family_name_1 as family_name_1,
-            person__given_name as given_name,
-            person__legal_name__formatted_name as legal_formatted_name,
-            person__legal_name__family_name_1 as legal_family_name,
-            person__legal_name__given_name as legal_given_name,
-            person__birth_date as birth_date,
-            person__race_code_name as race_code,
-            person__ethnicity_code_name as ethnicity_code,
-            person__gender_code_name as gender_code,
-            person__legal_address__city_name as legal_address_city_name,
-            person__legal_address__country_subdivision_level_1__code_value
+            w.associate_oid,
+            w.effective_date_end,
+            w.effective_date_end_timestamp,
+            w.is_current_record,
+            w.worker_id__id_value as worker_id,
+            w.worker_dates__original_hire_date as worker_original_hire_date,
+            w.worker_dates__rehire_date as worker_rehire_date,
+            w.worker_dates__termination_date as worker_termination_date,
+            w.worker_status__status_code__code_value as worker_status_code,
+            w.work_assignment__termination_date as work_assignment_termination_date,
+            w.work_assignment__actual_start_date as work_assignment_actual_start_date,
+            w.person__formatted_name as formatted_name,
+            w.person__family_name_1 as family_name_1,
+            w.person__given_name as given_name,
+            w.person__legal_name__formatted_name as legal_formatted_name,
+            w.person__legal_name__family_name_1 as legal_family_name,
+            w.person__legal_name__given_name as legal_given_name,
+            w.person__birth_date as birth_date,
+            w.person__race_code_name as race_code,
+            w.person__ethnicity_code_name as ethnicity_code,
+            w.person__gender_code_name as gender_code,
+            w.person__legal_address__city_name as legal_address_city_name,
+            w.person__legal_address__country_subdivision_level_1__code_value
             as legal_address_country_subdivision_level_1_code,
-            person__legal_address__line_one as legal_address_line_one,
-            person__legal_address__postal_code as legal_address_postal_code,
-            is_prestart,
-            item_id,
-            position_id,
-            job_title,
-            primary_indicator,
-            management_position_indicator,
-            assignment_status__status_code__name as assignment_status,
-            assignment_status__status_code__name__lag as assignment_status_lag,
-            assignment_status__effective_date as assignment_status_effective_date,
-            assignment_status__reason_code__name as assignment_status_reason,
-            home_work_location__name_code__name as home_work_location_name,
-            worker_type_code__name as worker_type_code,
-            wage_law_coverage__wage_law_name_code__name as wage_law_name,
-            wage_law_coverage__coverage_code__name as wage_law_coverage,
-            base_remuneration__annual_rate_amount__amount_value
+            w.person__legal_address__line_one as legal_address_line_one,
+            w.person__legal_address__postal_code as legal_address_postal_code,
+            w.is_prestart,
+            w.item_id,
+            w.position_id,
+            w.job_title,
+            w.primary_indicator,
+            w.management_position_indicator,
+            w.assignment_status__status_code__name as assignment_status,
+            w.assignment_status__status_code__name__lag as assignment_status_lag,
+            w.assignment_status__effective_date as assignment_status_effective_date,
+            w.assignment_status__reason_code__name as assignment_status_reason,
+            w.home_work_location__name_code__name as home_work_location_name,
+            w.worker_type_code__name as worker_type_code,
+            w.wage_law_coverage__wage_law_name_code__name as wage_law_name,
+            w.wage_law_coverage__coverage_code__name as wage_law_coverage,
+            w.base_remuneration__annual_rate_amount__amount_value
             as base_remuneration_annual_rate_amount,
-            additional_remunerations__rate__amount_value__sum
+            w.additional_remunerations__rate__amount_value__sum
             as additional_remunerations_rate_amount,
-            communication__personal_cell__formatted_number as personal_cell,
-            communication__personal_email__email_uri as personal_email,
-            communication__work_cell__formatted_number as work_cell,
-            communication__work_email__email_uri as work_email,
-            custom_field__employee_number,
-            wf_mgr_accrual_profile,
-            wf_mgr_badge_number,
-            wf_mgr_ee_type,
-            wf_mgr_pay_rule,
-            wf_mgr_trigger,
-            organizational_unit__assigned__business_unit__code_value
+            w.communication__personal_cell__formatted_number as personal_cell,
+            w.communication__personal_email__email_uri as personal_email,
+            w.communication__work_cell__formatted_number as work_cell,
+            w.communication__work_email__email_uri as work_email,
+            w.custom_field__employee_number,
+            w.wf_mgr_accrual_profile,
+            w.wf_mgr_badge_number,
+            w.wf_mgr_ee_type,
+            w.wf_mgr_pay_rule,
+            w.wf_mgr_trigger,
+            w.organizational_unit__assigned__business_unit__code_value
             as assigned_business_unit_code,
-            organizational_unit__assigned__business_unit__name
+            w.organizational_unit__assigned__business_unit__name
             as assigned_business_unit_name,
-            organizational_unit__assigned__department__name as assigned_department_name,
-            organizational_unit__home__business_unit__code_value
+            w.organizational_unit__assigned__department__name
+            as assigned_department_name,
+            w.organizational_unit__home__business_unit__code_value
             as home_business_unit_code,
-            organizational_unit__home__business_unit__name as home_business_unit_name,
-            organizational_unit__home__department__name as home_department_name,
-            reports_to_worker_id__id_value as reports_to_worker_id,
-            reports_to_formatted_name,
-            payroll_file_number,
-            payroll_group_code,
-            benefits_eligibility_class__group_code__name as benefits_eligibility_class,
-            worker_hire_date_recent,
-            wf_mgr_trigger_new,
+            w.organizational_unit__home__business_unit__name as home_business_unit_name,
+            w.organizational_unit__home__department__name as home_department_name,
+            w.reports_to_worker_id__id_value as reports_to_worker_id,
+            w.reports_to_formatted_name,
+            w.payroll_file_number,
+            w.payroll_group_code,
+            w.benefits_eligibility_class__group_code__name
+            as benefits_eligibility_class,
+            w.worker_hire_date_recent,
+            w.wf_mgr_trigger_new,
 
-            null as employee_number,
+            en.employee_number,
+
             null as race_ethnicity_reporting,
             null as reports_to_employee_number,
 
             if(
-                effective_date_start < '2021-01-01', '2021-01-01', effective_date_start
+                w.effective_date_start < '2021-01-01',
+                '2021-01-01',
+                w.effective_date_start
             ) as effective_date_start,
 
             if(
-                effective_date_start_timestamp
+                w.effective_date_start_timestamp
                 < timestamp('2021-01-01', '{{ var("local_timezone") }}'),
                 timestamp('2021-01-01', '{{ var("local_timezone") }}'),
-                effective_date_start_timestamp
+                w.effective_date_start_timestamp
             ) as effective_date_start_timestamp,
-        from {{ ref("int_adp_workforce_now__workers") }}
+        from {{ ref("int_adp_workforce_now__workers") }} as w
+        inner join
+            {{ ref("stg_people__employee_numbers") }} as en
+            on w.worker_id = en.adp_associate_id
+            and en.is_active
         /* after transistion from Dayforce */
-        where effective_date_end >= '2021-01-01'
+        where w.effective_date_end >= '2021-01-01'
 
         union all
 
@@ -170,12 +179,13 @@ with
 select
     w._dbt_source_relation,
     w.associate_oid,
+    w.worker_id,
+    w.employee_number,
     w.effective_date_start,
     w.effective_date_end,
     w.effective_date_start_timestamp,
     w.effective_date_end_timestamp,
     w.is_current_record,
-    w.worker_id,
     w.worker_original_hire_date,
     w.worker_rehire_date,
     w.worker_termination_date,
@@ -222,6 +232,7 @@ select
     w.wf_mgr_ee_type,
     w.wf_mgr_pay_rule,
     w.wf_mgr_trigger,
+    w.wf_mgr_trigger_new,
     w.assigned_business_unit_code,
     w.assigned_business_unit_name,
     w.assigned_department_name,
@@ -234,7 +245,6 @@ select
     w.payroll_group_code,
     w.benefits_eligibility_class,
     w.worker_hire_date_recent,
-    w.wf_mgr_trigger_new,
 
     lc.region as home_work_location_region,
     lc.dagster_code_location as home_work_location_dagster_code_location,
@@ -276,12 +286,8 @@ select
     lower(rtldap.user_principal_name) as reports_to_user_principal_name,
     lower(rtldap.mail) as reports_to_mail,
 
-    coalesce(w.employee_number, en.employee_number) as employee_number,
-
     coalesce(
-        idps.powerschool_teacher_number,
-        cast(w.employee_number as string),
-        cast(en.employee_number as string)
+        idps.powerschool_teacher_number, cast(w.employee_number as string)
     ) as powerschool_teacher_number,
 
     coalesce(
@@ -308,10 +314,6 @@ select
         else false
     end as is_hispanic,
 from adp_df_union as w
-inner join
-    {{ ref("stg_people__employee_numbers") }} as en
-    on w.worker_id = en.adp_associate_id
-    and en.is_active
 left join
     {{ ref("stg_people__employee_numbers") }} as rten
     on w.reports_to_worker_id = rten.adp_associate_id
@@ -321,14 +323,14 @@ left join
     on w.home_work_location_name = lc.name
 left join
     {{ ref("stg_ldap__user_person") }} as ldap
-    on coalesce(w.employee_number, en.employee_number) = ldap.employee_number
+    on w.employee_number = ldap.employee_number
 left join
     {{ ref("stg_people__powerschool_crosswalk") }} as idps
-    on coalesce(w.employee_number, en.employee_number) = idps.employee_number
+    on w.employee_number = idps.employee_number
     and idps.is_active
 left join
     {{ ref("int_surveys__staff_information_survey_pivot") }} as sis
-    on coalesce(w.employee_number, en.employee_number) = sis.employee_number
+    on w.employee_number = sis.employee_number
 left join
     {{ ref("stg_ldap__user_person") }} as rtldap
     on coalesce(w.reports_to_employee_number, rten.employee_number)
