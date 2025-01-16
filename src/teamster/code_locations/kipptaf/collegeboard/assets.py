62c98067f55dafd6c1cf87cad280ae59bbc8aa31
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-01-16 21:53:36 +0000

    feat: college board assets

---
+++ src/teamster/code_locations/kipptaf/collegeboard/assets.py
@@ -0,0 +1,18 @@
+from dagster import StaticPartitionsDefinition
+
+from teamster.code_locations.kipptaf import CODE_LOCATION
+from teamster.code_locations.kipptaf.collegeboard.schema import PSAT_SCHEMA
+from teamster.libraries.sftp.assets import build_sftp_folder_asset
+
+psat = build_sftp_folder_asset(
+    asset_key=[CODE_LOCATION, "collegeboard", "psat"],
+    remote_dir_regex=r"/data-team/kipptaf/collegeboard/psat",
+    remote_file_regex=r"\d+_(?P<test_type>\w+)_\d+_\d+\.csv",
+    ssh_resource_key="ssh_couchdrop",
+    avro_schema=PSAT_SCHEMA,
+    partitions_def=StaticPartitionsDefinition(["PSAT89", "PSATNM"]),
+)
+
+assets = [
+    psat,
+]
