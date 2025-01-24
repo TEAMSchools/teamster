6ec53d77d1cb3e140c1f8d9ec67d57a10a98575d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-01-24 17:49:35 +0000

    fix: replace get table with query job

--- src/teamster/code_locations/kipptaf/adp/workforce_now/api/jobs.py
+++ src/teamster/code_locations/kipptaf/adp/workforce_now/api/jobs.py
@@ -2,17 +2,14 @@ from dagster import RunConfig, job
 
 from teamster.code_locations.kipptaf import CODE_LOCATION
 from teamster.libraries.adp.workforce_now.api.ops import adp_wfn_update_workers_op
-from teamster.libraries.google.bigquery.ops import (
-    BigQueryGetTableOpConfig,
-    bigquery_get_table_op,
-)
+from teamster.libraries.google.bigquery.ops import BigQueryOpConfig, bigquery_query_op
 
 
 @job(
     name=f"{CODE_LOCATION}_adp_wfn_update_workers_job",
     config=RunConfig(
         ops={
-            "bigquery_get_table_op": BigQueryGetTableOpConfig(
+            "bigquery_query_op": BigQueryOpConfig(
                 dataset_id="kipptaf_extracts",
                 table_id="rpt_adp_workforce_now__worker_update",
             )
@@ -21,6 +18,6 @@ from teamster.libraries.google.bigquery.ops import (
     tags={"job_type": "op"},
 )
 def adp_wfn_update_workers_job():
-    worker_data = bigquery_get_table_op()
+    worker_data = bigquery_query_op()
 
     adp_wfn_update_workers_op(worker_data)
