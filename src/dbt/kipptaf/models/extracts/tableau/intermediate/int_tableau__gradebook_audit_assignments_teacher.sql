7dc44e944f2851cbac0528e3b4a739d52f30ed5d
Gaby Rangel <grangel@kippteamandfamily.org>
2025-01-27 20:37:21 +0000

    added new completion calculation for 70%

--- src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__gradebook_audit_assignments_teacher.sql
+++ src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__gradebook_audit_assignments_teacher.sql
@@ -29,6 +29,25 @@ with
             sum(a.totalpointvalue) over (
                 partition by sec._dbt_source_relation, sec.quarter, sec.sectionid
             ) as sum_totalpointvalue_section_quarter,
+
+            sum(asg.n_expected) over (
+                partition by
+                    sec._dbt_source_relation,
+                    sec.sectionid,
+                    sec.quarter,
+                    sec.week_number_quarter,
+                    sec.assignment_category_code
+            ) as total_expected_section_quarter_week_category,
+
+            sum(asg.n_expected_scored) over (
+                partition by
+                    sec._dbt_source_relation,
+                    sec.sectionid,
+                    sec.quarter,
+                    sec.week_number_quarter,
+                    sec.assignment_category_code
+            ) as total_expected_scored_section_quarter_week_category,
+
         from
             {{ ref("int_tableau__gradebook_audit_section_week_category_scaffold") }}
             as sec
@@ -43,12 +62,42 @@ with
             on a.assignmentsectionid = asg.assignmentsectionid
             and {{ union_dataset_join_clause(left_alias="a", right_alias="asg") }}
 
+    ),
+
+    final as (
+        select
+            *,
+
+            safe_divide(
+                total_expected_scored_section_quarter_week_category,
+                total_expected_section_quarter_week_category
+            ) as percent_graded_for_quarter_week_class,
+
+        from assignments
     )
 
 select
     *,
 
     if(assignmentid is not null, 1, 0) as teacher_assign_count,
+
+    if(
+        assignment_category_code = 'W' and percent_graded_for_quarter_week_class < .7,
+        true,
+        false
+    ) as w_percent_graded_min_not_met,
+
+    if(
+        assignment_category_code = 'F' and percent_graded_for_quarter_week_class < .7,
+        true,
+        false
+    ) as f_percent_graded_min_not_met,
+
+    if(
+        assignment_category_code = 'S' and percent_graded_for_quarter_week_class < .7,
+        true,
+        false
+    ) as s_percent_graded_min_not_met,
 
     if(
         assignment_category_code = 'W'
@@ -92,4 +141,5 @@ select
         true,
         false
     ) as qt_teacher_s_total_greater_200,
-from assignments
+
+from final
