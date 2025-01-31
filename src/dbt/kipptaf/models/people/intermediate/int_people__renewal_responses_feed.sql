dc6eafbca55c226bdc5421aaca5b022bc60e7725
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2025-01-31 16:44:18 +0000

    building out the renewal feed

--- src/dbt/kipptaf/models/people/intermediate/int_people__renewal_responses_feed.sql
+++ src/dbt/kipptaf/models/people/intermediate/int_people__renewal_responses_feed.sql
@@ -8,13 +8,13 @@ with
             fr.create_timestamp as date_started,
             fr.last_submitted_timestamp as date_submitted,
 
-            up.employee_number as respondent_df_employee_number,
+            up.employee_number as respondent_employee_number,
 
             rt.academic_year as campaign_academic_year,
             rt.name as campaign_name,
             rt.code as campaign_reporting_term,
 
-            ra.approval_level
+            ra.approval_level,
 
             safe_cast(
                 regexp_extract(fr.text_value, r'\((\d{6})\)') as integer
@@ -29,16 +29,16 @@ with
             and rt.type = 'RENEW'
             and rt.code = 'RENEW'
         left join
-            {{ ref("stg_people__renewal_approvers")}} as ra
+            {{ ref("stg_people__renewal_approvers") }} as ra
             on fr.respondent_email = ra.approver_email
-           and rt.academic_year = ra.academic_year
+            and rt.academic_year = ra.academic_year
         where
             fr.form_id = '1QfYO4OCK3vLxAL_Xx9jIoDVnb29l-lRb1tP8PmKU2iU'
             and fr.question_id = '4d8bc5d5'
     ),
 
-submissions_counter as (
-            select
+    submissions_counter as (
+        select
             survey_id,
             survey_title,
             survey_response_id,
@@ -46,32 +46,130 @@ submissions_counter as (
             campaign_academic_year,
             campaign_name,
             campaign_reporting_term,
-            respondent_df_employee_number,
-            subject_df_employee_number,
-            approval_level
+            respondent_employee_number,
+            subject_employee_number,
+            approval_level,
             date_started,
             date_submitted,
 
             row_number() over (
                 partition by
                     survey_id,
-                    survey_response_id,
                     campaign_academic_year,
                     campaign_reporting_term,
-                    respondent_df_employee_number,
-                    subject_df_employee_number,
+                    respondent_employee_number,
+                    subject_employee_number,
                     approval_level
             ) as rn_level_approval,
             row_number() over (
                 partition by
                     survey_id,
-                    survey_response_id,
                     campaign_academic_year,
                     campaign_reporting_term,
-                    respondent_df_employee_number,
-                    subject_df_employee_number
+                    respondent_employee_number,
+                    subject_employee_number
             ) as rn_approval,
         from response_identifiers
-)
+    ),
 
-select * from submissions_counter
+    pivoted_responses as (
+        select
+            response_id as survey_response_id,
+            max(
+                case when item_abbreviation = 'dept_and_job' then text_value end
+            ) as dept_and_job,
+            max(
+                case when item_abbreviation = 'final_confirmation' then text_value end
+            ) as final_confirmation,
+            max(
+                case when item_abbreviation = 'next_year_seat' then text_value end
+            ) as next_year_seat,
+            max(
+                case when item_abbreviation = 'renewal_decision' then text_value end
+            ) as renewal_decision,
+            max(case when item_abbreviation = 'salary' then text_value end) as salary,
+            max(
+                case when item_abbreviation = 'salary_confirmation' then text_value end
+            ) as salary_confirmation,
+            max(
+                case
+                    when item_abbreviation = 'salary_modification_explanation'
+                    then text_value
+                end
+            ) as salary_modification_explanation,
+            max(
+                case when item_abbreviation = 'salary_rule' then text_value end
+            ) as salary_rule,
+            max(
+                case when item_abbreviation = 'add_comp_amt_1' then text_value end
+            ) as add_comp_amt_1,
+            max(
+                case when item_abbreviation = 'add_comp_amt_2' then text_value end
+            ) as add_comp_amt_2,
+            max(
+                case when item_abbreviation = 'add_comp_amt_3' then text_value end
+            ) as add_comp_amt_3,
+            max(
+                case when item_abbreviation = 'add_comp_amt_4' then text_value end
+            ) as add_comp_amt_4,
+            max(
+                case when item_abbreviation = 'add_comp_amt_5' then text_value end
+            ) as add_comp_amt_5,
+            max(
+                case when item_abbreviation = 'add_comp_name_1' then text_value end
+            ) as add_comp_name_1,
+            max(
+                case when item_abbreviation = 'add_comp_name_2' then text_value end
+            ) as add_comp_name_2,
+            max(
+                case when item_abbreviation = 'add_comp_name_3' then text_value end
+            ) as add_comp_name_3,
+            max(
+                case when item_abbreviation = 'add_comp_name_4' then text_value end
+            ) as add_comp_name_4,
+            max(
+                case when item_abbreviation = 'add_comp_name_5' then text_value end
+            ) as add_comp_name_5,
+
+        from {{ ref("base_google_forms__form_responses") }}
+        where
+            form_id = '1QfYO4OCK3vLxAL_Xx9jIoDVnb29l-lRb1tP8PmKU2iU'
+            and question_id != '4d8bc5d5'
+        group by response_id
+    )
+
+select
+    sc.survey_id,
+    sc.survey_title,
+    sc.survey_response_id,
+    sc.respondent_email,
+    sc.campaign_academic_year,
+    sc.campaign_name,
+    sc.campaign_reporting_term,
+    sc.respondent_employee_number,
+    sc.subject_employee_number,
+    sc.approval_level,
+    sc.date_started,
+    sc.date_submitted,
+
+    pr.dept_and_job,
+    pr.final_confirmation,
+    pr.next_year_seat,
+    pr.renewal_decision,
+    pr.salary,
+    pr.salary_confirmation,
+    pr.salary_modification_explanation,
+    pr.salary_rule,
+    pr.add_comp_amt_1,
+    pr.add_comp_amt_2,
+    pr.add_comp_amt_3,
+    pr.add_comp_amt_4,
+    pr.add_comp_amt_5,
+    pr.add_comp_name_1,
+    pr.add_comp_name_2,
+    pr.add_comp_name_3,
+    pr.add_comp_name_4,
+    pr.add_comp_name_5,
+
+from submissions_counter as sc
+inner join pivoted_responses as pr on sc.survey_response_id = pr.survey_response_id
