d275a0a2988a04644651e1859f9d7b9ba1942299
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-02-03 17:40:31 +0000

    style

--- src/teamster/libraries/dlt/zendesk/pipeline/helpers/api_helpers.py
+++ src/teamster/libraries/dlt/zendesk/pipeline/helpers/api_helpers.py
@@ -13,6 +13,7 @@ class TCustomFieldInfo(TypedDict):
 def _parse_date_or_none(value: str | None) -> pendulum.DateTime | None:
     if not value:
         return None
+
     return ensure_pendulum_datetime(value)
 
 
@@ -22,27 +23,35 @@ def process_ticket(
     pivot_custom_fields: bool = True,
 ) -> DictStrAny:
     """
-    Helper function that processes a ticket object and returns a dictionary of ticket data.
+    Helper function that processes a ticket object and returns a dictionary of ticket
+    data.
 
     Args:
         ticket: The ticket dict object returned by a Zendesk API call.
-        custom_fields: A dictionary containing all the custom fields available for tickets.
-        pivot_custom_fields: A boolean indicating whether to pivot all custom fields or not.
+        custom_fields: A dictionary containing all the custom fields available for
+            tickets.
+        pivot_custom_fields: A boolean indicating whether to pivot all custom fields or
+            not.
             Defaults to True.
 
     Returns:
         DictStrAny: A dictionary containing cleaned data about a ticket.
     """
+
     # pivot custom field if indicated as such
     # get custom fields
     pivoted_fields = set()
+
     for custom_field in ticket["custom_fields"]:
         if pivot_custom_fields:
             cus_field_id = str(custom_field["id"])
+
             field = custom_fields.get(cus_field_id, None)
+
             if field is None:
                 logger.warning(
-                    "Custom field with ID %s does not exist in fields state. It may have been created after the pipeline run started.",
+                    "Custom field with ID %s does not exist in fields state. It may "
+                    "have been created after the pipeline run started.",
                     cus_field_id,
                 )
                 custom_field["ticket_id"] = ticket["id"]
@@ -52,6 +61,7 @@ def process_ticket(
             field_name = field["title"]
             current_value = custom_field["value"]
             options = field["options"]
+
             # Map dropdown values to labels
             if not current_value or not options:
                 ticket[field_name] = current_value
@@ -63,18 +73,23 @@ def process_ticket(
                 ticket[field_name] = options.get(current_value)
         else:
             custom_field["ticket_id"] = ticket["id"]
+
     # delete fields that are not needed for pivoting
     if pivot_custom_fields:
         ticket["custom_fields"] = [
             f for f in ticket["custom_fields"] if str(f["id"]) not in pivoted_fields
         ]
+
         if not ticket["custom_fields"]:
             del ticket["custom_fields"]
+
     del ticket["fields"]
+
     # modify dates to return datetime objects instead
     ticket["updated_at"] = _parse_date_or_none(ticket["updated_at"])
     ticket["created_at"] = _parse_date_or_none(ticket["created_at"])
     ticket["due_at"] = _parse_date_or_none(ticket["due_at"])
+
     return ticket
 
 
@@ -82,20 +97,27 @@ def process_ticket_field(
     field: DictStrAny, custom_fields_state: dict[str, TCustomFieldInfo]
 ) -> TDataItem:
     """Update custom field mapping in dlt state for the given field."""
+
     # grab id and update state dict
-    # if the id is new, add a new key to indicate that this is the initial value for title
-    # New dropdown options are added to existing field but existing options are not changed
+    # if the id is new, add a new key to indicate that this is the initial value for
+    # title
+    # New dropdown options are added to existing field but existing options are not
+    # changed
     return_dict = field.copy()
     field_id = str(field["id"])
 
     options = field.get("custom_field_options", [])
+
     new_options = {o["value"]: o["name"] for o in options}
     existing_field = custom_fields_state.get(field_id)
+
     if existing_field:
         existing_options = existing_field["options"]
+
         if return_options := return_dict.get("custom_field_options"):
             for item in return_options:
                 item["name"] = existing_options.get(item["value"], item["name"])
+
         for key, value in new_options.items():
             if key not in existing_options:
                 existing_options[key] = value
@@ -105,4 +127,5 @@ def process_ticket_field(
             "options": new_options,
         }
         return_dict["initial_title"] = field["title"]
+
     return return_dict
