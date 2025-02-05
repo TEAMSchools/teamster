af24bb12538a06815b786e3aaf36b24e0a046932
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2025-02-05 21:08:14 +0000

    updating and killing

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_renewal_feed.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_renewal_feed.sql
@@ -5,10 +5,6 @@ select
     b.preferred_name_given_name as preferred_first,
     b.preferred_name_family_name as preferred_last,
     b.preferred_name_lastfirst as preferred_name,
-    b.business_unit_home_name as legal_entity_name,
-    b.home_work_location_name as location_description,
-    b.department_home_name as home_department_description,
-    b.job_title as job_title_description,
     b.assignment_status as position_status,
     b.worker_original_hire_date as original_hire_date,
     b.worker_rehire_date as rehire_date,
@@ -24,27 +20,31 @@ select
     b.report_to_mail as manager_mail,
     b.race_ethnicity_reporting,
     b.gender_identity,
-    b.base_remuneration_annual_rate_amount_amount_value as base_salary,
 
-    s.salary_rule,
+    s.ay_business_unit,
+    s.ay_job_title,
+    s.ay_location,
+    s.ay_salary,
+    s.academic_year,
+    s.ay_pm4_overall_score,
+    s.ay_pm4_overall_tier,
+    s.ay_primary_grade_level_taught,
     s.scale_cy_salary,
     s.scale_ny_salary,
     s.scale_step,
-
-    p.final_score as pm4_overall_score,
-
-    tgl.grade_level as primary_grade_level_taught,
+    s.pm_salary_increase,
+    s.seat_tracker_id_number,
+    s.ny_location,
+    s.ny_dept,
+    s.ny_title,
+    s.nonrenewal_reason,
+    s.nonrenewal_notes,
+    s.ny_salary,
+    s.salary_rule,
 from {{ ref("base_people__staff_roster") }} as b
 left join
-    {{ ref("int_people__expected_next_year_salary") }} as s
+    {{ ref("int_people__renewal_status") }} as s
     on b.employee_number = s.employee_number
-left join
-    {{ ref("int_performance_management__overall_scores") }} as p
-    on b.employee_number = p.employee_number
-    and p.academic_year = {{ var("current_academic_year") }}
-left join
-    {{ ref("int_powerschool__teacher_grade_levels") }} as tgl
-    on b.powerschool_teacher_number = tgl.teachernumber
-    and tgl.academic_year = {{ var("current_academic_year") }}
-    and tgl.grade_level_rank = 1
-where b.assignment_status not in ('Terminated', 'Deceased')
+    and s.academic_year = {{ var("current_academic_year") }}
+
+
