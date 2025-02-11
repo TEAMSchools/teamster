d49f6522e0366297ac73d70ac7e8349067c65309
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-02-11 17:00:29 +0000

    fix: schema

--- src/teamster/libraries/smartrecruiters/schema.py
+++ src/teamster/libraries/smartrecruiters/schema.py
@@ -41,6 +41,7 @@ class Application(BaseModel):
     job_city: str | None = None
     job_title: str | None = None
     recruiters: str | None = None
+    screening_question_answer_knjm_application_subject_preference: str | None = None
     source_subtype: str | None = None
     source_type: str | None = None
     source: str | None = None
