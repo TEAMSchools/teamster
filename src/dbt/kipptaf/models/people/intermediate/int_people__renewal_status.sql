a7e5387927946bd27e5ced3da8bf4a9bb614afe3
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2025-02-11 21:50:27 +0000

    moving salaray projections to int

--- src/dbt/kipptaf/models/people/intermediate/int_people__renewal_status.sql
+++ src/dbt/kipptaf/models/people/intermediate/int_people__renewal_status.sql
@@ -13,44 +13,26 @@ with
 
 select
     h.employee_number,
-    h.business_unit_assigned_name as ay_business_unit,
+    h.home_business_unit_name as ay_business_unit,
     h.job_title as ay_job_title,
     h.home_work_location_name as ay_location,
-    h.base_remuneration_annual_rate_amount_amount_value as ay_salary,
+    h.base_remuneration_annual_rate_amount as ay_salary,
     y.academic_year,
     p.final_score as ay_pm4_overall_score,
     p.final_tier as ay_pm4_overall_tier,
     tgl.grade_level as ay_primary_grade_level_taught,
-    pss.scale_cy_salary,
-    pss.scale_ny_salary,
-    pss.scale_step,
-    tss.scale_ny_salary as pm_salary_increase,
+    sp.scale_cy_salary,
+    sp.scale_ny_salary,
+    sp.scale_step,
+    sp.scale_ny_salary as pm_salary_increase,
+    sp.ny_salary,
+    sp.salary_rule,
     s.staffing_model_id as seat_tracker_id_number,
     s.adp_location as ny_location,
     s.adp_dept as ny_dept,
     s.adp_title as ny_title,
     stp.nonrenewal_reason,
     stp.nonrenewal_notes,
-    coalesce(
-        pss.scale_ny_salary,
-        tss.scale_ny_salary + h.base_remuneration_annual_rate_amount_amount_value,
-        case
-            when h.base_remuneration_annual_rate_amount_amount_value < 60000
-            then
-                h.base_remuneration_annual_rate_amount_amount_value
-                + h.base_remuneration_annual_rate_amount_amount_value * 0.5
-            when h.base_remuneration_annual_rate_amount_amount_value < 100000
-            then
-                h.base_remuneration_annual_rate_amount_amount_value
-                + h.base_remuneration_annual_rate_amount_amount_value * 0.4
-            when h.base_remuneration_annual_rate_amount_amount_value >= 100000
-            then
-                h.base_remuneration_annual_rate_amount_amount_value
-                + h.base_remuneration_annual_rate_amount_amount_value * 0.3
-        end
-    ) as ny_salary,
-
-    coalesce(pss.salary_rule, tss.salary_rule, 'Annual Adjustment') as salary_rule,
 
 from {{ ref("int_people__staff_roster") }} as c
 inner join
@@ -61,8 +43,7 @@ inner join
 left join
     {{ ref("int_people__staff_roster_history") }} as h
     on c.employee_number = h.employee_number
-    and y.effective_date
-    between h.work_assignment_start_date and h.work_assignment_end_date
+    and y.effective_date between h.effective_date_start and h.effective_date_end
     and h.primary_indicator
     and h.job_title is not null
 left join
@@ -75,26 +56,9 @@ left join
     and tgl.academic_year = y.academic_year
     and tgl.grade_level_rank = 1
 left join
-    {{ ref("stg_people__salary_scale") }} as pss
-    on h.job_title = pss.job_title
-    and y.academic_year = pss.academic_year
-    and h.business_unit_assigned_name = pss.region
-    and h.home_work_location_grade_band
-    = coalesce(pss.school_level, h.home_work_location_grade_band)
-    and (
-        (h.base_remuneration_annual_rate_amount_amount_value + 150)
-        between pss.scale_cy_salary and (pss.scale_ny_salary + 0.01)
-        or (
-            h.base_remuneration_annual_rate_amount_amount_value
-            between (pss.scale_cy_salary - 1) and (pss.scale_cy_salary + 1)
-        )
-    )
-left join
-    {{ ref("stg_people__salary_scale") }} as tss
-    on h.job_title = tss.job_title
-    and y.academic_year = tss.academic_year
-    and h.business_unit_assigned_name = tss.region
-    and p.final_tier = tss.scale_step
+    {{ ref("int_people__next_year_salary_projections") }} as sp
+    on c.employee_number = sp.employee_number
+    and c.academic_year = sp.academic_year
 left join
     {{ ref("stg_people__seats") }} as s
     on c.employee_number = s.teammate
