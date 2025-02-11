9643689315e22ae1d2fd270c1d3a90d5d8a8d011
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2025-02-11 22:11:18 +0000

    updating int things

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_renewal_feed.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_renewal_feed.sql
@@ -1,25 +1,25 @@
 select
     b.employee_number as df_employee_number,
-    b.legal_name_given_name as first_name,
-    b.legal_name_family_name as last_name,
-    b.preferred_name_given_name as preferred_first,
-    b.preferred_name_family_name as preferred_last,
-    b.preferred_name_lastfirst as preferred_name,
+    b.legal_name__given_name as first_name,
+    b.legal_name__family_name_1 as last_name,
+    b.given_name as preferred_first,
+    b.family_name_1 as preferred_last,
     b.assignment_status as position_status,
     b.worker_original_hire_date as original_hire_date,
     b.worker_rehire_date as rehire_date,
     b.worker_termination_date as termination_date,
-    b.worker_type as worker_category_description,
-    b.worker_group_value as benefits_eligibility_class_description,
-    b.wage_law_coverage_short_name as flsa_description,
-    b.ethnicity_long_name as eeo_ethnic_description,
+    b.worker_type_code as worker_category_description,
+    b.wage_law_coverage as benefits_eligibility_class_description,
+    b.wage_law_name as flsa_description,
+    b.race_ethnicity as eeo_ethnic_description,
     b.mail,
     b.user_principal_name as userprincipalname,
-    b.report_to_employee_number as manager_df_employee_number,
-    b.report_to_preferred_name_lastfirst as manager_name,
-    b.report_to_mail as manager_mail,
+
     b.race_ethnicity_reporting,
     b.gender_identity,
+
+    m.employee_number as manager_df_employee_number,
+    m.mail as manager_mail,
 
     s.ay_business_unit,
     s.ay_job_title,
@@ -41,7 +41,12 @@ select
     s.nonrenewal_notes,
     s.ny_salary,
     s.salary_rule,
+
+    concat(b.family_name_1,', ',b.given_name) as preferred_name,
+    concat(m.family_name_1,', ',m.given_name) as manager_name,
 from {{ ref("int_people__staff_roster") }} as b
+left join {{ ref("int_people__staff_roster")}} as m
+    on b.reports_to_employee_number = m.employee_number
 left join
     {{ ref("int_people__renewal_status") }} as s
     on b.employee_number = s.employee_number
