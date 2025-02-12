ce94ac20822d1ed438e7d53d6a59cdebe32b6168
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-02-12 15:43:01 +0000

    refactor: calcs to staging

--- src/dbt/kipptaf/models/people/intermediate/int_people__next_year_salary_projections.sql
+++ src/dbt/kipptaf/models/people/intermediate/int_people__next_year_salary_projections.sql
@@ -12,15 +12,23 @@ with
     )
 
 select
-    h.employee_number,
+    c.employee_number,
+
+    y.academic_year,
+
     h.home_business_unit_name,
     h.base_remuneration_annual_rate_amount as ay_salary,
-    y.academic_year,
+
     p.final_tier as ay_pm4_overall_tier,
+
     pss.scale_cy_salary,
     pss.scale_ny_salary,
     pss.scale_step,
+
     tss.scale_ny_salary as pm_salary_increase,
+
+    coalesce(pss.salary_rule, tss.salary_rule, 'Annual Adjustment') as salary_rule,
+
     coalesce(
         pss.scale_ny_salary,
         tss.scale_ny_salary + h.base_remuneration_annual_rate_amount,
@@ -39,9 +47,6 @@ select
                 + h.base_remuneration_annual_rate_amount * 0.3
         end
     ) as ny_salary,
-
-    coalesce(pss.salary_rule, tss.salary_rule, 'Annual Adjustment') as salary_rule,
-
 from {{ ref("int_people__staff_roster") }} as c
 inner join
     years as y
@@ -57,30 +62,28 @@ left join
 left join
     {{ ref("int_performance_management__overall_scores") }} as p
     on c.employee_number = p.employee_number
-    and p.academic_year = y.academic_year
+    and y.academic_year = p.academic_year
 left join
     {{ ref("int_powerschool__teacher_grade_levels") }} as tgl
     on c.powerschool_teacher_number = tgl.teachernumber
-    and tgl.academic_year = y.academic_year
+    and y.academic_year = tgl.academic_year
     and tgl.grade_level_rank = 1
 left join
     {{ ref("stg_people__salary_scale") }} as pss
-    on h.job_title = pss.job_title
-    and y.academic_year = pss.academic_year
+    on y.academic_year = pss.academic_year
     and h.home_business_unit_name = pss.region
+    and h.job_title = pss.job_title
     and h.home_work_location_grade_band
     = coalesce(pss.school_level, h.home_work_location_grade_band)
     and (
-        (h.base_remuneration_annual_rate_amount + 150)
-        between pss.scale_cy_salary and (pss.scale_ny_salary + 0.01)
-        or (
-            h.base_remuneration_annual_rate_amount
-            between (pss.scale_cy_salary - 1) and (pss.scale_cy_salary + 1)
-        )
+        h.base_remuneration_annual_rate_amount + 150
+        between pss.scale_cy_salary and pss.scale_ny_salary_plus_1_cent
+        or h.base_remuneration_annual_rate_amount
+        between pss.scale_ny_salary_minus_1_dollar and pss.scale_ny_salary_plus_1_dollar
     )
 left join
     {{ ref("stg_people__salary_scale") }} as tss
-    on h.job_title = tss.job_title
-    and y.academic_year = tss.academic_year
+    on y.academic_year = tss.academic_year
+    and h.job_title = tss.job_title
     and h.home_business_unit_name = tss.region
     and p.final_tier = tss.scale_step
