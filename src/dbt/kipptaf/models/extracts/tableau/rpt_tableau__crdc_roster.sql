b4bd0bdad9268fc1a13afdcb5b1300f718ee7c71
Gaby Rangel <grangel@kippteamandfamily.org>
2025-02-13 20:52:24 +0000

    custom schedule with all the tags

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__crdc_roster.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__crdc_roster.sql
@@ -1,142 +1,55 @@
 with
     custom_schedule as (
         select
-            _dbt_source_relation,
-            cc_academic_year,
-            cc_schoolid,
-            cc_studentid,
-            students_student_number,
-            courses_course_number,
-            courses_course_name,
-            cc_dateenrolled,
-            cc_dateleft,
-            ap_course_subject,
-            is_ap_course,
-            nces_subject_area,
-            nces_course_id,
+            c._dbt_source_relation,
+            c.cc_academic_year,
+            c.cc_schoolid,
+            c.cc_studentid,
+            c.students_student_number,
+            c.courses_course_number,
+            c.courses_course_name,
+            c.sections_external_expression,
+            c.cc_dateenrolled,
+            c.cc_dateleft,
+            c.is_dropped_course,
+            c.is_dropped_section,
 
-            rn_credittype_year,
-            rn_course_number_year,
+            c.ap_course_subject,
+            c.is_ap_course,
+            c.courses_isfitnesscourse,
 
-            concat(nces_subject_area, nces_subject_area) as sced_code,
+            c.rn_credittype_year,
+            c.rn_course_number_year,
+
+            c.terms_lastday,
+
+            x.sced_course_name,
+            x.crdc_course_group,
+            x.crdc_subject_group,
+            x.crdc_ap_group,
+            x.ap_tag,
+            x.sced_code as sced_code_xwalk,
+
+            concat(c.nces_subject_area, c.nces_course_id) as sced_code_courses,
+
+            if(c.courses_course_name like '%(DE)%', true, false) as is_dual_enrollment,
 
             if(
-                cc_dateenrolled <= '2023-10-02' and cc_dateleft >= '2023-10-02',
+                c.cc_dateenrolled <= '2023-10-02' and c.cc_dateleft >= '2023-10-02',
                 true,
                 false
-            ) as oct_01_course,
+            ) as is_oct_01_course,
 
-        from {{ ref("base_powerschool__course_enrollments") }}
-        where cc_academic_year = 2023
+            if(c.cc_dateleft >= c.terms_lastday, true, false) as is_last_day_course,
 
-    ),
-
-    discipline_rollup as (
-        select
-            dli.student_school_id as student_number,
-            dli.create_ts_academic_year as academic_year,
-
-            case
-                when
-                    count(
-                        distinct case
-                            when cf.restraint_used = 'Y' then dli.incident_id else null
-                        end
-                    )
-                    > 0
-                then 1
-                else 0
-            end as is_restraint_int,
-
-            count(
-                distinct case
-                    when cf.restraint_used = 'Y' then dli.incident_id else null
-                end
-            ) as n_restraint_incidents,
-
-            case
-                when
-                    count(
-                        distinct case
-                            when dlp.penalty_name like '%Out-of-School Suspension'
-                            then dlp.incident_penalty_id
-                        end
-                    )
-                    = 1
-                then 1
-                else 0
-            end as is_oss_one_int,
-
-            case
-                when
-                    count(
-                        distinct case
-                            when dlp.penalty_name like '%Out-of-School Suspension'
-                            then dlp.incident_penalty_id
-                        end
-                    )
-                    > 1
-                then 1
-                else 0
-            end as is_oss_two_plus_int,
-
-            max(
-                case
-                    when dlp.penalty_name like '%In-School Suspension' then 1 else 0
-                end
-            ) as is_iss_int,
-
-            sum(
-                case
-                    when dlp.penalty_name like '%Out-of-School Suspension'
-                    then dlp.num_days
-                    else null
-                end
-            ) as oss_days_missed,
-
-            count(
-                distinct case
-                    when dlp.penalty_name like '%Out-of-School Suspension'
-                    then dlp.incident_penalty_id
-                end
-            ) as oss_incident_count,
-
-        from {{ ref("stg_deanslist__incidents") }} as dli
+        from {{ ref("base_powerschool__course_enrollments") }} as c
         left join
-            {{ ref("stg_deanslist__incidents__penalties") }} as dlp
-            on dli.incident_id = dlp.incident_id
-            and {{ union_dataset_join_clause(left_alias="dli", right_alias="dlp") }}
-        left join
-            {{ ref("int_deanslist__incidents__custom_fields__pivot") }} as cf
-            on dli.incident_id = cf.incident_id
-            and {{ union_dataset_join_clause(left_alias="dli", right_alias="cf") }}
-        where dli.is_active
-        group by dli.student_school_id, dli.create_ts_academic_year
-    ),
+            {{ ref("stg_crdc__sced_code_crosswalk") }} as x
+            on concat(c.nces_subject_area, c.nces_course_id) = x.sced_code
+        -- submission is always for the previous school year
+        where c.cc_academic_year = {{ var("current_academic_year") - 1 }}
 
-    algebra_pass as (
-        select
-            sg.studentid,
-            sg.academic_year,
-            sg._dbt_source_relation,
-
-            max(if sg.grade like 'F%', false, true) is_algebra1_pass,
-
-        from {{ ref("stg_powerschool__storedgrades") }} as sg
-        left join
-            {{ ref("stg_powerschool__courses") }} as c
-            on sg.course_number = c.course_number
-            and {{ union_dataset_join_clause(left_alias="sg", right_alias="c") }}
-        left join
-            {{ ref("stg_powerschool__s_nj_crs_x") }} as nj
-            on c.dcid = nj.coursesdcid
-            and {{ union_dataset_join_clause(left_alias="c", right_alias="nj") }}
-        where
-            nj.nces_subject_area in ('02', '52')
-            and nj.nces_course_id = '052'
-            and sg.storecode = 'Y1'
-        group by sg.studentid, sg.academic_year, sg._dbt_source_relation
     )
 
 select *
-from algebra_pass
+from custom_schedule
