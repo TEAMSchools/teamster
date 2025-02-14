06436c4af647f0f9d0153449d2af56a4df7aa431
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-02-14 16:16:20 +0000

    refactor: use current roster

--- src/dbt/kipptaf/models/extracts/rpt_adp_workforce_now__worker_update.sql
+++ src/dbt/kipptaf/models/extracts/rpt_adp_workforce_now__worker_update.sql
@@ -7,11 +7,9 @@ select
     wf_mgr_badge_number as adp__wf_mgr_badge_number,
     work_email as adp__work_email,
     wf_mgr_trigger as adp__wf_mgr_trigger,
-from {{ ref("int_people__staff_roster_history") }}
+from {{ ref("int_people__staff_roster") }}
 where
-    (is_current_record or is_prestart)
-    and primary_indicator
-    and mail is not null
+    mail is not null
     and (
         wf_mgr_trigger_new != coalesce(wf_mgr_trigger, '')
         or employee_number != coalesce(custom_field__employee_number, -1)
