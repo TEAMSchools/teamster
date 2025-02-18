382323f28933e13b827f0e368d427d04672e54fc
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-02-18 21:42:42 +0000

    feat: coupa sched

---
+++ src/teamster/code_locations/kipptaf/coupa/schedules.py
@@ -0,0 +1,15 @@
+from dagster import ScheduleDefinition
+
+from teamster.code_locations.kipptaf import CODE_LOCATION, LOCAL_TIMEZONE
+from teamster.code_locations.kipptaf.coupa import assets
+
+coupa_asset_job_schedule = ScheduleDefinition(
+    name=f"{CODE_LOCATION}__coupa__asset_job_schedule",
+    target=assets,
+    cron_schedule="0 2 * * *",
+    execution_timezone=str(LOCAL_TIMEZONE),
+)
+
+schedules = [
+    coupa_asset_job_schedule,
+]
