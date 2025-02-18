37600faa566e857a8a2cb53785380e9fdde8dd68
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2025-02-18 15:58:47 +0000

    approval checks + listing approvers on tableau view

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_renewal_feed.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__staff_renewal_feed.sql
@@ -1,3 +1,42 @@
+with
+    approvals as (
+        select
+            subject_employee_number,
+            campaign_academic_year,
+            max(
+                if(
+                    approval_level = 'School Leader/DSO',
+                    coalesce(respondent_preferred_name, respondent_email),
+                    null
+                )
+            ) as sl_dso_approval_name,
+            max(
+                if(
+                    approval_level = 'ER',
+                    coalesce(respondent_preferred_name, respondent_email),
+                    null
+                )
+            ) as er_approval_name,
+            max(
+                if(
+                    approval_level = 'HOS/MDO',
+                    coalesce(respondent_preferred_name, respondent_email),
+                    null
+                )
+            ) as hos_mdo_approval_name,
+            max(
+                if(approval_level = 'School Leader/DSO', date_submitted, null)
+            ) as sl_dso_approval_date,
+            max(if(approval_level = 'ER', date_submitted, null)) as er_approval_date,
+            max(
+                if(approval_level = 'HOS/MDO', date_submitted, null)
+            ) as hos_mdo_approval_date,
+        from `teamster-332318.kipptaf_surveys.int_surveys__renewal_responses_feed`
+        where rn_level_approval = 1 and valid_approval = 'Valid Approval'
+
+        group by subject_employee_number, campaign_academic_year, rn_level_approval
+    )
+
 select
     b.employee_number as df_employee_number,
     b.legal_given_name as first_name,
@@ -42,6 +81,13 @@ select
     s.ny_salary,
     s.salary_rule,
 
+    ap.sl_dso_approval_name,
+    ap.er_approval_name,
+    ap.hos_mdo_approval_name,
+    ap.sl_dso_approval_date,
+    ap.er_approval_date,
+    ap.hos_mdo_approval_date,
+
     concat(b.family_name_1, ', ', b.given_name) as preferred_name,
 
     concat(m.family_name_1, ', ', m.given_name) as manager_name,
@@ -53,3 +99,12 @@ left join
     {{ ref("int_people__renewal_status") }} as s
     on b.employee_number = s.employee_number
     and s.academic_year = {{ var("current_academic_year") }}
+left join
+    {{ ref("int_surveys__renewal_responses_feed") }} as rf
+    on s.academic_year = rf.campaign_academic_year
+    and b.employee_number = rf.subject_employee_number
+    and rf.rn_approval = 1
+left join
+    approvals as ap
+    on s.academic_year = ap.campaign_academic_year
+    and b.employee_number = ap.subject_employee_number
