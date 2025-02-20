3d750c51eafc0858076cac1bf93e29992f326acf
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-02-20 21:54:24 +0000

    fix: asset selections

--- src/teamster/code_locations/kipptaf/_dlt/zendesk/schedules.py
+++ src/teamster/code_locations/kipptaf/_dlt/zendesk/schedules.py
@@ -1,11 +1,18 @@
 from dagster import ScheduleDefinition
 
 from teamster.code_locations.kipptaf import CODE_LOCATION, LOCAL_TIMEZONE
-from teamster.code_locations.kipptaf._dlt.zendesk.assets import assets
+
+key_prefix = f"{CODE_LOCATION}/dlt/zendesk/support"
 
 zendesk_dlt_asset_job_schedule = ScheduleDefinition(
     name=f"{CODE_LOCATION}__dlt__zendesk__asset_job",
-    target=assets,
+    target=[
+        f"{key_prefix}/groups",
+        f"{key_prefix}/ticket_events",
+        f"{key_prefix}/ticket_metrics",
+        f"{key_prefix}/tickets",
+        f"{key_prefix}/users",
+    ],
     cron_schedule="0 5 * * *",
     execution_timezone=str(LOCAL_TIMEZONE),
 )
