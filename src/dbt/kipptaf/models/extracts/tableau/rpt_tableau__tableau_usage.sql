56d4b0b598a8033fd1b7506bf0a4cd66c0623b33
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-02-20 17:56:20 +0000

    converting timestamp in staging, creating rpt view

---
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__tableau_usage.sql
@@ -0,0 +1,25 @@
+select
+    srh.employee_number,
+    srh.formatted_name,
+    srh.job_title,
+    srh.assignment_status,
+    srh.home_work_location_name as location,
+    srh.home_work_location_abbreviation as location_shortname,
+    srh.home_work_location_grade_band as grade_band,
+    srh.home_business_unit_name as entity,
+    srh.home_department_name as department,
+    srh.reports_to_formatted_name as manager,
+
+    vc.name_hist_projects as project,
+    vc.name_hist_workbooks as workbook,
+    vc.name_hist_views as `view`,
+    vc.`action`,
+    vc.created_at_local_new_york as viewed_at,
+    vc.id as view_count_id,
+    concat("https://tableau.kipp.org/t/KIPPNJ/views/", view_url, "?:embed=y") as url,
+from {{ ref("stg_tableau__view_count_per_view") }} as vc
+left join
+    {{ ref("int_people__staff_roster_history") }} as srh
+    on lower(vc.`user_name`) = lower(srh.sam_account_name)
+    and vc.created_at_local_new_york
+    between srh.effective_date_start_timestamp and srh.effective_date_end_timestamp
