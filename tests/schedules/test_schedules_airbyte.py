b298333f6a66206de39b4cbe827aed5c36d08e31
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-02-20 18:22:36 +0000

    refactor: airbyte assets

--- tests/schedules/test_schedules_airbyte.py
+++ tests/schedules/test_schedules_airbyte.py
@@ -2,7 +2,6 @@ from dagster import SkipReason, build_schedule_context
 
 from teamster.code_locations.kipptaf.airbyte.schedules import (
     kippadb_start_sync_schedule,
-    zendesk_start_sync_schedule,
 )
 from teamster.code_locations.kipptaf.resources import AIRBYTE_CLOUD_RESOURCE
 
@@ -11,18 +10,6 @@ def test_kippadb_start_sync_schedule():
     context = build_schedule_context()
 
     output = kippadb_start_sync_schedule(
-        context=context, airbyte=AIRBYTE_CLOUD_RESOURCE
-    )
-
-    assert isinstance(output, SkipReason)
-
-    context.log.info(output)
-
-
-def test_zendesk_start_sync_schedule():
-    context = build_schedule_context()
-
-    output = zendesk_start_sync_schedule(
         context=context, airbyte=AIRBYTE_CLOUD_RESOURCE
     )
 
