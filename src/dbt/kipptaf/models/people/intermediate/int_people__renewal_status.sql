57834e62bade4ac9d532e69546e9303d9f21b4b4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-02-20 16:43:21 +0000

    refactor: int location xwalk

--- src/dbt/kipptaf/models/people/intermediate/int_people__renewal_status.sql
+++ src/dbt/kipptaf/models/people/intermediate/int_people__renewal_status.sql
@@ -25,6 +25,9 @@ select
     h.job_title as ay_job_title,
     h.home_work_location_name as ay_location,
     h.base_remuneration_annual_rate_amount as ay_salary,
+    h.home_work_location_abbreviation as ay_school_shortname,
+    h.home_work_location_campus_name as ay_campus_name,
+    h.head_of_schools_sam_account_name as ay_head_of_school_samaccount,
 
     p.final_score as ay_pm4_overall_score,
     p.final_tier as ay_pm4_overall_tier,
@@ -46,22 +49,13 @@ select
     s.edited_at as seat_tracker_last_edited,
     s.status_detail as ny_status,
 
+    nyl.location_abbreviation as ny_school_shortname,
+    nyl.campus_name as ny_campus_name,
+    nyl.head_of_schools_sam_account_name as ny_head_of_school_samaccount,
+
     stp.nonrenewal_reason,
     stp.nonrenewal_notes,
     stp.gutcheck,
-
-    ayl.abbreviation as ay_school_shortname,
-
-    nyl.abbreviation as ny_school_shortname,
-
-    ayc.name as ay_campus_name,
-
-    nyc.name as ny_campus_name,
-
-    ayhos.sam_account_name as ay_head_of_school_samaccount,
-
-    nyhos.sam_account_name as ny_head_of_school_samaccount,
-
 from {{ ref("int_people__staff_roster") }} as c
 inner join
     years as y
@@ -92,24 +86,9 @@ left join
     on c.employee_number = s.teammate
     and y.fiscal_year = s.academic_year
 left join
+    {{ ref("int_people__location_crosswalk") }} as nyl
+    on s.adp_location = nyl.location_name
+left join
     {{ ref("stg_people__seat_tracker_people") }} as stp
     on c.employee_number = stp.employee_number
     and y.academic_year = stp.academic_year
-
-left join
-    {{ ref("stg_people__location_crosswalk") }} as ayl
-    on h.home_work_location_name = ayl.name
-left join
-    {{ ref("stg_people__location_crosswalk") }} as nyl on s.adp_location = nyl.name
-left join
-    {{ ref("stg_people__campus_crosswalk") }} as ayc
-    on ayl.clean_name = ayc.location_name
-left join
-    {{ ref("stg_people__campus_crosswalk") }} as nyc
-    on nyl.clean_name = nyc.location_name
-left join
-    {{ ref("int_people__staff_roster") }} as ayhos
-    on ayl.head_of_schools_employee_number = ayhos.employee_number
-left join
-    {{ ref("int_people__staff_roster") }} as nyhos
-    on nyl.head_of_schools_employee_number = nyhos.employee_number
