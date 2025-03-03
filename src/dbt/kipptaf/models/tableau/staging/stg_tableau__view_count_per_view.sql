8fd62c7813ef2a4159e630e62f8fd571c8c0cacc
Cristina Baldor <ccbaldor@icloud.com>
2025-03-03 16:36:50 +0000

    requested changes

--- src/dbt/kipptaf/models/tableau/staging/stg_tableau__view_count_per_view.sql
+++ src/dbt/kipptaf/models/tableau/staging/stg_tableau__view_count_per_view.sql
@@ -2,12 +2,12 @@ select
     *,
     lower(`user_name`) as user_name_lower,
     concat(
-        'https://tableau.kipp.org/t/KIPPNJ/views/', vc.view_url, '?:embed=y'
+        'https://tableau.kipp.org/t/KIPPNJ/views/', view_url, '?:embed=y'
     ) as `url`,
     parse_timestamp('%m/%d/%Y %I:%M:%S %p', created_at) as created_at_timestamp,
     datetime(
         parse_timestamp('%m/%d/%Y %I:%M:%S %p', created_at),
         '{{ var("local_timezone") }}'
-    ) as created_at_local,
+    ) as created_at_local_ny,
 
 from {{ source("tableau", "src_tableau__view_count_per_view") }}
