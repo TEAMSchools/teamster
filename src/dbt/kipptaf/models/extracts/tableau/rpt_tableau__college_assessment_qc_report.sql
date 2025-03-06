9a25ca22b5868e74f6d944940f91b61b03f74c07
Gaby Rangel <grangel@kippteamandfamily.org>
2025-03-06 17:46:11 +0000

    ....

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_qc_report.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_qc_report.sql
@@ -1,9 +1,8 @@
-with
-    psat as (
-        select cb_id, secondary_id, test_name, powerschool_student_number, birth_date,
-        from {{ ref("stg_collegeboard__psat_unpivot") }}
-    )
-
+-- with
+-- psat as (
+-- select cb_id, secondary_id, test_name, powerschool_student_number, birth_date,
+-- from {{ ref("int_collegeboard__psat_unpivot") }}
+-- )
 select student_number, first_name, middle_name, last_name, dob, gender,
 from {{ ref("base_powerschool__student_enrollments") }}
 where rn_undergrad = 1
