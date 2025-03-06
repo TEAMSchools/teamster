56b21868204e6a3e700f1ec36ea744258acc26c4
Gaby Rangel <grangel@kippteamandfamily.org>
2025-03-06 19:59:27 +0000

    fields to be able to identify errors

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_qc_report.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_qc_report.sql
@@ -3,25 +3,45 @@ with
         select
             cb_id,
             secondary_id,
-            test_type,
-            powerschool_student_number,
-            birth_date,
-            gender,
+            powerschool_student_number as xwalk_student_number,
+            initcap(name_first) as cb_first_name,
+            initcap(name_mi) as cb_middle_name,
+            initcap(name_last) as cb_last_name,
+            gender as cb_gender,
+            birth_date as cb_dob,
+
+            concat(
+                initcap(name_last), ', ', initcap(name_first), ' ', initcap(name_mi)
+            ) as cb_student_name,
+
+            row_number() over (partition by secondary_id, test_type) as rn_distinct,
         from {{ ref("int_collegeboard__psat_unpivot") }}
+    ),
+
+    roster as (
+        select
+            e.student_number,
+            concat(
+                e.last_name, ', ', e.first_name, ' ', left(e.middle_name, 1)
+            ) as student_name,
+            e.first_name,
+            left(e.middle_name, 1) as middle_initial,
+            e.last_name,
+            e.dob,
+            e.gender,
+            p.cb_id,
+            p.xwalk_student_number,
+            p.cb_student_name,
+            p.cb_first_name,
+            p.cb_middle_name,
+            p.cb_last_name,
+            p.cb_gender,
+            p.cb_dob,
+
+        from {{ ref("base_powerschool__student_enrollments") }} as e
+        left join psat as p on e.student_number = p.secondary_id and p.rn_distinct = 1
+        where e.rn_undergrad = 1 and e.region != 'Miami'
     )
 
-select
-    e.student_number,
-    e.first_name,
-    e.middle_name,
-    e.last_name,
-    e.dob,
-    e.gender,
-    p.cb_id,
-    p.powerschool_student_number as xwalk_student_number,
-    p.birth_date as cb_dob,
-    p.gender as cb_gender,
-    p.test_type as cb_test_type,
-from {{ ref("base_powerschool__student_enrollments") }} as e
-left join psat as p on e.student_number = p.secondary_id
-where e.rn_undergrad = 1
+select *, if(student_name = cb_student_name, true, false) name_match,
+from roster
