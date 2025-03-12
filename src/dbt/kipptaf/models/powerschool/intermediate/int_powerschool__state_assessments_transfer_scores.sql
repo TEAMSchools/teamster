0787f7ba412bee8c2922537ec0615459673838f0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-03-12 19:04:36 +0000

    refactor: simplify joins

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__state_assessments_transfer_scores.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__state_assessments_transfer_scores.sql
@@ -1,33 +1,35 @@
 select
-    b._dbt_source_relation,
-    b.name as test_name,
+    t._dbt_source_relation,
+    t.name as test_name,
 
-    s.studentid,
-    s.grade_level as assessment_grade_level,
+    st.studentid,
+    st.grade_level as assessment_grade_level,
 
-    t.numscore as testscalescore,
-    t.alphascore as testperformancelevel,
+    sts.numscore as testscalescore,
+    sts.alphascore as testperformancelevel,
 
-    r.name as testcode,
+    ts.name as testcode,
 
-    case r.name when 'ELAGP' then 'ELA' when 'MATGP' then 'Math' end as discipline,
+    case ts.name when 'ELAGP' then 'ELA' when 'MATGP' then 'Math' end as discipline,
 
     case
-        r.name when 'ELAGP' then 'English Language Arts' when 'MATGP' then 'Mathematics'
+        ts.name
+        when 'ELAGP'
+        then 'English Language Arts'
+        when 'MATGP'
+        then 'Mathematics'
     end as `subject`,
-from {{ ref("stg_powerschool__test") }} as b
+from {{ ref("stg_powerschool__test") }} as t
 inner join
-    {{ ref("stg_powerschool__studenttest") }} as s
-    on b.id = s.testid
-    and {{ union_dataset_join_clause(left_alias="b", right_alias="s") }}
+    {{ ref("stg_powerschool__studenttest") }} as st
+    on t.id = st.testid
+    and {{ union_dataset_join_clause(left_alias="t", right_alias="st") }}
 inner join
-    {{ ref("stg_powerschool__studenttestscore") }} as t
-    on s.studentid = t.studentid
-    and s.id = t.studenttestid
-    and {{ union_dataset_join_clause(left_alias="s", right_alias="t") }}
+    {{ ref("stg_powerschool__studenttestscore") }} as sts
+    on st.id = sts.studenttestid
+    and {{ union_dataset_join_clause(left_alias="st", right_alias="sts") }}
 inner join
-    {{ ref("stg_powerschool__testscore") }} as r
-    on s.testid = r.testid
-    and t.testscoreid = r.id
-    and {{ union_dataset_join_clause(left_alias="s", right_alias="r") }}
-where b.name = 'NJGPA'
+    {{ ref("stg_powerschool__testscore") }} as ts
+    on sts.testscoreid = ts.id
+    and {{ union_dataset_join_clause(left_alias="sts", right_alias="ts") }}
+where t.name = 'NJGPA'
