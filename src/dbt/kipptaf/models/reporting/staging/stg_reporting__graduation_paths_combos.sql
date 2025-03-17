b93608694355591bf01bc3db8dfc61520e8d0892
Gaby Rangel <grangel@kippteamandfamily.org>
2025-03-17 18:03:59 +0000

    test again

--- src/dbt/kipptaf/models/reporting/staging/stg_reporting__graduation_paths_combos.sql
+++ src/dbt/kipptaf/models/reporting/staging/stg_reporting__graduation_paths_combos.sql
@@ -1,1 +1,12 @@
-select *, from {{ source("reporting", "src_reporting__graduation_path_combos") }}
+select
+    grade_level,
+    has_fafsa,
+    njgpa_season_11th,
+    fafsa_season_12th,
+    met_dlm,
+    met_portfolio,
+    njgpa_attempt,
+    met_ela,
+    met_math,
+    grad_eligibility,
+from {{ source("reporting", "src_reporting__graduation_path_combos") }}
