e461414e300050612e6e0ee72746817c09a9fdbf
Gaby Rangel <grangel@kippteamandfamily.org>
2025-03-17 20:20:12 +0000

    not minding my biz

---
+++ src/dbt/kipptaf/tests/test_new_grad_path_combo.sql
@@ -0,0 +1,12 @@
+{{-
+    config(
+        severity="warn",
+        store_failures=true,
+        store_failures_as="view",
+        enabled=true,
+    )
+-}}
+
+select *
+from {{ ref("model_name") }}
+where grad_eligibility = 'New category. Need new logic.'
