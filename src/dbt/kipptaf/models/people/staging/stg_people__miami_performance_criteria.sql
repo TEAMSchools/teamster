d0d9bb67632fd462d5c5f42cfda28ffb98ebf3da
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-03-18 20:03:04 +0000

    fix: reserved keyword

--- src/dbt/kipptaf/models/people/staging/stg_people__miami_performance_criteria.sql
+++ src/dbt/kipptaf/models/people/staging/stg_people__miami_performance_criteria.sql
@@ -1,1 +1,8 @@
-select *, from {{ source("people", "src_people__miami_performance_criteria") }}
+select
+    academic_year,
+    criteria,
+    grade_level,
+    `group` as criteria_group,
+    measure,
+    payout_amount,
+from {{ source("people", "src_people__miami_performance_criteria") }}
