a2711b4d107110c1ec4cd19561e5db5c969116bc
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-03-18 20:26:25 +0000

    fix: list cols in def

--- src/dbt/kipptaf/models/assessments/staging/stg_assessments__course_subject_crosswalk.sql
+++ src/dbt/kipptaf/models/assessments/staging/stg_assessments__course_subject_crosswalk.sql
@@ -1,1 +1,9 @@
-select *, from {{ source("assessments", "src_assessments__course_subject_crosswalk") }}
+select
+    powerschool_course_number,
+    powerschool_course_name,
+    illuminate_subject_area,
+    is_foundations,
+    is_advanced_math,
+    exclude_from_gradebook,
+    duplicate_audit,
+from {{ source("assessments", "src_assessments__course_subject_crosswalk") }}
--- src/dbt/kipptaf/models/people/staging/stg_people__powerschool_crosswalk.sql
+++ src/dbt/kipptaf/models/people/staging/stg_people__powerschool_crosswalk.sql
@@ -1,1 +1,2 @@
-select *, from {{ source("people", "src_people__powerschool_crosswalk") }}
+select employee_number, adp_worker_id, powerschool_teacher_number, `name`, is_active,
+from {{ source("people", "src_people__powerschool_crosswalk") }}
