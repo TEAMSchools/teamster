b5f2c717847fc02e008ebd2b3e97756f3978be7b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-03-18 14:20:34 +0000

    fix: schema change workaround

--- src/dbt/kipptaf/models/students/intermediate/int_students__graduation_path_codes.sql
+++ src/dbt/kipptaf/models/students/intermediate/int_students__graduation_path_codes.sql
@@ -438,7 +438,6 @@ select
     row_number() over (
         partition by r.student_number, r.discipline order by r.pathway_option
     ) as rn_discipline_distinct,
-
 from roster as r
 left join
     {{ ref("stg_reporting__graduation_paths_combos") }} as g
@@ -448,6 +447,6 @@ left join
     and r.fafsa_season_12th = g.fafsa_season_12th
     and r.met_dlm = g.met_dlm
     and r.met_portfolio = g.met_portfolio
-    and r.njgpa_attempt = g.njgpa_attempt
+    -- and r.njgpa_attempt = g.njgpa_attempt
     and r.met_ela = g.met_ela
     and r.met_math = g.met_math
