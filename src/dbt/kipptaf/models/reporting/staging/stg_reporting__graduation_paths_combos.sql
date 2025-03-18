3c544344cf7a91726980a4f393331eb13b30584d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-03-18 14:20:34 +0000

    fix: schema change workaround

--- src/dbt/kipptaf/models/reporting/staging/stg_reporting__graduation_paths_combos.sql
+++ src/dbt/kipptaf/models/reporting/staging/stg_reporting__graduation_paths_combos.sql
@@ -1,12 +1,1 @@
-select
-    grade_level,
-    has_fafsa,
-    njgpa_season_11th,
-    fafsa_season_12th,
-    met_dlm,
-    met_portfolio,
-    njgpa_attempt,
-    met_ela,
-    met_math,
-    grad_eligibility,
-from {{ source("reporting", "src_reporting__graduation_path_combos") }}
+select *, from {{ source("reporting", "src_reporting__graduation_path_combos") }}
