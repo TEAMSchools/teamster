fc9c7951d7b87dbe25ae8f9cace237cb31ef0eca
Gaby Rangel <grangel@kippteamandfamily.org>
2025-03-20 21:02:26 +0000

    ok, this is for tomorrow for sure

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__grad_plans_progress_students.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__grad_plans_progress_students.sql
@@ -1,49 +1,40 @@
+with
+    students as (
+        select
+            s._dbt_source_relation,
+            s.studentsdcid,
+            s.gpnodeid as plan_id,
+            s.earnedcredits,  -- from stored grades
+            s.enrolledcredits,  -- from cc table
+            s.requestedcredits,  -- from schedule requests table
+            s.requiredcredits,
+            s.waivedcredits,
+            s.isadvancedplan,
+
+            g.id as subject_id,
+            g.name as subject_name,
+
+        from {{ ref("stg_powerschool__gpprogresssubject") }} as s
+        inner join
+            {{ ref("stg_powerschool__gpnode") }} as g
+            on s.gpnodeid = g.id
+            and s.nodetype = g.nodetype
+            and {{ union_dataset_join_clause(left_alias="s", right_alias="g") }}
+    )
+
 select
-    s._dbt_source_relation,
+    p.*,
+
     s.studentsdcid,
-    s.gpnodeid as plan_id,
-    s.earnedcredits,  -- from stored grades
-    s.enrolledcredits,  -- from cc table
-    s.requestedcredits,  -- from schedule requests table
-    s.requiredcredits,
-    s.waivedcredits,
-    s.isadvancedplan,
+    s.isadvancedplan as advanced_plan,
+    s.earnedcredits as subject_earnedcredits,
+    s.enrolledcredits as subject_enrolledcredits,
+    s.requestedcredits as subject_requestedcredits,
+    s.requiredcredits as subject_requiredcredits,
+    s.waivedcredits as subject_waivedcredits,
 
-    g.id as subject_id,
-    g.name as subject_name,
-
-from {{ ref("stg_powerschool__gpprogresssubject") }} as s
+from {{ ref("int_powerschool__grad_plans") }} as p
 inner join
-    {{ ref("stg_powerschool__gpnode") }} as g
-    on s.gpnodeid = g.id
-    and s.nodetype = g.nodetype
-    and {{ union_dataset_join_clause(left_alias="s", right_alias="g") }}
-    -- and g.name not in ('Total Credits', 'Overall Credits')
-    -- and g.creditcapacity is not null
-    /* 
-union all
-
-select
-    s._dbt_source_relation,
-    s.studentsdcid,
-    s.gpnodeid as plan_id,
-    s.earnedcredits,  -- from stored grades
-    s.enrolledcredits,  -- from cc table
-    s.requestedcredits,  -- from schedule requests table
-    s.requiredcredits,
-    s.waivedcredits,
-    s.isadvancedplan,
-
-    g.id as subject_id,
-    g.name as subject_name,
-
-from {{ ref("stg_powerschool__gpprogresssubject") }} as s
-inner join
-    {{ ref("stg_powerschool__gpnode") }} as g
-    on s.gpnodeid = g.id
-    and s.nodetype = g.nodetype
-    and {{ union_dataset_join_clause(left_alias="s", right_alias="g") }}
-    and g.name in ('Total Credits', 'Overall Credits')
-    and g.creditcapacity is not null
-*/
-    
+    students as s
+    on p.subject_id = s.subject_id
+    and {{ union_dataset_join_clause(left_alias="p", right_alias="s") }}
