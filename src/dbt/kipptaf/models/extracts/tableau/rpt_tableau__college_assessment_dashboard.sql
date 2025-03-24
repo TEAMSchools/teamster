f716e70d094478f37b89da2ae0c5857dff82b7fd
Gaby Rangel <grangel@kippteamandfamily.org>
2025-03-24 18:24:29 +0000

    first pass: new multiyear carat data model

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard.sql
@@ -1,4 +1,44 @@
 with
+    base_courses as (
+        /*
+            this CTE and the following one were added to help figure out which base
+            course to pick for a student, since not all of them have CCR, and not all
+            have HR, so i wrote this to try to catch everyone possible
+        */
+        select
+            _dbt_source_relation,
+            cc_academic_year,
+            students_student_number as student_number,
+            courses_course_name,
+
+            if(courses_course_name = 'HR', 2, 1) as course_rank,
+
+            min(if(courses_course_name = 'HR', 2, 1)) over (
+                partition by cc_academic_year, students_student_number
+            ) as expected_course_rank,
+        from {{ ref("base_powerschool__course_enrollments") }}
+        where
+            courses_course_name in (
+                'HR',
+                'College and Career IV',
+                'College and Career I',
+                'College and Career III',
+                'College and Career II'
+            )
+            and rn_course_number_year = 1
+            and not is_dropped_section
+    ),
+
+    expected_course as (
+        select distinct
+            _dbt_source_relation,
+            cc_academic_year,
+            student_number,
+            courses_course_name as courses_course_name_expected,
+        from base_courses
+        where course_rank = expected_course_rank
+    ),
+
     roster as (
         select
             e.academic_year,
@@ -11,6 +51,8 @@ with
             e.grade_level,
             e.enroll_status,
             e.cohort,
+            e.entrydate,
+            e.exitdate,
             e.is_504,
             e.lep_status,
             e.gifted_and_talented,
@@ -20,6 +62,7 @@ with
             e.contact_owner_name,
             e.college_match_gpa,
             e.college_match_gpa_bands,
+            e.ms_attended,
 
             s.courses_course_name,
             s.teacher_lastfirst,
@@ -30,6 +73,7 @@ with
             t.subject_area as expected_subject_area,
 
             if(e.iep_status = 'No IEP', 0, 1) as sped,
+
         from {{ ref("int_extracts__student_enrollments") }} as e
         left join
             {{ ref("base_powerschool__course_enrollments") }} as s
@@ -41,17 +85,27 @@ with
                 'College and Career IV',
                 'College and Career I',
                 'College and Career III',
-                'College and Career II'
+                'College and Career II',
+                'HR'
             )
+        left join
+            expected_course as ec
+            on s.cc_academic_year = ec.cc_academic_year
+            and s.students_student_number = ec.student_number
+            and s.courses_course_name = ec.courses_course_name_expected
+            and {{ union_dataset_join_clause(left_alias="s", right_alias="ec") }}
+        left join
+            {{ ref("int_extracts__student_enrollments_subjects") }} as f
+            on s.cc_academic_year = f.academic_year
+            and s.students_student_number = f.student_number
+            and s.courses_credittype = f.powerschool_credittype
         left join
             {{ ref("stg_assessments__assessment_expectations") }} as t
             on e.academic_year = t.academic_year
             and e.grade_level = t.grade
             and e.region = t.region
             and t.assessment_type = 'College Entrance'
-        where
-            e.academic_year = {{ var("current_academic_year") }}
-            and e.school_level = 'HS'
+        where e.school_level = 'HS' and ec.courses_course_name_expected is not null
     ),
 
     course_subjects_roster as (
@@ -94,6 +148,8 @@ select
     e.grade_level,
     e.enroll_status,
     e.cohort,
+    e.entrydate,
+    e.exitdate,
     e.sped,
     e.is_504,
     e.lep_status,
@@ -104,6 +160,7 @@ select
     e.contact_owner_name,
     e.college_match_gpa,
     e.college_match_gpa_bands,
+    e.ms_attended,
     e.courses_course_name,
     e.teacher_lastfirst,
     e.sections_external_expression,
@@ -139,6 +196,7 @@ select
     c.is_math_double_blocked,
 
     coalesce(c.is_exempt_state_testing, false) as is_exempt_state_testing,
+
 from roster as e
 left join
     {{ ref("int_assessments__college_assessment") }} as o
@@ -166,6 +224,8 @@ select
     e.grade_level,
     e.enroll_status,
     e.cohort,
+    e.entrydate,
+    e.exitdate,
     e.sped,
     e.is_504,
     e.lep_status,
@@ -176,6 +236,7 @@ select
     e.contact_owner_name,
     e.college_match_gpa,
     e.college_match_gpa_bands,
+    e.ms_attended,
     e.courses_course_name,
     e.teacher_lastfirst,
     e.sections_external_expression,
@@ -211,6 +272,7 @@ select
     c.is_math_double_blocked,
 
     coalesce(c.is_exempt_state_testing, false) as is_exempt_state_testing,
+
 from roster as e
 left join
     {{ ref("int_assessments__college_assessment") }} as o
@@ -224,10 +286,13 @@ left join
     and e.academic_year = c.academic_year
     and o.course_discipline = c.courses_credittype
 where
-    e.expected_test_type = 'Official' and e.expected_scope in ('PSAT NMSQT', 'PSAT 8/9')
+    e.expected_test_type = 'Official'
+    and e.expected_scope in ('PSAT NMSQT', 'PSAT 8/9', 'PSAT10')
 
--- this code is a placeholder for now, as we are not reporting practice scores for
--- sy2425, but will do so again for sy2526
+/* 
+   this code is a placeholder for now, as we are not reporting practice 
+   scores for sy2425, but will do so again for sy2526 
+*/
 union all
 
 select
@@ -241,6 +306,8 @@ select
     e.grade_level,
     e.enroll_status,
     e.cohort,
+    e.entrydate,
+    e.exitdate,
     e.sped,
     e.is_504,
     e.lep_status,
@@ -251,6 +318,7 @@ select
     e.contact_owner_name,
     e.college_match_gpa,
     e.college_match_gpa_bands,
+    e.ms_attended,
     e.courses_course_name,
     e.teacher_lastfirst,
     e.sections_external_expression,
@@ -285,6 +353,7 @@ select
     c.is_math_double_blocked,
 
     coalesce(c.is_exempt_state_testing, false) as is_exempt_state_testing,
+
 from roster as e
 left join
     {{ ref("int_assessments__college_assessment_practice") }} as p
