bec12506bdc4b8ad134fe25aab62c3671365f8e8
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-03-28 14:57:26 +0000

    fix: contact priority order

--- src/dbt/powerschool/models/intermediate/int_powerschool__student_contacts.sql
+++ src/dbt/powerschool/models/intermediate/int_powerschool__student_contacts.sql
@@ -1,16 +1,13 @@
 select
-    c.student_number,
-    c.family_ident,
+    c.studentdcid,
     c.person_type,
     c.relationship_type,
     c.isemergency,
     c.schoolpickupflg,
+    c.contact_name,
 
     pc.contact_category,
     pc.contact,
-    pc.priority_order as contact_priority_order,
-
-    concat(c.firstname, ' ', c.lastname) as contact_name,
 
     if(
         pc.contact_category in ('Email', 'Address'),
@@ -22,7 +19,4 @@ inner join
     {{ ref("int_powerschool__person_contacts") }} as pc
     on c.personid = pc.personid
     and pc.contact_type in ('Current', 'Daytime', 'Home', 'Mobile', 'Not Set', 'Work')
-    and (
-        (pc.contact_category in ('Address', 'Email') and pc.priority_order = 1)
-        or pc.contact_category = 'Phone'
-    )
+    and pc.contact_category in ('Address', 'Email', 'Phone')
