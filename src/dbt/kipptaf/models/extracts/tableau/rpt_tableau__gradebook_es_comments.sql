43a1ac8213c10014dc30247deb1d0b8e92f0193b
Gaby Rangel <grangel@kippteamandfamily.org>
2025-04-01 18:02:30 +0000

    added ms comments for miami

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__gradebook_es_comments.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__gradebook_es_comments.sql
@@ -123,7 +123,10 @@ with
             audit_flag_name,
             audit_flag_value as flag_value,
         from {{ ref("int_tableau__gradebook_audit_flags") }}
-        where audit_category = 'Comments'
+        where
+            audit_category = 'Comments'
+            or concat(region_school_level, audit_flag_name)
+            = 'MiamiMSqt_grade_70_comment_missing'
     )
 
 select
@@ -194,7 +197,4 @@ left join
     and v.sectionid = e.sectionid
     and v.student_number = e.student_number
     and e.audit_category = 'Effort Grade'
-where
-    t.code_type = 'Quarter'
-    and t.audit_category = 'Comments'
-    and t.audit_start_date <= current_date('{{ var("local_timezone") }}')
+where t.audit_start_date <= current_date('{{ var("local_timezone") }}')
