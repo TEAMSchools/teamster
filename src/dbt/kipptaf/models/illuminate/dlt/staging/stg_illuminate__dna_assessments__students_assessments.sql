174f00defce436aa919116f1706ba8bf576b4df4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-04-03 16:20:51 +0000

    fix: duplicates

--- src/dbt/kipptaf/models/illuminate/dlt/staging/stg_illuminate__dna_assessments__students_assessments.sql
+++ src/dbt/kipptaf/models/illuminate/dlt/staging/stg_illuminate__dna_assessments__students_assessments.sql
@@ -1,9 +1,20 @@
-select
-    student_assessment_id,
-    student_id,
-    assessment_id,
-    date_taken,
-    created_at,
-    updated_at,
-    version_id,
-from {{ source("illuminate_dna_assessments", "students_assessments") }}
+with
+    students_assessments as (
+        select
+            student_assessment_id,
+            student_id,
+            assessment_id,
+            date_taken,
+            created_at,
+            updated_at,
+            version_id,
+        from {{ source("illuminate_dna_assessments", "students_assessments") }}
+    )
+
+    {{
+        dbt_utils.deduplicate(
+            relation="students_assessments",
+            partition_by="student_id, assessment_id",
+            order_by="updated_at desc, student_assessment_id desc",
+        )
+    }}
