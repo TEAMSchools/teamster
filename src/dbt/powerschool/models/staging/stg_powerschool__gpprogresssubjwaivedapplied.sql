66a478f969d394265261a09be91d22ab0b9101e6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-04-14 20:55:26 +0000

    feat: staging models

--- src/dbt/powerschool/models/staging/stg_powerschool__gpprogresssubjwaivedapplied.sql
+++ src/dbt/powerschool/models/staging/stg_powerschool__gpprogresssubjwaivedapplied.sql
@@ -1,7 +1,8 @@
 select
+    -- whencreated,
     /* records */
     id.int_value as id,
     gpprogresssubjectid.int_value as gpprogresssubjectid,
     gpstudentwaiverid.int_value as gpstudentwaiverid,
-    waivedcredits.double_value as waivedcredits,
-from {{ source("powerschool", "src_powerschool__gpprogresssubjectwaived") }}
+    appliedcredits.double_value as appliedcredits,
+from {{ source("powerschool", "src_powerschool__gpprogresssubjwaivedapplied") }}
