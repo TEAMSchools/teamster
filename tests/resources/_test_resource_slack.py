49c8367c648d67dd9bf11ca55b67c6dfc034f0f8
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-04-15 19:39:16 +0000

    test

--- tests/resources/_test_resource_slack.py
+++ tests/resources/_test_resource_slack.py
@@ -1,9 +1,8 @@
-from dagster import EnvVar
+r"""from dagster import EnvVar
 from dagster_slack import SlackResource
 
 
 def test_slack_resource():
-    # trunk-ignore(pyright/reportArgumentType)
     slack = SlackResource(token=EnvVar("SLACK_TOKEN").get_value())
 
     slack_client = slack.get_client()
@@ -11,3 +10,4 @@ def test_slack_resource():
     exceptions = ["*`foo` errors:*", "bar", "baz", "spam", "eggs", "google.com"]
 
     slack_client.chat_postMessage(channel="#dagster-alerts", text="\n".join(exceptions))
+"""
