7c2150c42d598fd0bbd505e231ccc4d052b91366
Gaby Rangel <grangel@kippteamandfamily.org>
2025-04-16 19:16:11 +0000

    trunk

--- src/dbt/kipptaf/macros/tests/duplicate_grad_eligibility.sql
+++ src/dbt/kipptaf/macros/tests/duplicate_grad_eligibility.sql
@@ -1,4 +1,4 @@
-select student_number, count(distinct grad_eligibility) as n
+select student_number, count(distinct grad_eligibility) as n,
 from {{ ref("int_students__graduation_path_codes") }}
 group by all
 having count(distinct grad_eligibility) > 1
