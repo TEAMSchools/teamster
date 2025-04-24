57747c277d10f034fdbc941b6d1fcc9ce305bb66
Cristina Baldor <ccbaldor@icloud.com>
2025-04-24 15:35:54 +0000

    fix: filters and references

--- src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__leadership_pm_reminders.sql
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__leadership_pm_reminders.sql
@@ -1,5 +1,6 @@
 select
     l.preferred_name_lastfirst,
+    l.term,
 
     sr.mail,
     sr.reports_to_formatted_name as manager,
@@ -7,12 +8,12 @@ select
 
     max(l.round_completion_self) as round_completion_self,
     max(l.round_completion_manager) as round_completion_manager,
-from `kipptaf_tableau.rpt_tableau__leadership_development` as l
+from {{ ref("rpt_tableau__leadership_development") }} as l
 left join
-    `kipptaf_people.int_people__staff_roster` as sr
+    {{ ref("int_people__staff_roster") }} as sr
     on l.employee_number = sr.employee_number
 left join
-    `kipptaf_appsheet.stg_leadership_development_active_users` as a
+    {{ ref("stg_leadership_development__active_users") }} as a
     on l.employee_number = safe_cast(a.employee_number as int)
 where
     l.academic_year = 2024
@@ -20,8 +21,10 @@ where
     and l.active_assignment
     and a.active_title
     and not a.special_case
+    and sr.assignment_status in ('Active', 'Leave')
 group by
     l.preferred_name_lastfirst,
+    l.term,
     sr.mail,
     sr.reports_to_formatted_name,
     sr.reports_to_mail
