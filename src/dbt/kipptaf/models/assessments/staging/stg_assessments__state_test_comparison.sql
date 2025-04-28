bf8ad64e9fae1ed6a6a389437f319d235eed7b92
Gaby Rangel <grangel@kippteamandfamily.org>
2025-04-28 20:50:43 +0000

    ?

--- src/dbt/kipptaf/models/assessments/staging/stg_assessments__state_test_comparison.sql
+++ src/dbt/kipptaf/models/assessments/staging/stg_assessments__state_test_comparison.sql
@@ -1,3 +1,1 @@
-select *,
-from {{ source("assessments", "src_assessments__state_test_comparison") }}
-where total_number_of_students is not null
+select *, from {{ source("assessments", "src_assessments__state_test_comparison") }}
