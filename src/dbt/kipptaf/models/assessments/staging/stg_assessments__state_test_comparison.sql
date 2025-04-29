43fed0026a6d8c3e5f44140e1924fce482772d7e
Gaby Rangel <grangel@kippteamandfamily.org>
2025-04-29 14:20:43 +0000

    ?

--- src/dbt/kipptaf/models/assessments/staging/stg_assessments__state_test_comparison.sql
+++ src/dbt/kipptaf/models/assessments/staging/stg_assessments__state_test_comparison.sql
@@ -1,1 +1,9 @@
-select *, from {{ source("assessments", "src_assessments__state_test_comparison") }}
+select
+    academic_year,
+    test_name,
+    test_code,
+    region,
+    comparison_entity,
+    percent_proficient,
+    total_number_of_students,
+from {{ source("assessments", "src_assessments__state_test_comparison") }}
