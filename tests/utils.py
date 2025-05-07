6fbe0b78d21129c147f03ea94e4dd08633b3b17d
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-05-07 15:31:58 +0000

    fix: schema

--- tests/utils.py
+++ tests/utils.py
@@ -42,7 +42,8 @@ def get_renlearn_ssh_resource(code_location: str):
     code_location = code_location.upper()
 
     return SSHResource(
-        remote_host="sftp.renaissance.com",
+        remote_host=EnvVar("RENLEARN_SFTP_HOST"),
+        remote_port=22,
         username=EnvVar(f"RENLEARN_SFTP_USERNAME_{code_location}"),
         password=EnvVar(f"RENLEARN_SFTP_PASSWORD_{code_location}"),
     )
