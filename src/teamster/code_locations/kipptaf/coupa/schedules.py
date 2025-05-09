a56ea7f7e4e8d455236acb1fe3e00749c674b0ab
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-05-09 15:13:06 +0000

    feat: schedule, dbt models, secrets

--- src/teamster/code_locations/kipptaf/coupa/schedules.py
+++ src/teamster/code_locations/kipptaf/coupa/schedules.py
@@ -1,7 +1,7 @@
 from dagster import ScheduleDefinition
 
 from teamster.code_locations.kipptaf import CODE_LOCATION, LOCAL_TIMEZONE
-from teamster.code_locations.kipptaf.coupa import assets
+from teamster.code_locations.kipptaf.coupa.assets import assets
 
 coupa_asset_job_schedule = ScheduleDefinition(
     name=f"{CODE_LOCATION}__coupa__asset_job_schedule",
