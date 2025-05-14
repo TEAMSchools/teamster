df29698d28e634ebba1024940f1200cbc69eeacd
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-05-14 21:19:28 +0000

    feat: zendesk extract queries

--- src/dbt/kipptaf/models/extracts/zendesk/rpt_zendesk__users.sql
+++ src/dbt/kipptaf/models/extracts/zendesk/rpt_zendesk__users.sql
@@ -1,23 +1,87 @@
+with
+    staff_roster as (
+        select
+            sr.user_principal_name as email,
+            sr.personal_email,
+            sr.google_email,
+
+            {# TODO: add lookup table for these #}
+            {# sr.home_business_unit_code, #}
+            {# sr.home_work_location_name, #}
+            u.organization_id,
+            u.secondary_location,
+
+            u.id as zendesk_user_id,
+            u.name as zendesk_name,
+            u.suspended as zendesk_suspended,
+            u.organization_id as zendesk_organization_id,
+            u.secondary_location as zendesk_secondary_location,
+            u.email as zendesk_email,
+
+            sr.given_name || ' ' || sr.family_name_1 as `name`,
+
+            cast(sr.employee_number as string) as external_id,
+
+            coalesce(u.role, 'end-user') as `role`,
+
+            if(sr.user_principal_name != sr.mail, sr.mail, null) as mail_alias,
+
+            if(
+                sr.assignment_status = 'Terminated' and u.role != 'agent', true, false
+            ) as suspended,
+        from {{ ref("int_people__staff_roster") }} as sr
+        left join
+            {{ ref("stg_zendesk__users") }} as u on sr.user_principal_name = u.email
+        where sr.user_principal_name is not null
+    ),
+
+    comparison as (
+        select
+            *,
+
+            if(zendesk_user_id is null, true, false) as is_create,
+
+            {{
+                dbt_utils.generate_surrogate_key(
+                    field_list=[
+                        "name",
+                        "suspended",
+                        "organization_id",
+                        "secondary_location",
+                    ]
+                )
+            }} as adp_surrogate_key,
+
+            {{
+                dbt_utils.generate_surrogate_key(
+                    field_list=[
+                        "zendesk_name",
+                        "zendesk_suspended",
+                        "zendesk_organization_id",
+                        "zendesk_secondary_location",
+                    ]
+                )
+            }} as zendesk_surrogate_key,
+        from staff_roster
+    )
+
 select
-    sr.formatted_name as `name`,
-    sr.assigned_business_unit_code,
-    sr.physical_delivery_office_name,
-    sr.user_principal_name as email,
-    sr.mail as ad_mail,
-    sr.google_email,
-    sr.personal_email,
+    external_id,
+    zendesk_user_id as user_id,
+    `name`,
+    suspended,
+    email,
+    `role`,
+    organization_id,
+    secondary_location,
+    is_create,
 
-    {# TODO: add lookup table for these #}
-    u.organization_id,
-    u.secondary_location,
-
-    cast(sr.employee_number as string) as external_id,
-
-    coalesce(u.role, 'end-user') as `role`,
+    /* user identities */
+    mail_alias,
+    google_email,
+    personal_email,
 
     if(
-        sr.assignment_status = 'Terminated' and u.role != 'agent', true, false
-    ) as suspended,
-from {{ ref("int_people__staff_roster") }} as sr
-left join {{ ref("stg_zendesk__users") }} as u on sr.user_principal_name = u.email
-where sr.user_principal_name is not null
+        not is_create and adp_surrogate_key != zendesk_surrogate_key, true, false
+    ) as is_update,
+from comparison
