3b27787473eb9cde2f7f624f50848a433bb1859b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-05-14 16:49:13 +0000

    fix: use upn

--- src/dbt/kipptaf/models/extracts/zendesk/rpt_zendesk__users.sql
+++ src/dbt/kipptaf/models/extracts/zendesk/rpt_zendesk__users.sql
@@ -2,14 +2,14 @@ select
     sr.formatted_name as `name`,
     sr.assigned_business_unit_code,
     sr.physical_delivery_office_name,
-    sr.mail as email,
-    sr.user_principal_name,
+    sr.user_principal_name as email,
+    sr.mail as ad_mail,
     sr.google_email,
     sr.personal_email,
 
     {# TODO: add lookup table for these #}
-    null as organization_id,
-    null as secondary_location,
+    u.organization_id,
+    u.secondary_location,
 
     cast(sr.employee_number as string) as external_id,
 
@@ -19,5 +19,5 @@ select
         sr.assignment_status = 'Terminated' and u.role != 'agent', true, false
     ) as suspended,
 from {{ ref("int_people__staff_roster") }} as sr
-left join {{ ref("stg_zendesk__users") }} as u on sr.mail = u.email
-where sr.mail is not null
+left join {{ ref("stg_zendesk__users") }} as u on sr.user_principal_name = u.email
+where sr.user_principal_name is not null
