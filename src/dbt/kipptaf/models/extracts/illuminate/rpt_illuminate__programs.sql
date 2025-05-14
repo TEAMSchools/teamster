8482efbb425da16e151b634444fc60328d48fecd
Anthony Walters <awalters@kippteamandfamily.org>
2025-05-14 20:15:28 +0000

    add bucket data to illuminate programs

--- src/dbt/kipptaf/models/extracts/illuminate/rpt_illuminate__programs.sql
+++ src/dbt/kipptaf/models/extracts/illuminate/rpt_illuminate__programs.sql
@@ -12,7 +12,7 @@ select
 
     dob as `06 Birth Date`,
 
-    127 as `07 Program ID`,
+    '127' as `07 Program ID`,
     null as `08 Eligibility Start Date`,
     null as `09 Eligibility End Date`,
 
@@ -48,7 +48,7 @@ select
 
     dob as `06 Birth Date`,
 
-    120 as `07 Program ID`,
+    '120' as `07 Program ID`,
     null as `08 Eligibility Start Date`,
     null as `09 Eligibility End Date`,
 
@@ -67,3 +67,46 @@ where
     and rn_year = 1
     and lep_status
     and grade_level != 99
+
+union all
+/* buckets */
+select
+    student_number as `01 Import Student ID`,
+
+    null as `02 State Student ID`,
+
+    student_last_name as `03 Student Last Name`,
+    student_first_name as `04 Student First Name`,
+
+    null as `05 Student Middle Name`,
+
+    dob as `06 Birth Date`,
+
+    case
+        when discipline = 'ELA' and nj_student_tier = 'Bucket 1'
+        then 'B1E'
+        when discipline = 'Math' and nj_student_tier = 'Bucket 1'
+        then 'B1M'
+        when discipline = 'ELA' and nj_student_tier = 'Bucket 2'
+        then 'B2E'
+        when discipline = 'Math' and nj_student_tier = 'Bucket 2'
+        then 'B2M'
+        when discipline = 'ELA' and nj_student_tier is null
+        then 'BUE'
+        when discipline = 'Math' and nj_student_tier is null
+        then 'BUM'
+    end as `07 Program ID`,
+    null as `08 Eligibility Start Date`,
+    null as `09 Eligibility End Date`,
+
+    entrydate as `10 Program Start Date`,
+    exitdate as `11 Program End Date`,
+
+    concat(academic_year, '-', (academic_year + 1)) as `12 Academic Year`,
+
+    null as `13 Program Eligibility Code`,
+    null as `14 Program Exit Code`,
+    null as `15 Site ID`,
+-- trunk-ignore-end(sqlfluff/RF05)
+from {{ ref("int_extracts__student_enrollments_subjects") }}
+where academic_year = {{ var("current_academic_year") }} and nj_student_tier is not null
