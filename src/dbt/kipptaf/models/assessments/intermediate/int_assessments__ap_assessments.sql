9b9724678129d83d7c273f08a485e350fe4b1881
Gaby Rangel <grangel@kippteamandfamily.org>
2025-05-20 13:02:37 +0000

    removed rn

--- src/dbt/kipptaf/models/assessments/intermediate/int_assessments__ap_assessments.sql
+++ src/dbt/kipptaf/models/assessments/intermediate/int_assessments__ap_assessments.sql
@@ -11,12 +11,6 @@ with
             c.ap_course_name,
             c.data_source,
 
-            row_number() over (
-                partition by
-                    a.academic_year, a.school_specific_id, a.test_subject, a.score
-                order by a.date desc
-            ) as rn_distinct,
-
         from {{ ref("int_kippadb__standardized_test_unpivot") }} as a
         left join
             {{ ref("stg_collegeboard__ap_course_crosswalk") }} as c
@@ -40,15 +34,6 @@ with
             c.ap_course_name,
             c.data_source,
 
-            row_number() over (
-                partition by
-                    a.academic_year,
-                    a.powerschool_student_number,
-                    a.exam_code_description,
-                    a.exam_grade
-                order by a.rn_exam_number desc
-            ) as rn_distinct,
-
         from {{ ref("int_collegeboard__ap_unpivot") }} as a
         left join
             {{ ref("stg_collegeboard__ap_course_crosswalk") }} as c
@@ -71,7 +56,6 @@ select
     null as irregularity_code_2,
 
 from adb_scores
-where rn_distinct = 1
 
 union all
 
@@ -89,4 +73,3 @@ select
     irregularity_code_2,
 
 from cb_scores
-where rn_distinct = 1
