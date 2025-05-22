d055212fda8ae2bb7eaf179656fcb275d26004ae
Gaby Rangel <grangel@kippteamandfamily.org>
2025-05-22 19:52:22 +0000

    new athletic elig view

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__grad_plan_tracking.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__grad_plan_tracking.sql
@@ -1,195 +1,133 @@
 with
-    credits as (
-        select
-            g._dbt_source_relation,
-            g.academic_year,
-            g.studentsdcid,
-            g.plan_id,
-            g.plan_name,
-            g.discipline_id,
-            g.discipline_name,
-            g.subject_id,
-            g.subject_name,
-            g.teacher_name,
-            g.course_number,
-            g.course_name,
-            g.sectionid,
-            g.credit_type,
-            g.letter_grade,
-            g.is_transfer_grade,
-            g.credit_status,
-            g.earned_credits,
-            g.potential_credits,
-
-            e.region,
-            e.schoolid,
-            e.school,
-            e.studentid,
-            e.state_studentnumber,
-            e.salesforce_id,
-            e.student_number,
-            e.student_name,
-            e.grade_level,
-            e.student_email,
-            e.enroll_status,
-            e.cohort,
-            e.gender,
-            e.ethnicity,
-            e.dob,
-            e.lunch_status,
-            e.lep_status,
-            e.gifted_and_talented,
-            e.iep_status,
-            e.is_504,
-            e.is_homeless,
-            e.is_out_of_district,
-            e.is_self_contained,
-            e.is_counseling_services,
-            e.is_student_athlete,
-            e.is_tutoring,
-            e.year_in_network,
-            e.has_fafsa,
-            e.college_match_gpa,
-            e.college_match_gpa_bands,
-            e.contact_owner_name,
-            e.ktc_cohort,
-            e.ms_attended,
-            e.hos,
-            e.`ada`,
-            e.ada_above_or_at_80,
-            e.advisory,
-
-            sp.requiredcredits as plan_required_credits,
-            sp.enrolledcredits as plan_enrolled_credits,
-            sp.requestedcredits as plan_requested_credits,
-            sp.earnedcredits as plan_earned_credits,
-            sp.waivedcredits as plan_waived_credits,
-
-            sd.requiredcredits as discipline_required_credits,
-            sd.enrolledcredits as discipline_enrolled_credits,
-            sd.requestedcredits as discipline_requested_credits,
-            sd.earnedcredits as discipline_earned_credits,
-            sd.waivedcredits as discipline_waived_credits,
-
-            ss.requiredcredits as subject_required_credits,
-            ss.enrolledcredits as subject_enrolled_credits,
-            ss.requestedcredits as subject_requested_credits,
-            ss.earnedcredits as subject_earned_credits,
-            ss.waivedcredits as subject_waived_credits,
-
-            gty.gpa_y1,
-
-            if(
-                g.academic_year = {{ var("current_academic_year") }}, true, false
-            ) as is_current_academic_year,
-
-            if(
-                g.academic_year = {{ var("current_academic_year") - 1 }}, true, false
-            ) as is_previous_academic_year,
-
-        from {{ ref("int_powerschool__gpprogress_grades") }} as g
-        left join
-            {{ ref("int_extracts__student_enrollments") }} as e
-            on g.studentsdcid = e.students_dcid
-            and {{ union_dataset_join_clause(left_alias="g", right_alias="e") }}
-            and e.academic_year = {{ var("current_academic_year") }}
-        left join
-            {{ ref("int_powerschool__gpprogresssubject") }} as sp
-            on g.studentsdcid = sp.studentsdcid
-            and g.plan_id = sp.id
-            and {{ union_dataset_join_clause(left_alias="g", right_alias="sp") }}
-            and sp.degree_plan_section = 'Plan'
-        left join
-            {{ ref("int_powerschool__gpprogresssubject") }} as sd
-            on g.studentsdcid = sd.studentsdcid
-            and g.discipline_id = sd.id
-            and {{ union_dataset_join_clause(left_alias="g", right_alias="sd") }}
-            and sd.degree_plan_section = 'Discipline'
-        left join
-            {{ ref("int_powerschool__gpprogresssubject") }} as ss
-            on g.studentsdcid = ss.studentsdcid
-            and g.subject_id = ss.id
-            and {{ union_dataset_join_clause(left_alias="g", right_alias="ss") }}
-            and ss.degree_plan_section = 'Subject'
-        left join
-            {{ ref("int_powerschool__gpa_term") }} as gty
-            on e.studentid = gty.studentid
-            and e.yearid = gty.yearid
-            and e.schoolid = gty.schoolid
-            and {{ union_dataset_join_clause(left_alias="e", right_alias="gty") }}
-            and gty.is_current
-    ),
-
-    yearly_credits as (
-        select
-            *,
-
-            sum(earned_credits) over (
-                partition by academic_year, student_number, plan_id
-            ) as academic_year_credits_earned,
-
-            sum(potential_credits) over (
-                partition by academic_year, student_number, plan_id
-            ) as academic_year_credits_potential,
-
-        from credits
-    ),
-
-    custom_yearly_credits as (
+    ae_status as (
         select
             _dbt_source_relation,
-            student_number,
-            plan_id,
-
-            avg(
-                case
-                    when is_previous_academic_year then academic_year_credits_earned
-                end
-            ) as earned_credits_previous_year,
-
-            avg(
-                case when is_current_academic_year then academic_year_credits_earned end
-            ) as earned_credits_current_year,
-
-            avg(
-                case
-                    when is_current_academic_year
-                    then (0.5 * academic_year_credits_earned)
-                end
-            ) as half_earned_credits_current_year,
-
-            avg(
-                case
-                    when is_current_academic_year then academic_year_credits_potential
-                end
-            ) as potential_credits_current_year,
-
-        from yearly_credits
-        group by _dbt_source_relation, student_number, plan_id
+            studentid,
+            q1_ae_status,
+            q2_ae_status,
+            q3_ae_status,
+            q4_ae_status,
+        from
+            {{ ref("int_students__athletic_eligibility") }} pivot (
+                max(quarter_athletic_eligibility_status) for term in (
+                    'Q1' as q1_ae_status,
+                    'Q2' as q2_ae_status,
+                    'Q3' as q3_ae_status,
+                    'Q4' as q4_ae_status
+                )
+            )
     )
 
 select
-    y.*,
+    g._dbt_source_relation,
+    g.academic_year,
+    g.studentsdcid,
+    g.plan_id,
+    g.plan_name,
+    g.discipline_id,
+    g.discipline_name,
+    g.subject_id,
+    g.subject_name,
+    g.teacher_name,
+    g.course_number,
+    g.course_name,
+    g.sectionid,
+    g.credit_type,
+    g.letter_grade,
+    g.is_transfer_grade,
+    g.credit_status,
+    g.earned_credits,
+    g.potential_credits,
 
-    c.earned_credits_previous_year,
-    c.earned_credits_current_year,
-    c.half_earned_credits_current_year,
-    c.potential_credits_current_year,
+    e.region,
+    e.schoolid,
+    e.school,
+    e.studentid,
+    e.state_studentnumber,
+    e.salesforce_id,
+    e.student_number,
+    e.student_name,
+    e.grade_level,
+    e.student_email,
+    e.enroll_status,
+    e.cohort,
+    e.gender,
+    e.ethnicity,
+    e.dob,
+    e.lunch_status,
+    e.lep_status,
+    e.gifted_and_talented,
+    e.iep_status,
+    e.is_504,
+    e.is_homeless,
+    e.is_out_of_district,
+    e.is_self_contained,
+    e.is_counseling_services,
+    e.is_student_athlete,
+    e.is_tutoring,
+    e.year_in_network,
+    e.has_fafsa,
+    e.college_match_gpa,
+    e.college_match_gpa_bands,
+    e.contact_owner_name,
+    e.ktc_cohort,
+    e.ms_attended,
+    e.hos,
+    e.`ada`,
+    e.ada_above_or_at_80,
+    e.advisory,
 
-    {{ var("current_academic_year") }} as current_academic_year,
+    sp.requiredcredits as plan_required_credits,
+    sp.enrolledcredits as plan_enrolled_credits,
+    sp.requestedcredits as plan_requested_credits,
+    sp.earnedcredits as plan_earned_credits,
+    sp.waivedcredits as plan_waived_credits,
 
-    if(
-        potential_credits_current_year / 2 = c.half_earned_credits_current_year,
-        true,
-        false
-    ) as is_cy_credits_on_track,
+    sd.requiredcredits as discipline_required_credits,
+    sd.enrolledcredits as discipline_enrolled_credits,
+    sd.requestedcredits as discipline_requested_credits,
+    sd.earnedcredits as discipline_earned_credits,
+    sd.waivedcredits as discipline_waived_credits,
 
-from yearly_credits as y
-inner join
-    custom_yearly_credits as c
-    on y.student_number = c.student_number
-    and y.plan_id = c.plan_id
-    and {{ union_dataset_join_clause(left_alias="y", right_alias="c") }}
+    ss.requiredcredits as subject_required_credits,
+    ss.enrolledcredits as subject_enrolled_credits,
+    ss.requestedcredits as subject_requested_credits,
+    ss.earnedcredits as subject_earned_credits,
+    ss.waivedcredits as subject_waived_credits,
+
+    ae.q1_ae_status,
+    ae.q2_ae_status,
+    ae.q3_ae_status,
+    ae.q4_ae_status,
+
+from {{ ref("int_powerschool__gpprogress_grades") }} as g
+left join
+    {{ ref("int_extracts__student_enrollments") }} as e
+    on g.studentsdcid = e.students_dcid
+    and {{ union_dataset_join_clause(left_alias="g", right_alias="e") }}
+    and e.academic_year = {{ var("current_academic_year") }}
+left join
+    {{ ref("int_powerschool__gpprogresssubject") }} as sp
+    on g.studentsdcid = sp.studentsdcid
+    and g.plan_id = sp.id
+    and {{ union_dataset_join_clause(left_alias="g", right_alias="sp") }}
+    and sp.degree_plan_section = 'Plan'
+left join
+    {{ ref("int_powerschool__gpprogresssubject") }} as sd
+    on g.studentsdcid = sd.studentsdcid
+    and g.discipline_id = sd.id
+    and {{ union_dataset_join_clause(left_alias="g", right_alias="sd") }}
+    and sd.degree_plan_section = 'Discipline'
+left join
+    {{ ref("int_powerschool__gpprogresssubject") }} as ss
+    on g.studentsdcid = ss.studentsdcid
+    and g.subject_id = ss.id
+    and {{ union_dataset_join_clause(left_alias="g", right_alias="ss") }}
+    and ss.degree_plan_section = 'Subject'
+left join
+    ae_status as ae
+    on e.studentid = ae.studentid
+    and {{ union_dataset_join_clause(left_alias="e", right_alias="ae") }}
 where
-    y.plan_name in ('NJ State Diploma', 'HS Distinction Diploma')
-    and y.enroll_status = 0
+    g.plan_name in ('NJ State Diploma', 'HS Distinction Diploma')
+    and e.enroll_status = 0
---
+++ src/dbt/kipptaf/models/students/intermediate/int_students__athletic_eligibility.sql
@@ -0,0 +1,293 @@
+with
+    credits as (
+        select
+            _dbt_source_relation,
+            studentsdcid,
+            studentid,
+            academic_year,
+            cc_dcid,
+            course_number,
+            is_transfer_grade,
+            credit_status,
+            official_potential_credits,
+
+            if(earned_credits = 0.0, 0.0, official_potential_credits) as credits_earned,
+
+            if(
+                academic_year = {{ var("current_academic_year") }}, true, false
+            ) as is_current_academic_year,
+
+            if(
+                academic_year = {{ var("current_academic_year") - 1 }}, true, false
+            ) as is_previous_academic_year,
+
+        from {{ ref("int_powerschool__gpprogress_grades") }}
+        where plan_name = 'NJ State Diploma'
+    ),
+
+    yearly_credits as (
+        select
+            *,
+
+            sum(credits_earned) over (
+                partition by _dbt_source_relation, academic_year, studentsdcid
+            ) as academic_year_credits_earned,
+
+            sum(official_potential_credits) over (
+                partition by _dbt_source_relation, academic_year, studentsdcid
+            ) as academic_year_credits_potential,
+
+        from credits
+    ),
+
+    custom_yearly_credits as (
+        select
+            _dbt_source_relation,
+            studentsdcid,
+
+            avg(
+                case
+                    when is_previous_academic_year then academic_year_credits_earned
+                end
+            ) as earned_credits_previous_year,
+
+            avg(
+                case when is_current_academic_year then academic_year_credits_earned end
+            ) as earned_credits_current_year,
+
+            avg(
+                case
+                    when is_current_academic_year
+                    then (0.5 * academic_year_credits_earned)
+                end
+            ) as half_earned_credits_current_year,
+
+            avg(
+                case
+                    when is_current_academic_year then academic_year_credits_potential
+                end
+            ) as potential_credits_current_year,
+
+        from yearly_credits
+        group by _dbt_source_relation, studentsdcid
+    ),
+
+    credits_component as (
+        select
+            c._dbt_source_relation,
+            c.academic_year,
+            c.studentsdcid,
+            c.studentid,
+
+            cy.earned_credits_previous_year,
+            cy.earned_credits_current_year,
+            cy.half_earned_credits_current_year,
+            cy.potential_credits_current_year,
+
+            c.academic_year - 1990 as yearid,
+
+            if(
+                cy.potential_credits_current_year / 2
+                = cy.half_earned_credits_current_year,
+                true,
+                false
+            ) as is_cy_credits_on_track,
+
+        from credits as c
+        inner join
+            custom_yearly_credits as cy
+            on c.studentsdcid = cy.studentsdcid
+            and {{ union_dataset_join_clause(left_alias="c", right_alias="cy") }}
+        where c.academic_year = {{ var("current_academic_year") }}
+    ),
+
+    term as (
+        select
+            t._dbt_source_relation,
+            t.schoolid,
+            t.yearid,
+
+            tb.storecode as term,
+            tb.date1 as term_start_date,
+            tb.date2 as term_end_date,
+
+            t.yearid + 1990 as academic_year,
+
+            if(
+                current_date('{{ var("local_timezone") }}')
+                between tb.date1 and tb.date2,
+                true,
+                false
+            ) as is_current_term,
+
+            case
+                when tb.storecode in ('Q1', 'Q2')
+                then 'S1'
+                when tb.storecode in ('Q3', 'Q4')
+                then 'S2'
+            end as semester,
+
+        from {{ ref("stg_powerschool__terms") }} as t
+        inner join
+            {{ ref("stg_powerschool__termbins") }} as tb
+            on t.id = tb.termid
+            and t.schoolid = tb.schoolid
+            and {{ union_dataset_join_clause(left_alias="t", right_alias="tb") }}
+            and tb.storecode in ('Q1', 'Q2', 'Q3', 'Q4')
+        where t.isyearrec = 1
+    ),
+
+    membership_days as (
+        select
+            a._dbt_source_relation,
+            a.yearid,
+            a.studentid,
+            a.schoolid,
+            a.membershipvalue,
+            a.attendancevalue,
+
+            t.academic_year,
+            t.semester,
+            t.term,
+
+        from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }} as a
+        inner join
+            term as t
+            on a.yearid = t.yearid
+            and a.schoolid = t.schoolid
+            and {{ union_dataset_join_clause(left_alias="a", right_alias="t") }}
+            and a.calendardate >= t.term_start_date
+            and a.calendardate <= t.term_end_date
+        where
+            a.membershipvalue = 1
+            and a.calendardate <= current_date('{{ var("local_timezone") }}')
+    ),
+
+    ada_component as (
+
+        select
+            _dbt_source_relation,
+            studentid,
+            academic_year,
+            yearid,
+            semester,
+            term,
+
+            sum(membershipvalue) as days_in_membership,
+            sum(attendancevalue) as days_present,
+            sum(abs(attendancevalue - 1)) as days_absent_unexcused,
+            avg(attendancevalue) as `ada`,
+
+        from membership_days
+        group by _dbt_source_relation, academic_year, yearid, studentid, semester, term
+    )
+
+select
+    c._dbt_source_relation,
+    c.academic_year,
+    c.studentsdcid,
+    c.studentid,
+    c.earned_credits_previous_year,
+    c.earned_credits_current_year,
+    c.half_earned_credits_current_year,
+    c.potential_credits_current_year,
+    c.is_cy_credits_on_track,
+
+    a.semester,
+    a.term,
+    a.`ada`,
+
+    g.gpa_term,
+
+    case
+        -- semester 1 conditions
+        when
+            a.semester = 'S1'
+            and c.earned_credits_previous_year >= 30
+            and gpa_term >= 2.5
+            and a.`ada` >= .9
+        then 'Eligible'
+        when
+            a.semester = 'S1'
+            and c.earned_credits_previous_year >= 30
+            and g.gpa_term >= 2.5
+            and a.`ada` < .9
+        then 'Probabtion - ADA'
+        when
+            a.semester = 'S1'
+            and c.earned_credits_previous_year >= 30
+            and (g.gpa_term >= 2.2 and g.gpa_term <= 2.4)
+            and a.`ada` >= .9
+        then 'Probation - GPA'
+        when
+            a.semester = 'S1'
+            and c.earned_credits_previous_year >= 30
+            and (g.gpa_term >= 2.2 and g.gpa_term <= 2.4)
+            and a.`ada` < .9
+        then 'Probation - GPA and ADA'
+        when
+            a.semester = 'S1'
+            and c.earned_credits_previous_year < 30
+            and gpa_term >= 2.5
+            and a.`ada` >= .9
+        then 'Ineligible - Credits'
+        when
+            a.semester = 'S1'
+            and c.earned_credits_previous_year >= 30
+            and g.gpa_term < 2.2
+            and a.`ada` >= .9
+        then 'Ineligible - GPA'
+
+        -- semester 2 conditions
+        when
+            a.semester = 'S2'
+            and c.is_cy_credits_on_track
+            and gpa_term >= 2.5
+            and a.`ada` >= .9
+        then 'Eligible'
+        when
+            a.semester = 'S2'
+            and c.is_cy_credits_on_track
+            and g.gpa_term >= 2.5
+            and a.`ada` < .9
+        then 'Probabtion - ADA'
+        when
+            a.semester = 'S2'
+            and c.is_cy_credits_on_track
+            and (g.gpa_term >= 2.2 and g.gpa_term <= 2.4)
+            and a.`ada` >= .9
+        then 'Probation - GPA'
+        when
+            a.semester = 'S2'
+            and not c.is_cy_credits_on_track
+            and (g.gpa_term >= 2.2 and g.gpa_term <= 2.4)
+            and a.`ada` < .9
+        then 'Probation - GPA and ADA'
+        when
+            a.semester = 'S2'
+            and not c.is_cy_credits_on_track
+            and gpa_term >= 2.5
+            and a.`ada` >= .9
+        then 'Ineligible - Credits'
+        when
+            a.semester = 'S2'
+            and c.is_cy_credits_on_track
+            and g.gpa_term < 2.2
+            and a.`ada` >= .9
+        then 'Ineligible - GPA'
+
+        else 'New combination'
+    end as quarter_athletic_eligibility_status,
+
+from credits_component as c
+left join
+    ada_component as a
+    on c.studentid = a.studentid
+    and a.academic_year = c.academic_year
+    and {{ union_dataset_join_clause(left_alias="c", right_alias="a") }}
+left join
+    {{ ref("int_powerschool__gpa_term") }} as g
+    on a.studentid = g.studentid
+    and a.yearid = g.yearid
+    and a.term = g.term_name
+    and {{ union_dataset_join_clause(left_alias="a", right_alias="g") }}
