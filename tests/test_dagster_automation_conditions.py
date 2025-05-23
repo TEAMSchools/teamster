a0edfbfe8056357036bfb0bc154ded0d9cb02427
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-05-23 20:38:05 +0000

    test

--- tests/test_dagster_automation_conditions.py
+++ tests/test_dagster_automation_conditions.py
@@ -3,6 +3,7 @@ from typing import AbstractSet, Any, Generic, Sequence
 
 from dagster import (
     AssetKey,
+    AssetsDefinition,
     AssetSelection,
     AutomationCondition,
     AutomationContext,
@@ -14,6 +15,7 @@ from dagster import (
 )
 from dagster._annotations import public
 from dagster._core.asset_graph_view.asset_graph_view import U_EntityKey
+from dagster._core.definitions.asset_graph import AssetNode
 from dagster._core.definitions.asset_key import T_EntityKey
 from dagster._core.definitions.base_asset_graph import BaseAssetGraph, BaseAssetNode
 from dagster._core.definitions.declarative_automation.automation_condition import (
@@ -113,14 +115,15 @@ class DbtTableDepsAutomationCondition(BuiltinAutomationCondition[T_EntityKey]):
     def _get_dep_keys(
         self, key: T_EntityKey, asset_graph: BaseAssetGraph[BaseAssetNode]
     ) -> AbstractSet[AssetKey]:
-        dep_keys = asset_graph.get(key).parent_entity_keys
-
+        # trunk-ignore(pyright/reportArgumentType)
+        dep_keys = asset_graph.get_ancestor_asset_keys(key)
         if self.allow_selection is not None:
             dep_keys &= self.allow_selection.resolve(asset_graph, allow_missing=True)
 
         if self.ignore_selection is not None:
             dep_keys -= self.ignore_selection.resolve(asset_graph, allow_missing=True)
 
+        print(dep_keys)
         return dep_keys
 
     @public
@@ -193,7 +196,6 @@ class DbtTableAnyDepsCondition(DbtTableDepsAutomationCondition[T_EntityKey]):
     #     if asset_def.metadata_by_key[key]["dagster-dbt/materialization_type"] == "view":
     #         return set()
 
-    #     # TODO: follow the graph to the closest dbt table ancestors
     #     dep_keys = {
     #         a.key
     #         for a in asset_graph.asset_nodes
@@ -279,27 +281,6 @@ class DbtTableEntityMatchesCondition(
 
 
 class DbtTableAutomationCondition(AutomationCondition):
-    # @public
-    # @staticmethod
-    # def custom_any_deps_match(
-    #     condition: "DbtTableAutomationCondition",
-    # ) -> DbtTableDepsAutomationCondition:
-    #     return DbtTableAnyDepsCondition(operand=condition)
-
-    # @public
-    # @staticmethod
-    # def custom_any_deps_updated() -> DbtTableDepsAutomationCondition:
-    #     return DbtTableAutomationCondition.custom_any_deps_match(
-    #         (
-    #             DbtTableAutomationCondition.newly_updated()
-    #             # executed_with_root_target is fairly expensive on a per-partition
-    #             # basis, but newly_updated is bounded in the number of partitions that
-    #             # might be updated on a single tick
-    #             & ~DbtTableAutomationCondition.executed_with_root_target()
-    #         ).with_label("newly_updated_without_root")
-    #         | DbtTableAutomationCondition.will_be_requested()
-    #     ).with_label("any_deps_updated")
-
     @public
     @staticmethod
     # trunk-ignore(pyright/reportIncompatibleMethodOverride)
@@ -334,7 +315,7 @@ class DbtTableAutomationCondition(AutomationCondition):
         ).with_label("any_deps_updated")
 
 
-test_automation_condition = (
+test_table_automation_condition = (
     DbtTableAutomationCondition.in_latest_time_window()
     & (
         DbtTableAutomationCondition.newly_missing()
@@ -345,27 +326,38 @@ test_automation_condition = (
     & ~DbtTableAutomationCondition.in_progress()
 )
 
+test_view_automation_condition = (
+    AutomationCondition.in_latest_time_window()
+    & (
+        AutomationCondition.newly_missing() | AutomationCondition.code_version_changed()
+    ).since(AutomationCondition.newly_requested() | AutomationCondition.newly_updated())
+    & ~AutomationCondition.any_deps_missing()
+    & ~AutomationCondition.any_deps_in_progress()
+    & ~AutomationCondition.in_progress()
+).ignore(AssetSelection.all(include_sources=True).sources())
+
 
 def test_foo():
     @asset(
-        automation_condition=test_automation_condition,
-        metadata={"dagster-dbt/materialization_type": "table"},
+        automation_condition=test_table_automation_condition,
+        # metadata={"dagster-dbt/materialization_type": "table"},
     )
     def upstream_table():
         return
 
     @asset(
         deps=[upstream_table],
-        automation_condition=test_automation_condition,
-        metadata={"dagster-dbt/materialization_type": "view"},
+        automation_condition=test_view_automation_condition,
+        # automation_condition=test_table_automation_condition,
+        # metadata={"dagster-dbt/materialization_type": "view"},
     )
     def intermediate_view():
         return
 
     @asset(
         deps=[intermediate_view],
-        automation_condition=test_automation_condition,
-        metadata={"dagster-dbt/materialization_type": "table"},
+        automation_condition=test_table_automation_condition,
+        # metadata={"dagster-dbt/materialization_type": "table"},
     )
     def downstream_table():
         return
