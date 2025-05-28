0620fd3b1cae512ca327103490fefa6f5105c364
Gaby Rangel <grangel@kippteamandfamily.org>
2025-05-28 19:59:02 +0000

    i know it will fail but i need to swap branches

--- src/dbt/kipptaf/models/students/intermediate/int_students__athletic_eligibility.sql
+++ src/dbt/kipptaf/models/students/intermediate/int_students__athletic_eligibility.sql
@@ -1,5 +1,69 @@
 with
-    gpa_and_grade_level as (
+    credits as (
+        select
+            _dbt_source_relation,
+            studentsdcid,
+            studentid,
+            academic_year,
+            cc_dcid,
+            credit_status,
+            official_potential_credits,
+
+            if(earned_credits = 0.0, 0.0, official_potential_credits) as credits_earned,
+
+            if(
+                academic_year = {{ var("current_academic_year") }}, true, false
+            ) as is_current_academic_year,
+
+            if(
+                academic_year = {{ var("current_academic_year") - 1 }}, true, false
+            ) as is_previous_academic_year,
+
+        from {{ ref("int_powerschool__gpprogress_grades") }}
+        where plan_name = 'NJ State Diploma'
+    ),
+
+    yearly_credits as (
+        select
+            *,
+
+            sum(credits_earned) over (
+                partition by _dbt_source_relation, academic_year, studentsdcid
+            ) as academic_year_credits_earned,
+
+            sum(official_potential_credits) over (
+                partition by _dbt_source_relation, academic_year, studentsdcid
+            ) as academic_year_credits_potential,
+
+        from credits
+    ),
+
+    custom_yearly_credits as (
+        select
+            _dbt_source_relation,
+            studentsdcid,
+
+            avg(
+                case
+                    when is_previous_academic_year then academic_year_credits_earned
+                end
+            ) as earned_credits_previous_year,
+
+            avg(
+                case when is_current_academic_year then academic_year_credits_earned end
+            ) as earned_credits_current_year,
+
+            avg(
+                case
+                    when is_current_academic_year then academic_year_credits_potential
+                end
+            ) as potential_credits_current_year,
+
+        from yearly_credits
+        group by _dbt_source_relation, studentsdcid
+    ),
+
+    base as (
         select
             e._dbt_source_relation,
             e.academic_year,
@@ -10,11 +74,16 @@ with
             e.grade_level,
             e.grade_level_prev,
 
-            term,
+            a.ada_term as cy_q1_ada,
+            a.ada_semester as cy_s1_ada,
 
-            g.gpa_term,
-            gc.gpa_y1 as q2_y1_gpa,
-            gp.gpa_y1 as py_gpa,
+            apy.ada_year as py_y1_ada,
+
+            gq1.gpa_term as cy_q1_gpa,
+
+            gs1.gpa_y1 as cy_s1_gpa,
+
+            gp.gpa_y1 as py_y1_gpa,
 
             if(
                 e.grade_level = 9
@@ -24,24 +93,35 @@ with
             ) as is_first_time_ninth,
 
         from {{ ref("int_extracts__student_enrollments") }} as e
-        cross join unnest(['Q1', 'Q2']) as term
         left join
-            {{ ref("int_powerschool__gpa_term") }} as g
-            on e.studentid = g.studentid
-            and e.yearid = g.yearid
-            and term = g.term_name
-            and {{ union_dataset_join_clause(left_alias="e", right_alias="g") }}
+            {{ ref("int_powerschool__ada_term") }} as a
+            on e.academic_year = a.academic_year
+            and e.studentid = a.studentid
+            and {{ union_dataset_join_clause(left_alias="e", right_alias="a") }}
+            and a.term = 'Q1'
         left join
-            {{ ref("int_powerschool__gpa_term") }} as gc
-            on g.studentid = gc.studentid
-            and g.yearid = gc.yearid
-            and {{ union_dataset_join_clause(left_alias="g", right_alias="gc") }}
-            and gc.term_name = 'Q2'
+            {{ ref("int_powerschool__ada_term") }} as apy
+            on e.academic_year - 1 = apy.academic_year
+            and e.studentid = apy.studentid
+            and {{ union_dataset_join_clause(left_alias="e", right_alias="apy") }}
+            and apy.term = 'Q4'
+        left join
+            {{ ref("int_powerschool__gpa_term") }} as gq1
+            on e.studentid = gq1.studentid
+            and e.yearid = gq1.yearid
+            and {{ union_dataset_join_clause(left_alias="e", right_alias="gq1") }}
+            and gq1.term = 'Q1'
+        left join
+            {{ ref("int_powerschool__gpa_term") }} as gs1
+            on e.studentid = gs1.studentid
+            and e.yearid = gs1.yearid
+            and {{ union_dataset_join_clause(left_alias="e", right_alias="gs1") }}
+            and gs1.term_name = 'Q2'
         left join
             {{ ref("int_powerschool__gpa_term") }} as gp
-            on g.studentid = gp.studentid
-            and g.yearid - 1 = gp.yearid
-            and {{ union_dataset_join_clause(left_alias="g", right_alias="gp") }}
+            on e.studentid = gp.studentid
+            and e.yearid - 1 = gp.yearid
+            and {{ union_dataset_join_clause(left_alias="e", right_alias="gp") }}
             and gp.is_current
         where
             e.enroll_status = 0
@@ -96,79 +176,6 @@ with
             end as s2_gpa_eligibility_status,
 
         from gpa_and_grade_level pivot (avg(gpa_term) for term in ('Q1', 'Q2'))
-    ),
-
-    credits as (
-        select
-            _dbt_source_relation,
-            studentsdcid,
-            studentid,
-            academic_year,
-            cc_dcid,
-            course_number,
-            is_transfer_grade,
-            credit_status,
-            official_potential_credits,
-
-            if(earned_credits = 0.0, 0.0, official_potential_credits) as credits_earned,
-
-            if(
-                academic_year = {{ var("current_academic_year") }}, true, false
-            ) as is_current_academic_year,
-
-            if(
-                academic_year = {{ var("current_academic_year") - 1 }}, true, false
-            ) as is_previous_academic_year,
-
-        from {{ ref("int_powerschool__gpprogress_grades") }}
-        where plan_name = 'NJ State Diploma'
-    ),
-
-    yearly_credits as (
-        select
-            *,
-
-            sum(credits_earned) over (
-                partition by _dbt_source_relation, academic_year, studentsdcid
-            ) as academic_year_credits_earned,
-
-            sum(official_potential_credits) over (
-                partition by _dbt_source_relation, academic_year, studentsdcid
-            ) as academic_year_credits_potential,
-
-        from credits
-    ),
-
-    custom_yearly_credits as (
-        select
-            _dbt_source_relation,
-            studentsdcid,
-
-            avg(
-                case
-                    when is_previous_academic_year then academic_year_credits_earned
-                end
-            ) as earned_credits_previous_year,
-
-            avg(
-                case when is_current_academic_year then academic_year_credits_earned end
-            ) as earned_credits_current_year,
-
-            avg(
-                case
-                    when is_current_academic_year
-                    then (0.5 * academic_year_credits_earned)
-                end
-            ) as half_earned_credits_current_year,
-
-            avg(
-                case
-                    when is_current_academic_year then academic_year_credits_potential
-                end
-            ) as potential_credits_current_year,
-
-        from yearly_credits
-        group by _dbt_source_relation, studentsdcid
     ),
 
     credits_component as (
