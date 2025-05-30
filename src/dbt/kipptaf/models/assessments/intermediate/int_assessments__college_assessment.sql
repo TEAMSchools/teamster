b50131740be582411b99a738e5247a0f378aaed1
Gaby Rangel <grangel@kippteamandfamily.org>
2025-05-30 18:37:04 +0000

    custom admin seasons

--- src/dbt/kipptaf/models/assessments/intermediate/int_assessments__college_assessment.sql
+++ src/dbt/kipptaf/models/assessments/intermediate/int_assessments__college_assessment.sql
@@ -14,6 +14,17 @@ select
     'Official' as test_type,
     null as salesforce_id,
 
+    case
+        when format_date('%B', latest_psat_date) in ('August', 'September', 'October')
+        then 'BOY'
+        when
+            format_date('%B', latest_psat_date)
+            in ('November', 'December', 'January', 'February', 'March')
+        then 'MOY'
+        when format_date('%B', latest_psat_date) in ('April', 'May', 'June', 'July')
+        then 'EOY'
+    end as admin_season
+
 from {{ ref("int_collegeboard__psat_unpivot") }}
 
 union all
@@ -33,6 +44,17 @@ select
     format_date('%B', `date`) as test_month,
     'Official' as test_type,
     contact as salesforce_id,
+
+    case
+        when format_date('%B', `date`) in ('August', 'September')
+        then 'BOY'
+        when
+            format_date('%B', `date`)
+            in ('October', 'November', 'December', 'January', 'February', 'March')
+        then 'MOY'
+        when format_date('%B', `date`) in ('April', 'May', 'June', 'July')
+        then 'EOY'
+    end as admin_season
 
 from {{ ref("int_kippadb__standardized_test_unpivot") }}
 where
