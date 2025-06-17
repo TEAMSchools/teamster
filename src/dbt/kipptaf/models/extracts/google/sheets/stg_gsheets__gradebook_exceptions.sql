afbb3a8c081105aa6476a62207c2d00d5b1098a2
Gaby Rangel <grangel@kippteamandfamily.org>
2025-06-17 16:20:21 +0000

    moved gradebook reference tables to new g-sheet

---
+++ src/dbt/kipptaf/models/extracts/google/sheets/stg_gsheets__gradebook_exceptions.sql
@@ -0,0 +1,1 @@
+select *, from {{ source("google", "src_gsheets__gradebook_exceptions") }}
---
+++ src/dbt/kipptaf/models/extracts/google/sheets/stg_gsheets__gradebook_expectations_assignments.sql
@@ -0,0 +1,17 @@
+select
+    *,
+
+    concat(assignment_category_code, right(`quarter`, 1)) as assignment_category_term,
+
+    case
+        assignment_category_code
+        when 'W'
+        then 'Work Habits'
+        when 'F'
+        then 'Formative Mastery'
+        when 'S'
+        then 'Summative Mastery'
+    end as assignment_category_name,
+from
+    {{ source("google", "src_gsheets__gradebook_expectations_assignments") }}
+    unpivot (expectation for assignment_category_code in (`W`, `F`, `S`))
