a37091a564d4ebb6062dece256c5e5d311c12287
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-06-24 16:38:43 +0000

    revert: col drops

--- src/dbt/kipptaf/models/adp/workforce_now/api/intermediate/int_adp_workforce_now__workers__custom_fields__pivot.sql
+++ src/dbt/kipptaf/models/adp/workforce_now/api/intermediate/int_adp_workforce_now__workers__custom_fields__pivot.sql
@@ -15,20 +15,29 @@ with
 select
     associate_oid,
 
+    date_value_wfmgrloareturndate as wf_mgr_loa_return_date,
+
     indicator_value_receivedsignonbonus as received_sign_on_bonus,
+    indicator_value_wfmgrloa as wf_mgr_loa,
 
     string_value_miamiacesnumber as miami_aces_number,
     string_value_njpensionnumber as nj_pension_number,
+    string_value_wfmgrtrigger as wf_mgr_trigger,
 
     code_value_preferredraceethnicity as preferred_race_ethnicity,
     code_value_remoteworkstatus as remote_work_status,
     code_value_teacherprepprogram as teacher_prep_program,
+    code_value_wfmgraccrualprofile as wf_mgr_accrual_profile,
+    code_value_wfmgreetype as wf_mgr_ee_type,
+    code_value_wfmgrhomehyperfind as wf_mgr_home_hyperfind,
+    code_value_wfmgrpayrule as wf_mgr_pay_rule,
     code_value_lifeexperienceincommunitiesweserve
     as life_experience_in_communities_we_serve,
     code_value_professionalexperienceincommunitiesweserve
     as professional_experience_in_communities_we_serve,
 
     safe_cast(string_value_employeenumber as int) as employee_number,
+    safe_cast(string_value_wfmgrbadgenumber as int) as wf_mgr_badge_number,
 from
     from_item pivot (
         max(date_value) as date_value,
@@ -45,6 +54,14 @@ from
             'ProfessionalExperienceinCommunitiesWeServe',
             'ReceivedSignOnBonus',
             'RemoteWorkStatus',
-            'TeacherPrepProgram'
+            'TeacherPrepProgram',
+            'WFMgrAccrualProfile',
+            'WFMgrBadgeNumber',
+            'WFMgrEEType',
+            'WFMgrHomeHyperfind',
+            'WFMgrLOA',
+            'WFMgrLOAReturnDate',
+            'WFMgrPayRule',
+            'WFMgrTrigger'
         )
     )
