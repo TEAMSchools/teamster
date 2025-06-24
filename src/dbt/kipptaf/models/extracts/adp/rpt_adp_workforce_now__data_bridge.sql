f51e6e3aea110c91c5feb8c12048270f666e2874
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-06-24 16:26:59 +0000

    feat: complete data bridge import

--- src/dbt/kipptaf/models/extracts/adp/rpt_adp_workforce_now__data_bridge.sql
+++ src/dbt/kipptaf/models/extracts/adp/rpt_adp_workforce_now__data_bridge.sql
@@ -3,21 +3,72 @@ with
         select distinct reports_to_position_id,
         from {{ ref("int_people__staff_roster") }}
         where reports_to_position_id is not null
+    ),
+
+    staff_roster as (
+        select
+            sr.position_id,
+            sr.employee_number,
+            sr.mail,
+            sr.reports_to_position_id,
+
+            sr.custom_field__employee_number as adp__custom_field__employee_number,
+            sr.work_email as adp__work_email,
+
+            {# TODO: calculate using ??? #}
+            null as pay_class,
+            {# TODO: add from API when available #}
+            null as adp__badge,
+            null as adp__supervisor_id,
+            null as adp__supervisor_flag,
+            null as adp__pay_class,
+
+            if(s.reports_to_position_id is not null, 'Y', 'N') as supervisor_flag,
+        from {{ ref("int_people__staff_roster") }} as sr
+        left join supervisors as s on sr.position_id = s.reports_to_position_id
+    ),
+
+    surrogate_keys as (
+        select
+            *,
+
+            {{
+                dbt_utils.generate_surrogate_key(
+                    [
+                        "employee_number",
+                        "mail",
+                        "employee_number",
+                        "reports_to_position_id",
+                        "supervisor_flag",
+                        "pay_class",
+                    ]
+                )
+            }} as source_surrogate_key,
+
+            {{
+                dbt_utils.generate_surrogate_key(
+                    [
+                        "adp__custom_field__employee_number",
+                        "adp__work_email",
+                        "adp__badge",
+                        "adp__supervisor_id",
+                        "adp__supervisor_flag",
+                        "adp__pay_class",
+                    ]
+                )
+            }} as destination_surrogate_key,
+        from staff_roster
     )
 
 select
     -- trunk-ignore-begin(sqlfluff/RF05)
-    sr.worker_id as `Employee ID`,
-    sr.position_id as `Position ID`,
-    sr.employee_number as `Badge`,
-    sr.mail as `Work E-mail`,
-    sr.reports_to_position_id as `Supervisorid`,
-    sr.employee_number as `CDF Value`,
-    null as `Payclass`,
-
-    sr.custom_field__employee_number as adp__custom_field__employee_number,
-    sr.work_email as adp__work_email,
-    null as adp__badge,
+    position_id as `Position ID`,
+    mail as `Work E-mail`,
+    employee_number as `CDF Value`,
+    employee_number as `Badge`,
+    reports_to_position_id as `Supervisorid`,
+    supervisor_flag as `Supervisorflag`,
+    pay_class as `Payclass`,
 
     'Employment Custom Fields' as `CDF Category`,
     'Employee Number' as `CDF Label`,
@@ -29,8 +80,6 @@ select
     format_date(
         '%m/%d/%Y', current_date('{{ var("local_timezone") }}')
     ) as `Change Effective On`,
-
-    if(s.reports_to_position_id is not null, 'Y', 'N') as `Supervisorflag`,
 -- trunk-ignore-end(sqlfluff/RF05)
-from {{ ref("int_people__staff_roster") }} as sr
-left join supervisors as s on sr.position_id = s.reports_to_position_id
+from surrogate_keys
+where source_surrogate_key != destination_surrogate_key
