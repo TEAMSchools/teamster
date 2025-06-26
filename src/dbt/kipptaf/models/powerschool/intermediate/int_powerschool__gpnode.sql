2fe7579a9f035550b27b4838e6ce216b6f204323
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-06-26 19:33:55 +0000

    refactor: rm unpivot, fix node alignment

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__gpnode.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__gpnode.sql
@@ -1,56 +1,83 @@
-with
-    gpnode as (
-        select
-            p._dbt_source_relation,
-            p.id as plan_id,
-            p.gpversionid as plan_gpversionid,
-            p.parentid as plan_parentid,
-            p.name as plan_name,
+select
+    p._dbt_source_relation,
+    p.id as plan_id,
+    p.gpversionid as plan_gpversionid,
+    p.parentid as plan_parentid,
+    p.name as plan_name,
 
-            o.creditcapacity as plan_credit_capacity,
+    o.creditcapacity as plan_credit_capacity,
 
-            d.id as discipline_id,
-            d.name as discipline_name,
-            d.creditcapacity as discipline_credit_capacity,
+    d.id as discipline_id,
+    d.name as discipline_name,
+    d.creditcapacity as discipline_credit_capacity,
 
-            coalesce(s.id, d.id) as subject_id,
-            coalesce(s.name, d.name) as subject_name,
-            coalesce(s.creditcapacity, d.creditcapacity) as subject_credit_capacity,
-        from {{ ref("stg_powerschool__gpnode") }} as p
-        inner join
-            {{ ref("stg_powerschool__gpnode") }} as o
-            on p.id = o.parentid
-            and {{ union_dataset_join_clause(left_alias="p", right_alias="o") }}
-        inner join
-            {{ ref("stg_powerschool__gpnode") }} as d
-            on o.id = d.parentid
-            and {{ union_dataset_join_clause(left_alias="o", right_alias="d") }}
-        left join
-            {{ ref("stg_powerschool__gpnode") }} as s
-            on d.id = s.parentid
-            and {{ union_dataset_join_clause(left_alias="d", right_alias="s") }}
-        where p.parentid is null
-    )
+    s.id as subject_id,
+    s.name as subject_name,
+    s.creditcapacity as subject_credit_capacity,
+
+    ps.id as gpprogresssubject_id,
+    ps.studentsdcid,
+
+    psen.ccdcid,
+
+    psea.storedgradesdcid,
+from {{ ref("stg_powerschool__gpnode") }} as p
+left join
+    {{ ref("stg_powerschool__gpnode") }} as o
+    on p.id = o.parentid
+    and {{ union_dataset_join_clause(left_alias="p", right_alias="o") }}
+left join
+    {{ ref("stg_powerschool__gpnode") }} as d
+    on o.id = d.parentid
+    and {{ union_dataset_join_clause(left_alias="o", right_alias="d") }}
+left join
+    {{ ref("stg_powerschool__gpnode") }} as s
+    on d.id = s.parentid
+    and {{ union_dataset_join_clause(left_alias="d", right_alias="s") }}
+left join
+    {{ ref("stg_powerschool__gpprogresssubject") }} as ps
+    on s.id = ps.gpnodeid
+    and {{ union_dataset_join_clause(left_alias="s", right_alias="ps") }}
+left join
+    {{ ref("stg_powerschool__gpprogresssubjectenrolled") }} as psen
+    on ps.id = psen.gpprogresssubjectid
+    and {{ union_dataset_join_clause(left_alias="ps", right_alias="psen") }}
+left join
+    {{ ref("stg_powerschool__gpprogresssubjectearned") }} as psea
+    on ps.id = psea.gpprogresssubjectid
+    and {{ union_dataset_join_clause(left_alias="ps", right_alias="psea") }}
+where p.parentid is null and p.name in ('NJ State Diploma', 'HS Distinction Diploma')
+
+union all
 
 select
-    gpn.*,
+    p._dbt_source_relation,
+    p.id as plan_id,
+    p.gpversionid as plan_gpversionid,
+    p.parentid as plan_parentid,
+    p.name as plan_name,
+    p.creditcapacity as plan_credit_capacity,
 
-    gpps.id as gpprogresssubject_id,
-    gpps.studentsdcid,
+    d.id as discipline_id,
+    d.name as discipline_name,
+    d.creditcapacity as discipline_credit_capacity,
 
-    gppsen.ccdcid,
+    s.id as subject_id,
+    s.name as subject_name,
+    s.creditcapacity as subject_credit_capacity,
 
-    gppsea.storedgradesdcid,
-from gpnode as gpn
+    null as gpprogresssubject_id,
+    null as studentsdcid,
+    null as ccdcid,
+    null as storedgradesdcid,
+from {{ ref("stg_powerschool__gpnode") }} as p
 left join
-    {{ ref("stg_powerschool__gpprogresssubject") }} as gpps
-    on gpn.subject_id = gpps.gpnodeid
-    and {{ union_dataset_join_clause(left_alias="gpn", right_alias="gpps") }}
+    {{ ref("stg_powerschool__gpnode") }} as d
+    on p.id = d.parentid
+    and {{ union_dataset_join_clause(left_alias="p", right_alias="d") }}
 left join
-    {{ ref("stg_powerschool__gpprogresssubjectenrolled") }} as gppsen
-    on gpps.id = gppsen.gpprogresssubjectid
-    and {{ union_dataset_join_clause(left_alias="gpps", right_alias="gppsen") }}
-left join
-    {{ ref("stg_powerschool__gpprogresssubjectearned") }} as gppsea
-    on gpps.id = gppsea.gpprogresssubjectid
-    and {{ union_dataset_join_clause(left_alias="gpps", right_alias="gppsea") }}
+    {{ ref("stg_powerschool__gpnode") }} as s
+    on d.id = s.parentid
+    and {{ union_dataset_join_clause(left_alias="d", right_alias="s") }}
+where
+    p.parentid is null and p.name not in ('NJ State Diploma', 'HS Distinction Diploma')
