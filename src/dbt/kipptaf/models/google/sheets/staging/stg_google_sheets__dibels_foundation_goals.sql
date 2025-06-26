c6e5f0c8d652eb6bdb4c7190d16584c51b5b89f5
Gaby Rangel <grangel@kippteamandfamily.org>
2025-06-26 19:55:30 +0000

    trunk

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__dibels_foundation_goals.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__dibels_foundation_goals.sql
@@ -1,1 +1,2 @@
-select *, from {{ source("google_sheets", "src_google_sheets__dibels_foundation_goals") }}
+select *,
+from {{ source("google_sheets", "src_google_sheets__dibels_foundation_goals") }}
