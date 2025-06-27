e5e64bcf7af5a1ce3948be4e98a5c5a798eb83ca
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-06-27 20:28:28 +0000

    fix: zendesk status

--- tests/ops/test_ops_zendesk.py
+++ tests/ops/test_ops_zendesk.py
@@ -15,11 +15,14 @@ def test_zendesk_user_sync_op():
                 query="""
                     select
                         email,
+                        name,
                         suspended,
-                        null as organization_id,
-                        struct(null as secondary_location, null as user_group) as user_fields,
+                        organization_id,
+                        struct(
+                            secondary_location as secondary_location,
+                            user_group as user_group
+                        ) as user_fields,
                     from kipptaf_extracts.rpt_zendesk__users
-                    where suspended
                 """
             ),
         )
