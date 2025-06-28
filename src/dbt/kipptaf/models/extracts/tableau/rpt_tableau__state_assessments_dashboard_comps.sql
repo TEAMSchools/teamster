c4cc407caf10b79698fcada050ae446458323652
Gaby Rangel <grangel@kippteamandfamily.org>
2025-06-28 22:45:51 +0000

    trunk

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__state_assessments_dashboard_comps.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__state_assessments_dashboard_comps.sql
@@ -1,4 +1,4 @@
-select *
+select *,
 from {{ ref("int_tableau__state_assessments_demographic_comps_cubed") }}
 where
     academic_year is not null and assessment_name is not null and test_code is not null
