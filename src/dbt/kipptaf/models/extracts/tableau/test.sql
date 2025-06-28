e03db0369641bb0c322e7b89aca5508d3e66522b
Gaby Rangel <grangel@kippteamandfamily.org>
2025-06-28 22:14:40 +0000

    more focused dimensions

--- src/dbt/kipptaf/models/extracts/tableau/test.sql
+++ src/dbt/kipptaf/models/extracts/tableau/test.sql
@@ -21,12 +21,15 @@
     generate_cube_query(
         dims,
         aggs,
-        (
-            "("
-            ~ "SELECT * FROM "
-            ~ ref("rpt_tableau__state_assessments_dashboard_cmo_comps")
-            ~ " WHERE test_code IS NOT NULL AND assessment_name IS NOT NULL"
-            ~ ")"
-        ),
+        ref("rpt_tableau__state_assessments_dashboard_cmo_comps"),
+        where_clause="test_code IS NOT NULL AND assessment_name IS NOT NULL",
+        focus_group=True,
+        focus_dims=[
+            "gender",
+            "race_ethnicity",
+            "lunch_status",
+            "lep_status",
+            "iep_status",
+        ],
     )
 }}
