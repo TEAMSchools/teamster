11acad0b78b770ab0d243c9d8b135622551f254f
Gaby Rangel <grangel@kippteamandfamily.org>
2025-06-29 16:23:34 +0000

    added other comps

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__state_assessments_dashboard_comps.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__state_assessments_dashboard_comps.sql
@@ -1,75 +1,199 @@
+with
+    ktaf as (
+        select
+            b.academic_year,
+            b.assessment_name,
+            b.test_code,
+            b.total_proficient_students,
+            b.total_students,
+            b.percent_proficient,
+
+            if(b.region is null, regions, b.region) as region,
+
+            case
+                when b.region is null
+                then 'Total'
+                when b.focus_level in ('ml_status', 'iep_status', 'lunch_status,')
+                then 'Subgroup'
+                else initcap(regexp_replace(b.focus_level, r'_', ' '))
+            end as comparison_demographic_group,
+
+            case
+                when b.region is null and b.focus_level = 'all_null'
+                then 'All Students'
+                else
+                    coalesce(
+                        b.gender,
+                        b.aggregate_ethnicity,
+                        b.lunch_status,
+                        b.ml_status,
+                        b.iep_status
+                    )
+            end as comparison_demographic_subgroup,
+
+            if(b.region is null, b.district_state, 'Region') as comparison_entity,
+
+        from {{ ref("int_tableau__state_assessments_demographic_comps_cubed") }} as b
+        cross join unnest(['Camden', 'Newark']) as regions
+        where
+            b.academic_year is not null
+            and b.assessment_name is not null
+            and b.test_code is not null
+            and b.district_state = 'KTAF NJ'
+
+        union all
+
+        select
+            academic_year,
+            assessment_name,
+            test_code,
+            total_proficient_students,
+            total_students,
+            percent_proficient,
+
+            coalesce(region, 'Miami') as region,
+
+            case
+                when region is null
+                then 'Total'
+                when focus_level in ('ml_status', 'iep_status', 'lunch_status,')
+                then 'Subgroup'
+                else initcap(regexp_replace(focus_level, r'_', ' '))
+            end as comparison_demographic_group,
+
+            case
+                when region is null and focus_level = 'all_null'
+                then 'All Students'
+                else
+                    coalesce(
+                        gender, aggregate_ethnicity, lunch_status, ml_status, iep_status
+                    )
+            end as comparison_demographic_subgroup,
+
+            if(region is null, district_state, 'Region') as comparison_entity,
+
+        from {{ ref("int_tableau__state_assessments_demographic_comps_cubed") }}
+        where
+            academic_year is not null
+            and assessment_name is not null
+            and test_code is not null
+            and district_state = 'KTAF FL'
+    )
+
 select
-    b.academic_year,
-    b.assessment_name,
-    b.test_code,
-    b.total_proficient_students,
-    b.total_students,
-    b.percent_proficient,
+    academic_year,
+    assessment_name,
+    region,
+    comparison_entity,
+    comparison_demographic_subgroup,
 
-    if(b.region is null, regions, b.region) as region,
+    total_students,
+    percent_proficient,
 
-    case
-        when b.region is null
-        then 'Total'
-        when b.focus_level in ('ml_status', 'iep_status', 'lunch_status,')
-        then 'Subgroup'
-        else initcap(regexp_replace(b.focus_level, r'_', ' '))
-    end as comparison_demographic_group,
+    comparison_demographic_group,
+
+    total_proficient_students,
+
+    if(test_code like 'ALG01%', 'ALG01', test_code) as test_code,
 
     case
-        when b.region is null and b.focus_level = 'all_null'
-        then 'All Students'
-        else
-            coalesce(
-                b.gender,
-                b.aggregate_ethnicity,
-                b.lunch_status,
-                b.ml_status,
-                b.iep_status
+        when
+            test_code in (
+                'ELA09',
+                'ELA10',
+                'ELA11',
+                'ELAGP',
+                'ALG01_HS',
+                'GEO01',
+                'ALG02',
+                'MATGP',
+                'SCI11'
             )
-    end as comparison_demographic_subgroup,
+        then 'HS'
+        when safe_cast(left(test_code, 2) as numeric) between 5 and 8
+        then 'MS'
+        else 'ES'
+    end as school_level,
 
-    if(b.region is null, b.district_state, 'Region') as comparison_entity,
+    case
+        when
+            test_code in (
+                'ELA09',
+                'ELA10',
+                'ELA11',
+                'ELAGP',
+                'ALG01_HS',
+                'GEO01',
+                'ALG02',
+                'MATGP',
+                'SCI11'
+            )
+        then 'HS'
+        else '3-8'
+    end as grade_range_band,
 
-from {{ ref("int_tableau__state_assessments_demographic_comps_cubed") }} as b
-cross join unnest(['Camden', 'Newark']) as regions
-where
-    b.academic_year is not null
-    and b.assessment_name is not null
-    and b.test_code is not null
-    and b.district_state = 'KTAF NJ'
+    case
+        when left(test_code, 3) in ('MAT', 'ALG', 'GEO')
+        then 'Math'
+        when left(test_code, 3) = 'ELA'
+        then 'ELA'
+        when left(test_code, 3) = 'SCI'
+        then 'Science'
+        when left(test_code, 3) = 'SOC'
+        then 'Social Studies'
+    end as discipline,
+
+from ktaf
+where comparison_demographic_subgroup not in ('Not ML', 'Students Without Disabilities')
 
 union all
 
 select
     academic_year,
-    assessment_name,
-    test_code,
-    total_proficient_students,
+    test_name as assessment_name,
+    region,
+    comparison_entity,
+    comparison_demographic_subgroup,
+
     total_students,
     percent_proficient,
 
-    coalesce(region, 'Miami') as region,
+    comparison_demographic_group,
+
+    round(percent_proficient * total_students, 0) as total_proficient_students,
+
+    test_code,
 
     case
-        when region is null
-        then 'Total'
-        when focus_level in ('ml_status', 'iep_status', 'lunch_status,')
-        then 'Subgroup'
-        else initcap(regexp_replace(focus_level, r'_', ' '))
-    end as comparison_demographic_group,
+        when comparison_demographic_subgroup = 'Grade - 08'
+        then 'MS'
+        when comparison_demographic_subgroup in ('Grade - 09', 'Grade - 10')
+        then 'HS'
+        when test_code in ('ELA09', 'ELA10', 'ELA11', 'ELAGP', 'MATGP', 'SCI11')
+        then 'HS'
+        when safe_cast(left(test_code, 2) as numeric) between 5 and 8
+        then 'MS'
+        else 'ES'
+    end as school_level,
 
     case
-        when region is null and focus_level = 'all_null'
-        then 'All Students'
-        else coalesce(gender, aggregate_ethnicity, lunch_status, ml_status, iep_status)
-    end as comparison_demographic_subgroup,
+        when comparison_demographic_subgroup in ('Grade - 09', 'Grade - 10')
+        then 'HS'
+        when test_code in ('ELA09', 'ELA10', 'ELA11', 'ELAGP', 'MATGP', 'SCI11')
+        then 'HS'
+        else '3-8'
+    end as grade_range_band,
 
-    if(region is null, district_state, 'Region') as comparison_entity,
+    case
+        when left(test_code, 3) in ('MAT', 'ALG', 'GEO')
+        then 'Math'
+        when left(test_code, 3) = 'ELA'
+        then 'ELA'
+        when left(test_code, 3) = 'SCI'
+        then 'Science'
+        when left(test_code, 3) = 'SOC'
+        then 'Social Studies'
+    end as discipline,
 
-from {{ ref("int_tableau__state_assessments_demographic_comps_cubed") }}
-where
-    academic_year is not null
-    and assessment_name is not null
-    and test_code is not null
-    and district_state = 'KTAF FL'
+from {{ ref("stg_google_sheets__state_test_comparison_demographics") }}
+where comparison_demographic_subgroup != 'SE Accommodation'
