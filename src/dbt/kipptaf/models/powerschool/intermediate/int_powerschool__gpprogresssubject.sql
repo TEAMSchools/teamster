f441cd9470bcf940cd0d0d8b5726821bcf064536
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-06-30 22:25:58 +0000

    fix: prog subj union

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__gpprogresssubject.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__gpprogresssubject.sql
@@ -9,14 +9,32 @@ select
     ps.earnedcredits,
     ps.waivedcredits,
 
-    psen.ccdcid,
+    psen.ccdcid as grade_dcid,
 
-    psea.storedgradesdcid,
+    'Enrolled' as credit_status,
 from {{ ref("stg_powerschool__gpprogresssubject") }} as ps
 left join
     {{ ref("stg_powerschool__gpprogresssubjectenrolled") }} as psen
     on ps.id = psen.gpprogresssubjectid
     and {{ union_dataset_join_clause(left_alias="ps", right_alias="psen") }}
+
+union all
+
+select
+    ps._dbt_source_relation,
+    ps.id,
+    ps.gpnodeid,
+    ps.studentsdcid,
+    ps.requiredcredits,
+    ps.enrolledcredits,
+    ps.requestedcredits,
+    ps.earnedcredits,
+    ps.waivedcredits,
+
+    psea.storedgradesdcid as grade_dcid,
+
+    'Earned' as credit_status,
+from {{ ref("stg_powerschool__gpprogresssubject") }} as ps
 left join
     {{ ref("stg_powerschool__gpprogresssubjectearned") }} as psea
     on ps.id = psea.gpprogresssubjectid
