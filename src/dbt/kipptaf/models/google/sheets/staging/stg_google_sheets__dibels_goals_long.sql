6f6e0ee9816643e67c93a02c7fd80dc4887a7985
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-02 13:34:55 +0000

    modified goals long

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__dibels_goals_long.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__dibels_goals_long.sql
@@ -1,1 +1,10 @@
-select *, from {{ source("google_sheets", "src_google_sheets__dibels_goals_long") }}
+select
+    *,
+
+    case
+        bm_period when 'MOY' then 'BOY->MOY' when 'EOY' then 'MOY->EOY'
+    end as matching_pm_season,
+
+    if(grade = 'K', 0, safe_cast(grade as int64)) as grade_level,
+
+from {{ source("google_sheets", "src_google_sheets__dibels_goals_long") }}
