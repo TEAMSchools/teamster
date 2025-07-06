b6fd3c0a9d9669081d302892f09553510dbe7e78
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-06 16:49:19 +0000

    added missing fields

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__gradebook_exceptions.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__gradebook_exceptions.sql
@@ -1,1 +1,19 @@
-select *, from {{ source("google_sheets", "src_google_sheets__gradebook_exceptions") }}
+select
+    academic_year,
+    region,
+    school_level,
+    school_id,
+    quarter,
+    course_number,
+    section_number,
+    credit_type,
+    gradebook_category,
+    audit_flag_name,
+    view_name,
+    cte,
+    purpose,
+    `include`,
+
+    safe_cast(grade_level as int64) as grade_level,
+
+from {{ source("google_sheets", "src_google_sheets__gradebook_exceptions") }}
