5b0000f96c36811b1927e25fe9f53a00421249b2
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-06 03:16:05 +0000

    updated int_tableau__gradebook_audit_section_week_category_scaffold

--- src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__gradebook_audit_section_week_category_scaffold.sql
+++ src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__gradebook_audit_section_week_category_scaffold.sql
@@ -8,20 +8,16 @@ select
     ge.notes,
 from {{ ref("int_tableau__gradebook_audit_section_week_scaffold") }} as s
 inner join
-    {{ ref("stg_reporting__gradebook_expectations") }} as ge
+    {{ ref("stg_google_sheets__gradebook_expectations_assignments") }} as ge
     on s.region = ge.region
     and s.school_level = ge.school_level
     and s.academic_year = ge.academic_year
     and s.quarter = ge.quarter
     and s.week_number_quarter = ge.week_number
-where
-    concat(s.course_number, ge.assignment_category_code) not in (
-        'SEM72005G1F',
-        'SEM72005G2F',
-        'SEM72005G3F',
-        'SEM72005G4F',
-        'SEM72005G1S',
-        'SEM72005G2S',
-        'SEM72005G3S',
-        'SEM72005G4S'
-    )
+left join
+    {{ ref("stg_google_sheets__gradebook_exceptions") }} as e1
+    on s.academic_year = e1.academic_year
+    and s.course_number = e1.course_number
+    and ge.assignment_category_code = e1.gradebook_category
+    and e1.view_name = 'gradebook_audit_section_week_category_scaffold'
+where e1.include is null
