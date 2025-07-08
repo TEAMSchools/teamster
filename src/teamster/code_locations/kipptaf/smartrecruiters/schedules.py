2b8e4a777cc881b1a6ba1e7062f782107acd8329
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-07-08 17:26:26 +0000

    chore: rename scheudles

--- src/teamster/code_locations/kipptaf/smartrecruiters/schedules.py
+++ src/teamster/code_locations/kipptaf/smartrecruiters/schedules.py
@@ -1,12 +1,11 @@
-from dagster import ScheduleDefinition, define_asset_job
+from dagster import ScheduleDefinition
 
 from teamster.code_locations.kipptaf import CODE_LOCATION, LOCAL_TIMEZONE
 from teamster.code_locations.kipptaf.smartrecruiters.assets import assets
 
 smartrecruiters_report_assets_schedule = ScheduleDefinition(
-    job=define_asset_job(
-        name=f"{CODE_LOCATION}_smartrecruiters_report_asset_job", selection=assets
-    ),
+    name=f"{CODE_LOCATION}__smartrecruiters__assets_schedule",
+    target=assets,
     cron_schedule="0 6 * * *",
     execution_timezone=str(LOCAL_TIMEZONE),
 )
