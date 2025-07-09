cbc4427b439b5ae630ff67dc7c779ebe1842717e
Anthony Walters <awalters@kippteamandfamily.org>
2025-07-09 22:44:12 +0000

    updates

--- src/dbt/kipptaf/models/extracts/topline/int_topline__student_metrics_ada_week_running.sql
+++ src/dbt/kipptaf/models/extracts/topline/int_topline__student_metrics_ada_week_running.sql
@@ -1,40 +1,84 @@
 with
-    school_level as (
-        select powerschool_school_id as schoolid, max(grade_band) as grade_band,
-        from {{ ref("stg_people__location_crosswalk") }}
-        group by powerschool_school_id
+    ada_week_values as (
+        select
+            mem._dbt_source_relation,
+            mem.studentid,
+            mem.yearid,
+            mem.schoolid,
+
+            w.week_start_monday,
+
+            /* standard ada calcs */
+            sum(mem.attendancevalue) as sum_attendance_value_week,
+            count(mem.attendancevalue) as count_attendance_value_week,
+            round(avg(mem.attendancevalue), 3) as ada_week,
+
+            /* HS weighted ada cals */
+            sum(
+                if(
+                    w.school_level = 'HS' and att.att_code like 'T%',
+                    0.67,
+                    mem.attendancevalue
+                )
+            ) as sum_attendance_value_week_weighted,
+            round(
+                avg(
+                    if(
+                        w.school_level = 'HS' and att.att_code like 'T%',
+                        0.67,
+                        mem.attendancevalue
+                    )
+                ),
+                3
+            ) as ada_week_weighted,
+        from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }} as mem
+        left join
+            {{ ref("int_powerschool__ps_attendance_daily") }} as att
+            on mem.studentid = att.studentid
+            and mem.calendardate = att.att_date
+            and {{ union_dataset_join_clause(left_alias="mem", right_alias="att") }}
+        inner join
+            {{ ref("int_powerschool__calendar_week") }} as w
+            on mem.schoolid = w.schoolid
+            and mem.yearid = w.yearid
+            and mem.calendardate between w.week_start_monday and w.week_end_sunday
+        where
+            membershipvalue = 1
+            and calendardate <= current_date('{{ var("local_timezone") }}')
+        group by all
     )
 
 select
-    mem.studentid,
-    mem.yearid,
-    mem.schoolid,
+    _dbt_source_relation,
+    studentid,
+    yearid,
+    schoolid,
+    week_start_monday,
+    ada_week,
+    ada_week_weighted,
 
-    w.week_start_monday,
+    (
+        sum(sum_attendance_value_week) over (
+            partition by
+                _dbt_source_relation, studentid, yearid, schoolid, week_start_monday
+            order by week_start_monday asc
+        ) / sum(count_attendance_value_week) over (
+            partition by
+                _dbt_source_relation, studentid, yearid, schoolid, week_start_monday
+            order by week_start_monday asc
+        )
+    ) as ada_week_running,
 
-    round(avg(mem.attendancevalue), 3) as ada_running,
     round(
-        avg(
-            if(
-                sl.grade_band = 'HS' and att.att_code like 'T%',
-                0.67,
-                mem.attendancevalue
+        (
+            sum(sum_attendance_value_week_weighted) over (
+                partition by _dbt_source_relation, studentid, yearid, schoolid
+                order by week_start_monday asc
+            ) / sum(count_attendance_value_week) over (
+                partition by _dbt_source_relation, studentid, yearid, schoolid
+                order by week_start_monday asc
             )
         ),
         3
-    ) as ada_running_weighted,
-from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }} as mem
-left join
-    {{ ref("int_powerschool__ps_attendance_daily") }} as att
-    on mem.studentid = att.studentid
-    and mem.calendardate = att.att_date
-    and {{ union_dataset_join_clause(left_alias="mem", right_alias="att") }}
-inner join school_level as sl on mem.schoolid = sl.schoolid
-inner join
-    {{ ref("int_powerschool__calendar_week") }} as w
-    on mem.schoolid = w.schoolid
-    and mem.yearid = w.yearid
-    and mem.calendardate <= w.week_end_sunday
-where
-    membershipvalue = 1 and calendardate <= current_date('{{ var("local_timezone") }}')
-group by all
+    ) as ada_week_running_weighted,
+from ada_week_values
