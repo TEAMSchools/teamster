de2363cbe8f2d92227f7839e1d9a440f1e6005e8
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-15 14:40:52 +0000

    added tag for expected rows based and not based on enroll dates

--- src/dbt/kipptaf/models/amplify/intermediate/int_amplify__participation_roster.sql
+++ src/dbt/kipptaf/models/amplify/intermediate/int_amplify__participation_roster.sql
@@ -1,8 +1,15 @@
 with
     students as (
-        select academic_year, region, student_number, grade_level, enroll_status,
+        select
+            academic_year,
+            region,
+            student_number,
+            grade_level,
+            enroll_status,
+            entrydate,
+            exitdate
         from {{ ref("int_extracts__student_enrollments") }}
-        where enroll_status != -1 and grade_level <= 8
+        where enroll_status != -1 and grade_level <= 8 and academic_year >= 2023
     ),
 
     expected_tests as (
@@ -13,6 +20,9 @@ with
             e.assessment_type,
             e.admin_season,
             e.`round`,
+
+            t.start_date,
+            t.end_date,
 
             count(*) over (
                 partition by
@@ -30,13 +40,15 @@ with
         where e.assessment_include is null
     ),
 
-    roster as (
+    roster_enrollment_dates as (
         select
             s.academic_year,
             s.region,
             s.student_number,
             s.grade_level,
             s.enroll_status,
+            s.entrydate,
+            s.exitdate,
 
             e.assessment_type,
             e.admin_season,
@@ -44,6 +56,62 @@ with
             e.expected_row_count,
 
             coalesce(a.actual_row_count, 0) as actual_row_count,
+
+            true as enrollment_dates_account,
+
+            if(
+                e.expected_row_count = a.actual_row_count, true, false
+            ) as completed_test_round,
+
+            if(
+                e.expected_row_count = a.actual_row_count, 1, 0
+            ) as completed_test_round_int,
+
+            row_number() over (
+                partition by
+                    s.academic_year,
+                    s.region,
+                    s.student_number,
+                    s.grade_level,
+                    e.admin_season,
+                    e.`round`
+            ) as rn,
+
+        from students as s
+        inner join
+            expected_tests as e
+            on s.academic_year = e.academic_year
+            and s.region = e.region
+            and s.grade_level = e.grade
+            and s.entrydate between e.start_date and e.end_date
+        left join
+            {{ ref("int_amplify__all_assessments") }} as a
+            on s.academic_year = a.academic_year
+            and s.region = a.region
+            and s.student_number = a.student_number
+            and s.grade_level = a.assessment_grade_int
+            and e.admin_season = a.period
+            and e.`round` = a.`round`
+    ),
+
+    roster_no_enrollment_dates as (
+        select
+            s.academic_year,
+            s.region,
+            s.student_number,
+            s.grade_level,
+            s.enroll_status,
+            s.entrydate,
+            s.exitdate,
+
+            e.assessment_type,
+            e.admin_season,
+            e.`round`,
+            e.expected_row_count,
+
+            coalesce(a.actual_row_count, 0) as actual_row_count,
+
+            false as enrollment_dates_account,
 
             if(
                 e.expected_row_count = a.actual_row_count, true, false
@@ -84,6 +152,7 @@ select
     region,
     student_number,
     grade_level,
+    enrollment_dates_account,
     assessment_type,
     admin_season,
     `round`,
@@ -92,5 +161,24 @@ select
     completed_test_round,
     completed_test_round_int,
 
-from roster
+from roster_enrollment_dates
+where rn = 1
+
+union all
+
+select
+    academic_year,
+    region,
+    student_number,
+    grade_level,
+    enrollment_dates_account,
+    assessment_type,
+    admin_season,
+    `round`,
+    expected_row_count,
+    actual_row_count,
+    completed_test_round,
+    completed_test_round_int,
+
+from roster_no_enrollment_dates
 where rn = 1
