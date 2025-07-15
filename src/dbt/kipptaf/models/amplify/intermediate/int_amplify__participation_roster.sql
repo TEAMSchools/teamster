9864ff984dfe06a4d806eb9b0b304c3118c87bd5
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-15 17:03:09 +0000

    better join for pm completion rate

--- src/dbt/kipptaf/models/amplify/intermediate/int_amplify__participation_roster.sql
+++ src/dbt/kipptaf/models/amplify/intermediate/int_amplify__participation_roster.sql
@@ -5,12 +5,27 @@ with
             region,
             student_number,
             grade_level,
-            enroll_status,
             entrydate,
             exitdate,
 
-        from {{ ref("int_extracts__student_enrollments") }}
-        where enroll_status != -1 and grade_level <= 8 and academic_year >= 2023
+            if(
+                dibels_boy_composite in ('Below Benchmark', 'Well Below Benchmark'),
+                'Yes',
+                'No'
+            ) as boy_probe_eligible,
+
+            if(
+                dibels_moy_composite in ('Below Benchmark', 'Well Below Benchmark'),
+                'Yes',
+                'No'
+            ) as moy_probe_eligible,
+
+        from {{ ref("int_extracts__student_enrollments_subjects") }}
+        where
+            enroll_status != -1
+            and grade_level <= 8
+            and academic_year >= 2023
+            and discipline = 'ELA'
     ),
 
     expected_tests as (
@@ -47,9 +62,6 @@ with
             s.region,
             s.student_number,
             s.grade_level,
-            s.enroll_status,
-            s.entrydate,
-            s.exitdate,
 
             e.assessment_type,
             e.admin_season,
@@ -84,6 +96,7 @@ with
             on s.academic_year = e.academic_year
             and s.region = e.region
             and s.grade_level = e.grade
+            and e.assessment_type = 'Benchmark'
             and e.start_date between s.entrydate and s.exitdate
         left join
             {{ ref("int_amplify__all_assessments") }} as a
@@ -91,6 +104,114 @@ with
             and s.region = a.region
             and s.student_number = a.student_number
             and s.grade_level = a.assessment_grade_int
+            and e.admin_season = a.period
+            and e.`round` = a.`round`
+
+        union all
+
+        select
+            s.academic_year,
+            s.region,
+            s.student_number,
+            s.grade_level,
+
+            e.assessment_type,
+            e.admin_season,
+            e.`round`,
+            e.expected_row_count,
+
+            true as enrollment_dates_account,
+
+            coalesce(a.actual_row_count, 0) as actual_row_count,
+
+            if(
+                e.expected_row_count = a.actual_row_count, true, false
+            ) as completed_test_round,
+
+            if(
+                e.expected_row_count = a.actual_row_count, 1, 0
+            ) as completed_test_round_int,
+
+            row_number() over (
+                partition by
+                    s.academic_year,
+                    s.region,
+                    s.student_number,
+                    s.grade_level,
+                    e.admin_season,
+                    e.`round`
+            ) as rn,
+
+        from students as s
+        inner join
+            expected_tests as e
+            on s.academic_year = e.academic_year
+            and s.region = e.region
+            and s.grade_level = e.grade
+            and s.boy_probe_eligible = 'Yes'
+            and e.admin_season = 'BOY->MOY'
+            and e.start_date between s.entrydate and s.exitdate
+        left join
+            {{ ref("int_amplify__all_assessments") }} as a
+            on s.academic_year = a.academic_year
+            and s.region = a.region
+            and s.student_number = a.student_number
+            and s.grade_level = a.assessment_grade_int
+            and s.boy_probe_eligible = a.boy_probe_eligible
+            and e.admin_season = a.period
+            and e.`round` = a.`round`
+
+        union all
+
+        select
+            s.academic_year,
+            s.region,
+            s.student_number,
+            s.grade_level,
+
+            e.assessment_type,
+            e.admin_season,
+            e.`round`,
+            e.expected_row_count,
+
+            true as enrollment_dates_account,
+
+            coalesce(a.actual_row_count, 0) as actual_row_count,
+
+            if(
+                e.expected_row_count = a.actual_row_count, true, false
+            ) as completed_test_round,
+
+            if(
+                e.expected_row_count = a.actual_row_count, 1, 0
+            ) as completed_test_round_int,
+
+            row_number() over (
+                partition by
+                    s.academic_year,
+                    s.region,
+                    s.student_number,
+                    s.grade_level,
+                    e.admin_season,
+                    e.`round`
+            ) as rn,
+
+        from students as s
+        inner join
+            expected_tests as e
+            on s.academic_year = e.academic_year
+            and s.region = e.region
+            and s.grade_level = e.grade
+            and s.moy_probe_eligible = 'Yes'
+            and e.admin_season = 'MOY->EOY'
+            and e.start_date between s.entrydate and s.exitdate
+        left join
+            {{ ref("int_amplify__all_assessments") }} as a
+            on s.academic_year = a.academic_year
+            and s.region = a.region
+            and s.student_number = a.student_number
+            and s.grade_level = a.assessment_grade_int
+            and s.moy_probe_eligible = a.moy_probe_eligible
             and e.admin_season = a.period
             and e.`round` = a.`round`
     ),
@@ -101,9 +222,6 @@ with
             s.region,
             s.student_number,
             s.grade_level,
-            s.enroll_status,
-            s.entrydate,
-            s.exitdate,
 
             e.assessment_type,
             e.admin_season,
@@ -138,12 +256,119 @@ with
             on s.academic_year = e.academic_year
             and s.region = e.region
             and s.grade_level = e.grade
+            and e.assessment_type = 'Benchmark'
         left join
             {{ ref("int_amplify__all_assessments") }} as a
             on s.academic_year = a.academic_year
             and s.region = a.region
             and s.student_number = a.student_number
             and s.grade_level = a.assessment_grade_int
+            and e.admin_season = a.period
+            and e.`round` = a.`round`
+
+        union all
+
+        select
+            s.academic_year,
+            s.region,
+            s.student_number,
+            s.grade_level,
+
+            e.assessment_type,
+            e.admin_season,
+            e.`round`,
+            e.expected_row_count,
+
+            false as enrollment_dates_account,
+
+            coalesce(a.actual_row_count, 0) as actual_row_count,
+
+            if(
+                e.expected_row_count = a.actual_row_count, true, false
+            ) as completed_test_round,
+
+            if(
+                e.expected_row_count = a.actual_row_count, 1, 0
+            ) as completed_test_round_int,
+
+            row_number() over (
+                partition by
+                    s.academic_year,
+                    s.region,
+                    s.student_number,
+                    s.grade_level,
+                    e.admin_season,
+                    e.`round`
+            ) as rn,
+
+        from students as s
+        inner join
+            expected_tests as e
+            on s.academic_year = e.academic_year
+            and s.region = e.region
+            and s.grade_level = e.grade
+            and s.boy_probe_eligible = 'Yes'
+            and e.admin_season = 'BOY->MOY'
+        left join
+            {{ ref("int_amplify__all_assessments") }} as a
+            on s.academic_year = a.academic_year
+            and s.region = a.region
+            and s.student_number = a.student_number
+            and s.grade_level = a.assessment_grade_int
+            and s.boy_probe_eligible = a.boy_probe_eligible
+            and e.admin_season = a.period
+            and e.`round` = a.`round`
+
+        union all
+
+        select
+            s.academic_year,
+            s.region,
+            s.student_number,
+            s.grade_level,
+
+            e.assessment_type,
+            e.admin_season,
+            e.`round`,
+            e.expected_row_count,
+
+            false as enrollment_dates_account,
+
+            coalesce(a.actual_row_count, 0) as actual_row_count,
+
+            if(
+                e.expected_row_count = a.actual_row_count, true, false
+            ) as completed_test_round,
+
+            if(
+                e.expected_row_count = a.actual_row_count, 1, 0
+            ) as completed_test_round_int,
+
+            row_number() over (
+                partition by
+                    s.academic_year,
+                    s.region,
+                    s.student_number,
+                    s.grade_level,
+                    e.admin_season,
+                    e.`round`
+            ) as rn,
+
+        from students as s
+        inner join
+            expected_tests as e
+            on s.academic_year = e.academic_year
+            and s.region = e.region
+            and s.grade_level = e.grade
+            and s.moy_probe_eligible = 'Yes'
+            and e.admin_season = 'MOY->EOY'
+        left join
+            {{ ref("int_amplify__all_assessments") }} as a
+            on s.academic_year = a.academic_year
+            and s.region = a.region
+            and s.student_number = a.student_number
+            and s.grade_level = a.assessment_grade_int
+            and s.moy_probe_eligible = a.moy_probe_eligible
             and e.admin_season = a.period
             and e.`round` = a.`round`
     )
