3be2f43197aa17c60d2cea5715a18bfea61e9942
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-16 17:46:45 +0000

    new pm goal setting view

--- src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__dibels_pm_goal_setting.sql
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__dibels_pm_goal_setting.sql
@@ -1,205 +1,103 @@
 with
-    school_directory as (
-        select
-            s.schoolcity as region,
-            s.school_number as schoolid,
-            s.abbreviation as school,
-
-            c.date_value,
-
-            row_number() over (
-                partition by s.schoolcity, c.date_value
-            ) as region_distinct,
-
-        from {{ ref("stg_powerschool__schools") }} as s
-        inner join
-            {{ ref("stg_powerschool__calendar_day") }} as c
-            on s.school_number = c.schoolid
-            and c.insession = 1
-            and {{ union_dataset_join_clause(left_alias="s", right_alias="c") }}
-        where s.state_excludefromreporting = 0
-    ),
-
-    custom_pm_reporting as (
-        select
-            t.academic_year,
-            t.region,
-            t.name,
-
-            cast(right(t.code, 1) as int) as `round`,
-
-            d.date_value,
-
-        from school_directory as d
-        inner join
-            {{ ref("stg_reporting__terms") }} as t
-            on d.region = t.region
-            and d.date_value between t.start_date and t.end_date
-            and d.region_distinct = 1
-            and t.type = 'LIT'
-            and t.name != 'EOY'
-            and t.academic_year = {{ var("current_academic_year") }}
-    ),
-
-    day_count as (
-        select
-            academic_year, region, name, `round`, count(distinct date_value) as n_days,
-
-        from custom_pm_reporting
-        group by academic_year, region, name, `round`
-    ),
-
     avg_scores as (
         select
-            s.academic_year,
-            s.assessment_grade,
-            s.assessment_grade_int,
-            s.period,
-            s.measure_standard,
+            academic_year,
+            region,
+            assessment_grade,
+            assessment_grade_int,
+            period,
+            matching_season,
+            measure_standard,
 
-            e.region,
+            round(avg(measure_standard_score), 0) as starting_words,
 
-            concat(s.period, s.measure_standard) as filter_tag,
-
-            if(s.period = 'BOY', 'BOY->MOY', 'MOY->EOY') as pm_period,
-
-            avg(s.measure_standard_score) as avg_score_raw,
-
-            round(avg(s.measure_standard_score), 0) as avg_score,
-
-        from {{ ref("int_amplify__all_assessments") }} as s
-        inner join
-            {{ ref("int_extracts__student_enrollments") }} as e
-            on s.academic_year = e.academic_year
-            and s.student_number = e.student_number
+        from {{ ref("int_amplify__all_assessments") }}
         where
-            s.academic_year = {{ var("current_academic_year") }}
-            and s.assessment_type = 'Benchmark'
-            and s.period != 'EOY'
-            and s.assessment_grade_int is not null
-            and s.measure_standard in (
-                'Phonemic Awareness (PSF)',
-                'Letter Sounds (NWF-CLS)',
-                'Decoding (NWF-WRC)',
-                'Reading Fluency (ORF)',
-                'Reading Accuracy (ORF-Accu)'
-            )
-            and (s.period = 'BOY' and s.boy_probe_eligible = 'Yes')
-            or (s.period = 'MOY' and s.moy_probe_eligible = 'Yes')
+            academic_year = 2024
+            and measure_standard != 'Composite'
+            and overall_probe_eligible = 'Yes'
+            and period in ('BOY', 'MOY')
         group by
-            s.academic_year,
-            s.assessment_grade,
-            s.assessment_grade_int,
-            s.period,
-            s.measure_standard,
-            e.region
+            academic_year,
+            region,
+            assessment_grade,
+            assessment_grade_int,
+            period,
+            matching_season,
+            measure_standard,
+            overall_probe_eligible
     ),
 
-    days_and_goals as (
+    scores_and_days as (
         select
-            s.academic_year,
-            s.region,
-            s.pm_period,
-            s.assessment_grade,
-            s.assessment_grade_int,
-            s.measure_standard,
-            s.avg_score as starting_words,
+            a.academic_year,
+            a.region,
+            a.assessment_grade,
+            a.assessment_grade_int,
+            a.period,
+            a.matching_season,
+            a.measure_standard,
+            a.starting_words,
 
-            c.round,
-            c.n_days,
+            e.code,
+            e.`round`,
+            e.pm_round_days,
+            e.pm_days,
+            e.pm_goal_include,
 
-            e.bm_goal + 3 as bm_benchmark,
+            e.benchmark_goal + 3 as benchmark_goal,
 
-            round((e.bm_goal + 3) - s.avg_score, 0) as required_growth_words,
+            round(
+                (e.benchmark_goal + 3) - a.starting_words, 0
+            ) as required_growth_words,
 
-            min(c.round) over (
-                partition by s.academic_year, s.region, s.pm_period, s.assessment_grade
-                order by c.`round`
-            ) as min_pm_round,
+            if(e.`round` = e.min_pm_round, true, false) as is_min_round,
 
-            max(c.`round`) over (
-                partition by s.academic_year, s.region, s.pm_period, s.assessment_grade
-                order by c.`round` desc
-            ) as max_pm_round,
+            if(e.`round` = e.max_pm_round, true, false) as is_max_round,
 
-        from avg_scores as s
+        from avg_scores as a
         inner join
-            day_count as c
-            on s.academic_year = c.academic_year
-            and s.region = c.region
-            and s.pm_period = c.name
-        inner join
-            {{ ref("stg_google_sheets__dibels_pm_expectations") }} as e
-            on s.academic_year = e.academic_year
-            and s.region = e.region
-            and s.pm_period = e.period
-            and s.assessment_grade_int = e.grade_level
-            and s.measure_standard = e.measure_standard
-        where
-            s.filter_tag in (
-                'BOYPhonemic Awareness (PSF)',
-                'MOYDecoding (NWF-WRC)',
-                'MOYLetter Sounds (NWF-CLS)',
-                'MOYReading Fluency (ORF)',
-                'MOYReading Accuracy (ORF-Accu)'
-            )
+            {{ ref("int_google_sheets__dibels_pm_expectations") }} as e
+            on a.academic_year = e.academic_year
+            and a.region = e.region
+            and a.assessment_grade_int = e.grade
+            and a.matching_season = e.admin_season
+            and a.measure_standard = e.expected_measure_standard
     ),
 
     calcs as (
         select
-            *,
+            academic_year,
+            region,
+            assessment_grade,
+            assessment_grade_int,
+            period,
+            matching_season,
+            measure_standard,
+            starting_words,
+            code,
+            `round`,
+            pm_round_days,
+            pm_days,
+            pm_goal_include,
+            benchmark_goal,
+            required_growth_words,
+            is_max_round,
 
-            if(`round` = min_pm_round, true, false) as is_min_round,
-
-            if(`round` = max_pm_round, true, false) as is_max_round,
-
-            sum(n_days) over (
-                partition by
-                    academic_year,
-                    region,
-                    pm_period,
-                    assessment_grade_int,
-                    measure_standard
-            ) as pm_n_days,
-
-            round(
-                required_growth_words / sum(n_days) over (
-                    partition by
-                        academic_year,
-                        region,
-                        pm_period,
-                        assessment_grade_int,
-                        measure_standard
-                ),
-                2
-            ) as daily_growth_rate,
+            round(safe_divide(required_growth_words, pm_days), 2) as daily_growth_rate,
 
             round(
                 case
-                    when `round` = min_pm_round
+                    when is_min_round
                     then
-                        (n_days * required_growth_words) / sum(n_days) over (
-                            partition by
-                                academic_year,
-                                region,
-                                pm_period,
-                                assessment_grade_int,
-                                measure_standard
-                        )
+                        safe_divide((pm_round_days * required_growth_words), pm_days)
                         + starting_words
-                    else
-                        (n_days * required_growth_words) / sum(n_days) over (
-                            partition by
-                                academic_year,
-                                region,
-                                pm_period,
-                                assessment_grade_int,
-                                measure_standard
-                        )
+                    else safe_divide((pm_round_days * required_growth_words), pm_days)
                 end,
                 0
             ) as round_growth_words_goal,
-        from days_and_goals
+
+        from scores_and_days
     )
 
 select
@@ -207,19 +105,19 @@ select
 
     case
         when is_max_round
-        then bm_benchmark
+        then benchmark_goal
         else
             sum(round_growth_words_goal) over (
                 partition by
                     academic_year,
                     region,
-                    pm_period,
+                    matching_season,
                     assessment_grade_int,
                     measure_standard
                 order by
                     academic_year,
                     region,
-                    pm_period,
+                    matching_season,
                     `round`,
                     assessment_grade_int,
                     measure_standard
