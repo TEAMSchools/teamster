7ee71656aedc08634d719915f5612a77c207d068
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-17 19:13:55 +0000

    new bm criteria goal

---
+++ src/dbt/kipptaf/models/amplify/intermediate/properties/int_amplify__bm_met_criteria.sql
@@ -0,0 +1,13 @@
+select
+    a.academic_year,
+    a.region,
+    a.student_number,
+    a.assessment_grade,
+    a.assessment_grade_int,
+    a.period,
+    a.measure_standard,
+    a.measure_standard_level,
+    a.aggregated_measure_standard_level,
+
+from {{ ref("int_amplify__all_assessments") }} as a
+where a.assessment_type = 'Benchmark' and measure_standard = 'Composite'
