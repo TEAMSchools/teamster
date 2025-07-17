653c0a32b602d658def71fc5bca48aa4d7d5af1e
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-17 16:45:42 +0000

    continue pm met criteria

--- src/dbt/kipptaf/models/amplify/intermediate/int_amplify__pm_met_criteria.sql
+++ src/dbt/kipptaf/models/amplify/intermediate/int_amplify__pm_met_criteria.sql
@@ -1,33 +1,39 @@
-select
-    g.academic_year,
-    g.region,
-    g.admin_season,
-    g.assessment_grade,
-    g.assessment_grade_int,
-    g.measure_standard,
-    g.`round`,
-    g.benchmark_goal,
-    g.round_growth_words_goal,
-    g.cumulative_growth_words,
-    g.pm_goal_criteria,
+with
+    base as (
+        select
+            g.academic_year,
+            g.region,
+            g.admin_season,
+            g.assessment_grade,
+            g.assessment_grade_int,
+            g.measure_standard,
+            g.`round`,
+            g.benchmark_goal,
+            g.round_growth_words_goal,
+            g.cumulative_growth_words,
+            g.pm_goal_criteria,
 
-    a.student_number,
-    a.measure_name_code,
-    a.measure_standard_score,
+            a.student_number,
+            a.measure_name_code,
+            a.measure_standard_score,
 
-    if(
-        a.measure_standard_score >= g.cumulative_growth_words, 1, 0
-    ) as met_measure_standard_goal,
+            if(
+                a.measure_standard_score >= g.cumulative_growth_words, 1, 0
+            ) as met_measure_standard_goal,
 
-from {{ ref("stg_google_sheets__dibels_pm_goals") }} as g
-left join
-    {{ ref("int_amplify__all_assessments") }} as a
-    on g.academic_year = a.academic_year
-    and g.region = a.region
-    and g.assessment_grade_int = g.assessment_grade_int
-    and g.admin_season = a.period
-    and g.`round` = a.`round`
-    and g.measure_standard = a.measure_standard
-    and a.assessment_type = 'PM'
-    and a.overall_probe_eligible = 'Yes'
-where g.pm_goal_include is null
+        from {{ ref("stg_google_sheets__dibels_pm_goals") }} as g
+        left join
+            {{ ref("int_amplify__all_assessments") }} as a
+            on g.academic_year = a.academic_year
+            and g.region = a.region
+            and g.assessment_grade_int = g.assessment_grade_int
+            and g.admin_season = a.period
+            and g.`round` = a.`round`
+            and g.measure_standard = a.measure_standard
+            and a.assessment_type = 'PM'
+            and a.overall_probe_eligible = 'Yes'
+        where g.pm_goal_include is null
+    )
+
+select *,
+from base
