2ad6f319503ed895cb61cb920303f7e9885d45c4
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-18 17:52:18 +0000

    calcs

--- src/dbt/kipptaf/models/amplify/intermediate/int_amplify__bm_met_criteria.sql
+++ src/dbt/kipptaf/models/amplify/intermediate/int_amplify__bm_met_criteria.sql
@@ -1,40 +1,79 @@
-select
-    a.academic_year,
-    a.region,
-    a.student_number,
-    a.assessment_grade,
-    a.assessment_grade_int,
-    a.period,
-    a.measure_standard,
-    a.measure_standard_level,
-    a.aggregated_measure_standard_level,
-    a.foundation_measure_standard_level,
-    a.client_date,
+with
+    roster as (
+        select
+            a.academic_year,
+            a.region,
+            a.assessment_grade,
+            a.assessment_grade_int,
+            a.period,
+            a.benchmark_goal_season,
+            a.aggregated_measure_standard_level,
+            a.foundation_measure_standard_level,
 
-    e.school,
-    e.entrydate,
-    e.exitdate,
+            e.school,
 
-    f.grade_goal,
-    f.grade_range_goal,
+            f.grade_goal,
+            f.grade_range_goal,
 
-from {{ ref("int_amplify__all_assessments") }} as a
-inner join
-    {{ ref("int_extracts__student_enrollments") }} as e
-    on a.academic_year = e.academic_year
-    and a.region = e.region
-    and a.student_number = e.student_number
-    and a.assessment_grade_int = e.grade_level
-    and a.client_date between e.entrydate and e.exitdate
-left join
-    {{ ref("stg_google_sheets__dibels_foundation_goals") }} as f
-    on a.academic_year = f.academic_year
-    and a.region = f.region
-    and a.assessment_grade_int = f.grade_level
-    and a.benchmark_goal_season = f.period
-    and a.foundation_measure_standard_level = f.grade_goal_type
-where
-    a.academic_year >= 2024
-    and a.assessment_type = 'Benchmark'
-    and a.measure_standard = 'Composite'
-    and a.period != 'EOY'
+            count(distinct a.student_number) over (
+                partition by a.academic_year, e.school, a.period, a.assessment_grade
+            ) as n_admin_season_school_gl_all,
+
+            count(distinct a.student_number) over (
+                partition by
+                    a.academic_year,
+                    e.school,
+                    a.period,
+                    a.assessment_grade,
+                    a.aggregated_measure_standard_level
+            ) as n_admin_season_school_gl_agg_measure_standard_level,
+
+            count(distinct a.student_number) over (
+                partition by a.academic_year, e.region, a.period, a.assessment_grade
+            ) as n_admin_season_region_gl_all,
+
+            count(distinct a.student_number) over (
+                partition by
+                    a.academic_year,
+                    e.region,
+                    a.period,
+                    a.assessment_grade,
+                    a.aggregated_measure_standard_level
+            ) as n_admin_season_region_gl_agg_measure_standard_level,
+
+            row_number() over (
+                partition by
+                    a.academic_year,
+                    a.region,
+                    a.assessment_grade,
+                    a.period,
+                    a.benchmark_goal_season,
+                    a.aggregated_measure_standard_level,
+                    e.school
+            ) as rn,
+
+        from {{ ref("int_amplify__all_assessments") }} as a
+        inner join
+            {{ ref("int_extracts__student_enrollments") }} as e
+            on a.academic_year = e.academic_year
+            and a.region = e.region
+            and a.student_number = e.student_number
+            and a.assessment_grade_int = e.grade_level
+            and a.client_date between e.entrydate and e.exitdate
+        left join
+            {{ ref("stg_google_sheets__dibels_foundation_goals") }} as f
+            on a.academic_year = f.academic_year
+            and a.region = f.region
+            and a.assessment_grade_int = f.grade_level
+            and a.benchmark_goal_season = f.period
+            and a.foundation_measure_standard_level = f.grade_goal_type
+        where
+            a.academic_year >= 2024
+            and a.assessment_type = 'Benchmark'
+            and a.measure_standard = 'Composite'
+            and a.period != 'EOY'
+    )
+
+select *,
+from roster
+where rn = 1
