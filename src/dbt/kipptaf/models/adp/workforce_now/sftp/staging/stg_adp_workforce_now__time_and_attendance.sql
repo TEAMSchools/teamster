0e7b697ea0fe8079d16c7158df626689526d4d94
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-07-22 16:52:25 +0000

    revert: reenable t&a

--- src/dbt/kipptaf/models/adp/workforce_now/sftp/staging/stg_adp_workforce_now__time_and_attendance.sql
+++ src/dbt/kipptaf/models/adp/workforce_now/sftp/staging/stg_adp_workforce_now__time_and_attendance.sql
@@ -5,7 +5,5 @@ select
     supervisor_id,
 
     left(supervisor_position, 1) as supervisor_flag,
-
-    parse_date('%m/%d/%Y', start_using_time_attendance) as start_using_time_attendance,
-    parse_date('%m/%d/%Y', stop_using_time_attendance) as stop_using_time_attendance,
 from {{ source("adp_workforce_now", "src_adp_workforce_now__time_and_attendance") }}
+where pay_class != 'NON-TIME'
