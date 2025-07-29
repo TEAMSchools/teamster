f747a4d86110c2b392475c535abce1d5541d49a6
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-07-29 21:31:36 +0000

    add: dimensional model and semantic model config yml

---
+++ src/dbt/kipptaf/metrics/time_spine.sql
@@ -0,0 +1,20 @@
+{{ 
+   config(
+       materialized = 'table'
+   ) 
+}}
+
+with days as (
+   {{ dbt_utils.date_spine(
+       datepart="day",
+       start_date="'2000-01-01'",
+       end_date="'2027-01-01'"
+   ) }}
+),
+
+final as (
+   select cast(date_day as date) as date_day
+   from days
+)
+
+select * from final
---
+++ src/dbt/kipptaf/models/marts/dim_staff.sql
@@ -0,0 +1,15 @@
+with staff_roster as (select * from {{ ref("int_people__staff_roster") }})
+
+select
+    employee_number,
+    home_business_unit_name as entity,
+    home_work_location_name as `location`,
+    home_work_location_grade_band as grade_band,
+    home_department_name as department,
+    job_title,
+    reports_to_formatted_name as manager,
+    worker_original_hire_date,
+    work_assignment_actual_start_date,
+    assignment_status,
+    race_ethnicity_reporting,
+from staff_roster
