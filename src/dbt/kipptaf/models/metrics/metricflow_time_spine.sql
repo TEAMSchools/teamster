c0afb5506f522eecb0f9ced038f4f7ebccaa61ea
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-07-29 22:12:24 +0000

    format

--- src/dbt/kipptaf/models/metrics/metricflow_time_spine.sql
+++ src/dbt/kipptaf/models/metrics/metricflow_time_spine.sql
@@ -1,20 +1,15 @@
-{{ 
-   config(
-       materialized = 'table'
-   ) 
-}}
+{{ config(materialized="table") }}
 
-with days as (
-   {{ dbt_utils.date_spine(
-       datepart="day",
-       start_date="'2000-01-01'",
-       end_date="'2027-01-01'"
-   ) }}
-),
+with
+    days as (
+        {{
+            dbt_utils.date_spine(
+                datepart="day", start_date="'2000-01-01'", end_date="'2027-01-01'"
+            )
+        }}
+    ),
 
-final as (
-   select cast(date_day as date) as date_day
-   from days
-)
+    final as (select cast(date_day as date) as date_day, from days)
 
-select * from final
+select *
+from final
