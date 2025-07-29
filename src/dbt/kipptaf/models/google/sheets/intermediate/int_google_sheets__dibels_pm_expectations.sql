a026bc02ea671b17a3ef202d2fda776207043f3c
Gaby Rangel <grangel@kippteamandfamily.org>
2025-07-29 14:52:51 +0000

    utils

--- src/dbt/kipptaf/models/google/sheets/intermediate/int_google_sheets__dibels_pm_expectations.sql
+++ src/dbt/kipptaf/models/google/sheets/intermediate/int_google_sheets__dibels_pm_expectations.sql
@@ -29,61 +29,63 @@ with
             and t.type = 'LIT'
             and t.name in ('BOY->MOY', 'MOY->EOY')
         where s.state_excludefromreporting = 0
-    )
+    ),
 
-select
-    e.academic_year,
-    e.region,
-    e.grade,
-    e.admin_season,
-    e.`round`,
-    e.min_pm_round,
-    e.max_pm_round,
-    e.expected_measure_name_code,
-    e.expected_measure_standard,
-    e.pm_goal_include,
-    e.pm_goal_criteria,
-
-    t.code,
-    t.start_date,
-    t.end_date,
-
-    d.pm_round_days,
-    d.pm_days,
-
-    g.admin_season as benchmark_season,
-    g.grade_level_standard as benchmark_goal,
-
-from {{ ref("stg_google_sheets__dibels_expected_assessments") }} as e
-inner join
-    {{ ref("stg_reporting__terms") }} as t
-    on e.academic_year = t.academic_year
-    and e.region = t.region
-    and e.admin_season = t.name
-    and e.test_code = t.code
-    and e.assessment_type = 'PM'
-    and t.type = 'LIT'
-left join
-    pm_round_days as d
-    on t.academic_year = d.academic_year
-    and t.region = d.region
-    and t.name = d.name
-    and e.`round` = d.`round`
-left join
-    {{ ref("stg_google_sheets__dibels_goals_long") }} as g
-    on e.expected_measure_standard = g.measure_standard
-    and e.grade = g.grade_level
-    and e.admin_season = g.matching_pm_season
-where e.academic_year >= 2024
-qualify
-    row_number() over (
-        partition by
+    final as (
+        select
             e.academic_year,
             e.region,
             e.grade,
             e.admin_season,
-            e.round,
+            e.`round`,
+            e.min_pm_round,
+            e.max_pm_round,
+            e.expected_measure_name_code,
             e.expected_measure_standard,
-            d.pm_round_days
+            e.pm_goal_include,
+            e.pm_goal_criteria,
+
+            t.code,
+            t.start_date,
+            t.end_date,
+
+            d.pm_round_days,
+            d.pm_days,
+
+            g.admin_season as benchmark_season,
+            g.grade_level_standard as benchmark_goal,
+
+        from {{ ref("stg_google_sheets__dibels_expected_assessments") }} as e
+        inner join
+            {{ ref("stg_reporting__terms") }} as t
+            on e.academic_year = t.academic_year
+            and e.region = t.region
+            and e.admin_season = t.name
+            and e.test_code = t.code
+            and e.assessment_type = 'PM'
+            and t.type = 'LIT'
+        left join
+            pm_round_days as d
+            on t.academic_year = d.academic_year
+            and t.region = d.region
+            and t.name = d.name
+            and e.`round` = d.`round`
+        left join
+            {{ ref("stg_google_sheets__dibels_goals_long") }} as g
+            on e.expected_measure_standard = g.measure_standard
+            and e.grade = g.grade_level
+            and e.admin_season = g.matching_pm_season
+        where e.academic_year >= 2024
+    ),
+
+    deduplicate as (
+        {{
+            dbt_utils.deduplicate(
+                relation="final",
+                partition_by="academic_year, region, grade, admin_season, round, expected_measure_standard, pm_round_days",
+            )
+        }}
     )
-    = 1
+
+select *
+from deduplicate
