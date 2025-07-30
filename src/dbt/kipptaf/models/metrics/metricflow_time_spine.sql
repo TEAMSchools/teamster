183e37a3fcb4668691d638fce0b9cf271222bcd4
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-07-30 16:23:14 +0000

    refactor: time spine

--- src/dbt/kipptaf/models/metrics/metricflow_time_spine.sql
+++ src/dbt/kipptaf/models/metrics/metricflow_time_spine.sql
@@ -1,15 +1,5 @@
-{{ config(materialized="table") }}
-
-with
-    days as (
-        {{
-            dbt_utils.date_spine(
-                datepart="day", start_date="'2000-01-01'", end_date="'2027-01-01'"
-            )
-        }}
-    ),
-
-    final as (select cast(date_day as date) as date_day, from days)
-
-select *,
-from final
+select date_day,
+from
+    unnest(
+        generate_date_array('2002-07-01', '{{ var("current_fiscal_year" ) }}-06-30')
+    ) as date_day
