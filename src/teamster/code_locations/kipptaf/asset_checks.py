e063a0cae9cf94cc49d0d1e5c48d634a04da7181
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-07-31 22:37:22 +0000

    chore: freshness check asset keys

--- src/teamster/code_locations/kipptaf/asset_checks.py
+++ src/teamster/code_locations/kipptaf/asset_checks.py
@@ -5,8 +5,10 @@ from dagster import AssetKey, build_last_update_freshness_checks
 from teamster.code_locations.kipptaf import LOCAL_TIMEZONE
 
 adp_wfn_asset_selection = [
-    AssetKey(["kipptaf", "adp_workforce_now", "stg_adp_workforce_now__workers"]),
+    AssetKey(["kipptaf", "people", "int_people__staff_roster"]),
+    AssetKey(["kipptaf", "people", "int_people__staff_roster_history"]),
     AssetKey(["kipptaf", "people", "stg_people__employee_numbers"]),
+    AssetKey(["kipptaf", "adp_workforce_now", "stg_adp_workforce_now__workers"]),
     AssetKey(
         [
             "kipptaf",
@@ -19,13 +21,6 @@ adp_wfn_asset_selection = [
             "kipptaf",
             "adp_workforce_now",
             "stg_adp_workforce_now__workers__work_assignments__reports_to",
-        ]
-    ),
-    AssetKey(
-        [
-            "kipptaf",
-            "adp_workforce_now",
-            "int_adp_workforce_now__workers__work_assignments__organizational_units__pivot",
         ]
     ),
 ]
