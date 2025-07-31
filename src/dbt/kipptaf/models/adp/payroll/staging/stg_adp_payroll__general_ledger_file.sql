41afaf7aed9d91ab472270d7a78ba7bb71dd0288
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-07-31 18:58:54 +0000

    fix: col types

--- src/dbt/kipptaf/models/adp/payroll/staging/stg_adp_payroll__general_ledger_file.sql
+++ src/dbt/kipptaf/models/adp/payroll/staging/stg_adp_payroll__general_ledger_file.sql
@@ -11,18 +11,27 @@ select
     position_id,
     sourceentity as source_entity,
 
-    cast(acct_no as int) as acct_no,
-    cast(dept_id as int) as dept_id,
-    cast(file_number as int) as file_number,
-    cast(glentry_classid as int) as gl_entry_class_id,
-    cast(glentry_projectid as int) as gl_entry_project_id,
-    cast(job_title as int) as job_title,
-    cast(line_no as int) as line_no,
-    cast(location_id as int) as location_id,
-    cast(reference_no as int) as reference_no,
-
     cast(credit as numeric) as credit,
     cast(debit as numeric) as debit,
+
+    cast(cast(dept_id as numeric) as int) as dept_id,
+    cast(cast(file_number as numeric) as int) as file_number,
+    cast(cast(glentry_classid as numeric) as int) as gl_entry_class_id,
+    cast(cast(glentry_projectid as numeric) as int) as gl_entry_project_id,
+    cast(cast(job_title as numeric) as int) as job_title,
+
+    cast(
+        cast(
+            if(regexp_contains(acct_no, r'[A-Za-z\s]'), null, acct_no) as numeric
+        ) as int
+    ) as acct_no,
+    cast(
+        cast(
+            if(
+                regexp_contains(location_id, r'[A-Za-z\s]'), null, location_id
+            ) as numeric
+        ) as int
+    ) as location_id,
 
     parse_date('%m/%d/%Y', `date`) as `date`,
 from {{ source("adp_payroll", "src_adp_payroll__general_ledger_file") }}
