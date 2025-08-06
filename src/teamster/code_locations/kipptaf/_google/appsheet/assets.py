909af2fb15e32d6c503f694c3d7ed2421ee30cfe
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-06 19:58:24 +0000

    fix: appsheet keys

--- src/teamster/code_locations/kipptaf/_google/appsheet/assets.py
+++ src/teamster/code_locations/kipptaf/_google/appsheet/assets.py
@@ -1,10 +1,11 @@
 from dagster import AssetSpec
 
+from teamster.code_locations.kipptaf import CODE_LOCATION
 from teamster.code_locations.kipptaf._dbt.assets import manifest
 
 asset_specs = [
     AssetSpec(
-        key=source["meta"]["dagster"]["asset_key"],
+        key=[CODE_LOCATION, *source["meta"]["dagster"]["asset_key"]],
         metadata={
             "dataset_id": source["schema"],
             "table_id": source.get("identifier") or source["name"],
