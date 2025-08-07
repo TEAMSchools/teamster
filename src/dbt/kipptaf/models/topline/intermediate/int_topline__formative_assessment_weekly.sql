432c673ac0b5d2101d3374c89f45ce098102b686
Anthony Walters <awalters@kippteamandfamily.org>
2025-08-07 20:09:25 +0000

    refactor mastery calc

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__formative_assessment_weekly.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__formative_assessment_weekly.sql
@@ -1,4 +1,31 @@
 with
+    subject_discipline_crosswalk as (
+        select distinct illuminate_subject_area, discipline,
+        from {{ ref("stg_assessments__course_subject_crosswalk") }}
+    ),
+
+    responses_discipline as (
+        select
+            rr.powerschool_student_number,
+            rr.academic_year,
+            rr.response_type,
+            rr.subject_area,
+            rr.module_type,
+            rr.title,
+            rr.administered_at,
+            rr.is_mastery,
+
+            cx.discipline,
+
+            case
+                when rr.is_mastery then 1 when not rr.is_mastery then 0 else -1
+            end as is_mastery_int,
+        from {{ ref("int_assessments__response_rollup") }} as rr
+        inner join
+            subject_discipline_crosswalk as cx
+            on rr.subject_area = cx.illuminate_subject_area
+    ),
+
     assessment_weeks as (
         select
             sw.student_number,
@@ -11,19 +38,37 @@ with
             rr.module_type,
             rr.title,
             rr.administered_at,
-
-            case
-                when rr.is_mastery then 1 when not rr.is_mastery then 0
-            end as is_mastery_int,
+            rr.is_mastery,
+            rr.is_mastery_int,
         from {{ ref("int_extracts__student_enrollments_subjects_weeks") }} as sw
         left join
-            {{ ref("int_assessments__response_rollup") }} as rr
+            responses_discipline as rr
             on sw.student_number = rr.powerschool_student_number
             and sw.academic_year = rr.academic_year
-            and sw.illuminate_subject_area = rr.subject_area
+            and sw.discipline = rr.discipline
             and rr.administered_at between sw.week_start_monday and sw.week_end_sunday
             and rr.response_type = 'overall'
             and rr.module_type in ('QA', 'MQQ')
+    ),
+
+    calcs as (
+        select
+            student_number,
+            academic_year,
+            subject_area,
+            discipline,
+            module_type,
+            title,
+            administered_at,
+            week_start_monday,
+            week_end_sunday,
+            is_mastery_int,
+
+            last_value(is_mastery_int ignore nulls) over (
+                partition by student_number, discipline, academic_year
+                order by week_start_monday asc
+            ) as mastery_as_of_week,
+        from assessment_weeks
     )
 
 select
@@ -37,9 +82,7 @@ select
     week_start_monday,
     week_end_sunday,
     is_mastery_int,
+    mastery_as_of_week,
 
-    last_value(is_mastery_int ignore nulls) over (
-        partition by student_number, discipline, academic_year
-        order by week_start_monday asc
-    ) as mastery_as_of_week,
-from assessment_weeks
+    if(mastery_as_of_week = -1, null, mastery_as_of_week) as is_mastery_running_int,
+from calcs
