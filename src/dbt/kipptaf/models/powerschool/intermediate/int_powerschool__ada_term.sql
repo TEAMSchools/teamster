6765bd7b0fa3c25f0120359632d9cfcd760ca118
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-07 17:06:55 +0000

    refactor: combine ada detail with base mem view

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada_term.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada_term.sql
@@ -5,23 +5,29 @@ with
             studentid,
             academic_year,
             semester,
-            `quarter`,
+            term,
 
-            sum(is_present_weighted) as sum_attendance_value_term_weighted,
+            sum(is_present_weighted) as sum_attendance_value_weighted_term,
             sum(attendancevalue) as sum_attendance_value_term,
+
             count(attendancevalue) as count_attendance_value_term,
-            round(avg(is_present), 3) as ada_term,
-        from {{ ref("int_powerschool__ada_detail") }}
-        where calendardate <= current_date('{{ var("local_timezone") }}')
-        group by _dbt_source_relation, studentid, academic_year, semester, quarter
+
+            avg(attendancevalue) as ada_term,
+        from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }}
+        where
+            membershipvalue = 1
+            and attendancevalue is not null
+            and calendardate <= current_date('{{ var("local_timezone") }}')
+        group by _dbt_source_relation, studentid, academic_year, semester, term
     )
 
 select
     _dbt_source_relation,
     studentid,
     academic_year,
-    `quarter`,
-    ada_term,
+    term,
+
+    round(ada_term, 3) as ada_term,
 
     round(
         (
@@ -60,12 +66,12 @@ select
             (
                 sum(sum_attendance_value_term) over (
                     partition by _dbt_source_relation, studentid, academic_year
-                    order by `quarter` asc
+                    order by term asc
                 )
             ) / (
                 sum(count_attendance_value_term) over (
                     partition by _dbt_source_relation, studentid, academic_year
-                    order by `quarter` asc
+                    order by term asc
                 )
             )
         ),
@@ -75,17 +81,17 @@ select
     round(
         (
             (
-                sum(sum_attendance_value_term_weighted) over (
+                sum(sum_attendance_value_weighted_term) over (
                     partition by _dbt_source_relation, studentid, academic_year
-                    order by `quarter` asc
+                    order by term asc
                 )
             ) / (
                 sum(count_attendance_value_term) over (
                     partition by _dbt_source_relation, studentid, academic_year
-                    order by `quarter` asc
+                    order by term asc
                 )
             )
         ),
         3
-    ) as ada_quarter_running_weighted,
+    ) as ada_weighted_quarter_running,
 from ada_by_term
