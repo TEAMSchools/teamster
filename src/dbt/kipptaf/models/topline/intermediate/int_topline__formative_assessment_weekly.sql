011be10c8cb3211888845a1866be6049b0d7465c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-07 21:37:52 +0000

    refactor: combine ctes

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__formative_assessment_weekly.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__formative_assessment_weekly.sql
@@ -31,8 +31,11 @@ with
             rr.module_type,
             rr.title,
             rr.administered_at,
-            rr.is_mastery,
-            rr.is_mastery_int,
+
+            last_value(rr.is_mastery_int ignore nulls) over (
+                partition by sw.student_number, sw.discipline, sw.academic_year
+                order by sw.week_start_monday asc
+            ) as mastery_as_of_week,
         from {{ ref("int_extracts__student_enrollments_subjects_weeks") }} as sw
         left join
             responses_discipline as rr
@@ -40,26 +43,6 @@ with
             and sw.academic_year = rr.academic_year
             and sw.discipline = rr.discipline
             and rr.administered_at between sw.week_start_monday and sw.week_end_sunday
-    ),
-
-    calcs as (
-        select
-            student_number,
-            academic_year,
-            subject_area,
-            discipline,
-            module_type,
-            title,
-            administered_at,
-            week_start_monday,
-            week_end_sunday,
-            is_mastery_int,
-
-            last_value(is_mastery_int ignore nulls) over (
-                partition by student_number, discipline, academic_year
-                order by week_start_monday asc
-            ) as mastery_as_of_week,
-        from assessment_weeks
     )
 
 select
@@ -73,7 +56,6 @@ select
     week_start_monday,
     week_end_sunday,
     is_mastery_int,
-    mastery_as_of_week,
 
     if(mastery_as_of_week = -1, null, mastery_as_of_week) as is_mastery_running_int,
-from calcs
+from assessment_weeks
