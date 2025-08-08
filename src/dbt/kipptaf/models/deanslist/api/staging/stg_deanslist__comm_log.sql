0131785646601a1bbfa5eb5eae820ef7933337f0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-08 19:07:48 +0000

    refactor

--- src/dbt/kipptaf/models/deanslist/api/staging/stg_deanslist__comm_log.sql
+++ src/dbt/kipptaf/models/deanslist/api/staging/stg_deanslist__comm_log.sql
@@ -1,9 +1,24 @@
-{{
-    dbt_utils.union_relations(
-        relations=[
-            source("kippnewark_deanslist", model.name),
-            source("kippcamden_deanslist", model.name),
-            source("kippmiami_deanslist", model.name),
-        ]
+with
+    union_relations as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    source("kippnewark_deanslist", "stg_deanslist__comm_log"),
+                    source("kippcamden_deanslist", "stg_deanslist__comm_log"),
+                    source("kippmiami_deanslist", "stg_deanslist__comm_log"),
+                ]
+            )
+        }}
     )
-}}
+
+select
+    *,
+
+    cast(call_date_time as date) as call_date_time_date,
+
+    {{
+        date_to_fiscal_year(
+            date_field="call_date_time", start_month=7, year_source="start"
+        )
+    }} as academic_year,
+from union_relations
