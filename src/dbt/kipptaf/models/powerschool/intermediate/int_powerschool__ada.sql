feebd03dfd393df111f9a97cced689fcdfc7864a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-08 19:31:44 +0000

    refactor: dbt join

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada.sql
@@ -12,10 +12,5 @@ with
     )
 
 -- trunk-ignore(sqlfluff/AM04)
-select
-    *,
-
-    yearid + 1990 as academic_year,
-
-    regexp_extract(_dbt_source_relation, r'(kipp\w+)_') as _dbt_source_project,
+select *, yearid + 1990 as academic_year
 from union_relations
