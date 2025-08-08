b4f62c65d0849d93e3417402d9109add77bd78e6
Gaby Rangel <grangel@kippteamandfamily.org>
2025-08-08 18:41:55 +0000

    updated weighted vs unweighted calcs for athletic elig

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada_term_pivot.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada_term_pivot.sql
@@ -8,6 +8,10 @@ select
     round(ada_term_q2, 3) as ada_term_q2,
     round(ada_term_q3, 3) as ada_term_q3,
     round(ada_term_q4, 3) as ada_term_q4,
+    round(ada_weighted_term_q1, 3) as ada_weighted_term_q1,
+    round(ada_weighted_term_q2, 3) as ada_weighted_term_q2,
+    round(ada_weighted_term_q3, 3) as ada_weighted_term_q3,
+    round(ada_weighted_term_q4, 3) as ada_weighted_term_q4,
     round(ada_year_running_q1, 3) as ada_year_running_q1,
     round(ada_year_running_q2, 3) as ada_year_running_q2,
     round(ada_year_running_q3, 3) as ada_year_running_q3,
@@ -20,12 +24,26 @@ select
     round(coalesce(ada_semester_q2, ada_semester_q1), 3) as ada_semester_s1,
     round(coalesce(ada_semester_q4, ada_semester_q3), 3) as ada_semester_s2,
     round(coalesce(ada_year_q4, ada_year_q3, ada_year_q2, ada_year_q1), 3) as ada_year,
+
+    round(
+        coalesce(ada_weighted_semester_q2, ada_weighted_semester_q1), 3
+    ) as ada_weighted_semester_s1,
+    round(
+        coalesce(ada_weighted_semester_q4, ada_weighted_semester_q3), 3
+    ) as ada_weighted_semester_s2,
+    round(
+        coalesce(ada_year_q4, ada_year_q3, ada_year_q2, ada_year_q1), 3
+    ) as ada_weighted_year,
+    
 from
     {{ ref("int_powerschool__ada_term") }} pivot (
         max(ada_term) as ada_term,
         max(ada_semester) as ada_semester,
         max(ada_year) as ada_year,
         max(ada_year_running) as ada_year_running,
+        max(ada_weighted_term) as ada_weighted_term,
+        max(ada_weighted_semester) as ada_weighted_semester,
+        max(ada_weighted_year) as ada_weighted_year,
         max(ada_weighted_year_running) as ada_weighted_year_running for term
         in ('Q1', 'Q2', 'Q3', 'Q4')
     )
