4d24ac219a5fa3be72006d7afdfffb1c88dad495
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-11 16:01:21 +0000

    feat: add sat/act snapshot

--- src/dbt/kipptaf/models/kippadb/intermediate/int_kippadb__standardized_test_rollup.sql
+++ src/dbt/kipptaf/models/kippadb/intermediate/int_kippadb__standardized_test_rollup.sql
@@ -1,13 +1,21 @@
+{{ config(materialized="ephemeral") }}
+
+with
+    tests as (
+        select school_specific_id, test_type, test_subject, score,
+        from {{ ref("int_kippadb__standardized_test_unpivot") }}
+        where test_type in ('SAT', 'ACT') and rn_highest = 1
+    )
+
 select school_specific_id, test_type, test_subject, score,
-from {{ ref("int_kippadb__standardized_test_unpivot") }}
-where test_type in ('SAT', 'ACT') and rn_highest = 1
+from tests
 
 union all
 
 /* SAT superscore - sum of highest EBRW and Math */
 select school_specific_id, test_type, 'Superscore' as test_subject, sum(score) as score,
-from {{ ref("int_kippadb__standardized_test_unpivot") }}
-where rn_highest = 1 and test_type = 'SAT' and test_subject in ('EBRW', 'Math')
+from tests
+where test_type = 'SAT' and test_subject in ('EBRW', 'Math')
 group by school_specific_id, test_type
 
 union all
@@ -16,11 +24,10 @@ union all
 select
     school_specific_id,
     test_type,
+
     'Superscore' as test_subject,
+
     round(avg(score), 1) as score,
 from {{ ref("int_kippadb__standardized_test_unpivot") }}
-where
-    rn_highest = 1
-    and test_type = 'ACT'
-    and test_subject in ('English', 'Math', 'Reading', 'Science')
+where test_type = 'ACT' and test_subject in ('English', 'Math', 'Reading', 'Science')
 group by school_specific_id, test_type
