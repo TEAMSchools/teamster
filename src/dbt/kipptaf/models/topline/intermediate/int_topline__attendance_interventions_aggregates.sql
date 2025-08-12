c3e1b8143b00e990a5543a5e2aa02864d8520821
Anthony Walters <awalters@kippteamandfamily.org>
2025-08-12 21:12:53 +0000

    tunk

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__attendance_interventions_aggregates.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__attendance_interventions_aggregates.sql
@@ -5,6 +5,6 @@ select
 
     sum(intervention_status_required_int) as successful_call_count,
     count(intervention_status_required_int) as total_anticipated_calls,
-    avg(intervention_status_required_int) as pct_interventions_complete
+    avg(intervention_status_required_int) as pct_interventions_complete,
 from {{ ref("int_topline__attendance_interventions") }}
 group by student_number, academic_year, schoolid
