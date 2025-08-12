cb6eee8abea4fcd9ff3e8445f9105cb4d4af3cdf
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-12 22:39:29 +0000

    refactor: year calc to ps core

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__ada.sql
@@ -1,16 +1,9 @@
-with
-    union_relations as (
-        {{
-            dbt_utils.union_relations(
-                relations=[
-                    source("kippnewark_powerschool", "int_powerschool__ada"),
-                    source("kippcamden_powerschool", "int_powerschool__ada"),
-                    source("kippmiami_powerschool", "int_powerschool__ada"),
-                ]
-            )
-        }}
+{{
+    dbt_utils.union_relations(
+        relations=[
+            source("kippnewark_powerschool", "int_powerschool__ada"),
+            source("kippcamden_powerschool", "int_powerschool__ada"),
+            source("kippmiami_powerschool", "int_powerschool__ada"),
+        ]
     )
-
--- trunk-ignore(sqlfluff/AM04)
-select *, yearid + 1990 as academic_year,
-from union_relations
+}}
