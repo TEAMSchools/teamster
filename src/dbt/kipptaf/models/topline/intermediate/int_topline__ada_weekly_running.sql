72a72358cd004adb91332e181562bd489c0228cb
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-13 19:29:05 +0000

    refactor: mv scaffold joins to metric views

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__ada_weekly_running.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__ada_weekly_running.sql
@@ -3,49 +3,83 @@ with
         select
             _dbt_source_relation,
             studentid,
+            academic_year,
             schoolid,
             week_start_monday,
             week_end_sunday,
 
-            yearid + 1990 as academic_year,
-            sum(attendancevalue) as weekly_attendance_value,
-            sum(membershipvalue) as weekly_membership_value,
+            sum(attendancevalue) as attendance_value_sum,
+            sum(membershipvalue) as membership_value_sum,
         from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }}
         where attendancevalue is not null
         group by
             _dbt_source_relation,
             studentid,
-            yearid,
+            academic_year,
             schoolid,
             week_start_monday,
             week_end_sunday
+    ),
+
+    running_calcs as (
+        select
+            _dbt_source_relation,
+            studentid,
+            schoolid,
+            academic_year,
+            week_start_monday,
+            week_end_sunday,
+            attendance_value_sum,
+            membership_value_sum,
+
+            sum(attendance_value_sum) over (
+                partition by studentid, academic_year, schoolid
+                order by week_start_monday asc
+            ) as attendance_value_sum_running,
+
+            sum(membership_value_sum) over (
+                partition by studentid, academic_year, schoolid
+                order by week_start_monday asc
+            ) as membership_value_sum_running,
+        from agg_weeks
     )
 
 select
-    _dbt_source_relation,
-    studentid,
-    schoolid,
-    academic_year,
-    week_start_monday,
-    week_end_sunday,
+    co.academic_year,
+    co.region,
+    co.school_level,
+    co.schoolid,
+    co.school,
+    co.studentid,
+    co.student_number,
+    co.state_studentnumber,
+    co.student_name,
+    co.grade_level,
+    co.gender,
+    co.ethnicity,
+    co.iep_status,
+    co.is_504,
+    co.lep_status,
+    co.gifted_and_talented,
+    co.entrydate,
+    co.exitdate,
+    co.enroll_status,
 
-    sum(weekly_attendance_value) over (
-        partition by studentid, academic_year, schoolid order by week_start_monday
-    ) as attendancevalue_running,
-    sum(weekly_membership_value) over (
-        partition by studentid, academic_year, schoolid order by week_start_monday
-    ) as membershipvalue_running,
+    rc.week_start_monday,
+    rc.week_end_sunday,
+    rc.attendance_value_sum,
+    rc.membership_value_sum,
+    rc.attendance_value_sum_running,
+    rc.membership_value_sum_running,
+
     round(
-        safe_divide(
-            sum(weekly_attendance_value) over (
-                partition by studentid, academic_year, schoolid
-                order by week_start_monday
-            ),
-            sum(weekly_membership_value) over (
-                partition by studentid, academic_year, schoolid
-                order by week_start_monday
-            )
-        ),
-        3
+        safe_divide(attendance_value_sum_running, membership_value_sum_running), 3
     ) as ada_running,
-from agg_weeks
+from {{ ref("int_extracts__student_enrollments") }} as co
+left join
+    running_calcs as rc
+    on co.studentid = rc.studentid
+    and co.schoolid = rc.schoolid
+    and co.academic_year = rc.academic_year
+    and {{ union_dataset_join_clause(left_alias="co", right_alias="rc") }}
+where co.academic_year >= {{ var("current_academic_year") - 1 }}
--- src/dbt/kipptaf/models/topline/intermediate/int_topline__student_metrics.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__student_metrics.sql
@@ -1,129 +1,100 @@
-with
-    metrics_union_sid as (
-        select
-            'ADA Running' as metric_name,
-            _dbt_source_relation,
-            studentid,
-            academic_year,
-            schoolid,
-
-            'Week' as term_type,
-            week_start_monday as term_name,
-            week_start_monday as term_start,
-            week_end_sunday as term_end,
-
-            attendancevalue_running as numerator,
-            membershipvalue_running as denominator,
-            ada_running as metric_value,
-        from {{ ref("int_topline__ada_weekly_running") }}
-    ),
-
-    metrics_union_sn as (
-        select
-            'i-Ready Lessons Passed' as metric_name,
-            student_number,
-            academic_year,
-            schoolid,
-            'Week' as term_type,
-            week_start_monday as term_name,
-            week_start_monday as term_start,
-            week_end_sunday as term_end,
-            null as numerator,
-            null as denominator,
-            n_lessons_passed as metric_value,
-        from {{ ref("int_topline__iready_lessons_weeks") }}
-
-        union all
-
-        select
-            'i-Ready Diagnostic Proficient' as metric_name,
-            student_number,
-            academic_year,
-            schoolid,
-            'Week' as term_type,
-            week_start_monday as term_name,
-            week_start_monday as term_start,
-            week_end_sunday as term_end,
-            null as numerator,
-            null as denominator,
-
-            case
-                when is_proficient then 1 when not is_proficient then 0
-            end as metric_value,
-        from {{ ref("int_topline__iready_diagnostic_weeks") }}
-    )
-
 select
-    co.academic_year,
-    co.region,
-    co.school_level,
-    co.schoolid,
-    co.school,
-    co.student_number,
-    co.state_studentnumber,
-    co.student_name,
-    co.grade_level,
-    co.gender,
-    co.ethnicity,
-    co.iep_status,
-    co.is_504,
-    co.lep_status,
-    co.gifted_and_talented,
-    co.entrydate,
-    co.exitdate,
-    co.enroll_status,
+    academic_year,
+    region,
+    school_level,
+    schoolid,
+    school,
+    student_number,
+    state_studentnumber,
+    student_name,
+    grade_level,
+    gender,
+    ethnicity,
+    iep_status,
+    is_504,
+    lep_status,
+    gifted_and_talented,
+    entrydate,
+    exitdate,
+    enroll_status,
 
-    mu.metric_name,
-    mu.term_type,
-    mu.term_name,
-    mu.term_start,
-    mu.term_end,
-    mu.numerator,
-    mu.denominator,
-    mu.metric_value,
-from {{ ref("int_extracts__student_enrollments") }} as co
-left join
-    metrics_union_sid as mu
-    on co.studentid = mu.studentid
-    and co.schoolid = mu.schoolid
-    and co.academic_year = mu.academic_year
-    and {{ union_dataset_join_clause(left_alias="co", right_alias="mu") }}
-where co.academic_year >= {{ var("current_academic_year") - 1 }}
+    week_start_monday as term_name,
+    week_start_monday as term_start,
+    week_end_sunday as term_end,
+    attendancevalue_running as numerator,
+    membershipvalue_running as denominator,
+    ada_running as metric_value,
+
+    'ADA Running' as metric_name,
+    'Week' as term_type,
+from {{ ref("int_topline__ada_weekly_running") }}
 
 union all
 
 select
-    co.academic_year,
-    co.region,
-    co.school_level,
-    co.schoolid,
-    co.school,
-    co.student_number,
-    co.state_studentnumber,
-    co.student_name,
-    co.grade_level,
-    co.gender,
-    co.ethnicity,
-    co.iep_status,
-    co.is_504,
-    co.lep_status,
-    co.gifted_and_talented,
-    co.entrydate,
-    co.exitdate,
-    co.enroll_status,
+    academic_year,
+    region,
+    school_level,
+    schoolid,
+    school,
+    student_number,
+    state_studentnumber,
+    student_name,
+    grade_level,
+    gender,
+    ethnicity,
+    iep_status,
+    is_504,
+    lep_status,
+    gifted_and_talented,
+    entrydate,
+    exitdate,
+    enroll_status,
 
-    mu.metric_name,
-    mu.term_type,
-    mu.term_name,
-    mu.term_start,
-    mu.term_end,
-    mu.numerator,
-    mu.denominator,
-    mu.metric_value,
-from {{ ref("int_extracts__student_enrollments") }} as co
-left join
-    metrics_union_sn as mu
-    on co.student_number = mu.student_number
-    and co.schoolid = mu.schoolid
-    and co.academic_year = mu.academic_year
-where co.academic_year >= {{ var("current_academic_year") - 1 }}
+    week_start_monday as term_name,
+    week_start_monday as term_start,
+    week_end_sunday as term_end,
+
+    null as numerator,
+    null as denominator,
+
+    n_lessons_passed as metric_value,
+
+    'i-Ready Lessons Passed' as metric_name,
+    'Week' as term_type,
+from {{ ref("int_topline__iready_lessons_weeks") }}
+
+union all
+
+select
+    academic_year,
+    region,
+    school_level,
+    schoolid,
+    school,
+    student_number,
+    state_studentnumber,
+    student_name,
+    grade_level,
+    gender,
+    ethnicity,
+    iep_status,
+    is_504,
+    lep_status,
+    gifted_and_talented,
+    entrydate,
+    exitdate,
+    enroll_status,
+
+    week_start_monday as term_name,
+    week_start_monday as term_start,
+    week_end_sunday as term_end,
+
+    null as numerator,
+    null as denominator,
+
+    is_proficient as metric_value,
+
+    'i-Ready Diagnostic Proficient' as metric_name,
+    'Week' as term_type,
+from {{ ref("int_topline__iready_diagnostic_weeks") }}
