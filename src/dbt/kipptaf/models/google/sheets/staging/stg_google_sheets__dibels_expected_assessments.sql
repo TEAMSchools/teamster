2279f8d46588c78e4e55d4747d93df4483d55111
Gaby Rangel <grangel@kippteamandfamily.org>
2025-08-13 19:41:41 +0000

    edited expected assessments

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__dibels_expected_assessments.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__dibels_expected_assessments.sql
@@ -30,15 +30,26 @@ with
     )
 
 select
-    *,
+    m.*,
 
-    min(round_number) over (
-        partition by academic_year, region, admin_season, grade order by round_number
+    min(m.round_number) over (
+        partition by m.academic_year, m.region, m.admin_season, m.grade
+        order by m.round_number
     ) as min_pm_round,
 
-    max(round_number) over (
-        partition by academic_year, region, admin_season, grade
-        order by round_number desc
+    max(m.round_number) over (
+        partition by m.academic_year, m.region, m.admin_season, m.grade
+        order by m.round_number desc
     ) as max_pm_round,
 
-from modified
+    t.start_date,
+    t.end_date,
+
+from modified as m
+left join
+    {{ ref("stg_reporting__terms") }} as t
+    on m.academic_year = t.academic_year
+    and m.region = t.region
+    and m.admin_season = t.name
+    and m.test_code = t.code
+    and t.type = 'LIT'
