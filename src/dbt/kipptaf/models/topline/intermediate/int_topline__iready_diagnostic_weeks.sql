dd24c3386110ea8d0da314e30004e043a8db598e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-13 19:29:05 +0000

    refactor: mv scaffold joins to metric views

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__iready_diagnostic_weeks.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__iready_diagnostic_weeks.sql
@@ -1,17 +1,33 @@
 select
-    ir.student_id as student_number,
-    ir.schoolid,
-    ir.test_round,
-    ir.is_proficient,
-
-    subj as subject,
-
+    cw.student_number,
+    cw.state_studentnumber,
+    cw.student_name,
     cw.academic_year,
+    cw.iready_subject,
     cw.week_start_monday,
     cw.week_end_sunday,
     cw.week_number_academic_year,
-from {{ ref("int_powerschool__calendar_week") }} as cw
-cross join unnest(['Reading', 'Math']) as subj
+    cw.region,
+    cw.school_level,
+    cw.schoolid,
+    cw.school,
+    cw.grade_level,
+    cw.gender,
+    cw.ethnicity,
+    cw.iep_status,
+    cw.is_504,
+    cw.lep_status,
+    cw.gifted_and_talented,
+    cw.entrydate,
+    cw.exitdate,
+    cw.enroll_status,
+
+    rt.name as test_round,
+
+    case
+        when ir.is_proficient then 1 when not ir.is_proficient then 0
+    end as is_proficient,
+from {{ ref("int_extracts__student_enrollments_subjects_weeks") }} as cw
 inner join
     {{ ref("stg_reporting__terms") }} as rt
     on cw.academic_year = rt.academic_year
@@ -20,8 +36,10 @@ inner join
     and rt.type = 'IREX'
 left join
     {{ ref("base_iready__diagnostic_results") }} as ir
-    on rt.academic_year = ir.academic_year_int
-    and rt.name = ir.test_round
+    on cw.student_number = ir.student_id
+    and cw.academic_year = ir.academic_year_int
+    and cw.iready_subject = ir.subject
+    and {{ union_dataset_join_clause(left_alias="cw", right_alias="ir") }}
     and rt.region = ir.region
-    and subj = ir.subject
+    and rt.name = ir.test_round
     and ir.rn_subj_round = 1
