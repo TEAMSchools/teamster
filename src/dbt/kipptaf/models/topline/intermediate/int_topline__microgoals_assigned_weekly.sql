f982a54654162d0a226927af59d67497945a5732
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-08-14 16:05:10 +0000

    create int view for microgoal count

---
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__microgoals_assigned_weekly.sql
@@ -0,0 +1,57 @@
+with
+    teachers as (
+        select
+            employee_number,
+            home_work_location_powerschool_school_id as school_id,
+            effective_date_start,
+            effective_date_end,
+        from {{ ref("int_people__staff_roster_history") }}
+        where
+            primary_indicator
+            and assignment_status = 'Active'
+            and job_title in (
+                'Teacher',
+                'Teacher in Residence',
+                'ESE Teacher',
+                'Learning Specialist',
+                'Teacher ESL',
+                'Teacher in Residence ESL'
+            )
+    ),
+
+    grow_users as (
+        select user_id, internal_id_int from {{ ref("stg_schoolmint_grow__users") }}
+    ),
+
+    microgoals as (
+        select user_id, assignment_id, created_date_local,
+        from {{ ref("stg_schoolmint_grow__assignments") }}
+    ),
+
+    calendar as (select * from {{ ref("int_powerschool__calendar_week") }}),
+
+    final as (
+        select
+            teachers.employee_number,
+            teachers.school_id,
+            calendar.week_start_monday,
+            calendar.week_end_sunday,
+            count(distinct(microgoals.assignment_id)) as microgoals_assigned,
+        from teachers
+        left join grow_users on teachers.employee_number = grow_users.internal_id_int
+        left join microgoals on grow_users.user_id = microgoals.user_id
+        left join
+            calendar
+            on teachers.school_id = calendar.schoolid
+            and microgoals.created_date_local
+            between calendar.week_start_monday and calendar.week_end_sunday
+        group by all
+    )
+
+select
+    employee_number,
+    school_id,
+    week_start_monday,
+    week_end_sunday,
+    coalesce(microgoals_assigned, 0) as microgoals_assigned,
+from final
