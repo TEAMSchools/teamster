c09561ecd0b22f3edb86cdef2f207ff0b8fba052
Anthony Walters <awalters@kippteamandfamily.org>
2025-08-16 19:23:53 +0000

    updates to calendar_weeks

--- src/dbt/kipptaf/models/extracts/intermediate/int_extracts__student_enrollments_weeks.sql
+++ src/dbt/kipptaf/models/extracts/intermediate/int_extracts__student_enrollments_weeks.sql
@@ -9,23 +9,11 @@ select
     cw.school_week_end_date,
     cw.week_number_academic_year,
     cw.week_number_quarter,
+    cw.is_current_week_mon_sun,
 
     if(
         cw.week_start_monday between co.entrydate and co.exitdate, true, false
     ) as is_enrolled_week,
-    case
-        when
-            co.academic_year = {{ var("current_academic_year") }}
-            and current_date('America/New_York')
-            between cw.week_start_monday and cw.week_end_sunday
-        then true
-        when
-            cw.week_start_monday = max(cw.week_start_monday) over (
-                partition by co.academic_year, co.schoolid
-            )
-        then true
-        else false
-    end as is_current_week,
 from {{ ref("int_extracts__student_enrollments") }} as co
 inner join
     {{ ref("int_powerschool__calendar_week") }} as cw
