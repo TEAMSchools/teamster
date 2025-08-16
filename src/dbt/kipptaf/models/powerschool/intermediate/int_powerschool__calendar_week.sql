a10f4295bd9bf512eb5a992916db02c48cbd0695
Anthony Walters <awalters@kippteamandfamily.org>
2025-08-16 19:23:53 +0000

    updates to calendar_weeks

--- src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__calendar_week.sql
+++ src/dbt/kipptaf/models/powerschool/intermediate/int_powerschool__calendar_week.sql
@@ -16,5 +16,19 @@ with
     )
 
 -- trunk-ignore(sqlfluff/AM04)
-select *, initcap(regexp_extract(_dbt_source_relation, r'kipp(\w+)_')) as region,
+select
+    *,
+    initcap(regexp_extract(_dbt_source_relation, r'kipp(\w+)_')) as region,
+    case
+        when
+            academic_year = {{ var("current_academic_year") }}
+            and current_date('America/New_York')
+            between week_start_monday and week_end_sunday
+        then true
+        when
+            week_start_monday
+            = max(week_start_monday) over (partition by academic_year, schoolid)
+        then true
+        else false
+    end as is_current_week_mon_sun,
 from union_relations
