8fafdf4bd6a6a6d87adb23ffd2635c42ec3458b2
Gaby Rangel <grangel@kippteamandfamily.org>
2025-08-20 18:23:25 +0000

    save

--- src/dbt/kipptaf/models/students/intermediate/int_students__college_assessment_participation_roster.sql
+++ src/dbt/kipptaf/models/students/intermediate/int_students__college_assessment_participation_roster.sql
@@ -4,12 +4,20 @@ select
     e.salesforce_id,
     e.grade_level,
 
+    a.scope,
     a.test_month,
     a.score_type,
 
 from {{ ref("int_extracts__student_enrollments") }} as e
-inner join
+left join
     {{ ref("int_assessments__college_assessment") }} as a
     on e.academic_year = a.academic_year
     and e.student_number = a.student_number
+    and a.score_type in (
+        'act_composite',
+        'sat_total_score',
+        'psat89_total',
+        'psatnmsqt_total',
+        'psat10_total'
+    )
 where e.school_level = 'HS' and e.rn_year = 1
