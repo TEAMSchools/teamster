3c7697f7a8909cbc4d6624aa2a8ce93769083b55
Gaby Rangel <grangel@kippteamandfamily.org>
2025-08-21 21:13:48 +0000

    replace reference on int all assessments

--- src/dbt/kipptaf/models/amplify/intermediate/int_amplify__all_assessments.sql
+++ src/dbt/kipptaf/models/amplify/intermediate/int_amplify__all_assessments.sql
@@ -33,7 +33,7 @@ with
             e.end_date,
             e.matching_pm_season as matching_season,
 
-        from {{ ref("stg_amplify__benchmark_student_summary") }} as bss
+        from {{ ref("int_amplify__mclass__benchmark_student_summary") }} as bss
         inner join
             {{ ref("int_amplify__benchmark_student_summary_unpivot") }} as u
             on bss.surrogate_key = u.surrogate_key
