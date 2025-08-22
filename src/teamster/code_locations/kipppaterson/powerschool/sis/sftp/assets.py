531a150aa84f7d0989890185c2fb3f5736be91e6
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-08-22 15:19:40 +0000

    fix: imposts

--- src/teamster/code_locations/kipppaterson/powerschool/sis/sftp/assets.py
+++ src/teamster/code_locations/kipppaterson/powerschool/sis/sftp/assets.py
@@ -10,19 +10,13 @@ from teamster.libraries.sftp.assets import (
 
 config_dir = pathlib.Path(__file__).parent / "config"
 
-powerschool_tables = [
+assets = [
     build_sftp_file_asset(
         asset_key=[CODE_LOCATION, "powerschool", "sis", "sftp", a["asset_name"]],
         remote_dir_regex=r"/data-team/kipppaterson/powerschool",
         remote_file_regex=rf"{a['asset_name']}\.csv",
         ssh_resource_key="ssh_couchdrop",
-        avro_schema=SCHEMAS.get(
-            a["asset_name"]
-        ),  # TODO: use regular dict selection syntax for prod
+        avro_schema=SCHEMAS[a["asset_name"]],
     )
     for a in config_from_files([(f"{config_dir}/assets.yaml")])["assets"]
-]
-
-assets = [
-    *powerschool_tables,
 ]
