65e710547b739c261ef6a84ce46650b518dc425a
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-08-22 15:14:52 +0000

    troubleshooting

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__recruitment_metrics.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__recruitment_metrics.sql
@@ -9,23 +9,22 @@ with
         select
             seat_tracker.staffing_model_id,
             locations.location_powerschool_school_id as school_id,
-            {# calendar.week_start_monday,
-            calendar.week_end_sunday, #}
+            calendar.week_start_monday,
+            calendar.week_end_sunday,
             if(seat_tracker.is_staffed, 1, 0) as is_staffed,
         from seat_tracker
         left join locations on seat_tracker.adp_location = locations.location_name
         inner join
             calendar
-            on calendar.week_end_sunday
+            on locations.location_powerschool_school_id = calendar.schoolid
+            and calendar.week_end_sunday
             between seat_tracker.valid_to and seat_tracker.valid_from
-        where calendar.academic_year = {{ var("current_academic_year") }}
-    )
+        
 
 select
-    staffing_model_id,
     school_id,
     week_start_monday,
     week_end_sunday,
     avg(is_staffed) as percent_staffed_current_year,
 from staffed_current_year
-group by staffing_model_id, school_id week_start_monday, week_end_sunday
+group by school_id, week_start_monday, week_end_sunday
