1e24d11d05b9d02e4172894214d815ad09f7066b
Gaby Rangel <grangel@kippteamandfamily.org>
2025-08-25 18:12:59 +0000

    replaced case statement with seasons stg table

--- src/dbt/kipptaf/models/students/intermediate/int_students__college_assessment_roster.sql
+++ src/dbt/kipptaf/models/students/intermediate/int_students__college_assessment_roster.sql
@@ -20,53 +20,16 @@ select
     a.max_scale_score,
     a.superscore,
 
-    /* i also have this as a reference table on the new CARAT g-sheet. when you move
-       it over to the IN folder, i will replace this long case with a join to it */
-    case
-        when a.scope like 'PSAT%' and a.test_month in ('August', 'September', 'October')
-        then 'BOY'
-        when
-            a.scope like 'PSAT%'
-            and a.test_month in ('November', 'December', 'January', 'February', 'March')
-        then 'MOY'
-        when a.scope like 'PSAT%' and a.test_month in ('April', 'May', 'June', 'July')
-        then 'EOY'
-        when
-            a.scope in ('SAT', 'ACT')
-            and e.grade_level = 11
-            and a.test_month in ('August', 'September')
-        then 'BOY'
-        when
-            a.scope in ('SAT', 'ACT')
-            and e.grade_level = 11
-            and a.test_month
-            in ('October', 'November', 'December', 'January', 'February', 'March')
-        then 'MOY'
-        when
-            a.scope in ('SAT', 'ACT')
-            and e.grade_level = 11
-            and a.test_month in ('April', 'May', 'June', 'July')
-        then 'EOY'
-        when
-            a.scope in ('SAT', 'ACT')
-            and e.grade_level = 12
-            and a.test_month in ('August', 'September', 'October')
-        then 'BOY'
-        when
-            a.scope in ('SAT', 'ACT')
-            and e.grade_level = 12
-            and a.test_month in ('November', 'December', 'January', 'February', 'March')
-        then 'MOY'
-        when
-            a.scope in ('SAT', 'ACT')
-            and e.grade_level = 12
-            and a.test_month in ('April', 'May', 'June', 'July')
-        then 'EOY'
-    end as admin_season,
+    s.admin_season,
 
 from {{ ref("int_extracts__student_enrollments") }} as e
 inner join
     {{ ref("int_assessments__college_assessment") }} as a
     on e.academic_year = a.academic_year
     and e.student_number = a.student_number
+left join
+    {{ ref("stg_google_sheets__kippfwd_seasons") }} as s
+    on a.scope = s.scope
+    and a.test_month = a.test_month
+    and e.grade_level = s.grade_level
 where e.school_level = 'HS' and e.rn_year = 1
