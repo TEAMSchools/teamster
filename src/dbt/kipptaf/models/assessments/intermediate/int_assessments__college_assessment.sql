62194dee8652ef2c7f71dc865c5036b5f9bdac0e
Gaby Rangel <grangel@kippteamandfamily.org>
2025-08-28 17:58:36 +0000

    moving to vscode

--- src/dbt/kipptaf/models/assessments/intermediate/int_assessments__college_assessment.sql
+++ src/dbt/kipptaf/models/assessments/intermediate/int_assessments__college_assessment.sql
@@ -69,6 +69,28 @@ with
         group by student_number, scope, score_type
     ),
 
+    running_max_score as (
+        select
+            student_number,
+            academic_year,
+            scope,
+            score_type,
+            test_date,
+
+            max(scale_score) over (
+                partition by student_number, score_type order by test_date
+            ) as running_max_scale_score,
+
+        from scores
+        where
+            score_type not in (
+                'psat10_reading',
+                'psat10_math_test',
+                'sat_math_test_score',
+                'sat_reading_test_score'
+            )
+    ),
+
     -- trunk-ignore(sqlfluff/ST03)
     max_total_score as (
         select
@@ -95,6 +117,32 @@ with
         }}
     ),
 
+    -- trunk-ignore(sqlfluff/ST03)
+    running_superscore as (
+        select
+            student_number,
+            scope,
+
+            if(
+                scope = 'ACT',
+                avg(max_scale_score) over (partition by student_number, scope),
+                sum(max_scale_score) over (partition by student_number, scope)
+            ) as running_superscore,
+
+        from max_score
+        where score_type not in ('act_composite', 'sat_total_score')
+    ),
+
+    dedup_runnning_superscore as (
+        {{
+            dbt_utils.deduplicate(
+                relation="running_superscore",
+                partition_by="student_number,scope",
+                order_by="student_number",
+            )
+        }}
+    ),
+
     alt_superscore as (
         select student_number, scope, avg(max_scale_score) as superscore,
 
@@ -104,7 +152,13 @@ with
     )
 
 select
-    s.*, m.max_scale_score, round(coalesce(d.superscore, a.superscore)) as superscore,
+    s.*,
+
+    m.max_scale_score,
+    mr.max_running_scale_score,
+
+    round(coalesce(d.superscore, a.superscore)) as superscore,
+    round(dr.running_superscore) as running_superscore,
 
 from scores as s
 left join
@@ -115,3 +169,11 @@ left join
     dedup_superscore as d on s.student_number = d.student_number and s.scope = d.scope
 left join
     alt_superscore as a on s.student_number = a.student_number and s.scope = a.scope
+left join
+    max_running_scale_score as mr
+    on s.student_number = mr.student_number
+    and s.score_type = mr.score_type
+left join
+    dedup_running_superscore as dr
+    on s.student_number = dr.student_number
+    and s.scope = dr.scope
