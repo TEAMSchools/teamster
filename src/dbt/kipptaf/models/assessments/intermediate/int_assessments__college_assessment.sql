42a508d4aeeab108eeef1ba40edd2c59e0d0bff0
Gaby R. <140853376+GabyRangelB@users.noreply.github.com>
2025-08-28 19:34:20 +0000

    wut

--- src/dbt/kipptaf/models/assessments/intermediate/int_assessments__college_assessment.sql
+++ src/dbt/kipptaf/models/assessments/intermediate/int_assessments__college_assessment.sql
@@ -69,28 +69,6 @@ with
         group by student_number, scope, score_type
     ),
 
-    running_max_score as (
-        select
-            student_number,
-            academic_year,
-            scope,
-            score_type,
-            test_date,
-
-            max(scale_score) over (
-                partition by student_number, score_type order by test_date
-            ) as running_max_scale_score,
-
-        from scores
-        where
-            score_type not in (
-                'psat10_reading',
-                'psat10_math_test',
-                'sat_math_test_score',
-                'sat_reading_test_score'
-            )
-    ),
-
     -- trunk-ignore(sqlfluff/ST03)
     max_total_score as (
         select
@@ -103,7 +81,7 @@ with
                 sum(max_scale_score) over (partition by student_number, scope)
             ) as superscore,
 
-        from running_max_score
+        from max_score
         where score_type not in ('act_composite', 'sat_total_score')
     ),
 
@@ -111,32 +89,6 @@ with
         {{
             dbt_utils.deduplicate(
                 relation="max_total_score",
-                partition_by="student_number,scope",
-                order_by="student_number",
-            )
-        }}
-    ),
-
-    -- trunk-ignore(sqlfluff/ST03)
-    running_superscore as (
-        select
-            student_number,
-            scope,
-
-            if(
-                scope = 'ACT',
-                avg(max_scale_score) over (partition by student_number, scope),
-                sum(max_scale_score) over (partition by student_number, scope)
-            ) as running_superscore,
-
-        from max_score
-        where score_type not in ('act_composite', 'sat_total_score')
-    ),
-
-    dedup_runnning_superscore as (
-        {{
-            dbt_utils.deduplicate(
-                relation="running_superscore",
                 partition_by="student_number,scope",
                 order_by="student_number",
             )
@@ -152,13 +104,7 @@ with
     )
 
 select
-    s.*,
-
-    m.max_scale_score,
-    mr.max_running_scale_score,
-
-    round(coalesce(d.superscore, a.superscore)) as superscore,
-    round(dr.running_superscore, 1) as running_superscore,
+    s.*, m.max_scale_score, round(coalesce(d.superscore, a.superscore)) as superscore,
 
 from scores as s
 left join
@@ -169,11 +115,3 @@ left join
     dedup_superscore as d on s.student_number = d.student_number and s.scope = d.scope
 left join
     alt_superscore as a on s.student_number = a.student_number and s.scope = a.scope
-left join
-    max_running_scale_score as mr
-    on s.student_number = mr.student_number
-    and s.score_type = mr.score_type
-left join
-    dedup_running_superscore as dr
-    on s.student_number = dr.student_number
-    and s.scope = dr.scope
