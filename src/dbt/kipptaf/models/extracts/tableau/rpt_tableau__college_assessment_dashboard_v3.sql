5ac00553b2d48471f05ba5366837ec6dd358f414
Gaby Rangel <grangel@kippteamandfamily.org>
2025-09-02 19:49:23 +0000

    added goals

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_v3.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_v3.sql
@@ -41,12 +41,25 @@ select
     p.psatnmsqt_count,
     p.sat_count,
     p.act_count,
-
     p.psat89_count_ytd,
     p.psat10_count_ytd,
     p.psatnmsqt_count_ytd,
     p.sat_count_ytd,
     p.act_count_ytd,
+
+    g1.aligned_goal_category,
+    g1.goal_category,
+    g1.goal_type,
+    g1.goal_subtype,
+    g1.score,
+    g1.goal,
+
+    g2.aligned_goal_category as board_aligned_goal_category,
+    g2.goal_category as board_goal_category,
+    g2.goal_type as board_goal_type,
+    g2.goal_subtype as board_goal_subtype,
+    g2.score as board_score,
+    g2.goal as board_goal,
 
     if(e.iep_status = 'No IEP', 0, 1) as sped,
 
@@ -61,4 +74,17 @@ left join
     on e.student_number = p.student_number
     and e.grade_level = p.grade_level
     and {{ union_dataset_join_clause(left_alias="e", right_alias="p") }}
+left join
+    {{ ref("stg_google_sheets__kippfwd_goals") }} as g1
+    on r.academic_year = g1.academic_year
+    and r.scope = g1.expected_scope
+    and r.subject_area = g1.expected_subject_area
+    and g1.goal_type in ('Attempts', 'Benchmark')
+left join
+    {{ ref("stg_google_sheets__kippfwd_goals") }} as g2
+    on r.academic_year = g2.academic_year
+    and r.grade_level = g2.grade_level
+    and r.scope = g2.expected_scope
+    and r.subject_area = g2.expected_subject_area
+    and g1.goal_type = 'Board'
 where e.rn_year = 1 and e.school_level = 'HS'
