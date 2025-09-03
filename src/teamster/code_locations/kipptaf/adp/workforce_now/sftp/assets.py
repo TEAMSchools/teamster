b896677684d1a35aee68cc8e36ffb67c7c5e7bb0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-09-03 16:28:08 +0000

    feat: adp memberships

--- src/teamster/code_locations/kipptaf/adp/workforce_now/sftp/assets.py
+++ src/teamster/code_locations/kipptaf/adp/workforce_now/sftp/assets.py
@@ -1,6 +1,7 @@
 from teamster.code_locations.kipptaf import CODE_LOCATION
 from teamster.code_locations.kipptaf.adp.workforce_now.sftp.schema import (
     ADDITIONAL_EARNINGS_REPORT_SCHEMA,
+    EMPLOYEE_MEMBERSHIPS_SCHEMA,
     PENSION_AND_BENEFITS_ENROLLMENTS_SCHEMA,
     TIME_AND_ATTENDANCE_SCHEMA,
 )
@@ -32,6 +33,13 @@ time_and_attendance = build_sftp_file_asset(
     asset_key=[*key_prefix, "time_and_attendance"],
     remote_file_regex=r"adp__wfn__employment_profile__time_and_attendance\.csv",
     avro_schema=TIME_AND_ATTENDANCE_SCHEMA,
+    **asset_kwargs,
+)
+
+employee_memberships = build_sftp_file_asset(
+    asset_key=[*key_prefix, "employee_memberships"],
+    remote_file_regex=r"employee_memberships\.csv",
+    avro_schema=EMPLOYEE_MEMBERSHIPS_SCHEMA,
     **asset_kwargs,
 )
 
