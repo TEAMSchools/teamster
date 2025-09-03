7cb64e1c3d4796fd18773789f70a6a19bfb71a98
Kevin Verhoff <kverhoff@kippteamandfamily.org>
2025-09-03 20:19:42 +0000

    current memberships

---
+++ src/dbt/kipptaf/models/people/intermediate/properties/int_people__employee_program_memberships.sql
@@ -0,0 +1,26 @@
+with
+    memberships as (
+        select associate_id, membership_description, category_description,
+        from {{ ref("stg_adp_workforce_now__employee_memberships") }}
+        where
+            membership_code is not null
+            and current_date('{{ var("local_timezone") }}')
+            between effective_date and expiration_date
+    ),
+
+    memberships_concat as (
+        select associate_id, string_agg(membership_description, ', ') as memberships,
+        from memberships
+        group by associate_id
+    )
+
+select
+    mc.*,
+    if(
+        m.category_description = 'Program - Leader Development', true, false
+    ) as in_leadership_program,
+    if(
+        m.category_description = 'Program - Teacher Development', true, false
+    ) as in_teacher_program,
+from memberships_concat mc
+join memberships m on mc.associate_id = m.associate_id
