dc2a1a052a9351cf97ab66971af3a332b4ee9613
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-09-05 16:06:53 +0000

    fix: contract

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__topline_aggregate_goals.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__topline_aggregate_goals.sql
@@ -1,2 +1,2 @@
-select *,
+select * except (goal), cast(goal as numeric) as goal,
 from {{ source("google_sheets", "src_google_sheets__topline_aggregate_goals") }}
--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__topline_student_goals.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__topline_student_goals.sql
@@ -1,1 +1,2 @@
-select *, from {{ source("google_sheets", "src_google_sheets__topline_student_goals") }}
+select * except (cutoff), cast(cutoff as numeric) as cutoff,
+from {{ source("google_sheets", "src_google_sheets__topline_student_goals") }}
