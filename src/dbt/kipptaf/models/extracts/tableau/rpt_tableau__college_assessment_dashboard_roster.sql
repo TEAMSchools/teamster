0aac7cd9b79dd4019101540c160b43a0ab3bc57a
Gaby Rangel <grangel@kippteamandfamily.org>
2025-09-05 00:24:21 +0000

    label changes

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_roster.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_roster.sql
@@ -1,44 +1,147 @@
 {% set tests = [
     {
-        "label": "s_112 ACT Composite Official",
-        "prefix": "s_112_act_composite_official",
+        "label": "s_91 PSAT 8/9 Math Official",
+        "prefix": "s_91_psat_89_math_official",
     },
-    {"label": "s_112 ACT Math Official", "prefix": "s_112_act_math_official"},
-    {"label": "s_112 SAT EBRW Official", "prefix": "s_112_sat_ebrw_official"},
+    {
+        "label": "s_91 PSAT 8/9 EBRW Official",
+        "prefix": "s_91_psat_89_ebrw_official",
+    },
+    {
+        "label": "s_91 PSAT 8/9 Combined Official",
+        "prefix": "s_91_psat_89_combined_official",
+    },
+    {"label": "s_123 SAT Math Official", "prefix": "s_123_sat_math_official"},
+    {"label": "s_123 SAT EBRW Official", "prefix": "s_123_sat_ebrw_official"},
+    {
+        "label": "s_123 SAT Combined Official",
+        "prefix": "s_123_sat_combined_official",
+    },
+    {
+        "label": "s_123 ACT Science Official",
+        "prefix": "s_123_act_science_official",
+    },
+    {
+        "label": "s_123 ACT Reading Official",
+        "prefix": "s_123_act_reading_official",
+    },
+    {"label": "s_123 ACT Math Official", "prefix": "s_123_act_math_official"},
+    {
+        "label": "s_123 ACT English Official",
+        "prefix": "s_123_act_english_official",
+    },
+    {
+        "label": "s_123 ACT Composite Official",
+        "prefix": "s_123_act_composite_official",
+    },
+    {"label": "s_122 SAT Math Official", "prefix": "s_122_sat_math_official"},
+    {"label": "s_122 SAT EBRW Official", "prefix": "s_122_sat_ebrw_official"},
+    {
+        "label": "s_122 SAT Combined Official",
+        "prefix": "s_122_sat_combined_official",
+    },
+    {
+        "label": "s_122 ACT Science Official",
+        "prefix": "s_122_act_science_official",
+    },
+    {
+        "label": "s_122 ACT Reading Official",
+        "prefix": "s_122_act_reading_official",
+    },
+    {"label": "s_122 ACT Math Official", "prefix": "s_122_act_math_official"},
+    {
+        "label": "s_122 ACT English Official",
+        "prefix": "s_122_act_english_official",
+    },
+    {
+        "label": "s_122 ACT Composite Official",
+        "prefix": "s_122_act_composite_official",
+    },
+    {"label": "s_121 SAT Math Official", "prefix": "s_121_sat_math_official"},
+    {"label": "s_121 SAT EBRW Official", "prefix": "s_121_sat_ebrw_official"},
     {
         "label": "s_121 SAT Combined Official",
         "prefix": "s_121_sat_combined_official",
     },
-    {"label": "s_123 SAT EBRW Official", "prefix": "s_123_sat_ebrw_official"},
     {
-        "label": "s_123 SAT Reading Test Official",
-        "prefix": "s_123_sat_reading_test_official",
+        "label": "s_121 ACT Science Official",
+        "prefix": "s_121_act_science_official",
     },
     {
-        "label": "s_112 SAT Combined Official",
-        "prefix": "s_112_sat_combined_official",
+        "label": "s_121 ACT Reading Official",
+        "prefix": "s_121_act_reading_official",
+    },
+    {"label": "s_121 ACT Math Official", "prefix": "s_121_act_math_official"},
+    {
+        "label": "s_121 ACT English Official",
+        "prefix": "s_121_act_english_official",
     },
     {
-        "label": "s_113 SAT Reading Test Official",
-        "prefix": "s_113_sat_reading_test_official",
+        "label": "s_121 ACT Composite Official",
+        "prefix": "s_121_act_composite_official",
     },
-    {"label": "s_112 SAT Math Official", "prefix": "s_112_sat_math_official"},
-    {"label": "s_123 SAT Math Official", "prefix": "s_123_sat_math_official"},
-    {
-        "label": "s_121 SAT Reading Test Official",
-        "prefix": "s_121_sat_reading_test_official",
-    },
-    {
-        "label": "s_101 PSAT NMSQT Math Official",
-        "prefix": "s_101_psat_nmsqt_math_official",
-    },
+    {"label": "s_113 SAT Math Official", "prefix": "s_113_sat_math_official"},
+    {"label": "s_113 SAT EBRW Official", "prefix": "s_113_sat_ebrw_official"},
     {
         "label": "s_113 SAT Combined Official",
         "prefix": "s_113_sat_combined_official",
     },
     {
-        "label": "s_122 SAT Reading Test Official",
-        "prefix": "s_122_sat_reading_test_official",
+        "label": "s_113 ACT Science Official",
+        "prefix": "s_113_act_science_official",
+    },
+    {
+        "label": "s_113 ACT Reading Official",
+        "prefix": "s_113_act_reading_official",
+    },
+    {"label": "s_113 ACT Math Official", "prefix": "s_113_act_math_official"},
+    {
+        "label": "s_113 ACT English Official",
+        "prefix": "s_113_act_english_official",
+    },
+    {
+        "label": "s_113 ACT Composite Official",
+        "prefix": "s_113_act_composite_official",
+    },
+    {"label": "s_112 SAT Math Official", "prefix": "s_112_sat_math_official"},
+    {"label": "s_112 SAT EBRW Official", "prefix": "s_112_sat_ebrw_official"},
+    {
+        "label": "s_112 SAT Combined Official",
+        "prefix": "s_112_sat_combined_official",
+    },
+    {
+        "label": "s_112 ACT Science Official",
+        "prefix": "s_112_act_science_official",
+    },
+    {
+        "label": "s_112 ACT Reading Official",
+        "prefix": "s_112_act_reading_official",
+    },
+    {"label": "s_112 ACT Math Official", "prefix": "s_112_act_math_official"},
+    {
+        "label": "s_112 ACT English Official",
+        "prefix": "s_112_act_english_official",
+    },
+    {
+        "label": "s_112 ACT Composite Official",
+        "prefix": "s_112_act_composite_official",
+    },
+    {
+        "label": "s_111 ACT Science Official",
+        "prefix": "s_111_act_science_official",
+    },
+    {
+        "label": "s_111 ACT Reading Official",
+        "prefix": "s_111_act_reading_official",
+    },
+    {"label": "s_111 ACT Math Official", "prefix": "s_111_act_math_official"},
+    {
+        "label": "s_111 ACT English Official",
+        "prefix": "s_111_act_english_official",
+    },
+    {
+        "label": "s_111 ACT Composite Official",
+        "prefix": "s_111_act_composite_official",
     },
     {
         "label": "s_102 PSAT10 Math Official",
@@ -49,73 +152,23 @@
         "prefix": "s_102_psat10_ebrw_official",
     },
     {
-        "label": "s_122 SAT Combined Official",
-        "prefix": "s_122_sat_combined_official",
+        "label": "s_102 PSAT10 Combined Official",
+        "prefix": "s_102_psat10_combined_official",
     },
-    {"label": "s_122 SAT EBRW Official", "prefix": "s_122_sat_ebrw_official"},
-    {"label": "s_122 SAT Math Official", "prefix": "s_122_sat_math_official"},
+    {
+        "label": "s_101 PSAT NMSQT Math Official",
+        "prefix": "s_101_psat_nmsqt_math_official",
+    },
     {
         "label": "s_101 PSAT NMSQT EBRW Official",
         "prefix": "s_101_psat_nmsqt_ebrw_official",
     },
     {
-        "label": "s_123 ACT Composite Official",
-        "prefix": "s_123_act_composite_official",
-    },
-    {"label": "s_121 ACT Math Official", "prefix": "s_121_act_math_official"},
-    {
-        "label": "s_111 ACT Composite Official",
-        "prefix": "s_111_act_composite_official",
-    },
-    {"label": "s_111 ACT Math Official", "prefix": "s_111_act_math_official"},
-    {
         "label": "s_101 PSAT NMSQT Combined Official",
         "prefix": "s_101_psat_nmsqt_combined_official",
     },
-    {
-        "label": "s_121 ACT Composite Official",
-        "prefix": "s_121_act_composite_official",
-    },
-    {"label": "s_123 ACT Math Official", "prefix": "s_123_act_math_official"},
-    {"label": "s_113 SAT Math Official", "prefix": "s_113_sat_math_official"},
-    {
-        "label": "s_91 PSAT 8/9 Combined Official",
-        "prefix": "s_91_psat_89_combined_official",
-    },
-    {
-        "label": "s_91 PSAT 8/9 Math Official",
-        "prefix": "s_91_psat_89_math_official",
-    },
-    {
-        "label": "s_123 SAT Combined Official",
-        "prefix": "s_123_sat_combined_official",
-    },
-    {"label": "s_113 SAT EBRW Official", "prefix": "s_113_sat_ebrw_official"},
-    {
-        "label": "s_113 ACT Composite Official",
-        "prefix": "s_113_act_composite_official",
-    },
-    {"label": "s_113 ACT Math Official", "prefix": "s_113_act_math_official"},
-    {
-        "label": "s_122 ACT Composite Official",
-        "prefix": "s_122_act_composite_official",
-    },
-    {"label": "s_122 ACT Math Official", "prefix": "s_122_act_math_official"},
-    {
-        "label": "s_112 SAT Reading Test Official",
-        "prefix": "s_112_sat_reading_test_official",
-    },
-    {"label": "s_121 SAT EBRW Official", "prefix": "s_121_sat_ebrw_official"},
-    {"label": "s_121 SAT Math Official", "prefix": "s_121_sat_math_official"},
-    {
-        "label": "s_102 PSAT10 Combined Official",
-        "prefix": "s_102_psat10_combined_official",
-    },
-    {
-        "label": "s_91 PSAT 8/9 EBRW Official",
-        "prefix": "s_91_psat_89_ebrw_official",
-    },
 ] %}
+
 
 with
     superscore_pivot as (
