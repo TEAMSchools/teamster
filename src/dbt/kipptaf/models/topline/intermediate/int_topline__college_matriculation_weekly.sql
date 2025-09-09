d4a9af79f099c911f1753f19c648dbcbbfcd1518
Anthony Walters <awalters@kippteamandfamily.org>
2025-09-09 01:47:50 +0000

    updates

---
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__college_matriculation_weekly.sql
@@ -0,0 +1,43 @@
+with
+    -- trunk-ignore(sqlfluff/ST03)
+    matriculation_metrics as (
+        select
+            applicant,
+            dbt_valid_from,
+            dbt_valid_to,
+            is_submitted_ba,
+            is_accepted_ba,
+            is_matriculated_ba,
+
+            cast(dbt_valid_from as date) as dbt_valid_from_date,
+            cast(dbt_valid_to as date) as dbt_valid_to_date,
+        from {{ ref("snapshot_kippadb__app_rollup") }}
+    ),
+
+    deduplicate as (
+        {{
+            dbt_utils.deduplicate(
+                relation="matriculation_metrics",
+                partition_by="applicant, dbt_valid_from_date",
+                order_by="dbt_valid_to desc",
+            )
+        }}
+    )
+
+select
+    co.student_number,
+    co.schoolid,
+    co.academic_year,
+    co.week_start_monday,
+    co.week_end_sunday,
+    co.week_number_academic_year,
+
+    m.is_submitted_ba,
+    m.is_accepted_ba,
+    m.is_matriculated_ba,
+from {{ ref("int_extracts__student_enrollments_weeks") }} as co
+left join
+    deduplicate as m
+    on co.salesforce_id = m.applicant
+    and co.week_start_monday between m.dbt_valid_from_date and m.dbt_valid_to_date
+where co.is_enrolled_week and co.grade_level = 12
