bab5a81650a843e8f0f86935c43fd03c299727bb
Cristina Baldor <ccbaldor@icloud.com>
2025-09-09 14:22:19 +0000

    add: current week for tracking, remove is_current, adjust join

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__seat_tracker_snapshot.sql
@@ -1,12 +1,21 @@
 with
     date_spine as (
-        select date_week,
+        select
+            date_week,
+            {{
+                date_to_fiscal_year(
+                    date_field="date_week",
+                    start_month=7,
+                    year_source="start",
+                )
+            }} as academic_year,
         from
             unnest(
                 generate_date_array(
-                    /* first date of the appsheet snapshot */
+                    {# first date of the appsheet snapshot #}
                     '2023-08-04',
-                    current_date('{{ var("local_timezone") }}'),
+                    {# need current week as it happens #}
+                    current_date('{{ var("local_timezone") }}') + 7,
                     interval 1 week
                 )
             ) as date_week
@@ -16,11 +25,11 @@ with
 
     seats_detail as (select *, from {{ ref("stg_seat_tracker__seats") }}),
 
-    staff_roster as (select *, from {{ ref("int_people__staff_roster") }}),
-
     projections as (
         select *, from {{ ref("stg_google_sheets__recruitment__school_projections") }}
     ),
+
+    staff_roster as (select *, from {{ ref("int_people__staff_roster") }}),
 
     final as (
         select
@@ -43,44 +52,38 @@ with
             seats_detail.short_name,
             seats_detail.recruitment_group,
 
+            projections.anticipated_hires,
+
             teammate_info.formatted_name as teammate,
 
             recruiter_info.formatted_name as recruiter,
             recruiter_info.reports_to_formatted_name as recruiter_manager,
-
-            projections.anticipated_hires,
 
             if(seats_snapshot.is_open, 1, 0) as snapshot_open,
             if(seats_snapshot.is_new_hire, 1, 0) as snapshot_new_hire,
             if(seats_snapshot.is_staffed, 1, 0) as snapshot_staffed,
             if(seats_snapshot.is_active, 1, 0) as snapshot_active,
             if(seats_snapshot.is_mid_year_hire, 1, 0) as snapshot_mid_year_hire,
-            if(
-                current_date()
-                between seats_snapshot.valid_from and seats_snapshot.valid_to,
-                true,
-                false
-            ) as is_current,
-
         from date_spine
-        left join
+        inner join
             seats_snapshot
-            on date_spine.date_week
+            on date_spine.academic_year = seats_snapshot.academic_year
+            and date_spine.date_week
             between seats_snapshot.valid_from and seats_snapshot.valid_to
-        left join
+        inner join
             seats_detail
             on seats_snapshot.staffing_model_id = seats_detail.staffing_model_id
             and seats_snapshot.academic_year = seats_detail.academic_year
+        inner join
+            projections
+            on seats_detail.adp_location = projections.primary_site
+            and seats_detail.academic_year = projections.academic_year
         left join
             staff_roster as teammate_info
             on seats_snapshot.teammate_employee_number = teammate_info.employee_number
         left join
             staff_roster as recruiter_info
             on seats_detail.recruiter = recruiter_info.employee_number
-        left join
-            projections
-            on seats_snapshot.adp_location = projections.primary_site
-            and seats_snapshot.academic_year = projections.academic_year
     )
 
 select *,
