ca4b0d1cfb8b959748c12d1c714d726817cb4d82
Anthony Walters <awalters@kippteamandfamily.org>
2025-09-09 02:16:37 +0000

    updates

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__dashboard_aggregations.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__dashboard_aggregations.sql
@@ -1,17 +1,6 @@
 with
     goals as (
-        select
-            *,
-
-            case
-                org_level
-                when 'org'
-                then 'org_' || grade_low || '-' || grade_high
-                when 'region'
-                then entity || '_' || grade_low || '-' || grade_high
-                when 'school'
-                then schoolid || '_' || grade_low || '-' || grade_high
-            end as aggregation_hash,
+        select *, if(indicator = 'Total Enrollment', 'Integer', 'Decimal') as data_type,
         from {{ ref("stg_google_sheets__topline_aggregate_goals") }}
     ),
 
