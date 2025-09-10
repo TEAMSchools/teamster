88d65c244f62efbadf1628ce82a5cb2b1e7d7ef4
Gaby Rangel <grangel@kippteamandfamily.org>
2025-09-10 16:27:13 +0000

    hs act

--- src/dbt/kipptaf/models/csgf/staging/stg_csgf__hs_act.sql
+++ src/dbt/kipptaf/models/csgf/staging/stg_csgf__hs_act.sql
@@ -1,9 +1,7 @@
 select
     e.student_number as studentid,
 
-    if(
-        a.subject_area = 'EBRW', 'Evidence-Based Reading and Writing', a.subject_area
-    ) as `section`,
+    a.subject_area as `section`,
 
     round(a.scale_score, 0) as score,
 
@@ -11,8 +9,7 @@ from {{ ref("int_extracts__student_enrollments") }} as e
 inner join
     {{ ref("int_assessments__college_assessment") }} as a
     on e.student_number = a.student_number
-    and a.scope = 'SAT'
-    and a.subject_area in ('Math', 'EBRW')
+    and a.scope = 'ACT'
     and a.rn_highest = 1
 where
     e.academic_year = {{ var("current_academic_year") - 1 }}
