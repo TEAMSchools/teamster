5bca3ac73f2759a10c9ea8130b82053773558c83
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-09-11 15:02:03 +0000

    refactor

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__academic_goals_rollup.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__academic_goals_rollup.sql
@@ -1,44 +1,30 @@
 with
-    subject_croswalk as (
-        select
-            `subject`,
-
-            if(
-                `subject` = 'Reading', 'Text Study', 'Mathematics'
-            ) as illuminate_subject_area,
-        from unnest(['Reading', 'Math']) as `subject`
-    ),
-
-    grade_bands as (
-        select 'K' as band, 0 as grade_level,
-        union all
-        select '1-2' as band, grade_level,
-        from unnest([1, 2]) as grade_level
-        union all
-        select '3-8' as band, grade_level,
-        from unnest([3, 4, 5, 6, 7, 8]) as grade_level
-    ),
-
     goals as (
         select
-            g.academic_year,
-            g.region,
-
-            gb.band,
+            academic_year,
+            region,
 
             case
-                g.illuminate_subject_area
+                when grade_level = 0
+                then 'K'
+                when grade_level in (1, 2)
+                then '1-2'
+                when grade_level between 3 and 8
+                then '3-8'
+            end as band,
+
+            case
+                illuminate_subject_area
                 when 'Text Study'
                 then 'Reading'
                 when 'Mathematics'
                 then 'Math'
-                else g.illuminate_subject_area
+                else illuminate_subject_area
             end as `subject`,
 
-            max(g.grade_band_goal) as grade_band_goal,
-        from {{ ref("int_assessments__academic_goals") }} as g
-        inner join grade_bands as gb on g.grade_level = gb.grade_level
-        group by all
+            max(grade_band_goal) as grade_band_goal,
+        from {{ ref("int_assessments__academic_goals") }}
+        group by academic_year, region, band, `subject`
     ),
 
     state_test_union as (
@@ -170,10 +156,7 @@ with
             co.school as school,
             co.team as homeroom_section,
             co.advisor_lastfirst as homeroom_teacher_name,
-
-            gb.band,
-
-            s.subject,
+            co.iready_subject as `subject`,
 
             cc.sections_section_number as course_section,
             cc.teacher_lastfirst as course_teacher_name,
@@ -182,8 +165,6 @@ with
             st.is_proficient_int as is_proficient_int_state,
 
             ir.scale_score as scale_score_iready,
-
-            if(co.spedlep like 'SPED%', 'Has IEP', 'No IEP') as iep_status,
 
             coalesce(
                 st.assessment_type, ir.assessment_type, 'Untested'
@@ -196,9 +177,20 @@ with
             ) as is_approaching_int,
             coalesce(st.is_below_int, ir.is_below_int) as is_below_int,
 
+            if(co.spedlep like 'SPED%', 'Has IEP', 'No IEP') as iep_status,
+
             if(
                 st.scale_score is not null or ir.scale_score is not null, 1, 0
             ) as is_tested_int,
+
+            case
+                when co.grade_level = 0
+                then 'K'
+                when co.grade_level in (1, 2)
+                then '1-2'
+                when co.grade_level between 3 and 8
+                then '3-8'
+            end as band,
 
             case
                 when
@@ -214,39 +206,30 @@ with
                 then true
                 else false
             end as is_bucket2_eligible,
-        from {{ ref("int_extracts__student_enrollments") }} as co
-        cross join subject_croswalk as s
-        inner join grade_bands as gb on co.grade_level = gb.grade_level
-        inner join
-            {{ ref("int_extracts__student_enrollments_subjects") }} as sf
-            on co.academic_year = sf.academic_year
-            and co.student_number = sf.student_number
-            and {{ union_dataset_join_clause(left_alias="co", right_alias="sf") }}
-            and s.subject = sf.iready_subject
-            and sf.rn_year = 1
-            and not sf.is_exempt_state_testing
+        from {{ ref("int_extracts__student_enrollments_subjects") }} as co
         left join
             {{ ref("base_powerschool__course_enrollments") }} as cc
             on co.studentid = cc.cc_studentid
             and co.yearid = cc.cc_yearid
+            and co.illuminate_subject_area = cc.illuminate_subject_area
             and {{ union_dataset_join_clause(left_alias="co", right_alias="cc") }}
-            and s.illuminate_subject_area = cc.illuminate_subject_area
-            and not cc.is_dropped_section
             and cc.rn_student_year_illuminate_subject_desc = 1
+            and not cc.is_dropped_section
         left join
             state_test_union as st
             on co.student_number = st.student_number
             and co.academic_year = st.academic_year_plus
+            and co.iready_subject = st.subject
             and {{ union_dataset_join_clause(left_alias="co", right_alias="st") }}
-            and s.subject = st.subject
         left join
             iready as ir
             on co.student_number = ir.student_number
             and co.academic_year = ir.academic_year
-            and s.subject = ir.subject
+            and co.iready_subject = ir.subject
         where
             co.rn_year = 1
             and co.enroll_status = 0
+            and not co.is_exempt_state_testing
             and co.grade_level between 3 and 8
             and co.academic_year >= {{ var("current_academic_year") - 1 }}
 
@@ -263,10 +246,7 @@ with
             co.school as school,
             co.team as homeroom_section,
             co.advisor_lastfirst as homeroom_teacher_name,
-
-            gb.band,
-
-            s.subject,
+            co.iready_subject as `subject`,
 
             cc.sections_section_number as course_section,
             cc.teacher_lastfirst as course_teacher_name,
@@ -274,8 +254,6 @@ with
             null as scale_score_state,
             null as is_proficient_int_state,
             null as scale_score_iready,
-
-            if(co.spedlep like 'SPED%', 'Has IEP', 'No IEP') as iep_status,
 
             coalesce(ir.assessment_type, 'Untested') as benchmark_assessment_type,
 
@@ -285,35 +263,38 @@ with
             ir.is_approaching_int,
             ir.is_below_int,
 
+            if(co.spedlep like 'SPED%', 'Has IEP', 'No IEP') as iep_status,
+
             if(ir.scale_score is not null, 1, 0) as is_tested_int,
+
+            case
+                when co.grade_level = 0
+                then 'K'
+                when co.grade_level in (1, 2)
+                then '1-2'
+                when co.grade_level between 3 and 8
+                then '3-8'
+            end as band,
+
             if(ir.is_approaching_int = 1, true, false) as is_bucket2_eligible,
-        from {{ ref("int_extracts__student_enrollments") }} as co
-        cross join subject_croswalk as s
-        inner join grade_bands as gb on co.grade_level = gb.grade_level
-        inner join
-            {{ ref("int_extracts__student_enrollments_subjects") }} as sf
-            on co.academic_year = sf.academic_year
-            and co.student_number = sf.student_number
-            and {{ union_dataset_join_clause(left_alias="co", right_alias="sf") }}
-            and s.subject = sf.iready_subject
-            and sf.rn_year = 1
-            and not sf.is_exempt_state_testing
+        from {{ ref("int_extracts__student_enrollments_subjects") }} as co
         left join
             {{ ref("base_powerschool__course_enrollments") }} as cc
             on co.studentid = cc.cc_studentid
             and co.yearid = cc.cc_yearid
+            and co.illuminate_subject_area = cc.illuminate_subject_area
             and {{ union_dataset_join_clause(left_alias="co", right_alias="cc") }}
-            and s.illuminate_subject_area = cc.illuminate_subject_area
-            and not cc.is_dropped_section
             and cc.rn_student_year_illuminate_subject_desc = 1
+            and not cc.is_dropped_section
         left join
             iready as ir
             on co.student_number = ir.student_number
             and co.academic_year = ir.academic_year
-            and s.subject = ir.subject
+            and co.iready_subject = ir.subject
         where
             co.rn_year = 1
             and co.enroll_status = 0
+            and not co.is_exempt_state_testing
             and co.grade_level between 0 and 2
             and co.academic_year >= {{ var("current_academic_year") - 1 }}
     ),
@@ -323,12 +304,12 @@ with
             *,
 
             rank() over (
-                partition by academic_year, school, grade_level, subject
+                partition by academic_year, school, grade_level, `subject`
                 order by if(is_bucket2_eligible, scale_score_state, null) desc
             ) as rank_scale_score_state,
 
             rank() over (
-                partition by academic_year, school, grade_level, subject
+                partition by academic_year, school, grade_level, `subject`
                 order by if(is_bucket2_eligible, scale_score, null) desc
             ) as rank_scale_score,
         from roster
@@ -355,7 +336,7 @@ with
             and r.subject = g.subject
             and r.academic_year = g.academic_year
             and r.band = g.band
-        group by all
+        group by r.academic_year, r.region, r.subject, r.band, g.grade_band_goal
     ),
 
     school_grade_goals as (
@@ -386,7 +367,14 @@ with
             and r.region = p.region
             and r.subject = p.subject
             and r.band = p.band
-        group by all
+        group by
+            r.academic_year,
+            r.region,
+            r.school,
+            r.grade_level,
+            r.subject,
+            p.bubble_parameter,
+            p.grade_band_goal
     ),
 
     foo as (
--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__assessment_dashboard.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__assessment_dashboard.sql
@@ -1,23 +1,36 @@
 with
-    dashboard as (
+    roster as (
         select
-            co.student_number,
-            co.student_name as lastfirst,
-            co.academic_year,
-            co.reporting_schoolid as schoolid,
-            co.region,
-            co.grade_level,
-            co.advisory_name as team,
-            co.enroll_status,
-            co.cohort,
-            co.spedlep as iep_status,
-            co.lep_status,
-            co.is_504 as c_504_status,
-            co.is_self_contained as is_pathways,
-            co.school,
-            co.school_level,
-            co.advisor_teachernumber as hr_teachernumber,
+            student_number,
+            student_name,
+            academic_year,
+            reporting_schoolid,
+            region,
+            grade_level,
+            advisory_name,
+            enroll_status,
+            cohort,
+            spedlep,
+            lep_status,
+            is_504,
+            is_self_contained,
+            school,
+            school_level,
+            advisor_teachernumber,
+            hos,
+            nj_student_tier,
+            is_tutoring,
+            territory,
+            powerschool_credittype,
+            illuminate_subject_area,
+        from {{ ref("int_extracts__student_enrollments_subjects") }}
+        where rn_year = 1 and academic_year >= {{ var("current_academic_year") - 1 }}
+    ),
 
+    assessment_enrollments as (
+        select
+            asr.powerschool_student_number,
+            asr.academic_year,
             asr.assessment_id,
             asr.title,
             asr.scope,
@@ -29,106 +42,201 @@ with
             asr.module_type,
             asr.module_code,
             asr.response_type,
-            asr.response_type_code as standard_code,
-            asr.response_type_description as standard_description,
-            asr.response_type_root_description as domain_description,
+            asr.response_type_code,
+            asr.response_type_description,
+            asr.response_type_root_description,
             asr.percent_correct,
             asr.is_mastery,
-            asr.performance_band_label_number as performance_band_number,
+            asr.performance_band_label_number,
             asr.performance_band_label,
             asr.is_replacement,
-            asr.is_internal_assessment as is_normed_scope,
+            asr.is_internal_assessment,
 
-            enr.teachernumber as enr_teachernumber,
-            enr.teacher_lastfirst as teacher_name,
-            enr.courses_course_name as course_name,
-            enr.sections_expression as expression,
-            enr.sections_section_number as section_number,
+            enr.teachernumber,
+            enr.teacher_lastfirst,
+            enr.courses_course_name,
+            enr.courses_credittype,
+            enr.sections_expression,
+            enr.sections_section_number,
             enr.is_foundations,
-
-            lc.head_of_school_preferred_name_lastfirst as head_of_school,
-
-            if(
-                co.grade_level >= 9, enr.courses_credittype, asr.subject_area
-            ) as filter_join,
-        from {{ ref("int_extracts__student_enrollments") }} as co
-        inner join
-            {{ ref("int_assessments__response_rollup") }} as asr
-            on co.student_number = asr.powerschool_student_number
-            and co.academic_year = asr.academic_year
+        from {{ ref("int_assessments__response_rollup") }} as asr
         left join
             {{ ref("base_powerschool__course_enrollments") }} as enr
-            on co.studentid = enr.cc_studentid
-            and co.yearid = enr.cc_yearid
-            and {{ union_dataset_join_clause(left_alias="co", right_alias="enr") }}
+            on asr.powerschool_student_number = enr.students_student_number
+            and asr.academic_year = enr.terms_academic_year
             and asr.subject_area = enr.illuminate_subject_area
-            and not enr.is_dropped_section
             and enr.rn_student_year_illuminate_subject_desc = 1
-        left join
-            {{ ref("int_people__leadership_crosswalk") }} as lc
-            on co.schoolid = lc.home_work_location_powerschool_school_id
-        where
-            co.academic_year >= {{ var("current_academic_year") - 1 }}
-            and co.rn_year = 1
+            and not enr.is_dropped_section
+        where asr.academic_year >= {{ var("current_academic_year") - 1 }}
+    ),
+
+    dashboard as (
+        select
+            co.student_number,
+            co.student_name,
+            co.academic_year,
+            co.reporting_schoolid,
+            co.region,
+            co.grade_level,
+            co.advisory_name,
+            co.enroll_status,
+            co.cohort,
+            co.spedlep,
+            co.lep_status,
+            co.is_504,
+            co.is_self_contained,
+            co.school,
+            co.school_level,
+            co.advisor_teachernumber,
+            co.hos,
+            co.nj_student_tier,
+            co.is_tutoring,
+            co.territory,
+
+            ae.assessment_id,
+            ae.title,
+            ae.scope,
+            ae.subject_area,
+            ae.term_administered,
+            ae.administered_at,
+            ae.term_taken,
+            ae.date_taken,
+            ae.module_type,
+            ae.module_code,
+            ae.response_type,
+            ae.response_type_code,
+            ae.response_type_description,
+            ae.response_type_root_description,
+            ae.percent_correct,
+            ae.is_mastery,
+            ae.performance_band_label_number,
+            ae.performance_band_label,
+            ae.is_replacement,
+            ae.is_internal_assessment,
+            ae.teachernumber,
+            ae.teacher_lastfirst,
+            ae.courses_course_name,
+            ae.sections_expression,
+            ae.sections_section_number,
+            ae.is_foundations,
+        from roster as co
+        inner join
+            assessment_enrollments as ae
+            on co.student_number = ae.powerschool_student_number
+            and co.academic_year = ae.academic_year
+            and co.powerschool_credittype = ae.courses_credittype
+        where co.grade_level >= 9
+
+        union all
+
+        select
+            co.student_number,
+            co.student_name,
+            co.academic_year,
+            co.reporting_schoolid,
+            co.region,
+            co.grade_level,
+            co.advisory_name,
+            co.enroll_status,
+            co.cohort,
+            co.spedlep,
+            co.lep_status,
+            co.is_504,
+            co.is_self_contained,
+            co.school,
+            co.school_level,
+            co.advisor_teachernumber,
+            co.hos,
+            co.nj_student_tier,
+            co.is_tutoring,
+            co.territory,
+
+            ae.assessment_id,
+            ae.title,
+            ae.scope,
+            ae.subject_area,
+            ae.term_administered,
+            ae.administered_at,
+            ae.term_taken,
+            ae.date_taken,
+            ae.module_type,
+            ae.module_code,
+            ae.response_type,
+            ae.response_type_code,
+            ae.response_type_description,
+            ae.response_type_root_description,
+            ae.percent_correct,
+            ae.is_mastery,
+            ae.performance_band_label_number,
+            ae.performance_band_label,
+            ae.is_replacement,
+            ae.is_internal_assessment,
+            ae.teachernumber,
+            ae.teacher_lastfirst,
+            ae.courses_course_name,
+            ae.sections_expression,
+            ae.sections_section_number,
+            ae.is_foundations,
+        from roster as co
+        inner join
+            assessment_enrollments as ae
+            on co.student_number = ae.powerschool_student_number
+            and co.academic_year = ae.academic_year
+            and co.illuminate_subject_area = ae.subject_area
+        where co.grade_level <= 9
     )
 
 select
-    d.student_number,
-    d.lastfirst,
-    d.academic_year,
-    d.schoolid,
-    d.region,
-    d.grade_level,
-    d.team,
-    d.enroll_status,
-    d.cohort,
-    d.iep_status,
-    d.lep_status,
-    d.c_504_status,
-    d.is_pathways,
-    d.school,
-    d.school_level,
-    d.assessment_id,
-    d.title,
-    d.scope,
-    d.subject_area,
-    d.term_administered,
-    d.administered_at,
-    d.term_taken,
-    d.date_taken,
-    d.module_type,
-    d.module_code as module_number,
-    d.response_type,
-    d.standard_code,
-    d.standard_description,
-    d.domain_description,
-    d.percent_correct,
-    d.is_mastery,
-    d.performance_band_number,
-    d.performance_band_label,
-    d.is_replacement,
-    d.is_normed_scope,
-    d.hr_teachernumber,
-    d.enr_teachernumber,
-    d.teacher_name,
-    d.course_name,
-    d.expression,
-    d.section_number,
-    d.is_foundations,
-    d.head_of_school,
+    student_number,
+    student_name as lastfirst,
+    academic_year,
+    region,
+    territory,
+    school_level,
+    reporting_schoolid as schoolid,
+    school,
+    grade_level,
+    advisory_name as team,
+    enroll_status,
+    cohort,
+    spedlep as iep_status,
+    lep_status,
+    is_504 as c_504_status,
+    is_self_contained as is_pathways,
+    hos as head_of_school,
+    nj_student_tier,
+    is_tutoring as tutoring_nj,
+
+    assessment_id,
+    title,
+    scope,
+    subject_area,
+    term_administered,
+    administered_at,
+    term_taken,
+    date_taken,
+    module_type,
+    module_code as module_number,
+    response_type,
+    response_type_code as standard_code,
+    response_type_description as standard_description,
+    response_type_root_description as domain_description,
+    percent_correct,
+    is_mastery,
+    performance_band_label_number as performance_band_number,
+    performance_band_label,
+    is_replacement,
+    is_internal_assessment as is_normed_scope,
+    advisor_teachernumber as hr_teachernumber,
+    teachernumber as enr_teachernumber,
+    teacher_lastfirst as teacher_name,
+    courses_course_name as course_name,
+    sections_expression as expression,
+    sections_section_number as section_number,
+    is_foundations,
 
     /* retired fields kept for tableau compatibility */
     null as power_standard_goal,
     null as is_power_standard,
     null as standard_domain,
-
-    sf.nj_student_tier,
-    sf.is_tutoring as tutoring_nj,
-    sf.territory,
-from dashboard as d
-left join
-    {{ ref("int_extracts__student_enrollments_subjects") }} as sf
-    on d.student_number = sf.student_number
-    and d.academic_year = sf.academic_year
-    and d.filter_join = sf.assessment_dashboard_join
-    and sf.rn_year = 1
+from dashboard
