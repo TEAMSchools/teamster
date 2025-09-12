b412322619f03af4201d5323abb9cfc4a965284c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-09-12 15:44:33 +0000

    refactor: rename, aggregate

--- src/dbt/kipptaf/models/people/intermediate/int_people__staff_roster.sql
+++ src/dbt/kipptaf/models/people/intermediate/int_people__staff_roster.sql
@@ -1,15 +1,8 @@
 with
     -- trunk-ignore(sqlfluff/ST03)
     staff_roster_active as (
-        select
-            srh.*,
-            epm.memberships,
-            epm.is_in_leadership_program,
-            epm.is_in_teacher_program,
+        select srh.*,
         from {{ ref("int_people__staff_roster_history") }} as srh
-        left join
-            {{ ref("int_people__employee_program_memberships") }} as epm
-            on srh.worker_id = epm.associate_id
         where srh.primary_indicator and (srh.is_current_record or srh.is_prestart)
     ),
 
@@ -23,6 +16,14 @@ with
         }}
     )
 
--- trunk-ignore(sqlfluff/AM04)
-select d.*,
+select
+    d.*,
+
+    epm.memberships,
+    epm.is_leader_development_program,
+    epm.is_teacher_development_program,
 from deduplicate as d
+left join
+    {{ ref("int_adp_workforce_now__employee_memberships") }} as epm
+    on d.worker_id = epm.associate_id
+    and epm.is_current
