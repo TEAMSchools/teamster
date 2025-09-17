735072ba2f3d0a76693102fed677eeccc26a547e
Gaby Rangel <grangel@kippteamandfamily.org>
2025-09-17 17:53:19 +0000

    changed model for goals

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_over_time.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_over_time.sql
@@ -35,55 +35,49 @@ select
     e.schoolid,
     e.school,
     e.student_number,
-    e.student_name,
-    e.student_first_name,
-    e.student_last_name,
     e.grade_level,
     e.enroll_status,
-    e.cohort,
     e.iep_status,
     e.is_504,
     e.grad_iep_exempt_status_overall,
     e.lep_status,
-    e.gifted_and_talented,
-    e.advisory,
-    e.student_email,
-    e.rn_undergrad,
-    e.salesforce_id,
     e.ktc_cohort,
     e.graduation_year,
     e.year_in_network,
-    e.contact_owner_name,
-    e.college_match_gpa,
     e.college_match_gpa_bands,
-    e.cumulative_y1_gpa,
-    e.cumulative_y1_gpa_unweighted,
-    e.cumulative_y1_gpa_projected,
-    e.cumulative_y1_gpa_projected_s1,
-    e.cumulative_y1_gpa_projected_s1_unweighted,
-    e.core_cumulative_y1_gpa,
     e.administration_round,
     e.test_type,
     e.test_date,
     e.test_month,
     e.scope,
     e.subject_area,
-    e.course_discipline,
     e.score_type,
     e.scale_score,
-    e.previous_total_score_change,
-    e.rn_highest,
     e.max_scale_score,
     e.superscore,
     e.running_max_scale_score,
     e.running_superscore,
 
-    bg.hs_ready_min_score,
-    bg.college_ready_min_score,
-    bg.hs_ready_goal,
-    bg.college_ready_goal,
+    metrics,
+
+    case
+        metrics
+        when 'HS-Ready'
+        then bg.hs_ready_min_score
+        when 'College-Ready'
+        then bg.college_ready_min_score
+    end as metric_min_score,
+
+    case
+        metrics
+        when 'HS-Ready'
+        then bg.hs_ready_goal
+        when 'College-Ready'
+        then bg.college_ready_goal
+    end as metric_pct_goal,
 
 from {{ ref("int_students__college_assessment_roster") }} as e
+cross join unnest(["HS-Ready", "College-Ready"]) as metrics
 left join
     benchmark_goals as bg
     on e.test_type = bg.expected_test_type
