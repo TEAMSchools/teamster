799f406067fba6cbfa9ead55817aab3ff58fbe63
Gaby Rangel <grangel@kippteamandfamily.org>
2025-09-17 18:49:11 +0000

    met/not met

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_over_time.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_over_time.sql
@@ -26,60 +26,104 @@ with
             and goal_type = 'Benchmark'
             and expected_subject_area in ('Composite', 'Combined')
         group by expected_test_type, expected_scope, expected_subject_area
+    ),
+
+    metrics_unpivot as (
+        select
+            e.academic_year,
+            e.academic_year_display,
+            e.region,
+            e.schoolid,
+            e.school,
+            e.student_number,
+            e.grade_level,
+            e.enroll_status,
+            e.iep_status,
+            e.is_504,
+            e.grad_iep_exempt_status_overall,
+            e.lep_status,
+            e.ktc_cohort,
+            e.graduation_year,
+            e.year_in_network,
+            e.college_match_gpa_bands,
+            e.administration_round,
+            e.test_type,
+            e.test_date,
+            e.test_month,
+            e.scope,
+            e.subject_area,
+            e.score_type,
+
+            score_category,
+            score,
+
+            metric_name,
+
+            case
+                metric_name
+                when 'HS-Ready'
+                then bg.hs_ready_min_score
+                when 'College-Ready'
+                then bg.college_ready_min_score
+            end as metric_min_score,
+
+            case
+                metric_name
+                when 'HS-Ready'
+                then bg.hs_ready_pct_goal
+                when 'College-Ready'
+                then bg.college_ready_pct_goal
+            end as metric_pct_goal,
+
+        from
+            {{ ref("int_students__college_assessment_roster") }} unpivot (
+                score for score_category in (
+                    scale_score as 'Scale Score',
+                    max_scale_score as 'Max Scale Score',
+                    superscore as 'Superscore',
+                    running_max_scale_score as 'Running Max Scale Score',
+                    running_superscore as 'Running Superscore'
+                )
+            ) as e
+        cross join unnest(['HS-Ready', 'College-Ready']) as metric_name
+        left join
+            benchmark_goals as bg
+            on e.test_type = bg.expected_test_type
+            and e.scope = bg.expected_scope
+            and e.subject_area = bg.expected_subject_area
     )
 
 select
-    e.academic_year,
-    e.academic_year_display,
-    e.region,
-    e.schoolid,
-    e.school,
-    e.student_number,
-    e.grade_level,
-    e.enroll_status,
-    e.iep_status,
-    e.is_504,
-    e.grad_iep_exempt_status_overall,
-    e.lep_status,
-    e.ktc_cohort,
-    e.graduation_year,
-    e.year_in_network,
-    e.college_match_gpa_bands,
-    e.administration_round,
-    e.test_type,
-    e.test_date,
-    e.test_month,
-    e.scope,
-    e.subject_area,
-    e.score_type,
-    e.scale_score,
-    e.max_scale_score,
-    e.superscore,
-    e.running_max_scale_score,
-    e.running_superscore,
+    academic_year,
+    academic_year_display,
+    region,
+    schoolid,
+    school,
+    student_number,
+    grade_level,
+    enroll_status,
+    iep_status,
+    is_504,
+    grad_iep_exempt_status_overall,
+    lep_status,
+    ktc_cohort,
+    graduation_year,
+    year_in_network,
+    college_match_gpa_bands,
+    administration_round,
+    test_type,
+    test_date,
+    test_month,
+    scope,
+    subject_area,
+    score_type,
+    score_category,
+    score,
+    metric_name,
+    metric_min_score,
+    metric_pct_goal,
 
-    metrics,
+    if(score >= metric_min_score, 1, 0) as met_min_score_int,
+    if(metric_min_score is not null, 1, 0) as denominator_int,
 
-    case
-        metrics
-        when 'HS-Ready'
-        then bg.hs_ready_min_score
-        when 'College-Ready'
-        then bg.college_ready_min_score
-    end as metric_min_score,
-
-    case
-        metrics
-        when 'HS-Ready'
-        then bg.hs_ready_pct_goal
-        when 'College-Ready'
-        then bg.college_ready_pct_goal
-    end as metric_pct_goal,
-
-from {{ ref("int_students__college_assessment_roster") }} as e
-cross join unnest(['HS-Ready', 'College-Ready']) as metrics
-left join
-    benchmark_goals as bg
-    on e.test_type = bg.expected_test_type
-    and e.scope = bg.expected_scope
-    and e.subject_area = bg.expected_subject_area
+from metrics_unpivot
