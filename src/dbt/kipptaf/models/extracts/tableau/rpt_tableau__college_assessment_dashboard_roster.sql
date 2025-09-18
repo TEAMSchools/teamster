22a35d6bf4d680d7a631aa1c270c4e629c072043
Gaby Rangel <grangel@kippteamandfamily.org>
2025-09-18 14:42:59 +0000

    refactor

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_roster.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_roster.sql
@@ -35,6 +35,31 @@ with
             graduation_year = {{ var("current_academic_year") + 1 }} and scope != 'ACT'
     ),
 
+    superscores as (
+        select
+            student_number, sat_combined_superscore, sat_ebrw_highest, sat_math_highest,
+
+        from
+            scores pivot (
+                avg(score) for filter_group in (
+                    'SAT Combined Superscore' as sat_combined_superscore,
+                    'SAT EBRW Max Scale Score' as sat_ebrw_highest,
+                    'SAT Math Max Scale Score' as sat_math_highest
+                )
+            )
+    ),
+
+    superscores_dedup as (
+        select
+            student_number,
+            avg(sat_combined_superscore) as sat_combined_superscore,
+            avg(sat_ebrw_highest) as sat_ebrw_highest,
+            avg(sat_math_highest) as sat_math_highest,
+
+        from superscores
+        group by student_number
+    ),
+
     focus_scores as (
         select
             student_number,
@@ -45,7 +70,6 @@ with
             case
                 when
                     filter_group in (
-                        'SAT Combined Superscore',
                         'SAT Combined Scale Score',
                         'PSAT NMSQT Combined Scale Score',
                         'PSAT10 Combined Scale Score'
@@ -54,7 +78,6 @@ with
 
                 when
                     filter_group in (
-                        'SAT EBRW Max Scale Score',
                         'SAT Combined Previous Total Score Change',
                         'PSAT NMSQT Combined Previous Total Score Change',
                         'PSAT10 Combined Previous Total Score Change'
@@ -63,7 +86,6 @@ with
 
                 when
                     filter_group in (
-                        'SAT Math Max Scale Score',
                         'SAT EBRW Scale Score',
                         'PSAT NMSQT EBRW Scale Score',
                         'PSAT10 EBRW Scale Score'
@@ -71,20 +93,14 @@ with
                 then concat(filter_group_month, '_3')
 
                 when
-                    filter_group in (
-                        'SAT Math Scale Score',
-                        'PSAT NMSQT Math Scale Score',
-                        'PSAT10 Math Scale Score'
-                    )
+                    filter_group
+                    in ('PSAT NMSQT Math Scale Score', 'PSAT10 Math Scale Score')
                 then concat(filter_group_month, '_4')
             end as field_name_order,
 
         from scores
         where
             filter_group in (
-                'SAT Combined Superscore',
-                'SAT EBRW Max Scale Score',
-                'SAT Math Max Scale Score',
                 'SAT Combined Scale Score',
                 'SAT Combined Previous Total Score Change',
                 'SAT EBRW Scale Score',
@@ -122,6 +138,10 @@ select
     e.college_match_gpa,
     e.college_match_gpa_bands,
 
+    s.sat_combined_superscore,
+    s.sat_ebrw_highest,
+    s.sat_math_highest,
+
     expected,
 
     f.filter_group,
@@ -130,9 +150,13 @@ select
     f.score,
 
 from {{ ref("int_extracts__student_enrollments") }} as e
+left join superscores_dedup as s on e.student_number = s.student_number
 cross join
     unnest(
         [
+            'g10_3_official_3_1_1',
+            'g10_3_official_3_2_3',
+            'g10_3_official_3_3_4',
             'g10_7_official_4_1_1',
             'g10_7_official_4_2_3',
             'g10_7_official_4_3_4',
@@ -141,35 +165,23 @@ cross join
             'g10_8_official_4_3_4',
             'g11_10_official_2_1_1',
             'g11_10_official_2_1_2',
-            'g11_10_official_2_2_2',
             'g11_10_official_2_2_3',
-            'g11_10_official_2_3_3',
-            'g11_10_official_2_3_4',
             'g11_11_official_2_1_1',
             'g11_11_official_2_1_2',
-            'g11_11_official_2_2_2',
             'g11_11_official_2_2_3',
-            'g11_11_official_2_3_3',
-            'g11_11_official_2_3_4',
             'g11_3_official_3_1_1',
             'g11_3_official_3_2_3',
             'g11_3_official_3_3_4',
             'g11_5_official_2_1_1',
-            'g11_5_official_2_2_2',
+            'g11_5_official_2_1_2',
             'g11_5_official_2_2_3',
-            'g11_5_official_2_3_3',
-            'g11_5_official_2_3_4',
             'g11_8_official_2_1_1',
-            'g11_8_official_2_2_2',
             'g11_8_official_2_2_3',
-            'g11_8_official_2_3_3',
-            'g11_8_official_2_3_4',
             'g11_9_official_2_1_1',
             'g11_9_official_2_1_2',
-            'g11_9_official_2_2_2',
             'g11_9_official_2_2_3',
-            'g11_9_official_2_3_3',
-            'g11_9_official_2_3_4'
+            'g12_9_official_2_1_1',
+            'g12_9_official_2_2_3'
         ]
     ) as expected
 left join
