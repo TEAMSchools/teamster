187b3337df2580be6bbd773dbf5f0dfa208b77d8
Anthony Walters <awalters@kippteamandfamily.org>
2025-09-21 23:18:15 +0000

    add staff data to final agg

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__dashboard_aggregations.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__dashboard_aggregations.sql
@@ -166,6 +166,164 @@ with
             g.aggregation_type,
             g.aggregation_hash,
             g.goal
+    ),
+
+    agg_union_staff as (
+        select
+            m.academic_year,
+            m.home_business_unit_name as region,
+            m.home_work_location_powerschool_school_id as schoolid,
+            m.home_work_location_name as school,
+            m.layer,
+            m.indicator,
+            cast(null as string) as discipline,
+            m.term,
+            m.is_current_week,
+
+            g.org_level,
+            g.has_goal,
+            g.goal_direction,
+            g.aggregation_data_type,
+            g.aggregation_type,
+            g.aggregation_hash,
+            g.goal,
+
+            case
+                g.aggregation_type
+                when 'Average'
+                then round(avg(m.metric_value), 3)
+                when 'Divide'
+                then round(safe_divide(sum(m.numerator), sum(m.denominator)), 3)
+                when 'Sum'
+                then round(sum(m.metric_value), 0)
+            end as metric_aggregate_value,
+        from {{ ref("int_topline__staff_metrics") }} as m
+        left join
+            {{ ref("stg_google_sheets__topline_aggregate_goals") }} as g
+            on m.home_business_unit_name = g.entity
+            and m.home_work_location_powerschool_school_id = g.schoolid
+            and m.layer = g.layer
+            and m.indicator = g.topline_indicator
+            and g.org_level = 'school'
+        group by
+            m.academic_year,
+            m.home_business_unit_name,
+            m.home_work_location_powerschool_school_id,
+            m.home_work_location_name,
+            m.layer,
+            m.indicator,
+            m.term,
+            m.is_current_week,
+            g.org_level,
+            g.has_goal,
+            g.goal_direction,
+            g.aggregation_data_type,
+            g.aggregation_type,
+            g.aggregation_hash,
+            g.goal
+
+    -- union all
+    -- select
+    -- m.academic_year,
+    -- m.region,
+    -- null as schoolid,
+    -- 'All' as school,
+    -- m.layer,
+    -- m.indicator,
+    -- m.discipline,
+    -- m.term,
+    -- m.is_current_week,
+    -- g.org_level,
+    -- g.has_goal,
+    -- g.goal_direction,
+    -- g.aggregation_data_type,
+    -- g.aggregation_type,
+    -- g.aggregation_hash,
+    -- g.goal,
+    -- case
+    -- g.aggregation_type
+    -- when 'Average'
+    -- then round(avg(m.metric_value), 3)
+    -- when 'Divide'
+    -- then round(safe_divide(sum(m.numerator), sum(m.denominator)), 3)
+    -- when 'Sum'
+    -- then round(sum(m.metric_value), 0)
+    -- when 'Comp'
+    -- then round(sum(m.metric_value), 0) / max(target_value)
+    -- end as metric_aggregate_value,
+    -- from {{ ref("int_topline__student_metrics") }} as m
+    -- left join
+    -- {{ ref("stg_google_sheets__topline_aggregate_goals") }} as g
+    -- on m.region = g.entity
+    -- and m.grade_level between g.grade_low and g.grade_high
+    -- and m.layer = g.layer
+    -- and m.indicator = g.topline_indicator
+    -- and g.org_level = 'region'
+    -- group by
+    -- m.academic_year,
+    -- m.region,
+    -- m.layer,
+    -- m.indicator,
+    -- m.discipline,
+    -- m.term,
+    -- m.is_current_week,
+    -- g.org_level,
+    -- g.has_goal,
+    -- g.goal_direction,
+    -- g.aggregation_data_type,
+    -- g.aggregation_type,
+    -- g.aggregation_hash,
+    -- g.goal
+    -- union all
+    -- select
+    -- m.academic_year,
+    -- 'All' as region,
+    -- null as schoolid,
+    -- 'All' as school,
+    -- m.layer,
+    -- m.indicator,
+    -- m.discipline,
+    -- m.term,
+    -- m.is_current_week,
+    -- g.org_level,
+    -- g.has_goal,
+    -- g.goal_direction,
+    -- g.aggregation_data_type,
+    -- g.aggregation_type,
+    -- g.aggregation_hash,
+    -- g.goal,
+    -- case
+    -- g.aggregation_type
+    -- when 'Average'
+    -- then round(avg(m.metric_value), 3)
+    -- when 'Divide'
+    -- then round(safe_divide(sum(m.numerator), sum(m.denominator)), 3)
+    -- when 'Sum'
+    -- then round(sum(m.metric_value), 0)
+    -- when 'Comp'
+    -- then round(sum(m.metric_value), 0) / max(target_value)
+    -- end as metric_aggregate_value,
+    -- from {{ ref("int_topline__student_metrics") }} as m
+    -- left join
+    -- {{ ref("stg_google_sheets__topline_aggregate_goals") }} as g
+    -- on m.grade_level between g.grade_low and g.grade_high
+    -- and m.layer = g.layer
+    -- and m.indicator = g.topline_indicator
+    -- and g.org_level = 'org'
+    -- group by
+    -- m.academic_year,
+    -- m.layer,
+    -- m.indicator,
+    -- m.discipline,
+    -- m.term,
+    -- m.is_current_week,
+    -- g.org_level,
+    -- g.has_goal,
+    -- g.goal_direction,
+    -- g.aggregation_data_type,
+    -- g.aggregation_type,
+    -- g.aggregation_hash,
+    -- g.goal
     )
 
 select
@@ -196,4 +354,36 @@ select
         then (metric_aggregate_value - goal) / goal
     end as goal_difference_percent,
 from agg_union_student
+where term <= current_date('America/New_York')
+
+union all
+
+select
+    *,
+
+    if(
+        aggregation_data_type = 'Numeric', metric_aggregate_value, null
+    ) as metric_aggregate_value_numeric,
+    if(
+        aggregation_data_type = 'Integer', metric_aggregate_value, null
+    ) as metric_aggregate_value_integer,
+    case
+        when not has_goal
+        then null
+        when goal_direction = 'baseball' and metric_aggregate_value >= goal
+        then true
+        when goal_direction = 'golf' and metric_aggregate_value < goal
+        then true
+        else false
+    end as is_goal_met,
+
+    case
+        when not has_goal
+        then null
+        when goal_direction = 'baseball'
+        then (goal - metric_aggregate_value) / goal
+        when goal_direction = 'golf'
+        then (metric_aggregate_value - goal) / goal
+    end as goal_difference_percent,
+from agg_union_staff
 where term <= current_date('America/New_York')
