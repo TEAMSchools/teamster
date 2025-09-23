052ff9943527c34df4cd1ec27e2c89b19be4314a
Anthony Walters <awalters@kippteamandfamily.org>
2025-09-23 19:05:58 +0000

    fix calc

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__dashboard_aggregations.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__dashboard_aggregations.sql
@@ -377,7 +377,7 @@ select
             then safe_divide(metric_aggregate_value, goal)
             when goal_direction = 'golf'
             then 1 - safe_divide(metric_aggregate_value, goal)
-        end,
+        end
         > 1,
         1,
         case
@@ -423,13 +423,25 @@ select
         when goal_direction = 'golf'
         then (metric_aggregate_value - goal) / goal
     end as goal_difference_percent,
-    case
-        when not has_goal
-        then null
-        when goal_direction = 'baseball'
-        then safe_divide(metric_aggregate_value, goal)
-        when goal_direction = 'golf'
-        then 1 - safe_divide(metric_aggregate_value, goal)
-    end as progress_to_goal_pct,
+    if(
+        case
+            when not has_goal
+            then null
+            when goal_direction = 'baseball'
+            then safe_divide(metric_aggregate_value, goal)
+            when goal_direction = 'golf'
+            then 1 - safe_divide(metric_aggregate_value, goal)
+        end
+        > 1,
+        1,
+        case
+            when not has_goal
+            then null
+            when goal_direction = 'baseball'
+            then safe_divide(metric_aggregate_value, goal)
+            when goal_direction = 'golf'
+            then 1 - safe_divide(metric_aggregate_value, goal)
+        end
+    ) as progress_to_goal_pct,
 from agg_union_staff
 where term <= current_date('America/New_York')
