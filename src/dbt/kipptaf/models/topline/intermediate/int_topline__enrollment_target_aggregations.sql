221aab2752e08708e874c2b7a8193ec4d240cf4d
Anthony Walters <awalters@kippteamandfamily.org>
2025-09-24 18:01:38 +0000

    create target view

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__enrollment_target_aggregations.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__enrollment_target_aggregations.sql
@@ -1,13 +1,23 @@
-select
-    *,
+with
+    enrollment as (
+        select
+            *,
 
-    case
-        when org_level = 'org'
-        then org_level
-        when org_level = 'region'
-        then region
-        when org_level = 'school'
-        then cast(schoolid as string)
-    end as join_clause,
-from {{ ref("int_topline__dashboard_aggregations") }}
-where indicator = 'Total Enrollment'
+            case
+                when org_level = 'org'
+                then org_level
+                when org_level = 'region'
+                then region
+                when org_level = 'school'
+                then cast(schoolid as string)
+            end as join_clause,
+        from {{ ref("int_topline__dashboard_aggregations") }}
+        where indicator = 'Total Enrollment'
+    )
+
+select e.*, t.seat_target, t.budget_target,
+from enrollment as e
+inner join
+    {{ ref("stg_google_sheets__topline_enrollment_targets") }} as t
+    on e.academic_year = t.academic_year
+    and e.join_clause = t.join_clause
