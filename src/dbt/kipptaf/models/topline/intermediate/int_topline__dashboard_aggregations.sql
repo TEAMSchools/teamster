a08ee584f2d9452588d3625cffcc3577a705b28e
Anthony Walters <awalters@kippteamandfamily.org>
2025-09-24 19:14:54 +0000

    update dashboard aggregations

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__dashboard_aggregations.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__dashboard_aggregations.sql
@@ -1,5 +1,5 @@
 with
-    agg_union_student as (
+    pre_agg_union_student as (
         select
             m.academic_year,
             m.region,
@@ -174,6 +174,103 @@ with
             g.goal
     ),
 
+    enrollment as (
+        select
+            *,
+
+            case
+                when org_level = 'org'
+                then org_level
+                when org_level = 'region'
+                then region
+                when org_level = 'school'
+                then cast(schoolid as string)
+            end as join_clause
+        from pre_agg_union_student
+        where indicator = 'Total Enrollment'
+    ),
+
+    targets as (
+        select e.*, t.seat_target, t.budget_target
+        from enrollment as e
+        inner join
+            {{ ref("stg_google_sheets__topline_enrollment_targets") }} as t
+            on e.academic_year = t.academic_year
+            and e.join_clause = t.join_clause
+    ),
+
+    target_unpivot as (
+        select
+            academic_year,
+            region,
+            schoolid,
+            school,
+            layer,
+            discipline,
+            term,
+            is_current_week,
+            indicator_display,
+            org_level,
+            has_goal,
+            goal_direction,
+            aggregation_data_type,
+            aggregation_type,
+            aggregation_hash,
+            join_clause,
+            metric_aggregate_value,
+            if(
+                target_type = 'seat_target', 'Seat Target', 'Budget Target'
+            ) as indicator,
+            cast(target_value as int64) as goal,
+        from
+            targets
+            unpivot (target_value for target_type in (seat_target, budget_target))
+        where region != 'Paterson'
+    ),
+
+    target_goals as (
+        select
+            tu.academic_year,
+            tu.region,
+            tu.schoolid,
+            tu.school,
+            tu.layer,
+            tu.indicator,
+            tu.discipline,
+            tu.term,
+            tu.is_current_week,
+
+            tg.indicator_display,
+            tg.org_level,
+            tg.has_goal,
+            tg.goal_direction,
+            tg.aggregation_data_type,
+            tg.aggregation_type,
+            tg.aggregation_hash,
+            tg.goal,
+
+            round(
+                safe_divide(tu.metric_aggregate_value, tu.goal), 3
+            ) as metric_aggregate_value,
+        from target_unpivot as tu
+        left join
+            {{ ref("stg_google_sheets__topline_aggregate_goals") }} as tg
+            on tu.indicator = tg.topline_indicator
+            and tu.layer = tg.layer
+            and tu.aggregation_hash = tg.aggregation_hash
+        where tg.has_goal
+    ),
+
+    agg_union_student as (
+        select *
+        from pre_agg_union_student
+
+        union all
+
+        select *
+        from target_goals
+    ),
+
     agg_union_staff as (
         select
             m.academic_year,
@@ -341,7 +438,7 @@ with
     )
 
 select
-    'Staff Metrics' as metric_type,
+    'Student Metrics' as metric_type,
 
     *,
 
