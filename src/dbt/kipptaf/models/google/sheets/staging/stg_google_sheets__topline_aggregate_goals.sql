2b7abb1b05996edbe0d191572ba40f49b6145792
Anthony Walters <awalters@kippteamandfamily.org>
2025-09-29 01:14:00 +0000

    update lookup

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__topline_aggregate_goals.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__topline_aggregate_goals.sql
@@ -1,6 +1,6 @@
 select
-    * except (goal),
-    cast(goal as numeric) as goal,
+    g.* except (goal),
+    cast(g.goal as numeric) as goal,
 
     case
         when layer = 'Outstanding Teammates' and org_level = 'org'
@@ -16,4 +16,45 @@ select
         when org_level = 'school'
         then schoolid || '_' || grade_low || '-' || grade_high
     end as aggregation_hash,
-from {{ source("google_sheets", "src_google_sheets__topline_aggregate_goals") }}
+
+    case
+        when layer = 'Outstanding Teammates' and org_level = 'org'
+        then org_level
+        when layer = 'Outstanding Teammates' and org_level = 'region'
+        then entity
+        when layer = 'Outstanding Teammates' and org_level = 'school'
+        then s.abbreviation
+        when org_level = 'org'
+        then
+            'Org ' || if(
+                g.grade_low = g.grade_high,
+                cast(g.grade_high as string),
+                if(g.grade_low = 0, 'K', g.grade_low || '-' || g.grade_high)
+            )
+        when org_level = 'region'
+        then
+            entity
+            || ' '
+            || if(
+                g.grade_low = g.grade_high,
+                cast(g.grade_high as string),
+                if(g.grade_low = 0, 'K', g.grade_low || '-' || g.grade_high)
+            )
+        when org_level = 'school'
+        then
+            s.abbreviation
+            || ' '
+            || if(
+                g.grade_low = g.grade_high,
+                cast(g.grade_high as string),
+                if(g.grade_low = 0, 'K', g.grade_low || '-' || g.grade_high)
+            )
+    end as aggregation_display,
+from
+    {{
+        source(
+            "google_sheets",
+            "src_google_sheets__topline_aggregate_goals",
+        )
+    }} as g
+left join {{ ref("stg_powerschool__schools") }} as s on g.schoolid = s.school_number
