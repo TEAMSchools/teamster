0f8eced7ed961e251735449055e8f934ada58379
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-09-30 14:52:35 +0000

    refactor

--- src/dbt/kipptaf/models/google/sheets/intermediate/int_google_sheets__topline_aggregate_goals.sql
+++ src/dbt/kipptaf/models/google/sheets/intermediate/int_google_sheets__topline_aggregate_goals.sql
@@ -9,30 +9,11 @@ select
         when g.layer = 'Outstanding Teammates' and g.org_level = 'school'
         then s.abbreviation
         when g.org_level = 'org'
-        then
-            'Org ' || if(
-                g.grade_low = g.grade_high,
-                cast(g.grade_high as string),
-                if(g.grade_low = 0, 'K', g.grade_low || '-' || g.grade_high)
-            )
+        then 'Org ' || g.grade_band
         when g.org_level = 'region'
-        then
-            g.entity
-            || ' '
-            || if(
-                g.grade_low = g.grade_high,
-                cast(g.grade_high as string),
-                if(g.grade_low = 0, 'K', g.grade_low || '-' || g.grade_high)
-            )
+        then g.entity || ' ' || g.grade_band
         when g.org_level = 'school'
-        then
-            s.abbreviation
-            || ' '
-            || if(
-                g.grade_low = g.grade_high,
-                cast(g.grade_high as string),
-                if(g.grade_low = 0, 'K', g.grade_low || '-' || g.grade_high)
-            )
+        then s.abbreviation || ' ' || g.grade_band
     end as aggregation_display,
 from {{ ref("stg_google_sheets__topline_aggregate_goals") }} as g
 left join {{ ref("stg_powerschool__schools") }} as s on g.schoolid = s.school_number
