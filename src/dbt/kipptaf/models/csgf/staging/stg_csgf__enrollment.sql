8cb6f8c8c69a8af31e2dd8b2d8d69bba04a5aeca
Gaby Rangel <grangel@kippteamandfamily.org>
2025-10-07 18:54:57 +0000

    enrollment counts

--- src/dbt/kipptaf/models/csgf/staging/stg_csgf__enrollment.sql
+++ src/dbt/kipptaf/models/csgf/staging/stg_csgf__enrollment.sql
@@ -4,12 +4,131 @@ with
         from {{ ref("int_powerschool__calendar_rollup") }}
         where yearid = {{ var("current_academic_year") - 1990 }}
         group by yearid, schoolid
+    ),
+
+    students as (
+        select
+            schoolid,
+            student_number,
+
+            case when lunch_status in ('F', 'f', 'R') then 1 else 0 end as frl,
+
+            case
+                race_ethnicity
+                when 'A'
+                then 'Asian'
+                when 'B'
+                then 'BL/AA'
+                when 'H'
+                then 'Hispanic or Latino'
+                when 'I'
+                then 'AI/AN'
+                when 'M'
+                then 'DTS'
+                when 'N'
+                then 'DTS'
+                when 'P'
+                then 'NH/OPI'
+                when 'T'
+                then '2+ races'
+                when 'W'
+                then 'White'
+                when 'Y'
+                then 'DTS'
+                else 'DTS'
+            end race_ethnicity,
+
+            if(grade_level = 0, 'K', cast(grade_level as string)) as grade_level,
+
+            if(iep_status = 'No IEP', 0, 1) as iep,
+
+            if(lep_status, 1, 0) as ell,
+
+        from {{ ref("int_extracts__student_enrollments") }}
+        where
+            academic_year = {{ var("current_academic_year") }}
+            and enroll_status = 0
+            and rn_year = 1
+    ),
+
+    enrollment_counts_pivot as (
+        select
+            schoolid,
+
+            enrollment_k,
+            enrollment_1st,
+            enrollment_2nd,
+            enrollment_3rd,
+            enrollment_4th,
+            enrollment_5th,
+            enrollment_6th,
+            enrollment_7th,
+            enrollment_8th,
+            enrollment_9th,
+            enrollment_10th,
+            enrollment_11th,
+            enrollment_12th,
+
+        from
+            students pivot (
+                count(student_number) for grade_level in (
+                    'k' as enrollment_k,
+                    '1' as enrollment_1st,
+                    '2' as enrollment_2nd,
+                    '3' as enrollment_3rd,
+                    '4' as enrollment_4th,
+                    '5' as enrollment_5th,
+                    '6' as enrollment_6th,
+                    '7' as enrollment_7th,
+                    '8' as enrollment_8th,
+                    '9' as enrollment_9th,
+                    '10' as enrollment_10th,
+                    '11' as enrollment_11th,
+                    '12' as enrollment_12th
+                )
+            )
+    ),
+
+    enrollment_counts as (
+        select
+            schoolid,
+
+            sum(enrollment_k) as enrollment_k,
+            sum(enrollment_1st) as enrollment_1st,
+            sum(enrollment_2nd) as enrollment_2nd,
+            sum(enrollment_3rd) as enrollment_3rd,
+            sum(enrollment_4th) as enrollment_4th,
+            sum(enrollment_5th) as enrollment_5th,
+            sum(enrollment_6th) as enrollment_6th,
+            sum(enrollment_7th) as enrollment_7th,
+            sum(enrollment_8th) as enrollment_8th,
+            sum(enrollment_9th) as enrollment_9th,
+            sum(enrollment_10th) as enrollment_10th,
+            sum(enrollment_11th) as enrollment_11th,
+            sum(enrollment_12th) as enrollment_12th,
+
+        from enrollment_counts_pivot
+        group by schoolid
     )
 
 select
     c.total_instructional_days,
 
     et.budget_target as total_budgeted_enrollment,
+
+    ec.enrollment_k,
+    ec.enrollment_1st,
+    ec.enrollment_2nd,
+    ec.enrollment_3rd,
+    ec.enrollment_4th,
+    ec.enrollment_5th,
+    ec.enrollment_6th,
+    ec.enrollment_7th,
+    ec.enrollment_8th,
+    ec.enrollment_9th,
+    ec.enrollment_10th,
+    ec.enrollment_11th,
+    ec.enrollment_12th,
 
     coalesce(
         case
@@ -48,4 +167,5 @@ left join
     `kipptaf_google_sheets.stg_google_sheets__topline_enrollment_targets` as et
     on s.school_number = et.schoolid
     and et.academic_year = {{ var("current_academic_year") }}
+left join enrollment_counts as ec on s.school_number = ec.schoolid
 left join {{ ref("int_people__staff_roster") }} as r on s.principalemail = r.work_email
