8dcd0dcaa3b80cd88207568dbdef8d8f91474b84
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-10-09 20:01:37 +0000

    refactor: inner join

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__ddi_dashboard.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__ddi_dashboard.sql
@@ -203,14 +203,13 @@ with
                 date(timestamp_trunc(timestamp(a.created), week)), interval 1 day
             ) as week_start_monday,
         from {{ ref("stg_schoolmint_grow__users") }} as u
-        left join
+        inner join
             {{ ref("stg_schoolmint_grow__assignments") }} as a on u.user_id = a.user_id
-        left join
+        inner join
             {{ ref("int_schoolmint_grow__assignments__tags") }} as t
             on a.assignment_id = t.assignment_id
-        left join
+        inner join
             {{ ref("int_schoolmint_grow__microgoals") }} as m on t.tag_id = m.tag_id
-        where m.tag_name is not null
     ),
 
     microgoals as (
