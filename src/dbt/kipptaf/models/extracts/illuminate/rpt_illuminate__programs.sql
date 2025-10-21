d834cc759358857aaa21f0ba9b7f8298359d3d10
Anthony Walters <awalters@kippteamandfamily.org>
2025-10-21 01:01:19 +0000

    update bucket data

--- src/dbt/kipptaf/models/extracts/illuminate/rpt_illuminate__programs.sql
+++ src/dbt/kipptaf/models/extracts/illuminate/rpt_illuminate__programs.sql
@@ -83,33 +83,33 @@ union all
 /* Buckets */
 select
     -- trunk-ignore-begin(sqlfluff/RF05)
-    s.student_number as `01 Import Student ID`,
+    student_number as `01 Import Student ID`,
 
     null as `02 State Student ID`,
 
-    s.last_name as `03 Student Last Name`,
-    s.first_name as `04 Student First Name`,
+    student_last_name as `03 Student Last Name`,
+    student_first_name as `04 Student First Name`,
 
     null as `05 Student Middle Name`,
 
-    s.dob as `06 Birth Date`,
+    dob as `06 Birth Date`,
 
     case
-        when b.discipline = 'ELA' and b.bucket = 'Bucket 1'
+        when discipline = 'ELA' and nj_student_tier = 'Bucket 1'
         then 'B1E'
-        when b.discipline = 'Math' and b.bucket = 'Bucket 1'
+        when discipline = 'Math' and nj_student_tier = 'Bucket 1'
         then 'B1M'
-        when b.discipline = 'ELA' and b.bucket = 'Bucket 2'
+        when discipline = 'ELA' and nj_student_tier = 'Bucket 2'
         then 'B2E'
-        when b.discipline = 'Math' and b.bucket = 'Bucket 2'
+        when discipline = 'Math' and nj_student_tier = 'Bucket 2'
         then 'B2M'
-        when b.discipline = 'ELA' and b.bucket = 'Bucket 3'
+        when discipline = 'ELA' and nj_student_tier = 'Bucket 3'
         then 'B3E'
-        when b.discipline = 'Math' and b.bucket = 'Bucket 3'
+        when discipline = 'Math' and nj_student_tier = 'Bucket 3'
         then 'B3M'
-        when b.discipline = 'ELA' and b.bucket = 'Bucket 4'
+        when discipline = 'ELA' and nj_student_tier = 'Bucket 4'
         then 'BUE'
-        when b.discipline = 'Math' and b.bucket = 'Bucket 4'
+        when discipline = 'Math' and nj_student_tier = 'Bucket 4'
         then 'BUM'
     end as `07 Program ID`,
 
@@ -125,10 +125,5 @@ select
     null as `14 Program Exit Code`,
     null as `15 Site ID`,
 -- trunk-ignore-end(sqlfluff/RF05)
-from bucket_programs as b
-inner join
-    {{ ref("stg_powerschool__students") }} as s
-    on b.studentid = s.id
-    and {{ union_dataset_join_clause(left_alias="b", right_alias="s") }}
-    and s.enroll_status = 0
-where academic_year = {{ current_school_year(var("local_timezone")) }}
+from {{ ref("int_extracts__student_enrollments_subjects") }}
+where academic_year = {{ current_school_year(var("local_timezone")) }} and rn_year = 1
