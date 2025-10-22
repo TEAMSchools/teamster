58b96ccfd38c139ca8eb70f213b5c2fb5f274d29
Gaby Rangel <grangel@kippteamandfamily.org>
2025-10-22 13:08:29 +0000

    roster view

---
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__kippfwd_expected_assessments.sql
@@ -0,0 +1,83 @@
+with
+    months as (
+        select
+            expected_region,
+            expected_grade_level,
+            expected_test_type,
+            expected_scope,
+            expected_admin_season,
+
+            string_agg(
+                regexp_extract(expected_month_round, r'^([^ ]+)'), ', '
+            ) as expected_months_included,
+
+        from
+            {{
+                source(
+                    "google_sheets", "src_google_sheets__kippfwd_expected_assessments"
+                )
+            }}
+        where
+            expected_admin_season != 'Not Official'
+            and expected_score_type like '%total%'
+            and expected_score_type not like '%growth%'
+        group by
+            expected_region,
+            expected_grade_level,
+            expected_test_type,
+            expected_scope,
+            expected_admin_season
+    ),
+
+    scores as (
+        select
+            *,
+
+            case
+                when expected_score_type like '%growth%'
+                then 'Growth'
+                when expected_score_type like '%total%'
+                then 'Total'
+                when expected_score_type like '%ebrw%'
+                then 'EBRW'
+                when expected_score_type like '%math%'
+                then 'Math'
+            end as expected_grouping,
+
+            regexp_extract(expected_month_round, r'^([^ ]+)') as expected_month,
+
+        from
+            {{
+                source(
+                    "google_sheets", "src_google_sheets__kippfwd_expected_assessments"
+                )
+            }}
+    )
+
+select
+    s.*,
+
+    m.expected_months_included,
+
+    concat(
+        'G',
+        cast(s.expected_grade_level as string),
+        ' ',
+        s.expected_admin_season,
+        ' ',
+        s.expected_test_type,
+        ' ',
+        s.expected_scope,
+        ' ',
+        s.expected_grouping
+    ) as expected_field_name,
+
+from scores as s
+left join
+    months as m
+    on s.expected_region = m.expected_region
+    and s.expected_grade_level = m.expected_grade_level
+    and s.expected_test_type = m.expected_test_type
+    and s.expected_scope = m.expected_scope
+    and s.expected_admin_season = m.expected_admin_season
+where s.expected_admin_season != 'Not Official'
