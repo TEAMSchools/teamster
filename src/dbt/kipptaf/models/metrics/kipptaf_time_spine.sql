02614fd6a7d372ee910f0383d766a07412d5fe7b
Cristina Baldor <ccbaldor@icloud.com>
2025-10-23 21:01:48 +0000

    add: time spine config

--- src/dbt/kipptaf/models/metrics/kipptaf_time_spine.sql
+++ src/dbt/kipptaf/models/metrics/kipptaf_time_spine.sql
@@ -1,32 +1,29 @@
 with
     date_spine as (
-        select
-            date_day,
-            -- Calculation for week start/end
-            date_trunc('week', date_day) as week_start,
-            dateadd(day, 6, date_trunc('week', date_day)) as week_end,
-
-            -- Placeholder for joining to a lookup table
-            -- e.g., joined_lookup.reporting_term,
-            'Term A' as reporting_term,  -- Replace with actual join logic
-
-            -- Placeholder for academic year logic
-            case
-                when extract(month from date_day) >= 8
-                then extract(year from date_day) + 1
-                else extract(year from date_day)
-            end as academic_year,
-
-            -- Placeholder for financial quarter logic
-            date_trunc('quarter', date_day) as financial_quarter  -- Or more complex logic
-
+        select date_day,
         from
             unnest(
                 generate_date_array(
                     '2002-07-01', '{{ var("current_fiscal_year" ) }}-06-30'
                 )
             ) as date_day
+    ),
+
+    kipptaf_date_spine as (
+        select
+            date_spine.date_day,
+            date_trunc(date_spine.date_day, week(monday)) as week_start_monday,
+            date_add(
+                date_trunc(date_spine.date_day, week(monday)), interval 6 day
+            ) as week_end_sunday,
+            {{
+                date_to_fiscal_year(
+                    date_field="date_day", start_month=7, year_source="start"
+                )
+            }} as academic_year,
+        from date_spine
+
     )
 
-select *
-from date_spine
+select *,
+from kipptaf_date_spine
