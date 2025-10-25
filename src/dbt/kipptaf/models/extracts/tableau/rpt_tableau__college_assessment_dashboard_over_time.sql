1c385e969fc0fdc704e0bef77b91ff9c23814425
Gaby Rangel <grangel@kippteamandfamily.org>
2025-10-25 21:08:52 +0000

    new calcs

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_over_time.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_over_time.sql
@@ -174,9 +174,14 @@ select
         partition by student_number, test_type, score_type, expected_metric_name
     ) as met_min_score_int_overall,
 
-    max(if(score >= min_score, 1, 0)) over (
+    max(if(score >= min_score and scope in ('ACT', 'SAT'), 1, 0)) over (
         partition by
             student_number, test_type, aligned_subject_area, expected_metric_name
-    ) as met_min_score_int_act_or_sat_overall,
+    ) as met_min_score_int_act_or_sat_overall_aligned_subject_area,
+
+    max(if(score >= min_score and scope in ('ACT', 'SAT'), 1, 0)) over (
+        partition by
+            student_number, test_type, aligned_subject_area, expected_metric_name
+    ) as met_min_score_int_act_or_sat_overall_aligned_subject,
 
 from roster
