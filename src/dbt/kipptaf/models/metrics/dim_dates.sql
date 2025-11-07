2cfffb583f9025f9328577dd0032c8ccdcb2d264
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-11-07 17:53:14 +0000

    refactor: date spine

---
+++ src/dbt/kipptaf/models/metrics/dim_dates.sql
@@ -0,0 +1,26 @@
+with
+    date_spine as (
+        select date_day,
+        from
+            unnest(
+                generate_date_array(
+                    '2002-07-01', '{{ var("current_fiscal_year" ) }}-06-30'
+                )
+            ) as date_day
+    ),
+final as (
+select
+    date_day,
+    date_trunc(date_day, week(monday)) as week_start_monday,
+    date_add(date_trunc(date_day, week(monday)), interval 6 day) as week_end_sunday,
+    {{
+        date_to_fiscal_year(
+            date_field="date_day",
+            start_month=7,
+            year_source="start",
+        )
+    }} as academic_year,
+from date_spine
+)
+
+select * from final where date_day = '2025-10-29'
