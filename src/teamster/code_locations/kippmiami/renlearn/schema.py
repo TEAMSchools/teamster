44601a648b9ed344a06ddc8d74d1b83f3e154cc0
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-11-11 21:21:22 +0000

    chore: disable unused assets

--- src/teamster/code_locations/kippmiami/renlearn/schema.py
+++ src/teamster/code_locations/kippmiami/renlearn/schema.py
@@ -2,19 +2,13 @@ import json
 
 import py_avro_schema
 
-from teamster.libraries.renlearn.schema import (
-    FastStar,
-    Star,
-    StarDashboardStandard,
-    StarSkillArea,
-)
+from teamster.libraries.renlearn.schema import Star, StarDashboardStandard
 
 STAR_SCHEMA = json.loads(py_avro_schema.generate(py_type=Star))
-
-FAST_STAR_SCHEMA = json.loads(py_avro_schema.generate(py_type=FastStar))
 
 STAR_DASHBOARD_STANDARDS_SCHEMA = json.loads(
     py_avro_schema.generate(py_type=StarDashboardStandard)
 )
 
-STAR_SKILL_AREA_SCHEMA = json.loads(py_avro_schema.generate(py_type=StarSkillArea))
+# FAST_STAR_SCHEMA = json.loads(py_avro_schema.generate(py_type=FastStar))
+# STAR_SKILL_AREA_SCHEMA = json.loads(py_avro_schema.generate(py_type=StarSkillArea))
