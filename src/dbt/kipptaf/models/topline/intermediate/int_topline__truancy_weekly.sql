94c6848ba267bc82bc664b4db1bc21df3ba20374
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-11-12 17:09:53 +0000

    refactor: mv running calcs to ps ada

--- src/dbt/kipptaf/models/topline/intermediate/int_topline__truancy_weekly.sql
+++ src/dbt/kipptaf/models/topline/intermediate/int_topline__truancy_weekly.sql
@@ -1,23 +1,17 @@
 select
-    co.student_number,
-    co.schoolid,
-    co.academic_year,
-    co.week_start_monday,
-    co.week_end_sunday,
-    co.week_number_academic_year,
+    student_number,
+    schoolid,
+    academic_year,
+    week_start_monday,
+    week_end_sunday,
+    week_number_academic_year,
 
-    max(if(t.is_truant, 1, 0)) as is_truant_int,
-from {{ ref("int_extracts__student_enrollments_weeks") }} as co
-inner join
-    {{ ref("int_students__truancy") }} as t
-    on co.student_number = t.student_number
-    and co.academic_year = t.academic_year
-    and t.date_day between co.week_start_monday and co.week_end_sunday
-where co.is_enrolled_week
+    max(if(is_truant, 1, 0)) as is_truant_int,
+from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }}
 group by
-    co.student_number,
-    co.schoolid,
-    co.academic_year,
-    co.week_start_monday,
-    co.week_end_sunday,
-    co.week_number_academic_year
+    student_number,
+    schoolid,
+    academic_year,
+    week_start_monday,
+    week_end_sunday,
+    week_number_academic_year
