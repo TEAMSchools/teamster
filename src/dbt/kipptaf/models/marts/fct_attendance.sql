e4fbe70052d9ffec0aa85bdf2f766e356a25bb67
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-12-02 23:46:29 +0000

    add: student max calculations

--- src/dbt/kipptaf/models/marts/fct_attendance.sql
+++ src/dbt/kipptaf/models/marts/fct_attendance.sql
@@ -26,15 +26,50 @@ with
             max(is_suspended) over (
                 partition by student_number, academic_year order by calendardate
             ) as is_suspended_running,
+            max(calendardate) over (
+                partition by student_number, academic_year
+            ) as student_max_date,
         from daily_attendance
+    ),
+
+    student_max_date as (
+        select *, if(date_day = student_max_date, true, false) as is_student_max_date
+        from running_calculations
+    ),
+
+    student_max_values as (
+        select
+            student_number,
+            school_id,
+            date_day,
+            membership_value,
+            is_present,
+            is_absent,
+            is_present_weighted,
+            is_tardy,
+            is_ontime,
+            is_suspended,
+            semester,
+            term,
+            ada_running,
+            pct_ontime_running,
+            if(is_student_max_date, ada_running, null) as ada_running_student_max,
+            if(
+                is_student_max_date, ada_running, null
+            ) as pct_ontime_running_student_max,
+        from student_max_date
     ),
 
     final as (
         select
             *,
-            if(final.ada_running <= .90, 1, 0) as is_chronic_absentee,
-            if(final.pct_ontime_running <= .90, 1, 0) as is_chronic_tardy,
-        from final
+            if(
+                student_max_values.ada_running_student_max <= .90, 1, 0
+            ) as is_chronic_absentee,
+            if(
+                student_max_values.pct_ontime_running_student_max <= .90, 1, 0
+            ) as is_chronic_tardy,
+        from student_max_values
     )
 
 select *
