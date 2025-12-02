fbfdad5b5adad4942d22738cf179fff547b06947
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-12-02 17:22:44 +0000

    refactor: enable, use ps student enr

--- src/dbt/kipptaf/tests/test_incorrect_student_number_pearson.sql
+++ src/dbt/kipptaf/tests/test_incorrect_student_number_pearson.sql
@@ -1,21 +1,18 @@
-{{
-    config(
-        severity="warn", store_failures=true, store_failures_as="view", enabled=false
-    )
-}}
+{{ config(severity="warn", store_failures=true, store_failures_as="view") }}
 
 select
-    a.academic_year,
+    a.studenttestuuid,
     a.localstudentidentifier,
     a.statestudentidentifier,
     a.firstname,
     a.lastorsurname,
+    a.academic_year,
     a.testcode,
 
     e.student_number,
 from {{ ref("int_pearson__all_assessments") }} as a
 left join
-    {{ ref("int_extracts__student_enrollments") }} as e
+    {{ ref("base_powerschool__student_enrollments") }} as e
     on a.localstudentidentifier = e.student_number
     and a.academic_year = e.academic_year
     and {{ union_dataset_join_clause(left_alias="a", right_alias="e") }}
