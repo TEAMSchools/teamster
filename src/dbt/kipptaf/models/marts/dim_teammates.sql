cac8ceaa8f9fac8a93fd5cca661a0940804cf728
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-12-04 20:20:17 +0000

    add: years experience and memberships

--- src/dbt/kipptaf/models/marts/dim_teammates.sql
+++ src/dbt/kipptaf/models/marts/dim_teammates.sql
@@ -16,7 +16,7 @@ with
 
     grade_levels as (select *, from {{ ref("int_powerschool__teacher_grade_levels") }}),
 
-    final as (
+    years_experience as (
         select
             r.employee_number,
             r.effective_date_start,
@@ -33,7 +33,14 @@ with
             r.reports_to_formatted_name,
             r.primary_indicator,
             r.academic_year,
+            r.years_exp_outside_kipp,
+
             gl.grade_level as grade_taught,
+
+            m.memberships,
+            m.is_leader_development_program,
+            m.is_teacher_development_program,
+
             if(
                 r.job_title in (
                     'Teacher',
@@ -46,12 +53,47 @@ with
                 true,
                 false
             ) as is_teacher,
+
+            sum(
+                if(
+                    job_title in (
+                        'Teacher',
+                        'Teacher in Residence',
+                        'Learning Specialist',
+                        'Teacher ESL',
+                        'Teacher,ESL',
+                        'Teacher in Residence ESL',
+                        'Co-Teacher',
+                        'Co-Teacher_historical'
+                    ),
+                    1,
+                    0
+                )
+            ) over (partition by r.employee_number order by r.academic_year)
+            as years_teaching_at_kipp,
+
+            sum(1) over (
+                partition by employee_number order by academic_year
+            ) as year_at_kipp,
+
         from roster as r
         left join
             grade_levels as gl
             on r.powerschool_teacher_number = gl.teachernumber
             and r.academic_year = gl.academic_year
             and gl.grade_level_rank = 1
+        left join
+            {{ ref("int_adp_workforce_now__employee_memberships_by_year") }} as m
+            on r.worker_id = m.associate_id
+            and r.academic_year = m.academic_year
+    ),
+
+    final as (
+        select
+            *,
+            coalesce(years_exp_outside_kipp, 0)
+            + years_teaching_at_kipp as total_years_teaching,
+        from years_experience
     )
 
 select *,
