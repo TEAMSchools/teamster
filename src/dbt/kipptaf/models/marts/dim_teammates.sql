713f5ff1d174a5ac703b388bd075bf108cdb0db5
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-12-04 21:23:08 +0000

    add: years experience cte

--- src/dbt/kipptaf/models/marts/dim_teammates.sql
+++ src/dbt/kipptaf/models/marts/dim_teammates.sql
@@ -2,7 +2,6 @@ with
     roster as (
         select
             *,
-            {# creating field to join to PowerSchool grade levels #}
             {{
                 date_to_fiscal_year(
                     date_field="effective_date_start",
@@ -10,17 +9,47 @@ with
                     year_source="start",
                 )
             }} as academic_year,
+            if(
+                job_title in (
+                    'Teacher',
+                    'Teacher in Residence',
+                    'Learning Specialist',
+                    'Teacher ESL',
+                    'Teacher,ESL',
+                    'Teacher in Residence ESL',
+                    'Co-Teacher',
+                    'Co-Teacher_historical'
+                    'ESE Teacher'
+                ),
+                1,
+                0
+            ) as is_teacher,
         from {{ ref("int_people__staff_roster_history") }}
 
     ),
 
+    years_experience as (
+        select
+            employee_number,
+            academic_year,
+            sum(1) over (
+                partition by employee_number order by academic_year
+            ) as years_at_kipp,
+            sum(is_teacher) over (
+                partition by employee_number order by academic_year
+            ) as years_teaching_at_kipp,
+        from roster
+        group by employee_number, academic_year, is_teacher
+    ),
+
     grade_levels as (select *, from {{ ref("int_powerschool__teacher_grade_levels") }}),
 
-    years_experience as (
+    final as (
         select
             r.employee_number,
             r.effective_date_start,
             r.effective_date_end,
+            r.worker_termination_date,
             r.formatted_name,
             r.assignment_status,
             r.race_ethnicity_reporting,
@@ -34,6 +63,9 @@ with
             r.primary_indicator,
             r.academic_year,
             r.years_exp_outside_kipp,
+            r.years_teaching_in_njfl,
+            r.years_teaching_outside_njfl,
+            r.is_teacher,
 
             gl.grade_level as grade_taught,
 
@@ -41,40 +73,17 @@ with
             m.is_leader_development_program,
             m.is_teacher_development_program,
 
+            y.years_at_kipp,
+            y.years_teaching_at_kipp,
+
+            coalesce(r.years_exp_outside_kipp,0)
+            + y.years_teaching_at_kipp as total_years_teaching,
+
             if(
-                r.job_title in (
-                    'Teacher',
-                    'Teacher in Residence',
-                    'ESE Teacher',
-                    'Learning Specialist',
-                    'Teacher ESL',
-                    'Teacher in Residence ESL'
-                ),
-                true,
-                false
-            ) as is_teacher,
-
-            sum(
-                if(
-                    job_title in (
-                        'Teacher',
-                        'Teacher in Residence',
-                        'Learning Specialist',
-                        'Teacher ESL',
-                        'Teacher,ESL',
-                        'Teacher in Residence ESL',
-                        'Co-Teacher',
-                        'Co-Teacher_historical'
-                    ),
-                    1,
-                    0
-                )
-            ) over (partition by r.employee_number order by r.academic_year)
-            as years_teaching_at_kipp,
-
-            sum(1) over (
-                partition by employee_number order by academic_year
-            ) as year_at_kipp,
+                r.assignment_status in ('Terminated', 'Deceased'),
+                coalesce(r.assignment_status_reason, 'Missing/no Reason'),
+                null
+            ) as termination_reason,
 
         from roster as r
         left join
@@ -86,15 +95,12 @@ with
             {{ ref("int_adp_workforce_now__employee_memberships_by_year") }} as m
             on r.worker_id = m.associate_id
             and r.academic_year = m.academic_year
-    ),
-
-    final as (
-        select
-            *,
-            coalesce(years_exp_outside_kipp, 0)
-            + years_teaching_at_kipp as total_years_teaching,
-        from years_experience
+        left join
+            years_experience as y
+            on r.employee_number = y.employee_number
+            and r.academic_year = y.academic_year
     )
 
 select *,
 from final
+
