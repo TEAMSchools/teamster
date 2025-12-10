eda21ccfdf9a00bedec326eebdc972ee4ed9b275
Charlie Bini <5003326+cbini@users.noreply.github.com>
2025-12-10 19:51:14 +0000

    fix: date

--- src/dbt/finalsite/models/staging/stg_finalsite__status_report.sql
+++ src/dbt/finalsite/models/staging/stg_finalsite__status_report.sql
@@ -10,7 +10,7 @@ select
 
     cast(powerschool_student_number as int) as powerschool_student_number,
 
-    cast(left(enrollment_year, 4) as int) as academic_year,
+    cast(`timestamp` as date) as effective_date,
 
-    parse_date('%m/%d/%y', `timestamp`) as effective_date,
+    cast(left(enrollment_year, 4) as int) as academic_year,
 from {{ source("finalsite", "status_report") }}
