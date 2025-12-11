495d8d795537c2b1310277a1ad6d2bc46ba65ba6
Gaby Rangel <grangel@kippteamandfamily.org>
2025-12-11 21:33:18 +0000

    ktaf only

--- src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__kippfwd_contacts.sql
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__kippfwd_contacts.sql
@@ -69,6 +69,7 @@ with
             and regexp_contains(c.reason, r'^KF:')
         /* record not fixable on SF by either UI or data loader */
         where c.record_id != 14846967
+        and ktc.contact_advising_provider is null
     ),
 
     salesforce_notes as (
