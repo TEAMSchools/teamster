cbc7dbee20c7a87bd196377eb11dd118e94aab1a
Cristina Baldor <cbaldor@kippteamandfamily.org>
2025-12-15 22:30:26 +0000

    full comm log and attendance interventions

--- src/dbt/kipptaf/models/marts/fct_attendance_interventions.sql
+++ src/dbt/kipptaf/models/marts/fct_attendance_interventions.sql
@@ -9,7 +9,7 @@ select
     commlog_type,
     commlog_staff_name,
     schoolid as school_id,
-    if(reason is not null, 'Complete', 'Missing') as intervention_status,
-    if(reason is not null, 1, 0) as intervention_status_required_int,
+    if(commlog_reason is not null, 'Complete', 'Missing') as intervention_status,
+    if(commlog_reason is not null, 1, 0) as intervention_status_required_int,
 
 from {{ ref("int_students__attendance_interventions") }}
--- src/dbt/kipptaf/models/marts/fct_comm_log.sql
+++ src/dbt/kipptaf/models/marts/fct_comm_log.sql
@@ -1,5 +1,5 @@
 with
-    call_completion as (
+    comm_log as (
         select
             student_school_id as student_number,
             call_date as commlog_date,
@@ -9,43 +9,26 @@ with
             topic as commlog_topic,
             call_type as commlog_type,
             call_status as commlog_status,
+            is_attendance_call,
+            is_truancy_call,
 
             if(
-                com.student_school_id is not null
-                and com.reason not like 'Att: Unknown%',
+                student_school_id is not null and reason not like 'Att: Unknown%',
                 true,
                 false
             ) as is_successful,
 
             if(
-                com.student_school_id is not null
-                and com.reason not like 'Att: Unknown%',
-                1,
-                0
+                student_school_id is not null and reason not like 'Att: Unknown%', 1, 0
             ) as is_successful_int,
 
             row_number() over (
                 partition by student_school_id, call_date order by call_date_time desc
             ) as rn_date,
         from {{ ref("int_deanslist__comm_log") }}
-        where is_attendance_call and call_status = 'Completed'
     ),
 
-    final as (
-        select
-            student_number,
-            commlog_date,
-            commlog_staff_name,
-            commlog_reason,
-            commlog_notes,
-            commlog_topic,
-            commlog_type,
-            commlog_status,
-            is_successful,
-            is_successful_int,
-        from call_completion
-        where rn = 1
-    )
+    final as (select *, from comm_log where rn_date = 1)
 
 select *,
 from final
