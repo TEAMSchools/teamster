a78d62e470edd316b31d69c7e52e9240b15960a2
Gaby Rangel <grangel@kippteamandfamily.org>
2025-12-19 19:05:16 +0000

    added surrogate key

--- src/dbt/kipptaf/models/finalsite/intermediate/int_finalsite__status_report.sql
+++ src/dbt/kipptaf/models/finalsite/intermediate/int_finalsite__status_report.sql
@@ -1,25 +1,49 @@
+with
+    transformations as (
+        select
+            f.* except (grade_level, `status`),
+
+            f.grade_level as grade_level_name,
+
+            x.abbreviation as school_abbreviation,
+            x.powerschool_school_id as schoolid,
+
+            initcap(regexp_extract(x.dagster_code_location, r'kipp(\w+)_')) as region,
+
+            initcap(replace(f.`status`, '_', ' ')) as `detailed status`,
+
+            if(
+                f.grade_level = 'Kindergarten',
+                'K',
+                regexp_extract(f.grade_level, r'\d+')
+            ) as grade_level_string,
+
+            if(
+                f.grade_level = 'Kindergarten',
+                0,
+                safe_cast(regexp_extract(f.grade_level, r'\d+') as int64)
+            ) as grade_level,
+
+        from {{ ref("stg_finalsite__status_report") }} as f
+        left join
+            {{ ref("stg_google_sheets__people__location_crosswalk") }} as x
+            on f.school = x.name
+    )
+
 select
-    f.* except (grade_level, `status`),
+    *,
 
-    f.grade_level as grade_level_name,
+    {{
+        dbt_utils.generate_surrogate_key(
+            [
+                "enrollment_year",
+                "last_name",
+                "first_name",
+                "grade_level",
+                "school",
+                "powerschool_student_number",
+            ]
+        )
+    }} as surrogate_key,
 
-    x.abbreviation as school_abbreviation,
-    x.powerschool_school_id as schoolid,
-
-    initcap(regexp_extract(x.dagster_code_location, r'kipp(\w+)_')) as region,
-
-    initcap(replace(f.`status`, '_', ' ')) as `status`,
-
-    if(
-        f.grade_level = 'Kindergarten', 'K', regexp_extract(f.grade_level, r'\d+')
-    ) as grade_level_string,
-
-    if(
-        f.grade_level = 'Kindergarten',
-        0,
-        safe_cast(regexp_extract(f.grade_level, r'^(.*?)\s*(?:st|nd|rd|th)') as int64)
-    ) as grade_level,
-
-from {{ ref("stg_finalsite__status_report") }} as f
-left join
-    {{ ref("stg_google_sheets__people__location_crosswalk") }} as x on f.school = x.name
+from transformations
