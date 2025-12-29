a25c83f16d5d7a809a0eabaf762d223e732dbac6
Gaby Rangel <grangel@kippteamandfamily.org>
2025-12-29 18:53:44 +0000

    leave only fs data

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__finalsite_recruitment_enrollment_students_hub_dashboard.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__finalsite_recruitment_enrollment_students_hub_dashboard.sql
@@ -7,35 +7,26 @@ with
                 order_by="effective_date",
             )
         }}
-    ),
-
-    powerschool_roster as (
-        select
-            _dbt_source_relation,
-            academic_year,
-            region,
-            school_level,
-            school_level_alt,
-            schoolid,
-            school_name,
-            school,
-            student_number,
-            student_name,
-            student_first_name,
-            student_last_name,
-            grade_level,
-            enroll_status,
-            enroll_status_string,
-            entrydate,
-            exitdate,
-            yearid_prev,
-            is_enrolled_oct01,
-            is_enrolled_oct15,
-            boy_status,
-
-        from {{ ref("int_extracts__student_enrollments") }}
-        where grade_level != 99
     )
 
-select *,
-from powerschool_roster
+select
+    t.*,
+
+    x.overall_status,
+    x.funnel_status,
+    x.status_category,
+    x.detailed_status_ranking,
+    x.powerschool_enroll_status,
+    x.valid_detailed_status,
+    x.offered,
+    x.conversion,
+    x.offered_ops,
+    x.conversion_ops,
+
+from temp_deduplicate as t
+left join
+    {{ ref("stg_google_sheets__finalsite_status_crosswalk") }} as x
+    -- fix this later when int view is fixed
+    on t.academic_year = x.enrollment_academic_year
+    and t.enrollment_type = x.enrollment_type
+    and t.detailed_status = x.detailed_status
