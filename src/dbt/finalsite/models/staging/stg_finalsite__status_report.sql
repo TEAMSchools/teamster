7de803091aaa52b4725b2e458c0b4449752899f1
Charlie Bini <5003326+cbini@users.noreply.github.com>
2026-01-06 17:35:28 +0000

    refactor: dedupe staging

--- src/dbt/finalsite/models/staging/stg_finalsite__status_report.sql
+++ src/dbt/finalsite/models/staging/stg_finalsite__status_report.sql
@@ -1,50 +1,35 @@
 with
+    distinct_rows as (
+        select distinct *, from {{ source("finalsite", "status_report") }}
+    ),
+
     transformations as (
         select
-            finalsite_student_id,
-            enrollment_year,
-            enrollment_type,
-            `status`,
-            last_name,
-            first_name,
+            * except (powerschool_student_number, `timestamp`, grade_level),
+
             grade_level as grade_level_name,
-            school,
 
             cast(powerschool_student_number as int) as powerschool_student_number,
 
             cast(`timestamp` as date) as status_start_date,
 
-            cast(left(enrollment_year, 4) as int) as enrollment_academic_year,
+            cast(left(enrollment_year, 4) as int) as academic_year,
 
-            cast(left(enrollment_year, 4) as int) - 1 as academic_year,
-
-            if(
-                grade_level = 'Kindergarten', 'K', regexp_extract(grade_level, r'\d+')
-            ) as grade_level_string,
+            initcap(replace(`status`, '_', ' ')) as detailed_status,
 
             if(
                 grade_level = 'Kindergarten',
                 0,
-                safe_cast(regexp_extract(grade_level, r'\d+') as int64)
+                cast(regexp_extract(grade_level, r'\d+') as int)
             ) as grade_level,
-
-        from {{ source("finalsite", "status_report") }}
+        from distinct_rows
     )
 
 select
     *,
 
-    {{
-        dbt_utils.generate_surrogate_key(
-            [
-                "academic_year",
-                "last_name",
-                "first_name",
-                "grade_level",
-                "school",
-                "powerschool_student_number",
-            ]
-        )
-    }} as surrogate_key,
-
+    lead(status_start_date, 1, '9999-12-31') over (
+        partition by finalsite_student_id, enrollment_year
+        order by status_start_date asc
+    ) as status_end_date,
 from transformations
