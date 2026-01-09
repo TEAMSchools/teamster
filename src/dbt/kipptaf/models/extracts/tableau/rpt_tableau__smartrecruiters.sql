8b9b794151a84e0597cce81013ebb1beef30cd73
Cristina Baldor <cbaldor@kippteamandfamily.org>
2026-01-09 18:00:17 +0000

    fmt

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__smartrecruiters.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__smartrecruiters.sql
@@ -58,7 +58,7 @@ with
             case
                 when
                     department_org_field_value not in ('Teacher', 'Teaching Fellow')
-                    and time_in_application_state_new <=7
+                    and time_in_application_state_new <= 7
                     and application_state <> 'NEW'
                 then 1
                 when
