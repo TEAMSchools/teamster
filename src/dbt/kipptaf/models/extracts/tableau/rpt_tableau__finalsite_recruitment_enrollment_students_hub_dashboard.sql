14ed478ec4061b86b14fc0a335ca1890317b9da7
Gaby Rangel <grangel@kippteamandfamily.org>
2026-01-12 17:48:05 +0000

    changes

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__finalsite_recruitment_enrollment_students_hub_dashboard.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__finalsite_recruitment_enrollment_students_hub_dashboard.sql
@@ -1,60 +1,44 @@
 with
     finalsite_report as (
         select
-            f.*,
+            f.* except (school),
 
             x.region,
-            x.powerschool_school_id,
-            x.abbreviation as school_abbreviation,
+            x.powerschool_school_id as schoolid,
+            x.abbreviation as school,
 
-            concat(f.first_name, f.last_name) as name_join,
-            concat(f.first_name, f.last_name, f.grade_level) as name_grade_level_join,
-        from {{ ref("stg_finalsite__status_report") }} as f
+            cast(f.academic_year as string)
+            || '-'
+            || right(cast(f.academic_year + 1 as string), 2) as academic_year_display,
+
+        from {{ ref("stg_google_sheets__finalsite__sample_data") }} as f
         left join
             {{ ref("stg_google_sheets__people__location_crosswalk") }} as x
             on f.school = x.name
-
-    ),
-
-    enrollment_history_calc as (
-        select
-            student_number,
-            student_first_name,
-            student_last_name,
-            academic_year,
-
-            academic_year + 1 as academic_year_next,
-
-            concat(student_first_name, student_last_name) as name_join,
-
-            sum(
-                if(date_diff(exitdate, entrydate, day) >= 7, 1, 0)
-            ) as enroll_type_check,
-
-        from {{ ref("int_extracts__student_enrollments") }}
-        where grade_level != 99
-        group by student_number, student_first_name, student_last_name, academic_year
     ),
 
     enrollment_type_calc as (
         select
-            * except (enroll_type_check),
+            academic_year,
+            student_number,
+            student_first_name,
+            student_last_name,
 
             case
                 when
                     coalesce(
-                        lag(enroll_type_check) over (
-                            partition by student_number order by academic_year
-                        ),
+                        lag(
+                            sum(if(date_diff(exitdate, entrydate, day) >= 7, 1, 0))
+                        ) over (partition by student_number order by academic_year),
                         0
                     )
                     = 0
                 then 'New'
                 when
                     coalesce(
-                        lag(enroll_type_check) over (
-                            partition by student_number order by academic_year
-                        ),
+                        lag(
+                            sum(if(date_diff(exitdate, entrydate, day) >= 7, 1, 0))
+                        ) over (partition by student_number order by academic_year),
                         0
                     )
                     = 1
@@ -69,109 +53,88 @@ with
                 else 'Returner'
             end as enrollment_type,
 
-        from enrollment_history_calc
+        from {{ ref("int_extracts__student_enrollments") }}
+        where grade_level != 99
+        group by academic_year, student_number, student_first_name, student_last_name
     ),
 
     mod_enrollment_type as (
         select
-            d.* except (enrollment_type),
+            f._dbt_source_relation,
+            f.finalsite_student_id,
+            f.academic_year,
+            f.academic_year_display,
+            f.enrollment_year,
+            f.region,
+            f.schoolid,
+            f.school,
+            f.powerschool_student_number,
+            f.last_name,
+            f.first_name,
+            f.grade_level,
+            f.grade_level_string,
+            f.detailed_status,
+            f.status_start_date,
+            f.status_end_date,
+            f.days_in_status,
 
-            j1.student_number as ps_student_number,
+            coalesce(e.enrollment_type, 'New') as enrollment_type,
 
-            coalesce(
-                if(
-                    d.powerschool_student_number is not null,
-                    j1.enrollment_type,
-                    j2.enrollment_type
-                ),
-                'New'
-            ) as enrollment_type,
-
-        from finalsite_report as d
+        from finalsite_report as f
         left join
-            enrollment_type_calc as j1
-            on d.powerschool_student_number = j1.student_number
-            and d.academic_year = j1.academic_year_next
-            and d.powerschool_student_number is not null
-        left join
-            enrollment_type_calc as j2
-            on d.name_join = j2.name_join
-            and d.academic_year = j1.academic_year_next
-            and d.powerschool_student_number is null
+            enrollment_type_calc as e on f.powerschool_student_number = e.student_number
+    ),
+
+    weekly_spine as (
+        select
+            week_start as week_start_monday,
+            date_add(week_start, interval 6 day) as week_end_sunday
+
+        from
+            -- TODO: hardcoded because idk what dates SRE will ask for
+            unnest(
+                generate_date_array(
+                    date_trunc(date '2025-09-01', week(monday)),
+                    date_trunc(date '2026-08-31', week(monday)),
+                    interval 7 day
+                )
+            ) as week_start
     )
 
 select
-    m.academic_year,
-    m.detailed_status,
-    m.enrollment_year,
-    m.finalsite_student_id,
-    m.first_name,
-    m.grade_level,
-    m.grade_level_name,
-    m.last_name,
-    m.powerschool_student_number,
-    m.school,
-    m.status,
-    m.status_start_date,
-    m.region,
-    m.powerschool_school_id,
-    m.school_abbreviation,
-    m.name_join,
-    m.name_grade_level_join,
-    m.ps_student_number,
-    m.enrollment_type,
+    m.*,
 
-    x.overall_status,
-    x.funnel_status,
-    x.status_category,
-    x.detailed_status_ranking,
-    x.powerschool_enroll_status,
-    x.valid_detailed_status,
+    w.week_start_monday,
+    w.week_end_sunday,
 
-    current_date('{{ var("local_timezone") }}') as today_date,
+    c.overall_status,
+    c.funnel_status,
+    c.status_category,
+    c.offered_status,
+    c.offered_status_detailed,
+    c.detailed_status_ranking,
+    c.detailed_status_branched_ranking,
+    c.powerschool_enroll_status,
+    c.valid_detailed_status,
 
-    count(*) over (partition by m.academic_year, m.finalsite_student_id) as rn,
-
-    case
-        when count(*) over (partition by m.academic_year, m.finalsite_student_id) = 1
-        then
-            coalesce(
-                lag(m.status_start_date) over (
-                    partition by m.academic_year, m.finalsite_student_id
-                    order by x.detailed_status_ranking
-                ),
-                current_date('{{ var("local_timezone") }}')
-            )
-        else
-            coalesce(
-                lead(m.status_start_date - 1) over (
-                    partition by m.academic_year, m.finalsite_student_id
-                    order by x.detailed_status_ranking
-                ),
-                current_date('{{ var("local_timezone") }}')
-            )
-    end as status_end_date,
-
-    coalesce(
-        lag(m.status_start_date) over (
-            partition by m.academic_year, m.name_grade_level_join
-            order by x.detailed_status_ranking
-        ),
-        current_date('{{ var("local_timezone") }}')
-    ) as previous_date,
-
-    coalesce(
-        lead(m.status_start_date - 1) over (
-            partition by m.academic_year, m.finalsite_student_id
-            order by x.detailed_status_ranking
-        ),
-        current_date('{{ var("local_timezone") }}')
-    ) as next_date,
+    c.applicant_ops,
+    c.offered_ops,
+    c.pending_offer_ops,
+    c.overall_conversion_ops,
+    c.offers_to_accepted_den,
+    c.offers_to_accepted_num,
+    c.accepted_to_enrolled_den,
+    c.accepted_to_enrolled_num,
+    c.offers_to_enrolled_den,
+    c.offers_to_enrolled_num,
+    c.waitlisted,
 
 from mod_enrollment_type as m
+inner join
+    weekly_spine as w
+    on m.status_start_date between w.week_start_monday and w.week_end_sunday
 left join
-    {{ ref("stg_google_sheets__finalsite__status_crosswalk") }} as x
-    -- TODO: fix this later when int view is fixed
-    on m.academic_year = x.enrollment_academic_year
-    and m.enrollment_type = x.enrollment_type
-    and m.detailed_status = x.detailed_status
+    {{ ref("stg_google_sheets__finalsite__status_crosswalk") }} as c
+    on m.academic_year = c.academic_year
+    and m.enrollment_type = c.enrollment_type
+    and m.detailed_status = c.detailed_status
