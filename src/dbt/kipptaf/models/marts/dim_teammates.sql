23c275f6e7b06aa8239a08606ac77026cc6ee9a0
Cristina Baldor <cbaldor@kippteamandfamily.org>
2026-01-12 21:43:05 +0000

    add: all roster fields, additional earnings and enrollments tables

--- src/dbt/kipptaf/models/marts/dim_teammates.sql
+++ src/dbt/kipptaf/models/marts/dim_teammates.sql
@@ -11,52 +11,16 @@ with
                 )
             }} as academic_year,
         from {{ ref("int_people__staff_roster_history") }}
-
     ),
 
     grade_levels as (select *, from {{ ref("int_powerschool__teacher_grade_levels") }}),
 
-    additional_earnings_dedupe as (
-
-    {{
-        dbt_utils.deduplicate(
-            relation=ref("stg_adp_workforce_now__additional_earnings_report"),
-            partition_by="employee_number, academic_year, pay_date,additional_earnings_description, gross_pay",
-            order_by="pay_date",
-        )
-    }}
-    ),
-
-    additional_earnings_total as (
-        select
-            employee_number, academic_year, sum(gross_pay) as additional_earnings_total,
-        from additional_earnings_clean
-        group by employee_number, academic_year
-    )
-
     final as (
         select
-            r.employee_number,
-            r.effective_date_start,
-            r.effective_date_end,
-            r.formatted_name,
-            r.assignment_status,
-            r.race_ethnicity_reporting,
-            r.gender_identity,
-            r.home_business_unit_name,
-            r.home_department_name,
-            r.home_work_location_name,
-            r.home_work_location_grade_band,
-            r.job_title,
-            r.reports_to_formatted_name,
-            r.primary_indicator,
-            r.academic_year,
-            r.base_remuneration_annual_rate_amount as salary,
-            r.additional_remunerations_rate_amount as additional_compensation,
-            gl.grade_level as grade_taught,
-            aet.additional_earnings_total,
+            roster.*,
+            grade_levels.grade_level as grade_taught,
             if(
-                r.job_title in (
+                job_title in (
                     'Teacher',
                     'Teacher in Residence',
                     'ESE Teacher',
@@ -67,15 +31,12 @@ with
                 true,
                 false
             ) as is_teacher,
-        from roster as r
+        from roster
         left join
-            grade_levels as gl
-            on r.powerschool_teacher_number = gl.teachernumber
-            and r.academic_year = gl.academic_year
-            and gl.grade_level_rank = 1
-        left join additional_earnings_total as aet
-        on r.employee_number = aet.employee_number
-        and r.academic_year = aet.academic_year
+            grade_levels
+            on roster.powerschool_teacher_number = grade_levels.teachernumber
+            and roster.academic_year = grade_levels.academic_year
+            and grade_levels.grade_level_rank = 1
     )
 
 select *,
---
+++ src/dbt/kipptaf/models/marts/fct_benefits_enrollments.sql
@@ -0,0 +1,4 @@
+    benefits_enrollments as (
+        select *
+        from {{ ref("stg_adp_workforce_now__pension_and_benefits_enrollments") }}
+    )
