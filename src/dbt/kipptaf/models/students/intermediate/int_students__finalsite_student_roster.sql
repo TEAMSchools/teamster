97ba2dc4cfeb404b73c106bd9fe9a89a023f4722
Gaby Rangel <grangel@kippteamandfamily.org>
2026-01-15 17:56:53 +0000

    save

--- src/dbt/kipptaf/models/students/intermediate/int_students__finalsite_student_roster.sql
+++ src/dbt/kipptaf/models/students/intermediate/int_students__finalsite_student_roster.sql
@@ -19,6 +19,7 @@ with
 
     latest_school_id_gl as (
         select
+            _dbt_source_relation,
             academic_year,
             finalsite_student_id,
             schoolid,
@@ -37,6 +38,7 @@ with
 
     enrollment_type_calc as (
         select
+            _dbt_source_relation,
             academic_year,
             student_number,
             first_name,
@@ -73,7 +75,8 @@ with
 
         from {{ ref("base_powerschool__student_enrollments") }}
         where grade_level != 99
-        group by academic_year, student_number, first_name, last_name
+        group by
+            _dbt_source_relation, academic_year, student_number, first_name, last_name
     ),
 
     mod_enrollment_type as (
@@ -101,16 +104,20 @@ with
 
         from finalsite_report as f
         left join
-            enrollment_type_calc as e on f.powerschool_student_number = e.student_number
+            enrollment_type_calc as e
+            on f.powerschool_student_number = e.student_number
+            and {{ union_dataset_join_clause(left_alias="f", right_alias="e") }}
         left join
             latest_school_id_gl as r
             on f.academic_year = r.academic_year
             and f.finalsite_student_id = r.finalsite_student_id
+            and {{ union_dataset_join_clause(left_alias="f", right_alias="r") }}
     ),
 
-    weekly_spine as (
+    scaffold as (
         -- distinct: get a list of schools open tied to an academic year
         select distinct
+            e._dbt_source_relation,
             e.academic_year,
             e.region,
             e.schoolid,
@@ -120,8 +127,34 @@ with
             f.sre_year_start,
             f.sre_year_end,
 
+            c.overall_status,
+            c.funnel_status,
+            c.status_category,
+            c.offered_status,
+            c.offered_status_detailed,
+            c.detailed_status_ranking,
+            c.detailed_status_branched_ranking,
+            c.powerschool_enroll_status,
+            c.valid_detailed_status,
+
+            c.applicant_ops,
+            c.offered_ops,
+            c.pending_offer_ops,
+            c.overall_conversion_ops,
+            c.offers_to_accepted_den,
+            c.offers_to_accepted_num,
+            c.accepted_to_enrolled_den,
+            c.accepted_to_enrolled_num,
+            c.offers_to_enrolled_den,
+            c.offers_to_enrolled_num,
+            c.waitlisted,
+
             week_start as week_start_monday,
             date_add(week_start, interval 6 day) as week_end_sunday,
+
+            cast(e.academic_year as string)
+            || '-'
+            || right(cast(e.academic_year + 1 as string), 2) as academic_year_display,
 
         from {{ ref("int_extracts__student_enrollments") }} as e
         inner join
@@ -138,59 +171,82 @@ with
                     interval 7 day
                 )
             ) as week_start
-        where e.grade_level != 99
+        cross join {{ ref("stg_google_sheets__finalsite__status_crosswalk") }} as c
+        where e.grade_level != 99 and c.academic_year = c.academic_year
     )
 
 select
-    m._dbt_source_relation,
-    m.finalsite_student_id,
-    m.academic_year,
-    m.academic_year_display,
-    m.enrollment_year,
-    m.region,
-    m.schoolid,
-    m.school,
-    m.powerschool_student_number,
-    m.last_name,
-    m.first_name,
-    m.grade_level,
-    m.grade_level_string,
-    m.detailed_status,
-    m.status_start_date,
-    m.status_end_date,
-    m.days_in_status,
-    m.enrollment_type,
-    c.overall_status,
-    c.funnel_status,
-    c.status_category,
-    c.offered_status,
-    c.offered_status_detailed,
-    c.detailed_status_ranking,
-    c.detailed_status_branched_ranking,
-    c.powerschool_enroll_status,
-    c.valid_detailed_status,
+    s._dbt_source_relation,
+    s.academic_year,
+    s.academic_year_display,
+    s.region,
+    s.schoolid,
+    s.school,
+    s.grade_level,
+    s.grade_level_string,
+    s.sre_year_start,
+    s.sre_year_end,
+    s.week_start_monday,
+    s.week_end_sunday,
+    s.overall_status,
+    s.funnel_status,
+    s.status_category,
+    s.offered_status,
+    s.offered_status_detailed,
+    s.detailed_status_ranking,
+    s.detailed_status_branched_ranking,
+    s.powerschool_enroll_status,
+    s.valid_detailed_status,
+    s.applicant_ops,
+    s.offered_ops,
+    s.pending_offer_ops,
+    s.overall_conversion_ops,
+    s.offers_to_accepted_den,
+    s.offers_to_accepted_num,
+    s.accepted_to_enrolled_den,
+    s.accepted_to_enrolled_num,
+    s.offers_to_enrolled_den,
+    s.offers_to_enrolled_num,
+    s.waitlisted,
 
-    c.applicant_ops,
-    c.offered_ops,
-    c.pending_offer_ops,
-    c.overall_conversion_ops,
-    c.offers_to_accepted_den,
-    c.offers_to_accepted_num,
-    c.accepted_to_enrolled_den,
-    c.accepted_to_enrolled_num,
-    c.offers_to_enrolled_den,
-    c.offers_to_enrolled_num,
-    c.waitlisted,
+    coalesce(m1.finalsite_student_id, m2.finalsite_student_id) as finalsite_student_id,
+    coalesce(m1.enrollment_year, m2.enrollment_year) as student_enrollment_year,
+    coalesce(m1.region, m2.region) as student_region,
+    coalesce(m1.schoolid, m2.schoolid) as student_schoolid,
+    coalesce(m1.school, m2.school) as student_school,
+    coalesce(
+        m1.powerschool_student_number, m2.powerschool_student_number
+    ) as powerschool_student_number,
+    coalesce(m1.last_name, m2.last_name) as student_last_name,
+    coalesce(m1.first_name, m2.first_name) as student_first_name,
+    coalesce(m1.grade_level, m2.grade_level) as student_grade_level,
+    coalesce(
+        m1.grade_level_string, m2.grade_level_string
+    ) as student_grade_level_string,
+    coalesce(m1.detailed_status, m2.detailed_status) as student_detailed_status,
+    coalesce(m1.status_start_date, m2.status_start_date) as status_start_date,
+    coalesce(m1.status_end_date, m2.status_end_date) as status_end_date,
+    coalesce(m1.days_in_status, m2.days_in_status) as days_in_status,
+    coalesce(m1.enrollment_type, m2.enrollment_type) as student_enrollment_type,
 
-    w.week_start_monday,
-    w.week_end_sunday,
-
-from mod_enrollment_type as m
-inner join
-    weekly_spine as w
-    on m.status_start_date between w.week_start_monday and w.week_end_sunday
+from scaffold as s
 left join
-    {{ ref("stg_google_sheets__finalsite__status_crosswalk") }} as c
-    on m.academic_year = c.academic_year
-    and m.enrollment_type = c.enrollment_type
-    and m.detailed_status = c.detailed_status
+    mod_enrollment_type as m1
+    on s.academic_year = m1.academic_year
+    and s.schoolid = m1.schoolid
+    and s.grade_level = m1.grade_level
+    and s.detailed_status = m1.detailed_status
+    and s.enrollment_type = m1.enrollment_type
+    and {{ union_dataset_join_clause(left_alias="s", right_alias="m1") }}
+    and m1.status_start_date between s.week_start_monday and s.week_end_sunday
+    and m1.schoolid is not null
+left join
+    mod_enrollment_type as m2
+    on s.academic_year = m2.academic_year
+    and s.schoolid = m2.schoolid
+    and s.grade_level = m2.grade_level
+    and s.detailed_status = m2.detailed_status
+    and s.enrollment_type = m2.enrollment_type
+    and {{ union_dataset_join_clause(left_alias="s", right_alias="m2") }}
+    and m2.status_start_date between s.week_start_monday and s.week_end_sunday
+    and m2.schoolid is null
