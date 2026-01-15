8024044e1a7a0639de20bdd586d9e1e4ca5eb04d
Cristina Baldor <cbaldor@kippteamandfamily.org>
2026-01-15 21:59:21 +0000

    refactor: location field names

--- src/dbt/kipptaf/models/marts/dim_locations.sql
+++ src/dbt/kipptaf/models/marts/dim_locations.sql
@@ -3,17 +3,14 @@ with
 
     final as (
         select
-            location_name,
-            location_clean_name,
+            location_clean_name as location_name,
             location_region,
             location_abbreviation,
             location_grade_band,
             location_powerschool_school_id,
             location_deanslist_school_id,
-            location_reporting_school_id,
             location_is_campus,
             location_is_pathways,
-            location_dagster_code_location,
             location_head_of_schools_employee_number,
             campus_name,
         from location_crosswalk
