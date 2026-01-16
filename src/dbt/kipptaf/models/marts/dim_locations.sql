ba630930058209593eb0e4bc980ef47b78251c57
Cristina Baldor <cbaldor@kippteamandfamily.org>
2026-01-16 19:45:06 +0000

    format, add coalesce

--- src/dbt/kipptaf/models/marts/dim_locations.sql
+++ src/dbt/kipptaf/models/marts/dim_locations.sql
@@ -1,11 +1,10 @@
 with
-    location_crosswalk as (select *, from {{ ref("int_people__location_crosswalk") }})
+    location_crosswalk as (select *, from {{ ref("int_people__location_crosswalk") }}),
 
     final as (
         select
             location_clean_name as location_name,
             location_region,
-            location_abbreviation,
             location_grade_band,
             location_powerschool_school_id,
             location_deanslist_school_id,
@@ -13,8 +12,11 @@ with
             location_is_pathways,
             location_head_of_schools_employee_number,
             campus_name,
+            coalesce(
+                location_abbreviation, location_clean_name
+            ) as location_abbreviation,
         from location_crosswalk
     )
 
-select *
+select *,
 from final
