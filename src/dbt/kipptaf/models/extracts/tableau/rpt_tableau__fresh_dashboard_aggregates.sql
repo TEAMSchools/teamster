c25953a90ce8494c223ab25f5e9d2005fc9513ef
Gaby Rangel <grangel@kippteamandfamily.org>
2026-01-19 19:55:49 +0000

    new agg view

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregates.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregates.sql
@@ -1,5 +1,119 @@
 with
+    daily_spine as (
+        -- need only one row per expected sre academic year
+        select distinct academic_year, calendar_day,
+
+        from {{ ref("int_finalsite__status_report") }}
+        cross join
+            unnest(
+                generate_date_array(
+                    -- trunk-ignore(sqlfluff/LT01)
+                    date_trunc(sre_year_start, week(monday)),
+                    -- trunk-ignore(sqlfluff/LT01)
+                    date_trunc(sre_year_end, week(monday)),
+                    interval 1 day
+                )
+            ) as calendar_day
+        where rn = 1
+    ),
+
+    scaffold as (
+        -- distinct: get a list of schools open tied to an academic year
+        select distinct
+            e._dbt_source_relation,
+            e.academic_year,
+            e.academic_year_display,
+            'KTAF' as org,
+            e.region,
+            e.schoolid,
+            e.school,
+            e.grade_level,
+
+            d.calendar_day,
+
+            metric,
+
+        from {{ ref("int_extracts__student_enrollments") }} as e
+        inner join daily_spine as d on e.academic_year = d.academic_year
+        cross join
+            unnest(
+                [
+                    'Applications',
+                    'Offers',
+                    'Pending Offer <= 4',
+                    'Pending Offer >= 5 & <=10',
+                    'Pending Offer > 10'
+                ]
+            ) as metric
+        where e.grade_level != 99 and e.rn_year = 1
+
+        union all
+
+        /* distinct: get a list of grade levels but schoolid by region tied to an
+           academic year */
+        select distinct
+            e._dbt_source_relation,
+            e.academic_year,
+            e.academic_year_display,
+            'KTAF' as org,
+            e.region,
+            0 as schoolid,
+            'No School Assigned' as school,
+            e.grade_level,
+
+            d.calendar_day,
+
+            metric,
+
+        from {{ ref("int_extracts__student_enrollments") }} as e
+        inner join daily_spine as d on e.academic_year = d.academic_year
+        cross join
+            unnest(
+                [
+                    'Applications',
+                    'Offers',
+                    'Pending Offer <= 4',
+                    'Pending Offer >= 5 & <=10',
+                    'Pending Offer > 10'
+                ]
+            ) as metric
+        where e.grade_level != 99 and e.rn_year = 1
+    ),
+
     summary as (
+        select
+            r._dbt_source_relation,
+            r.academic_year,
+            r.academic_year_display,
+            r.org,
+            r.region,
+            r.school,
+            r.grade_level,
+
+            r.student_finalsite_student_id,
+            r.student_grade_level,
+            r.student_detailed_status,
+            r.status_start_date,
+            r.student_applicant_ops,
+            r.student_offered_ops,
+            r.student_pending_offer_ops,
+            r.student_overall_conversion_ops,
+            r.student_offers_to_accepted_num,
+            r.student_offers_to_accepted_den,
+            r.student_accepted_to_enrolled_num,
+            r.student_accepted_to_enrolled_den,
+            r.student_offers_to_enrolled_num,
+            r.student_offers_to_enrolled_den,
+
+            d.calendar_day,
+
+        from {{ ref("int_students__finalsite_student_roster") }} as r
+        inner join
+            daily_spine as d
+            on d.calendar_day between r.status_start_date and r.status_end_date
+    ),
+
+    max_custom_status as (
         select
             _dbt_source_relation,
             academic_year,
@@ -7,71 +121,73 @@ with
             org,
             region,
             school,
-            finalsite_student_id,
-            student_finalsite_student_id,
             grade_level,
-            detailed_status,
-            status_start_date,
-            days_in_status,
+            student_finalsite_student_id,
+            student_grade_level,
+            calendar_day,
 
-            metric_name,
-            metric_value,
+            max(student_applicant_ops) over (
+                partition by academic_year, student_finalsite_student_id
+            ) as application_cumulative,
 
-        from
-            {{ ref("int_students__finalsite_student_roster") }} unpivot (
-                metric_value for metric_name in (
-                    student_applicant_ops,
-                    student_offered_ops,
-                    student_pending_offer_ops,
-                    student_overall_conversion_ops,
-                    student_offers_to_accepted_den,
-                    student_offers_to_accepted_num,
-                    student_accepted_to_enrolled_den,
-                    student_accepted_to_enrolled_num,
-                    student_offers_to_enrolled_den,
-                    student_offers_to_enrolled_num,
-                    student_waitlisted
-                )
-            )
+            max(student_offered_ops) over (
+                partition by academic_year, student_finalsite_student_id
+            ) as offers_cumulative,
+
+            max(student_pending_offer_ops) over (
+                partition by academic_year, student_finalsite_student_id, calendar_day
+            ) as pending_offer_daily,
+
+        from summary
     ),
 
-    latest_offer_days as (
+    pending_offer_calcs as (
         select
             _dbt_source_relation,
             academic_year,
-            finalsite_student_id,
+            academic_year_display,
+            org,
+            region,
+            school,
+            grade_level,
             student_finalsite_student_id,
+            student_grade_level,
+            calendar_day,
+            application_cumulative,
+            offers_cumulative,
+            pending_offer_daily,
 
-            1 as pending_offer_ops,
+            case
+                when
+                    pending_offer_daily = 1
+                    and sum(pending_offer_daily) over (
+                        partition by academic_year, student_finalsite_student_id
+                        order by calendar_day asc
+                    )
+                    <= 4
+                then 'Pending Offer <= 4'
+                when
+                    pending_offer_daily = 1
+                    and sum(pending_offer_daily) over (
+                        partition by academic_year, student_finalsite_student_id
+                        order by calendar_day asc
+                    )
+                    between 5 and 10
+                then 'Pending Offer >= 5 & <=10'
+                when
+                    pending_offer_daily = 1
+                    and sum(pending_offer_daily) over (
+                        partition by academic_year, student_finalsite_student_id
+                        order by calendar_day asc
+                    )
+                    > 10
+                then 'Pending Offer > 10'
+            end as pending_offer_timing_status,
 
-            if(max(days_in_status) <= 4, 1, 0) as pending_offer_less_equal_4,
-            if(
-                max(days_in_status) between 5 and 10, 1, 0
-            ) as pending_offer_between_5_10,
-            if(max(days_in_status) > 10, 1, 0) as pending_offer_greater_10,
-
-        from summary
-        where metric_name = 'pending_offer_ops' and metric_value = 1
-        group by
-            _dbt_source_relation,
-            academic_year,
-            finalsite_student_id,
-            student_finalsite_student_id
+        from max_custom_status
     ),
 
-    offered as (
-        select
-            _dbt_source_relation,
-            academic_year,
-            finalsite_student_id,
-            student_finalsite_student_id,
-            metric_value as offered_ops,
-
-        from summary
-        where metric_name = 'offered_ops' and metric_value = 1
-    ),
-
-    final_offer_tracking as (
+    final as (
         select
             s._dbt_source_relation,
             s.academic_year,
@@ -80,118 +196,72 @@ with
             s.region,
             s.school,
             s.grade_level,
-            s.finalsite_student_id,
-            s.student_finalsite_student_id,
+            s.calendar_day,
+            s.metric,
 
-            coalesce(o.offered_ops, 0) as offered_ops,
+            p.student_finalsite_student_id,
 
-            coalesce(l.pending_offer_ops, 0) as pending_offer_ops,
-            coalesce(l.pending_offer_less_equal_4, 0) as pending_offer_less_equal_4,
-            coalesce(l.pending_offer_between_5_10, 0) as pending_offer_between_5_10,
-            coalesce(l.pending_offer_greater_10, 0) as pending_offer_greater_10,
-
-            row_number() over (
-                partition by s.academic_year, s.finalsite_student_id
-                order by s.status_start_date desc
-            ) as rn,
-
-        from summary as s
+        from scaffold as s
         left join
-            offered as o
-            on s.academic_year = o.academic_year
-            and s.finalsite_student_id = o.finalsite_student_id
-        left join
-            latest_offer_days as l
-            on s.academic_year = l.academic_year
-            and s.finalsite_student_id = l.finalsite_student_id
-        qualify rn = 1
-    ),
+            pending_offer_calcs as p
+            on s.academic_year = p.academic_year
+            and s.region = p.region
+            and s.school = p.school
+            and s.grade_level = p.grade_level
+            and s.calendar_day = p.calendar_day
+        where s.metric = 'Applications'
 
-    unpivot_offer_tracking as (
+        union all
+
         select
-            _dbt_source_relation,
-            academic_year,
-            academic_year_display,
-            org,
-            region,
-            school,
-            grade_level,
-            finalsite_student_id,
-            student_finalsite_student_id,
+            s._dbt_source_relation,
+            s.academic_year,
+            s.academic_year_display,
+            s.org,
+            s.region,
+            s.school,
+            s.grade_level,
+            s.calendar_day,
+            s.metric,
 
-            metric_name,
-            metric_value,
+            p.student_finalsite_student_id,
 
-        from
-            final_offer_tracking unpivot (
-                metric_value for metric_name in (
-                    offered_ops,
-                    pending_offer_ops,
-                    pending_offer_less_equal_4,
-                    pending_offer_between_5_10,
-                    pending_offer_greater_10
-                )
-            )
+        from scaffold as s
+        left join
+            pending_offer_calcs as p
+            on s.academic_year = p.academic_year
+            and s.region = p.region
+            and s.school = p.school
+            and s.grade_level = p.grade_level
+            and s.calendar_day = p.calendar_day
+        where s.metric = 'Offers'
+
+        union all
+
+        select
+            s._dbt_source_relation,
+            s.academic_year,
+            s.academic_year_display,
+            s.org,
+            s.region,
+            s.school,
+            s.grade_level,
+            s.calendar_day,
+            s.metric,
+
+            p.student_finalsite_student_id,
+
+        from scaffold as s
+        left join
+            pending_offer_calcs as p
+            on s.academic_year = p.academic_year
+            and s.region = p.region
+            and s.school = p.school
+            and s.grade_level = p.grade_level
+            and s.calendar_day = p.calendar_day
+            and s.metric = p.pending_offer_timing_status
     )
 
-select
-    _dbt_source_relation,
-    academic_year,
-    academic_year_display,
-    finalsite_student_id,
-    student_finalsite_student_id,
-    metric_name,
-    metric_value,
-
-    'Org' as org_level,
-    org as org_level_name,
-
-from unpivot_offer_tracking
-
-union all
-
-select
-    _dbt_source_relation,
-    academic_year,
-    academic_year_display,
-    finalsite_student_id,
-    student_finalsite_student_id,
-    metric_name,
-    metric_value,
-
-    'Region' as org_level,
-    region as org_level_name,
-
-from unpivot_offer_tracking
-
-union all
-
-select
-    _dbt_source_relation,
-    academic_year,
-    academic_year_display,
-    finalsite_student_id,
-    student_finalsite_student_id,
-    metric_name,
-    metric_value,
-
-    'School' as org_level,
-    school as org_level_name,
-
-from unpivot_offer_tracking
-
-union all
-
-select
-    _dbt_source_relation,
-    academic_year,
-    academic_year_display,
-    finalsite_student_id,
-    student_finalsite_student_id,
-    metric_name,
-    metric_value,
-
-    'Grade' as org_level,
-    if(grade_level = 0, 'K', safe_cast(grade_level as string)) as org_level_name,
-
-from unpivot_offer_tracking
+select *,
+from final
+where calendar_day = current_date('{{ var("local_timezone") }}')
