8cf96dce801821555da1aae5ac0bb979b2c65b6d
Gaby Rangel <grangel@kippteamandfamily.org>
2026-01-21 14:49:18 +0000

    dual-enroll

---
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_de.sql
@@ -0,0 +1,30 @@
+select
+    s.student_number || "_" || sg.course_number as `hash`,
+
+    s.student_number,
+    s.lastfirst,
+
+    sg.studentid,
+    sg.schoolname,
+    sg.academic_year,
+    sg.course_name,
+    sg.course_number,
+    sg.teacher_name,
+    sg.dcid as storedgrades_dcid,
+
+    de.de_course_name,
+    de.de_pass_yn,
+    de.de_score,
+    de.de_semester,
+    de.de_institution,
+
+from {{ ref("stg_powerschool__storedgrades") }} sg
+left join
+    {{ ref("stg_powerschool__students") }} as s
+    on sg.studentid = s.id
+    and {{ union_dataset_join_clause(left_alias="sg", right_alias="s") }}
+left join
+    {{ ref("stg_powerschool__u_storedgrades_de") }} de
+    on sg.dcid = de.storedgradesdcid
+    and {{ union_dataset_join_clause(left_alias="sg", right_alias="de") }}
+where sg.course_name like '%(DE)' and sg.storecode = 'Y1'
