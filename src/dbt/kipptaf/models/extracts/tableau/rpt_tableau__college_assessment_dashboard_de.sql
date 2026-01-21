330403fc30b8f88a935b872c29ba07d35e948ba2
Gaby Rangel <grangel@kippteamandfamily.org>
2026-01-21 14:54:04 +0000

    trunk

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_de.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__college_assessment_dashboard_de.sql
@@ -16,7 +16,7 @@ select
     de.de_semester,
     de.de_institution,
 
-    s.student_number || "_" || sg.course_number as `hash`,
+    s.student_number || '_' || sg.course_number as `hash`,
 
 from {{ ref("stg_powerschool__storedgrades") }} sg
 left join
@@ -24,7 +24,7 @@ left join
     on sg.studentid = s.id
     and {{ union_dataset_join_clause(left_alias="sg", right_alias="s") }}
 left join
-    {{ ref("stg_powerschool__u_storedgrades_de") }} de
+    {{ ref("stg_powerschool__u_storedgrades_de") }} as de
     on sg.dcid = de.storedgradesdcid
     and {{ union_dataset_join_clause(left_alias="sg", right_alias="de") }}
 where sg.course_name like '%(DE)' and sg.storecode = 'Y1'
