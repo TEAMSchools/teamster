ed48a422318c360d9e837499cf35f15bd02d3a04
Charlie Bini <5003326+cbini@users.noreply.github.com>
2026-01-22 15:50:45 +0000

    fix: missing config

--- src/dbt/kipptaf/models/kippadb/staging/stg_kippadb__contact_note.sql
+++ src/dbt/kipptaf/models/kippadb/staging/stg_kippadb__contact_note.sql
@@ -37,4 +37,4 @@ select
     as academic_year,
 
 from {{ source("kippadb", "contact_note") }}
-where not isdeleted
+where not isdeleted and id != 'a0LQg00000SOadzMAD'  -- record not accesible to fix on SF
--- src/dbt/kipptaf/tests/test_sf_contact_note_duplicate.sql
+++ src/dbt/kipptaf/tests/test_sf_contact_note_duplicate.sql
@@ -1,79 +1,85 @@
-{{ config(severity="warn", store_failures=true, enabled=true) }}
+{{
+    config(
+        severity="warn",
+        store_failures=true,
+        store_failures_as="view",
+        meta={
+            "dagster": {
+                "ref": {"name": "stg_kippadb__contact_note"},
+            },
+        },
+    )
+}}
 
 with
     salesforce_notes as (
         select
             n.id,
             n.name,
-            n.contact as contact__c,
-            n.`subject` as subject__c,
-            n.`date` as date__c,
-            n.`status` as status__c,
-            n.`type` as type__c,
+            n.contact,
+            n.subject,
+            n.date,
+            n.status,
+            n.type,
             n.academic_year,
 
             trim(
                 regexp_replace(regexp_replace(n.comments, r'\r|\n', ' '), r'\s+', ' ')
-            ) as comments__c,
+            ) as comments,
 
             trim(
                 regexp_replace(regexp_replace(n.next_steps, r'\r|\n', ' '), r'\s+', ' ')
-            ) as next_steps__c,
+            ) as next_steps,
 
         from {{ ref("stg_kippadb__contact_note") }} as n
-        left join {{ ref("int_kippadb__roster") }} as ktc on n.contact = ktc.contact_id
-        where
-            n.academic_year >= 2025
-            -- this record is not accesible to fix on SF neither by UI nor via data
-            -- loader
-            and n.id != 'a0LQg00000SOadzMAD'
-            and ktc.contact_advising_provider is null
+        left join {{ ref("stg_kippadb__contact") }} as ktc on n.contact = ktc.id
+        where n.academic_year >= 2025 and ktc.advising_provider is null
     ),
 
     dups as (
         select
-            contact__c,
-            subject__c,
-            comments__c,
-            next_steps__c,
-            date__c,
-            status__c,
-            type__c,
+            contact,
+            `subject`,
+            comments,
+            next_steps,
+            `date`,
+            `status`,
+            `type`,
 
-            count(distinct id) as rows_total,
+            count(id) as rows_total,
 
         from salesforce_notes
-        group by all
-        having count(distinct id) > 1
+        group by contact, `subject`, comments, next_steps, `date`, `status`, `type`
     ),
 
     row_counting as (
         select
             s.*,
+
             d.rows_total,
 
             row_number() over (
-                partition by s.contact__c, s.comments__c order by s.name
+                partition by s.contact, s.comments order by s.name
             ) as order_rows,
 
         from salesforce_notes as s
-        left join
+        inner join
             dups as d
-            on s.contact__c = d.contact__c
-            and s.subject__c = d.subject__c
-            and s.comments__c = d.comments__c
-        where d.rows_total is not null
+            on s.contact = d.contact
+            and s.subject = d.subject
+            and s.comments = d.comments
+            and d.rows_total > 1
     )
 
 select
     id,
-    name,
-    contact__c,
-    subject__c,
-    comments__c,
-    next_steps__c,
-    date__c,
-    status__c,
-    type__c,
+    `name`,
+    contact as contact__c,
+    `subject` as subject__c,
+    comments as comments__c,
+    next_steps as next_steps__c,
+    `date` as date__c,
+    `status` as status__c,
+    `type` as type__c,
 from row_counting
-where order_rows != 1
+where order_rows > 1
