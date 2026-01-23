825bbb024da67c7519157e13a7b75e04fcab20c7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2026-01-23 18:19:40 +0000

    fix

--- src/dbt/overgrad/models/intermediate/int_overgrad__admissions.sql
+++ src/dbt/overgrad/models/intermediate/int_overgrad__admissions.sql
@@ -1,6 +1,6 @@
-select a.*, cf.*,
+select a.*, cf.* except (id, source_object),
 from {{ ref("stg_overgrad__admissions") }} as a
 left join
     {{ ref("int_overgrad__custom_fields_pivot") }} as cf
-    on s.id = cf.id
+    on a.id = cf.id
     and cf.source_object = 'admissions'
