4e7488d5b3ff5f5bfd992e19a7b39e1892bbde04
Cristina Baldor <cbaldor@kippteamandfamily.org>
2026-01-27 19:12:18 +0000

    add: additional recruitment calcs

--- src/dbt/kipptaf/models/marts/fct_teammate_attrition.sql
+++ src/dbt/kipptaf/models/marts/fct_teammate_attrition.sql
@@ -9,8 +9,8 @@ with
 
     academic_years as (select distinct academic_year from teammate_history),
 
-    {# latest record for staff not in an inactive status between 9/1 and 4/30 of an academic year #}
-    denominator_cohort as (
+    {# Foundation Attrition: latest record for staff not in an inactive status between 9/1 and 4/30 of an academic year #}
+    foundation_year_cohort as (
         select
             ay.academic_year,
             th.employee_number,
@@ -24,8 +24,60 @@ with
         group by ay.academic_year, th.employee_number
     ),
 
-    {# any staff not in terminated or deceased status on 9/1 of the following academic year #}
-    returner_cohort as (
+    {# Foundation Attrition: any staff not in terminated or deceased status on 9/1 of the following academic year #}
+    foundation_returner_cohort as (
+        select distinct ay.academic_year, th.employee_number
+        from academic_years as ay
+        inner join
+            teammate_history as th
+            on th.effective_date_start <= date(ay.academic_year + 1, 9, 1)
+            and th.effective_date_end >= date(ay.academic_year + 1, 9, 1)
+        where th.assignment_status not in ('Pre-Start', 'Terminated', 'Deceased')
+    ),
+
+    {# New Jersey Compliance Attrition: latest record for staff not in an inactive status between 7/1 and 6/30 of an academic year #}
+    nj_year_cohort as (
+        select
+            ay.academic_year,
+            th.employee_number,
+            max(th.effective_date_start) as max_effective_date_start,
+        from academic_years as ay
+        inner join
+            teammate_history as th
+            on th.effective_date_start <= date(ay.academic_year + 1, 6, 30)
+            and th.effective_date_end >= date(ay.academic_year, 7, 1)
+        where th.assignment_status not in ('Pre-Start', 'Terminated', 'Deceased')
+        group by ay.academic_year, th.employee_number
+    ),
+
+    {# New Jersey Compliance  Attrition: any staff not in terminated or deceased status on 7/1 of the following academic year #}
+    nj_returner_cohort as (
+        select distinct ay.academic_year, th.employee_number
+        from academic_years as ay
+        inner join
+            teammate_history as th
+            on th.effective_date_start <= date(ay.academic_year + 1, 7, 1)
+            and th.effective_date_end >= date(ay.academic_year + 1, 7, 1)
+        where th.assignment_status not in ('Pre-Start', 'Terminated', 'Deceased')
+    ),
+
+    {# Recruitment Attrition: latest record for staff not in an inactive status between 9/1 and 8/31 of an academic year #}
+    recruitment_year_cohort as (
+        select
+            ay.academic_year,
+            th.employee_number,
+            max(th.effective_date_start) as max_effective_date_start,
+        from academic_years as ay
+        inner join
+            teammate_history as th
+            on th.effective_date_start <= date(ay.academic_year + 1, 8, 31)
+            and th.effective_date_end >= date(ay.academic_year, 9, 1)
+        where th.assignment_status not in ('Pre-Start', 'Terminated', 'Deceased')
+        group by ay.academic_year, th.employee_number
+    ),
+
+    {# Recruitment  Attrition: any staff not in terminated or deceased status on 9/1 of the following academic year #}
+    recruitment_returner_cohort as (
         select distinct ay.academic_year, th.employee_number
         from academic_years as ay
         inner join
@@ -36,16 +88,60 @@ with
     ),
 
     {# left join to compare prior year to following year rosters #}
+    attrition_type_union as (
+        select
+            'foundation' as attrition_type,
+            fyc.academic_year,
+            fyc.employee_number,
+            if(frc.employee_number is null, true, false) as is_attrition
+        from foundation_year_cohort as fyc
+        left join
+            foundation_returner_cohort as frc
+            on fyc.employee_number = frc.employee_number
+            and fyc.academic_year = frc.academic_year
+
+        union all
+
+        select
+            'nj_compliance' as attrition_type,
+            njyc.academic_year,
+            njyc.employee_number,
+            if(njrc.employee_number is null, true, false) as is_attrition
+        from nj_year_cohort as njyc
+        left join
+            nj_returner_cohort as njrc
+            on njyc.employee_number = njrc.employee_number
+            and njyc.academic_year = njrc.academic_year
+
+        union all
+
+        select
+            'recruitment' as attrition_type,
+            ryc.academic_year,
+            ryc.employee_number,
+            if(rrc.employee_number is null, true, false) as is_attrition
+        from recruitment_year_cohort as ryc
+        left join
+            recruitment_returner_cohort as rrc
+            on ryc.employee_number = rrc.employee_number
+            and ryc.academic_year = rrc.academic_year
+    ),
+
     final as (
         select
-            dc.academic_year,
-            dc.employee_number,
-            if(rc.employee_number is null, true, false) as is_attrition
-        from denominator_cohort as dc
-        left join
-            returner_cohort as rc
-            on dc.employee_number = rc.employee_number
-            and dc.academic_year = rc.academic_year
+            academic_year,
+            employee_number,
+            max(
+                case when attrition_type = 'foundation' then is_attrition end
+            ) as foundation_attrition,
+            max(
+                case when attrition_type = 'nj_compliance' then is_attrition end
+            ) as nj_compliance_attrition,
+            max(
+                case when attrition_type = 'recruitment' then is_attrition end
+            ) as recruitment_attrition
+        from attrition_type_union
+        group by academic_year, employee_number
     )
 
 select *,
