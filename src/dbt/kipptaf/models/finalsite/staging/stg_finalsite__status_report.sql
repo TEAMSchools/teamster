90bd744dc28164e0747df64b6723904ad0955d8b
Charlie Bini <5003326+cbini@users.noreply.github.com>
2026-01-29 16:14:48 +0000

    feat: add newark finalsite

--- src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
+++ src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
@@ -3,7 +3,8 @@ with
         {{
             dbt_utils.union_relations(
                 relations=[
-                    source("kippmiami_finalsite", "stg_finalsite__status_report")
+                    source("kippmiami_finalsite", "stg_finalsite__status_report"),
+                    source("kippnewark_finalsite", "stg_finalsite__status_report"),
                 ]
             )
         }}
