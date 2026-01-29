d22c836010f3f855286cff69de29217e92ca0571
Cristina Baldor <cbaldor@kippteamandfamily.org>
2026-01-29 19:19:15 +0000

    add: pm and years experience

--- src/dbt/kipptaf/models/marts/dim_teammates.sql
+++ src/dbt/kipptaf/models/marts/dim_teammates.sql
@@ -25,6 +25,13 @@ with
 
     managers as (select distinct reports_to_employee_number, from roster),
 
+    performance_management_tiers as (
+        select * from {{ ref('int_performance_management__overall_scores') }}),
+
+    years_experience as (
+        select * from {{ ref('int_people__years_experience') }}
+    ),
+
     final as (
         select
             r.teammate_history_key,
@@ -42,7 +49,7 @@ with
             r.home_business_unit_name as entity,
             r.home_department_name as department,
             r.home_work_location_grade_band as grade_band,
-            r.home_work_location_name as location,
+            r.home_work_location_name as `location`,
             r.is_current_record,
             r.is_prestart,
             r.job_title,
@@ -56,6 +63,9 @@ with
             r.worker_rehire_date,
             r.worker_termination_date,
             gl.grade_level as grade_taught,
+            pm.final_tier as performance_management_tier,
+            ye.years_experience_total,
+            ye.years_teaching_total,
             if(
                 r.job_title in (
                     'Teacher',
@@ -75,19 +85,25 @@ with
                 false
             ) as is_manager,
             lag(r.base_remuneration_annual_rate_amount) over (
-                partition by employee_number order by effective_date_start
+                partition by r.employee_number order by r.effective_date_start
             ) as previous_salary,
             lag(r.job_title) over (
-                partition by employee_number order by effective_date_start
+                partition by r.employee_number order by r.effective_date_start
             ) as previous_job_title,
+            
         from roster as r
         left join
             grade_levels as gl
             on r.powerschool_teacher_number = gl.teachernumber
             and r.academic_year = gl.academic_year
             and gl.grade_level_rank = 1
+        left join performance_management_tiers as pm
+        on r.employee_number = pm.employee_number
+        and r.academic_year = pm.academic_year
+        left join years_experience as ye
+        on r.employee_number = ye.employee_number
+        and r.academic_year = ye.academic_year
     )
 
 select *,
 from final
-where employee_number = 101068
