12b8b9ac2326ab7ff0dfb9b03bc20e09c1cb23e3
Casey Gibson <cgibson@kippteamandfamily.org>
2026-01-29 19:18:32 +0000

    gpasat

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__kfwd_spi.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__kfwd_spi.sql
@@ -9,6 +9,7 @@ select
     r.contact_expected_hs_graduation as expected_hs_graduation,
     r.contact_college_match_display_gpa as hs_gpa,
     r.contact_highest_act_score as highest_act_score,
+    r.contact_highest_sat_score as highest_sat_score,
 
     e.status,
     e.pursuing_degree_type,
