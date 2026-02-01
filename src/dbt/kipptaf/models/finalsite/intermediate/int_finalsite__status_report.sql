74024a3176869e1c056349de34752dbce467bdae
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-01 23:58:08 +0000

    more-base-tables

---
+++ src/dbt/kipptaf/models/finalsite/intermediate/int_finalsite__status_report.sql
@@ -0,0 +1,59 @@
+with
+    finalsite_report as (
+        select
+            f.* except (school),
+
+            x.powerschool_school_id as schoolid,
+            x.abbreviation as school,
+
+            row_number() over (
+                partition by f.enrollment_academic_year, f.finalsite_student_id
+                order by f.status_start_date desc
+            ) as rn,
+
+            cast(f.enrollment_academic_year as string)
+            || '-'
+            || right(
+                cast(f.enrollment_academic_year + 1 as string), 2
+            ) as enrollment_academic_year_display,
+
+        from {{ ref("stg_finalsite__status_report") }} as f
+        left join
+            {{ ref("stg_google_sheets__people__location_crosswalk") }} as x
+            on f.school = x.name
+    )
+
+select
+    * except (powerschool_student_number),
+
+    'KTAF' as org,
+
+    /* since we get snapshot data, these will ensure only the latest of these fields is
+    used for a student, retroactively, for a given enroll academic year */
+    coalesce(
+        first_value(schoolid) over (
+            partition by enrollment_academic_year, finalsite_student_id
+            order by status_start_date desc
+        ),
+        0
+    ) as latest_schoolid,
+
+    coalesce(
+        first_value(school) over (
+            partition by enrollment_academic_year, finalsite_student_id
+            order by status_start_date desc
+        ),
+        'No School Assigned'
+    ) as latest_school,
+
+    first_value(region) over (
+        partition by enrollment_academic_year, finalsite_student_id
+        order by status_start_date desc
+    ) as latest_region,
+
+    first_value(powerschool_student_number) over (
+        partition by enrollment_academic_year, finalsite_student_id
+        order by status_start_date desc
+    ) as powerschool_student_number,
+
+from finalsite_report
--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__status_crosswalk.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__status_crosswalk.sql
@@ -1,2 +1,15 @@
-select *,
+select
+    * except (academic_year),
+
+    academic_year as sre_academic_year,
+
+    cast(enrollment_academic_year as string)
+    || '-'
+    || right(
+        cast(enrollment_academic_year + 1 as string), 2
+    ) as enrollment_academic_year_display,
+
+    date(enrollment_academic_year - 1, 10, 16) as sre_academic_year_start,
+    date(enrollment_academic_year, 10, 15) as sre_academic_year_end,
+
 from {{ source("google_sheets", "src_google_sheets__finalsite__status_crosswalk") }}
