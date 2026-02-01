8aefe6945d047d7fcdc7312f0ab17415a4123b2a
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-01 22:43:11 +0000

    upstream display

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__goals.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__goals.sql
@@ -1,1 +1,2 @@
-select *, from {{ source("google_sheets", "src_google_sheets__finalsite__goals") }}
+select * except (academic_year), academic_year as sre_academic_year,
+from {{ source("google_sheets", "src_google_sheets__finalsite__goals") }}
--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__status_crosswalk.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__status_crosswalk.sql
@@ -1,2 +1,12 @@
-select * except (academic_year), academic_year as sre_academic_year,
+select
+    * except (academic_year),
+
+    academic_year as sre_academic_year,
+
+    cast(enrollment_academic_year as string)
+    || '-'
+    || right(
+        cast(enrollment_academic_year + 1 as string), 2
+    ) as enrollment_academic_year_display,
+
 from {{ source("google_sheets", "src_google_sheets__finalsite__status_crosswalk") }}
