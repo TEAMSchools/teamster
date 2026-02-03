e4497045721cee71ad2d299a2df3c1c770c193fd
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-03 12:56:47 +0000

    solve conflicts

--- src/dbt/kipptaf/models/finalsite/intermediate/int_finalsite__status_report.sql
+++ src/dbt/kipptaf/models/finalsite/intermediate/int_finalsite__status_report.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 with
     finalsite_report as (
         select
@@ -58,53 +57,3 @@ select
     ) as powerschool_student_number,
 
 from finalsite_report
-=======
-with
-    finalsite_report as (
-        select
-            f.* except (school),
-
-            x.powerschool_school_id as schoolid,
-            x.abbreviation as school,
-
-        from {{ ref("stg_finalsite__status_report") }} as f
-        left join
-            {{ ref("stg_google_sheets__people__location_crosswalk") }} as x
-            on f.school = x.name
-    )
-
-select
-    * except (powerschool_student_number),
-
-    'KTAF' as org,
-
-    /* since we get snapshot data, these will ensure only the latest of these fields is
-    used for a student, retroactively, for a given enroll academic year */
-    coalesce(
-        first_value(schoolid) over (
-            partition by enrollment_academic_year, finalsite_student_id
-            order by status_start_date desc
-        ),
-        0
-    ) as latest_schoolid,
-
-    coalesce(
-        first_value(school) over (
-            partition by enrollment_academic_year, finalsite_student_id
-            order by status_start_date desc
-        ),
-        'No School Assigned'
-    ) as latest_school,
-
-    first_value(region) over (
-        partition by enrollment_academic_year, finalsite_student_id
-        order by status_start_date desc
-    ) as latest_region,
-
-    first_value(powerschool_student_number) over (
-        partition by enrollment_academic_year, finalsite_student_id
-        order by status_start_date desc
-    ) as powerschool_student_number,
-
-from finalsite_report
->>>>>>> 9f8d38831ad7186d76c66caf89dcfeb87b3e9443
