0abd47afbf3f37879d796d9f5f8e9c68817d7cfa
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-04 17:14:54 +0000

    removed deprecated columns

--- src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
+++ src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
@@ -10,6 +10,22 @@ with
         }}
     )
 
--- trunk-ignore(sqlfluff/AM04)
-select *,
+select
+    _dbt_source_relation,
+    finalsite_student_id,
+    enrollment_year,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    school,
+    powerschool_student_number,
+    last_name,
+    first_name,
+    grade_level_name,
+    grade_level,
+    detailed_status,
+    status_start_date,
+    status_end_date,
+    days_in_status,
+    rn,
+
 from union_relations
