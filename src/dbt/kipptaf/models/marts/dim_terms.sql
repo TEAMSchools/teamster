05ea71ade2a3dddcae570b16d06234260d4cf51e
Cristina Baldor <cbaldor@kippteamandfamily.org>
2026-02-05 21:20:21 +0000

    refactor terms

--- src/dbt/kipptaf/models/marts/dim_terms.sql
+++ src/dbt/kipptaf/models/marts/dim_terms.sql
@@ -1,1 +1,27 @@
-select *, from {{ ref("stg_google_sheets__reporting__terms") }}
+with
+    reporting_terms as (
+        select *, from {{ ref("stg_google_sheets__reporting__terms") }}
+    ),
+
+    final as (
+        select
+            `type` as term_type,
+            code as term_code,
+            `name` as term_name,
+            `start_date` as term_start_date,
+            end_date as term_end_date,
+            academic_year,
+            fiscal_year,
+            powerschool_year_id,
+            powerschool_term_id,
+            school_id,
+            region as entity,
+            grade_band,
+            lockbox_date,
+            is_current,
+            city,
+        from reporting_terms
+    )
+
+select *,
+from final
