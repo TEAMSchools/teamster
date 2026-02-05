e478f018d82d960f4ee3ce64c9bbe425f2f7b033
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-05 14:35:06 +0000

    aligning to main

--- src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
+++ src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
@@ -1,31 +1,8 @@
-with
-    union_relations as (
-        {{
-            dbt_utils.union_relations(
-                relations=[
-                    source("kippmiami_finalsite", "stg_finalsite__status_report"),
-                    source("kippnewark_finalsite", "stg_finalsite__status_report"),
-                ]
-            )
-        }}
+{{
+    dbt_utils.union_relations(
+        relations=[
+            source("kippmiami_finalsite", "stg_finalsite__status_report"),
+            source("kippnewark_finalsite", "stg_finalsite__status_report"),
+        ]
     )
-
-select
-    _dbt_source_relation,
-    finalsite_student_id,
-    enrollment_year,
-    enrollment_academic_year,
-    enrollment_academic_year_display,
-    school,
-    powerschool_student_number,
-    last_name,
-    first_name,
-    grade_level_name,
-    grade_level,
-    detailed_status,
-    status_start_date,
-    status_end_date,
-    days_in_status,
-    rn,
-
-from union_relations
+}}
