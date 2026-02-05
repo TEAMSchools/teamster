ea8e95af9d79a775cacaf75d4ac5207794874839
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-05 17:59:05 +0000

    add sre stat/end dates to xwalk

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__status_crosswalk.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__status_crosswalk.sql
@@ -7,4 +7,7 @@ select
         cast(enrollment_academic_year + 1 as string), 2
     ) as enrollment_academic_year_display,
 
+    date(enrollment_academic_year - 1, 10, 16) as sre_academic_year_start,
+    date(enrollment_academic_year, 6, 30) as sre_academic_year_end,
+
 from {{ source("google_sheets", "src_google_sheets__finalsite__status_crosswalk") }}
