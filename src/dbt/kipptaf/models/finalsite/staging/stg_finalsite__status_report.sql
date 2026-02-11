6bfc08b04f2b910f90b294e108db2cf609375d78
Gaby Rangel <grangel@kippteamandfamily.org>
2026-02-11 22:22:45 +0000

    new stg table for omit

--- src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
+++ src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
@@ -13,17 +13,38 @@ with
     region_calc as (
         -- trunk-ignore(sqlfluff/AM04)
         select
-            *, initcap(regexp_extract(_dbt_source_relation, r'kipp(\w+)_')) as region,
+            * except (extract_year, first_name),
+
+            initcap(regexp_extract(_dbt_source_relation, r'kipp(\w+)_')) as region,
+
+            initcap(first_name) as first_name,
+
+            coalesce(extract_year, 'Next_Year') as extract_year,
+
         from union_relations
+    ),
+
+    latest_region_grade as (
+        select
+            *,
+
+            last_value(region ignore nulls) over (
+                partition by enrollment_academic_year, finalsite_student_id
+                order by status_start_timestamp asc, status_order asc
+                rows between unbounded preceding and unbounded following
+            ) as latest_region,
+
+            last_value(grade_level ignore nulls) over (
+                partition by enrollment_academic_year, finalsite_student_id
+                order by status_start_timestamp asc, status_order asc
+                rows between unbounded preceding and unbounded following
+            ) as latest_grade_level,
+
+        from region_calc
     )
 
-select
-    *,
-
-    last_value(region ignore nulls) over (
-        partition by enrollment_academic_year, finalsite_student_id
-        order by status_start_timestamp asc, status_order asc
-        rows between unbounded preceding and unbounded following
-    ) as latest_region,
-
-from region_calc
+select l.*,
+from latest_region_grade as l
+left join {{ ref('stg_google_sheets__finalsite__exclude_ids') }} as e
+on l.finalsite_student_id = e.finalsite_student_id
+where e.finalsite_student_id is null
