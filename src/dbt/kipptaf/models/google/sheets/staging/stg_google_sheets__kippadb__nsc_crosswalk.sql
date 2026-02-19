9a6772269906c87e248cf091375e7cda8dfe88f8
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-19 14:33:40 +0000

    catch up to main

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__kippadb__nsc_crosswalk.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__kippadb__nsc_crosswalk.sql
@@ -1,5 +1,12 @@
 select
-    *,
+    account_id,
+    college_name_nsc,
+    college_state_nsc,
+    college_code_nsc,
+    meets_full_need,
+    is_strong_oos_option,
+    nces_id,
+    overgrad_urm_grad_rate,
 
     row_number() over (
         partition by account_id order by college_name_nsc desc
