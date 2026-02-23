114a594cb9f8ef78c9c29417953f2b792f62c9c1
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-23 21:13:35 +0000

    new status xwalk

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__status_crosswalk.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__status_crosswalk.sql
@@ -1,3 +1,22 @@
+with
+    transformations as (
+        select
+            *,
+
+            if(
+                enrollment_year_extract = 'Next_Year',
+                {{ var("current_academic_year") + 1 }},
+                {{ var("current_academic_year") }}
+            ) as enrollment_academic_year,
+
+        from
+            {{
+                source(
+                    "google_sheets", "src_google_sheets__finalsite__status_crosswalk"
+                )
+            }}
+    )
+
 select
     *,
 
@@ -10,4 +29,4 @@ select
     date(enrollment_academic_year - 1, 10, 16) as sre_academic_year_start,
     date(enrollment_academic_year, 6, 30) as sre_academic_year_end,
 
-from {{ source("google_sheets", "src_google_sheets__finalsite__status_crosswalk") }}
+from transformations
