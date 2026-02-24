d81754728d4ae2fa9ce584ddc754238c68c8fec9
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-02-24 16:19:23 +0000

    updated int tableau table

--- src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__finalsite_student_scaffold.sql
+++ src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__finalsite_student_scaffold.sql
@@ -1,3 +1,5 @@
+/* denominator groupings for inquiries, applications, waitlisted, offers, assigned
+   school and accepted */
 select
     aligned_enrollment_academic_year,
     aligned_enrollment_academic_year_display,
@@ -15,21 +17,226 @@ select
     last_name,
     grade_level,
     self_contained,
-    detailed_status,
-    status_order,
-    next_academic_year_enrollment_type,
-    ps_region,
-    ps_school,
-    ps_grade_level,
-    ps_enroll_status,
-    detailed_status_branched_ranking,
-    valid_detailed_status,
-    qa_flag,
-    status_group_numerator,
-    status_group_denominator,
-    conversion_metric_numerator,
-    conversion_metric_denominator,
+    enrollment_academic_year_enrollment_type,
+    status_group_denominator as grouped_status,
     sre_academic_year_start,
     sre_academic_year_end,
 
+    max(status_start_date) as status_start_date,
+
 from {{ ref("int_students__finalsite_student_roster") }}
+where
+    status_group_denominator is not null
+    and status_start_date is not null
+    and not qa_flag
+group by
+    aligned_enrollment_academic_year,
+    aligned_enrollment_academic_year_display,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    current_academic_year,
+    next_academic_year,
+    org,
+    region,
+    schoolid,
+    school,
+    finalsite_student_id,
+    powerschool_student_number,
+    first_name,
+    last_name,
+    grade_level,
+    self_contained,
+    enrollment_academic_year_enrollment_type,
+    status_group_denominator,
+    sre_academic_year_start,
+    sre_academic_year_end
+
+union all
+
+-- denominator for conversion metrics groupings 
+select
+    aligned_enrollment_academic_year,
+    aligned_enrollment_academic_year_display,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    current_academic_year,
+    next_academic_year,
+    org,
+    region,
+    schoolid,
+    school,
+    finalsite_student_id,
+    powerschool_student_number,
+    first_name,
+    last_name,
+    grade_level,
+    self_contained,
+    enrollment_academic_year_enrollment_type,
+    conversion_metric_denominator as grouped_status,
+    sre_academic_year_start,
+    sre_academic_year_end,
+
+    max(status_start_date) as status_start_date,
+
+from {{ ref("int_students__finalsite_student_roster") }}
+where
+    conversion_metric_denominator is not null
+    and status_start_date is not null
+    and not qa_flag
+group by
+    aligned_enrollment_academic_year,
+    aligned_enrollment_academic_year_display,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    current_academic_year,
+    next_academic_year,
+    org,
+    region,
+    schoolid,
+    school,
+    finalsite_student_id,
+    powerschool_student_number,
+    first_name,
+    last_name,
+    grade_level,
+    self_contained,
+    enrollment_academic_year_enrollment_type,
+    conversion_metric_denominator,
+    sre_academic_year_start,
+    sre_academic_year_end
+
+union all
+
+-- currently enrolled numerator
+select
+    aligned_enrollment_academic_year,
+    aligned_enrollment_academic_year_display,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    current_academic_year,
+    next_academic_year,
+    org,
+    region,
+    schoolid,
+    school,
+    finalsite_student_id,
+    powerschool_student_number,
+    first_name,
+    last_name,
+    grade_level,
+    self_contained,
+    enrollment_academic_year_enrollment_type,
+    detailed_status as grouped_status,
+    sre_academic_year_start,
+    sre_academic_year_end,
+
+    status_start_date,
+
+from {{ ref("int_students__finalsite_student_roster") }}
+where detailed_status = 'Enrolled' and status_start_date is not null and not qa_flag
+
+union all
+
+/* numerator groupings for pending offer and currently accepted */
+select
+    aligned_enrollment_academic_year,
+    aligned_enrollment_academic_year_display,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    current_academic_year,
+    next_academic_year,
+    org,
+    region,
+    schoolid,
+    school,
+    finalsite_student_id,
+    powerschool_student_number,
+    first_name,
+    last_name,
+    grade_level,
+    self_contained,
+    enrollment_academic_year_enrollment_type,
+    status_group_numerator as grouped_status,
+    sre_academic_year_start,
+    sre_academic_year_end,
+
+    max(status_start_date) as status_start_date,
+
+from {{ ref("int_students__finalsite_student_roster") }}
+where
+    status_group_numerator is not null and status_start_date is not null and not qa_flag
+group by
+    aligned_enrollment_academic_year,
+    aligned_enrollment_academic_year_display,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    current_academic_year,
+    next_academic_year,
+    org,
+    region,
+    schoolid,
+    school,
+    finalsite_student_id,
+    powerschool_student_number,
+    first_name,
+    last_name,
+    grade_level,
+    self_contained,
+    enrollment_academic_year_enrollment_type,
+    status_group_numerator,
+    sre_academic_year_start,
+    sre_academic_year_end
+
+union all
+
+-- numerator for conversion metrics groupings 
+select
+    aligned_enrollment_academic_year,
+    aligned_enrollment_academic_year_display,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    current_academic_year,
+    next_academic_year,
+    org,
+    region,
+    schoolid,
+    school,
+    finalsite_student_id,
+    powerschool_student_number,
+    first_name,
+    last_name,
+    grade_level,
+    self_contained,
+    enrollment_academic_year_enrollment_type,
+    conversion_metric_numerator as grouped_status,
+    sre_academic_year_start,
+    sre_academic_year_end,
+
+    max(status_start_date) as status_start_date,
+
+from {{ ref("int_students__finalsite_student_roster") }}
+where
+    conversion_metric_numerator is not null
+    and status_start_date is not null
+    and not qa_flag
+group by
+    aligned_enrollment_academic_year,
+    aligned_enrollment_academic_year_display,
+    enrollment_academic_year,
+    enrollment_academic_year_display,
+    current_academic_year,
+    next_academic_year,
+    org,
+    region,
+    schoolid,
+    school,
+    finalsite_student_id,
+    powerschool_student_number,
+    first_name,
+    last_name,
+    grade_level,
+    self_contained,
+    enrollment_academic_year_enrollment_type,
+    conversion_metric_numerator,
+    sre_academic_year_start,
+    sre_academic_year_end
