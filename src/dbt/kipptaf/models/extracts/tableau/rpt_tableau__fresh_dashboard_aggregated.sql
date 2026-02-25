ca4da26a806f2fd0a9d148e7c509f3bb0ebb8dd7
Gaby Rangel <grangel@kippteamandfamily.org>
2026-02-25 16:15:19 +0000

    trunk

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregated.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregated.sql
@@ -1,62 +1,12 @@
 with
     scaffold as (
-        -- join for school/grade level
-        select
-            b.academic_year,
-            b.org,
-            b.region,
-            b.schoolid,
-            b.school,
-            b.grade_level,
-
-            g.school_level,
-            g.goal_granularity,
-            g.goal_type,
-            g.goal_name,
-            g.goal_value,
-
-        from {{ ref("stg_google_sheets__finalsite__school_scaffold") }} as b
-        inner join
-            {{ ref("stg_google_sheets__finalsite__goals") }} as g
-            on b.academic_year = g.enrollment_academic_year
-            and b.region = g.region
-            and b.schoolid = g.schoolid
-            and b.grade_level = g.grade_level
-
-        union all
-
-        -- join for school, no grade level
-        select
-            b.academic_year,
-            b.org,
-            b.region,
-            b.schoolid,
-            b.school,
-            null as grade_level,
-
-            g.school_level,
-            g.goal_granularity,
-            g.goal_type,
-            g.goal_name,
-            g.goal_value,
-
-        from {{ ref("stg_google_sheets__finalsite__school_scaffold") }} as b
-        inner join
-            {{ ref("stg_google_sheets__finalsite__goals") }} as g
-            on b.academic_year = g.enrollment_academic_year
-            and b.region = g.region
-            and b.schoolid = g.schoolid
-            and b.grade_level = g.grade_level
-
-        union all
-
         -- join for region/grade level
         select
             b.academic_year,
             b.org,
             b.region,
             b.schoolid,
-            null as school,
+            b.school,
             b.grade_level,
 
             g.school_level,
@@ -71,6 +21,7 @@ with
             on b.academic_year = g.enrollment_academic_year
             and b.region = g.region
             and b.grade_level = g.grade_level
+            and g.goal_granularity = 'Region/Grade Level'
     )
 
 select
