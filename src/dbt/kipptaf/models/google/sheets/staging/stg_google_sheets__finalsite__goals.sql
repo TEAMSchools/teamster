f5de9d9d585cd136c20466f9a89a679dd184cdd7
Gaby Rangel <grangel@kippteamandfamily.org>
2026-02-26 14:10:19 +0000

    agg

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__goals.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__finalsite__goals.sql
@@ -1,13 +1,1 @@
-select
-    enrollment_academic_year,
-    region,
-    school_level,
-    schoolid,
-    school,
-    grade_level,
-    goal_granularity,
-    goal_type,
-    goal_name,
-    goal_value,
-
-from {{ source("google_sheets", "src_google_sheets__finalsite__goals") }}
+select *, from {{ source("google_sheets", "src_google_sheets__finalsite__goals") }}
