d8e4dd99d797eb10c00e4db90a547e09327fa1e0
Gaby Rangel <grangel@kippteamandfamily.org>
2026-02-26 19:37:56 +0000

    fix academic year display

--- src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
+++ src/dbt/kipptaf/models/finalsite/staging/stg_finalsite__status_report.sql
@@ -26,10 +26,10 @@ with
                 cast(regexp_extract(application_grade, r'\d+') as int)
             ) as grade_level,
 
-            cast(active_school_year as string)
+            cast(active_school_year_int as string)
             || '-'
             || right(
-                cast(active_school_year + 1 as string), 2
+                cast(active_school_year_int + 1 as string), 2
             ) as active_school_year_display,
 
         from union_relations
