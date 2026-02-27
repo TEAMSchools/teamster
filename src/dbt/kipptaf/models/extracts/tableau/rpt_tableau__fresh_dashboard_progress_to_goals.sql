ec770d50bb1b712d691a518d77eef2f3020b0b86
Gaby Rangel <grangel@kippteamandfamily.org>
2026-02-27 21:28:30 +0000

    dedupe

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_progress_to_goals.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_progress_to_goals.sql
@@ -1,96 +1,125 @@
-/* PART 1: THE STUDENTS (Actuals) */
+with
+    data_stack as (
+        /* PART 1: THE STUDENTS (Actuals) */
+        select
+            r.aligned_enrollment_academic_year,
+            r.region,
+            r.schoolid,
+            r.school,
+            r.finalsite_id,
+            r.latest_status,
+            r.aligned_enrollment_type as enrollment_type,
+
+            'Student' as row_type,
+
+            1 as student_count,
+
+            null as seat_target,
+            null as fdos_target,
+            null as budget_target,
+            null as new_student_target,
+            null as re_enroll_projection,
+
+        from {{ ref("int_tableau__finalsite_student_scaffold") }} as r
+        where r.latest_status = 'Enrolled' and r.grouped_status = 'Enrolled'
+
+        union all
+
+        select
+            r.aligned_enrollment_academic_year,
+            r.region,
+            r.schoolid,
+            r.school,
+            r.finalsite_id,
+            r.latest_status,
+            r.enrollment_academic_year_enrollment_type as enrollment_type,
+
+            'Student' as row_type,
+
+            1 as student_count,
+
+            null as seat_target,
+            null as fdos_target,
+            null as budget_target,
+            null as new_student_target,
+            null as re_enroll_projection,
+
+        from {{ ref("int_tableau__finalsite_student_scaffold") }} as r
+        where r.latest_status = 'Enrolled' and r.grouped_status = 'Enrolled'
+
+        union all
+
+        /* PART 2: THE GOALS (Targets) */
+        select
+            gs.enrollment_academic_year as aligned_enrollment_academic_year,
+            gs.region,
+            gs.schoolid,
+            gs.school,
+
+            null as finalsite_id,
+
+            'Goal Record' as latest_status,
+            null as enrollment_type,
+            'Goal' as row_type,
+
+            0 as student_count,
+
+            gs.goal_value as seat_target,
+
+            gf.goal_value as fdos_target,
+
+            gb.goal_value as budget_target,
+
+            gn.goal_value as new_student_target,
+
+            ge.goal_value as re_enroll_projection,
+
+        from {{ ref("stg_google_sheets__finalsite__goals") }} as gs
+        left join
+            {{ ref("stg_google_sheets__finalsite__goals") }} as gf
+            on gs.schoolid = gf.schoolid
+            and gs.goal_granularity = gf.goal_granularity
+            and gf.goal_name = 'FDOS Target'
+        left join
+            {{ ref("stg_google_sheets__finalsite__goals") }} as gb
+            on gs.schoolid = gb.schoolid
+            and gs.goal_granularity = gb.goal_granularity
+            and gb.goal_name = 'Budget Target'
+        left join
+            {{ ref("stg_google_sheets__finalsite__goals") }} as gn
+            on gs.schoolid = gn.schoolid
+            and gs.goal_granularity = gn.goal_granularity
+            and gn.goal_name = 'New Student Target'
+        left join
+            {{ ref("stg_google_sheets__finalsite__goals") }} as ge
+            on gs.schoolid = ge.schoolid
+            and gs.goal_granularity = ge.goal_granularity
+            and ge.goal_name = 'Re-Enroll Projection'
+        where gs.goal_name = 'Seat Target' and gs.goal_granularity = 'School'
+    )
+
 select
-    r.aligned_enrollment_academic_year,
-    r.region,
-    r.schoolid,
-    r.school,
-    r.finalsite_id,
-    r.latest_status,
-    r.aligned_enrollment_type as enrollment_type,
+    s.academic_year as aligned_enrollment_academic_year,
+    s.org,
+    s.region,
+    s.schoolid,
+    s.school,
 
-    'Student' as row_type,
+    d.finalsite_id,
+    d.latest_status,
+    d.enrollment_type,
+    d.row_type,
+    d.student_count,
+    d.seat_target,
+    d.fdos_target,
+    d.budget_target,
+    d.new_student_target,
+    d.re_enroll_projection,
 
-    1 as student_count,
-
-    null as seat_target,
-    null as fdos_target,
-    null as budget_target,
-    null as new_student_target,
-    null as re_enroll_projection,
-
-from {{ ref("int_tableau__finalsite_student_scaffold") }} as r
-where r.grouped_status = 'Enrolled'
-
-union all
-
-select
-    r.aligned_enrollment_academic_year,
-    r.region,
-    r.schoolid,
-    r.school,
-    r.finalsite_id,
-    r.latest_status,
-    r.enrollment_academic_year_enrollment_type as enrollment_type,
-
-    'Student' as row_type,
-
-    1 as student_count,
-
-    null as seat_target,
-    null as fdos_target,
-    null as budget_target,
-    null as new_student_target,
-    null as re_enroll_projection,
-
-from {{ ref("int_tableau__finalsite_student_scaffold") }} as r
-where r.grouped_status = 'Enrolled'
-
-union all
-
-/* PART 2: THE GOALS (Targets) */
-select
-    gs.enrollment_academic_year as aligned_enrollment_academic_year,
-    gs.region,
-    gs.schoolid,
-    gs.school,
-
-    null as finalsite_student_id,
-
-    'Goal Record' as latest_status,
-    null as enrollment_type,
-    'Goal' as row_type,
-
-    0 as student_count,
-
-    gs.goal_value as seat_target,
-
-    gf.goal_value as fdos_target,
-
-    gb.goal_value as budget_target,
-
-    gn.goal_value as new_student_target,
-
-    ge.goal_value as re_enroll_projection,
-
-from {{ ref("stg_google_sheets__finalsite__goals") }} as gs
+from {{ ref("stg_google_sheets__finalsite__school_scaffold") }} as s
 left join
-    {{ ref("stg_google_sheets__finalsite__goals") }} as gf
-    on gs.schoolid = gf.schoolid
-    and gs.goal_granularity = gf.goal_granularity
-    and gf.goal_name = 'FDOS Target'
-left join
-    {{ ref("stg_google_sheets__finalsite__goals") }} as gb
-    on gs.schoolid = gb.schoolid
-    and gs.goal_granularity = gb.goal_granularity
-    and gb.goal_name = 'Budget Target'
-left join
-    {{ ref("stg_google_sheets__finalsite__goals") }} as gn
-    on gs.schoolid = gn.schoolid
-    and gs.goal_granularity = gn.goal_granularity
-    and gn.goal_name = 'New Student Target'
-left join
-    {{ ref("stg_google_sheets__finalsite__goals") }} as ge
-    on gs.schoolid = ge.schoolid
-    and gs.goal_granularity = ge.goal_granularity
-    and ge.goal_name = 'Re-Enroll Projection'
-where gs.goal_name = 'Seat Target' and gs.goal_granularity = 'School'
+    data_stack as d
+    on s.academic_year = d.aligned_enrollment_academic_year
+    and s.region = d.region
+    and s.schoolid = d.schoolid
+where s.academic_year = {{ var("current_academic_year") + 1 }} and s.grade_level = -1
