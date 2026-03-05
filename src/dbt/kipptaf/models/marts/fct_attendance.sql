397651b112f20d85bd3dc0fdb5d8cce98b962a7a
Cristina Baldor <ccbaldor@icloud.com>
2026-03-05 21:04:53 +0000

    fix formatting

--- src/dbt/kipptaf/models/marts/fct_attendance.sql
+++ src/dbt/kipptaf/models/marts/fct_attendance.sql
@@ -1,48 +1,21 @@
-with
-    daily_attendance as (
-        select
-            student_number,
-            calendardate as attendance_date,
-            academic_year,
-            membershipvalue as membership_value,
-            attendancevalue as is_present,
-            is_absent,
-            is_present_weighted,
-            is_tardy,
-            is_ontime,
-            is_oss,
-            is_iss,
-            is_suspended,
-            is_truant,
-            semester,
-            term,
-            att_code as attendance_code,
-            {{ dbt_utils.generate_surrogate_key(["student_number", "calendardate"]) }}
-            as attendance_key,
-        from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }}
-    )
+select
+    student_number,
+    calendardate as attendance_date,
+    academic_year,
+    membershipvalue as membership_value,
+    attendancevalue as is_present,
+    is_absent,
+    is_present_weighted,
+    is_tardy,
+    is_ontime,
+    is_oss,
+    is_iss,
+    is_suspended,
+    is_truant,
+    semester,
+    term,
+    att_code as attendance_code,
 
-    final as (
-        select
-            student_number,
-            attendance_date,
-            academic_year,
-            membership_value,
-            is_present,
-            is_absent,
-            is_present_weighted,
-            is_tardy,
-            is_ontime,
-            is_oss,
-            is_iss,
-            is_suspended,
-            is_truant,
-            semester,
-            term,
-            attendance_code,
-            attendance_key,
-        from daily_attendance
-    )
-
-select *
-from final
+    {{ dbt_utils.generate_surrogate_key(["student_number", "calendardate"]) }}
+    as attendance_key,
+from {{ ref("int_powerschool__ps_adaadm_daily_ctod") }}
