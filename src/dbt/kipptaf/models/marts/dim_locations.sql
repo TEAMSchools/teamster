dff9309fc08caa12ccc764df76452d2a87cfec48
Cristina Baldor <ccbaldor@icloud.com>
2026-03-05 20:07:25 +0000

    removing is_campus filter to allow staff records

--- src/dbt/kipptaf/models/marts/dim_locations.sql
+++ src/dbt/kipptaf/models/marts/dim_locations.sql
@@ -13,7 +13,4 @@ select distinct
 
     {{ dbt_utils.generate_surrogate_key(["location_clean_name"]) }} as locations_key,
 from {{ ref("int_people__location_crosswalk") }}
-where
-    not location_is_campus
-    and not location_is_pathways
-    and location_clean_name <> 'KIPP Whittier Elementary'
+where and not location_is_pathways and location_clean_name <> 'KIPP Whittier Elementary'
