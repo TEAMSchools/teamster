76836e0970a1f5135c994b4a33712cc52ae4842d
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-03-08 19:24:13 +0000

    separate curernt and ever

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregated.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregated.sql
@@ -62,6 +62,59 @@ with
             and enrollment_type = 'New'
     )
 
+-- latest status
+select
+    s.academic_year,
+    s.org,
+    s.region,
+    s.school_level,
+    s.schoolid,
+    s.school,
+    s.grade_level,
+    s.goal_granularity,
+    s.goal_type,
+    s.goal_name,
+    s.goal_value,
+    s.grouped_status_timeframe,
+
+    f.enrollment_academic_year,
+    f.enrollment_academic_year_display,
+    f.finalsite_id,
+    f.powerschool_student_number,
+    f.first_name,
+    f.last_name,
+    f.grade_level as student_grade_level,
+    f.enroll_status,
+    f.birthdate,
+    f.gender,
+    f.grouped_status,
+    f.latest_status,
+    f.self_contained,
+    f.enrollment_type,
+    f.is_enrolled_fdos,
+    f.is_enrolled_oct01,
+    f.is_enrolled_oct15,
+    f.aligned_enrollment_type,
+    f.grouped_status_order,
+    f.grouped_status_start_date,
+    f.grouped_status_end_date,
+    f.days_in_grouped_status,
+
+from scaffold as s
+left join
+    {{ ref("int_tableau__finalsite_student_scaffold") }} as f
+    on s.academic_year = f.enrollment_academic_year
+    and s.region = f.region
+    and s.schoolid = f.schoolid
+    and s.grade_level = f.grade_level
+    and s.goal_type = f.goal_type
+    and s.goal_name = f.latest_status
+    and s.grouped_status_timeframe = f.grouped_status_timeframe
+where s.grouped_status_timeframe = 'Current'
+
+union all
+
+-- ever status
 select
     s.academic_year,
     s.org,
@@ -109,3 +162,4 @@ left join
     and s.goal_type = f.goal_type
     and s.goal_name = f.goal_name
     and s.grouped_status_timeframe = f.grouped_status_timeframe
+where s.grouped_status_timeframe = 'Ever'
