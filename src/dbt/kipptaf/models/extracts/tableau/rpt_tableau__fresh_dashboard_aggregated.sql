f3082decf370935f3185e7dd2e7a581041b0f77f
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-03-08 15:28:41 +0000

    conversions

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregated.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregated.sql
@@ -207,6 +207,66 @@ where
 
 union all
 
+-- current statuses with grouped
+select
+    s.academic_year,
+    s.org,
+    s.region,
+    s.school_level,
+    s.schoolid,
+    s.school,
+    s.grade_level,
+    s.goal_granularity,
+    s.goal_type,
+    s.goal_name,
+    s.goal_value,
+    s.grouped_status_timeframe,
+
+    f.enrollment_academic_year,
+    f.enrollment_academic_year_display,
+    f.finalsite_id,
+    f.powerschool_student_number,
+    f.first_name,
+    f.last_name,
+    f.grade_level as student_grade_level,
+    f.enroll_status,
+    f.birthdate,
+    f.gender,
+    f.grouped_status,
+    f.latest_status,
+    f.self_contained,
+    f.enrollment_type,
+    f.is_enrolled_fdos,
+    f.is_enrolled_oct01,
+    f.is_enrolled_oct15,
+    f.aligned_enrollment_type,
+
+    d.grouped_status_order,
+    d.grouped_status_start_date,
+    d.grouped_status_end_date,
+    d.days_in_grouped_status,
+
+    null as goal_name_value,
+
+from scaffold as s
+left join
+    {{ ref("int_tableau__finalsite_student_scaffold") }} as f
+    on s.academic_year = f.enrollment_academic_year
+    and s.region = f.region
+    and s.schoolid = f.schoolid
+    and s.grade_level = f.grade_level
+    and s.goal_type = f.grouped_status
+    and s.goal_name = f.grouped_status
+    and s.grouped_status_timeframe = f.grouped_status_timeframe
+left join
+    days_in_status as d
+    on f.enrollment_academic_year = d.enrollment_academic_year
+    and f.finalsite_id = d.finalsite_id
+    and f.grouped_status = d.grouped_status
+where s.grouped_status_timeframe = 'Current' and s.goal_name = 'Pending Offers'
+
+union all
+
 -- current statuses with grouped status for pending offers cats
 select
     s.academic_year,
@@ -267,3 +327,116 @@ left join
 where
     s.grouped_status_timeframe = 'Current'
     and s.goal_name in ('<= 4 Days', '>= 5 & <= 10 Days', '> 10 Days')
+
+union all
+
+-- ever statuses with grouped_status (goal type)
+select
+    s.academic_year,
+    s.org,
+    s.region,
+    s.school_level,
+    s.schoolid,
+    s.school,
+    s.grade_level,
+    s.goal_granularity,
+    s.goal_type,
+    s.goal_name,
+    s.goal_value,
+    s.grouped_status_timeframe,
+
+    f.enrollment_academic_year,
+    f.enrollment_academic_year_display,
+    f.finalsite_id,
+    f.powerschool_student_number,
+    f.first_name,
+    f.last_name,
+    f.grade_level as student_grade_level,
+    f.enroll_status,
+    f.birthdate,
+    f.gender,
+    f.grouped_status,
+    f.latest_status,
+    f.self_contained,
+    f.enrollment_type,
+    f.is_enrolled_fdos,
+    f.is_enrolled_oct01,
+    f.is_enrolled_oct15,
+    f.aligned_enrollment_type,
+
+    f.grouped_status_order,
+    f.grouped_status_start_date,
+    null as grouped_status_end_date,
+    null as days_in_grouped_status,
+
+    null as goal_name_value,
+
+from scaffold as s
+left join
+    {{ ref("int_tableau__finalsite_student_scaffold") }} as f
+    on s.academic_year = f.enrollment_academic_year
+    and s.region = f.region
+    and s.schoolid = f.schoolid
+    and s.grade_level = f.grade_level
+    and s.goal_type = f.grouped_status
+    and s.grouped_status_timeframe = f.grouped_status_timeframe
+where
+    s.grouped_status_timeframe = 'Ever'
+    and s.goal_name in ('Inquiries', 'Applications', 'Offers', 'Accepted')
+
+union all
+
+-- conversions
+select
+    s.academic_year,
+    s.org,
+    s.region,
+    s.school_level,
+    s.schoolid,
+    s.school,
+    s.grade_level,
+    s.goal_granularity,
+    s.goal_type,
+    s.goal_name,
+    s.goal_value,
+    s.grouped_status_timeframe,
+
+    f.enrollment_academic_year,
+    f.enrollment_academic_year_display,
+    f.finalsite_id,
+    f.powerschool_student_number,
+    f.first_name,
+    f.last_name,
+    f.grade_level as student_grade_level,
+    f.enroll_status,
+    f.birthdate,
+    f.gender,
+    f.grouped_status,
+    f.latest_status,
+    f.self_contained,
+    f.enrollment_type,
+    f.is_enrolled_fdos,
+    f.is_enrolled_oct01,
+    f.is_enrolled_oct15,
+    f.aligned_enrollment_type,
+
+    null as grouped_status_order,
+    null as grouped_status_start_date,
+    null as grouped_status_end_date,
+    null as days_in_grouped_status,
+
+    c.finalsite_id as goal_name_value,
+
+from scaffold as s
+left join
+    {{ ref("int_tableau__finalsite_student_scaffold") }} as f
+    on s.academic_year = f.enrollment_academic_year
+    and s.region = f.region
+    and s.school = f.school
+    and s.grade_level = f.grade_level
+    and s.goal_type = f.grouped_status
+left join
+    conversion_grouping_numerator as c
+    on f.enrollment_academic_year = c.enrollment_academic_year
+    and f.finalsite_id = c.finalsite_id
+where s.goal_type = 'Conversion'
