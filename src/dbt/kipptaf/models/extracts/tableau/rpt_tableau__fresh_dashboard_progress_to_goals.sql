bd3a606f6443a65c18e12a93732c45c9435d6aa7
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-03-11 11:05:52 +0000

    removed school scaffold in favor of CTE

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_progress_to_goals.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_progress_to_goals.sql
@@ -1,4 +1,55 @@
 with
+    scaffold as (
+        /* dont have a better location where only one schoolid matches a single school
+           name */
+        select distinct
+            s.academic_year,
+            s.org,
+            s.region,
+            s.schoolid,
+            s.school,
+            s.grade_level,
+
+            x.grade_band as school_level,
+
+            enrollment_type,
+
+            'School' as goal_granularity,
+
+        from {{ ref("stg_google_sheets__finalsite__school_scaffold") }} as s
+        left join
+            {{ ref("stg_google_sheets__people__location_crosswalk") }} as x
+            on s.schoolid = x.powerschool_school_id
+        cross join unnest(['All', 'New', 'Returning']) as enrollment_type
+        /* hardcoding year to avoid issues when PS rollsover and next year because
+           current year */
+        where s.academic_year = 2026 and s.grade_level = -1
+
+        union all
+
+        /* dont have a better location where only one schoolid matches a single school
+           name */
+        select distinct
+            s.academic_year,
+            s.org,
+            s.region,
+            s.schoolid,
+            s.school,
+            s.grade_level,
+
+            s.school_level,
+
+            enrollment_type,
+
+            'School/Grade Level' as goal_granularity,
+
+        from {{ ref("stg_google_sheets__finalsite__school_scaffold") }} as s
+        cross join unnest(['All', 'New', 'Returning']) as enrollment_type
+        /* hardcoding year to avoid issues when PS rollsover and next year because
+           current year */
+        where s.academic_year = 2026 and s.grade_level != -1 and s.schoolid != 0
+    ),
+
     data_stack_school as (
         -- PART 1A: THE STUDENTS (Actuals) by enroll type
         select
@@ -217,7 +268,7 @@ select
     d.new_student_target,
     d.re_enroll_projection,
 
-from {{ ref("int_tableau__finalsite_ptg_school_scaffold") }} as s
+from scaffold as s
 left join
     data_stack_school as d
     on s.academic_year = d.enrollment_academic_year
@@ -251,7 +302,7 @@ select
     d.new_student_target,
     d.re_enroll_projection,
 
-from {{ ref("int_tableau__finalsite_ptg_school_scaffold") }} as s
+from scaffold as s
 left join
     data_stack_school_grade as d
     on s.academic_year = d.enrollment_academic_year
