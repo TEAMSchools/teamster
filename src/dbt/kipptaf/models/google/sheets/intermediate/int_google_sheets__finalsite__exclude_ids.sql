2827c47a4d8b8ccc3750876fd163d4a18bdd28f0
Anthony Walters <awalters@kippteamandfamily.org>
2026-03-12 21:09:42 +0000

    tunk

--- src/dbt/kipptaf/models/google/sheets/intermediate/int_google_sheets__finalsite__exclude_ids.sql
+++ src/dbt/kipptaf/models/google/sheets/intermediate/int_google_sheets__finalsite__exclude_ids.sql
@@ -6,5 +6,4 @@ union distinct
 select text_value as finalsite_student_id,
 from {{ ref("int_google_forms__form_responses") }}
 where
-    form_id = '1iH4Cjy_RIc8TWJf6Ts0sxhpVmCnx4paiT5RSNOS2Rn4'
-    and text_value is not null
+    form_id = '1iH4Cjy_RIc8TWJf6Ts0sxhpVmCnx4paiT5RSNOS2Rn4' and text_value is not null
