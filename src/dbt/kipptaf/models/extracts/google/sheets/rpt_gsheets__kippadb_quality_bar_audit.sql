ab7b9665190b2b385d421c24e1621f2ea8e48324
Anthony Walters <awalters@kippteamandfamily.org>
2026-03-18 20:34:37 +0000

    trunk

--- src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__kippadb_quality_bar_audit.sql
+++ src/dbt/kipptaf/models/extracts/google/sheets/rpt_gsheets__kippadb_quality_bar_audit.sql
@@ -171,6 +171,5 @@ left join
     on a.applicant = og.student__external_student_id
     and acc.nces_id = og.university_ipeds_id_str
 left join
-    {{ ref("int_overgrad__choice_counts") }} as ogc
-    on a.applicant = ogc.contact_id
+    {{ ref("int_overgrad__choice_counts") }} as ogc on a.applicant = ogc.contact_id
 where a.application_submission_status in ('Wishlist', 'Submitted')
