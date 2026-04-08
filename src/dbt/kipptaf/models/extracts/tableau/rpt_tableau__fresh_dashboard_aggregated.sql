b0a54b07ed99992f43962bb54aed5661c416aaa4
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-04-08 18:22:58 +0000

    added union all

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregated.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__fresh_dashboard_aggregated.sql
@@ -63,6 +63,8 @@ left join
 where
     s.grouped_status_timeframe = 'Current' and s.goal_name in ('Deferred', 'Waitlisted')
 
+union all
+
 -- latest status: enrollment in progress
 select
     s.academic_year,
