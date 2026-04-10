9a9a06f12e1f794936a7ca1a25d26ef671af5c5d
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-04-10 17:29:12 +0000

    simplify demo gsheet

--- src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__state_test_comparison_demographics.sql
+++ src/dbt/kipptaf/models/google/sheets/staging/stg_google_sheets__state_test_comparison_demographics.sql
@@ -10,8 +10,19 @@ with
             aligned_test_code,
             region,
             comparison_entity,
-            comparison_demographic_group,
-            comparison_demographic_subgroup,
+
+            if(
+                comparison_demographic_group = 'Grade',
+                'Total',
+                comparison_demographic_group
+            ) as comparison_demographic_group,
+
+            if(
+                comparison_demographic_subgroup = 'Grade - 08',
+                'All Students',
+                comparison_demographic_subgroup
+            ) as comparison_demographic_subgroup,
+
             percent_proficient,
             total_students,
 
@@ -37,6 +48,7 @@ with
             aligned_test_code,
             region,
             comparison_entity,
+
             'Total' as comparison_demographic_group,
             'All Students' as comparison_demographic_subgroup,
 
@@ -50,7 +62,7 @@ with
                     "src_google_sheets__state_test_comparison_demographics",
                 )
             }}
-        where remove_row and aligned_test_code = 'ALG01'
+        where remove_row and aligned_test_code = 'ALG01' and grade_range_band = 'HS'
         group by
             academic_year,
             assessment_name,
@@ -66,22 +78,10 @@ select
     *,
 
     case
-        when aligned_test_code = 'ALG01' and school_level != 'MS_HS'
+        when aligned_test_code = 'ALG01'
         then concat(aligned_test_code, '_', school_level)
         else aligned_test_code
     end as aligned_level_test_code,
-
-    if(
-        comparison_demographic_subgroup in ('Grade - 08', 'Grade - 09', 'Grade - 10'),
-        'Total',
-        comparison_demographic_group
-    ) as aligned_comparison_demographic_group,
-
-    if(
-        comparison_demographic_group = 'Grade',
-        'All Students',
-        comparison_demographic_subgroup
-    ) as aligned_comparison_demographic_subgroup,
 
     round(percent_proficient * total_students, 0) as total_proficient_students,
 
