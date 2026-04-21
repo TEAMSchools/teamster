6a144fe8d7f57e761624826742cb62519b3c2ba5
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-04-21 20:03:44 +0000

    test(amplify): add relationships test on school_name for aimline int model

--- src/dbt/kipptaf/models/amplify/mclass/intermediate/int_amplify__mclass__sftp__pm_student_summary_aimline.sql
+++ src/dbt/kipptaf/models/amplify/mclass/intermediate/int_amplify__mclass__sftp__pm_student_summary_aimline.sql
@@ -1,20 +1,20 @@
 with
     transformations as (
         select
-            sa.* except (_dbt_source_relation),
+            s.* except (_dbt_source_relation),
 
             lc.region,
             lc.abbreviation as school,
             lc.powerschool_school_id as schoolid,
 
             regexp_replace(
-                sa._dbt_source_relation, r'kipp[a-z]+_', lc.dagster_code_location || '_'
+                s._dbt_source_relation, r'kipp[a-z]+_', lc.dagster_code_location || '_'
             ) as _dbt_source_relation,
 
-        from {{ ref("stg_amplify__mclass__sftp__pm_student_summary_aimline") }} as sa
+        from {{ ref("stg_amplify__mclass__sftp__pm_student_summary_aimline") }} as s
         left join
             {{ ref("stg_google_sheets__people__location_crosswalk") }} as lc
-            on sa.school_name = lc.name
+            on s.school_name = lc.name
     )
 
 select *,
