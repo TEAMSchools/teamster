1079a43ca4edb2bcfb48b8a49f3077149e38b5c2
Charlie Bini <5003326+cbini@users.noreply.github.com>
2026-05-08 18:26:31 +0000

    fix(dbt-powerschool): backtick-quote reserved word and drop wrapper CTE (#3756)

--- src/dbt/powerschool/models/sis/staging/odbc/stg_powerschool__u_expectations.sql
+++ src/dbt/powerschool/models/sis/staging/odbc/stg_powerschool__u_expectations.sql
@@ -1,19 +1,13 @@
-with
-    transformations as (
-        select
-            * except (dcid, id, quarter, week_number, cnt_w, cnt_h, cnt_f, cnt_s),
+select
+    * except (dcid, id, `quarter`, week_number, cnt_w, cnt_h, cnt_f, cnt_s),
 
-            /* column transformations */
-            dcid.int_value as dcid,
-            id.int_value as id,
-            quarter.int_value as quarter,
-            week_number.int_value as week_number,
-            cnt_w.int_value as cnt_w,
-            cnt_h.int_value as cnt_h,
-            cnt_f.int_value as cnt_f,
-            cnt_s.int_value as cnt_s,
-        from {{ source("powerschool_odbc", "src_powerschool__u_expectations") }}
-    )
-
-select *
-from transformations
+    /* column transformations */
+    dcid.int_value as dcid,
+    id.int_value as id,
+    `quarter`.int_value as `quarter`,
+    week_number.int_value as week_number,
+    cnt_w.int_value as cnt_w,
+    cnt_h.int_value as cnt_h,
+    cnt_f.int_value as cnt_f,
+    cnt_s.int_value as cnt_s,
+from {{ source("powerschool_odbc", "src_powerschool__u_expectations") }}
