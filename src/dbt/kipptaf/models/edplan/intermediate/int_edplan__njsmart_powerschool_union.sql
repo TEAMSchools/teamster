26915963e8e31fae1684f1f22afc6011eae2548e
Charlie Bini <5003326+cbini@users.noreply.github.com>
2026-05-21 18:37:33 +0000

    feat(dbt/kipptaf): materialize _dbt_source_project on int_edplan__njsmart_powerschool_union (#3960, refs #3142)

--- src/dbt/kipptaf/models/edplan/intermediate/int_edplan__njsmart_powerschool_union.sql
+++ src/dbt/kipptaf/models/edplan/intermediate/int_edplan__njsmart_powerschool_union.sql
@@ -1,8 +1,20 @@
-{{
-    dbt_utils.union_relations(
-        relations=[
-            source("kippnewark_edplan", "int_edplan__njsmart_powerschool_union"),
-            source("kippcamden_edplan", "int_edplan__njsmart_powerschool_union"),
-        ]
+with
+    unioned as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    source(
+                        "kippnewark_edplan",
+                        "int_edplan__njsmart_powerschool_union",
+                    ),
+                    source(
+                        "kippcamden_edplan",
+                        "int_edplan__njsmart_powerschool_union",
+                    ),
+                ]
+            )
+        }}
     )
-}}
+
+select *, regexp_extract(_dbt_source_relation, r'(kipp\w+)_') as _dbt_source_project,
+from unioned
