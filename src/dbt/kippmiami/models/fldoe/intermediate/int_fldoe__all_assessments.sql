123c0f4c1a528045f5c3a74b9d6f5b294783cbe8
Charlie Bini <5003326+cbini@users.noreply.github.com>
2026-06-08 16:47:29 +0000

    fix(dbt): use safe.parse_date for FLDOE test_date fallback

--- src/dbt/kippmiami/models/fldoe/intermediate/int_fldoe__all_assessments.sql
+++ src/dbt/kippmiami/models/fldoe/intermediate/int_fldoe__all_assessments.sql
@@ -32,7 +32,7 @@ with
             coalesce(student_id, fleid) as student_id,
 
             coalesce(
-                safe_cast(date_taken as date), parse_date('%m/%d/%Y', date_taken)
+                safe_cast(date_taken as date), safe.parse_date('%m/%d/%Y', date_taken)
             ) as test_date,
 
             regexp_extract(
