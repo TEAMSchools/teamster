730de84dd7313b075b9f85b63fc18fae72a7236a
grangel <140853376+GabyRangelB@users.noreply.github.com>
2026-06-22 19:11:36 +0000

    fix(kipptaf): remove stale assignment_category_code join in percent_graded_min_not_met agg

--- src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__gradebook_audit_flags_calculations.sql
+++ src/dbt/kipptaf/models/extracts/tableau/intermediate/int_tableau__gradebook_audit_flags_calculations.sql
@@ -35,6 +35,29 @@ with
             academic_year = {{ var("current_academic_year") - 1 }}
             and storecode_type = 'Q'
             and not is_transfer_grade
+    ),
+
+    section_category_quarterly_scores_agg as (
+        select
+            s._dbt_source_project,
+            s.sectionid,
+            s.sections_dcid,
+            s.quarter,
+
+            countif(a.is_expected) as n_expected,
+            countif(a.is_expected_scored) as n_expected_scored,
+
+        from {{ ref("int_tableau__gradebook_audit_scaffold") }} as s
+        left join
+            {{ ref("int_powerschool__gradebook_assignments_scores") }} as a
+            on s.sections_dcid = a.sectionsdcid
+            and s.assignment_category_code = a.category_code
+            and s._dbt_source_project = a._dbt_source_project
+            and a.duedate between s.quarter_start_date and s.quarter_end_date
+            and a.iscountedinfinalgrade = 1
+            and a.scoretype in ('POINTS', 'PERCENT')
+        where s.scaffold_name = 'teacher_scaffold_category'
+        group by s._dbt_source_project, s.sectionid, s.sections_dcid, s.quarter
     )
 
 -- student_course flags: qt_grade_70_comment_missing and qt_percent_grade_greater_100
@@ -165,6 +188,8 @@ select
     ) as qt_grade_70_comment_missing,
 
     null as teacher_running_total_assign_by_cat,
+
+    false as percent_graded_min_not_met,
 
 from {{ ref("int_tableau__gradebook_audit_scaffold") }} as s
 left join
@@ -307,6 +332,8 @@ select
     false as qt_grade_70_comment_missing,
 
     null as teacher_running_total_assign_by_cat,
+
+    false as percent_graded_min_not_met,
 
 from {{ ref("int_tableau__gradebook_audit_scaffold") }} as s
 left join
@@ -465,6 +492,8 @@ select
         partition by s._dbt_source_project, s.sectionid, s.assignment_category_term
     ) as teacher_running_total_assign_by_cat,
 
+    false as percent_graded_min_not_met,
+
 from {{ ref("int_tableau__gradebook_audit_scaffold") }} as s
 left join
     quarter_course_grades as qg
@@ -489,4 +518,158 @@ left join
     and s.assignment_category_name = a.category_name
     and a.duedate between s.quarter_start_date and s.quarter_end_date
     and s._dbt_source_project = a._dbt_source_project
+where s.scaffold_name = 'teacher_scaffold_category'
+
+union all
+
+/* class_category flags: percent_graded_min_not_met */
+select
+    s._dbt_source_project,
+    s.academic_year,
+    s.academic_year_display,
+    s.region_school_level,
+    s.school_level,
+    s.region,
+    s.schoolid,
+    s.school,
+
+    s.students_dcid,
+    s.studentid,
+    s.student_number,
+    s.student_name,
+    s.grade_level,
+    s.dateenrolled,
+    s.dateleft,
+    s.salesforce_id,
+    s.ktc_cohort,
+    s.enroll_status,
+    s.cohort,
+    s.gender,
+    s.ethnicity,
+    s.advisory,
+    s.year_in_school,
+    s.year_in_network,
+    s.rn_undergrad,
+    s.is_out_of_district,
+    s.is_self_contained,
+    s.is_retained_year,
+    s.is_retained_ever,
+    s.lunch_status,
+    s.gifted_and_talented,
+    s.iep_status,
+    s.lep_status,
+    s.is_504,
+    s.is_counseling_services,
+    s.is_student_athlete,
+    s.`ada`,
+    s.ada_above_or_at_80,
+
+    s.course_number,
+    s.course_name,
+    s.credit_type,
+    s.exclude_from_gpa,
+    s.sections_dcid,
+    s.sectionid,
+    s.section_number,
+    s.external_expression,
+    s.section_or_period,
+    s.teacher_number,
+    s.teacher_name,
+    s.school_leader,
+    s.manager_employee_number,
+    s.manager_name,
+    s.hos,
+
+    s.teacher_tableau_username,
+    s.manager_tableau_username,
+    s.school_leader_tableau_username,
+
+    s.quarter,
+    s.semester,
+    s.quarter_start_date,
+    s.quarter_end_date,
+    s.is_current_quarter,
+
+    s.scaffold_name,
+
+    -- these are NOT null
+    s.assignment_category_code,
+    s.assignment_category_name,
+    s.assignment_category_term,
+    s.expectation,
+    s.notes,
+
+    qg.quarter_course_percent_grade,
+    qg.quarter_course_grade_points,
+    qg.quarter_comment_value,
+
+    cg.percent_grade as category_quarter_percent_grade,
+
+    null as assignmentid,
+    null as assignment_name,
+    null as duedate,
+    null as scoretype,
+    null as totalpointvalue,
+
+    null as scorepoints,
+    null as is_expected_late,
+    null as is_exempt,
+    null as is_expected_missing,
+    null as is_expected_zero,
+    null as is_expected_academic_dishonesty,
+    null as score_entered,
+    null as assign_final_score_percent,
+
+    false as assign_null_score,
+    false as assign_score_above_max,
+    false as assign_w_score_less_5,
+    false as assign_h_score_less_5,
+    false as assign_f_score_less_5,
+    false as assign_w_missing_score_not_5,
+    false as assign_f_missing_score_not_5,
+    false as assign_h_missing_score_not_5,
+    false as assign_w_missing_score_not_0,
+    false as assign_f_missing_score_not_0,
+    false as assign_h_missing_score_not_0,
+    false as assign_s_missing_score_not_0,
+    false as assign_s_score_less_50p,
+    false as assign_s_hs_score_less_50p,
+
+    false as w_assign_max_score_not_10,
+    false as f_assign_max_score_not_10,
+    false as h_assign_max_score_not_10,
+
+    false as qt_percent_grade_greater_100,
+    false as qt_grade_70_comment_missing,
+
+    null as teacher_running_total_assign_by_cat,
+
+    if(
+        safe_divide(agg.n_expected_scored, agg.n_expected) < 0.9, true, false
+    ) as percent_graded_min_not_met,
+
+from {{ ref("int_tableau__gradebook_audit_scaffold") }} as s
+left join
+    quarter_course_grades as qg
+    on s.academic_year = qg.academic_year
+    and s.studentid = qg.studentid
+    and s.sectionid = qg.sectionid
+    and s._dbt_source_project = qg._dbt_source_project
+    and s.quarter = qg.storecode
+    and s.quarter_start_date <= current_date('{{ var("local_timezone") }}')
+    and qg.grades_type = 'current_year'  /* summer toggle: see skill */
+left join
+    {{ ref("int_powerschool__category_grades") }} as cg
+    on s.academic_year = cg.academic_year
+    and s.studentid = cg.studentid
+    and s.sectionid = cg.sectionid
+    and s._dbt_source_project = cg._dbt_source_project
+    and s.quarter = cg.storecode
+    and s.quarter_start_date <= current_date('{{ var("local_timezone") }}')
+left join
+    section_category_quarterly_scores_agg as agg
+    on s.sections_dcid = agg.sections_dcid
+    and s._dbt_source_project = agg._dbt_source_project
+    and s.sectionid = agg.sectionid
+    and s.quarter = agg.quarter
 where s.scaffold_name = 'teacher_scaffold_category'
