9d4a6ef6ec4fe37a8e9186afbe58131434a6a656
Charlie Bini <5003326+cbini@users.noreply.github.com>
2026-07-02 20:05:42 +0000

    feat(kipptaf): route Illuminate Paterson roles through the roster

--- src/dbt/kipptaf/models/extracts/illuminate/rpt_illuminate__roles.sql
+++ src/dbt/kipptaf/models/extracts/illuminate/rpt_illuminate__roles.sql
@@ -75,33 +75,10 @@ where
     worker_status_code != 'Terminated'
     and home_department_name not in ('Teaching and Learning', 'Data', 'Executive')
     and not home_work_location_is_campus
-
-union all
-
-/* Paterson = all schools in region */
-select
-    -- trunk-ignore-begin(sqlfluff/RF05)
-    u.teachernumber as `01 Local User ID`,
-
-    s.school_number as `02 Site ID`,
-
-    'School Leadership' as `03 Role Name`,
-
-    concat(
-        {{ current_school_year(var("local_timezone")) }},
-        '-',
-        {{ current_school_year(var("local_timezone")) }} + 1
-    ) as `04 Academic Year`,
-
-    1 as `05 Session Type ID`,
--- trunk-ignore-end(sqlfluff/RF05)
-from {{ ref("stg_powerschool__users") }} as u
-inner join
-    {{ ref("stg_powerschool__schools") }} as s
-    on {{ union_dataset_join_clause(left_alias="u", right_alias="s") }}
-    and s.state_excludefromreporting = 0
-where
-    u._dbt_source_relation like '%kipppaterson%' and (u.ptaccess = 1 or u.psaccess = 1)
+    and not (
+        home_business_unit_name = 'KIPP Paterson'
+        and home_work_location_powerschool_school_id = 0
+    )
 
 union all
 
@@ -122,3 +99,32 @@ select
     1 as `05 Session Type ID`,
 -- trunk-ignore-end(sqlfluff/RF05)
 from {{ ref("int_people__temp_staff") }}
+
+union all
+
+/* Paterson regional/CMO staff = all Paterson schools */
+select
+    -- trunk-ignore-begin(sqlfluff/RF05)
+    sr.powerschool_teacher_number as `01 Local User ID`,
+
+    s.school_number as `02 Site ID`,
+
+    'School Leadership' as `03 Role Name`,
+
+    concat(
+        {{ current_school_year(var("local_timezone")) }},
+        '-',
+        {{ current_school_year(var("local_timezone")) }} + 1
+    ) as `04 Academic Year`,
+
+    1 as `05 Session Type ID`,
+-- trunk-ignore-end(sqlfluff/RF05)
+from {{ ref("int_people__staff_roster") }} as sr
+inner join
+    {{ ref("stg_powerschool__schools") }} as s
+    on s.state_excludefromreporting = 0
+    and s._dbt_source_relation like '%kipppaterson%'
+where
+    sr.worker_status_code != 'Terminated'
+    and sr.home_business_unit_name = 'KIPP Paterson'
+    and sr.home_work_location_powerschool_school_id = 0
