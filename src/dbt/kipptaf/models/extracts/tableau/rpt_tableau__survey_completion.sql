07078ac123750cab11c91e3bf917a55e8388c1c7
Cristina Baldor <ccbaldor@icloud.com>
2026-07-30 21:09:47 +0000

    feat(dbt): apply tableau access contract to rpt_tableau__survey_completion
    Refs #4638

--- src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__survey_completion.sql
+++ src/dbt/kipptaf/models/extracts/tableau/rpt_tableau__survey_completion.sql
@@ -21,25 +21,53 @@ with
         }}
     )
 
+-- trunk-ignore(sqlfluff/ST06): contract column order is mandated
 select
     sl.employee_number,
     sl.assignment_status,
     sl.preferred_name_lastfirst,
-    sl.business_unit,
-    sl.location,
-    sl.department,
-    sl.job_title,
     sl.hire_date,
     sl.survey,
     sl.academic_year,
     sl.survey_round,
     sl.is_current,
-    sl.mail,
+
+    lc.location_clean_name,
+    lc.campus_name,
+
+    case
+        sr2.home_business_unit_name
+        when 'TEAM'
+        then 'TEAM Academy Charter School'
+        when 'KCNA'
+        then 'KIPP Cooper Norcross Academy'
+        when 'MIA'
+        then 'KIPP Miami'
+        when 'KNJ'
+        then 'KIPP TEAM and Family Schools Inc.'
+        else sr2.home_business_unit_name
+    end as home_business_unit_name,
+    sr2.home_department_name,
+    sr2.job_function,
+    sr2.job_title,
+
+    sr2.mail,
+    sr2.user_principal_name,
+    sr2.sam_account_name,
+
+    sr2.reports_to_mail,
+    sr2.reports_to_sam_account_name,
 
     sr.survey_response_id,
 
     if(sr.survey_response_id is not null, 1, 0) as completion,
 from {{ ref("rpt_tableau__survey_links") }} as sl
+left join
+    {{ ref("int_people__staff_roster") }} as sr2
+    on sl.employee_number = sr2.employee_number
+left join
+    {{ ref("int_people__location_crosswalk") }} as lc
+    on sr2.home_work_location_name = lc.location_name
 left join
     deduplicate as sr
     on sl.employee_number = sr.employee_number
